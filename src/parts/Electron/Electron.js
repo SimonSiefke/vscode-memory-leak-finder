3679b9e646f0ca7ddc5baef0889edaf5df2f57e5
Simon Siefke <simon.siefke@gmail.com>
2022-04-18 11:19:54 +0200

    pass custom user data dir to tests

--- src/parts/Electron/Electron.js
+++ src/parts/Electron/Electron.js
@@ -4,10 +4,11 @@ import * as TmpDir from "../TmpDir/TmpDir.js";
 
 export const launch = async () => {
   const tmpDir = await TmpDir.create();
+  const userDataDir = await TmpDir.create();
   const binaryPath = await Platform.getBinaryPath();
   console.log({ binaryPath });
   const child = await _electron.launch({
-    args: ["--wait", "--new-window", "."],
+    args: ["--wait", "--new-window", "--user-data-dir", userDataDir, "."],
     cwd: tmpDir,
     executablePath: binaryPath,
   });
