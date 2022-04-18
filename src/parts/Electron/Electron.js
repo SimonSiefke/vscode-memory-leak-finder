e5bfbf79912debb5c873a9e0ec209fa86e124d80
Simon Siefke <simon.siefke@gmail.com>
2022-04-18 09:16:47 +0200

    add working getWebsocket function

--- src/parts/Electron/Electron.js
+++ src/parts/Electron/Electron.js
@@ -1,6 +1,7 @@
 import * as Platform from "../Platform/Platform.js";
 import * as TmpDir from "../TmpDir/TmpDir.js";
 import { spawn } from "node:child_process";
+import pTimeout from "p-timeout";
 
 const getWebSocketUrl = (data) => {
   return data.trim().slice("DevTools listening on ".length);
@@ -9,8 +10,9 @@ const getWebSocketUrl = (data) => {
 export const launch = async () => {
   const tmpDir = await TmpDir.create();
   const binaryPath = Platform.getBinaryPath();
-  const args = ["--wait", "--remote-debugging-port=0", tmpDir];
+  const args = ["--wait", "--remote-debugging-port=0", "--new-window", "."];
   const child = spawn(binaryPath, args, {
+    cwd: tmpDir,
     stdio: "pipe",
     env: {
       ...process.env,
@@ -28,15 +30,17 @@ export const launch = async () => {
     child.kill();
   });
 
-  const line = await new Promise((resolve, reject) => {
-    child.stderr.once("data", (data) => {
+  const linePromise = new Promise((resolve, reject) => {
+    const handleData = (data) => {
+      console.log({ data: data.toString() });
       if (data.toString().includes("DevTools listening on ws")) {
+        child.stderr.off("data", handleData);
         resolve(data.toString());
-      } else {
-        reject(new Error("expected websocket url"));
       }
-    });
+    };
+    child.stderr.on("data", handleData);
   });
+  const line = await pTimeout(linePromise, 1750);
   const webSocketUrl = getWebSocketUrl(line);
   return webSocketUrl;
 };
