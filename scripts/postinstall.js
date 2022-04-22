25eda4aececfcb8fbf5c0d0c356dbf8e69c2ba05
Simon Siefke <simon.siefke@gmail.com>
2022-04-22 22:12:31 +0200

    try to fix macos

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,17 +3,45 @@ import { readFileSync } from "fs";
 import { readdir, writeFile } from "fs/promises";
 import { dirname, join } from "path";
 
+const isWindows = () => {
+  return process.platform === "win32";
+};
+
+const isLinux = () => {
+  process.platform === "linux";
+};
+
+const isMacos = () => {};
+
+const getPlatform = () => {
+  switch (process.platform) {
+    case "win32":
+      return "windows";
+    case "linux":
+      return "linux";
+    case "darwin":
+      return "macos";
+    default:
+      throw new Error(`unsupported platform "${process.platform}"`);
+  }
+};
+
+const getPackageJsonPath = (path) => {
+  const platform = getPlatform();
+  switch (platform) {
+    case "linux":
+      return join(dirname(path), "resources", "app", "package.json");
+    case "macos":
+      return join(dirname(dirname(path)), "Resources", "app", "package.json");
+    case "windows":
+      throw new Error("not yet supported");
+  }
+};
+
 const main = async () => {
   const path = await downloadAndUnzipVSCode("1.66.2");
-  const packageJsonPath = join(
-    dirname(path),
-    "resources",
-    "app",
-    "package.json"
-  );
-  console.log(await readdir(dirname(dirname(dirname(packageJsonPath)))));
-  console.log(await readdir(dirname(dirname(packageJsonPath))));
-  console.log(await readdir(dirname(packageJsonPath)));
+  const packageJsonPath = getPackageJsonPath(path);
+  console.log({ packageJsonPath });
   const originalContent = readFileSync(packageJsonPath, "utf-8");
   const originalParsed = JSON.parse(originalContent);
   const newParsed = {
