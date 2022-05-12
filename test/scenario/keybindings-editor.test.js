8404880c1dc558f55f5768558c1f7cb45a2c4497
Simon Siefke <simon.siefke@gmail.com>
2022-05-12 14:42:58 +0200

    disable flaky test

--- test/scenario/keybindings-editor.test.js
+++ test/scenario/keybindings-editor.test.js
@@ -10,7 +10,8 @@ afterAll(async () => {
   await runner.kill();
 });
 
-test("keybindings-editor", async () => {
+// TODO test is flaky https://github.com/SimonSiefke/vscode-memory-leak-finder/runs/6405988542?check_suite_focus=true
+test.skip("keybindings-editor", async () => {
   await runner.waitForSucceeded();
   expect(runner.stdout).toContainEqual(
     expect.stringContaining(`event listener equal:`)
