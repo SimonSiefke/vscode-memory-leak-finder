57701b0247e8a4a0b15a4f6216d6767a3c8ea0ba
Simon Siefke <simon.siefke@gmail.com>
2023-01-11 16:03:56 +0100

    add more reliable caching for github actions (#19)

---
+++ scripts/computeNodeModulesCacheKey.js
@@ -0,0 +1,50 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const root = join(__dirname, "..");
+
+const locations = [
+  "package-lock.json",
+  ".github/workflows/ci.yml",
+  ".github/workflows/release.yml",
+  "scripts/computeNodeModulesCacheKey.js",
+  "src/parts/VsCodeVersion/VsCodeVersion.js",
+];
+
+const getAbsolutePath = (relativePath) => {
+  return join(root, relativePath);
+};
+
+const getContent = (absolutePath) => {
+  return readFile(absolutePath, "utf8");
+};
+
+export const computeHash = (contents) => {
+  const hash = createHash("sha1");
+  if (Array.isArray(contents)) {
+    for (const content of contents) {
+      hash.update(content);
+    }
+  } else if (typeof contents === "string") {
+    hash.update(contents);
+  }
+  return hash.digest("hex");
+};
+
+const computeCacheKey = async (locations) => {
+  const absolutePaths = locations.map(getAbsolutePath);
+  const contents = await Promise.all(absolutePaths.map(getContent));
+  const hash = computeHash(contents);
+  return hash;
+};
+
+const main = async () => {
+  const hash = await computeCacheKey(locations);
+  process.stdout.write(hash);
+};
+
+main();
