432daf712bde48138e76a3e70e826b436a1164a7
Simon Siefke <simon.siefke@gmail.com>
2023-01-11 21:54:56 +0100

    add workaround for lerna bug

---
+++ scripts/postinstall.js
@@ -0,0 +1,22 @@
+import { execa } from "execa";
+
+const exec = async (command) => {
+  console.info(command);
+  await execa(command, {
+    shell: true,
+    stdio: "inherit",
+  });
+};
+
+// prettier-ignore
+const applyLernaBugWorkaround =async  () => {
+  // workaround for https://github.com/lerna/lerna/issues/2352
+  await exec(`cd packages/core && npm ci --prefer-offline && cd ../../`);
+  await exec(`cd packages/e2e && npm ci --prefer-offline && cd ../../`);
+}
+
+const main = async () => {
+  await applyLernaBugWorkaround();
+};
+
+main();
