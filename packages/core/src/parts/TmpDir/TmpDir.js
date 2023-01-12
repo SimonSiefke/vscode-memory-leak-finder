e7a109706c7b1f18186b0091ad8cb507bc9fddf4
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 22:29:45 +0100

    remove unused comment (#51)

--- packages/core/src/parts/TmpDir/TmpDir.js
+++ packages/core/src/parts/TmpDir/TmpDir.js
@@ -1,5 +1,3 @@
-// For more information on chrome devtools protocol, see https://github.com/aslushnikov/getting-started-with-cdp/blob/master/README.md
-
 import { mkdtemp } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
