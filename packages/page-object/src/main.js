44249644ff19811db12cf096de2faa9b5edc49c8
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 10:04:31 +0100

    add title bar helper functions (#30)

--- packages/page-object/src/main.js
+++ packages/page-object/src/main.js
@@ -6,6 +6,7 @@ import * as QuickPick from "./parts/QuickPick/QuickPick.js";
 import * as Search from "./parts/Search/Search.js";
 import * as SideBar from "./parts/SideBar/SideBar.js";
 import * as Suggest from "./parts/Suggest/Suggest.js";
+import * as TitleBar from "./parts/TitleBar/TitleBar.js";
 
 export const create = ({ page, expect, VError }) => {
   return {
@@ -17,5 +18,6 @@ export const create = ({ page, expect, VError }) => {
     Search: Search.create({ page, expect, VError }),
     SideBar: SideBar.create({ page, expect, VError }),
     Suggest: Suggest.create({ page, expect, VError }),
+    TitleBar: TitleBar.create({ page, expect, VError }),
   };
 };
