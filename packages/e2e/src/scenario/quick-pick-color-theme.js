a3893aaa760d0d85e3ffbc9989f14441de75c859
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 08:41:18 +0100

    add helper functions for quick pick color theme

--- packages/e2e/src/scenario/quick-pick-color-theme.js
+++ packages/e2e/src/scenario/quick-pick-color-theme.js
@@ -1,27 +1,15 @@
-export const setup = async ({ page, expect }) => {
-  await page.keyboard.press("Control+Shift+P");
-  const quickPick = page.locator(".quick-input-widget");
-  await expect(quickPick).toBeVisible();
-  const quickPickInput = quickPick.locator('[role="combobox"]');
-  await quickPickInput.type("Preferences: Color Theme");
-  const firstOption = quickPick.locator(".monaco-list-row").first();
-  await firstOption.click();
+export const setup = async ({ QuickPick }) => {
+  await QuickPick.showColorTheme();
 };
 
-export const run = async ({ page, expect }) => {
-  const quickPick = page.locator(".quick-input-widget");
-  const quickPickInput = quickPick.locator('[role="combobox"]');
-  await expect(quickPickInput).toHaveText("");
+export const run = async ({ page, expect, QuickPick }) => {
   const workbench = page.locator(".monaco-workbench");
   await expect(workbench).toHaveCSS("--vscode-editor-background", " #1e1e1e");
-
-  await page.keyboard.press("ArrowDown");
+  await QuickPick.focusNext();
   await expect(workbench).toHaveCSS("--vscode-editor-background", " #221a0f");
-
-  await page.keyboard.press("ArrowDown");
+  await QuickPick.focusNext();
   await expect(workbench).toHaveCSS("--vscode-editor-background", " #272822");
-
-  await page.keyboard.press("ArrowUp");
-  await page.keyboard.press("ArrowUp");
+  await QuickPick.focusPrevious();
+  await QuickPick.focusPrevious();
   await expect(workbench).toHaveCSS("--vscode-editor-background", " #1e1e1e");
 };
