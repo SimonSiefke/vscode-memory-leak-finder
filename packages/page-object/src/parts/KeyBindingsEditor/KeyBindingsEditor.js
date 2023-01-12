5b143da72fd3ae4d3493cdbf0e52dfcc76464a71
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 11:20:48 +0100

    simplify keybindings editor test

---
+++ packages/page-object/src/parts/KeyBindingsEditor/KeyBindingsEditor.js
@@ -0,0 +1,48 @@
+const getKeybindingButtonsText = (keyBinding) => {
+  if (keyBinding.startsWith("Control+")) {
+    return `Ctrl+${keyBinding.slice("Control+".length)}`;
+  }
+  return keyBinding;
+};
+
+export const create = ({ expect, page, VError }) => {
+  return {
+    async show() {
+      try {
+        await page.keyboard.press("Control+P");
+        const quickPick = page.locator(".quick-input-widget");
+        await expect(quickPick).toBeVisible();
+        const quickPickInput = quickPick.locator('[role="combobox"]');
+        await quickPickInput.type("> open keyboard shortcuts");
+        const firstOption = quickPick.locator(".monaco-list-row").nth(1);
+        await firstOption.click();
+        const keyBindingsEditor = page.locator(".keybindings-editor");
+        await expect(keyBindingsEditor).toBeVisible();
+      } catch (error) {
+        throw new VError(error, `Failed to show keybindings editor`);
+      }
+    },
+    async setKeyBinding(commandName, keyBinding) {
+      try {
+        const keyBindingsEditor = page.locator(".keybindings-editor");
+        await expect(keyBindingsEditor).toBeVisible();
+        const row = keyBindingsEditor
+          .locator(`.monaco-list-row[aria-label^="${commandName}"]`)
+          .first();
+        await row.dblclick();
+        const defineKeyBindingWidget = page.locator(".defineKeybindingWidget");
+        await expect(defineKeyBindingWidget).toBeVisible();
+        const defineKeyBindingInput = defineKeyBindingWidget.locator("input");
+        await expect(defineKeyBindingInput).toBeFocused();
+        await page.keyboard.press(keyBinding);
+        await page.keyboard.press("Enter");
+        await expect(defineKeyBindingWidget).toBeHidden();
+        const keyBindingsButtonText = getKeybindingButtonsText(keyBinding);
+        const rowKeybinding = row.locator(".keybinding");
+        await expect(rowKeybinding).toHaveText(keyBindingsButtonText);
+      } catch (error) {
+        throw new VError(error, `Failed to type into search input`);
+      }
+    },
+  };
+};
