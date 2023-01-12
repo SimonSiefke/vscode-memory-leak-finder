350a337d095779c21b247104bf80e7776e1d6003
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 07:55:45 +0100

    add activity bar helper functions (#29)

--- packages/page-object/src/main.js
+++ packages/page-object/src/main.js
@@ -1,3 +1,4 @@
+import * as ActivityBar from "./parts/ActivityBar/ActivityBar.js";
 import * as Editor from "./parts/Editor/Editor.js";
 import * as Explorer from "./parts/Explorer/Explorer.js";
 import * as Panel from "./parts/Panel/Panel.js";
@@ -5,6 +6,7 @@ import * as Suggest from "./parts/Suggest/Suggest.js";
 
 export const create = ({ page, expect, VError }) => {
   return {
+    ActivityBar: ActivityBar.create({ page, expect, VError }),
     Editor: Editor.create({ page, expect, VError }),
     Explorer: Explorer.create({ page, expect, VError }),
     Suggest: Suggest.create({ page, expect, VError }),
