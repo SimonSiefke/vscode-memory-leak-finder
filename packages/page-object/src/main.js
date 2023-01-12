a015ee9e75dd3b4421257c7af09d0103e5e7aa5e
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 07:48:35 +0100

    add panel helper functions (#28)

--- packages/page-object/src/main.js
+++ packages/page-object/src/main.js
@@ -1,5 +1,6 @@
 import * as Editor from "./parts/Editor/Editor.js";
 import * as Explorer from "./parts/Explorer/Explorer.js";
+import * as Panel from "./parts/Panel/Panel.js";
 import * as Suggest from "./parts/Suggest/Suggest.js";
 
 export const create = ({ page, expect, VError }) => {
@@ -7,5 +8,6 @@ export const create = ({ page, expect, VError }) => {
     Editor: Editor.create({ page, expect, VError }),
     Explorer: Explorer.create({ page, expect, VError }),
     Suggest: Suggest.create({ page, expect, VError }),
+    Panel: Panel.create({ page, expect, VError }),
   };
 };
