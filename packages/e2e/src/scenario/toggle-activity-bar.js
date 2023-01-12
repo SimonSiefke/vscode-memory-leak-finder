5b4830388a48e6e6a39fbc5a335557dd279f0f18
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 07:51:34 +0100

    add activity bar helper functions

--- packages/e2e/src/scenario/toggle-activity-bar.js
+++ packages/e2e/src/scenario/toggle-activity-bar.js
@@ -1,19 +1,4 @@
-export const run = async ({ page, expect }) => {
-  const activityBar = page.locator(".part.activitybar");
-  await expect(activityBar).toBeVisible();
-
-  await page.keyboard.press("Control+Shift+P");
-  const quickPick = page.locator(".quick-input-widget");
-  await expect(quickPick).toBeVisible();
-  const quickPickInput = quickPick.locator('[role="combobox"]');
-  await quickPickInput.type("Toggle Activity Bar Visibility");
-  const firstOption = quickPick.locator(".monaco-list-row").first();
-  await firstOption.click();
-  await expect(activityBar).toBeHidden();
-
-  await page.keyboard.press("Control+Shift+P");
-  await expect(quickPick).toBeVisible();
-  await quickPickInput.type("Toggle Activity Bar Visibility");
-  await firstOption.click();
-  await expect(activityBar).toBeVisible();
+export const run = async ({ ActivityBar }) => {
+  await ActivityBar.hide();
+  await ActivityBar.show();
 };
