e5fde68259ffd5411437c38df66a5df0be27176b
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 10:47:55 +0100

    enable keybindings editor e2e test

--- packages/e2e/test/scenario/keybindings-editor.test.js
+++ packages/e2e/test/scenario/keybindings-editor.test.js
@@ -10,10 +10,9 @@ afterAll(async () => {
   await runner.kill();
 });
 
-// TODO test is flaky https://github.com/SimonSiefke/vscode-memory-leak-finder/runs/6405988542?check_suite_focus=true
-test.skip("keybindings-editor", async () => {
+test("keybindings-editor", async () => {
   await runner.waitForSucceeded();
   expect(runner.stdout).toContainEqual(
-    expect.stringContaining(`event listener equal:`)
+    expect.stringContaining(`event listener increase:`)
   );
 });
