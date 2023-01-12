31c06945b0efc94fd47f55d85a069f55649ee32f
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 08:52:24 +0100

    refactor: use explorer focus function to simplify code (#35)

--- packages/e2e/src/scenario/explorer-expand-and-collapse.js
+++ packages/e2e/src/scenario/explorer-expand-and-collapse.js
@@ -6,25 +6,15 @@ export const beforeSetup = async ({ tmpDir, writeFile, mkdir, join }) => {
   await writeFile(join(tmpDir, "folder", `file-4.txt`), `file content 4`);
 };
 
-export const run = async ({ page, expect }) => {
-  await page.keyboard.press("Control+Shift+P");
-  const quickPick = page.locator(".quick-input-widget");
-  await expect(quickPick).toBeVisible();
-  const quickPickInput = quickPick.locator('[role="combobox"]');
-  await quickPickInput.type("Focus Explorer");
-  const firstOption = quickPick.locator(".monaco-list-row").first();
-  await firstOption.click();
+export const run = async ({ page, expect, Explorer }) => {
+  await Explorer.focus();
   const explorer = page.locator(".explorer-folders-view .monaco-list");
-  await expect(explorer).toBeFocused();
-
   const folder = explorer.locator(".monaco-list-row", { hasText: "folder" });
   await folder.click();
-
   const file3 = explorer.locator(".monaco-list-row", { hasText: "file-3.txt" });
   await expect(file3).toBeVisible();
   const file4 = explorer.locator(".monaco-list-row", { hasText: "file-4.txt" });
   await expect(file4).toBeVisible();
-
   await folder.click();
   await expect(file3).toBeHidden();
   await expect(file4).toBeHidden();
