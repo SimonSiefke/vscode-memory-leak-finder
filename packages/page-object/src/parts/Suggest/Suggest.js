3166e2e27f1ad5982658105ce2ffde4a1164d865
Simon Siefke <simon.siefke@gmail.com>
2023-01-12 15:14:49 +0100

    increase timeout for opening completions

--- packages/page-object/src/parts/Suggest/Suggest.js
+++ packages/page-object/src/parts/Suggest/Suggest.js
@@ -2,13 +2,17 @@ import * as Platform from "../Platform/Platform.js";
 
 const modifier = Platform.isMacos ? "Meta" : "Control";
 
+const TIMEOUT_COMPLETION_OPEN = 30_000;
+
 export const create = ({ page, expect, VError }) => {
   return {
     async open() {
       try {
         await page.keyboard.press(`${modifier}+Space`);
         const suggestWidget = page.locator(".suggest-widget");
-        await expect(suggestWidget).toBeVisible();
+        await expect(suggestWidget).toBeVisible({
+          timeout: TIMEOUT_COMPLETION_OPEN,
+        });
       } catch (error) {
         throw new VError(error, `Failed to open suggest widget`);
       }
