02d5c53896c313b4155a1779a0f2b26f3605233e
Simon Siefke <simon.siefke@gmail.com>
2023-06-15 00:58:27 +0200

    refactor: simplify editor selection test (#68)

--- packages/e2e/src/scenario/editor-selection.js
+++ packages/e2e/src/scenario/editor-selection.js
@@ -2,30 +2,13 @@ export const beforeSetup = async ({ tmpDir, writeFile, join }) => {
   await writeFile(join(tmpDir, "index.html"), "<h1>hello world</h1>");
 };
 
-export const setup = async ({ page, expect }) => {
-  await page.keyboard.press("Control+P");
-  const quickPick = page.locator(".quick-input-widget");
-  await expect(quickPick).toBeVisible();
-  const quickPickInput = quickPick.locator('[role="combobox"]');
-  await quickPickInput.type("index");
-  const firstOption = quickPick.locator(".monaco-list-row").first();
-  await firstOption.click();
-  const editor = page.locator(".editor-instance");
-  await expect(editor).toBeVisible();
+export const setup = async ({ Editor }) => {
+  await Editor.open("index.html");
 };
 
-export const run = async ({ page, expect }) => {
-  const editor = page.locator(".editor-instance");
-  await expect(editor).toBeVisible();
-
-  const startTag = editor.locator('[class^="mtk"]', { hasText: "h1" }).first();
-  await startTag.dblclick();
-
-  const selection = page.locator(".selected-text");
-  await expect(selection).toBeVisible();
-  await expect(selection).toHaveCSS("left", "8px");
-  await expect(selection).toHaveCSS("width", /(15px|17px)/);
-
-  await page.keyboard.press("ArrowRight");
-  await expect(selection).toBeHidden();
+export const run = async ({ Editor }) => {
+  await Editor.select("h1");
+  await Editor.shouldHaveSelection("8px", /(15px|17px)/);
+  await Editor.cursorRight();
+  await Editor.shouldHaveEmptySelection();
 };
