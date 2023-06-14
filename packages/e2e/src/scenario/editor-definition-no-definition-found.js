a1f432b2527cd7ffd9df866cc11d7d90940443d7
Simon Siefke <simon.siefke@gmail.com>
2023-06-15 01:18:58 +0200

    refactor: simplify editor definition test (#70)

--- packages/e2e/src/scenario/editor-definition-no-definition-found.js
+++ packages/e2e/src/scenario/editor-definition-no-definition-found.js
@@ -2,35 +2,17 @@ export const beforeSetup = async ({ tmpDir, writeFile, join }) => {
   await writeFile(join(tmpDir, "index.html"), "<h1>hello world</h1>");
 };
 
-export const setup = async ({ page, expect }) => {
-  await page.keyboard.press("Control+P");
-  const quickPick = page.locator(".quick-input-widget");
-  await expect(quickPick).toBeVisible();
-  const quickPickInput = quickPick.locator('[role="combobox"]');
-  await quickPickInput.type("index");
-  const firstOption = quickPick.locator(".monaco-list-row").first();
-  await firstOption.click();
-  const editor = page.locator(".editor-instance");
-  await expect(editor).toBeVisible();
+export const setup = async ({ Editor }) => {
+  await Editor.open("index.html");
 };
 
-export const run = async ({ page, expect }) => {
+export const run = async ({ page, expect, Editor }) => {
   const editor = page.locator(".editor-instance");
   await expect(editor).toBeVisible();
 
   const startTag = editor.locator('[class^="mtk"]', { hasText: "h1" }).first();
   await startTag.click();
 
-  await page.keyboard.press("Control+Shift+P");
-  const quickPick = page.locator(".quick-input-widget");
-  await expect(quickPick).toBeVisible();
-  const quickPickInput = quickPick.locator('[role="combobox"]');
-  await quickPickInput.type("Go to Definition");
-  const firstOption = quickPick.locator(".monaco-list-row").first();
-  await expect(firstOption).toContainText("Go to Definition");
-  await firstOption.click();
-
-  const message = page.locator(".monaco-editor-overlaymessage");
-  await expect(message).toBeVisible();
-  await expect(message).toHaveText("No definition found for 'h1'");
+  await Editor.goToDefinition("No definition found for 'h1'");
+  await Editor.shouldHaveOverlayMessage();
 };
