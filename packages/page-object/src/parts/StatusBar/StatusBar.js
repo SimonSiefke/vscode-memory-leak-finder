e08f2a81217ab02e301c5c4b0049db772b2b4086
Simon Siefke <simon.siefke@gmail.com>
2023-06-15 22:39:06 +0200

    feature: add toggle status bar problems e2e test

--- packages/page-object/src/parts/StatusBar/StatusBar.js
+++ packages/page-object/src/parts/StatusBar/StatusBar.js
@@ -1,3 +1,6 @@
+// TODO avoid using timeout
+const SHORT_TIMEOUT = 250;
+
 export const create = ({ page, VError, expect }) => {
   return {
     async click(label) {
@@ -20,9 +23,51 @@ export const create = ({ page, VError, expect }) => {
           async click() {
             await item.click();
           },
+          async hide() {
+            await item.click({
+              button: "right",
+            });
+            const contextMenu = page.locator(
+              ".context-view.monaco-menu-container .actions-container"
+            );
+            await expect(contextMenu).toBeVisible();
+            await expect(contextMenu).toBeFocused();
+            const contextMenuItem = contextMenu
+              .locator(".action-item", {
+                hasText: "Hide 'Problems'",
+              })
+              .first();
+            await expect(contextMenuItem).toBeVisible();
+            await contextMenuItem.focus();
+            await page.waitForTimeout(SHORT_TIMEOUT);
+            await contextMenuItem.click({ force: true });
+            await expect(contextMenu).toBeHidden();
+            await expect(item).toBeHidden();
+          },
+          async show() {
+            await expect(item).toBeHidden();
+            const statusBar = page.locator(".part.statusbar");
+            await statusBar.click({
+              button: "right",
+            });
+            const contextMenu = page.locator(
+              ".context-view.monaco-menu-container .actions-container"
+            );
+            await expect(contextMenu).toBeVisible();
+            await expect(contextMenu).toBeFocused();
+            const contextMenuItem = contextMenu
+              .locator(".action-item", {
+                hasText: "Problems",
+              })
+              .first();
+            await page.waitForTimeout(SHORT_TIMEOUT);
+            await contextMenuItem.click({ force: true });
+            await expect(contextMenu).toBeHidden();
+            await expect(item).toBeVisible();
+          },
         };
       } catch (error) {
-        throw new VError(error, `Failed to click status bar item ${label}`);
+        throw new VError(error, `Failed to click status bar item ${id}`);
       }
     },
   };
