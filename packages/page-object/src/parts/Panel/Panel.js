86a07d9bffee0c4ebd8688d2a93d9db1cbc4cb97
Simon Siefke <simon.siefke@gmail.com>
2023-06-15 10:08:17 +0200

    try to fix flaky toggle panel test

--- packages/page-object/src/parts/Panel/Panel.js
+++ packages/page-object/src/parts/Panel/Panel.js
@@ -1,16 +1,26 @@
 export const create = ({ expect, page, VError }) => {
   return {
-    async show() {
+    async toggle() {
       try {
-        const panel = page.locator(".part.panel");
-        await expect(panel).toBeHidden();
-        await page.keyboard.press("Control+Shift+P");
         const quickPick = page.locator(".quick-input-widget");
+        await page.keyboard.press("Control+Shift+P");
         await expect(quickPick).toBeVisible();
         const quickPickInput = quickPick.locator('[role="combobox"]');
         await quickPickInput.type("Toggle Panel Visibility");
         const firstOption = quickPick.locator(".monaco-list-row").first();
+        const firstOptionLabel = firstOption.locator(".label-name");
+        await expect(firstOptionLabel).toHaveText("Toggle Panel Visibility");
+        await expect(firstOption).toBeVisible();
         await firstOption.click();
+      } catch (error) {
+        throw new VError(error, `Failed to toggle panel`);
+      }
+    },
+    async show() {
+      try {
+        const panel = page.locator(".part.panel");
+        await expect(panel).toBeHidden();
+        await this.toggle();
         await expect(panel).toBeVisible();
       } catch (error) {
         throw new VError(error, `Failed to show panel`);
@@ -20,13 +30,7 @@ export const create = ({ expect, page, VError }) => {
       try {
         const panel = page.locator(".part.panel");
         await expect(panel).toBeVisible();
-        const quickPick = page.locator(".quick-input-widget");
-        await page.keyboard.press("Control+Shift+P");
-        await expect(quickPick).toBeVisible();
-        const quickPickInput = quickPick.locator('[role="combobox"]');
-        await quickPickInput.type("Toggle Panel Visibility");
-        const firstOption = quickPick.locator(".monaco-list-row").first();
-        await firstOption.click();
+        await this.toggle();
         await expect(panel).toBeHidden();
       } catch (error) {
         throw new VError(error, `Failed to hide panel`);
