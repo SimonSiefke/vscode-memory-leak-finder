04966f17bf9bb6744b6ff7cc407aa4e75e859a86
Simon Siefke <simon.siefke@gmail.com>
2023-06-16 13:10:10 +0200

    try to make test less flaky

--- packages/e2e/src/scenario/sample.extension-detail-view-open.js
+++ packages/e2e/src/scenario/sample.extension-detail-view-open.js
@@ -1,11 +1,13 @@
 export const setup = async ({ Extensions }) => {
   await Extensions.show();
+  await Extensions.search("@builtin html");
 };
 
 export const run = async ({ page, expect, Editor }) => {
   const firstExtension = page.locator(".extension-list-item").first();
   await expect(firstExtension).toBeVisible();
   const nameLocator = firstExtension.locator(".name");
+  await expect(nameLocator).toHaveText("HTML Language Basics");
   const name = await nameLocator.textContent();
   await firstExtension.click();
   const tabLabel = page.locator(".tab-label");
