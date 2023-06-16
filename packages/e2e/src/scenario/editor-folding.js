89a907fdf38c45b0e12a2f24f7f13b48adf0b70b
Simon Siefke <simon.siefke@gmail.com>
2023-06-16 13:17:46 +0200

    test: add editor folding test (#104)

---
+++ packages/e2e/src/scenario/editor-folding.js
@@ -0,0 +1,32 @@
+export const beforeSetup = async ({ tmpDir, writeFile, join }) => {
+  await writeFile(
+    join(tmpDir, "file.css"),
+    `h1 {
+  font-size: 20px
+}
+
+h2 {
+  font-size: 15px;
+}`
+  );
+};
+
+export const setup = async ({ Editor }) => {
+  await Editor.open("file.css");
+};
+
+export const run = async ({ Editor, page, expect }) => {
+  const inlineFolded = page.locator(".inline-folded");
+  await expect(inlineFolded).toBeHidden();
+  const collapsedIcon = page.locator(".codicon-folding-collapsed").first();
+  await expect(collapsedIcon).toBeHidden();
+  const foldingIcon = page.locator(".codicon-folding-expanded").first();
+  await expect(foldingIcon).toBeVisible();
+  const firstIcon = foldingIcon.first();
+  await firstIcon.click();
+  await expect(inlineFolded).toBeVisible();
+  await expect(collapsedIcon).toBeVisible();
+  await collapsedIcon.click();
+  await expect(inlineFolded).toBeHidden();
+  await expect(collapsedIcon).toBeHidden();
+};
