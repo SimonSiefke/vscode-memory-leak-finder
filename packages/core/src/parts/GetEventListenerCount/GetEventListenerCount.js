de9bc392fb7863004b181d0460f4ae0c22a7e083
Simon Siefke <simon.siefke@gmail.com>
2023-06-16 09:36:43 +0200

    refactor: add measureEventListener count and make measures more flexible

---
+++ packages/core/src/parts/GetEventListenerCount/GetEventListenerCount.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @param {import('@playwright/test').CDPSession} session
+ * @returns {Promise<number>}
+ */
+export const getEventListenerCount = async (session) => {
+  // TODO this does not include hidden dom nodes
+  const fnResult = await session.send("Runtime.evaluate", {
+    expression: `;(() => {
+  const nodes = Array.from(document.querySelectorAll('*'))
+  const listenerMap = Object.create(null)
+  for (const node of nodes) {
+    const listeners = getEventListeners(node)
+    for (const [key, value] of Object.entries(listeners)) {
+      listenerMap[key] ||= 0
+      listenerMap[key] += value.length
+      listenerMap.z_total ||= 0
+      listenerMap.z_total += value.length
+    }
+  }
+  return listenerMap
+})()`,
+    returnByValue: true,
+    includeCommandLineAPI: true,
+  });
+  const value = fnResult.result.value;
+  const total = value.z_total;
+  return total;
+};
