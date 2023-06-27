cea4c02d73d53396bf1080ac5c162632f1ce010b
Simon Siefke <simon.siefke@gmail.com>
2023-06-27 13:09:19 +0200

    feature: move vscode binary download to test worker commands (#122)

---
+++ packages/test-worker-commands/src/parts/GetVsCodeArgs/GetVsCodeArgs.js
@@ -0,0 +1,15 @@
+export const getVscodeArgs = ({ userDataDir, extraLaunchArgs }) => {
+  return [
+    '--wait',
+    '--new-window',
+    '--no-sandbox',
+    '--disable-updates',
+    '--skip-welcome',
+    '--skip-release-notes',
+    '--disable-workspace-trust',
+    '--disable-extensions',
+    '--user-data-dir',
+    ...extraLaunchArgs,
+    '.',
+  ]
+}
--- packages/test-worker-commands/src/parts/UtilityScript/UtilityScript.js
+++ packages/test-worker-commands/src/parts/UtilityScript/UtilityScript.js
@@ -3,6 +3,6 @@ import { join } from 'node:path'
 import * as Root from '../Root/Root.js'
 import * as EncodingType from '../EncodingType/EncodingType.js'
 
-const injectedCode = readFileSync(join(Root.root, '..', '..', 'injected-code', 'dist', 'injectedCode.js'), EncodingType.Utf8)
+const injectedCode = readFileSync(join(Root.root, 'packages', 'injected-code', 'dist', 'injectedCode.js'), EncodingType.Utf8)
 
 export const utilityScript = injectedCode
