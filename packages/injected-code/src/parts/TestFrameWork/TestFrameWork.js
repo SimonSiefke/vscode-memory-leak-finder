9ca9ce2b80cb76f8941fc78737a5527a704601c3
Simon Siefke <simon.siefke@gmail.com>
2023-06-27 12:54:39 +0200

    feature: add injected code package

---
+++ packages/injected-code/src/parts/TestFrameWork/TestFrameWork.js
@@ -0,0 +1,170 @@
+import * as Assert from '../Assert/Assert.js'
+import { AssertionError } from '../AssertionError/AssertionError.js'
+import * as ConditionErrorMap from '../ConditionErrorMap/ConditionErrorMap.js'
+import * as ElementActions from '../ElementActions/ElementActions.js'
+import * as KeyBoardActions from '../KeyBoardActions/KeyBoardActions.js'
+import * as MultiElementConditionMap from '../MultiElementConditionMap/MultiElementConditionMap.js'
+import * as QuerySelector from '../QuerySelector/QuerySelector.js'
+import * as SingleElementConditionMap from '../SingleElementConditionMap/SingleElementConditionMap.js'
+
+const create$Overlay = () => {
+  const $TestOverlay = document.createElement('div')
+  $TestOverlay.id = 'TestOverlay'
+  $TestOverlay.style.position = 'fixed'
+  $TestOverlay.style.bottom = '0px'
+  $TestOverlay.style.left = '0px'
+  $TestOverlay.style.right = '0px'
+  $TestOverlay.style.height = '20px'
+  $TestOverlay.style.whiteSpace = 'nowrap'
+  $TestOverlay.style.contain = 'strict'
+  $TestOverlay.style.userSelect = 'text'
+  $TestOverlay.style.color = 'black'
+  return $TestOverlay
+}
+
+export const showOverlay = (state, background, text) => {
+  const $TestOverlay = create$Overlay()
+  $TestOverlay.dataset.state = state
+  $TestOverlay.style.background = background
+  $TestOverlay.textContent = text
+  document.body.append($TestOverlay)
+}
+
+const Time = {
+  getTimeStamp() {
+    return performance.now()
+  },
+}
+
+const maxTimeout = 2000
+
+const Timeout = {
+  async short() {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  },
+  async waitForMutation(element, maxDelay) {
+    const disposables = []
+    await Promise.race([
+      new Promise((resolve) => {
+        const timeout = setTimeout(resolve, maxDelay)
+        disposables.push(() => {
+          clearTimeout(timeout)
+        })
+      }),
+      new Promise((resolve) => {
+        const callback = (mutations) => {
+          resolve(undefined)
+        }
+        const observer = new MutationObserver(callback)
+        observer.observe(document.body, {
+          childList: true,
+          attributes: true,
+          characterData: true,
+          subtree: true,
+          attributeOldValue: true,
+          characterDataOldValue: true,
+        })
+        disposables.push(() => {
+          observer.disconnect()
+        })
+      }),
+    ])
+    for (const disposable of disposables) {
+      disposable()
+    }
+  },
+}
+
+export const performAction = async (locator, fnName, options) => {
+  Assert.object(locator)
+  Assert.string(fnName)
+  Assert.object(options)
+  const startTime = Time.getTimeStamp()
+  const endTime = startTime + maxTimeout
+  let currentTime = startTime
+  const fn = ElementActions[fnName]
+  while (currentTime < endTime) {
+    const element = QuerySelector.querySelectorWithOptions(locator._selector, {
+      hasText: locator._hasText,
+      nth: locator._nth,
+    })
+    if (element) {
+      fn(element, options)
+      console.log('action was performed')
+      return
+    }
+    await Timeout.waitForMutation(document.body, 100)
+    currentTime = Time.getTimeStamp()
+  }
+}
+
+export const performKeyBoardAction = (fnName, options) => {
+  const fn = KeyBoardActions[fnName]
+  fn(options)
+}
+
+export const checkSingleElementCondition = async (locator, fnName, options) => {
+  Assert.object(locator)
+  Assert.string(fnName)
+  Assert.object(options)
+  const startTime = Time.getTimeStamp()
+  const endTime = startTime + maxTimeout
+  let currentTime = startTime
+  const fn = SingleElementConditionMap.getFunction(fnName)
+  while (currentTime < endTime) {
+    const element = QuerySelector.querySelectorWithOptions(locator._selector, {
+      hasText: locator._hasText,
+      nth: locator._nth,
+    })
+    if (element) {
+      const successful = fn(element, options)
+      if (successful) {
+        return
+      }
+    }
+    await Timeout.waitForMutation(document.body, 100)
+    currentTime = Time.getTimeStamp()
+  }
+  const errorMessageFn = ConditionErrorMap.getFunction(fnName)
+  const message = errorMessageFn(locator, options)
+  throw new AssertionError(message)
+}
+
+export const checkTitle = async (expectedTitle) => {
+  Assert.string(expectedTitle)
+  const startTime = Time.getTimeStamp()
+  const endTime = startTime + maxTimeout
+  let currentTime = startTime
+  while (currentTime < endTime) {
+    const element = document.querySelector('title')
+    if (element) {
+      const successful = document.title === expectedTitle
+      if (successful) {
+        return
+      }
+    }
+    await Timeout.waitForMutation(document.head, 100)
+    currentTime = Time.getTimeStamp()
+  }
+  const message = `expected title to be "${expectedTitle}" but was "${document.title}"`
+  throw new AssertionError(message)
+}
+
+export const checkMultiElementCondition = async (locator, fnName, options) => {
+  const startTime = Time.getTimeStamp()
+  const endTime = startTime + maxTimeout
+  let currentTime = startTime
+  const fn = MultiElementConditionMap.getFunction(fnName)
+  while (currentTime < endTime) {
+    const elements = QuerySelector.querySelector(locator._selector)
+    const successful = fn(elements, options)
+    if (successful) {
+      return
+    }
+    await Timeout.waitForMutation(document.body, 100)
+    currentTime = Time.getTimeStamp()
+  }
+  const errorMessageFn = ConditionErrorMap.getFunction(fnName)
+  const message = errorMessageFn(locator, options)
+  throw new AssertionError(message)
+}
