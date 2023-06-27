77ec6f559e8bd89209fdefd70998e9c0ebb6a9cb
Simon Siefke <simon.siefke@gmail.com>
2023-06-27 19:29:13 +0200

    fix: vscode launching (#124)

--- packages/test-worker-commands/src/parts/RunTest/RunTest.js
+++ packages/test-worker-commands/src/parts/RunTest/RunTest.js
@@ -1,5 +1,7 @@
 import * as CleanUpTestState from '../CleanUpTestState/CleanUpTestState.js'
 import * as GetBinaryPath from '../GetBinaryPath/GetBinaryPath.js'
+import * as GetUserDataDir from '../GetUserDataDir/GetUserDataDir.js'
+import * as GetVsCodeArgs from '../GetVsCodeArgs/GetVsCodeArgs.js'
 import * as ImportScript from '../ImportScript/ImportScript.js'
 import * as JsonRpcEvent from '../JsonRpcEvent/JsonRpcEvent.js'
 import * as LaunchElectron from '../LaunchElectron/LaunchElectron.js'
@@ -12,14 +14,22 @@ export const runTest = async (state, file, relativeDirName, relativeFilePath, fi
     const start = performance.now()
     callback(JsonRpcEvent.create(TestWorkerEventType.TestRunning, [file, relativeDirName, fileName]))
     const binaryPath = await GetBinaryPath.getBinaryPath()
+    const userDataDir = GetUserDataDir.getUserDataDir()
+    const args = GetVsCodeArgs.getVscodeArgs({
+      userDataDir,
+      extraLaunchArgs: [],
+    })
     const electronApp = await LaunchElectronApp.launch({
       cliPath: binaryPath,
-      args: [],
+      args,
       headlessMode: false,
     })
 
     const module = await ImportScript.importScript(file)
     const win = await electronApp.firstWindow()
+    await new Promise((r) => {
+      setTimeout(r, 100000)
+    })
 
     console.log({ electronApp, binaryPath })
     // await module.test({ electronApp, expect });
