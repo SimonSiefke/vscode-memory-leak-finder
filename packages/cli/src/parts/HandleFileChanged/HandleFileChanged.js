c1c7f6362fa659d37af865b07731937bbd3dfff3
Simon Siefke <simon.siefke@gmail.com>
2023-06-27 19:25:40 +0200

    Discard changes to packages/cli/src/parts/HandleFileChanged/HandleFileChanged.js

--- packages/cli/src/parts/HandleFileChanged/HandleFileChanged.js
+++ packages/cli/src/parts/HandleFileChanged/HandleFileChanged.js
@@ -1,8 +1,7 @@
 import * as StartRunning from '../StartRunning/StartRunning.js'
 import * as StdinDataState from '../StdinDataState/StdinDataState.js'
 
-export const handleFileChanged = async (file) => {
-  console.log('file changed', file)
-  // const state = StdinDataState.getState()
-  // await StartRunning.startRunning(state.value, /* headlessMode */ true, /* color */ true)
+export const handleFileChanged = async () => {
+  const state = StdinDataState.getState()
+  await StartRunning.startRunning(state.value, /* headlessMode */ true, /* color */ true)
 }
