51d592ff2881171bcb67cf296944964ad8527be0
Simon Siefke <simon.siefke@gmail.com>
2023-06-30 20:56:38 +0200

    refactor: move locator into separate file

--- packages/test-worker-commands/src/parts/Locator/Locator.js
+++ packages/test-worker-commands/src/parts/Locator/Locator.js
@@ -1,10 +1,81 @@
-const click = (page) => {
-  // TODO
-}
+import * as ObjectType from '../ObjectType/ObjectType.js'
+import * as PageClick from '../PageClick/PageClick.js'
+import * as PageCount from '../PageCount/PageCount.js'
+import * as PageHover from '../PageHover/PageHover.js'
+import * as PageTextContent from '../PageTextContent/PageTextContent.js'
+import * as PageType from '../PageType/PageType.js'
 
-export const Locator = (selector) => {
+export const create = (rpc, sessionId, selector, { hasText = '', nth = -1 } = {}) => {
   return {
+    objectType: ObjectType.Locator,
     selector,
-    click,
+    sessionId,
+    hasText,
+    _nth: nth,
+    nth(value) {
+      return {
+        ...this,
+        selector: `${this.selector}`,
+        _nth: value + 1,
+      }
+    },
+    first() {
+      return {
+        ...this,
+        selector: `${this.selector}`,
+        _nth: 1,
+      }
+    },
+    count() {
+      return PageCount.count({
+        selector: this.selector,
+      })
+    },
+    locator(selector, { hasText = '', nth = -1 } = {}) {
+      return {
+        ...this,
+        selector: `${this.selector} ${selector}`,
+        hasText,
+        _nth: nth,
+      }
+    },
+    type(text) {
+      return PageType.type(
+        {
+          selector: this.selector,
+          hasText: this.hasText,
+          _nth: this._nth,
+        },
+        text
+      )
+    },
+    click() {
+      return PageClick.click({
+        selector: this.selector,
+        hasText: this.hasText,
+        _nth: this._nth,
+      })
+    },
+    dblclick() {
+      return PageClick.dblclick({
+        selector: this.selector,
+        hasText: this.hasText,
+        _nth: this._nth,
+      })
+    },
+    hover() {
+      return PageHover.hover({
+        selector: this.selector,
+        hasText: this.hasText,
+        _nth: this._nth,
+      })
+    },
+    textContent() {
+      return PageTextContent.getTextContent({
+        selector: this.selector,
+        hasText: this.hasText,
+        _nth: this._nth,
+      })
+    },
   }
 }
--- packages/test-worker-commands/src/parts/Page/Page.js
+++ packages/test-worker-commands/src/parts/Page/Page.js
@@ -1,14 +1,9 @@
 import * as DevtoolsTargetType from '../DevtoolsTargetType/DevtoolsTargetType.js'
-import * as ObjectType from '../ObjectType/ObjectType.js'
-import * as PageClick from '../PageClick/PageClick.js'
+import * as Locator from '../Locator/Locator.js'
 import * as PageClose from '../PageClose/PageClose.js'
-import * as PageCount from '../PageCount/PageCount.js'
 import * as PageEvaluate from '../PageEvaluate/PageEvaluate.js'
-import * as PageHover from '../PageHover/PageHover.js'
 import * as PageKeyBoard from '../PageKeyBoard/PageKeyBoard.js'
 import * as PageReload from '../PageReload/PageReload.js'
-import * as PageTextContent from '../PageTextContent/PageTextContent.js'
-import * as PageType from '../PageType/PageType.js'
 import * as WebWorker from '../WebWorker/WebWorker.js'
 
 const createKeyboard = (rpc) => {
@@ -52,79 +47,8 @@ export const create = async ({ electronRpc, electronObjectId, targetId, sessionI
     webWorker() {
       return WebWorker.waitForWebWorker({ sessionId })
     },
-    locator(selector, { hasText = '', nth = -1 } = {}) {
-      return {
-        objectType: ObjectType.Locator,
-        selector,
-        sessionId,
-        hasText,
-        _nth: nth,
-        nth(value) {
-          return {
-            ...this,
-            selector: `${this.selector}`,
-            _nth: value + 1,
-          }
-        },
-        first() {
-          return {
-            ...this,
-            selector: `${this.selector}`,
-            _nth: 1,
-          }
-        },
-        count() {
-          return PageCount.count({
-            selector: this.selector,
-          })
-        },
-        locator(selector, { hasText = '', nth = -1 } = {}) {
-          return {
-            ...this,
-            selector: `${this.selector} ${selector}`,
-            hasText,
-            _nth: nth,
-          }
-        },
-        type(text) {
-          return PageType.type(
-            {
-              selector: this.selector,
-              hasText: this.hasText,
-              _nth: this._nth,
-            },
-            text
-          )
-        },
-        click() {
-          return PageClick.click({
-            selector: this.selector,
-            hasText: this.hasText,
-            _nth: this._nth,
-          })
-        },
-        dblclick() {
-          return PageClick.dblclick({
-            selector: this.selector,
-            hasText: this.hasText,
-            _nth: this._nth,
-          })
-        },
-        hover() {
-          return PageHover.hover({
-            selector: this.selector,
-            hasText: this.hasText,
-            _nth: this._nth,
-          })
-        },
-        textContent() {
-          return PageTextContent.getTextContent({
-            selector: this.selector,
-            hasText: this.hasText,
-            _nth: this._nth,
-          })
-        },
-      }
+    locator(selector, options = {}) {
+      return Locator.create(this.rpc, this.sessionId, selector, options)
     },
   }
 }
