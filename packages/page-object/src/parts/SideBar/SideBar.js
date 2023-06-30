6414b5473ac5899e059771e5ecc6195410f332f6
Simon Siefke <simon.siefke@gmail.com>
2023-06-30 07:49:37 +0200

    refactor: simplify sidebar api and make use of quickpick

--- packages/page-object/src/parts/SideBar/SideBar.js
+++ packages/page-object/src/parts/SideBar/SideBar.js
@@ -1,70 +1,64 @@
+import * as QuickPick from '../QuickPick/QuickPick.js'
+
 export const create = ({ expect, page, VError }) => {
   return {
+    async toggle() {
+      try {
+        const quickPick = QuickPick.create({ expect, page, VError })
+        await quickPick.showCommands()
+        await quickPick.type('Toggle Side Bar Visibility')
+        await quickPick.select('View: Toggle Primary Side Bar Visibility')
+      } catch (error) {
+        throw new VError(error, `Failed to show side bar`)
+      }
+    },
     async show() {
       try {
-        const sideBar = page.locator(".part.sidebar");
-        await expect(sideBar).toBeHidden();
-        await page.keyboard.press("Control+Shift+P");
-        const quickPick = page.locator(".quick-input-widget");
-        await expect(quickPick).toBeVisible();
-        const quickPickInput = quickPick.locator('[role="combobox"]');
-        await quickPickInput.type("Toggle Side Bar Visibility");
-        const firstOption = quickPick.locator(".monaco-list-row").first();
-        await firstOption.click();
-        await expect(sideBar).toBeVisible();
+        const sideBar = page.locator('.part.sidebar')
+        await expect(sideBar).toBeHidden()
+        await this.toggle()
+        await expect(sideBar).toBeVisible()
       } catch (error) {
-        throw new VError(error, `Failed to show side bar`);
+        throw new VError(error, `Failed to show side bar`)
       }
     },
     async hide() {
       try {
-        const sideBar = page.locator(".part.sidebar");
-        await expect(sideBar).toBeVisible();
-        await page.keyboard.press("Control+Shift+P");
-        const quickPick = page.locator(".quick-input-widget");
-        await expect(quickPick).toBeVisible();
-        const quickPickInput = quickPick.locator('[role="combobox"]');
-        await quickPickInput.type("Toggle Side Bar Visibility");
-        const firstOption = quickPick.locator(".monaco-list-row").first();
-        await firstOption.click();
-        await expect(sideBar).toBeHidden();
+        const sideBar = page.locator('.part.sidebar')
+        await expect(sideBar).toBeVisible()
+        await this.toggle()
+        await expect(sideBar).toBeHidden()
       } catch (error) {
-        throw new VError(error, `Failed to hide side bar`);
+        throw new VError(error, `Failed to hide side bar`)
       }
+    },
+    async togglePosition() {
+      const quickPick = QuickPick.create({ expect, page, VError })
+      await quickPick.showCommands()
+      await quickPick.type('Toggle Side Bar Position')
+      await quickPick.select('View: Toggle Primary Side Bar Position')
     },
     async moveRight() {
       try {
-        const sideBar = page.locator(".part.sidebar");
-        await expect(sideBar).toBeVisible();
-        await expect(sideBar).toHaveClass("part sidebar left");
-        await page.keyboard.press("Control+Shift+P");
-        const quickPick = page.locator(".quick-input-widget");
-        await expect(quickPick).toBeVisible();
-        const quickPickInput = quickPick.locator('[role="combobox"]');
-        await quickPickInput.type("Toggle Side Bar Position");
-        const firstOption = quickPick.locator(".monaco-list-row").first();
-        await firstOption.click();
-        await expect(sideBar).toHaveClass("part sidebar right");
+        const sideBar = page.locator('.part.sidebar')
+        await expect(sideBar).toBeVisible()
+        await expect(sideBar).toHaveClass('part sidebar left')
+        await this.togglePosition()
+        await expect(sideBar).toHaveClass('part sidebar right')
       } catch (error) {
-        throw new VError(error, `Failed to move side bar right`);
+        throw new VError(error, `Failed to move side bar right`)
       }
     },
     async moveLeft() {
       try {
-        const sideBar = page.locator(".part.sidebar");
-        await expect(sideBar).toBeVisible();
-        await expect(sideBar).toHaveClass("part sidebar right");
-        await page.keyboard.press("Control+Shift+P");
-        const quickPick = page.locator(".quick-input-widget");
-        await expect(quickPick).toBeVisible();
-        const quickPickInput = quickPick.locator('[role="combobox"]');
-        await quickPickInput.type("Toggle Side Bar Position");
-        const firstOption = quickPick.locator(".monaco-list-row").first();
-        await firstOption.click();
-        await expect(sideBar).toHaveClass("part sidebar left");
+        const sideBar = page.locator('.part.sidebar')
+        await expect(sideBar).toBeVisible()
+        await expect(sideBar).toHaveClass('part sidebar right')
+        await this.togglePosition()
+        await expect(sideBar).toHaveClass('part sidebar left')
       } catch (error) {
-        throw new VError(error, `Failed to move side bar left`);
+        throw new VError(error, `Failed to move side bar left`)
       }
     },
-  };
-};
+  }
+}
