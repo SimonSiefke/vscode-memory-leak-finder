e7f900115d886c06fad12adeb4cb87cc5cf241f7
Simon Siefke <simon.siefke@gmail.com>
2023-06-30 15:34:08 +0200

    feature: reenable title bar test

--- packages/e2e/src/title-bar-menu-toggle.js
+++ packages/e2e/src/title-bar-menu-toggle.js
@@ -1,11 +1,3 @@
-export const skip = true
-
-export const beforeSetup = async ({ writeSettings }) => {
-  await writeSettings({
-    'window.titleBarStyle': 'custom',
-  })
-}
-
 export const run = async ({ TitleBar }) => {
   await TitleBar.showMenu()
   await TitleBar.hideMenu()
--- packages/page-object/src/parts/TitleBar/TitleBar.js
+++ packages/page-object/src/parts/TitleBar/TitleBar.js
@@ -2,42 +2,39 @@ export const create = ({ expect, page, VError }) => {
   return {
     async showMenu(text) {
       try {
-        const titleBar = page.locator(".part.titlebar");
-        await expect(titleBar).toBeVisible();
-
+        const titleBar = page.locator('.part.titlebar')
+        await expect(titleBar).toBeVisible()
         const menuItem = titleBar
-          .locator(".menubar-menu-button", {
+          .locator('.menubar-menu-button', {
             hasText: text,
           })
-          .first();
-        await menuItem.click();
-        const menu = page.locator(".monaco-menu .actions-container");
-        expect(menu).toBeVisible();
-        await expect(menu).toBeFocused();
+          .first()
+        await menuItem.click()
+        const menu = page.locator('.monaco-menu .actions-container')
+        expect(menu).toBeVisible()
+        await expect(menu).toBeFocused()
       } catch (error) {
-        throw new VError(error, `Failed to open title bar menu`);
+        throw new VError(error, `Failed to open title bar menu`)
       }
     },
     async hideMenu(text) {
       try {
-        const titleBar = page.locator(".part.titlebar");
-        await expect(titleBar).toBeVisible();
-
+        const titleBar = page.locator('.part.titlebar')
+        await expect(titleBar).toBeVisible()
         const menuItem = titleBar
-          .locator(".menubar-menu-button", {
+          .locator('.menubar-menu-button', {
             hasText: text,
           })
-          .first();
-        const menu = page.locator(".monaco-menu .actions-container");
-        expect(menu).toBeVisible();
-        await expect(menu).toBeFocused();
-
-        await page.keyboard.press("Escape");
-        await expect(menu).toBeHidden();
-        await expect(menuItem).toBeFocused();
+          .first()
+        const menu = page.locator('.monaco-menu .actions-container')
+        expect(menu).toBeVisible()
+        await expect(menu).toBeFocused()
+        await page.keyboard.press('Escape')
+        await expect(menu).toBeHidden()
+        await expect(menuItem).toBeFocused()
       } catch (error) {
-        throw new VError(error, `Failed to hide title bar menu`);
+        throw new VError(error, `Failed to hide title bar menu`)
       }
     },
-  };
-};
+  }
+}
