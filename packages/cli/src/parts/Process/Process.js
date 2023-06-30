57cd7a57d586b4d3268649e9ffa36d232d9c4b14
Simon Siefke <simon.siefke@gmail.com>
2023-06-30 21:16:27 +0200

    refactor: use node process import instead of global variable

--- packages/cli/src/parts/Process/Process.js
+++ packages/cli/src/parts/Process/Process.js
@@ -1,17 +1,19 @@
+import * as NodeProcess from 'node:process'
+
 export const exit = (exitCode) => {
-  process.exit(exitCode)
+  NodeProcess.exit(exitCode)
 }
 
-export const stdin = process.stdin
+export const stdin = NodeProcess.stdin
 
-export const stdout = process.stdout
+export const stdout = NodeProcess.stdout
 
-export const stderr = process.stdout
+export const stderr = NodeProcess.stdout
 
 export const kill = (pid) => {
-  process.kill(pid)
+  NodeProcess.kill(pid)
 }
 
 export const on = (event, listener) => {
-  process.on(event, listener)
+  NodeProcess.on(event, listener)
 }
