63b273e80657dea0e8b3efd003b3e28d10423470
Simon Siefke <simon.siefke@gmail.com>
2023-07-01 20:37:58 +0200

    fix postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,23 +1,22 @@
-import { execa } from "execa";
+import { execa } from 'execa'
 
 const exec = async (command) => {
-  console.info(command);
+  console.info(command)
   await execa(command, {
     shell: true,
-    stdio: "inherit",
-  });
-};
+    stdio: 'inherit',
+  })
+}
 
 // prettier-ignore
 const applyLernaBugWorkaround =async  () => {
   // workaround for https://github.com/lerna/lerna/issues/2352
   await exec(`cd packages/page-object && npm ci --prefer-offline && cd ../../`);
-  await exec(`cd packages/core && npm ci --prefer-offline && cd ../../`);
   await exec(`cd packages/e2e && npm ci --prefer-offline && cd ../../`);
 }
 
 const main = async () => {
-  await applyLernaBugWorkaround();
-};
+  await applyLernaBugWorkaround()
+}
 
-main();
+main()
