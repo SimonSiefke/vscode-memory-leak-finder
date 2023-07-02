188d8861baffd2901f819e9f4188eb81eeca108c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-02 10:37:36 +0200

    fix: remove stdout write during test

--- packages/cli/test/HandleTestStdoutData.test.js
+++ packages/cli/test/HandleTestStdoutData.test.js
@@ -10,6 +10,14 @@ jest.unstable_mockModule('../src/parts/TestStateOutput/TestStateOutput.js', () =
   }
 })
 
+jest.unstable_mockModule('../src/parts/StdinDataState/StdinDataState.js', () => {
+  return {
+    isBuffering() {
+      return true
+    },
+  }
+})
+
 const TestStateOutput = await import('../src/parts/TestStateOutput/TestStateOutput.js')
 const HandleTestStdoutData = await import('../src/parts/HandleTestStdoutData/HandleTestStdoutData.js')
 
