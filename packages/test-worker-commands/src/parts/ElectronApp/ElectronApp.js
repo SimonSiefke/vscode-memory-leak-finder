604d4ecdd360cd6114615f5dcedb28c4b032a3fe
Simon Siefke <simon.siefke@gmail.com>
2023-07-08 01:25:34 +0200

    fix: search/replace test

--- packages/test-worker-commands/src/parts/ElectronApp/ElectronApp.js
+++ packages/test-worker-commands/src/parts/ElectronApp/ElectronApp.js
@@ -1,5 +1,6 @@
 import * as ObjectType from '../ObjectType/ObjectType.js'
 import * as WaitForPage from '../WaitForPage/WaitForPage.js'
+import * as DevtoolsProtocolRuntime from '../DevtoolsProtocolRuntime/DevtoolsProtocolRuntime.js'
 
 export const create = ({ electronRpc, electronObjectId, callFrameId }) => {
   return {
@@ -16,6 +17,11 @@ export const create = ({ electronRpc, electronObjectId, callFrameId }) => {
     },
     thirdWindow() {
       return WaitForPage.waitForPage({ index: 2, electronRpc, electronObjectId })
+    },
+    evaluate(expression) {
+      return DevtoolsProtocolRuntime.evaluate(this.rpc, {
+        expression,
+      })
     },
   }
 }
