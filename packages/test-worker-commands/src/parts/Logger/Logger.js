8d19aa8c4a0cc09e0e5a913211f420bf202f900f
Simon Siefke <simon.siefke@gmail.com>
2023-07-07 21:17:34 +0200

    fix: logging on windows

--- packages/test-worker-commands/src/parts/Logger/Logger.js
+++ packages/test-worker-commands/src/parts/Logger/Logger.js
@@ -1,8 +1,11 @@
 import { Console } from 'node:console'
 import { createWriteStream } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
 
 const createConsole = () => {
-  const logFile = `/tmp/test-worker-log.txt`
+  const tmpDir = tmpdir()
+  const logFile = join(tmpDir, 'workr-log.txt')
   const writeStream = createWriteStream(logFile)
   const logger = new Console(writeStream)
   return logger
