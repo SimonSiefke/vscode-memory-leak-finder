c2320f4d82079bd5c336d39d91f63ca592638ae4
Simon Siefke <simon.siefke@gmail.com>
2023-07-07 18:26:04 +0200

    feature: improve output when running in github actions (#165)

--- packages/cli/src/parts/HandleTestStateChange/HandleTestStateChange.js
+++ packages/cli/src/parts/HandleTestStateChange/HandleTestStateChange.js
@@ -1,13 +1,18 @@
 import * as AnsiEscapes from '../AnsiEscapes/AnsiEscapes.js'
 import * as GetTestClearMessage from '../GetTestClearMessage/GetTestClearMessage.js'
+import * as IsGithubActions from '../IsGithubActions/IsGithubActions.js'
 import * as StdinDataState from '../StdinDataState/StdinDataState.js'
 import * as Stdout from '../Stdout/Stdout.js'
 import * as TestStateOutput from '../TestStateOutput/TestStateOutput.js'
 
 const getFullMessage = (message) => {
   const clearMessage = GetTestClearMessage.getTestClearMessage()
-  let fullMessage = clearMessage + message
-  if (!StdinDataState.isBuffering()) {
+  let fullMessage = ''
+  if (!IsGithubActions.isGithubActions) {
+    fullMessage += clearMessage
+  }
+  fullMessage += message
+  if (!IsGithubActions.isGithubActions && !StdinDataState.isBuffering()) {
     fullMessage = AnsiEscapes.clear + fullMessage
   }
   fullMessage += TestStateOutput.clearPending()
