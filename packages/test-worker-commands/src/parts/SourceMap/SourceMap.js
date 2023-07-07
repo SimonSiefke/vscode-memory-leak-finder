a92fe80b7e7fb90c703fb90afb327cf6725a058f
Simon Siefke <simon.siefke@gmail.com>
2023-07-07 22:40:23 +0200

    feature: add stricter assertions to sourcemap module (#175)

--- packages/test-worker-commands/src/parts/SourceMap/SourceMap.js
+++ packages/test-worker-commands/src/parts/SourceMap/SourceMap.js
@@ -3,6 +3,8 @@ import * as Assert from '../Assert/Assert.js'
 
 export const getOriginalPosition = async (rawSourceMap, line, column) => {
   Assert.object(rawSourceMap)
+  Assert.number(line)
+  Assert.number(column)
   const originalPosition = await SourceMapConsumer.with(rawSourceMap, null, (consumer) => {
     return consumer.originalPositionFor({
       line,
