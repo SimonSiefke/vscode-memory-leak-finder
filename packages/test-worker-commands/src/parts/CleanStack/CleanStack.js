cb6081704a78cfd1db9adf03e4bf27bab45eb54a
Simon Siefke <simon.siefke@gmail.com>
2023-08-12 12:47:28 +0200

    feature: improve clean stack output

--- packages/test-worker-commands/src/parts/CleanStack/CleanStack.js
+++ packages/test-worker-commands/src/parts/CleanStack/CleanStack.js
@@ -83,6 +83,24 @@ const formatLine = async (line, root) => {
   return line
 }
 
+const cleanLine = (line) => {
+  const trimmedLine = line.trim()
+  if (trimmedLine.startsWith('at Module.')) {
+    return line.replace('at Module.', 'at ')
+  }
+  if (trimmedLine.startsWith('at async Object.')) {
+    return line.replace('at async Object.', 'at async ')
+  }
+  if (trimmedLine.startsWith('at async Module.')) {
+    return line.replace('at async Module.', 'at async ')
+  }
+  return line
+}
+
+const getCleanLines = (lines) => {
+  return lines.map(cleanLine)
+}
+
 const formatLines = async (lines, root) => {
   const formattedLines = []
   for (const line of lines) {
@@ -121,8 +139,9 @@ export const cleanStack = async (stack, { root = '' } = {}) => {
   const lines = stack.split('\n')
   const relevantLines = getRelevantLines(lines, stack)
   const formattedLines = await formatLines(relevantLines, root)
-  if (formattedLines[0].startsWith('    at')) {
-    return formattedLines.join('\n')
+  const cleanLines = getCleanLines(formattedLines)
+  if (cleanLines[0].startsWith('    at')) {
+    return cleanLines.join('\n')
   }
-  return formattedLines.slice(1).join('\n')
+  return cleanLines.slice(1).join('\n')
 }
--- packages/test-worker-commands/test/CleanStack.test.js
+++ packages/test-worker-commands/test/CleanStack.test.js
@@ -8,9 +8,24 @@ test('cleanStack - show internal errors', async () => {
     at async Module.runTest (/test/packages/test-worker/src/parts/RunTest/RunTest.js:22:5)
     at async runTests (/test/packages/test-worker/src/parts/RunTests/RunTests.js:30:5)`
   expect(await CleanStack.cleanStack(stack))
-    .toBe(`    at Module.evaluate (/test/packages/test-worker/src/parts/DevtoolsProtocolRuntime/DevtoolsProtocolRuntime.js:13:11)
+    .toBe(`    at evaluate (/test/packages/test-worker/src/parts/DevtoolsProtocolRuntime/DevtoolsProtocolRuntime.js:13:11)
     at async toHaveText (/test/packages/test-worker/src/parts/ExpectLocatorToHaveText/ExpectLocatorToHaveText.js:14:16)
-    at async Module.test (/test/packages/e2e/src/sample.hello-world.js:15:3)
-    at async Module.runTest (/test/packages/test-worker/src/parts/RunTest/RunTest.js:22:5)
+    at async test (/test/packages/e2e/src/sample.hello-world.js:15:3)
+    at async runTest (/test/packages/test-worker/src/parts/RunTest/RunTest.js:22:5)
     at async runTests (/test/packages/test-worker/src/parts/RunTests/RunTests.js:30:5)`)
+})
+
+test('cleanStack - remove module prefix', async () => {
+  const stack = `TypeError: result.filter is not a function
+    at Module.removePlaywrightListeners (/test/packages/memory-leak-finder/src/parts/RemovePlaywrightListeners/RemovePlaywrightListeners.js:9:17)
+    at Module.start (/test/packages/memory-leak-finder/src/parts/MeasureEventListeners/MeasureEventListeners.js:23:68)
+    at async Object.start (/test/packages/memory-leak-finder/src/parts/MeasureCombined/MeasureCombined.js:5:31)
+    at async Module.runTest (/test/packages/test-worker-commands/src/parts/RunTest/RunTest.js:39:20)
+    at async runTests (/test/packages/test-worker-commands/src/parts/RunTests/RunTests.js:72:7)`
+  expect(await CleanStack.cleanStack(stack))
+    .toBe(`    at removePlaywrightListeners (/test/packages/memory-leak-finder/src/parts/RemovePlaywrightListeners/RemovePlaywrightListeners.js:9:17)
+    at start (/test/packages/memory-leak-finder/src/parts/MeasureEventListeners/MeasureEventListeners.js:23:68)
+    at async start (/test/packages/memory-leak-finder/src/parts/MeasureCombined/MeasureCombined.js:5:31)
+    at async runTest (/test/packages/test-worker-commands/src/parts/RunTest/RunTest.js:39:20)
+    at async runTests (/test/packages/test-worker-commands/src/parts/RunTests/RunTests.js:72:7)`)
 })
