2a69755704c96af0fcebde5bbcff5ccaa3424671
Simon Siefke <simon.siefke@gmail.com>
2023-08-12 12:21:47 +0200

    fix: unwrapping devtools result object

--- packages/devtools-protocol/src/parts/UnwrapDevtoolsEvaluateResult/UnwrapDevtoolsEvaluateResult.js
+++ packages/devtools-protocol/src/parts/UnwrapDevtoolsEvaluateResult/UnwrapDevtoolsEvaluateResult.js
@@ -26,6 +26,9 @@ export const unwrapResult = (rawResult) => {
           return rawResult
       }
     }
+    if (rawResult.result.result) {
+      return rawResult.result.result
+    }
     return rawResult.result
   }
   return rawResult
