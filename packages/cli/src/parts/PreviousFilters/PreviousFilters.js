ee494e52454e6be7286eb70ef25906a6ddca28c5
Simon Siefke <simon.siefke@gmail.com>
2023-08-20 12:31:22 +0200

    feature: add functionality to apply previous filter with arrowup

---
+++ packages/cli/src/parts/PreviousFilters/PreviousFilters.js
@@ -0,0 +1,33 @@
+import { dirname, join } from 'path'
+import * as Root from '../Root/Root.js'
+import { mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { VError } from '../VError/VError.js'
+
+const getPreviousFilterPath = () => {
+  return join(Root.root, '.vscode-previous-filters', 'filters.json')
+}
+
+export const get = () => {
+  try {
+    const previousFilterPath = getPreviousFilterPath()
+    const content = readFileSync(previousFilterPath, 'utf8')
+    const json = JSON.parse(content)
+    return json
+  } catch {
+    return []
+  }
+}
+
+export const add = (value) => {
+  try {
+    const previousFilterPath = getPreviousFilterPath()
+    const current = get()
+    const newValues = [value, ...current]
+    const newContent = JSON.stringify(newValues, null, 2)
+    mkdirSync(dirname(previousFilterPath), { recursive: true })
+    const content = writeFileSync(previousFilterPath, newContent)
+    return content
+  } catch (error) {
+    throw new VError(error, `Failed to add filter`)
+  }
+}
