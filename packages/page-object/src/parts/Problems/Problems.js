ffd2b0e7f4e068430d61ea1af321706922e59b32
Simon Siefke <simon.siefke@gmail.com>
2023-08-20 15:59:44 +0200

    refactor: format code

--- packages/page-object/src/parts/Problems/Problems.js
+++ packages/page-object/src/parts/Problems/Problems.js
@@ -2,19 +2,17 @@ export const create = ({ expect, page, VError }) => {
   return {
     async shouldHaveCount(count) {
       try {
-        const problemsBadge = page.locator(
-          '[role="tab"] [aria-label^="Problems"] + .badge'
-        );
-        const badgeContent = problemsBadge.locator(".badge-content");
+        const problemsBadge = page.locator('[role="tab"] [aria-label^="Problems"] + .badge')
+        const badgeContent = problemsBadge.locator('.badge-content')
         if (count === 0) {
-          await expect(problemsBadge).toBeHidden();
+          await expect(problemsBadge).toBeHidden()
         } else {
-          await expect(problemsBadge).toBeVisible();
-          await expect(badgeContent).toHaveText(`${count}`);
+          await expect(problemsBadge).toBeVisible()
+          await expect(badgeContent).toHaveText(`${count}`)
         }
       } catch (error) {
-        throw new VError(error, `Failed to assert problems count of ${count}`);
+        throw new VError(error, `Failed to assert problems count of ${count}`)
       }
     },
-  };
-};
+  }
+}
