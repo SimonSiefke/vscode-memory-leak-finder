a4574bc58bcc0742bae3829c2f40dbdb075d739e
Simon Siefke <simon.siefke@gmail.com>
2023-08-20 16:26:49 +0200

    feature: add evaluate function to electron app object (#234)

--- packages/test-worker-commands/src/parts/ElectronApp/ElectronApp.js
+++ packages/test-worker-commands/src/parts/ElectronApp/ElectronApp.js
@@ -1,5 +1,6 @@
 import * as ObjectType from '../ObjectType/ObjectType.js'
 import * as WaitForPage from '../WaitForPage/WaitForPage.js'
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 
 export const create = ({ electronRpc, electronObjectId, callFrameId }) => {
   return {
@@ -16,6 +17,11 @@ export const create = ({ electronRpc, electronObjectId, callFrameId }) => {
     },
     thirdWindow() {
       return WaitForPage.waitForPage({ index: 2, electronRpc, electronObjectId })
+    },
+    evaluate(expression) {
+      return DevtoolsProtocolRuntime.evaluate(this.rpc, {
+        expression,
+      })
     },
   }
 }
