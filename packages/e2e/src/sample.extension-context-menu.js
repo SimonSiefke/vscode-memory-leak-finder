47da663b122cb74d5eb479537906c2ab1c9245b4
Simon Siefke <simon.siefke@gmail.com>
2023-08-20 16:07:34 +0200

    improve test

--- packages/e2e/src/sample.extension-context-menu.js
+++ packages/e2e/src/sample.extension-context-menu.js
@@ -1,5 +1,7 @@
 export const setup = async ({ Extensions }) => {
   await Extensions.show()
+  await Extensions.search('@builtin html ')
+  await Extensions.first.shouldBe('HTML Language Basics')
 }
 
 export const run = async ({ Extensions, ContextMenu }) => {
