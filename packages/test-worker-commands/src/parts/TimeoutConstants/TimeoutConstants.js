75295c1a8a539544acf97a5b393fd15871efc66e
Simon Siefke <simon.siefke@gmail.com>
2023-08-20 15:58:02 +0200

    feature: increase utility execution context timeout

--- packages/test-worker-commands/src/parts/TimeoutConstants/TimeoutConstants.js
+++ packages/test-worker-commands/src/parts/TimeoutConstants/TimeoutConstants.js
@@ -3,5 +3,5 @@ export const DefaultExecutionContext = 500
 export const PageEvent = 2000
 export const SessionState = 500
 export const Target = 8000
-export const UtilityExecutionContext = 8000
+export const UtilityExecutionContext = 8500
 export const WaitForDebuggerToBePaused = 1000
