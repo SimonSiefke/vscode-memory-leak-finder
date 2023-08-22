1001100e08802c34f8237fefd8ac2d1595ab7c9b
Simon Siefke <simon.siefke@gmail.com>
2023-08-22 22:06:08 +0200

    remove duplicate code

--- packages/test-coordinator-commands/src/parts/TimeoutConstants/TimeoutConstants.js
+++
@@ -1,7 +0,0 @@
-export const AttachToPage = 1000
-export const DefaultExecutionContext = 500
-export const PageEvent = 2000
-export const SessionState = 500
-export const Target = 8000
-export const UtilityExecutionContext = 8500
-export const WaitForDebuggerToBePaused = 1000
