7608320b8d6ae36db0bdd8235b6910a120044e5a
Simon Siefke <simon.siefke@gmail.com>
2023-08-24 18:30:17 +0200

    simplify code

--- packages/cli/src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.js
+++ packages/cli/src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.js
@@ -1,25 +1,10 @@
 import * as FirstNodeWorkerEventType from '../FirstNodeWorkerEventType/FirstNodeWorkerEventType.js'
+import * as GetFirstEvent from '../GetFirstEvent/GetFirstEvent.js'
 
-export const getFirstNodeWorkerEvent = async (worker) => {
-  const { type, event } = await new Promise((resolve, reject) => {
-    const cleanup = (value) => {
-      worker.off('message', handleMessage)
-      worker.off('exit', handleExit)
-      worker.off('error', handleError)
-      resolve(value)
-    }
-    const handleExit = (event) => {
-      cleanup({ type: FirstNodeWorkerEventType.Exit, event })
-    }
-    const handleError = (event) => {
-      cleanup({ type: FirstNodeWorkerEventType.Error, event })
-    }
-    const handleMessage = (event) => {
-      cleanup({ type: FirstNodeWorkerEventType.Message, event })
-    }
-    worker.on('message', handleMessage)
-    worker.on('exit', handleExit)
-    worker.on('error', handleError)
+export const getFirstNodeWorkerEvent = (worker) => {
+  return GetFirstEvent.getFirstEvent(worker, {
+    message: FirstNodeWorkerEventType.Message,
+    error: FirstNodeWorkerEventType.Error,
+    exit: FirstNodeWorkerEventType.Exit,
   })
-  return { type, event }
 }
