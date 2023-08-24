84dbebc14d729d5ef6284578841c43eb049c4b9d
Simon Siefke <simon.siefke@gmail.com>
2023-08-24 21:54:54 +0200

    run first test again

--- packages/test-worker-commands/src/parts/PageObject/PageObject.js
+++ packages/test-worker-commands/src/parts/PageObject/PageObject.js
@@ -1,18 +1,31 @@
 import * as Assert from '../Assert/Assert.js'
 import * as ElectronAppState from '../ElectronAppState/ElectronAppState.js'
+import * as Expect from '../Expect/Expect.js'
 import * as GetPageObjectPath from '../GetPageObjectPath/GetPageObjectPath.js'
 import * as ImportScript from '../ImportScript/ImportScript.js'
 import * as PageObjectState from '../PageObjectState/PageObjectState.js'
 import { VError } from '../VError/VError.js'
+import * as WaitForVsCodeToBeReady from '../WaitForVsCodeToBeReady/WaitForVsCodeToBeReady.js'
 
 export const create = async (connectionId) => {
   try {
     Assert.number(connectionId)
     const pageObjectPath = GetPageObjectPath.getPageObjectPath()
     const pageObjectModule = await ImportScript.importScript(pageObjectPath)
-    const context = ElectronAppState.get(connectionId)
+    const electronApp = ElectronAppState.get(connectionId)
     ElectronAppState.remove(connectionId)
-    const pageObject = pageObjectModule.create(context)
+    const firstWindow = await electronApp.firstWindow()
+    await WaitForVsCodeToBeReady.waitForVsCodeToBeReady({
+      page: firstWindow,
+      expect: Expect.expect,
+    })
+    const pageObjectContext = {
+      page: firstWindow,
+      expect: Expect.expect,
+      VError,
+      electronApp,
+    }
+    const pageObject = pageObjectModule.create(pageObjectContext)
     PageObjectState.set(connectionId, pageObject)
   } catch (error) {
     throw new VError(error, `Failed to create page object`)
