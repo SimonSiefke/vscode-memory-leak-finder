51df226a7cbcf3177b8fccfbf4841fc2fa9ae8e5
Simon Siefke <simon.siefke@gmail.com>
2023-08-25 23:07:14 +0200

    fix: display full stdout/stderr on unexpected error

--- packages/cli/src/parts/HandleTestsUnexpectedError/HandleTestsUnexpectedError.js
+++ packages/cli/src/parts/HandleTestsUnexpectedError/HandleTestsUnexpectedError.js
@@ -3,11 +3,13 @@ import * as ModeType from '../ModeType/ModeType.js'
 import * as Process from '../Process/Process.js'
 import * as StdinDataState from '../StdinDataState/StdinDataState.js'
 import * as Stdout from '../Stdout/Stdout.js'
+import * as TestStateOutput from '../TestStateOutput/TestStateOutput.js'
 
 export const handleTestsUnexpectedError = (prettyError) => {
   const isWatchMode = StdinDataState.isWatchMode()
   const message = GetTestsUnexpectedErrorMessage.getTestsUnexpectedErrorMessage(prettyError)
-  Stdout.write(message)
+  const fullMessage = TestStateOutput.clearPending() + message
+  Stdout.write(fullMessage)
   StdinDataState.setState({
     ...StdinDataState.getState(),
     mode: ModeType.FinishedRunning,
