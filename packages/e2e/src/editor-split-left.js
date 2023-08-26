1f6c2a87af27a4745d4163a33500c91aa10d4b2e
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 14:48:29 +0200

    feature: add split left and split up tests (#251)

---
+++ packages/e2e/src/editor-split-left.js
@@ -0,0 +1,15 @@
+export const setup = async ({ Editor, Workspace }) => {
+  await Workspace.setFiles([
+    {
+      name: 'file.txt',
+      content: 'sample text',
+    },
+  ])
+  await Editor.closeAll()
+  await Editor.open('file.txt')
+}
+
+export const run = async ({ Editor }) => {
+  await Editor.splitLeft()
+  await Editor.closeAll()
+}
