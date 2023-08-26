3cdedd8deae92a7ea4bdd1ff1cae77cdb6879815
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 21:36:38 +0200

    feature: improve error message when page idle check fails

--- packages/page-object/src/parts/WaitForIdle/WaitForIdle.js
+++ packages/page-object/src/parts/WaitForIdle/WaitForIdle.js
@@ -1,9 +1,15 @@
 export const waitForIdle = async (page) => {
-  await page.evaluate({
-    expression: `await new Promise(resolve => {
+  try {
+    await page.evaluate({
+      expression: `await new Promise(resolve => {
   requestIdleCallback(resolve)
 })`,
-    awaitPromise: true,
-    replMode: true,
-  })
+      awaitPromise: true,
+      replMode: true,
+    })
+  } catch (error) {
+    // @ts-ignore
+    error.message = `Failed to verify that page is idle: ` + error.message
+    throw error
+  }
 }
