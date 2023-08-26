75a107c2110b365edc39fdedf9cdebc5c7553891
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 16:39:42 +0200

    feature: close all tabs for extension detail view test

--- packages/e2e/src/sample.extension-detail-view-open.js
+++ packages/e2e/src/sample.extension-detail-view-open.js
@@ -1,4 +1,5 @@
-export const setup = async ({ Extensions }) => {
+export const setup = async ({ Extensions, Editor }) => {
+  await Editor.closeAll()
   await Extensions.show()
   await Extensions.search('@builtin html')
   await Extensions.first.shouldBe('HTML Language Basics')
