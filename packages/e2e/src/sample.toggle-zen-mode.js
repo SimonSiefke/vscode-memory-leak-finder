e5bafad8279e9e8bb71c6e66cf682b58c4ddcbea
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 20:34:29 +0200

    remove unused code

--- packages/e2e/src/sample.toggle-zen-mode.js
+++
@@ -1,6 +0,0 @@
-export const skip = true
-
-export const run = async ({ QuickPick }) => {
-  await QuickPick.executeCommand('View: Toggle Zen Mode')
-  await QuickPick.executeCommand('View: Toggle Zen Mode')
-}
