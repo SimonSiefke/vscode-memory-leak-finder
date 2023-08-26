efc86ff0f4afd144982b7cbe72a086a6ca695ed5
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 19:37:58 +0200

    fix: mocking dialog

--- packages/page-object/src/parts/Electron/Electron.js
+++ packages/page-object/src/parts/Electron/Electron.js
@@ -6,10 +6,12 @@ export const create = ({ electronApp, VError }) => {
     async mockDialog(response) {
       try {
         const responseString = JSON.stringify(JSON.stringify(response))
-        await this.evaluate(`const electron = globalThis._VSCODE_NODE_MODULES["electron"];
-electron.dialog.showMessageBox = () => {
-  return JSON.parse(${responseString})
-}`)
+        await this.evaluate(`(() => {
+  const electron = globalThis._VSCODE_NODE_MODULES["electron"];
+  electron.dialog.showMessageBox = () => {
+    return JSON.parse(${responseString})
+  }
+})()`)
       } catch (error) {
         throw new VError(error, `Failed to mock electron dialog`)
       }
