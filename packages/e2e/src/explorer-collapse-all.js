d5f1861bf8c6146d9e890d2d8d2790325ec6544f
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 20:42:25 +0200

    feature: add collapse all test (#273)

---
+++ packages/e2e/src/explorer-collapse-all.js
@@ -0,0 +1,37 @@
+export const beforeSetup = async ({ Workspace, Explorer }) => {
+  await Workspace.setFiles([
+    {
+      name: 'folder-1/file-1.txt',
+      content: 'file content 1',
+    },
+    {
+      name: 'folder-1/file-2.txt',
+      content: 'file content 2',
+    },
+    {
+      name: 'folder-2/file-3.txt',
+      content: 'file content 3',
+    },
+    {
+      name: 'folder-2/file-4.txt',
+      content: 'file content 4',
+    },
+  ])
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('folder-1')
+}
+
+export const run = async ({ Explorer }) => {
+  await Explorer.expand('folder-1')
+  await Explorer.toHaveItem('file-1.txt')
+  await Explorer.toHaveItem('file-2.txt')
+  await Explorer.expand('folder-2')
+  await Explorer.toHaveItem('file-3.txt')
+  await Explorer.toHaveItem('file-4.txt')
+  await Explorer.collapseAll()
+  await Explorer.not.toHaveItem('file-1.txt')
+  await Explorer.not.toHaveItem('file-2.txt')
+  await Explorer.not.toHaveItem('file-3.txt')
+  await Explorer.not.toHaveItem('file-4.txt')
+  await Explorer.focus()
+}
