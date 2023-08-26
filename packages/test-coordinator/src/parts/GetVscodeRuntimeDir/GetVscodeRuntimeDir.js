fa38e708022164b8c694ed4c8533fd5af727cd73
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 22:40:33 +0200

    fix: custom runtime dir on gitpod (#288)

--- packages/test-coordinator/src/parts/GetVscodeRuntimeDir/GetVscodeRuntimeDir.js
+++ packages/test-coordinator/src/parts/GetVscodeRuntimeDir/GetVscodeRuntimeDir.js
@@ -1,7 +1,12 @@
 import { join } from '../Path/Path.js'
 import * as Root from '../Root/Root.js'
+import * as Env from '../Env/Env.js'
 
 export const getVscodeRuntimeDir = () => {
+  if (Env.env.GITPOD_INSTANCE_ID) {
+    // Gitpod doesn't support custom runtime dir
+    return ''
+  }
   const runtimeDir = join(Root.root, '.vscode-runtime-dir')
   // vscode doesn't allow a socket file that is longer than 107 characters
   // the socket file is ~30 characters longer than runtimeDir
