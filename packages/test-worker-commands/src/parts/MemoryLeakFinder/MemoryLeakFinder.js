a6b875bd62ef9e0136dff68de9852ca5725ebd49
Simon Siefke <simon.siefke@gmail.com>
2023-08-26 22:20:01 +0200

    feature: integrate memory leak finder again

---
+++ packages/test-worker-commands/src/parts/MemoryLeakFinder/MemoryLeakFinder.js
@@ -0,0 +1,11 @@
+const loadMemoryLeakFinder = async () => {
+  const MemoryLeakFinder = await import('../../../../memory-leak-finder/src/index.js')
+  return MemoryLeakFinder
+}
+
+export const create = async (context) => {
+  const MemoryLeakFinder = await loadMemoryLeakFinder()
+  const session = context.firstWindow.rpc
+  const measure = MemoryLeakFinder.combine(MemoryLeakFinder.Measures.MeasureEventListeners.create(session))
+  return measure
+}
