dd84739a6e0924da5033c95d9f736867951a80d2
Simon Siefke <simon.siefke@gmail.com>
2023-08-27 08:45:55 +0200

    try to make explorer rename test less flaky

--- packages/e2e/src/explorer-rename-file.js
+++ packages/e2e/src/explorer-rename-file.js
@@ -1,6 +1,7 @@
 export const skip = process.platform === 'darwin'
 
 export const beforeSetup = async ({ Workspace, Explorer }) => {
+  await Explorer.focus()
   await Workspace.setFiles([
     {
       name: 'file-1.txt',
@@ -15,7 +16,7 @@ export const beforeSetup = async ({ Workspace, Explorer }) => {
       content: '',
     },
   ])
-  await Explorer.focus()
+  await Explorer.refresh()
   await Explorer.shouldHaveItem('file-1.txt')
   await Explorer.shouldHaveItem('file-2.txt')
   await Explorer.shouldHaveItem('file-3.txt')
