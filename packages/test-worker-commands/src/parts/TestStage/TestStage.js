d2eaea2f23358dc43f9238fec312ef7569f49fbc
Simon Siefke <simon.siefke@gmail.com>
2023-08-27 09:14:10 +0200

    feature: remove backups before test run

--- packages/test-worker-commands/src/parts/TestStage/TestStage.js
+++ packages/test-worker-commands/src/parts/TestStage/TestStage.js
@@ -10,6 +10,12 @@ export const setup = async (module, context) => {
   }
 }
 
+export const teardown = async (module, context) => {
+  if (module.teardown) {
+    await module.teardown(context)
+  }
+}
+
 export const run = async (module, context) => {
   if (module.run) {
     await module.run(context)
