afae369079f420c9608b0a983af06db70e355d24
Simon Siefke <simon.siefke@gmail.com>
2023-08-27 13:27:56 +0200

    feature: add function to start video recording worker

---
+++ packages/test-coordinator/src/parts/VideoRecordingWorkerPath/VideoRecordingWorkerPath.js
@@ -0,0 +1,4 @@
+import { join } from 'path'
+import * as Root from '../Root/Root.js'
+
+export const videoRecordingWorkerPath = join(Root.root, 'packages', 'video-recording-worker', 'bin', 'video-recording-worker.js')
