fd24f93531b587f4ee1620d3566486a922bb538a
Simon Siefke <simon.siefke@gmail.com>
2023-08-27 14:25:39 +0200

    feature: launch video recording worker (#316)

--- packages/test-coordinator/src/parts/VideoRecording/VideoRecording.js
+++ packages/test-coordinator/src/parts/VideoRecording/VideoRecording.js
@@ -2,11 +2,13 @@ import * as Assert from '../Assert/Assert.js'
 import * as VideoRecordingWorker from '../VideoRecordingWorker/VideoRecordingWorker.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import * as Command from '../Command/Command.js'
+import * as Callback from '../Callback/Callback.js'
 
 export const start = async (devtoolsWebsocketUrl) => {
   Assert.string(devtoolsWebsocketUrl)
-  const ipc = VideoRecordingWorker.launch()
-  HandleIpc.handleIpc(ipc)
-  await JsonRpc.invoke(ipc, 'ConnectDevtools.connectDevtools')
+  const ipc = await VideoRecordingWorker.start()
+  HandleIpc.handleIpc(ipc, Command.execute, Callback.resolve)
+  await JsonRpc.invoke(ipc, 'ConnectDevtools.connectDevtools', devtoolsWebsocketUrl)
   await JsonRpc.invoke(ipc, 'VideoRecording.start')
 }
--- packages/video-recording-worker/src/parts/HandleIpc/HandleIpc.js
+++ packages/video-recording-worker/src/parts/HandleIpc/HandleIpc.js
@@ -1,8 +1,6 @@
-import * as Assert from '../Assert/Assert.js'
 import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.js'
 
 export const handleIpc = (ipc, execute, resolve) => {
-  Assert.object(ipc)
   const handleMessage = (message) => {
     return HandleJsonRpcMessage.handleJsonRpcMessage(ipc, message, execute, resolve)
   }
