3c179a99e9c0b4c18e518bf389d66b9fd00bf9ef
Simon Siefke <simon.siefke@gmail.com>
2023-08-27 09:19:38 +0200

    try to fix flaky markdown preview test (#304)

--- packages/page-object/src/parts/WebView/WebView.js
+++ packages/page-object/src/parts/WebView/WebView.js
@@ -2,7 +2,7 @@
 // where sometimes quickpick opens, but the webview is focused
 // and then quickpick doesn't work
 const waitForExtraIdle = async (page) => {
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 30; i++) {
     await page.waitForIdle()
   }
 }
