e3722d13942af96f14a43f0d69b5919c2005497a
Simon Siefke <simon.siefke@gmail.com>
2023-09-05 23:25:34 +0200

    feature: rename .videos to .vscode-videos (#329)

--- packages/video-recording-worker/src/parts/HandleFrame/HandleFrame.js
+++ packages/video-recording-worker/src/parts/HandleFrame/HandleFrame.js
@@ -6,11 +6,12 @@ import { DevtoolsProtocolPage } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 
 let i = 1
 
+const videosPath = join(Root.root, '.vscode-videos')
+
 export const handleFrame = async (message) => {
   const { data, metadata, sessionId } = message.params
   const session = SessionState.getSession(message.sessionId)
   await DevtoolsProtocolPage.screencastFrameAck(session.rpc, { sessionId })
-  const videosPath = join(Root.root, '.videos')
   await mkdir(videosPath, { recursive: true })
   const jpegPath = join(videosPath, `${i++}.jpeg`)
   await writeFile(jpegPath, data, 'base64')
