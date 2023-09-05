e205edf588d9bb6861a85ac62c6bdb8d99c44d1d
Simon Siefke <simon.siefke@gmail.com>
2023-09-05 23:25:07 +0200

    feature: launch memory leak worker (#331)

---
+++ packages/test-coordinator/src/parts/MemoryLeakWorker/MemoryLeakWorker.js
@@ -0,0 +1,13 @@
+import * as Assert from '../Assert/Assert.js'
+import * as VideoRecordingWorker from '../VideoRecordingWorker/VideoRecordingWorker.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import * as Command from '../Command/Command.js'
+import * as Callback from '../Callback/Callback.js'
+
+export const startWorker = async (devtoolsWebsocketUrl) => {
+  Assert.string(devtoolsWebsocketUrl)
+  const ipc = await VideoRecordingWorker.start()
+  HandleIpc.handleIpc(ipc, Command.execute, Callback.resolve)
+  await JsonRpc.invoke(ipc, 'ConnectDevtools.connectDevtools', devtoolsWebsocketUrl)
+}
