d41d5d423d719cde17dc90fa299965c150f400c4
Simon Siefke <simon.siefke@gmail.com>
2023-09-08 21:03:13 +0200

    refactor: remove unused code

--- packages/test-worker-commands/src/parts/CommandMap/CommandMap.js
+++ packages/test-worker-commands/src/parts/CommandMap/CommandMap.js
@@ -1,9 +1,5 @@
 import * as ConnectDevtools from '../ConnectDevtools/ConnectDevtools.js'
 import * as ConnectElectron from '../ConnectElectron/ConnectElectron.js'
-import * as MemoryLeakFinderCompare from '../MemoryLeakFinderCompare/MemoryLeakFinderCompare.js'
-import * as MemoryLeakFinderSetup from '../MemoryLeakFinderSetup/MemoryLeakFinderSetup.js'
-import * as MemoryLeakFinderStart from '../MemoryLeakFinderStart/MemoryLeakFinderStart.js'
-import * as MemoryLeakFinderStop from '../MemoryLeakFinderStop/MemoryLeakFinderStop.js'
 import * as PageObject from '../PageObject/PageObject.js'
 import * as RunTest from '../RunTest/RunTest.js'
 import * as SetupTest from '../SetupTest/SetupTest.js'
@@ -12,10 +8,6 @@ import * as TestWorkerCommandType from '../TestWorkerCommandType/TestWorkerComma
 export const commandMap = {
   [TestWorkerCommandType.ConnectDevtools]: ConnectDevtools.connectDevtools,
   [TestWorkerCommandType.ConnectElectron]: ConnectElectron.connectElectron,
-  [TestWorkerCommandType.MemoryLeakFinderCompare]: MemoryLeakFinderCompare.compare,
-  [TestWorkerCommandType.MemoryLeakFinderSetup]: MemoryLeakFinderSetup.setup,
-  [TestWorkerCommandType.MemoryLeakFinderStart]: MemoryLeakFinderStart.start,
-  [TestWorkerCommandType.MemoryLeakFinderStop]: MemoryLeakFinderStop.stop,
   [TestWorkerCommandType.PageObjectCreate]: PageObject.create,
   [TestWorkerCommandType.RunTest]: RunTest.runTest,
   [TestWorkerCommandType.SetupTest]: SetupTest.setupTest,
--- packages/test-worker-commands/src/parts/MemoryLeakFinderStop/MemoryLeakFinderStop.js
+++
@@ -1,9 +0,0 @@
-import * as MemoryLeakFinderState from '../MemoryLeakFinderState/MemoryLeakFinderState.js'
-
-export const stop = async (instanceId) => {
-  const measure = MemoryLeakFinderState.get(instanceId)
-  if (!measure) {
-    throw new Error(`no measure found`)
-  }
-  return measure.stop()
-}
