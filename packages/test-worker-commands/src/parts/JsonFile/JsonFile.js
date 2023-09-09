24396c23875ae73a185b7f5f95ab1e794fe5bfc9
Simon Siefke <simon.siefke@gmail.com>
2023-09-09 21:45:29 +0200

    refactor: remove unused code (#358)

--- packages/test-worker-commands/src/parts/JsonFile/JsonFile.js
+++
@@ -1,6 +0,0 @@
-import { writeFile } from 'node:fs/promises'
-
-export const writeJson = async (path, json) => {
-  const content = JSON.stringify(json, null, 2) + '\n'
-  await writeFile(path, content)
-}
