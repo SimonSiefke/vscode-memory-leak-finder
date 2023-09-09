678f74c48ec20464818227b31f14f75839054e33
Simon Siefke <simon.siefke@gmail.com>
2023-09-09 14:35:19 +0200

    feature: compare trimmed value when comparing css

--- packages/test-worker-commands/src/parts/ExpectLocatorToHaveCss/ExpectLocatorToHaveCss.js
+++ packages/test-worker-commands/src/parts/ExpectLocatorToHaveCss/ExpectLocatorToHaveCss.js
@@ -1,5 +1,8 @@
 import * as ExpectLocatorSingleElementCondition from '../ExpectLocatorSingleElementCondition/ExpectLocatorSingleElementCondition.js'
 
 export const toHaveCss = (locator, key, value, options = {}) => {
+  if (typeof value === 'string') {
+    value = value.trim()
+  }
   return ExpectLocatorSingleElementCondition.checkSingleElementCondition('toHaveCss', locator, { key, value, ...options })
 }
