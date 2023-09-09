d320f6a9a9b7c523114796f557f4af4ebd6ec4a6
Simon Siefke <simon.siefke@gmail.com>
2023-09-09 09:21:06 +0200

    increase interactive timeout

--- packages/test-worker-commands/src/parts/TimeoutConstants/TimeoutConstants.js
+++ packages/test-worker-commands/src/parts/TimeoutConstants/TimeoutConstants.js
@@ -7,4 +7,4 @@ export const SessionState = 500
 export const Target = 8000
 export const UtilityExecutionContext = IsWindows.IsWindows ? 16000 : 8500
 export const WaitForDebuggerToBePaused = 1000
-export const InteractiveTime = 9000
+export const InteractiveTime = 20_000
