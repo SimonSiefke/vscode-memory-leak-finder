ebad32da88f0deb4683d945c45a354afc2023dbf
Simon Siefke <simon.siefke@gmail.com>
2023-09-09 20:48:11 +0200

    feature: write memory leak results to disk (#359)

--- packages/test-coordinator/src/parts/JsonFile/JsonFile.js
+++ packages/test-coordinator/src/parts/JsonFile/JsonFile.js
@@ -1,6 +1,13 @@
-import { writeFile } from 'node:fs/promises'
+import { dirname } from 'node:path'
+import { VError } from '../VError/VError.js'
+import { mkdir, writeFile } from 'node:fs/promises'
 
 export const writeJson = async (path, json) => {
-  const content = JSON.stringify(json, null, 2) + '\n'
-  await writeFile(path, content)
+  try {
+    const content = JSON.stringify(json, null, 2) + '\n'
+    await mkdir(dirname(path), { recursive: true })
+    await writeFile(path, content)
+  } catch (error) {
+    throw new VError(error, `Failed to write json`)
+  }
 }
