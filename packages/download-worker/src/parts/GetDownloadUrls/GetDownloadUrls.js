0bbe829a10761c9662f6de5f05ae4cbb01f2d656
Simon Siefke <simon.siefke@gmail.com>
2023-09-10 17:18:53 +0200

    refactor: simplify getDownloadUrls

--- packages/download-worker/src/parts/GetDownloadUrls/GetDownloadUrls.js
+++ packages/download-worker/src/parts/GetDownloadUrls/GetDownloadUrls.js
@@ -1,16 +1,14 @@
 // based on microsoft/playwright/packages/playwright-core/src/server/registry/index.ts (License Apache 2.0)
 
-import { format } from 'node:util'
 import * as CreateUrls from '../CreateUrls/CreateUrls.js'
-import * as DownloadPaths from '../DownloadPaths/DownloadPaths.js'
+import * as GetDownloadUrl from '../GetDownloadUrl/GetDownloadUrl.js'
 import * as PlaywrightCdnMirrors from '../PlaywrightCdnMirrors/PlaywrightCdnMirrors.js'
 
 export const getDownloadUrls = (name, revision, hostPlatform) => {
-  const template = DownloadPaths.downloadPaths[name][hostPlatform]
-  if (!template) {
+  const downloadPath = GetDownloadUrl.getDownloadUrl(name, revision, hostPlatform)
+  if (!downloadPath) {
     return []
   }
-  const downloadPath = format(template, revision)
   const urls = CreateUrls.createUrls(PlaywrightCdnMirrors.playwrightCdnMirrors, downloadPath)
   return urls
 }
