774ced19afdd72b3b9bb16c7f9945d5e7e780062
Simon Siefke <simon.siefke@gmail.com>
2023-09-10 13:52:26 +0200

    feature: improve measuring event listeners

--- packages/memory-leak-finder/src/parts/GetEventListenerKey/GetEventListenerKey.js
+++ packages/memory-leak-finder/src/parts/GetEventListenerKey/GetEventListenerKey.js
@@ -1,3 +1,7 @@
+import * as Assert from '../Assert/Assert.js'
+
 export const getEventListenerKey = (listener) => {
+  Assert.number(listener.lineNumber)
+  Assert.number(listener.columnNumber)
   return `${listener.lineNumber}:${listener.columnNumber}`
 }
