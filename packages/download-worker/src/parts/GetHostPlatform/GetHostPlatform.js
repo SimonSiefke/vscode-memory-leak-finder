9f15e19d574e2944f6422003cfe04f68b072cf62
Simon Siefke <simon.siefke@gmail.com>
2023-09-10 17:16:26 +0200

    feature: pass executablePath when downloading executable (#382)

--- packages/download-worker/src/parts/GetHostPlatform/GetHostPlatform.js
+++ packages/download-worker/src/parts/GetHostPlatform/GetHostPlatform.js
@@ -1,12 +1,10 @@
 // based on microsoft/playwright/packages/playwright-core/src/utils/hostPlatform.ts (License Apache 2.0)
 
 import { VError } from '@lvce-editor/verror'
-import os from 'node:os'
 import * as GetHostPlatformModule from '../GetHostPlatformModule/GetHostPlatformModule.js'
 
-export const getHostPlatform = async () => {
+export const getHostPlatform = async (platform) => {
   try {
-    const platform = os.platform()
     const module = await GetHostPlatformModule.getHostPlatformModule(platform)
     const hostPlatform = await module.getHostPlatform()
     return hostPlatform
