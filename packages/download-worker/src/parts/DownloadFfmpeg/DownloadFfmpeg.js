816c6f91f19a4eee7eae54c0e36a43864c212971
Simon Siefke <simon.siefke@gmail.com>
2023-09-10 16:59:31 +0200

    feature: add browser config from playwright to download the right ffm… (#378)

--- packages/download-worker/src/parts/DownloadFfmpeg/DownloadFfmpeg.js
+++ packages/download-worker/src/parts/DownloadFfmpeg/DownloadFfmpeg.js
@@ -1,9 +1,14 @@
 import * as DownloadExecutable from '../DownloadExecutable/DownloadExecutable.js'
 import * as GetHostPlatform from '../GetHostPlatform/GetHostPlatform.js'
+import * as IsFfmpeg from '../IsFfmpeg/IsFfmpeg.js'
+import * as Registry from '../Registry/Registry.js'
 
 export const downloadFfmpeg = async () => {
   const hostPlatform = await GetHostPlatform.getHostPlatform()
-  const name = 'ffmpeg'
-  const revision = '1009'
-  await DownloadExecutable.downloadExecutable(name, revision, hostPlatform)
+  const browsers = Registry.load()
+  const ffmpeg = browsers.find(IsFfmpeg.isFfmpeg)
+  if (!ffmpeg) {
+    throw new Error(`no ffmpeg configuration found`)
+  }
+  await DownloadExecutable.downloadExecutable(ffmpeg.name, ffmpeg.revision, hostPlatform)
 }
