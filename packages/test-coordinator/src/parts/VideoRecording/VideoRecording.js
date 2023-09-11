31ffbdc1642945265fa30922708a2c0688e551ff
Simon Siefke <simon.siefke@gmail.com>
2023-09-11 20:38:24 +0200

    feature: store video in videos directory instead of tmp folder

--- packages/test-coordinator/src/parts/VideoRecording/VideoRecording.js
+++ packages/test-coordinator/src/parts/VideoRecording/VideoRecording.js
@@ -1,14 +1,17 @@
+import { join } from 'path'
 import * as Assert from '../Assert/Assert.js'
-import * as VideoRecordingWorker from '../VideoRecordingWorker/VideoRecordingWorker.js'
+import * as Callback from '../Callback/Callback.js'
+import * as Command from '../Command/Command.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-import * as Command from '../Command/Command.js'
-import * as Callback from '../Callback/Callback.js'
+import * as Root from '../Root/Root.js'
+import * as VideoRecordingWorker from '../VideoRecordingWorker/VideoRecordingWorker.js'
 
 export const start = async (devtoolsWebsocketUrl) => {
   Assert.string(devtoolsWebsocketUrl)
   const ipc = await VideoRecordingWorker.start()
   HandleIpc.handleIpc(ipc, Command.execute, Callback.resolve)
   await JsonRpc.invoke(ipc, 'ConnectDevtools.connectDevtools', devtoolsWebsocketUrl)
-  await JsonRpc.invoke(ipc, 'VideoRecording.start')
+  const outFile = join(Root.root, '.vscode-videos', 'video.webm')
+  await JsonRpc.invoke(ipc, 'VideoRecording.start', outFile)
 }
