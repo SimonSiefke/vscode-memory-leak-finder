2ba0962fdc7df499ad3398c6955e2881dc1bb02c
Simon Siefke <simon.siefke@gmail.com>
2023-09-11 20:57:19 +0200

    fix: don't crash when downloading ffmpeg fails, print warning instead

--- packages/download-worker/src/parts/PostInstall/PostInstall.js
+++ packages/download-worker/src/parts/PostInstall/PostInstall.js
@@ -1,7 +1,12 @@
+import { VError } from '@lvce-editor/verror'
 import * as DownloadFfmpeg from '../DownloadFfmpeg/DownloadFfmpeg.js'
 
 const main = async () => {
-  await DownloadFfmpeg.downloadFfmpeg()
+  try {
+    await DownloadFfmpeg.downloadFfmpeg()
+  } catch (error) {
+    console.warn(new VError(error, `Failed to download ffmpeg`))
+  }
 }
 
 main()
