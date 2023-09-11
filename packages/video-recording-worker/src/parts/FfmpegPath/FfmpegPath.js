841702b1858f9d757a9b4926282f35fe9e4041a1
Simon Siefke <simon.siefke@gmail.com>
2023-09-11 20:48:52 +0200

    refactor: remove unused code (#391)

--- packages/video-recording-worker/src/parts/FfmpegPath/FfmpegPath.js
+++
@@ -1,4 +0,0 @@
-import { join } from 'path'
-import * as Root from '../Root/Root.js'
-
-export const ffmpegPath = join(Root.root, 'packages', 'vscode-linux-x64-1.82.0', 'bin', 'video-recording-worker.js')
