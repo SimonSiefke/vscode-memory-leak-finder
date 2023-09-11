7754961d7435af87ef246842ca231a255ea684a9
Simon Siefke <simon.siefke@gmail.com>
2023-09-11 20:58:59 +0200

    feature: use dynamic path for ffmpeg, depending on platform (#394)

--- packages/video-recording-worker/src/parts/GetFfmpegPath/GetFfmpegPath.js
+++ packages/video-recording-worker/src/parts/GetFfmpegPath/GetFfmpegPath.js
@@ -1,6 +1,8 @@
 import { join } from 'path'
+import * as GetExecutablePath from '../GetExecutablePath/GetExecutablePath.js'
 import * as Root from '../Root/Root.js'
 
 export const getFfmpegPath = () => {
-  return join(Root.root, '.vscode-ffmpeg', 'ffmpeg-linux')
+  const executablePath = GetExecutablePath.getExecutablePath('ffmpeg')
+  return join(Root.root, '.vscode-ffmpeg', executablePath)
 }
