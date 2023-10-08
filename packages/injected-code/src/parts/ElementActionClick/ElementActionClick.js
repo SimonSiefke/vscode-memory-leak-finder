9beb64b17b0c6dc2f5464de31a93f353afff1f75
Simon Siefke <simon.siefke@gmail.com>
2023-10-08 20:11:31 +0200

    fix: extensions context menu test failing when extensions input is al… (#428)

--- packages/injected-code/src/parts/ElementActionClick/ElementActionClick.js
+++ packages/injected-code/src/parts/ElementActionClick/ElementActionClick.js
@@ -1,9 +1,11 @@
 import * as DispatchEvent from '../DispatchEvent/DispatchEvent.js'
 
 export const click = (element, options) => {
+  DispatchEvent.pointerDown(element, options)
   DispatchEvent.mouseDown(element, options)
   DispatchEvent.click(element, options)
   DispatchEvent.mouseUp(element, options)
+  DispatchEvent.pointerUp(element, options)
   if (options.button === 2 /* right */ || options.button === 'right') {
     DispatchEvent.contextMenu(element, options)
   }
