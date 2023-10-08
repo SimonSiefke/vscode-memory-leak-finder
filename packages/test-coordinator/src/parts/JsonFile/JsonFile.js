b22f2f32ac1a1f573244aeb24ca7be339c132211
Simon Siefke <simon.siefke@gmail.com>
2023-10-08 16:59:01 +0200

    feature: set empty extension marketplace url to avoid requests to mar… (#424)

--- packages/test-coordinator/src/parts/JsonFile/JsonFile.js
+++ packages/test-coordinator/src/parts/JsonFile/JsonFile.js
@@ -1,6 +1,6 @@
+import { mkdir, readFile, writeFile } from 'node:fs/promises'
 import { dirname } from 'node:path'
 import { VError } from '../VError/VError.js'
-import { mkdir, writeFile } from 'node:fs/promises'
 
 export const writeJson = async (path, json) => {
   try {
@@ -9,5 +9,14 @@ export const writeJson = async (path, json) => {
     await writeFile(path, content)
   } catch (error) {
     throw new VError(error, `Failed to write json`)
+  }
+}
+
+export const readJson = async (path) => {
+  try {
+    const content = await readFile(path, 'utf8')
+    return JSON.parse(content)
+  } catch (error) {
+    throw new VError(error, `Failed to read json`)
   }
 }
