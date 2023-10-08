248fa85b7eb0655b51a5b3702fa197820725ebd4
Simon Siefke <simon.siefke@gmail.com>
2023-10-08 13:58:37 +0200

    feature: set empty extension marketplace url to avoid requests to marketplace during tests

--- packages/test-coordinator/src/parts/DownloadAndUnzipVscode/DownloadAndUnzipVscode.js
+++ packages/test-coordinator/src/parts/DownloadAndUnzipVscode/DownloadAndUnzipVscode.js
@@ -1,10 +1,14 @@
+import { dirname, join } from 'node:path'
+import * as AdjustVscodeProductJson from '../AdjustVscodeProductJson/AdjustVscodeProductJson.js'
 import * as Env from '../Env/Env.js'
+import * as JsonFile from '../JsonFile/JsonFile.js'
 import * as VscodeTestCachePath from '../VscodeTestCachePath/VscodeTestCachePath.js'
 
 /**
  * @param {string} vscodeVersion
  */
 export const downloadAndUnzipVscode = async (vscodeVersion) => {
+  console.log({ vscodeVersion })
   if (Env.env.VSCODE_PATH) {
     return Env.env.VSCODE_PATH
   }
@@ -13,5 +17,10 @@ export const downloadAndUnzipVscode = async (vscodeVersion) => {
     version: vscodeVersion,
     cachePath: VscodeTestCachePath.vscodeTestCachePath,
   })
+  const folderPath = dirname(path)
+  const productPath = join(folderPath, 'resources', 'app', 'product.json')
+  const productJson = await JsonFile.readJson(productPath)
+  const newProductJson = AdjustVscodeProductJson.adjustVscodeProductJson(productJson)
+  await JsonFile.writeJson(productPath, newProductJson)
   return path
 }
