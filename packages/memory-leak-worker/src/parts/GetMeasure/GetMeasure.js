384597701f1f61b72159af970719d17cbd1ccb53
Simon Siefke <simon.siefke@gmail.com>
2023-10-11 20:01:19 +0200

    feature: add detached dom node count measure

--- packages/memory-leak-worker/src/parts/GetMeasure/GetMeasure.js
+++ packages/memory-leak-worker/src/parts/GetMeasure/GetMeasure.js
@@ -2,6 +2,8 @@ export const getMeasure = (MemoryLeakFinder, measureId) => {
   switch (measureId) {
     case 'event-listeners':
       return MemoryLeakFinder.Measures.MeasureEventListeners
+    case 'detached-dom-node-count':
+      return MemoryLeakFinder.Measures.MeasureDetachedDomNodeCount
     default:
       return MemoryLeakFinder.Measures.MeasureEventListenerCount
   }
