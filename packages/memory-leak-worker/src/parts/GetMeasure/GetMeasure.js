49b7a75fb97a31833c760742dfe0ac0496b991bf
Simon Siefke <simon.siefke@gmail.com>
2023-10-11 20:42:17 +0200

    feature: expand measure map

--- packages/memory-leak-worker/src/parts/GetMeasure/GetMeasure.js
+++ packages/memory-leak-worker/src/parts/GetMeasure/GetMeasure.js
@@ -4,6 +4,14 @@ export const getMeasure = (MemoryLeakFinder, measureId) => {
       return MemoryLeakFinder.Measures.MeasureEventListeners
     case 'detached-dom-node-count':
       return MemoryLeakFinder.Measures.MeasureDetachedDomNodeCount
+    case 'set-timeout':
+      return MemoryLeakFinder.Measures.SetTimeout
+    case 'promise-count':
+      return MemoryLeakFinder.Measures.PromiseCount
+    case 'resize-observer-count':
+      return MemoryLeakFinder.Measures.ResizeObserverCount
+    case 'intersection-observer-count':
+      return MemoryLeakFinder.Measures.IntersectionOberserverCount
     default:
       return MemoryLeakFinder.Measures.MeasureEventListenerCount
   }
