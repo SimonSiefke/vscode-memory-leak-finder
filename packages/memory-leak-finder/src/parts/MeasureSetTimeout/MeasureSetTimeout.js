732f7e2ee0ebdc256f2a94f2f4c9a8bac3b643ad
Simon Siefke <simon.siefke@gmail.com>
2023-10-11 20:04:52 +0200

    feature: add setTimeout measure

---
+++ packages/memory-leak-finder/src/parts/MeasureSetTimeout/MeasureSetTimeout.js
@@ -0,0 +1,27 @@
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as TimeoutCount from '../TimeoutCount/TimeoutCount.js'
+
+export const id = MeasureId.SetTimeout
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
+}
+
+export const start = async (session, objectGroup) => {
+  await TimeoutCount.startTrackingTimeouts(session, objectGroup)
+  return TimeoutCount.getTimeoutCount(session)
+}
+
+export const stop = async (session, objectGroup) => {
+  await TimeoutCount.stopTrackingTimeouts(session, objectGroup)
+  return TimeoutCount.getTimeoutCount(session)
+}
+
+export const compare = (before, after) => {
+  return {
+    before,
+    after,
+  }
+}
