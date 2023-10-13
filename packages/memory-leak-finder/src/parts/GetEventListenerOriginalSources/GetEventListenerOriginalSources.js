d245d8df6f87d8b2cf71e2bc666a866defd97c29
Simon Siefke <simon.siefke@gmail.com>
2023-10-13 21:26:28 +0200

    feature: improve performance of source resolving, don't parse source … (#452)

---
+++ packages/memory-leak-finder/src/parts/GetEventListenerOriginalSources/GetEventListenerOriginalSources.js
@@ -0,0 +1,88 @@
+import * as GetCleanPosition from '../GetCleanPosition/GetCleanPosition.js'
+import * as LoadSourceMap from '../LoadSourceMap/LoadSourceMap.js'
+import * as SourceMap from '../SourceMap/SourceMap.js'
+
+const RE_LINE_COLUMN = /(\d+):(\d+)/
+
+const parseLineAndColumn = (line) => {
+  const match = line.match(RE_LINE_COLUMN)
+  if (!match) {
+    return undefined
+  }
+  return {
+    line: parseInt(match[1]),
+    column: parseInt(match[2]),
+  }
+}
+
+const emptySourceMapUrl = {
+  sourceMapUrl: '',
+  line: 0,
+  column: 0,
+}
+
+const getSourceMapUrl = (eventListener) => {
+  const { stack, sourceMaps } = eventListener
+  if (!stack || !sourceMaps) {
+    return emptySourceMapUrl
+  }
+  const firstStackLine = stack[0]
+  const parsed = parseLineAndColumn(firstStackLine)
+  if (!parsed) {
+    return emptySourceMapUrl
+  }
+  const sourceMapUrl = sourceMaps[0]
+  return {
+    sourceMapUrl,
+    line: parsed.line,
+    column: parsed.column,
+  }
+}
+
+const getSourceMapUrlMap = (eventListeners) => {
+  const map = Object.create(null)
+  for (const eventListener of eventListeners) {
+    const { sourceMapUrl, line, column } = getSourceMapUrl(eventListener)
+    map[sourceMapUrl] ||= []
+    map[sourceMapUrl].push({ line, column })
+  }
+  return map
+}
+
+const getCleanPositionsMap = async (map) => {
+  const cleanPositionMap = Object.create(null)
+  for (const [key, value] of Object.entries(map)) {
+    const sourceMap = await LoadSourceMap.loadSourceMap(key)
+    const originalPositions = await SourceMap.getOriginalPositions(sourceMap, value)
+    const cleanPositions = originalPositions.map(GetCleanPosition.getCleanPosition)
+    cleanPositionMap[key] = cleanPositions
+  }
+  return cleanPositionMap
+}
+
+const getCleanEventlisteners = (cleanPositionMap, eventListeners) => {
+  const newEventListeners = []
+  const indexMap = Object.create(null)
+  for (const eventListener of eventListeners) {
+    const { sourceMapUrl } = getSourceMapUrl(eventListener)
+    indexMap[sourceMapUrl] ||= 0
+    const index = indexMap[sourceMapUrl]++
+    const cleanPosition = cleanPositionMap[sourceMapUrl][index]
+    if (cleanPosition) {
+      newEventListeners.push({
+        ...eventListener,
+        originalStack: [`${cleanPosition.source}:${cleanPosition.line}:${cleanPosition.column}`],
+      })
+    } else {
+      newEventListeners.push(eventListener)
+    }
+  }
+  return newEventListeners
+}
+
+export const getEventListenerOriginalSources = async (eventListeners) => {
+  const map = getSourceMapUrlMap(eventListeners)
+  const cleanPositionMap = await getCleanPositionsMap(map)
+  const cleanEventListeners = getCleanEventlisteners(cleanPositionMap, eventListeners)
+  return cleanEventListeners
+}
--- packages/memory-leak-finder/src/parts/SourceMap/SourceMap.js
+++ packages/memory-leak-finder/src/parts/SourceMap/SourceMap.js
@@ -18,3 +18,21 @@ export const getOriginalPosition = async (sourceMap, line, column) => {
     name: originalPosition.name,
   }
 }
+
+export const getOriginalPositions = async (sourceMap, positions) => {
+  Assert.object(sourceMap)
+  Assert.array(positions)
+  const originalPositions = await SourceMapConsumer.with(sourceMap, null, (consumer) => {
+    const originalPositions = []
+    for (const position of positions) {
+      originalPositions.push(
+        consumer.originalPositionFor({
+          line: position.line + 1,
+          column: position.column + 1,
+        }),
+      )
+    }
+    return originalPositions
+  })
+  return originalPositions
+}
