e9de7dc6ec9b35069d4bbefcb50292743f23af79
Simon Siefke <simon.siefke@gmail.com>
2023-10-13 20:36:14 +0200

    feature: add mutation observer count measure

--- packages/memory-leak-finder/src/parts/Measures/Measures.js
+++ packages/memory-leak-finder/src/parts/Measures/Measures.js
@@ -1,3 +1,4 @@
+export * as MeasureDetachedDomNodeCount from '../MeasureDetachedDomNodeCount/MeasureDetachedDomNodeCount.js'
 export * as MeasureEventListenerCount from '../MeasureEventListenerCount/MeasureEventListenerCount.js'
 export * as MeasureEventListeners from '../MeasureEventListeners/MeasureEventListeners.js'
-export * as MeasureDetachedDomNodeCount from '../MeasureDetachedDomNodeCount/MeasureDetachedDomNodeCount.js'
+export * as MeasureMutationObserverCount from '../MeasureMutationObserverCount/MeasureMutationObserverCount.js'
