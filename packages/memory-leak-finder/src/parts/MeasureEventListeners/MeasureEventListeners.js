bd4b7f0fd28eed428d178c38ca96a5d1027de726
Simon Siefke <simon.siefke@gmail.com>
2023-10-14 08:48:05 +0200

    feature: deduplicate event listeners to make output more readable

--- packages/memory-leak-finder/src/parts/MeasureEventListeners/MeasureEventListeners.js
+++ packages/memory-leak-finder/src/parts/MeasureEventListeners/MeasureEventListeners.js
@@ -1,3 +1,4 @@
+import * as DeduplicateEventListeners from '../DeduplicateEventListeners/DeduplicateEventListeners.js'
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 import * as GetEventListenerKey from '../GetEventListenerKey/GetEventListenerKey.js'
 import * as GetEventListenerOriginalSourcesCached from '../GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js'
@@ -61,7 +62,9 @@ export const compare = async (before, after) => {
       map[key]--
     }
   }
-  const cleanLeakedEventListeners = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(leaked)
+  const deduplicatedEventListeners = DeduplicateEventListeners.deduplicateEventListeners(leaked)
+  const cleanLeakedEventListeners =
+    await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(deduplicatedEventListeners)
   return cleanLeakedEventListeners
 }
 
