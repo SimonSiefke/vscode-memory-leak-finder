c99508b05689ea63d223eeb368c7b356e0331f4c
Simon Siefke <simon.siefke@gmail.com>
2023-11-05 13:12:10 +0100

    refactor: extract testPrefix constants into separate file (#495)

---
+++ packages/cli/src/parts/TestPrefix/TestPrefix.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk'
+import * as TestPrefixText from '../TestPrefixText/TestPrefixText.js'
+
+export const Pass = chalk.reset.inverse.bold.green(TestPrefixText.Pass)
+
+export const Leak = chalk.reset.inverse.blue(TestPrefixText.Leak)
+
+export const Runs = chalk.reset.inverse.yellow.bold(TestPrefixText.Runs)
+
+export const Fail = chalk.reset.inverse.bold.red(TestPrefixText.Fail)
