68660b3d106c4518d29e02bf27adc84b50a9ab83
Simon Siefke <simon.siefke@gmail.com>
2023-11-07 19:15:20 +0100

    refactor: move indent function into separate file

--- packages/cli/src/parts/GetTestsUnexpectedErrorMessage/GetTestsUnexpectedErrorMessage.js
+++ packages/cli/src/parts/GetTestsUnexpectedErrorMessage/GetTestsUnexpectedErrorMessage.js
@@ -1,17 +1,6 @@
-import * as Character from '../Character/Character.js'
 import * as FormatStack from '../FormatStack/FormatStack.js'
+import * as Indent from '../Indent/Indent.js'
 import * as TestPrefix from '../TestPrefix/TestPrefix.js'
-
-const indentLine = (line) => {
-  return '    ' + line
-}
-
-const indent = (string) => {
-  if (!string) {
-    return Character.EmptyString
-  }
-  return string.split('\n').map(indentLine).join('\n')
-}
 
 export const getTestsUnexpectedErrorMessage = (error) => {
   const formattedStack = FormatStack.formatStack(error.stack, '')
@@ -19,7 +8,7 @@ export const getTestsUnexpectedErrorMessage = (error) => {
 
       ${error.message}
 
-${indent(error.codeFrame)}
+${Indent.indent(error.codeFrame)}
 
 ${formattedStack}
 
