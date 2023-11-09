b1373c3d26db98d3cb65acb645c88d70c92b88ba
Simon Siefke <simon.siefke@gmail.com>
2023-11-09 19:20:12 +0100

    refactor: move getCleanPositionsMap function into separate file (#518)

--- packages/memory-leak-finder/src/parts/GetEventListenerOriginalSources/GetEventListenerOriginalSources.js
+++ packages/memory-leak-finder/src/parts/GetEventListenerOriginalSources/GetEventListenerOriginalSources.js
@@ -1,19 +1,6 @@
-import * as GetCleanPosition from '../GetCleanPosition/GetCleanPosition.js'
+import * as GetCleanPositionsMap from '../GetCleanPositionsMap/GetCleanPositionsMap.js'
 import * as GetSourceMapUrl from '../GetSourceMapUrl/GetSourceMapUrl.js'
 import * as GetSourceMapUrlMap from '../GetSourceMapUrlMap/GetSourceMapUrlMap.js'
-import * as LoadSourceMap from '../LoadSourceMap/LoadSourceMap.js'
-import * as SourceMap from '../SourceMap/SourceMap.js'
-
-const getCleanPositionsMap = async (map) => {
-  const cleanPositionMap = Object.create(null)
-  for (const [key, value] of Object.entries(map)) {
-    const sourceMap = await LoadSourceMap.loadSourceMap(key)
-    const originalPositions = await SourceMap.getOriginalPositions(sourceMap, value)
-    const cleanPositions = originalPositions.map(GetCleanPosition.getCleanPosition)
-    cleanPositionMap[key] = cleanPositions
-  }
-  return cleanPositionMap
-}
 
 const getCleanEventlisteners = (cleanPositionMap, eventListeners) => {
   const newEventListeners = []
@@ -37,7 +24,7 @@ const getCleanEventlisteners = (cleanPositionMap, eventListeners) => {
 
 export const getEventListenerOriginalSources = async (eventListeners) => {
   const map = GetSourceMapUrlMap.getSourceMapUrlMap(eventListeners)
-  const cleanPositionMap = await getCleanPositionsMap(map)
+  const cleanPositionMap = await GetCleanPositionsMap.getCleanPositionsMap(map)
   const cleanEventListeners = getCleanEventlisteners(cleanPositionMap, eventListeners)
   return cleanEventListeners
 }
