51dbe6473f66f47b3100526e6ba85bbdfef54180
Simon Siefke <simon.siefke@gmail.com>
2023-11-09 19:11:27 +0100

    refactor: move getCleanPositionsMap function into separate file

---
+++ packages/memory-leak-finder/src/parts/GetCleanPositionsMap/GetCleanPositionsMap.js
@@ -0,0 +1,14 @@
+import * as GetCleanPosition from '../GetCleanPosition/GetCleanPosition.js'
+import * as LoadSourceMap from '../LoadSourceMap/LoadSourceMap.js'
+import * as SourceMap from '../SourceMap/SourceMap.js'
+
+export const getCleanPositionsMap = async (map) => {
+  const cleanPositionMap = Object.create(null)
+  for (const [key, value] of Object.entries(map)) {
+    const sourceMap = await LoadSourceMap.loadSourceMap(key)
+    const originalPositions = await SourceMap.getOriginalPositions(sourceMap, value)
+    const cleanPositions = originalPositions.map(GetCleanPosition.getCleanPosition)
+    cleanPositionMap[key] = cleanPositions
+  }
+  return cleanPositionMap
+}
