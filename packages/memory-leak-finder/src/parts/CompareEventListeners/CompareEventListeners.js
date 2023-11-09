4376270c4b462daba75a72c92031433817a15026
Simon Siefke <simon.siefke@gmail.com>
2023-11-09 20:06:52 +0100

    refactor: simplify compareEventListeners function

--- packages/memory-leak-finder/src/parts/CompareEventListeners/CompareEventListeners.js
+++ packages/memory-leak-finder/src/parts/CompareEventListeners/CompareEventListeners.js
@@ -1,23 +1,10 @@
+import * as CompareMapLeak from '../CompareMapLeak/CompareMapLeak.js'
 import * as DeduplicateEventListeners from '../DeduplicateEventListeners/DeduplicateEventListeners.js'
 import * as GetEventListenerKey from '../GetEventListenerKey/GetEventListenerKey.js'
 import * as GetEventListenerOriginalSourcesCached from '../GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js'
 
 export const compareEventListeners = async (before, after) => {
-  const map = Object.create(null)
-  for (const listener of before) {
-    const key = GetEventListenerKey.getEventListenerKey(listener)
-    map[key] ||= 0
-    map[key]++
-  }
-  const leaked = []
-  for (const listener of after) {
-    const key = GetEventListenerKey.getEventListenerKey(listener)
-    if (!map[key]) {
-      leaked.push(listener)
-    } else {
-      map[key]--
-    }
-  }
+  const leaked = CompareMapLeak.compareMapLeak(before, after, GetEventListenerKey.getEventListenerKey)
   const deduplicatedEventListeners = DeduplicateEventListeners.deduplicateEventListeners(leaked)
   const cleanLeakedEventListeners =
     await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(deduplicatedEventListeners)
