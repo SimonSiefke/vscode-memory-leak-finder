75a31d1555d31e19017aae965a8c93526dea29fb
Simon Siefke <simon.siefke@gmail.com>
2023-11-09 22:11:48 +0100

    add actual measure

--- packages/memory-leak-finder/src/parts/GetArrayElementCount/GetArrayElementCount.js
+++ packages/memory-leak-finder/src/parts/GetArrayElementCount/GetArrayElementCount.js
@@ -1,3 +1,26 @@
-export const getArrayElementCount = (session) => {
-  return 0
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+
+export const getArrayElementCount = async (session) => {
+  const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: PrototypeExpression.Array,
+    includeCommandLineAPI: true,
+    returnByValue: false,
+  })
+  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
+    prototypeObjectId: prototypeDescriptor.objectId,
+  })
+  const fnResult1 = await DevtoolsProtocolRuntime.callFunctionOn(session, {
+    functionDeclaration: `function(){
+  const objects = this
+  let total=0
+  for(const object of objects){
+    total += object.length
+  }
+  return total
+}`,
+    objectId: objects.objects.objectId,
+    returnByValue: true,
+  })
+  return fnResult1
 }
