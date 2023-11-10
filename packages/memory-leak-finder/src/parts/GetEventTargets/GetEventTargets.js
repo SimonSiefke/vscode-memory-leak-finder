bf643a053b3ba186452f79dd60e3ff1ab2691a16
Simon Siefke <simon.siefke@gmail.com>
2023-11-10 18:27:44 +0100

    feature: add event targets measure (#539)

---
+++ packages/memory-leak-finder/src/parts/GetEventTargets/GetEventTargets.js
@@ -0,0 +1,28 @@
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as GetDescriptors from '../GetDescriptorValues/GetDescriptorValues.js'
+import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+
+/**
+ *
+ * @param {any} session
+ * @param {string} objectGroup
+ * @returns {Promise<any[]>}
+ */
+export const getEventTargets = async (session, objectGroup, scriptMap) => {
+  const prototype = await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: PrototypeExpression.EventTarget,
+    includeCommandLineAPI: true,
+    returnByValue: false,
+    objectGroup,
+  })
+  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
+    prototypeObjectId: prototype.objectId,
+    objectGroup,
+  })
+  const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
+    objectId: objects.objects.objectId,
+    ownProperties: true,
+  })
+  const descriptors = GetDescriptors.getDescriptorValues(fnResult1)
+  return descriptors
+}
