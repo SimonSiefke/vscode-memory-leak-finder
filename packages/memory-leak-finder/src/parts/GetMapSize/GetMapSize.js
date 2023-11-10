4a3f7458c1c696ff270546b5c4188b598a882cc1
Simon Siefke <simon.siefke@gmail.com>
2023-11-10 19:14:24 +0100

    feature: implement set size measure (#544)

--- packages/memory-leak-finder/src/parts/GetMapSize/GetMapSize.js
+++ packages/memory-leak-finder/src/parts/GetMapSize/GetMapSize.js
@@ -1,3 +1,26 @@
-export const getMapSize = (session) => {
-  return 0
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+
+export const getMapSize = async (session) => {
+  const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: PrototypeExpression.Map,
+    includeCommandLineAPI: true,
+    returnByValue: false,
+  })
+  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
+    prototypeObjectId: prototypeDescriptor.objectId,
+  })
+  const fnResult1 = await DevtoolsProtocolRuntime.callFunctionOn(session, {
+    functionDeclaration: `function(){
+  const objects = this
+  let total = 0
+  for(const object of objects){
+    total += object.size
+  }
+  return total
+}`,
+    objectId: objects.objects.objectId,
+    returnByValue: true,
+  })
+  return fnResult1
 }
