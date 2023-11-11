cf6112bd4563bf55f1261500e3c600108c43ccf2
Simon Siefke <simon.siefke@gmail.com>
2023-11-11 20:08:42 +0100

    feature: try to integrate sourcemaps for instance counts (#578)

--- packages/memory-leak-finder/src/parts/GetInstanceCounts/GetInstanceCounts.js
+++ packages/memory-leak-finder/src/parts/GetInstanceCounts/GetInstanceCounts.js
@@ -2,10 +2,10 @@ import * as GetConstructorLocations from '../GetConstructorLocations/GetConstruc
 import * as GetInstanceCountArray from '../GetInstanceCountArray/GetInstanceCountArray.js'
 import * as GetInstances from '../GetInstances/GetInstances.js'
 
-export const getInstanceCounts = async (session, objectGroup) => {
+export const getInstanceCounts = async (session, objectGroup, scriptMap) => {
   const objects = await GetInstances.getInstances(session, objectGroup)
   const fnResult1 = await GetInstanceCountArray.getInstanceCountArray(session, objectGroup, objects)
   const fnResult2 = await GetConstructorLocations.getConstructorLocations(session, objectGroup, objects)
-  console.log({ fnResult2 })
+  // console.log({ fnResult2, scriptMap })
   return fnResult1
 }
