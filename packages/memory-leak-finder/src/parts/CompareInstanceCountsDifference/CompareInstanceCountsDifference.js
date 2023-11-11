4d1dda8e8822ec9d4e39b0163e4c56f2cd58d676
Simon Siefke <simon.siefke@gmail.com>
2023-11-11 11:19:09 +0100

    feature: improve instance counts difference measure comparison: display a list of leaked instances

---
+++ packages/memory-leak-finder/src/parts/CompareInstanceCountsDifference/CompareInstanceCountsDifference.js
@@ -0,0 +1,22 @@
+import * as PrettifyInstanceCounts from '../PrettifyInstanceCounts/PrettifyInstanceCounts.js'
+
+export const compareInstanceCountsDifference = (before, after) => {
+  const beforeMap = Object.create(null)
+  for (const element of before) {
+    beforeMap[element.name] = element.count
+  }
+  const leaked = []
+  for (const element of after) {
+    const beforeCount = beforeMap[element.name] || 0
+    const afterCount = element.count
+    const delta = afterCount - beforeCount
+    if (delta > 0) {
+      leaked.push({
+        ...element,
+        delta,
+      })
+    }
+  }
+  const prettyLeaked = PrettifyInstanceCounts.prettifyInstanceCounts(leaked)
+  return prettyLeaked
+}
---
+++ packages/memory-leak-finder/test/CompareInstanceCountsDifference.test.js
@@ -0,0 +1,76 @@
+import * as CompareInstanceCountsDifference from '../src/parts/CompareInstanceCountsDifference/CompareInstanceCountsDifference.js'
+
+test('compareInstanceCountsDifference', () => {
+  const before = [
+    {
+      name: 'LoaderEvent',
+      count: 9392,
+    },
+    {
+      name: 'Node',
+      count: 5561,
+    },
+    {
+      name: 'AsyncFunction',
+      count: 4698,
+    },
+    {
+      name: 'Module',
+      count: 2560,
+    },
+    {
+      name: 'UniqueContainer',
+      count: 2453,
+    },
+    {
+      name: 'LinkedList',
+      count: 2389,
+    },
+    {
+      name: 'Emitter',
+      count: 2055,
+    },
+  ]
+  const after = [
+    {
+      name: 'LoaderEvent',
+      count: 9392,
+    },
+    {
+      name: 'Node',
+      count: 5561,
+    },
+    {
+      name: 'AsyncFunction',
+      count: 4698,
+    },
+    {
+      name: 'Module',
+      count: 2560,
+    },
+    {
+      name: 'UniqueContainer',
+      count: 2460,
+    },
+    {
+      name: 'LinkedList',
+      count: 2389,
+    },
+    {
+      name: 'Emitter',
+      count: 2118,
+    },
+  ]
+  expect(CompareInstanceCountsDifference.compareInstanceCountsDifference(before, after)).toEqual([
+    {
+      name: 'UniqueContainer',
+      count: 2460,
+      delta: 7,
+    },
+    {
+      name: 'Emitter',
+      count: 2118,
+      delta: 63,
+    },
+  ])
+})
