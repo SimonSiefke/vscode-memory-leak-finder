8cec7fe35554fce2a18b416832c671c0f2fafc29
Simon Siefke <simon.siefke@gmail.com>
2023-11-11 17:30:49 +0100

    feature: implement class count measure (#576)

--- packages/memory-leak-finder/src/parts/GetClassCount/GetClassCount.js
+++ packages/memory-leak-finder/src/parts/GetClassCount/GetClassCount.js
@@ -1,3 +1,17 @@
-export const getClassCount = (session) => {
-  return 0
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as GetConstructors from '../GetConstructors/GetConstructors.js'
+import * as GetInstances from '../GetInstances/GetInstances.js'
+
+export const getClassCount = async (session, objectGroup) => {
+  const instances = await GetInstances.getInstances(session, objectGroup)
+  const constructors = await GetConstructors.getConstructors(session, objectGroup, instances.objectId)
+  const fnResult1 = await DevtoolsProtocolRuntime.callFunctionOn(session, {
+    functionDeclaration: `function (){
+  const constructors = this
+  return constructors.length
+}`,
+    objectGroup,
+    objectId: constructors.objectId,
+  })
+  return fnResult1
 }
