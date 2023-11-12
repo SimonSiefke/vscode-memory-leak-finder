13ddfdc2283938dd0200d73acea41cad9ec8f798
Simon Siefke <simon.siefke@gmail.com>
2023-11-12 09:02:08 +0100

    fix: improve stack/sourceMap output when it is empty / not available (#598)

--- packages/memory-leak-finder/src/parts/GetSourceMapUrlMap/GetSourceMapUrlMap.js
+++ packages/memory-leak-finder/src/parts/GetSourceMapUrlMap/GetSourceMapUrlMap.js
@@ -1,44 +1,9 @@
-const RE_LINE_COLUMN = /(\d+):(\d+)/
-
-const parseLineAndColumn = (line) => {
-  const match = line.match(RE_LINE_COLUMN)
-  if (!match) {
-    return undefined
-  }
-  return {
-    line: parseInt(match[1]),
-    column: parseInt(match[2]),
-  }
-}
-
-const emptySourceMapUrl = {
-  sourceMapUrl: '',
-  line: 0,
-  column: 0,
-}
-
-const getSourceMapUrl = (eventListener) => {
-  const { stack, sourceMaps } = eventListener
-  if (!stack || !sourceMaps) {
-    return emptySourceMapUrl
-  }
-  const firstStackLine = stack[0]
-  const parsed = parseLineAndColumn(firstStackLine)
-  if (!parsed) {
-    return emptySourceMapUrl
-  }
-  const sourceMapUrl = sourceMaps[0]
-  return {
-    sourceMapUrl,
-    line: parsed.line,
-    column: parsed.column,
-  }
-}
+import * as GetSourceMapUrl from '../GetSourceMapUrl/GetSourceMapUrl.js'
 
 export const getSourceMapUrlMap = (eventListeners) => {
   const map = Object.create(null)
   for (const eventListener of eventListeners) {
-    const { sourceMapUrl, line, column } = getSourceMapUrl(eventListener)
+    const { sourceMapUrl, line, column } = GetSourceMapUrl.getSourceMapUrl(eventListener)
     map[sourceMapUrl] ||= []
     map[sourceMapUrl].push({ line, column })
   }
