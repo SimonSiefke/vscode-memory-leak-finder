d26a81f41035f867b91cbe5daad51b0f04398e64
Simon Siefke <simon.siefke@gmail.com>
2023-11-12 13:21:29 +0100

    fix: improve instance count output with sourcemaps

--- packages/memory-leak-finder/src/parts/PrettifyInstanceCountsWithSourceMap/PrettifyInstanceCountsWithSourceMap.js
+++ packages/memory-leak-finder/src/parts/PrettifyInstanceCountsWithSourceMap/PrettifyInstanceCountsWithSourceMap.js
@@ -1,14 +1,21 @@
+import * as Arrays from '../Arrays/Arrays.js'
 import * as Assert from '../Assert/Assert.js'
 import * as CompareInstance from '../CompareInstance/CompareInstance.js'
 import * as GetEventListenerOriginalSourcesCached from '../GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js'
 
+const cleanInstance = (instance) => {
+  const { name, originalName, count } = instance
+  return {
+    name: originalName || name,
+    count,
+  }
+}
+
 export const prettifyInstanceCountsWithSourceMap = async (instances) => {
   Assert.array(instances)
-  const prettyInstances = [...instances].sort(CompareInstance.compareInstance)
   const classNames = true
-  const cleanPrettyInstances = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(
-    prettyInstances,
-    classNames,
-  )
-  return cleanPrettyInstances
+  const cleanPrettyInstances = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(instances, classNames)
+  const sorted = Arrays.toSorted(cleanPrettyInstances, CompareInstance.compareInstance)
+  const clean = sorted.map(cleanInstance)
+  return clean
 }
