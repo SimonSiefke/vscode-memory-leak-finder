971546ea2530460dc38e095bfbd7b6bfde4b1c21
Simon Siefke <simon.siefke@gmail.com>
2023-11-12 12:36:20 +0100

    feature: sort event listener source map input to avoid cache misses by different order

--- packages/memory-leak-finder/src/parts/GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js
+++ packages/memory-leak-finder/src/parts/GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js
@@ -3,14 +3,16 @@ import * as GetResolvedSourceMapCachePath from '../GetResolvedSourceMapCachePath
 import * as GetSourceMapUrlMap from '../GetSourceMapUrlMap/GetSourceMapUrlMap.js'
 import * as Hash from '../Hash/Hash.js'
 import * as JsonFile from '../JsonFile/JsonFile.js'
+import * as SortEventListenersBySourceMap from '../SortEventListenersBySourceMap/SortEventListenersBySourceMap.js'
 
 export const getEventListenerOriginalSourcesCached = async (eventListeners, classNames) => {
-  const sourceMapUrlMap = GetSourceMapUrlMap.getSourceMapUrlMap(eventListeners)
+  const sorted = SortEventListenersBySourceMap.sortEventListenersBySourceMap(eventListeners)
+  const sourceMapUrlMap = GetSourceMapUrlMap.getSourceMapUrlMap(sorted)
   const hash = Hash.hash({ ...sourceMapUrlMap, classNames })
   const cachePath = GetResolvedSourceMapCachePath.getResolvedSourceMapCachePath(hash)
   if (!Exists.exists(cachePath)) {
     const GetEventListenerOriginalSources = await import('../GetEventListenerOriginalSources/GetEventListenerOriginalSources.js')
-    const result = await GetEventListenerOriginalSources.getEventListenerOriginalSources(eventListeners, sourceMapUrlMap, classNames)
+    const result = await GetEventListenerOriginalSources.getEventListenerOriginalSources(sorted, sourceMapUrlMap, classNames)
     await JsonFile.writeJson(cachePath, result)
   }
   return JsonFile.readJson(cachePath)
