1bd3ed3eda9b079a773e9554286d964e3fd41602
Simon Siefke <simon.siefke@gmail.com>
2023-11-12 10:03:04 +0100

    fix: try to sort source map input positions to avoid frequent source map resolve cache misses

--- packages/memory-leak-finder/src/parts/GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js
+++ packages/memory-leak-finder/src/parts/GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js
@@ -1,3 +1,4 @@
+import { join } from 'node:path'
 import * as Exists from '../Exists/Exists.js'
 import * as GetResolvedSourceMapCachePath from '../GetResolvedSourceMapCachePath/GetResolvedSourceMapCachePath.js'
 import * as GetSourceMapUrlMap from '../GetSourceMapUrlMap/GetSourceMapUrlMap.js'
@@ -9,9 +10,17 @@ export const getEventListenerOriginalSourcesCached = async (eventListeners, clas
   const hash = Hash.hash({ ...map, classNames })
   const cachePath = GetResolvedSourceMapCachePath.getResolvedSourceMapCachePath(hash)
   if (!Exists.exists(cachePath)) {
+    console.log('CACHE MISS', cachePath)
+    console.time('get-sources')
+    console.log(process.memoryUsage().heapUsed)
     const GetEventListenerOriginalSources = await import('../GetEventListenerOriginalSources/GetEventListenerOriginalSources.js')
     const result = await GetEventListenerOriginalSources.getEventListenerOriginalSources(eventListeners, classNames)
     await JsonFile.writeJson(cachePath, result)
+    await JsonFile.writeJson(join(cachePath + '-source.json'), map)
+    console.timeEnd('get-sources')
+    console.log(process.memoryUsage().heapUsed)
+  } else {
+    console.log('CACHE HIT', cachePath)
   }
   return JsonFile.readJson(cachePath)
 }
--- packages/memory-leak-finder/src/parts/Hash/Hash.js
+++ packages/memory-leak-finder/src/parts/Hash/Hash.js
@@ -1,6 +1,7 @@
 import { createHash } from 'node:crypto'
 
 export const hash = (object) => {
+  console.log(object)
   const string = JSON.stringify(object)
   const hash = createHash('sha1')
   hash.update(string)
