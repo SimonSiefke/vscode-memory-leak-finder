232364d096214fe2b207d95a2d1c668dec407e8d
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 20:45:24 +0100

    refactor: use constructorKey enum more often

--- packages/memory-leak-finder/src/parts/GetMutationObserversWithStackTraces/GetMutationObserversWithStackTraces.js
+++ packages/memory-leak-finder/src/parts/GetMutationObserversWithStackTraces/GetMutationObserversWithStackTraces.js
@@ -1,5 +1,6 @@
 import * as GetConstructorStackTraces from '../GetConstructorStackTraces/GetConstructorStackTraces.js'
+import * as ConstructorKey from '../ConstructorKey/ConstructorKey.js'
 
 export const getMutationObserversWithStackTraces = (session, objectGroup) => {
-  return GetConstructorStackTraces.getConstructorStackTraces(session, objectGroup, 'MutationObserver')
+  return GetConstructorStackTraces.getConstructorStackTraces(session, objectGroup, ConstructorKey.MutationObserver)
 }
