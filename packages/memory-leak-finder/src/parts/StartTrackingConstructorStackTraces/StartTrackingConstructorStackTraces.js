719d337ed5fa5b48b8c98d76652f8dfc9ef57299
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 19:57:07 +0100

    feature: add function to track global constructors (#623)

---
+++ packages/memory-leak-finder/src/parts/StartTrackingConstructorStackTraces/StartTrackingConstructorStackTraces.js
@@ -0,0 +1,41 @@
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+
+/**
+ * @param {any} session
+ * @param {string} objectGroup
+ * @returns {Promise<any>}
+ */
+export const startTrackingMutationObserverStackTraces = async (session, objectGroup, key) => {
+  await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: `(()=>{
+
+
+// based on https://github.com/sindresorhus/callsites
+const callsites = () => {
+	const _prepareStackTrace = Error.prepareStackTrace;
+	Error.prepareStackTrace = (_, stack) => stack;
+	const stack = new Error().stack.slice(2);
+	Error.prepareStackTrace = _prepareStackTrace;
+	return stack.join('\\n')
+}
+
+
+const mockGlobalConstructor = key => {
+  const originalConstructor = globalThis[key]
+  globalThis['___stackTraces'+key] = []
+  globalThis['___original'+key] = originalConstructor
+  globalThis[key] = function (...args){
+    console.trace('constructed'+key)
+    return Reflect.construct(originalConstructor, args)
+  }
+}
+
+mockGlobalConstructor('${key}')
+
+})()
+undefined
+`,
+
+    objectGroup,
+  })
+}
