57fe925dc4a35496f97affa3cde1c1903253edba
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 19:51:24 +0100

    feature: improve mutation observer stack trace measure

--- packages/memory-leak-finder/src/parts/MeasureMutationObserversWithStackTraces/MeasureMutationObserversWithStackTraces.js
+++ packages/memory-leak-finder/src/parts/MeasureMutationObserversWithStackTraces/MeasureMutationObserversWithStackTraces.js
@@ -1,19 +1,25 @@
-import * as MeasureId from '../MeasureId/MeasureId.js'
-import * as GetMutationObserverCount from '../GetMutationObserverCount/GetMutationObserverCount.js'
 import * as CompareCount from '../CompareCount/CompareCount.js'
+import * as GetMutationObserverCount from '../GetMutationObserverCount/GetMutationObserverCount.js'
 import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as StartTrackingMutationObserverStackTraces from '../StartTrackingMutationObserverStackTraces/StartTrackingMutationObserverStackTraces.js'
+import * as StopTrackingMutationObserverStackTraces from '../StopTrackingMutationObserverStackTraces/StopTrackingMutationObserverStackTraces.js'
 
 export const id = MeasureId.MutationObserversWithStackTraces
 
 export const create = (session) => {
-  return [session]
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
 }
 
-export const start = (session) => {
+export const start = async (session, objectGroup) => {
+  await StartTrackingMutationObserverStackTraces.startTrackingMutationObserverStackTraces(session, objectGroup)
   return GetMutationObserverCount.getMutationObserverCount(session)
 }
 
-export const stop = (session) => {
+export const stop = async (session, objectGroup) => {
+  await StopTrackingMutationObserverStackTraces.stopTrackingMutationObserverStackTraces(session, objectGroup)
   return GetMutationObserverCount.getMutationObserverCount(session)
 }
 
