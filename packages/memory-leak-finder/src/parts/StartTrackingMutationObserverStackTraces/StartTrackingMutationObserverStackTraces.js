f02f69b9d1b6231b20417f926cb420defa1739c1
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 20:01:16 +0100

    refactor: reuse constructor tracking for mutation observer constructor tracking

--- packages/memory-leak-finder/src/parts/StartTrackingMutationObserverStackTraces/StartTrackingMutationObserverStackTraces.js
+++ packages/memory-leak-finder/src/parts/StartTrackingMutationObserverStackTraces/StartTrackingMutationObserverStackTraces.js
@@ -1,4 +1,4 @@
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as StartTrackingConstructorStackTraces from '../StartTrackingConstructorStackTraces/StartTrackingConstructorStackTraces.js'
 
 /**
  * @param {any} session
@@ -6,35 +6,5 @@ import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js
  * @returns {Promise<any>}
  */
 export const startTrackingMutationObserverStackTraces = async (session, objectGroup) => {
-  await DevtoolsProtocolRuntime.evaluate(session, {
-    expression: `(()=>{
-globalThis.___mutationObserverStackTraces = []
-
-
-// based on https://github.com/sindresorhus/callsites
-const callsites = () => {
-	const _prepareStackTrace = Error.prepareStackTrace;
-	Error.prepareStackTrace = (_, stack) => stack;
-	const stack = new Error().stack.slice(2);
-	Error.prepareStackTrace = _prepareStackTrace;
-	return stack.join('\\n')
-}
-
-
-const mockGlobalConstructor = key => {
-  const originalConstructor = globalThis[key]
-  globalThis['___original'+key] = originalConstructor
-  globalThis[key] = function (...args){
-    console.trace('constructed'+key)
-    return Reflect.construct(originalConstructor, args)
-  }
-}
-
-mockGlobalConstructor('MutationObserver')
-
-})()
-undefined
-`,
-    objectGroup,
-  })
+  await StartTrackingConstructorStackTraces.startTrackingMutationObserverStackTraces(session, objectGroup, 'MutationObserver')
 }
--- packages/memory-leak-finder/src/parts/StopTrackingMutationObserverStackTraces/StopTrackingMutationObserverStackTraces.js
+++ packages/memory-leak-finder/src/parts/StopTrackingMutationObserverStackTraces/StopTrackingMutationObserverStackTraces.js
@@ -1,25 +1,9 @@
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
-
+import * as StopTrackingConstructorStackTraces from '../StopTrackingConstructorStackTraces/StopTrackingConstructorStackTraces.js'
 /**
  * @param {any} session
  * @param {string} objectGroup
  * @returns {Promise<any>}
  */
 export const stopTrackingMutationObserverStackTraces = async (session, objectGroup) => {
-  await DevtoolsProtocolRuntime.evaluate(session, {
-    expression: `(()=>{
-delete globalThis.___mutationObserverStackTraces
-
-const unmockGlobalConstructor = key => {
-  const originalConstructor = globalThis['___original'+key]
-  globalThis[key] = originalConstructor
-}
-
-unmockGlobalConstructor('MutationObserver')
-
-})()
-undefined
-`,
-    objectGroup,
-  })
+  await StopTrackingConstructorStackTraces.stopTrackingEventListenerStackTraces(session, objectGroup, 'MutationObserver')
 }
