0770a23c845056bf4452eb6a859f827e94b2f5eb
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 20:21:06 +0100

    refactor: add constructor key enum

--- packages/memory-leak-finder/src/parts/StopTrackingMutationObserverStackTraces/StopTrackingMutationObserverStackTraces.js
+++ packages/memory-leak-finder/src/parts/StopTrackingMutationObserverStackTraces/StopTrackingMutationObserverStackTraces.js
@@ -1,9 +1,11 @@
+import * as ConstructorKey from '../ConstructorKey/ConstructorKey.js'
 import * as StopTrackingConstructorStackTraces from '../StopTrackingConstructorStackTraces/StopTrackingConstructorStackTraces.js'
+
 /**
  * @param {any} session
  * @param {string} objectGroup
  * @returns {Promise<any>}
  */
 export const stopTrackingMutationObserverStackTraces = async (session, objectGroup) => {
-  await StopTrackingConstructorStackTraces.stopTrackingEventListenerStackTraces(session, objectGroup, 'MutationObserver')
+  await StopTrackingConstructorStackTraces.stopTrackingEventListenerStackTraces(session, objectGroup, ConstructorKey.MutationObserver)
 }
