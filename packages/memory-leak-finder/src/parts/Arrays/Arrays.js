30e0dc2a1fdf2c4d0979db634cbcf4d7d884a100
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 18:38:37 +0100

    feature: add isLeak function to measureInstanceCountsWithSourceMap (#618)

--- packages/memory-leak-finder/src/parts/Arrays/Arrays.js
+++ packages/memory-leak-finder/src/parts/Arrays/Arrays.js
@@ -1,3 +1,11 @@
 export const toSorted = (array, compare) => {
   return [...array].sort(compare)
 }
+
+export const sum = (values) => {
+  let total = 0
+  for (const value of values) {
+    total += value
+  }
+  return total
+}
