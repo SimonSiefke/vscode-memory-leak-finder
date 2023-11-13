d71aa7edd20af5c11567142dfbe2e7ab407cb739
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 20:46:00 +0100

    sort imports

--- packages/memory-leak-finder/src/parts/GetMutationObserversWithStackTraces/GetMutationObserversWithStackTraces.js
+++ packages/memory-leak-finder/src/parts/GetMutationObserversWithStackTraces/GetMutationObserversWithStackTraces.js
@@ -1,5 +1,5 @@
-import * as GetConstructorStackTraces from '../GetConstructorStackTraces/GetConstructorStackTraces.js'
 import * as ConstructorKey from '../ConstructorKey/ConstructorKey.js'
+import * as GetConstructorStackTraces from '../GetConstructorStackTraces/GetConstructorStackTraces.js'
 
 export const getMutationObserversWithStackTraces = (session, objectGroup) => {
   return GetConstructorStackTraces.getConstructorStackTraces(session, objectGroup, ConstructorKey.MutationObserver)
