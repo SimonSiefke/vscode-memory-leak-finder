ced79019d5299cb13f029189ab6050867c058834
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 22:37:04 +0100

    fix: release object groups after usage (#638)

--- packages/memory-leak-finder/src/parts/MeasureInstanceCountsDifference/MeasureInstanceCountsDifference.js
+++ packages/memory-leak-finder/src/parts/MeasureInstanceCountsDifference/MeasureInstanceCountsDifference.js
@@ -1,4 +1,5 @@
 import * as CompareInstanceCountsDifference from '../CompareInstanceCountsDifference/CompareInstanceCountsDifference.js'
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 import * as GetInstanceCounts from '../GetInstanceCounts/GetInstanceCounts.js'
 import * as MeasureId from '../MeasureId/MeasureId.js'
 import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
@@ -14,8 +15,10 @@ export const start = (session, objectGroup) => {
   return GetInstanceCounts.getInstanceCounts(session, objectGroup)
 }
 
-export const stop = (session, objectGroup) => {
-  return GetInstanceCounts.getInstanceCounts(session, objectGroup)
+export const stop = async (session, objectGroup) => {
+  const result = await GetInstanceCounts.getInstanceCounts(session, objectGroup)
+  await DevtoolsProtocolRuntime.releaseObjectGroup(session, { objectGroup })
+  return result
 }
 
 export const compare = CompareInstanceCountsDifference.compareInstanceCountsDifference
