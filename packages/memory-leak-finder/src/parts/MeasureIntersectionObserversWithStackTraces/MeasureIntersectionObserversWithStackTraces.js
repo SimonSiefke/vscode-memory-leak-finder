1548df2bdf96ee336e4be541d20575d3f162bd18
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 20:47:53 +0100

    feature: improve intersectionObserversWithStackTraces measure (#630)

--- packages/memory-leak-finder/src/parts/MeasureIntersectionObserversWithStackTraces/MeasureIntersectionObserversWithStackTraces.js
+++ packages/memory-leak-finder/src/parts/MeasureIntersectionObserversWithStackTraces/MeasureIntersectionObserversWithStackTraces.js
@@ -1,20 +1,20 @@
-import * as MeasureId from '../MeasureId/MeasureId.js'
-import * as GetMutationObserverCount from '../GetMutationObserverCount/GetMutationObserverCount.js'
 import * as CompareCount from '../CompareCount/CompareCount.js'
+import * as GetIntersectionObserverCount from '../GetIntersectionObserverCount/GetIntersectionObserverCount.js'
 import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
 
-export const id = MeasureId.MutationObserversWithStackTraces
+export const id = MeasureId.IntersectionObserversWithStackTraces
 
 export const create = (session) => {
   return [session]
 }
 
 export const start = (session) => {
-  return GetMutationObserverCount.getMutationObserverCount(session)
+  return GetIntersectionObserverCount.getIntersectionObserverCount(session)
 }
 
 export const stop = (session) => {
-  return GetMutationObserverCount.getMutationObserverCount(session)
+  return GetIntersectionObserverCount.getIntersectionObserverCount(session)
 }
 
 export const compare = CompareCount.compareCount
