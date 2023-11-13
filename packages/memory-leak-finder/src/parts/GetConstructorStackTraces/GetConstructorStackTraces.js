f3bef4830ef1aaf62c761db72aefc57006ec43d8
Simon Siefke <simon.siefke@gmail.com>
2023-11-13 20:50:29 +0100

    fix: runtime error

--- packages/memory-leak-finder/src/parts/GetConstructorStackTraces/GetConstructorStackTraces.js
+++ packages/memory-leak-finder/src/parts/GetConstructorStackTraces/GetConstructorStackTraces.js
@@ -1,6 +1,13 @@
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 import * as SplitLines from '../SplitLines/SplitLines.js'
 
+const getPrettyStackTrace = (value) => {
+  return {
+    ...value,
+    stackTrace: SplitLines.splitLines(value.stackTrace),
+  }
+}
+
 export const getConstructorStackTraces = async (session, objectGroup, key) => {
   const stackTraces = await DevtoolsProtocolRuntime.evaluate(session, {
     expression: `(()=>{
@@ -12,6 +19,7 @@ export const getConstructorStackTraces = async (session, objectGroup, key) => {
     returnByValue: true,
     objectGroup,
   })
-  const betterStackTraces = stackTraces.map(SplitLines.splitLines)
+  console.log({ stackTraces })
+  const betterStackTraces = stackTraces.map(getPrettyStackTrace)
   return betterStackTraces
 }
