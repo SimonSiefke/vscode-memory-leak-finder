5b7dd94ee4608fced13a3594916aeff68e18962a
Simon Siefke <simon.siefke@gmail.com>
2023-11-20 21:08:30 +0100

    feature: add explorer set files test

---
+++ packages/e2e/src/explorer.set-files.js
@@ -0,0 +1,21 @@
+export const skip = process.platform === 'darwin'
+
+export const run = async ({ Workspace, Editor, Explorer, ContextMenu, DiffEditor, SideBar }) => {
+  await Workspace.setFiles([
+    {
+      name: 'a.txt',
+      content: 'a',
+    },
+    {
+      name: 'b.txt',
+      content: 'b',
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('a.txt')
+  await Explorer.shouldHaveItem('b.txt')
+  await Explorer.focus()
+  await Explorer.openContextMenu('a.txt')
+  await ContextMenu.select('Select for Compare')
+}
