2b64fbf901ab85856f7689fa2e316a905b5bba83
Simon Siefke <simon.siefke@gmail.com>
2023-11-20 20:26:10 +0100

    feature: add explorer open test (#646)

---
+++ packages/e2e/src/explorer.open.js
@@ -0,0 +1,21 @@
+export const setup = async ({ Workspace, Editor, Explorer }) => {
+  await Workspace.setFiles([
+    {
+      name: 'a.txt',
+      content: 'a',
+    },
+    {
+      name: 'b.txt',
+      content: 'b',
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('a.txt')
+  await Explorer.shouldHaveItem('b.txt')
+}
+
+export const run = async ({ Explorer, SideBar }) => {
+  await Explorer.focus()
+  await SideBar.hide()
+}
