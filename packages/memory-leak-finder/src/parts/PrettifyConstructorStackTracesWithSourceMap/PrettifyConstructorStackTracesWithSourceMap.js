ce9c2ca779746483783eea7033b97a19ea22d9c5
Simon Siefke <simon.siefke@gmail.com>
2023-11-22 18:49:07 +0100

    improve stacktrace

--- packages/memory-leak-finder/src/parts/PrettifyConstructorStackTracesWithSourceMap/PrettifyConstructorStackTracesWithSourceMap.js
+++ packages/memory-leak-finder/src/parts/PrettifyConstructorStackTracesWithSourceMap/PrettifyConstructorStackTracesWithSourceMap.js
@@ -1,8 +1,5 @@
 import * as Arrays from '../Arrays/Arrays.js'
-import * as Assert from '../Assert/Assert.js'
-import * as CompareInstance from '../CompareInstance/CompareInstance.js'
 import * as GetEventListenerOriginalSourcesCached from '../GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js'
-import * as GetSourceMapUrl from '../GetSourceMapUrl/GetSourceMapUrl.js'
 
 const RE_URL = /(.*)\((.*):(\d+):(\d+)\)/s
 
@@ -23,31 +20,59 @@ const parseUrl = (stackLine) => {
   }
 }
 
-export const prettifyConstructorStackTracesWithSourceMap = async (constructorStackTraces, scriptMap) => {
-  const first = constructorStackTraces[0]
+const compareInstance = (a, b) => {
+  return b.originalIndex - a.originalIndex || b.uuid - a.uuid
+}
+
+const getLine = (instance) => {
+  return instance.originalStack[0]
+}
+
+const getEventListenerQuery = (stackLines, scriptMap, index) => {
   const reverseScriptMap = Object.create(null)
   for (const value of Object.values(scriptMap)) {
     reverseScriptMap[value.url] = value.sourceMapUrl
   }
-  const eventListeners = first.map((stackLine, index) => {
+  const eventListeners = stackLines.map((stackLine, index) => {
     const { prefix, url, line, column } = parseUrl(stackLine)
     const sourceMapUrl = reverseScriptMap[url]
     const newStackLine = `${prefix}(${url}:${line - 1}:${column})`
     return {
       url,
+      originalIndex: index,
       uuid: index,
       stack: [newStackLine],
       sourceMaps: [sourceMapUrl],
     }
   })
+  return eventListeners
+}
 
-  console.log({ eventListeners, scriptMap })
-  const cleanPrettyInstances = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(eventListeners, false)
-  console.log({ cleanPrettyInstances })
-  // return cleanPrettyInstances
-  return {
-    constructorStackTraces,
-    eventListeners,
-    cleanPrettyInstances,
+const sortOriginal = (cleanInstances) => {
+  const cleaned = []
+  const sorted = Arrays.toSorted(cleanInstances)
+  let current = []
+  let currentIndex = -1
+  for (const value of sorted) {
+    if (value.originalIndex > currentIndex) {
+      currentIndex = value.originalIndex
+      current = []
+      cleaned.push(current)
+    }
+    const originalStack = value.originalStack || []
+    current.push(originalStack[0])
   }
+  return cleaned
+}
+
+export const prettifyConstructorStackTracesWithSourceMap = async (constructorStackTraces, scriptMap) => {
+  const fullQuery = []
+  for (let i = 0; i < constructorStackTraces.length; i++) {
+    const stackTrace = constructorStackTraces[i]
+    const eventListeners = getEventListenerQuery(stackTrace, scriptMap, i)
+    fullQuery.push(...eventListeners)
+  }
+  const cleanPrettyInstances = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(fullQuery, false)
+  const sorted = sortOriginal(cleanPrettyInstances)
+  return sorted
 }
