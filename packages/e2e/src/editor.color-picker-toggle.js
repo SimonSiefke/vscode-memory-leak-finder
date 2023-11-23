ecb00fc1a08061ee021cc435069405fa0ed3d5ea
Simon Siefke <simon.siefke@gmail.com>
2023-11-23 19:51:07 +0100

    feature: add editor color picker e2e test (#660)

---
+++ packages/e2e/src/editor.color-picker-toggle.js
@@ -0,0 +1,19 @@
+export const skip = true
+
+export const setup = async ({ Editor, Workspace, Explorer }) => {
+  await Workspace.setFiles([
+    {
+      name: 'index.html',
+      content: '<h1>hello world</h1>',
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('index.html')
+  await Editor.open('index.html')
+}
+
+export const run = async ({ Editor }) => {
+  await Editor.showColorPicker()
+  await Editor.hideColorPicker()
+}
