723a59f2d73b2de3c954dba17bccae7e2c5cab11
Simon Siefke <simon.siefke@gmail.com>
2023-12-01 19:12:43 +0100

    refactor: move compareEventListenersWithStackTraces function into separate file

---
+++ packages/memory-leak-finder/src/parts/CompareEventListenersWithStackTraces/CompareEventListenersWithStackTraces.js
@@ -0,0 +1,28 @@
+import * as DeduplicateEventListeners from '../DeduplicateEventListeners/DeduplicateEventListeners.js'
+import * as GetEventListenerKey from '../GetEventListenerKey/GetEventListenerKey.js'
+import * as GetEventListenerOriginalSourcesCached from '../GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js'
+
+export const compareEventListenersWithStackTraces = async (before, after) => {
+  const map = Object.create(null)
+  for (const listener of before) {
+    const key = GetEventListenerKey.getEventListenerKey(listener)
+    map[key] ||= 0
+    map[key]++
+  }
+  const leaked = []
+  for (const listener of after) {
+    const key = GetEventListenerKey.getEventListenerKey(listener)
+    if (!map[key]) {
+      leaked.push(listener)
+    } else {
+      map[key]--
+    }
+  }
+  const deduplicatedEventListeners = DeduplicateEventListeners.deduplicateEventListeners(leaked)
+  const classNames = false
+  const cleanLeakedEventListeners = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(
+    deduplicatedEventListeners,
+    classNames,
+  )
+  return cleanLeakedEventListeners
+}
