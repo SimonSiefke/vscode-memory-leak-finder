2cec26c38213fc6c2df5ea37faf8a96a9bc0e59a
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 15:17:26 +0100

    feature: add detached dom nodes with stack traces measure (#695)

---
+++ packages/memory-leak-finder/src/parts/MeasureDetachedDomNodesWithStackTraces/MeasureDetachedDomNodesWithStackTraces.js
@@ -0,0 +1,33 @@
+import * as CompareDetachedDomNodes from '../CompareDetachedDomNodes/CompareDetachedDomNodes.js'
+import * as GetDetachedDomNodes from '../GetDetachedDomNodes/GetDetachedDomNodes.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as Arrays from '../Arrays/Arrays.js'
+
+export const id = MeasureId.DetachedDomNodesWithStackTraces
+
+export const create = (session) => {
+  return [session]
+}
+
+export const start = (session) => {
+  return GetDetachedDomNodes.getDetachedDomNodes(session)
+}
+
+export const stop = (session) => {
+  return GetDetachedDomNodes.getDetachedDomNodes(session)
+}
+
+export const compare = CompareDetachedDomNodes.compareDetachedDomNodes
+
+const getCount = (instance) => {
+  return instance.count
+}
+
+const getTotal = (instance) => {
+  const counts = instance.map(getCount)
+  return Arrays.sum(counts)
+}
+
+export const isLeak = ({ before, after }) => {
+  return getTotal(after) > getTotal(before)
+}
