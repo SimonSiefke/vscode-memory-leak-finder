82eb62e9581f318e1a8652a8c8c8de36a14772c1
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 21:51:51 +0100

    feature: improve problems toggle test

--- packages/e2e/src/problems.toggle.js
+++ packages/e2e/src/problems.toggle.js
@@ -1,15 +1,6 @@
 export const skip = true
 
-export const run = async ({ page, expect, StatusBar }) => {
-  const problemsButton = await StatusBar.item('status.problems')
-  await problemsButton.click()
-
-  const panel = page.locator('.panel')
-  const messageBoxContainer = panel.locator('.pane-body .message-box-container').first()
-  await expect(messageBoxContainer).toBeVisible()
-  await expect(messageBoxContainer).toBeFocused()
-  await expect(messageBoxContainer).toHaveText('No problems have been detected in the workspace.')
-
-  await problemsButton.click()
-  await expect(messageBoxContainer).toBeHidden()
+export const run = async ({ Problems }) => {
+  await Problems.show()
+  await Problems.hide()
 }
--- packages/page-object/src/parts/Problems/Problems.js
+++ packages/page-object/src/parts/Problems/Problems.js
@@ -1,3 +1,7 @@
+import * as QuickPick from '../QuickPick/QuickPick.js'
+import * as WellKnownCommands from '../WellKnownCommands/WellKnownCommands.js'
+import * as Panel from '../Panel/Panel.js'
+
 export const create = ({ expect, page, VError }) => {
   return {
     async shouldHaveCount(count) {
@@ -12,6 +16,28 @@ export const create = ({ expect, page, VError }) => {
         }
       } catch (error) {
         throw new VError(error, `Failed to assert problems count of ${count}`)
+      }
+    },
+    async show() {
+      try {
+        const markersPanel = page.locator('.markers-panel')
+        await expect(markersPanel).toBeHidden()
+        const quickPick = QuickPick.create({ expect, page, VError })
+        await quickPick.executeCommand(WellKnownCommands.ProblemsFocusOnProblemsView)
+        await expect(markersPanel).toBeVisible()
+      } catch (error) {
+        throw new VError(error, `Failed to show problems`)
+      }
+    },
+    async hide() {
+      try {
+        const markersPanel = page.locator('.markers-panel')
+        await expect(markersPanel).toBeVisible()
+        const panel = Panel.create({ expect, page, VError })
+        await panel.hide()
+        await expect(markersPanel).toBeHidden()
+      } catch (error) {
+        throw new VError(error, `Failed to hide problems`)
       }
     },
   }
