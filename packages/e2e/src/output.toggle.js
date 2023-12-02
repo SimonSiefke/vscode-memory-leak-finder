c512b3af2b8d8c0380df3bd8aff86839b0fdf0f2
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 22:10:05 +0100

    feature: add output toggle test (#708)

--- packages/e2e/src/output.toggle.js
+++ packages/e2e/src/output.toggle.js
@@ -1,5 +1,3 @@
-export const skip = true
-
 export const run = async ({ Output }) => {
   await Output.show()
   await Output.hide()
--- packages/page-object/src/parts/Output/Output.js
+++ packages/page-object/src/parts/Output/Output.js
@@ -1,18 +1,15 @@
+import * as Panel from '../Panel/Panel.js'
+import * as QuickPick from '../QuickPick/QuickPick.js'
+import * as WellKnownCommands from '../WellKnownCommands/WellKnownCommands.js'
+
 export const create = ({ expect, page, VError }) => {
   return {
     async show() {
       try {
         const outputView = page.locator('.pane-body.output-view')
         await expect(outputView).toBeHidden()
-        await page.keyboard.press('Control+P')
-        const quickPick = page.locator('.quick-input-widget')
-        await expect(quickPick).toBeVisible()
-        const quickPickInput = quickPick.locator('[role="combobox"]')
-        await quickPickInput.type('> Output: Focus on Output View')
-        const option = quickPick.locator('.monaco-list-row', {
-          hasText: 'Output: Focus on Output View',
-        })
-        await option.click()
+        const quickPick = QuickPick.create({ expect, page, VError })
+        await quickPick.executeCommand(WellKnownCommands.OutputFocusOnOutputView)
         await expect(outputView).toBeVisible()
       } catch (error) {
         throw new VError(error, `Failed to show output`)
@@ -22,18 +19,11 @@ export const create = ({ expect, page, VError }) => {
       try {
         const outputView = page.locator('.pane-body.output-view')
         await expect(outputView).toBeVisible()
-        await page.keyboard.press('Control+P')
-        const quickPick = page.locator('.quick-input-widget')
-        await expect(quickPick).toBeVisible()
-        const quickPickInput = quickPick.locator('[role="combobox"]')
-        await quickPickInput.type('> View: Close Panel')
-        const option = quickPick.locator('.monaco-list-row', {
-          hasText: 'View: Close Panel',
-        })
-        await option.click()
+        const panel = Panel.create({ expect, page, VError })
+        await panel.hide()
         await expect(outputView).toBeHidden()
       } catch (error) {
-        throw new VError(error, `Failed to show output`)
+        throw new VError(error, `Failed to hide output`)
       }
     },
   }
