eb3f70b2f9451b6be3c899f54fed2c827e39897f
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 19:41:35 +0100

    fix: error in compareCount function when name is undefined

--- packages/memory-leak-finder/src/parts/CompareInstance/CompareInstance.js
+++ packages/memory-leak-finder/src/parts/CompareInstance/CompareInstance.js
@@ -1,3 +1,14 @@
+const compareInstanceCount = (a, b) => {
+  return b.count - a.count
+}
+
+const compareInstanceName = (a, b) => {
+  if (a.name && b.name) {
+    return a.name.localeCompare(b.name)
+  }
+  return 0
+}
+
 export const compareInstance = (a, b) => {
-  return b.count - a.count || a.name.localeCompare(b.name)
+  return compareInstanceCount(a, b) || compareInstanceName(a, b)
 }
