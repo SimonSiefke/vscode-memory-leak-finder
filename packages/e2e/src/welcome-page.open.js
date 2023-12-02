e245065d36cb1c310a046ab9b70866d76d699b62
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 22:21:18 +0100

    feature: use welcome page object (#716)

--- packages/e2e/src/welcome-page.open.js
+++ packages/e2e/src/welcome-page.open.js
@@ -4,7 +4,7 @@ export const setup = async ({ Editor }) => {
   await Editor.closeAll()
 }
 
-export const run = async ({ Editor, QuickPick, WellKnownCommands }) => {
-  await QuickPick.executeCommand(WellKnownCommands.HelpWelcome)
-  await Editor.closeAll()
+export const run = async ({ WelcomePage }) => {
+  await WelcomePage.show()
+  await WelcomePage.hide()
 }
