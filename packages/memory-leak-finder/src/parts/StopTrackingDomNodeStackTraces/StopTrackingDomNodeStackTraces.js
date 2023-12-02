921a33fbb55156caede80006139e59974c324a9f
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 15:27:22 +0100

    feature: add function to start tracking dom nodes stack traces (#696)

---
+++ packages/memory-leak-finder/src/parts/StopTrackingDomNodeStackTraces/StopTrackingDomNodeStackTraces.js
@@ -0,0 +1,21 @@
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+
+/**
+ * @param {any} session
+ * @param {string} objectGroup
+ * @returns {Promise<any>}
+ */
+export const stopTrackingDomNodeStackTraces = async (session, objectGroup) => {
+  await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: `(()=>{
+document.createElement = globalThis.___originalCreateElement
+
+delete globalThis.___originalCreateElement
+delete globalThis.__domNodeStackTraces
+
+})()
+undefined
+`,
+    objectGroup,
+  })
+}
