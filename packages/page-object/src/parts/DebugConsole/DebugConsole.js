6a8c33d12cfb06545b19c6935b38fb196581d611
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 22:17:45 +0100

    feature: add debug console focus command

--- packages/page-object/src/parts/DebugConsole/DebugConsole.js
+++ packages/page-object/src/parts/DebugConsole/DebugConsole.js
@@ -1,13 +1,27 @@
+import * as Panel from '../Panel/Panel.js'
+import * as QuickPick from '../QuickPick/QuickPick.js'
+import * as WellKnownCommands from '../WellKnownCommands/WellKnownCommands.js'
+
 export const create = ({ expect, page, VError }) => {
   return {
     async show() {
       try {
+        const repl = page.locator('.repl')
+        await expect(repl).toBeHidden()
+        const quickPick = QuickPick.create({ expect, page, VError })
+        await quickPick.executeCommand(WellKnownCommands.DebugConsoleFocusOnDebugConsoleView)
+        await expect(repl).toBeVisible()
       } catch (error) {
         throw new VError(error, `Failed to show debug console`)
       }
     },
     async hide() {
       try {
+        const repl = page.locator('.repl')
+        await expect(repl).toBeVisible()
+        const panel = Panel.create({ expect, page, VError })
+        await panel.hide()
+        await expect(repl).toBeHidden()
       } catch (error) {
         throw new VError(error, `Failed to hide debug console`)
       }
