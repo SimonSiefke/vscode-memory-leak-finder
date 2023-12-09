39c86da0fc071f14ba7f1e164f88aa0969b30639
Simon Siefke <simon.siefke@gmail.com>
2023-12-09 15:48:10 +0100

    feature: add notebook code scrolling test

---
+++ packages/e2e/src/notebook.code-scrolling.js
@@ -0,0 +1,111 @@
+export const skip = true
+
+export const setup = async ({ Workspace, Explorer, Editor }) => {
+  const notebook = {
+    cells: [
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+      {
+        cell_type: 'code',
+        metadata: {},
+        source: ['aa'],
+      },
+    ],
+    metadata: {
+      language_info: {
+        name: 'python',
+      },
+    },
+    nbformat: 4,
+    nbformat_minor: 2,
+  }
+
+  await Workspace.setFiles([
+    {
+      name: 'test.ipynb',
+      content: JSON.stringify(notebook, null, 2) + '\n',
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('test.ipynb')
+  await Editor.open('test.ipynb')
+}
+
+export const run = async ({ Notebook }) => {
+  await Notebook.scrollDown()
+  await Notebook.scrollUp()
+}
