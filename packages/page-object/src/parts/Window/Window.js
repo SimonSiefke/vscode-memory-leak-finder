0521e1a6e05a7d70f44c6e9c46ea13bb75f9fdb7
Simon Siefke <simon.siefke@gmail.com>
2023-12-16 23:08:32 +0100

    improve test

--- packages/page-object/src/parts/Window/Window.js
+++ packages/page-object/src/parts/Window/Window.js
@@ -2,17 +2,20 @@ export const create = ({ page, VError }) => {
   return {
     async focus() {
       try {
+        await page.waitForIdle()
         await page.focus()
+        await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to focus window`)
       }
     },
     async blur() {
       try {
+        await page.waitForIdle()
         await page.blur()
         await page.waitForIdle()
       } catch (error) {
-        throw new VError(error, `Failed to focus window`)
+        throw new VError(error, `Failed to blur window`)
       }
     },
   }
