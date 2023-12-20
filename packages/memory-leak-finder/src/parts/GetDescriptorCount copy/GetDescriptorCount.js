e42140c537518d3a2537639c2d3710785d49b970
Simon Siefke <simon.siefke@gmail.com>
2023-12-20 12:32:23 +0100

    feature: measure detached dom node count more accurately: also includ… (#760)

--- packages/memory-leak-finder/src/parts/GetDescriptorCount copy/GetDescriptorCount.js
+++ packages/memory-leak-finder/src/parts/GetDescriptorCount copy/GetDescriptorCount.js
@@ -1,26 +1,24 @@
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
-import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
 
 /**
  *
  * @param {any} session
+ * @param {string} prototype
  * @returns {Promise<number>}
  */
-export const getDetachedDomNodeCount = async (session, objectGroup) => {
+export const getDescriptorCount = async (session, prototype) => {
   const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
-    expression: PrototypeExpression.Node,
+    expression: prototype,
     returnByValue: false,
-    objectGroup,
   })
   const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
     prototypeObjectId: prototypeDescriptor.objectId,
-    objectGroup,
   })
   const fnResult1 = await DevtoolsProtocolRuntime.callFunctionOn(session, {
     functionDeclaration: `function(){
 const objects = this
 
-const isGarbageCollected = node => {
+const isDetached = node => {
   try {
     node.nodeType
     return false
@@ -46,7 +44,7 @@ const getDetachedNodes = (nodes) => {
   const list = getAllNodes()
   const detached = []
   for (const node of nodes) {
-    if (list.includes(node) || isGarbageCollected(node)) {
+    if (list.includes(node) || node === document || isDetached(node)) {
       continue
     }
     detached.push(node)
@@ -54,12 +52,23 @@ const getDetachedNodes = (nodes) => {
   return detached
 }
 
+const getDetachedRoots = detachedNodes => {
+  const detachedRoots = []
+  for(const detachedNode of detachedNodes){
+    if(detachedNodes.includes(detachedNode.parentNode)){
+      continue
+    }
+    detachedRoots.push(detachedNode)
+  }
+  return detachedRoots
+}
+
 const detachedNodes = getDetachedNodes(objects)
-return detachedNodes.length
+const detachedRoots = getDetachedRoots(detachedNodes)
+return detachedRoots.length
 }`,
     objectId: objects.objects.objectId,
     returnByValue: true,
-    objectGroup,
   })
 
   return fnResult1
