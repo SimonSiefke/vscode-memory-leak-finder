06c3f94efd55177b57ac6211dbf4611995c97e27
Simon Siefke <simon.siefke@gmail.com>
2023-12-21 11:35:04 +0100

    fix: missing sourcemap version property

--- packages/memory-leak-finder/src/parts/LoadSourceMapModule/LoadSourceMapModule.js
+++ packages/memory-leak-finder/src/parts/LoadSourceMapModule/LoadSourceMapModule.js
@@ -5,6 +5,14 @@ export const getModule = (protocol) => {
     case 'http':
     case 'https':
       return import('../LoadSourceMapFromUrl/LoadSourceMapFromUrl.js')
+    case 'noop':
+      return {
+        loadSourceMap() {
+          return {
+            version: '3',
+          }
+        },
+      }
     default:
       throw new Error(`unsupported protocol ${protocol}`)
   }
