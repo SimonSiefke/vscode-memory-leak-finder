b8d0d217695fb600ad7a8840dfdd86b848aa8e6b
Simon Siefke <simon.siefke@gmail.com>
2023-12-21 01:19:36 +0100

    fix test

--- packages/memory-leak-finder/test/CleanFunctionLocations.test.js
+++ packages/memory-leak-finder/test/CleanFunctionLocations.test.js
@@ -1,6 +1,7 @@
 import * as CleanFunctionLocations from '../src/parts/CleanFunctionLocations/CleanFunctionLocations.js'
 
 test('cleanFunctionLocations', () => {
+  const names = ['a', 'b']
   const counts = [2, 1]
   const functionLocations = [
     {
@@ -14,18 +15,20 @@ test('cleanFunctionLocations', () => {
       columnNumber: 1229,
     },
   ]
-  expect(CleanFunctionLocations.cleanFunctionLocations(counts, functionLocations)).toEqual([
+  expect(CleanFunctionLocations.cleanFunctionLocations(names, counts, functionLocations)).toEqual([
     {
       columnNumber: 355,
       lineNumber: 1896,
       scriptId: '12',
       count: 2,
+      name: 'a',
     },
     {
       columnNumber: 1229,
       lineNumber: 36,
       scriptId: '12',
       count: 1,
+      name: 'b',
     },
   ])
 })
