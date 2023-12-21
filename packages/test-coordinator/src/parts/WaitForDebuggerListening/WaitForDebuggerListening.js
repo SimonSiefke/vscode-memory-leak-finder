def9a36e74c8d43128485d0dba72cda075b31f8f
Simon Siefke <simon.siefke@gmail.com>
2023-12-21 01:26:45 +0100

    fix: improve error message when yarn is not found (#773)

--- packages/test-coordinator/src/parts/WaitForDebuggerListening/WaitForDebuggerListening.js
+++ packages/test-coordinator/src/parts/WaitForDebuggerListening/WaitForDebuggerListening.js
@@ -1,13 +1,18 @@
 import { once } from 'node:events'
+import * as NodeVersion from '../NodeVersion/NodeVersion.js'
 
 const RE_STARTING_INSPECTOR_FAILED = /Starting inspector on .* failed/s
 const RE_LISTENING_ON = /Debugger listening on (.*)/
+const RE_YARN_NOT_INSTALLED = /spawn yarn ENOENT/
 
 export const waitForDebuggerListening = async (stream) => {
   const [firstData] = await once(stream, 'data')
   if (!firstData.includes('Debugger listening on')) {
     if (RE_STARTING_INSPECTOR_FAILED.test(firstData)) {
       throw new Error(firstData.trim())
+    }
+    if (RE_YARN_NOT_INSTALLED.test(firstData)) {
+      throw new Error(`yarn not installed in this node version (${NodeVersion.nodeVersion})`)
     }
     throw new Error(`Failed to connect to debugger: Unexpected first message: ${firstData}`)
   }
--- packages/test-coordinator/test/LaunchElectron.test.js
+++ packages/test-coordinator/test/LaunchElectron.test.js
@@ -13,6 +13,12 @@ jest.unstable_mockModule('../src/parts/Spawn/Spawn.js', () => {
   }
 })
 
+jest.unstable_mockModule('../src/parts/NodeVersion/NodeVersion.js', () => {
+  return {
+    nodeVersion: 'test-node-version',
+  }
+})
+
 const LaunchElectron = await import('../src/parts/LaunchElectron/LaunchElectron.js')
 const Spawn = await import('../src/parts/Spawn/Spawn.js')
 
@@ -35,7 +41,7 @@ test('launch - error - address already in use', async () => {
     }
   })
   // TODO mock WaitForDebuggerlistening module instead of mocking spawn
-  await expect(LaunchElectron.launchElectron({ cliPath: '', args: [], headlessMode: true })).rejects.toThrowError(
+  await expect(LaunchElectron.launchElectron({ cliPath: '', args: [], headlessMode: true })).rejects.toThrow(
     new Error(`Failed to launch electron: Starting inspector on 127.0.0.1:4444 failed: address already in use`),
   )
 })
@@ -58,7 +64,7 @@ test('launch - error - unexpected first message', async () => {
       stderr,
     }
   })
-  await expect(LaunchElectron.launchElectron({ cliPath: '', args: [], headlessMode: true })).rejects.toThrowError(
+  await expect(LaunchElectron.launchElectron({ cliPath: '', args: [], headlessMode: true })).rejects.toThrow(
     new Error('Failed to launch electron: Failed to connect to debugger: Unexpected first message: abc'),
   )
 })
@@ -68,7 +74,42 @@ test('launch - error - empty cli path', async () => {
   Spawn.spawn.mockImplementation(() => {
     throw new Error("The argument 'file' cannot be empty. Received ''")
   })
-  await expect(LaunchElectron.launchElectron({ cliPath: '', args: [], headlessMode: true })).rejects.toThrowError(
+  await expect(LaunchElectron.launchElectron({ cliPath: '', args: [], headlessMode: true })).rejects.toThrow(
     new Error(`Failed to launch electron: The argument 'file' cannot be empty. Received ''`),
+  )
+})
+
+test('launch - error - yarn is not installed', async () => {
+  // @ts-ignore
+  Spawn.spawn.mockImplementation(() => {
+    const stdout = new EventEmitter()
+    // @ts-ignore
+    stdout.setEncoding = () => {}
+    const stderr = new EventEmitter()
+    // @ts-ignore
+    stderr.setEncoding = () => {}
+
+    setTimeout(() => {
+      stderr.emit(
+        'data',
+        `Error: spawn yarn ENOENT
+    at ChildProcess._handle.onexit (node:internal/child_process:284:19)
+    at onErrorNT (node:internal/child_process:477:16)
+    at process.processTicksAndRejections (node:internal/process/task_queues:82:21) {
+  errno: -2,
+  code: 'ENOENT',
+  syscall: 'spawn yarn',
+  path: 'yarn',
+  spawnargs: [ 'electron' ]
+}`,
+      )
+    }, 0)
+    return {
+      stdout,
+      stderr,
+    }
+  })
+  await expect(LaunchElectron.launchElectron({ cliPath: '', args: [], headlessMode: true })).rejects.toThrow(
+    new Error(`Failed to launch electron: yarn not installed in this node version (test-node-version)`),
   )
 })
