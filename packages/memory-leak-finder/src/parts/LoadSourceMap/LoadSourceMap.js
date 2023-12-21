b3205dc6273c899db870cd550e87b0360e285d3d
Simon Siefke <simon.siefke@gmail.com>
2023-12-21 10:16:55 +0100

    fix: improve error message for unsupported source map protocol

--- packages/memory-leak-finder/src/parts/LoadSourceMap/LoadSourceMap.js
+++ packages/memory-leak-finder/src/parts/LoadSourceMap/LoadSourceMap.js
@@ -1,21 +1,10 @@
 import { VError } from '../VError/VError.js'
-
-const getModule = (protocol) => {
-  switch (protocol) {
-    case 'data':
-      return import('../LoadSourceMapFromDataUrl/LoadSourceMapFromData.js')
-    case 'http':
-    case 'https':
-      return import('../LoadSourceMapFromUrl/LoadSourceMapFromUrl.js')
-    default:
-      throw new Error(`unsupported protocol ${protocol}`)
-  }
-}
+import * as LoadSourceMapModule from '../LoadSourceMapModule/LoadSourceMapModule.js'
 
 const getProtocol = (url) => {
   const colonIndex = url.indexOf(':')
   if (colonIndex === -1) {
-    throw new Error(`unsupported url ${url}`)
+    throw new Error(`Unsupported source map url ${url}`)
   }
   return url.slice(0, colonIndex)
 }
@@ -23,7 +12,7 @@ const getProtocol = (url) => {
 export const loadSourceMap = async (url) => {
   try {
     const protocol = getProtocol(url)
-    const module = await getModule(protocol)
+    const module = await LoadSourceMapModule.getModule(protocol)
     const data = await module.loadSourceMap(url)
     return data
   } catch (error) {
