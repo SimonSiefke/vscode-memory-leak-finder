93db9df0be9702437b2c10d5834e690e313fdf9f
Simon Siefke <simon.siefke@gmail.com>
2023-12-24 09:26:19 +0100

    feature: improve terminal test

--- packages/page-object/src/parts/Terminal/Terminal.js
+++ packages/page-object/src/parts/Terminal/Terminal.js
@@ -16,16 +16,53 @@ export const create = ({ expect, page, VError }) => {
       try {
         const panel = Panel.create({ expect, page, VError })
         await panel.show()
-        // TODO show terminal next
+        const terminal = page.locator('.terminal')
+        await expect(terminal).toHaveCount(1)
+        await expect(terminal).toBeVisible()
+        await expect(terminal).toHaveClass('focus')
+        await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to show panel`)
       }
     },
     async add() {
       try {
-        // TODO add terminal
+        const newTerminal = page.locator('[aria-label^="New Terminal"]')
+        await newTerminal.click()
+        const terminalTabs = page.locator('[aria-label="Terminal tabs"]')
+        await expect(terminalTabs).toBeVisible()
+        const tabsEntry = page.locator('.terminal-tabs-entry')
+        await expect(tabsEntry).toHaveCount(2)
+        await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to add terminal`)
+      }
+    },
+    async killSecond() {
+      try {
+        const terminalTabs = page.locator('[aria-label="Terminal tabs"]')
+        await expect(terminalTabs).toBeVisible()
+        const tabsEntry = page.locator('.terminal-tabs-entry')
+        await expect(tabsEntry).toHaveCount(2)
+        const secondEntry = tabsEntry.nth(1)
+        const deleteAction = secondEntry.locator('[title^="Kill"]')
+        await deleteAction.click()
+        await expect(terminalTabs).toHaveCount(0)
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to kill second terminal`)
+      }
+    },
+    async killFirst() {
+      try {
+        await page.waitForIdle()
+        const killTerminal = page.locator('[aria-label="Kill Terminal"]')
+        await killTerminal.click()
+        const terminal = page.locator('.terminal')
+        await expect(terminal).toHaveCount(0)
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to kill terminal`)
       }
     },
   }
