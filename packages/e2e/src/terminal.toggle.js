5358c969055a3e05f7515c6b072e785c5e2e80a0
Simon Siefke <simon.siefke@gmail.com>
2023-12-24 09:37:17 +0100

    feature: improve terminal test (#791)

--- packages/e2e/src/terminal.toggle.js
+++ packages/e2e/src/terminal.toggle.js
@@ -3,10 +3,9 @@ export const skip = true
 export const setup = async ({ Terminal, Panel }) => {
   await Panel.hide()
   await Terminal.killAll()
-  await Terminal.show()
 }
 
 export const run = async ({ Terminal }) => {
-  await Terminal.add()
-  await Terminal.killSecond()
+  await Terminal.show()
+  await Terminal.killFirst()
 }
