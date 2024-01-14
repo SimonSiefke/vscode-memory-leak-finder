bf4fdf9797e27000f17084d23abf15c28356b147
Simon Siefke <simon.siefke@gmail.com>
2024-01-14 14:27:21 +0100

    feature: allow filtering by end match (#806)

--- packages/test-coordinator/src/parts/MatchesFilterValue/MatchesFilterValue.js
+++ packages/test-coordinator/src/parts/MatchesFilterValue/MatchesFilterValue.js
@@ -2,5 +2,8 @@ export const matchesFilterValue = (dirent, filterValue) => {
   if (filterValue.startsWith('^')) {
     return dirent.startsWith(filterValue.slice(1))
   }
+  if (filterValue.endsWith('$')) {
+    return dirent.endsWith(filterValue.slice(0, -1))
+  }
   return dirent.includes(filterValue)
 }
