11ba4c4321c206a9f3bfde87b2acfd7b42f579cf
Simon Siefke <simon.siefke@gmail.com>
2024-01-22 21:46:50 +0100

    fix: race conditions

--- packages/e2e/src/source-control.toggle.js
+++ packages/e2e/src/source-control.toggle.js
@@ -1,11 +1,12 @@
-export const setup = async ({ Editor, Workspace, Explorer, ActivityBar }) => {
+export const setup = async ({ Editor, Workspace, Explorer, SideBar, ActivityBar }) => {
   await Workspace.setFiles([])
   await Editor.closeAll()
   await Explorer.focus()
   await ActivityBar.showSourceControl()
+  await SideBar.hide()
 }
 
-export const run = async ({ ActivityBar, SideBar }) => {
+export const run = async ({ SideBar }) => {
+  await SideBar.show()
   await SideBar.hide()
-  await ActivityBar.showSourceControl()
 }
