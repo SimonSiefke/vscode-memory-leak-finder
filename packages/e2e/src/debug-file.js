68d5a10dbd0ae7249da9316dda44d8de206510c7
Simon Siefke <simon.siefke@gmail.com>
2024-01-22 20:07:40 +0100

    feature: add debug file test (#830)

---
+++ packages/e2e/src/debug-file.js
@@ -0,0 +1,25 @@
+export const skip = true
+
+export const setup = async ({ Editor, Workspace, Explorer }) => {
+  await Workspace.setFiles([
+    {
+      name: 'index.js',
+      content: `setInterval(()=>{
+
+}, 1000)`,
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('index.js')
+  await Editor.open('index.js')
+}
+
+export const run = async ({ ActivityBar, Explorer, RunAndDebug }) => {
+  await Explorer.focus()
+  await ActivityBar.showRunAndDebug()
+  await RunAndDebug.startRunAndDebug()
+  await RunAndDebug.pause()
+  await RunAndDebug.stop()
+  await Explorer.focus()
+}
