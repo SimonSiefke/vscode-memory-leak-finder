fb7cbf750a9bc4a9b3fbf9672d99e9b4386e4840
Simon Siefke <simon.siefke@gmail.com>
2024-01-24 21:44:14 +0100

    refactor: use scriptHandler helper function (#850)

--- packages/memory-leak-finder/src/parts/MeasureInstanceCountsWithSourceMap/MeasureInstanceCountsWithSourceMap.js
+++ packages/memory-leak-finder/src/parts/MeasureInstanceCountsWithSourceMap/MeasureInstanceCountsWithSourceMap.js
@@ -3,35 +3,24 @@ import * as CompareInstanceCountsWithSourceMap from '../CompareInstanceCountsWit
 import * as GetInstanceCountsWithSourceMap from '../GetInstanceCountsWithSourceMap/GetInstanceCountsWithSourceMap.js'
 import * as MeasureId from '../MeasureId/MeasureId.js'
 import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as ScriptHandler from '../ScriptHandler/ScriptHandler.js'
 
 export const id = MeasureId.InstanceCountsWithSourceMap
 
 export const create = (session) => {
   const objectGroup = ObjectGroupId.create()
-  const scriptMap = Object.create(null)
-  const handleScriptParsed = (event) => {
-    const { url, scriptId, sourceMapURL } = event.params
-    if (!url) {
-      return
-    }
-    scriptMap[scriptId] = {
-      url,
-      sourceMapUrl: sourceMapURL,
-    }
-  }
-  session.on('Debugger.scriptParsed', handleScriptParsed)
-  return [session, objectGroup, scriptMap, handleScriptParsed]
+  const scriptHandler = ScriptHandler.create()
+  return [session, objectGroup, scriptHandler]
 }
 
-export const start = async (session, objectGroup, scriptMap) => {
-  await session.invoke('Debugger.enable')
-  return GetInstanceCountsWithSourceMap.getInstanceCountsWithSourceMap(session, objectGroup, scriptMap)
+export const start = async (session, objectGroup, scriptHandler) => {
+  await scriptHandler.start(session)
+  return GetInstanceCountsWithSourceMap.getInstanceCountsWithSourceMap(session, objectGroup, scriptHandler.scriptMap)
 }
 
-export const stop = async (session, objectGroup, scriptMap, handleScriptParsed) => {
-  session.off('Debugger.scriptParsed', handleScriptParsed)
-  await session.invoke('Debugger.disable')
-  return GetInstanceCountsWithSourceMap.getInstanceCountsWithSourceMap(session, objectGroup, scriptMap)
+export const stop = async (session, objectGroup, scriptHandler) => {
+  await scriptHandler.stop(session)
+  return GetInstanceCountsWithSourceMap.getInstanceCountsWithSourceMap(session, objectGroup, scriptHandler.scriptMap)
 }
 
 export const compare = CompareInstanceCountsWithSourceMap.compareInstanceCountsWithSourceMap
