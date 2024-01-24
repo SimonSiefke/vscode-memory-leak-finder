a609252c3e5225873ddc9b015b9f930e3e21abc9
Simon Siefke <simon.siefke@gmail.com>
2024-01-24 23:35:57 +0100

    feature: add object count chart

--- packages/charts/src/parts/GenerateCharts/GenerateCharts.js
+++ packages/charts/src/parts/GenerateCharts/GenerateCharts.js
@@ -1,11 +1,25 @@
 import { mkdir, writeFile } from 'fs/promises'
 import { dirname, join } from 'path'
 import * as CreateFunctionCountChart from '../CreateFunctionCountChart/CreateFunctionCountChart.js'
+import * as CreateObjectCountChart from '../CreateObjectCountChart/CreateObjectCountChart.js'
 import * as Root from '../Root/Root.js'
 
+const visitors = [
+  {
+    name: 'function-count',
+    fn: CreateFunctionCountChart.createFunctionCountChart,
+  },
+  {
+    name: 'object-count',
+    fn: CreateObjectCountChart.createFunctionCountChart,
+  },
+]
+
 export const generateCharts = async () => {
-  const svg = await CreateFunctionCountChart.createFunctionCountChart()
-  const outPath = join(Root.root, '.vscode-charts', 'chart.svg')
-  await mkdir(dirname(outPath), { recursive: true })
-  await writeFile(outPath, svg)
+  for (const visitor of visitors) {
+    const svg = await visitor.fn()
+    const outPath = join(Root.root, '.vscode-charts', `${visitor.name}.svg`)
+    await mkdir(dirname(outPath), { recursive: true })
+    await writeFile(outPath, svg)
+  }
 }
