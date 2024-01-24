2b66daab5e679e77510b301f01a576b1b985ce15
Simon Siefke <simon.siefke@gmail.com>
2024-01-24 21:11:41 +0100

    feature: return function name and location in named function count measure

--- packages/memory-leak-finder/src/parts/GetNamedFunctionLocations/GetNamedFunctionLocations.js
+++ packages/memory-leak-finder/src/parts/GetNamedFunctionLocations/GetNamedFunctionLocations.js
@@ -1,12 +1,12 @@
 import * as Assert from '../Assert/Assert.js'
 import * as GetNamedFunctionLocation from '../GetNamedFunctionLocation/GetNamedFunctionLocation.js'
 
-export const getNamedFunctionLocations = async (session, objectIds) => {
+export const getNamedFunctionLocations = async (session, objectIds, scriptMap) => {
   Assert.object(session)
   Assert.array(objectIds)
   const promises = []
   for (const objectId of objectIds) {
-    promises.push(GetNamedFunctionLocation.getNamedFunctionLocation(session, objectId))
+    promises.push(GetNamedFunctionLocation.getNamedFunctionLocation(session, objectId, scriptMap))
   }
   const results = await Promise.all(promises)
   return results
