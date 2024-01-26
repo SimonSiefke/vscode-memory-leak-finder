703aad59d76beeb5849452afcddff75d44417ead
Simon Siefke <simon.siefke@gmail.com>
2024-01-26 19:57:50 +0100

    refactor: getNamedFunctionDifference into separate file

---
+++ packages/memory-leak-finder/src/parts/GetNamedFunctionDifference/GetNamedFunctionDifference.js
@@ -0,0 +1,24 @@
+import * as Assert from '../Assert/Assert.js'
+
+export const getDifference = (sortedBefore, sortedAfter) => {
+  Assert.array(sortedBefore)
+  Assert.array(sortedAfter)
+  const result = []
+  const beforeMap = Object.create(null)
+  for (const value of sortedBefore) {
+    beforeMap[value.url] = value.count
+  }
+  for (const value of sortedAfter) {
+    const beforeCount = beforeMap[value.url] || 0
+    if (value.count <= beforeCount) {
+      continue
+    }
+    result.push({
+      name: value.name,
+      count: value.count,
+      beforeCount,
+      url: value.url,
+    })
+  }
+  return result
+}
