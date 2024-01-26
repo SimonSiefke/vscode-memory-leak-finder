5da82cc6e86aab25de94c651fc2e2c7a28ed150a
Simon Siefke <simon.siefke@gmail.com>
2024-01-26 20:09:28 +0100

    refactor: getNamedFunctionDifference into separate file (#869)

--- packages/memory-leak-finder/src/parts/CompareFunctionDifference/CompareFunctionDifference.js
+++ packages/memory-leak-finder/src/parts/CompareFunctionDifference/CompareFunctionDifference.js
@@ -1,32 +1,12 @@
 import * as Assert from '../Assert/Assert.js'
+import * as GetNamedFunctionDifference from '../GetNamedFunctionDifference/GetNamedFunctionDifference.js'
 import * as SortNamedFunctions from '../SortNamedFunctions/SortNamedFunctions.js'
-
-const getDifference = (sortedBefore, sortedAfter) => {
-  const result = []
-  const beforeMap = Object.create(null)
-  for (const value of sortedBefore) {
-    beforeMap[value.url] = value.count
-  }
-  for (const value of sortedAfter) {
-    const beforeCount = beforeMap[value.url] || 0
-    if (value.count <= beforeCount) {
-      continue
-    }
-    result.push({
-      name: value.name,
-      count: value.count,
-      beforeCount,
-      url: value.url,
-    })
-  }
-  return result
-}
 
 export const compareFunctionDifference = (before, after) => {
   Assert.array(before)
   Assert.array(after)
   const sortedBefore = SortNamedFunctions.sortNamedFunctions(before)
   const sortedAfter = SortNamedFunctions.sortNamedFunctions(after)
-  const difference = getDifference(sortedBefore, sortedAfter)
+  const difference = GetNamedFunctionDifference.getDifference(sortedBefore, sortedAfter)
   return difference
 }
