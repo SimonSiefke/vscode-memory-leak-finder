4c755d62f5d00fdf4511393a4c11b3df4bffe4fc
Simon Siefke <simon.siefke@gmail.com>
2024-01-26 18:14:51 +0100

    refactor: simplify getNamedFunctionLocation function (#865)

---
+++ packages/memory-leak-finder/src/parts/GetNamedFunctionLocationProperty/GetNamedFunctionLocationProperty.js
@@ -0,0 +1,16 @@
+import * as EmptyFunctionLocation from '../EmptyFunctionLocation/EmptyFunctionLocation.js'
+import * as GetBoundFunctionValue from '../GetBoundFunctionValue/GetBoundFunctionValue.js'
+import * as IsFunctionLocation from '../IsFunctionLocation/IsFunctionLocation.js'
+
+export const getNamedFunctionLocationProperty = (session, fnResult, scriptMap, getNamedFunctionLocation) => {
+  const functionLocation = fnResult.internalProperties.find(IsFunctionLocation.isFunctionLocation)
+  if (!functionLocation) {
+    const boundFunctionValue = GetBoundFunctionValue.getBoundFunctionValue(fnResult)
+    if (!boundFunctionValue) {
+      return EmptyFunctionLocation.emptyFunctionLocation
+    }
+    const boundFunctionObjectId = boundFunctionValue.value.objectId
+    return getNamedFunctionLocation(boundFunctionObjectId, session, scriptMap)
+  }
+  return functionLocation.value.value
+}
