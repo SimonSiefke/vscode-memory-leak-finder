bbdbb61b8f7335888f95d63881547f828791b5db
Simon Siefke <simon.siefke@gmail.com>
2024-01-26 22:49:12 +0100

    feature: add instance counts difference with source map measure

---
+++ packages/memory-leak-finder/src/parts/MeasureInstanceCountsDifferenceWithSourceMap/MeasureInstanceCountsDifferenceWithSourceMap.js
@@ -0,0 +1,42 @@
+import * as Arrays from '../Arrays/Arrays.js'
+import * as CompareInstanceCountsWithSourceMap from '../CompareInstanceCountsWithSourceMap/CompareInstanceCountsWithSourceMap.js'
+import * as GetInstanceCountsWithSourceMap from '../GetInstanceCountsWithSourceMap/GetInstanceCountsWithSourceMap.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as ScriptHandler from '../ScriptHandler/ScriptHandler.js'
+
+export const id = MeasureId.InstanceCountsWithSourceMap
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  const scriptHandler = ScriptHandler.create()
+  return [session, objectGroup, scriptHandler]
+}
+
+export const start = async (session, objectGroup, scriptHandler) => {
+  await scriptHandler.start(session)
+  return GetInstanceCountsWithSourceMap.getInstanceCountsWithSourceMap(session, objectGroup, scriptHandler.scriptMap)
+}
+
+export const stop = async (session, objectGroup, scriptHandler) => {
+  await scriptHandler.stop(session)
+  return GetInstanceCountsWithSourceMap.getInstanceCountsWithSourceMap(session, objectGroup, scriptHandler.scriptMap)
+}
+
+// TODO can improve performance by only querying
+// constructor locations that have increased
+// instead of querying constructor locations for all instances
+// TODO compare instance counts with source maps
+export const compare = CompareInstanceCountsWithSourceMap.compareInstanceCountsWithSourceMap
+
+const getCount = (value) => {
+  return value.count
+}
+
+const getTotalCount = (instances) => {
+  return Arrays.sum(instances.map(getCount))
+}
+
+export const isLeak = ({ before, after }) => {
+  return getTotalCount(after) > getTotalCount(before)
+}
