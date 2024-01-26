295e18919e1122aad8c9fcf71017f0ee85d667de
Simon Siefke <simon.siefke@gmail.com>
2024-01-26 22:04:29 +0100

    test: add compareInstance test

---
+++ packages/memory-leak-finder/test/CompareInstance.test.js
@@ -0,0 +1,25 @@
+import * as CompareInstance from '../src/parts/CompareInstance/CompareInstance.js'
+
+test('compareInstance - by name', () => {
+  const a = {
+    name: 'NotebookOutlineAccessibility',
+    count: 2,
+  }
+  const b = {
+    name: 'class extends Action2',
+    count: 2,
+  }
+  expect(CompareInstance.compareInstance(a, b)).toBe(1)
+})
+
+test('compareInstance - by count', () => {
+  const a = {
+    name: 'NotebookOutlineAccessibility',
+    count: 2,
+  }
+  const b = {
+    name: 'NotebookOutlineAccessibility',
+    count: 3,
+  }
+  expect(CompareInstance.compareInstance(a, b)).toBe(1)
+})
