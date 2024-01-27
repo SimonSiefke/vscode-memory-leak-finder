d01b96bb1da3fcd2f05eb29a7954f651c168d4c1
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 21:27:35 +0100

    refactor: remove unused source map code from memory leak finder (#914)

--- packages/memory-leak-finder/src/parts/LoadSourceMapFromFile/LoadSourceMapFromFile.js
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'node:fs/promises'
-import { fileURLToPath } from 'url'
-
-export const loadSourceMap = async (url) => {
-  const path = fileURLToPath(url)
-  const content = await readFile(path, 'utf8')
-  const data = JSON.parse(content)
-  return data
-}
