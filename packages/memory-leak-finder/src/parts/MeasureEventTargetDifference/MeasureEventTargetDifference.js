16fe0ee4f54b33bf5dc654d7005777d3398c8a5c
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 23:03:22 +0100

    feature: add event target difference measure

---
+++ packages/memory-leak-finder/src/parts/MeasureEventTargetDifference/MeasureEventTargetDifference.js
@@ -0,0 +1,26 @@
+import * as CompareEventTargets from '../CompareEventTargets/CompareEventTargets.js'
+import * as GetEventTargets from '../GetEventTargets/GetEventTargets.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+
+export const id = MeasureId.EventTargetDifference
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
+}
+
+export const start = (session, objectGroup) => {
+  // TODO measure difference
+  return GetEventTargets.getEventTargets(session, objectGroup)
+}
+
+export const stop = (session, objectGroup) => {
+  return GetEventTargets.getEventTargets(session, objectGroup)
+}
+
+export const compare = CompareEventTargets.compareEventTargets
+
+export const isLeak = (leaked) => {
+  return leaked.length > 0
+}
