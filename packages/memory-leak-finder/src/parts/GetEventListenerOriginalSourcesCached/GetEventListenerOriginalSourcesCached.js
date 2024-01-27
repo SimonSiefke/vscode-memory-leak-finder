de50cb1fd086e489ef4f5293a3cd298e2a6c1494
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 20:46:14 +0100

    refactor: replace dynamic import with normal import

--- packages/memory-leak-finder/src/parts/GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js
+++ packages/memory-leak-finder/src/parts/GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js
@@ -1,5 +1,6 @@
 import * as CombineEventListenersWithSourceMapResults from '../CombineEventListenersWithSourceMapResults/CombineEventListenersWithSourceMapResults.js'
 import * as Exists from '../Exists/Exists.js'
+import * as GetCleanPositionsMap from '../GetCleanPositionsMap/GetCleanPositionsMap.js'
 import * as GetResolvedSourceMapCachePath from '../GetResolvedSourceMapCachePath/GetResolvedSourceMapCachePath.js'
 import * as GetSourceMapUrlMap from '../GetSourceMapUrlMap/GetSourceMapUrlMap.js'
 import * as Hash from '../Hash/Hash.js'
@@ -12,7 +13,6 @@ export const getEventListenerOriginalSourcesCached = async (eventListeners, clas
   const hash = Hash.hash({ ...sourceMapUrlMap, classNames })
   const cachePath = GetResolvedSourceMapCachePath.getResolvedSourceMapCachePath(hash)
   if (!Exists.exists(cachePath)) {
-    const GetCleanPositionsMap = await import('../GetCleanPositionsMap/GetCleanPositionsMap.js')
     const result = await GetCleanPositionsMap.getCleanPositionsMap(sourceMapUrlMap, classNames)
     await JsonFile.writeJson(cachePath, result)
   }
