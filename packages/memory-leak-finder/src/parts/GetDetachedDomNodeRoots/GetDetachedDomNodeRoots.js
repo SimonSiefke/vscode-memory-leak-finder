34d5e04ceccc2713f7ff711e13c2f0c27deb9ca2
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 22:56:56 +0100

    refactor: remove unused memory leak finder files (#920)
    * refactor: remove unused memory leak finder files
    
    * remove unused test

--- packages/memory-leak-finder/src/parts/GetDetachedDomNodeRoots/GetDetachedDomNodeRoots.js
+++
@@ -1,7 +0,0 @@
-import * as GetDescriptors from '../GetDescriptors/GetDescriptors.js'
-import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
-
-export const getDetachedDomNodes = async (session) => {
-  const descriptors = await GetDescriptors.getDescriptors(session, PrototypeExpression.Node)
-  return descriptors
-}
