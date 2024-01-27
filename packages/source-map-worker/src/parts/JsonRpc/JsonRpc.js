19b27eb1801c35391a8de3a4f6a60d8afefd054b
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 21:36:34 +0100

    refactor: remove unused sourcemap worker files

--- packages/source-map-worker/src/parts/JsonRpc/JsonRpc.js
+++
@@ -1,6 +0,0 @@
-import * as JsonRpcEvent from '../JsonRpcEvent/JsonRpcEvent.js'
-
-export const send = (transport, method, ...params) => {
-  const message = JsonRpcEvent.create(method, params)
-  transport.send(message)
-}
