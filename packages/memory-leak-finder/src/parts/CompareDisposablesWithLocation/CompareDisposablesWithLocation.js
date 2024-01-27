b986a55973911eaf65ded3151286b7e4e4e72d38
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 15:05:23 +0100

    refactor: move compareDisposablesWithLocation into separate file

--- packages/memory-leak-finder/src/parts/CompareDisposablesWithLocation/CompareDisposablesWithLocation.js
+++ packages/memory-leak-finder/src/parts/CompareDisposablesWithLocation/CompareDisposablesWithLocation.js
@@ -1,47 +1,5 @@
 import * as Assert from '../Assert/Assert.js'
-import * as GetEventListenerOriginalSourcesCached from '../GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js'
-import * as FormatUrl from '../FormatUrl/FormatUrl.js'
-
-const prepareDisposable = (disposable, scriptMap) => {
-  const { lineNumber, columnNumber, count, scriptId, name } = disposable
-  const script = scriptMap[scriptId] || {}
-  const formattedUrl = FormatUrl.formatUrl(script.url, lineNumber, columnNumber)
-  return {
-    stack: [formattedUrl],
-    sourceMaps: [script.sourceMapUrl],
-    count,
-    name,
-  }
-}
-
-const prepareDisposables = (disposables, scriptMap) => {
-  const prepared = []
-  for (const disposable of disposables) {
-    prepared.push(prepareDisposable(disposable, scriptMap))
-  }
-  return prepared
-}
-
-const finishDisposable = (disposableWithStack) => {
-  const { stack, count, originalStack, originalName, name } = disposableWithStack
-  return {
-    name: originalName || name,
-    count,
-    location: originalStack?.[0] || stack?.[0] || '',
-  }
-}
-
-const finishDisposables = (disposablesWithStack) => {
-  return disposablesWithStack.map(finishDisposable)
-}
-
-const improveDisposableOutput = async (disposables, scriptMap) => {
-  const prepared = prepareDisposables(disposables, scriptMap)
-  const classNames = true
-  const withOriginalStack = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(prepared, classNames)
-  const finished = finishDisposables(withOriginalStack)
-  return finished
-}
+import * as ImproveDisposableOutput from '../ImproveDisposableOutput/ImproveDisposableOutput.js'
 
 const addDeltas = (prettyBefore, prettyAfter) => {
   const newItems = []
@@ -68,8 +26,8 @@ export const compareDisposablesWithLocation = async (before, after) => {
   Assert.array(beforeResult)
   Assert.array(afterResult)
   Assert.object(scriptMap)
-  const prettyBefore = await improveDisposableOutput(beforeResult, scriptMap)
-  const prettyAfter = await improveDisposableOutput(afterResult, scriptMap)
+  const prettyBefore = await ImproveDisposableOutput.improveDisposableOutput(beforeResult, scriptMap)
+  const prettyAfter = await ImproveDisposableOutput.improveDisposableOutput(afterResult, scriptMap)
   const withDeltas = addDeltas(prettyBefore, prettyAfter)
   return withDeltas
 }
