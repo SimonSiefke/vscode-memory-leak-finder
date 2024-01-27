ea607739a23a44058f760233bc59d69e943c1451
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 21:58:42 +0100

    refactor: simplify import

--- packages/test-coordinator/src/parts/IpcParent/IpcParent.js
+++ packages/test-coordinator/src/parts/IpcParent/IpcParent.js
@@ -1,4 +1,4 @@
-import * as IpcParentModule from '../../../../test-coordinator/src/parts/IpcParentModule/IpcParentModule.js'
+import * as IpcParentModule from '../IpcParentModule/IpcParentModule.js'
 
 export const create = async ({ method, ...options }) => {
   const module = await IpcParentModule.getModule(method)
