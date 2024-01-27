e035ccd573ff7e624ebbd01f6cd72acc25c8b755
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 23:12:50 +0100

    feature: improve event target comparison (#925)

--- packages/memory-leak-finder/src/parts/CompareEventTargets/CompareEventTargets.js
+++ packages/memory-leak-finder/src/parts/CompareEventTargets/CompareEventTargets.js
@@ -1,7 +1,57 @@
-import * as CompareMapLeak from '../CompareMapLeak/CompareMapLeak.js'
-import * as GetEventTargetKey from '../GetEventTargetKey/GetEventTargetKey.js'
+import * as Assert from '../Assert/Assert.js'
+import * as Arrays from '../Arrays/Arrays.js'
+
+const getDifference = (prettyBefore, prettyAfter) => {
+  const beforeMap = Object.create(null)
+  for (const element of prettyBefore) {
+    beforeMap[element.description] = element.count
+  }
+  const result = []
+  for (const element of prettyAfter) {
+    const beforeCount = beforeMap[element.description] || 0
+    if (element.count > beforeCount) {
+      result.push({
+        ...element,
+        beforeCount,
+      })
+    }
+  }
+  return result
+}
+
+const compareEventTarget = (a, b) => {
+  return b.count - a.count || a.description.localeCompare(b.description)
+}
+
+const sort = (eventTargets) => {
+  return Arrays.toSorted(eventTargets, compareEventTarget)
+}
+
+const prettifyEventTargets = (eventTargets) => {
+  const countMap = Object.create(null)
+  for (const eventTarget of eventTargets) {
+    const { description } = eventTarget
+    countMap[description] ||= 0
+    countMap[description]++
+  }
+  const result = []
+  for (const [key, value] of Object.entries(countMap)) {
+    result.push({
+      description: key,
+      count: value,
+    })
+  }
+  const sorted = sort(result)
+  return sorted
+}
 
 export const compareEventTargets = (before, after) => {
-  const leaked = CompareMapLeak.compareMapLeak(before, after, GetEventTargetKey.getEventTargetKey)
-  return leaked
+  Assert.array(before)
+  Assert.array(after)
+  const prettyBefore = prettifyEventTargets(before)
+  const prettyAfter = prettifyEventTargets(after)
+  return {
+    before: prettyBefore,
+    after: prettyAfter,
+  }
 }
