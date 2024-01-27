637b559a4d94b3dbe9d41eed704c0f2c8613dd87
Simon Siefke <simon.siefke@gmail.com>
2024-01-27 20:55:58 +0100

    refactor: remove unused test worker command files

--- packages/test-worker-commands/src/parts/TestFinishedError/TestFinishedError.js
+++
@@ -1,6 +0,0 @@
-export class TestFinishedError extends Error {
-  constructor() {
-    super('test already finished')
-    this.name = 'TestFinishedError'
-  }
-}
--- packages/test-worker-commands/src/parts/TmpDir/TmpDir.js
+++
@@ -1,7 +0,0 @@
-import { mkdtemp } from 'node:fs/promises'
-import { tmpdir } from 'node:os'
-import { join } from 'node:path'
-
-export const create = () => {
-  return mkdtemp(join(tmpdir(), 'foo-'))
-}
