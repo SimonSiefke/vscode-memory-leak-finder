b8f2ae2a07cdb6a1edd8abf896e9a0bac85494ad
Simon Siefke <simon.siefke@gmail.com>
2024-01-28 10:07:39 +0100

    fix: release object group in measureDetachedDomNodesDifference (#935)

--- packages/memory-leak-finder/src/parts/MeasureDetachedDomNodesDifference/MeasureDetachedDomNodesDifference.js
+++ packages/memory-leak-finder/src/parts/MeasureDetachedDomNodesDifference/MeasureDetachedDomNodesDifference.js
@@ -1,19 +1,24 @@
 import * as CompareDetachedDomNodesDifference from '../CompareDetachedDomNodesDifference/CompareDetachedDomNodesDifference.js'
 import * as GetDetachedDomNodes from '../GetDetachedDomNodes/GetDetachedDomNodes.js'
 import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as ReleaseObjectGroup from '../ReleaseObjectGroup/ReleaseObjectGroup.js'
 
 export const id = MeasureId.DetachedDomNodesDifference
 
 export const create = (session) => {
-  return [session]
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
 }
 
-export const start = (session) => {
-  return GetDetachedDomNodes.getDetachedDomNodes(session)
+export const start = (session, objectGroup) => {
+  return GetDetachedDomNodes.getDetachedDomNodes(session, objectGroup)
 }
 
-export const stop = (session) => {
-  return GetDetachedDomNodes.getDetachedDomNodes(session)
+export const stop = async (session, objectGroup) => {
+  const result = await GetDetachedDomNodes.getDetachedDomNodes(session, objectGroup)
+  await ReleaseObjectGroup.releaseObjectGroup(session, objectGroup)
+  return result
 }
 
 export const compare = CompareDetachedDomNodesDifference.compareDetachedDomNodesDifference
