9eb60091435f60745bbe554cee2d72a5d94d1e6c
Simon Siefke <simon.siefke@gmail.com>
2024-01-28 09:46:12 +0100

    fix: release objectGroup in detachedDomNodes measure (#928)

--- packages/memory-leak-finder/src/parts/GetDetachedDomNodes/GetDetachedDomNodes.js
+++ packages/memory-leak-finder/src/parts/GetDetachedDomNodes/GetDetachedDomNodes.js
@@ -1,7 +1,7 @@
 import * as GetDescriptors from '../GetDescriptors/GetDescriptors.js'
 import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
 
-export const getDetachedDomNodes = async (session) => {
-  const descriptors = await GetDescriptors.getDescriptors(session, PrototypeExpression.Node)
+export const getDetachedDomNodes = async (session, objectGroup) => {
+  const descriptors = await GetDescriptors.getDescriptors(session, PrototypeExpression.Node, objectGroup)
   return descriptors
 }
