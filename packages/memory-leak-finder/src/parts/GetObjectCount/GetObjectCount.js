fa092aa18707a32ded37d8016ccea28e79c8bff8
Simon Siefke <simon.siefke@gmail.com>
2024-02-10 09:39:31 +0100

    feature: use objectgroup for object count measure (#956)

--- packages/memory-leak-finder/src/parts/GetObjectCount/GetObjectCount.js
+++ packages/memory-leak-finder/src/parts/GetObjectCount/GetObjectCount.js
@@ -7,14 +7,15 @@ import * as GetRemoteObjectLength from '../GetRemoteObjectLength/GetRemoteObject
  * @param {string} prototype
  * @returns {Promise<number>}
  */
-export const getObjectCount = async (session, prototype) => {
+export const getObjectCount = async (session, prototype, objectGroup = undefined) => {
   const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
     expression: prototype,
     returnByValue: false,
   })
   const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
     prototypeObjectId: prototypeDescriptor.objectId,
+    objectGroup,
   })
-  const fnResult1 = await GetRemoteObjectLength.getRemoteObjectLength(session, objects.objects.objectId, undefined)
+  const fnResult1 = await GetRemoteObjectLength.getRemoteObjectLength(session, objects.objects.objectId, objectGroup)
   return fnResult1
 }
