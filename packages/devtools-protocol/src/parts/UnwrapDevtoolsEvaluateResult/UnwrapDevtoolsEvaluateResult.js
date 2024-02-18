695105f1d9d7248578ffe6cc6480d50db7f0f6dc
Simon Siefke <simon.siefke@gmail.com>
2024-02-18 12:52:23 +0100

    feature: improve heap usage measure (#969)

--- packages/devtools-protocol/src/parts/UnwrapDevtoolsEvaluateResult/UnwrapDevtoolsEvaluateResult.js
+++ packages/devtools-protocol/src/parts/UnwrapDevtoolsEvaluateResult/UnwrapDevtoolsEvaluateResult.js
@@ -4,7 +4,7 @@ export const unwrapResult = (rawResult) => {
   if ('result' in rawResult) {
     rawResult = rawResult.result
   }
-  if ('documents' in rawResult) {
+  if ('documents' in rawResult || 'usedSize' in rawResult) {
     return rawResult
   }
   if ('error' in rawResult) {
