896efe2330865f23b555b2abb045f66defcfa78f
Simon Siefke <simon.siefke@gmail.com>
2024-02-18 12:17:28 +0100

    add export

--- packages/memory-leak-finder/src/parts/Measures/Measures.js
+++ packages/memory-leak-finder/src/parts/Measures/Measures.js
@@ -21,6 +21,7 @@ export * as MeasureEventListenersWithStackTraces from '../MeasureEventListenersW
 export * as MeasureEventTargetCount from '../MeasureEventTargetCount/MeasureEventTargetCount.js'
 export * as MeasureEventTargetDifference from '../MeasureEventTargetDifference/MeasureEventTargetDifference.js'
 export * as MeasureEventTargets from '../MeasureEventTargets/MeasureEventTargets.js'
+export * as MeasureHeapUsage from '../MeasureHeapUsage/MeasureHeapUsage.js'
 export * as MeasureFunctionCount from '../MeasureFunctionCount/MeasureFunctionCount.js'
 export * as MeasureGrowingDisposableStores from '../MeasureGrowingDisposableStores/MeasureGrowingDisposableStores.js'
 export * as MeasureInstanceCounts from '../MeasureInstanceCounts/MeasureInstanceCounts.js'
