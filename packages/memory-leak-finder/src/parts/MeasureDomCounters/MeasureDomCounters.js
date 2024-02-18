790ac382eb9928604cf9b9ef0a782980e547df30
Simon Siefke <simon.siefke@gmail.com>
2024-02-18 12:53:59 +0100

    feature: improve cli output for dom counters measure

--- packages/memory-leak-finder/src/parts/MeasureDomCounters/MeasureDomCounters.js
+++ packages/memory-leak-finder/src/parts/MeasureDomCounters/MeasureDomCounters.js
@@ -25,3 +25,20 @@ export const compare = (before, after) => {
 export const isLeak = () => {
   return false
 }
+
+export const summary = ({ before, after }) => {
+  const documentsBefore = before.documents
+  const nodesBefore = before.nodes
+  const eventListenersBefore = before.jsEventListeners
+  const documentsAfter = after.documents
+  const nodesAfter = after.nodes
+  const eventListenersAfter = after.jsEventListeners
+  return {
+    documentsBefore,
+    documentsAfter,
+    nodesBefore,
+    nodesAfter,
+    eventListenersBefore,
+    eventListenersAfter,
+  }
+}
