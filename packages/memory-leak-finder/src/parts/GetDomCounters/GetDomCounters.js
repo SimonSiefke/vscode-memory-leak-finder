3c0778197b5ee12c1da63907f403a9003237bf6d
Simon Siefke <simon.siefke@gmail.com>
2024-02-18 08:57:20 +0100

    feature: add dom counters measure

---
+++ packages/memory-leak-finder/src/parts/GetDomCounters/GetDomCounters.js
@@ -0,0 +1,8 @@
+export const getDomCounters = (session) => {
+  // TODO ask session for domCounters
+  return {
+    documents: 0,
+    nodes: 0,
+    jsEventListeners: 0,
+  }
+}
---
+++ packages/memory-leak-finder/src/parts/MeasureDomCounters/MeasureDomCounters.js
@@ -0,0 +1,27 @@
+import * as GetDomCounters from '../GetDomCounters/GetDomCounters.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+
+export const id = MeasureId.DetachedDomNodeCount
+
+export const create = (session) => {
+  return [session]
+}
+
+export const start = (session) => {
+  return GetDomCounters.getDomCounters(session)
+}
+
+export const stop = (session) => {
+  return GetDomCounters.getDomCounters(session)
+}
+
+export const compare = (before, after) => {
+  return {
+    before,
+    after,
+  }
+}
+
+export const isLeak = () => {
+  return false
+}
