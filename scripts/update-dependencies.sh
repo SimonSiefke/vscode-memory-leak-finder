adca9a06ec613ee9d013be479d9abbbc194b51c5
Simon Siefke <simon.siefke@gmail.com>
2024-04-23 17:36:39 +0200

    update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,7 +16,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x @types/jest -x got`
+  OUTPUT=`ncu -u -x @types/node -x @types/jest -x rollup`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
