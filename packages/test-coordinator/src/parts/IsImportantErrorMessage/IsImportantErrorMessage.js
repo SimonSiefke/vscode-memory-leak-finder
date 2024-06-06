5a16e9d03df0768cbbbb826cb582ea5458eb8c5e
Simon Siefke <simon.siefke@gmail.com>
2024-06-06 20:35:36 +0200

    feature: ignore mesa zink error (#1023)

--- packages/test-coordinator/src/parts/IsImportantErrorMessage/IsImportantErrorMessage.js
+++ packages/test-coordinator/src/parts/IsImportantErrorMessage/IsImportantErrorMessage.js
@@ -2,6 +2,9 @@ const RE_LIB_GIO_PROXY_ERROR = /Failed to load module: .*libgiolibproxy.so/
 const RE_LIB_PROXY_NOT_FOUND = /not found \(required by .*libproxy.so/
 
 export const isImportantErrorMessage = (data) => {
+  if (data.includes(`MESA: error: ZINK: vkCreateInstance failed (VK_ERROR_INCOMPATIBLE_DRIVER)`)) {
+    return false
+  }
   if (data.includes('Failed to connect to the bus')) {
     return false
   }
