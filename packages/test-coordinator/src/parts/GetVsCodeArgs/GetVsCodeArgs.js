52b3684feda4d1ef081832ddbad283ec46bdafe0
Simon Siefke <simon.siefke@gmail.com>
2024-06-20 20:00:03 +0200

    Discard changes to packages/test-coordinator/src/parts/GetVsCodeArgs/GetVsCodeArgs.js

--- packages/test-coordinator/src/parts/GetVsCodeArgs/GetVsCodeArgs.js
+++ packages/test-coordinator/src/parts/GetVsCodeArgs/GetVsCodeArgs.js
@@ -3,16 +3,16 @@ import * as ChromiumSwitches from '../ChromiumSwitches/ChromiumSwitches.js'
 export const getVscodeArgs = ({ userDataDir, extraLaunchArgs }) => {
   return [
     ...ChromiumSwitches.chromiumSwitches,
-    // '--wait',
-    // '--new-window',
-    // '--no-sandbox',
-    // '--disable-updates',
-    // '--skip-welcome',
-    // '--skip-release-notes',
-    // '--disable-workspace-trust',
-    // '--disable-extensions',
-    // '--user-data-dir',
-    // userDataDir,
+    '--wait',
+    '--new-window',
+    '--no-sandbox',
+    '--disable-updates',
+    '--skip-welcome',
+    '--skip-release-notes',
+    '--disable-workspace-trust',
+    '--disable-extensions',
+    '--user-data-dir',
+    userDataDir,
     ...extraLaunchArgs,
   ]
 }
