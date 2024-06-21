2424c5450da030644db066567961eae689ef45dd
Simon Siefke <simon.siefke@gmail.com>
2024-06-21 22:11:45 +0200

    format code

--- packages/e2e/src/settings.scroll.js
+++ packages/e2e/src/settings.scroll.js
@@ -1,12 +1,11 @@
-export const skip=true
-
+export const skip = true
 
 export const setup = async ({ Editor, Settings }) => {
   await Editor.closeAll()
   await Settings.open()
 }
 
-export const run = async ({ SettingsEditor, }) => {
+export const run = async ({ SettingsEditor }) => {
   // await Editor.closeAll()
   await SettingsEditor.scrollDown()
   // await SettingsEditor.scrollUp()
