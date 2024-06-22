78f37a4708f0f67c2b556d6b2157547408feb0b7
Simon Siefke <simon.siefke@gmail.com>
2024-06-22 20:12:39 +0200

    improve markdown test

--- packages/page-object/src/parts/MarkdownPreview/MarkdownPreview.js
+++ packages/page-object/src/parts/MarkdownPreview/MarkdownPreview.js
@@ -6,7 +6,10 @@ export const create = ({ expect, page, VError, electronApp }) => {
         const webView = page.locator('.webview')
         await expect(webView).toBeVisible()
         await expect(webView).toHaveClass('ready')
-        const heading = page.locator(id)
+        const childPage = await electronApp.waitForIframe({
+          url: /extensionId=vscode.markdown-language-features/,
+        })
+        const heading = childPage.locator(`#${id}`)
         await expect(heading).toBeVisible()
       } catch (error) {
         throw new VError(error, `Failed to check that markdown preview has heading ${id}`)
@@ -18,7 +21,7 @@ export const create = ({ expect, page, VError, electronApp }) => {
         const webView = page.locator('.webview')
         await expect(webView).toBeVisible()
         await expect(webView).toHaveClass('ready')
-        const childView = await electronApp.waitForIframe({
+        await electronApp.waitForIframe({
           url: /extensionId=vscode.markdown-language-features/,
         })
       } catch (error) {
