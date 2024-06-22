6b84f911cc6e81bd302bbf7a141c1ef74d8d8d1c
Simon Siefke <simon.siefke@gmail.com>
2024-06-22 19:39:19 +0200

    add test

---
+++ packages/download-worker/test/GetExecutablePathKey.test.js
@@ -0,0 +1,23 @@
+import { expect, test } from '@jest/globals'
+import * as Root from '../src/parts/Root/Root.js'
+import * as GetExecutablePathKey from '../src/parts/GetExecutablePathKey/GetExecutablePathKey.js'
+
+test('linux', () => {
+  const platform = 'linux'
+  expect(GetExecutablePathKey.getExecutablePathKey(platform)).toBe('linux')
+})
+
+test('darwin', () => {
+  const platform = 'darwin'
+  expect(GetExecutablePathKey.getExecutablePathKey(platform)).toBe('mac')
+})
+
+test('win32', () => {
+  const platform = 'win32'
+  expect(GetExecutablePathKey.getExecutablePathKey(platform)).toBe('win')
+})
+
+test('other', () => {
+  const platform = 'abc'
+  expect(GetExecutablePathKey.getExecutablePathKey(platform)).toBe('')
+})
