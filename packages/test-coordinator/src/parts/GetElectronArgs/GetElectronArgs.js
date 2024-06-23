0e86f911252482ea486621e4e3aa60434517fee9
Simon Siefke <simon.siefke@gmail.com>
2024-06-23 19:23:46 +0200

    Discard changes to packages/test-coordinator/src/parts/GetElectronArgs/GetElectronArgs.js

--- packages/test-coordinator/src/parts/GetElectronArgs/GetElectronArgs.js
+++ packages/test-coordinator/src/parts/GetElectronArgs/GetElectronArgs.js
@@ -3,6 +3,6 @@ export const getElectronArgs = ({ headlessMode, args = [] }) => {
   if (headlessMode) {
     allArgs.unshift('--headless')
   }
-  allArgs.unshift('--inspect-brk=0', '--remote-debugging-port=8220')
+  allArgs.unshift('--inspect-brk=0', '--remote-debugging-port=0')
   return allArgs
 }
