fa15ed1b523463fac92fcfaa31c6f25ce564cbb9
Simon Siefke <simon.siefke@gmail.com>
2024-06-23 20:16:50 +0200

    feature: add markdown preview update test (#1074)

---
+++ packages/e2e/src/markdown-preview.update.js
@@ -0,0 +1,22 @@
+export const skip = true
+
+export const setup = async ({ Workspace, Explorer, Editor }) => {
+  await Workspace.setFiles([
+    {
+      name: 'index.md',
+      content: `# hello world`,
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.refresh()
+  await Explorer.shouldHaveItem('index.md')
+  await Editor.open('index.md')
+}
+
+export const run = async ({ Editor, QuickPick, WellKnownCommands, MarkdownPreview }) => {
+  await QuickPick.executeCommand(WellKnownCommands.MarkdownOpenPreviewToTheSide)
+  await MarkdownPreview.shouldBeVisible()
+  await MarkdownPreview.shouldHaveHeading('hello-world')
+  // TODO update editor and verify that markdown preview updates as well
+}
