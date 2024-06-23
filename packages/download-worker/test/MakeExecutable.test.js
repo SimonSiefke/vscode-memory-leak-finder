d82baef406bca3ad10e8bad97e50844077d3fb05
Simon Siefke <simon.siefke@gmail.com>
2024-06-23 12:40:17 +0200

    feature: add makeExecutable test (#1066)

--- packages/download-worker/test/MakeExecutable.test.js
+++ packages/download-worker/test/MakeExecutable.test.js
@@ -1,4 +1,9 @@
-import { expect, jest, test } from '@jest/globals'
+import { beforeEach, expect, jest, test } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetModules()
+  jest.resetAllMocks()
+})
 
 jest.unstable_mockModule('node:fs/promises', () => {
   return {
@@ -8,6 +13,14 @@ jest.unstable_mockModule('node:fs/promises', () => {
 
 const MakeExecutable = await import('../src/parts/MakeExecutable/MakeExecutable.js')
 const fs = await import('node:fs/promises')
+
+test('makeExecutable - error', async () => {
+  jest.spyOn(fs, 'chmod').mockRejectedValue(new TypeError('x is not a function'))
+  const file = '/test/file.txt'
+  await expect(MakeExecutable.makeExecutable(file)).rejects.toThrow(
+    new Error('Failed to make file executable: TypeError: x is not a function'),
+  )
+})
 
 test('makeExecutable', async () => {
   const file = '/test/file.txt'
