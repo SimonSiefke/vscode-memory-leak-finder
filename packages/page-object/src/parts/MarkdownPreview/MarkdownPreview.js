be500b6582c4773e672cfca795660f9fbd6cc6fd
Simon Siefke <simon.siefke@gmail.com>
2024-06-23 19:42:32 +0200

    Discard changes to packages/page-object/src/parts/MarkdownPreview/MarkdownPreview.js

--- packages/page-object/src/parts/MarkdownPreview/MarkdownPreview.js
+++ packages/page-object/src/parts/MarkdownPreview/MarkdownPreview.js
@@ -9,8 +9,7 @@ export const create = ({ expect, page, VError, electronApp }) => {
         const childPage = await electronApp.waitForIframe({
           url: /extensionId=vscode.markdown-language-features/,
         })
-        const frame = childPage.frameLocator('iframe')
-        const heading = frame.locator(`#${id}`)
+        const heading = childPage.locator(`#${id}`)
         await expect(heading).toBeVisible()
       } catch (error) {
         throw new VError(error, `Failed to check that markdown preview has heading ${id}`)
