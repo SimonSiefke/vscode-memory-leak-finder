7525339959289c335a26ad85cb7605c9b7c076e7
Simon Siefke <simon.siefke@gmail.com>
2024-06-23 20:11:38 +0200

    feature: add extension detail tabs test

--- packages/page-object/src/parts/ExtensionsDetailView/ExtensionsDetailView.js
+++ packages/page-object/src/parts/ExtensionsDetailView/ExtensionsDetailView.js
@@ -11,5 +11,28 @@ export const create = ({ expect, page, VError }) => {
         throw new VError(error, `Failed to verify extension detail heading ${text}`)
       }
     },
+    async shouldHaveTab(text) {
+      try {
+        const tab = page.locator('.extension-editor .action-label', {
+          hasText: text,
+        })
+        await expect(tab).toBeVisible()
+        await expect(tab).toHaveAttribute('aria-checked', 'true')
+      } catch (error) {
+        throw new VError(error, `Failed to verify extension detail tab ${text}`)
+      }
+    },
+    async openTab(text) {
+      try {
+        const tab = page.locator('.extension-editor .action-label', {
+          hasText: text,
+        })
+        await expect(tab).toBeVisible()
+        await tab.click()
+        await expect(tab).toHaveAttribute('aria-checked', 'true')
+      } catch (error) {
+        throw new VError(error, `Failed to open extension detail tab ${text}`)
+      }
+    },
   }
 }
