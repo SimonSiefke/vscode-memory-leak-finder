6d2c4545afdfd9eef73837647567bef2c7a5ca2d
Simon Siefke <simon.siefke@gmail.com>
2024-06-23 23:39:17 +0200

    feature: add settings context menu test (#1080)

---
+++ packages/e2e/src/settings.context-menu.js
@@ -0,0 +1,15 @@
+export const skip = true
+
+export const setup = async ({ Editor, SettingsEditor }) => {
+  await Editor.closeAll()
+  await SettingsEditor.open()
+  await SettingsEditor.search({
+    value: 'Editor: Auto Closing Comments',
+    resultCount: 2,
+  })
+}
+
+export const run = async ({ SettingsEditor, ContextMenu }) => {
+  await SettingsEditor.openSettingsContextMenu('Comments')
+  await ContextMenu.close()
+}
--- packages/page-object/src/parts/SettingsEditor/SettingsEditor.js
+++ packages/page-object/src/parts/SettingsEditor/SettingsEditor.js
@@ -114,5 +114,24 @@ export const create = ({ expect, page, VError }) => {
         throw new VError(error, `Failed to disable checkbox "${name}"`)
       }
     },
+    async openSettingsContextMenu(name) {
+      try {
+        await page.waitForIdle()
+        const item = page.locator(`.setting-item-category`, {
+          hasText: `${name}: `,
+        })
+        await expect(item).toBeVisible()
+        await item.click()
+        await page.waitForIdle()
+        const outerItem = page.locator(`.settings-editor-tree .monaco-list-row[aria-label^="${name}"]`)
+        await expect(outerItem).toHaveCount(1)
+        await expect(outerItem).toHaveAttribute('aria-selected', 'true')
+        const moreActions = outerItem.locator('[aria-label^="More Actions"]')
+        await expect(moreActions).toBeVisible()
+        await moreActions.click()
+      } catch (error) {
+        throw new VError(error, `Failed to open settings context menu for "${name}"`)
+      }
+    },
   }
 }
