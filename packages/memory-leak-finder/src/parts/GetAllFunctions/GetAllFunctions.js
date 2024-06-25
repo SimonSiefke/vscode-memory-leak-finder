af2482521aee1a7dd5f4a03f1d51a06f930a8a51
Simon Siefke <simon.siefke@gmail.com>
2024-06-26 00:36:44 +0200

    feature: add function create array name map from scopes for better in… (#1100)

---
+++ packages/memory-leak-finder/src/parts/GetAllFunctions/GetAllFunctions.js
@@ -0,0 +1,26 @@
+import * as Assert from '../Assert/Assert.js'
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as GetDescriptorValues from '../GetDescriptorValues/GetDescriptorValues.js'
+import * as GetObjectIds from '../GetObjectIds/GetObjectIds.js'
+import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+
+export const getAllFunctions = async (session, objectGroup) => {
+  Assert.object(session)
+  Assert.string(objectGroup)
+  const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: PrototypeExpression.Function,
+    returnByValue: false,
+  })
+  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
+    prototypeObjectId: prototypeDescriptor.objectId,
+    objectGroup,
+  })
+  const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
+    objectId: objects.objects.objectId,
+    ownProperties: true,
+    generatePreview: false,
+  })
+  const descriptors = GetDescriptorValues.getDescriptorValues(fnResult1.result)
+  const objectIds = GetObjectIds.getObjectIds(descriptors)
+  return objectIds
+}
