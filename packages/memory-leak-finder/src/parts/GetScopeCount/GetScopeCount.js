5ebbb83e722e201a071be71e3c9ac03360215f7c
Simon Siefke <simon.siefke@gmail.com>
2024-06-25 23:08:43 +0200

    get all function object ids

--- packages/memory-leak-finder/src/parts/GetScopeCount/GetScopeCount.js
+++ packages/memory-leak-finder/src/parts/GetScopeCount/GetScopeCount.js
@@ -1,5 +1,9 @@
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+import * as GetDescriptorValues from '../GetDescriptorValues/GetDescriptorValues.js'
+import * as GetObjectIds from '../GetObjectIds/GetObjectIds.js'
+
+const getObjectIds = (descriptors) => {}
 
 export const getScopeCount = async (session, objectGroup) => {
   const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
@@ -10,6 +14,13 @@ export const getScopeCount = async (session, objectGroup) => {
     prototypeObjectId: prototypeDescriptor.objectId,
     objectGroup,
   })
-  console.log({ objects })
+  const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
+    objectId: objects.objects.objectId,
+    ownProperties: true,
+    generatePreview: false,
+  })
+  const descriptors = GetDescriptorValues.getDescriptorValues(fnResult1.result)
+  const objectIds = GetObjectIds.getObjectIds(descriptors)
+  console.log({ objectIds })
   return 0
 }
