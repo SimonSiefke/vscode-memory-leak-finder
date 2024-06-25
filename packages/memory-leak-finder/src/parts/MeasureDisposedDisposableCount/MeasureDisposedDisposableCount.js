c556e47d238e7450252e6be545c6f01281652665
Simon Siefke <simon.siefke@gmail.com>
2024-06-25 18:52:31 +0200

    feature: add disposed disposable count measure to measure disposed disposable count

---
+++ packages/memory-leak-finder/src/parts/MeasureDisposedDisposableCount/MeasureDisposedDisposableCount.js
@@ -0,0 +1,27 @@
+import * as CompareCount from '../CompareCount/CompareCount.js'
+import * as GetDisposableCount from '../GetDisposableCount/GetDisposableCount.js'
+import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as ReleaseObjectGroup from '../ReleaseObjectGroup/ReleaseObjectGroup.js'
+
+export const id = MeasureId.DisposedDisposableCount
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
+}
+
+export const start = (session, objectGroup) => {
+  return GetDisposableCount.getDisposableCount(session, objectGroup)
+}
+
+export const stop = async (session, objectGroup) => {
+  const result = await GetDisposableCount.getDisposableCount(session, objectGroup)
+  await ReleaseObjectGroup.releaseObjectGroup(session, objectGroup)
+  return result
+}
+
+export const compare = CompareCount.compareCount
+
+export const isLeak = IsLeakCount.isLeakCount
