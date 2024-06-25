99c9f93effe2947a11a1a3e741625a223bb818ae
Simon Siefke <simon.siefke@gmail.com>
2024-06-25 19:30:44 +0200

    feature: add functions to measure disposed disposables (#1094)

---
+++ packages/memory-leak-finder/src/parts/MeasureDisposedDisposables/MeasureDisposedDisposables.js
@@ -0,0 +1,34 @@
+import * as CompareDisposablesWithLocation from '../CompareDisposablesWithLocation/CompareDisposablesWithLocation.js'
+import * as GetDisposedDisposablesWithLocation from '../GetDisposedDisposablesWithLocation/GetDisposedDisposablesWithLocation.js'
+import * as IsLeakDisposables from '../IsLeakDisposables/IsLeakDisposables.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as ReleaseObjectGroup from '../ReleaseObjectGroup/ReleaseObjectGroup.js'
+import * as ScriptHandler from '../ScriptHandler/ScriptHandler.js'
+
+export const id = MeasureId.DisposedDisposables
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  const scriptHandler = ScriptHandler.create()
+  return [session, objectGroup, scriptHandler]
+}
+
+export const start = async (session, objectGroup, scriptHandler) => {
+  await scriptHandler.start(session)
+  return GetDisposedDisposablesWithLocation.getDisposedDisposablesWithLocation(session, objectGroup, scriptHandler.scriptMap)
+}
+
+export const stop = async (session, objectGroup, scriptHandler) => {
+  await scriptHandler.stop(session)
+  const result = await GetDisposedDisposablesWithLocation.getDisposedDisposablesWithLocation(session, objectGroup, scriptHandler.scriptMap)
+  await ReleaseObjectGroup.releaseObjectGroup(session, objectGroup)
+  return {
+    result,
+    scriptMap: scriptHandler.scriptMap,
+  }
+}
+
+export const compare = CompareDisposablesWithLocation.compareDisposablesWithLocation
+
+export const isLeak = IsLeakDisposables.isLeakDisposables
