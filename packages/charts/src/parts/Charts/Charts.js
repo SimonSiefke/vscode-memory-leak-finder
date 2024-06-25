002a55615023c535a890cf7ba8486f493375bc50
Simon Siefke <simon.siefke@gmail.com>
2024-06-25 19:05:18 +0200

    feature: add canvas chart (#1092)

--- packages/charts/src/parts/Charts/Charts.js
+++ packages/charts/src/parts/Charts/Charts.js
@@ -1,3 +1,4 @@
+export * as CanvasCount from '../CreateCanvasCountChart/CreateCanvasCountChart.js'
 export * as DetachedDomNodeCount from '../CreateDetachedDomNodeCountChart/CreateDetachedDomNodeCountChart.js'
 export * as FunctionCount from '../CreateFunctionCountChart/CreateFunctionCountChart.js'
 export * as ObjectCount from '../CreateObjectCountChart/CreateObjectCountChart.js'
