9db087253fc2ec2efd8d4d8e9139cbd916cde239
Simon Siefke <simon.siefke@gmail.com>
2024-06-27 23:18:12 +0200

    get items

--- packages/memory-leak-finder/src/parts/GetNamedArrayCount/GetNamedArrayCount.js
+++ packages/memory-leak-finder/src/parts/GetNamedArrayCount/GetNamedArrayCount.js
@@ -1,4 +1,15 @@
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 import * as GetFlatScopeList from '../GetFlatScopeList/GetFlatScopeList.js'
+
+const getScopeItems = async (session, objectId) => {
+  const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
+    objectId: objectId,
+    ownProperties: true,
+    generatePreview: false,
+  })
+  const items = fnResult1.result
+  return items
+}
 
 /**
  * @param {any} session
@@ -6,6 +17,16 @@ import * as GetFlatScopeList from '../GetFlatScopeList/GetFlatScopeList.js'
  */
 export const getNamedArrayCount = async (session, objectGroup) => {
   const flatScopeList = await GetFlatScopeList.getFlatScopeList(session, objectGroup)
-  console.log({ flatScopeList })
+  const maxLength = 10_000
+  if (flatScopeList.length > maxLength) {
+    flatScopeList.length = maxLength
+  }
+  const promises = []
+  for (const item of flatScopeList) {
+    promises.push(getScopeItems(session, item.objectId))
+  }
+  const scopeItems = await Promise.all(promises)
+  const flatScopeItems = scopeItems.flat(1)
+  console.log(flatScopeItems)
   return 0
 }
