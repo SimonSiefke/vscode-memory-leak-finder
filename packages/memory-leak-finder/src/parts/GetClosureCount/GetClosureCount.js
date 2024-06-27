0943f9f3fca28d65f0d8da922fdfe78eb958a440
Simon Siefke <simon.siefke@gmail.com>
2024-06-27 22:56:11 +0200

    feature: simplify scope count measure

--- packages/memory-leak-finder/src/parts/GetClosureCount/GetClosureCount.js
+++ packages/memory-leak-finder/src/parts/GetClosureCount/GetClosureCount.js
@@ -4,8 +4,20 @@ const isClosure = (scope) => {
   return scope.description.startsWith('Closure (')
 }
 
+const getUniqueCount = (flatScopeMap) => {
+  const counts = Object.create(null)
+  for (const item of flatScopeMap) {
+    counts[item.description] ||= 0
+    counts[item.description]++
+  }
+  return counts
+}
+
 export const getClosureCount = async (session, objectGroup) => {
   const flatScopeList = await GetFlatScopeList.getFlatScopeList(session, objectGroup)
+  // console.log({ flatScopeList })
+  const uniqueCounts = getUniqueCount(flatScopeList)
+  console.log(JSON.stringify(uniqueCounts, null, 2))
   const closures = flatScopeList.filter(isClosure)
   const closureCount = closures.length
   return closureCount
--- packages/memory-leak-finder/src/parts/GetScopeCount/GetScopeCount.js
+++ packages/memory-leak-finder/src/parts/GetScopeCount/GetScopeCount.js
@@ -1,8 +1,6 @@
-import * as GetAllFunctions from '../GetAllFunctions/GetAllFunctions.js'
-import * as GetAllScopeProperties from '../GetAllScopeProperties/GetAllScopeProperties.js'
+import * as GetFlatScopeList from '../GetFlatScopeList/GetFlatScopeList.js'
 
 export const getScopeCount = async (session, objectGroup) => {
-  const objectIds = await GetAllFunctions.getAllFunctions(session, objectGroup)
-  const scopeProperties = await GetAllScopeProperties.getAllScopeProperties(session, objectGroup, objectIds)
-  return scopeProperties.length
+  const flatScopeList = await GetFlatScopeList.getFlatScopeList(session, objectGroup)
+  return flatScopeList.length
 }
