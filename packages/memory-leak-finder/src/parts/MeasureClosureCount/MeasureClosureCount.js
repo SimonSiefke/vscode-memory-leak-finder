cafb0809301cc0490bf9e5b0dcccae35a601fa1f
Simon Siefke <simon.siefke@gmail.com>
2024-06-27 20:27:43 +0200

    fix measure

--- packages/memory-leak-finder/src/parts/MeasureClosureCount/MeasureClosureCount.js
+++ packages/memory-leak-finder/src/parts/MeasureClosureCount/MeasureClosureCount.js
@@ -1,36 +1,24 @@
-import * as GetAllFunctions from '../GetAllFunctions/GetAllFunctions.js'
-import * as GetFunctionScopeProperty from '../GetFunctionScopeProperty/GetScopeProperties.js'
-import * as Assert from '../Assert/Assert.js'
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
-import * as ParseFunctionScopeListProperty from '../ParseFunctionScopeListProperty/ParseFunctionScopeListProperty.js'
+import * as CompareCount from '../CompareCount/CompareCount.js'
+import * as GetClosureCount from '../GetClosureCount/GetClosureCount.js'
+import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
 
-const getScopeListProperties = async (session, objectId) => {
-  const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
-    objectId: objectId,
-    ownProperties: true,
-    generatePreview: false,
-  })
-  return fnResult1
+export const id = MeasureId.FunctionCount
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
 }
 
-export const getScopes = async (session, objectGroup) => {
-  const objectIds = await GetAllFunctions.getAllFunctions(session, objectGroup)
-  // console.log(objectIds)
-  const promises = []
-  for (const objectId of objectIds) {
-    promises.push(GetFunctionScopeProperty.getFunctionScopeProperty(session, objectGroup, objectId))
-  }
-  const scopeListsObjectIds = await Promise.all(promises)
-  const promises2 = []
-  for (const objectId of scopeListsObjectIds) {
-    if (!objectId) {
-      continue
-    }
-    promises2.push(getScopeListProperties(session, objectId))
-  }
-  const scopeLists = await Promise.all(promises2)
-  const flatScopeLists = scopeLists.flat(1)
-  console.log(flatScopeLists)
-  // const scopeProperties = await GetAllScopeProperties.getAllScopeProperties(session, objectGroup, objectIds)
-  return 0
+export const start = (session, objectGroup) => {
+  return GetClosureCount.getClosureCount(session, objectGroup)
 }
+
+export const stop = (session, objectGroup) => {
+  return GetClosureCount.getClosureCount(session, objectGroup)
+}
+
+export const compare = CompareCount.compareCount
+
+export const isLeak = IsLeakCount.isLeakCount
