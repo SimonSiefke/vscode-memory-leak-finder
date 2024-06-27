f0838995e29d057ff5f5dceaaacd8891540965b8
Simon Siefke <simon.siefke@gmail.com>
2024-06-27 20:33:18 +0200

    simplify measure

--- packages/memory-leak-finder/src/parts/GetClosureCount/GetClosureCount.js
+++ packages/memory-leak-finder/src/parts/GetClosureCount/GetClosureCount.js
@@ -1,37 +1,12 @@
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
-import * as GetAllFunctions from '../GetAllFunctions/GetAllFunctions.js'
-import * as GetFunctionScopeProperty from '../GetFunctionScopeProperty/GetScopeProperties.js'
-
-const getScopeListProperties = async (session, objectId) => {
-  const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
-    objectId: objectId,
-    ownProperties: true,
-    generatePreview: false,
-  })
-  return fnResult1
-}
+import * as GetFlatScopeList from '../GetFlatScopeList/GetFlatScopeList.js'
 
 const isClosure = (scope) => {
   return scope.value && scope.value.type === 'object' && scope.value.description.startsWith('Closure (')
 }
 
 export const getClosureCount = async (session, objectGroup) => {
-  const objectIds = await GetAllFunctions.getAllFunctions(session, objectGroup)
-  const promises = []
-  for (const objectId of objectIds) {
-    promises.push(GetFunctionScopeProperty.getFunctionScopeProperty(session, objectGroup, objectId))
-  }
-  const scopeListsObjectIds = await Promise.all(promises)
-  const promises2 = []
-  for (const objectId of scopeListsObjectIds) {
-    if (!objectId) {
-      continue
-    }
-    promises2.push(getScopeListProperties(session, objectId))
-  }
-  const scopeLists = await Promise.all(promises2)
-  const flatScopeLists = scopeLists.flat(1)
-  const closures = flatScopeLists.filter(isClosure)
+  const flatScopeList = await GetFlatScopeList.getFlatScopeList(session, objectGroup)
+  const closures = flatScopeList.filter(isClosure)
   const closureCount = closures.length
   return closureCount
 }
---
+++ packages/memory-leak-finder/src/parts/GetFlatScopeList/GetFlatScopeList.js
@@ -0,0 +1,31 @@
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as GetAllFunctions from '../GetAllFunctions/GetAllFunctions.js'
+import * as GetFunctionScopeProperty from '../GetFunctionScopeProperty/GetScopeProperties.js'
+
+const getScopeListProperties = async (session, objectId) => {
+  const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
+    objectId: objectId,
+    ownProperties: true,
+    generatePreview: false,
+  })
+  return fnResult1
+}
+
+export const getFlatScopeList = async (session, objectGroup) => {
+  const objectIds = await GetAllFunctions.getAllFunctions(session, objectGroup)
+  const promises = []
+  for (const objectId of objectIds) {
+    promises.push(GetFunctionScopeProperty.getFunctionScopeProperty(session, objectGroup, objectId))
+  }
+  const scopeListsObjectIds = await Promise.all(promises)
+  const promises2 = []
+  for (const objectId of scopeListsObjectIds) {
+    if (!objectId) {
+      continue
+    }
+    promises2.push(getScopeListProperties(session, objectId))
+  }
+  const scopeLists = await Promise.all(promises2)
+  const flatScopeLists = scopeLists.flat(1)
+  return flatScopeLists
+}
