8418aea3446dcbf4a4cad7702a59e21acf721df7
Simon Siefke <simon.siefke@gmail.com>
2024-06-28 21:58:20 +0200

    fix measure

--- packages/memory-leak-finder/src/parts/GetObjectShapeCount/GetObjectShapeCount.js
+++ packages/memory-leak-finder/src/parts/GetObjectShapeCount/GetObjectShapeCount.js
@@ -1,9 +1,19 @@
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import * as GetObjectShapeCountFromHeapSnapshot from '../GetObjectShapeCountFromHeapSnapshot/GetObjectShapeCountFromHeapSnapshot.js'
+import * as HeapSnapshot from '../HeapSnapshot/HeapSnapshot.js'
+import * as Root from '../Root/Root.js'
+
 /**
  *
  * @param {any} session
  * @returns {Promise<number>}
  */
 export const getObjectShapeCount = async (session, objectGroup) => {
-  // TODO get shape count from heapsnapshot
-  return 0
+  const outFile = join(Root.root, '.vscode-heapsnapshots', `object-shape.json`)
+  await HeapSnapshot.takeHeapSnapshot(session, outFile)
+  const content = await readFile(outFile, 'utf8')
+  const value = JSON.parse(content)
+  const count = GetObjectShapeCountFromHeapSnapshot.getObjectShapeCountFromHeapSnapshot(value)
+  return count
 }
