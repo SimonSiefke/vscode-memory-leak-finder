a4fb69812c80fce31070de651eb8fc3532462cc6
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 16:41:39 +0200

    feature: use jsonrpc package for video recording worker

--- packages/video-recording-worker/src/parts/HandleIpc/HandleIpc.js
+++ packages/video-recording-worker/src/parts/HandleIpc/HandleIpc.js
@@ -1,8 +1,20 @@
-import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.js'
+import * as Assert from '../Assert/Assert.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+
+const prepareError = (error) => {
+  return error
+}
+
+const logError = () => {}
+
+const requiresSocket = () => {
+  return false
+}
 
 export const handleIpc = (ipc, execute, resolve) => {
+  Assert.object(ipc)
   const handleMessage = (message) => {
-    return HandleJsonRpcMessage.handleJsonRpcMessage(ipc, message, execute, resolve)
+    return JsonRpc.handleJsonRpcMessage(ipc, message, execute, resolve, prepareError, logError, requiresSocket)
   }
   ipc.on('message', handleMessage)
 }
