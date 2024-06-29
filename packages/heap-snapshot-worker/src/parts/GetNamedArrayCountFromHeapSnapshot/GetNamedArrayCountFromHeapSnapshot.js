3f8b80689f0b45d9e2785a8b02707f9c0a54ee90
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 19:27:52 +0200

    feature: try to improve array measure

--- packages/heap-snapshot-worker/src/parts/GetNamedArrayCountFromHeapSnapshot/GetNamedArrayCountFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetNamedArrayCountFromHeapSnapshot/GetNamedArrayCountFromHeapSnapshot.js
@@ -1,16 +1,27 @@
 import * as Assert from '../Assert/Assert.js'
 import * as ParseHeapSnapshot from '../ParseHeapSnapshot/ParseHeapSnapshot.js'
 
-const isClosure = node=>{
-  return node.type==='closure'
+const isClosure = (node) => {
+  return node.type === 'closure'
 }
 
 export const getNamedArrayCountFromHeapSnapshot = (heapsnapshot) => {
   Assert.object(heapsnapshot)
   const { parsedNodes, graph } = ParseHeapSnapshot.parseHeapSnapshot(heapsnapshot)
   const closureNodes = parsedNodes.filter(isClosure)
+  // const closureChildren = closureNodes.flatMap((node) => {
+  //   const edges = graph[node.id]
+  //   return edges.map((edge) => {
+  //     return parsedNodes[edge]
+  //   })
+  // })
+  // for (const closureNode of closureNodes) {
+  // }
+  const interesting = closureNodes.find((item) => item.name === 'IpcChildWithModuleWorker')
+  const other = parsedNodes[204218]
+  console.log({ other, len: parsedNodes.length })
   return {
-    closureNodes,
+    interesting,
     graph,
   }
 }
