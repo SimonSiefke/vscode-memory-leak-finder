7650483725b20cad940906fe4fe7a9a702b92ed2
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 12:13:34 +0200

    feature: prepare to add chapters to video recordings (#1119)

--- packages/video-recording-worker/src/parts/VideoWorkerCommandType/VideoWorkerCommandType.js
+++ packages/video-recording-worker/src/parts/VideoWorkerCommandType/VideoWorkerCommandType.js
@@ -1,2 +1,3 @@
 export const VideoRecordingStart = 'VideoRecording.start'
+export const VideoRecordingAddChapter = 'VideoRecording.addChapter'
 export const ConnectDevtools = 'ConnectDevtools.connectDevtools'
