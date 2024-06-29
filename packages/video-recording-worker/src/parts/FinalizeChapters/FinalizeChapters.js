9baefba14474f686ecdd25fd9c277c81fff65289
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 15:24:35 +0200

    fix chapters

--- packages/video-recording-worker/src/parts/FinalizeChapters/FinalizeChapters.js
+++ packages/video-recording-worker/src/parts/FinalizeChapters/FinalizeChapters.js
@@ -1,17 +1,21 @@
 import { existsSync } from 'fs'
-import { writeFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
 import { basename, dirname, join } from 'path'
 import * as Exec from '../Exec/Exec.js'
 import * as FfmpegProcessState from '../FfmpegProcessState/FfmpegProcessState.js'
 import * as VideoChapter from '../VideoChapter/VideoChapter.js'
 
-const getMetaDataOptions = (baseName) => {
+const getMetaDataInputOptions = (baseName) => {
+  return ['-i', baseName, '-i', '-y', 'meta.ffmeta']
+}
+
+const getMetaDataOutputOptions = (baseName) => {
   return ['-i', baseName, '-i', 'metadata.txt', '-map_metadata', '1', '-codec', 'copy', '-y', 'out.webm']
 }
 
-const getChaptersData = (chapters) => {
+const getChaptersData = (previousMetaData, chapters) => {
   let start = 0
-  let data = ''
+  let data = previousMetaData + '\n'
   for (const chapter of chapters) {
     const end = chapter.time
     data += `[CHAPTER]
@@ -19,6 +23,7 @@ TIMEBASE=1/1000
 START=${Math.round(start)}
 END=${Math.round(end)}
 title=${chapter.name}
+
 `
     start = end
   }
@@ -37,10 +42,15 @@ export const finalizeChapters = async () => {
   const ffmpegPath = 'ffmpeg'
   const baseName = basename(outFile)
   const folderName = dirname(outFile)
-  const metaDataArgs = getMetaDataOptions(baseName)
+  const metaDataInputArgs = getMetaDataInputOptions(baseName)
+  await Exec.exec(ffmpegPath, metaDataInputArgs, {
+    cwd: folderName,
+  })
+  const previousMetaData = await readFile(join(folderName, 'meta.ffmeta'))
   const chapters = VideoChapter.state.chapters
-  const data = getChaptersData(chapters)
+  const data = getChaptersData(previousMetaData, chapters)
   const metaDataPath = join(folderName, 'metadata.txt')
+  const metaDataArgs = getMetaDataOutputOptions(baseName)
   await writeFile(metaDataPath, data)
   await Exec.exec(ffmpegPath, metaDataArgs, {
     cwd: folderName,
