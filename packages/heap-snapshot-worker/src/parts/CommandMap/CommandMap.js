e5523169f362ece2dcde8ec813e4cba5ba6e38c1
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 17:50:39 +0200

    add object shape count function

--- packages/heap-snapshot-worker/src/parts/CommandMap/CommandMap.js
+++ packages/heap-snapshot-worker/src/parts/CommandMap/CommandMap.js
@@ -1,7 +1,9 @@
+import * as GetObjectShapeCountFromHeapSnapshot from '../GetObjectShapeCountFromHeapSnapshot/GetObjectShapeCountFromHeapSnapshot.js'
 import * as ParseHeapSnapshot from '../ParseHeapSnapshot/ParseHeapSnapshot.js'
 import * as ParseHeapSnapshotStrings from '../ParseHeapSnapshotStrings/ParseHeapSnapshotStrings.js'
 
 export const commandMap = {
   'HeapSnapshot.parseStrings': ParseHeapSnapshotStrings.parseHeapSnapshotStrings,
   'HeapSnapshot.parseHeapSnapshot': ParseHeapSnapshot.parseHeapSnapshot,
+  'HeapSnapshot.parseObjectShapeCount': GetObjectShapeCountFromHeapSnapshot.getObjectShapeCountFromHeapSnapshot,
 }
