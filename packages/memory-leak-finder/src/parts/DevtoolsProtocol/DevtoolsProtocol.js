8975c5b734c3ac04b8ce5595030616bbe414e36b
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 15:55:09 +0200

    tsc

--- packages/memory-leak-finder/src/parts/DevtoolsProtocol/DevtoolsProtocol.js
+++ packages/memory-leak-finder/src/parts/DevtoolsProtocol/DevtoolsProtocol.js
@@ -1,7 +1,20 @@
 const url = new URL('../../../../devtools-protocol/src/index.js', import.meta.url).toString()
 const module = await import(url)
 
-const { DevtoolsProtocolHeapProfiler, DevtoolsProtocolPage, DevtoolsProtocolRuntime, DevtoolsProtocolTarget, DevtoolsProtocolMemory } =
-  module
+const {
+  DevtoolsProtocolHeapProfiler,
+  DevtoolsProtocolPage,
+  DevtoolsProtocolRuntime,
+  DevtoolsProtocolTarget,
+  DevtoolsProtocolMemory,
+  DevtoolsProtocolDomDebugger,
+} = module
 
-export { DevtoolsProtocolHeapProfiler, DevtoolsProtocolPage, DevtoolsProtocolRuntime, DevtoolsProtocolTarget, DevtoolsProtocolMemory }
+export {
+  DevtoolsProtocolHeapProfiler,
+  DevtoolsProtocolPage,
+  DevtoolsProtocolRuntime,
+  DevtoolsProtocolTarget,
+  DevtoolsProtocolMemory,
+  DevtoolsProtocolDomDebugger,
+}
