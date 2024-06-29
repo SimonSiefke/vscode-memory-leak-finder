8c66bedb99f9e50ccd9f5e837b77b1f88b78f70b
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 20:30:36 +0200

    try to improve measure

--- packages/heap-snapshot-worker/src/parts/GetNamedArrayCountFromHeapSnapshot/GetNamedArrayCountFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetNamedArrayCountFromHeapSnapshot/GetNamedArrayCountFromHeapSnapshot.js
@@ -8,20 +8,43 @@ const isClosure = (node) => {
 export const getNamedArrayCountFromHeapSnapshot = (heapsnapshot) => {
   Assert.object(heapsnapshot)
   const { parsedNodes, graph } = ParseHeapSnapshot.parseHeapSnapshot(heapsnapshot)
-  const closureNodes = parsedNodes.filter(isClosure)
+  // const closureNodes = parsedNodes.filter(isClosure)
   // const closureChildren = closureNodes.flatMap((node) => {
   //   const edges = graph[node.id]
   //   return edges.map((edge) => {
   //     return parsedNodes[edge]
   //   })
   // })
+  // const arrays = closureChildren.filter((item) => item.type === 'array')
+  // console.log({ arrays })
   // for (const closureNode of closureNodes) {
   // }
-  const interesting = closureNodes.find((item) => item.name === 'IpcChildWithModuleWorker')
-  // const
-  const other = parsedNodes[204218]
-  console.log({ other, len: parsedNodes.length })
-  const items = graph[interesting.id]
+  // console.log({ parsedNodes })
+  const nameMap = Object.create(null)
+  for (let i = 0; i < parsedNodes.length; i++) {
+    // const node = parsedNodes[i]
+    // // console.log({ node })
+    // if (node.type !== 'array') {
+    //   continue
+    // }
+    // console.log({ node })
+    // for (const [key, value] of Object.entries(graph)) {
+    //   if (value.includes(i)) {
+    //     console.log({ key })
+    //   }
+    // }
+  }
+  // const interesting = parsedNodes.find((item) => item.type === 'object' && item.name === 'Ipc')
+  // console.log({ interesting })
+  // // const
+  // const edges = graph[interesting.id]
+  // const firstEdge = edges[0]
+  // // console.log({ firstEdge })
+  // const other = parsedNodes[firstEdge]
+  // // console.log({ other, len: parsedNodes.length })
+  // const items = edges.map((edge) => parsedNodes[edge])
+  //  graph[interesting.id]
+  console.log({ items })
   return {
     interesting,
     // graph,
