503b77946e339bb0af5a581fff52dbe3704cf4a8
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 22:34:05 +0200

    create chart

--- packages/charts/src/parts/Charts/Charts.js
+++ packages/charts/src/parts/Charts/Charts.js
@@ -1,5 +1,6 @@
 export * as CanvasCount from '../CreateCanvasCountChart/CreateCanvasCountChart.js'
 export * as DetachedDomNodeCount from '../CreateDetachedDomNodeCountChart/CreateDetachedDomNodeCountChart.js'
 export * as FunctionCount from '../CreateFunctionCountChart/CreateFunctionCountChart.js'
+export * as MutationObserverCount from '../CreateMutationObserverCountChart/CreateMutationObserverCountChart.js'
 export * as ObjectCount from '../CreateObjectCountChart/CreateObjectCountChart.js'
 export * as PromiseCount from '../CreatePromiseCountChart/CreatePromiseCountChart.js'
