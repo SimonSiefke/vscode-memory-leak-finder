6ece7bb1687c989bd12f444ed8e690f19de7f92d
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 20:15:05 +0200

    improve parsing

--- packages/memory-leak-finder/src/parts/HeapSnapshotFunctions/HeapSnapshotFunctions.js
+++ packages/memory-leak-finder/src/parts/HeapSnapshotFunctions/HeapSnapshotFunctions.js
@@ -1,19 +1,18 @@
-import * as GetObjectShapeCountFromHeapSnapshot from '../GetObjectShapeCountFromHeapSnapshot/GetObjectShapeCountFromHeapSnapshot.js'
-import * as ParseHeapSnapshotStrings from '../ParseHeapSnapshotStrings/ParseHeapSnapshotStrings.js'
-import * as GetNamedArrayCountFromHeapSnapshot from '../GetNamedArrayCountFromHeapSnapshot/GetNamedArrayCountFromHeapSnapshot.js'
-
 // TODO move these functions to heapsnapshot worker,
 // process the whole heapsnapshot in worker
 // to reduce memory usage in memory leak worker
 
 export const getObjectShapeCountFromHeapSnapshot = async (value) => {
-  return GetObjectShapeCountFromHeapSnapshot.getObjectShapeCountFromHeapSnapshot(value)
+  // return GetObjectShapeCountFromHeapSnapshot.getObjectShapeCountFromHeapSnapshot(value)
+  return 0
 }
 
 export const parseHeapSnapshotStrings = async (value) => {
-  return ParseHeapSnapshotStrings.parseHeapSnapshotStrings(value)
+  // return ParseHeapSnapshotStrings.parseHeapSnapshotStrings(value)
+  return []
 }
 
 export const getNamedArrayCountFromHeapSnapshot = async (value) => {
-  return GetNamedArrayCountFromHeapSnapshot.getNamedArrayCountFromHeapSnapshot(value)
+  // return GetNamedArrayCountFromHeapSnapshot.getNamedArrayCountFromHeapSnapshot(value)
+  return {}
 }
