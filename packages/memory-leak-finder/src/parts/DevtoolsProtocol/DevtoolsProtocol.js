54d7ebdc0740779158b01a705e086f61ca0f765b
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 15:53:36 +0200

    add finder

--- packages/memory-leak-finder/src/parts/DevtoolsProtocol/DevtoolsProtocol.js
+++ packages/memory-leak-finder/src/parts/DevtoolsProtocol/DevtoolsProtocol.js
@@ -1,2 +1,7 @@
-// @ts-ignore
-export * from '../../../../devtools-protocol/src/index.js'
+const url = new URL('../../../../devtools-protocol/src/index.js', import.meta.url).toString()
+const module = await import(url)
+
+const { DevtoolsProtocolHeapProfiler, DevtoolsProtocolPage, DevtoolsProtocolRuntime, DevtoolsProtocolTarget, DevtoolsProtocolMemory } =
+  module
+
+export { DevtoolsProtocolHeapProfiler, DevtoolsProtocolPage, DevtoolsProtocolRuntime, DevtoolsProtocolTarget, DevtoolsProtocolMemory }
