c4664ab9f2d1077b19cbf6344fd974dc7b63c5cf
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 16:37:44 +0200

    feature: add more cli tests

--- packages/cli/test/HandleStdinDataWaitingMode.test.js
+++ packages/cli/test/HandleStdinDataWaitingMode.test.js
@@ -1,6 +1,9 @@
 import { jest } from '@jest/globals'
 import * as AnsiKeys from '../src/parts/AnsiKeys/AnsiKeys.js'
+import * as AnsiEscapes from '../src/parts/AnsiEscapes/AnsiEscapes.js'
+import * as CliKeys from '../src/parts/CliKeys/CliKeys.js'
 import * as ModeType from '../src/parts/ModeType/ModeType.js'
+import * as PatternUsage from '../src/parts/PatternUsage/PatternUsage.js'
 
 beforeEach(() => {
   jest.resetModules()
@@ -70,4 +73,72 @@ test('handleStdinDataWaitingMode - toggle headless mode', () => {
     mode: ModeType.Running,
   })
   expect(Stdout.write).not.toHaveBeenCalled()
+})
+
+test('handleStdinDataWaitingMode - other key', () => {
+  const state = {
+    value: 'abc',
+  }
+  const key = 'd'
+  const newState = HandleStdinDataWaitingMode.handleStdinDataWaitingMode(state, key)
+  expect(newState).toBe(state)
+  expect(Stdout.write).not.toHaveBeenCalled()
+})
+
+test('handleStdinDataWaitingMode - ctrl + backspace', () => {
+  const state = {
+    value: 'abc',
+  }
+  const key = AnsiKeys.ControlBackspace
+  const newState = HandleStdinDataWaitingMode.handleStdinDataWaitingMode(state, key)
+  expect(newState.value).toBe('')
+  expect(Stdout.write).toHaveBeenCalledTimes(1)
+  expect(Stdout.write).toHaveBeenCalledWith(AnsiEscapes.eraseLine + AnsiEscapes.cursorLeft)
+})
+
+test('handleStdinDataWaitingMode - backspace', () => {
+  const state = {
+    value: 'abc',
+  }
+  const key = AnsiKeys.Backspace
+  const newState = HandleStdinDataWaitingMode.handleStdinDataWaitingMode(state, key)
+  expect(newState.value).toBe('ab')
+  expect(Stdout.write).not.toHaveBeenCalled()
+})
+
+test('handleStdinDataWaitingMode - arrow left', () => {
+  const state = {
+    value: 'abc',
+  }
+  const key = AnsiKeys.ArrowLeft
+  const newState = HandleStdinDataWaitingMode.handleStdinDataWaitingMode(state, key)
+  expect(newState).toBe(state)
+  expect(Stdout.write).not.toHaveBeenCalled()
+})
+
+test('handleStdinDataWaitingMode - run all tests', () => {
+  const state = {
+    value: 'abc',
+  }
+  const key = CliKeys.All
+  const newState = HandleStdinDataWaitingMode.handleStdinDataWaitingMode(state, key)
+  expect(newState).toEqual({
+    value: '',
+    mode: ModeType.Running,
+  })
+  expect(Stdout.write).not.toHaveBeenCalled()
+})
+
+test('handleStdinDataWaitingMode - filter mode', () => {
+  const state = {
+    value: 'abc',
+  }
+  const key = CliKeys.FilterMode
+  const newState = HandleStdinDataWaitingMode.handleStdinDataWaitingMode(state, key)
+  expect(newState).toEqual({
+    value: '',
+    mode: ModeType.FilterWaiting,
+  })
+  expect(Stdout.write).toHaveBeenCalledTimes(1)
+  expect(Stdout.write).toHaveBeenCalledWith(AnsiEscapes.clear + PatternUsage.print())
 })
