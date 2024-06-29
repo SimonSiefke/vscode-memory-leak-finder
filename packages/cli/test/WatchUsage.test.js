ea64c32304803610895506690baab553ce7871d3
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 16:14:19 +0200

    feature: add coverage for cli

--- packages/cli/test/WatchUsage.test.js
+++ packages/cli/test/WatchUsage.test.js
@@ -1,3 +1,4 @@
+import { expect, test } from '@jest/globals'
 import * as WatchUsage from '../src/parts/WatchUsage/WatchUsage.js'
 
 test('print', () => {
@@ -9,6 +10,6 @@ test('print', () => {
       '\u001B[2m › Press \u001B[22mp\u001B[2m to filter tests by a filename regex pattern.\u001B[22m\n' +
       '\u001B[2m › Press \u001B[22mh\u001B[2m to toggle headless mode.\u001B[22m\n' +
       '\u001B[2m › Press \u001B[22mq\u001B[2m to quit watch mode.\u001B[22m\n' +
-      '\u001B[2m › Press \u001B[22mEnter\u001B[2m to trigger a test run.\u001B[22m\n'
+      '\u001B[2m › Press \u001B[22mEnter\u001B[2m to trigger a test run.\u001B[22m\n',
   )
 })
