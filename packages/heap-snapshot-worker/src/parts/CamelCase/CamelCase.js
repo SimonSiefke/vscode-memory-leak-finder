d6fd18f6b8363aa693bb1fc7b70bcd7b596b7387
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 17:31:46 +0200

    simplify fn

--- packages/heap-snapshot-worker/src/parts/CamelCase/CamelCase.js
+++ packages/heap-snapshot-worker/src/parts/CamelCase/CamelCase.js
@@ -1,8 +1,10 @@
 const RE_DASH = /(-|_)([a-z])/g
 
+const replacer = (g) => {
+  return g[1].toUpperCase()
+}
+
 export const camelCase = (value) => {
-  const camelCased = value.replace(RE_DASH, (g) => {
-    return g[1].toUpperCase()
-  })
+  const camelCased = value.replace(RE_DASH, replacer)
   return camelCased
 }
