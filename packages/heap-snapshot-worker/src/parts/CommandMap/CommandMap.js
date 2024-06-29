c763bfb14df5f0164843548ee4a55062feed564e
Simon Siefke <simon.siefke@gmail.com>
2024-06-29 17:30:17 +0200

    add fn

--- packages/heap-snapshot-worker/src/parts/CommandMap/CommandMap.js
+++ packages/heap-snapshot-worker/src/parts/CommandMap/CommandMap.js
@@ -1,1 +1,7 @@
-export const commandMap = {}
+import * as ParseHeapSnapshot from '../ParseHeapSnapshot/ParseHeapSnapshot.js'
+import * as ParseHeapSnapshotStrings from '../ParseHeapSnapshotStrings/ParseHeapSnapshotStrings.js'
+
+export const commandMap = {
+  'HeapSnapshot.parseStrings': ParseHeapSnapshotStrings.parseHeapSnapshotStrings,
+  'HeapSnapshot.parseHeapSnapshot': ParseHeapSnapshot.parseHeapSnapshot,
+}
