262c3819a45d05642aeaf3f2e76689de7c1b4cee
Simon Siefke <simon.siefke@gmail.com>
2024-06-30 13:27:18 +0200

    fix id

--- packages/memory-leak-finder/src/parts/MeasureMinimapCount/MeasureMinimapCount.js
+++ packages/memory-leak-finder/src/parts/MeasureMinimapCount/MeasureMinimapCount.js
@@ -4,7 +4,7 @@ import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
 import * as MeasureId from '../MeasureId/MeasureId.js'
 import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
 
-export const id = MeasureId.CodeEditorWidgetCount
+export const id = MeasureId.MinimapCount
 
 export const create = (session) => {
   const objectGroup = ObjectGroupId.create()
