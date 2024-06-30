223563a125ce90acf2e665309ab13bad3c0d83b2
Simon Siefke <simon.siefke@gmail.com>
2024-06-30 10:34:06 +0200

    sort items

--- packages/heap-snapshot-worker/src/parts/SortCountMap/SortCountMap.js
+++ packages/heap-snapshot-worker/src/parts/SortCountMap/SortCountMap.js
@@ -1,12 +1,12 @@
 import * as Arrays from '../Arrays/Arrays.js'
 import * as Assert from '../Assert/Assert.js'
 
-const compareFunction = (a, b) => {
-  return b.count - a.count || a.name.localeCompare(b.name)
+const compareItem = (a, b) => {
+  return b.count - a.count
 }
 
 export const sortCountMap = (items) => {
   Assert.array(items)
-  const sorted = Arrays.toSorted(items, compareFunction)
+  const sorted = Arrays.toSorted(items, compareItem)
   return sorted
 }
