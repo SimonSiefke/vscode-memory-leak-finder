ed8c60a4d7099d8c8d7001d5e40ed50929767239
Simon Siefke <simon.siefke@gmail.com>
2024-06-30 12:47:01 +0200

    id

--- packages/memory-leak-finder/src/parts/MeasureCodeEditorWidgetCount/MeasureCodeEditorWidgetCount.js
+++ packages/memory-leak-finder/src/parts/MeasureCodeEditorWidgetCount/MeasureCodeEditorWidgetCount.js
@@ -4,7 +4,7 @@ import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
 import * as MeasureId from '../MeasureId/MeasureId.js'
 import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
 
-export const id = MeasureId.WidgetCount
+export const id = MeasureId.CodeEditorWidgetCount
 
 export const create = (session) => {
   const objectGroup = ObjectGroupId.create()
