c34dd588a10b3658ce1f9e1484dd0973244d16a9
Simon Siefke <simon.siefke@gmail.com>
2024-06-30 12:40:22 +0200

    measure

--- packages/memory-leak-finder/src/parts/GetWidgets/GetWidgets.js
+++ packages/memory-leak-finder/src/parts/GetWidgets/GetWidgets.js
@@ -1,5 +1,4 @@
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
-import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+import * as GetConstructorInstances from '../GetConstructorInstances/GetConstructorInstances.js'
 
 /**
  *
@@ -7,38 +6,5 @@ import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression
  * @returns {Promise<any>}
  */
 export const getWidgets = async (session, objectGroup) => {
-  const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
-    expression: PrototypeExpression.Object,
-    returnByValue: false,
-    objectGroup,
-  })
-  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
-    prototypeObjectId: prototypeDescriptor.objectId,
-    objectGroup,
-  })
-  const fnResult1 = await DevtoolsProtocolRuntime.callFunctionOn(session, {
-    functionDeclaration: `function(){
-  const objects = this
-
-  const isWidgetConstructor = (object) => {
-    return object && typeof object === 'function' && object.name === 'Widget'
-  }
-
-  const widgetConstructor = objects.find(isWidgetConstructor)
-  if(!widgetConstructor){
-    throw new Error('no widget constructor found')
-  }
-
-  const isWidget = object => {
-    return object && object instanceof widgetConstructor
-  }
-
-  const instances = objects.filter(isWidget)
-  return instances
-}`,
-    objectId: objects.objects.objectId,
-    returnByValue: false,
-    objectGroup,
-  })
-  return fnResult1
+  return GetConstructorInstances.getConstructorInstances(session, objectGroup, 'Widget')
 }
