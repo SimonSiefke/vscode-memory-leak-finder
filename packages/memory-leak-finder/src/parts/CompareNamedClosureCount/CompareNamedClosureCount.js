69fa88260171b52bbae4302bf226d84ca46e7d8b
Simon Siefke <simon.siefke@gmail.com>
2024-07-01 20:35:36 +0200

    feature: measure growing closures (#1165)

--- packages/memory-leak-finder/src/parts/CompareNamedClosureCount/CompareNamedClosureCount.js
+++ packages/memory-leak-finder/src/parts/CompareNamedClosureCount/CompareNamedClosureCount.js
@@ -15,18 +15,21 @@ export const compareNamedClosureCount = (before, after) => {
   const beforeMap = Object.create(null)
   for (const item of before) {
     beforeMap[item.name] ||= 0
-    beforeMap[item.name] += item.contextNodeCount
+    beforeMap[item.name] += item.contextNodeCount + 1
   }
   const afterMap = Object.create(null)
   for (const item of after) {
     afterMap[item.name] ||= 0
-    afterMap[item.name] += item.contextNodeCount
+    afterMap[item.name] += item.contextNodeCount + 1
   }
   const result = []
   for (const [key, value] of Object.entries(afterMap)) {
     const beforeCount = beforeMap[key] || 0
     const afterCount = value
     const delta = afterCount - beforeCount
+    if (!delta || delta < 0) {
+      continue
+    }
     result.push({
       name: key,
       contextNodeCount: afterCount,
