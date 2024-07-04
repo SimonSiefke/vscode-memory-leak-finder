d443298f164f5117aa1fe5c60948accf34e7d1a9
Simon Siefke <simon.siefke@gmail.com>
2024-07-04 22:07:24 +0200

    get some data

--- packages/heap-snapshot-worker/src/parts/GetNamedEmitterCountFromHeapSnapshot/GetNamedEmitterCountFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetNamedEmitterCountFromHeapSnapshot/GetNamedEmitterCountFromHeapSnapshot.js
@@ -37,9 +37,48 @@ const getArrayNamesWithCount = (countMap) => {
   return arrayNamesWithCount
 }
 
+const isEventEmitterConstructorCandidate = (node) => {
+  return node.type === 'object' && node.name === 'Emitter'
+}
+
+const getEventEmitterNode = (nodes, graph) => {
+  const candidates = nodes.filter(isEventEmitterConstructorCandidate)
+  const allEdges = Object.values(graph).flat(1)
+  const explorerRootEdge = allEdges.find((edge) => edge.name === 'explorerRootErrorEmitter')
+  for (const candidate of candidates) {
+    const index = nodes.indexOf(candidate)
+    if (explorerRootEdge.index === index) {
+      return candidate
+    }
+  }
+  return undefined
+}
+
 export const getNamedEmitterCountFromHeapSnapshot = async (heapsnapshot) => {
   Assert.object(heapsnapshot)
   const { parsedNodes, graph } = ParseHeapSnapshot.parseHeapSnapshot(heapsnapshot)
-  console.log({ parsedNodes })
-  return []
+  const emitterNode = getEventEmitterNode(parsedNodes, graph)
+  console.log({ emitterNode })
+  if (!emitterNode) {
+    throw new Error('no emitter found')
+  }
+  const emitterNodeIndex = parsedNodes.indexOf(emitterNode)
+  const childNodes = parsedNodes.filter((node) => {
+    const edges = graph[node.id]
+    return edges.some((edge) => edge.index === emitterNodeIndex)
+  })
+  console.log({ childNodes })
+  const randomNode = parsedNodes.find((node) => node.id === 276279)
+  console.log({ randomNode })
+  const randomEdges = graph[randomNode.id]
+  console.log({ randomEdges })
+  const randomEdgeProto = randomEdges.find((edge) => edge.name === '__proto__')
+  console.log({ randomEdgeProto })
+  const protoNode = parsedNodes[randomEdgeProto.index]
+  console.log({ protoNode })
+  // const firstEdge = edges[0]
+  // const node = parsedNodes[39]
+  // const mapEdges = graph[node.id]
+  // console.log({ mapEdges })
+  return parsedNodes
 }
