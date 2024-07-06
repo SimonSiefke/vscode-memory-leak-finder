babea39500ebbef8e06454c7fa7ed7b445371af5
Simon Siefke <simon.siefke@gmail.com>
2024-07-06 13:07:57 +0200

    add test

--- packages/heap-snapshot-worker/src/parts/GetConstructorScope/GetConstructorScope.js
+++ packages/heap-snapshot-worker/src/parts/GetConstructorScope/GetConstructorScope.js
@@ -1,19 +1,7 @@
-import * as IsIgnoredConstructorScopeEdge from '../IsIgnoredConstructorScopeEdge/IsIgnoredConstructorScopeEdge.js'
-
-export const getConstructorScope = (parsedNodes, graph, node) => {
+export const getConstructorScope = (parsedNodes, constructorScopeMap, node) => {
+  // TODO avoid indexOf
   const nodeIndex = parsedNodes.indexOf(node)
-  const nodes = []
-  for (const parsedNode of parsedNodes) {
-    const edges = graph[parsedNode.id]
-    for (const edge of edges) {
-      if (edge.index === nodeIndex) {
-        if (IsIgnoredConstructorScopeEdge.isIgnoredConstructorScopeEdge(edge)) {
-          continue
-        }
-        nodes.push(edge)
-        // return parsedNode
-      }
-    }
-  }
-  return nodes
+  const constructorScopeIndex = constructorScopeMap[nodeIndex]
+  const constructorScope = parsedNodes[constructorScopeIndex]
+  return constructorScope
 }
