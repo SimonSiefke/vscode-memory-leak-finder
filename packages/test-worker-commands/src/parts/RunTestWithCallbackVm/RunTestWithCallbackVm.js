18c845755b1fd0948036db76f3a4452f353a985a
Simon Siefke <simon.siefke@gmail.com>
2024-07-09 23:44:48 +0200

    feature: use real vm

--- packages/test-worker-commands/src/parts/RunTestWithCallbackVm/RunTestWithCallbackVm.js
+++ packages/test-worker-commands/src/parts/RunTestWithCallbackVm/RunTestWithCallbackVm.js
@@ -1,15 +1,18 @@
 import { readFile } from 'node:fs/promises'
 import * as vm from 'node:vm'
+import * as TestStage from '../TestStage/TestStage.js'
 
 export const runTest = async (pageObject, file, forceRun) => {
-  // TODO use vm
-  console.log('use vm')
   const content = await readFile(file, 'utf8')
   const module = new vm.SourceTextModule(content)
   // @ts-ignore
   await module.link(() => {})
   await module.evaluate()
-  // const contextifiedObject = vm.createContext({ secret: 42 })
-  // await TestStage.run(module, pageObject)
+
+  // @ts-ignore
+  if (module.namespace.skip) {
+    return true
+  }
+  await TestStage.run(module.namespace, pageObject)
   return false
 }
