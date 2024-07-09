95d0da6e7d2c599e35f0456df4ac906581c781f6
Simon Siefke <simon.siefke@gmail.com>
2024-07-10 00:22:45 +0200

    feature: add stricter types for e2e tests, disallowing imports and no… (#1186)

--- packages/e2e/src/status-bar.item.js
+++ packages/e2e/src/status-bar.item.js
@@ -1,8 +1,11 @@
+// @ts-ignore
 import path, { dirname, join } from 'node:path'
+// @ts-ignore
 import { fileURLToPath } from 'node:url'
 
 export const skip = true
 
+// @ts-ignore
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const root = path.join(__dirname, '../../../')
