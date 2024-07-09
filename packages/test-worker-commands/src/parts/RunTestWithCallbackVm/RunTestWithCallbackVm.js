7b7ef870d7bc5966a82061914e082c7065954010
Simon Siefke <simon.siefke@gmail.com>
2024-07-09 23:24:50 +0200

    tsc

--- packages/test-worker-commands/src/parts/RunTestWithCallbackVm/RunTestWithCallbackVm.js
+++ packages/test-worker-commands/src/parts/RunTestWithCallbackVm/RunTestWithCallbackVm.js
@@ -1,6 +1,4 @@
 import { readFile } from 'node:fs/promises'
-import * as ImportTest from '../ImportTest/ImportTest.js'
-import * as TestStage from '../TestStage/TestStage.js'
 import * as vm from 'node:vm'
 
 export const runTest = async (pageObject, file, forceRun) => {
@@ -8,6 +6,7 @@ export const runTest = async (pageObject, file, forceRun) => {
   console.log('use vm')
   const content = await readFile(file, 'utf8')
   const module = new vm.SourceTextModule(content)
+  // @ts-ignore
   await module.link(() => {})
   await module.evaluate()
   // const contextifiedObject = vm.createContext({ secret: 42 })
