84522f4207e0a31bc84883958601d89f2a765845
Simon Siefke <simon.siefke@gmail.com>
2024-07-10 22:48:17 +0200

    feature: include scriptHandler for detached dom nodes stack trace mea… (#1191)

---
+++ packages/memory-leak-finder/src/parts/GetEventListenersQuery/GetEventListenersQuery.js
@@ -0,0 +1,41 @@
+import * as FormatUrl from '../FormatUrl/FormatUrl.js'
+
+const RE_URL = /(.*)\((.*):(\d+):(\d+)\)/s
+
+const parseUrl = (stackLine) => {
+  const urlMatch = stackLine.match(RE_URL)
+  if (!urlMatch) {
+    return {
+      url: '',
+      line: 0,
+      column: 0,
+    }
+  }
+  return {
+    prefix: urlMatch[1],
+    url: urlMatch[2],
+    line: Number.parseInt(urlMatch[3]),
+    column: Number.parseInt(urlMatch[4]),
+  }
+}
+
+export const getEventListenerQuery = (stackLines, scriptMap) => {
+  const reverseScriptMap = Object.create(null)
+  for (const value of Object.values(scriptMap)) {
+    reverseScriptMap[value.url] = value.sourceMapUrl
+  }
+  const eventListeners = stackLines.map((stackLine, index) => {
+    const { prefix, url, line, column } = parseUrl(stackLine)
+    const sourceMapUrl = reverseScriptMap[url]
+    const formattedUrl = FormatUrl.formatUrl(url, line - 1, column)
+    const newStackLine = `${prefix}(${formattedUrl})`
+    return {
+      url,
+      originalIndex: index,
+      uuid: index,
+      stack: [newStackLine],
+      sourceMaps: [sourceMapUrl],
+    }
+  })
+  return eventListeners
+}
--- packages/memory-leak-finder/src/parts/PrettifyConstructorStackTracesWithSourceMap/PrettifyConstructorStackTracesWithSourceMap.js
+++ packages/memory-leak-finder/src/parts/PrettifyConstructorStackTracesWithSourceMap/PrettifyConstructorStackTracesWithSourceMap.js
@@ -1,54 +1,6 @@
 import * as Arrays from '../Arrays/Arrays.js'
-import * as FormatUrl from '../FormatUrl/FormatUrl.js'
 import * as GetEventListenerOriginalSourcesCached from '../GetEventListenerOriginalSourcesCached/GetEventListenerOriginalSourcesCached.js'
-
-const RE_URL = /(.*)\((.*):(\d+):(\d+)\)/s
-
-const parseUrl = (stackLine) => {
-  const urlMatch = stackLine.match(RE_URL)
-  if (!urlMatch) {
-    return {
-      url: '',
-      line: 0,
-      column: 0,
-    }
-  }
-  return {
-    prefix: urlMatch[1],
-    url: urlMatch[2],
-    line: Number.parseInt(urlMatch[3]),
-    column: Number.parseInt(urlMatch[4]),
-  }
-}
-
-const compareInstance = (a, b) => {
-  return b.originalIndex - a.originalIndex || b.uuid - a.uuid
-}
-
-const getLine = (instance) => {
-  return instance.originalStack[0]
-}
-
-const getEventListenerQuery = (stackLines, scriptMap, index) => {
-  const reverseScriptMap = Object.create(null)
-  for (const value of Object.values(scriptMap)) {
-    reverseScriptMap[value.url] = value.sourceMapUrl
-  }
-  const eventListeners = stackLines.map((stackLine, index) => {
-    const { prefix, url, line, column } = parseUrl(stackLine)
-    const sourceMapUrl = reverseScriptMap[url]
-    const formattedUrl = FormatUrl.formatUrl(url, line - 1, column)
-    const newStackLine = `${prefix}(${formattedUrl})`
-    return {
-      url,
-      originalIndex: index,
-      uuid: index,
-      stack: [newStackLine],
-      sourceMaps: [sourceMapUrl],
-    }
-  })
-  return eventListeners
-}
+import * as GetEventListenersQuery from '../GetEventListenersQuery/GetEventListenersQuery.js'
 
 const sortOriginal = (cleanInstances) => {
   const cleaned = []
@@ -71,7 +23,7 @@ export const prettifyConstructorStackTracesWithSourceMap = async (constructorSta
   const fullQuery = []
   for (let i = 0; i < constructorStackTraces.length; i++) {
     const stackTrace = constructorStackTraces[i]
-    const eventListeners = getEventListenerQuery(stackTrace, scriptMap, i)
+    const eventListeners = GetEventListenersQuery.getEventListenerQuery(stackTrace, scriptMap)
     fullQuery.push(...eventListeners)
   }
   const cleanPrettyInstances = await GetEventListenerOriginalSourcesCached.getEventListenerOriginalSourcesCached(fullQuery, false)
