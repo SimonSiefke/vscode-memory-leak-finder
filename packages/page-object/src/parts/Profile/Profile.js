4eea903b4fe29ce6bbb96a01624cffcce9500669
Simon Siefke <simon.siefke@gmail.com>
2024-07-10 21:21:03 +0200

    improve test

--- packages/page-object/src/parts/Profile/Profile.js
+++ packages/page-object/src/parts/Profile/Profile.js
@@ -1,4 +1,5 @@
 import * as QuickPick from '../QuickPick/QuickPick.js'
+import * as WellKnownCommands from '../WellKnownCommands/WellKnownCommands.js'
 
 export const create = ({ page, expect, VError }) => {
   return {
@@ -6,13 +7,45 @@ export const create = ({ page, expect, VError }) => {
       try {
         await page.waitForIdle()
         const quickPick = QuickPick.create({ page, expect, VError })
+        await quickPick.executeCommand(WellKnownCommands.ProfilesCreateProfile, {
+          stayVisible: true,
+        })
+        const input = page.locator('[placeholder="Profile name"]')
+        await expect(input).toBeVisible()
+        await expect(input).toBeFocused()
+        await input.type(info.name)
         await page.waitForIdle()
+        const createButton = page.locator('.monaco-button', {
+          hasText: 'Create',
+        })
+        await createButton.click()
+        const profileBadge = page.locator('.profile-badge', {
+          hasText: 'TE',
+        })
+        await expect(profileBadge).toBeVisible()
       } catch (error) {
         throw new VError(error, `Failed to create profile`)
       }
     },
-    async remove(id) {
-      throw new Error('not implemented')
+    async remove(info) {
+      await page.waitForIdle()
+      const quickPick = QuickPick.create({ page, expect, VError })
+      await quickPick.executeCommand(WellKnownCommands.ProfilesDeleteProfile, {
+        stayVisible: true,
+      })
+      const input = page.locator('[placeholder="Select Profiles to Delete"]')
+      await input.type(info.name)
+      const row = page.locator('.monaco-list-row[aria-label="test, Current"]')
+      await row.click()
+      await expect(row).toHaveAttribute('aria-checked', 'true')
+      const okButton = page.locator('.monaco-button', {
+        hasText: 'OK',
+      })
+      await okButton.click()
+      const profileBadge = page.locator('.profile-badge', {
+        hasText: 'TE',
+      })
+      await expect(profileBadge).toHaveCount(0)
     },
   }
 }
