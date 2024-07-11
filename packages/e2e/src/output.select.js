c6e967201ba4f904c1deb3c9de68ea203abd8396
Simon Siefke <simon.siefke@gmail.com>
2024-07-11 22:37:17 +0200

    feature: add output select test (#1197)

---
+++ packages/e2e/src/output.select.js
@@ -0,0 +1,13 @@
+export const skip = true
+
+export const setup = async ({ Editor, Output, Panel }) => {
+  await Editor.closeAll()
+  await Panel.hide()
+  await Output.show()
+  await Output.select('Main')
+}
+
+export const run = async ({ Output }) => {
+  await Output.select('Window')
+  await Output.select('Main')
+}
--- packages/page-object/src/parts/Output/Output.js
+++ packages/page-object/src/parts/Output/Output.js
@@ -11,6 +11,11 @@ export const create = ({ expect, page, VError }) => {
         const quickPick = QuickPick.create({ expect, page, VError })
         await quickPick.executeCommand(WellKnownCommands.OutputFocusOnOutputView)
         await expect(outputView).toBeVisible()
+        const paneBody = page.locator('.pane-body.output-view')
+        await expect(paneBody).toBeVisible()
+        const inputArea = paneBody.locator('.inputarea')
+        await expect(inputArea).toBeFocused()
+        await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to show output`)
       }
@@ -24,6 +29,29 @@ export const create = ({ expect, page, VError }) => {
         await expect(outputView).toBeHidden()
       } catch (error) {
         throw new VError(error, `Failed to hide output`)
+      }
+    },
+    async select(channelName) {
+      try {
+        const outputView = page.locator('.pane-body.output-view')
+        await expect(outputView).toBeVisible()
+        const select = page.locator('[aria-label="Output actions"] .monaco-select-box')
+        for (let i = 0; i < 50; i++) {
+          await page.waitForIdle()
+        }
+        await select.click()
+        const monacoList = page.locator('.select-box-dropdown-list-container .monaco-list')
+        await expect(monacoList).toBeVisible()
+        await expect(monacoList).toBeFocused()
+        const option = monacoList.locator(`[role="option"][aria-label="${channelName}"]`)
+        await expect(option).toBeVisible()
+        await option.click()
+        await expect(monacoList).toBeHidden()
+        await expect(select).toBeFocused()
+        await expect(select).toHaveValue(channelName)
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to select output channel ${channelName}`)
       }
     },
   }
