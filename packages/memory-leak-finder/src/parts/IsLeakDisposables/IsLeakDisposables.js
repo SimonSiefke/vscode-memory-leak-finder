430e9651bf7e183c4e05d21b99492d1fef8fe61e
Simon Siefke <simon.siefke@gmail.com>
2024-07-11 22:02:19 +0200

    feature: add delta property to disposablesWithLocation (#1196)

--- packages/memory-leak-finder/src/parts/IsLeakDisposables/IsLeakDisposables.js
+++ packages/memory-leak-finder/src/parts/IsLeakDisposables/IsLeakDisposables.js
@@ -1,5 +1,5 @@
 const hasGreaterCount = (item) => {
-  return item.count > item.oldCount
+  return item.count > item.oldCount || item.delta > 0
 }
 
 export const isLeakDisposables = (newItems) => {
