c90a557f1858ece0e9469c525c688b15b18a7637
Simon Siefke <simon.siefke@gmail.com>
2024-07-11 22:55:06 +0200

    feature: add settings key value test

---
+++ packages/e2e/src/settings.edit-key-value.js
@@ -0,0 +1,19 @@
+export const setup = async ({ Editor, SettingsEditor }) => {
+  await Editor.closeAll()
+  await SettingsEditor.open()
+  await SettingsEditor.search({
+    value: 'Editor: Auto Closing Comments',
+    resultCount: 2,
+  })
+}
+
+export const run = async ({ SettingsEditor }) => {
+  await SettingsEditor.select({
+    name: 'editor.autoClosingComments',
+    value: 'never',
+  })
+  await SettingsEditor.select({
+    name: 'editor.autoClosingComments',
+    value: 'languageDefined',
+  })
+}
