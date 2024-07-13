720084e2000b6448595a2a4fc75fc2b549ada345
Simon Siefke <simon.siefke@gmail.com>
2024-07-13 09:11:49 +0200

    feature: improve profile export test

--- packages/e2e/src/profile-export.js
+++ packages/e2e/src/profile-export.js
@@ -1,15 +1,22 @@
-export const skip = true
+export const skip = false
 
-export const setup = async ({ Editor, Electron }) => {
+export const setup = async ({ Editor, Electron, Workspace }) => {
+  await Workspace.setFiles([])
+  const filePath = Workspace.getWorkspaceFilePath('exported-profile.code-profile')
   await Electron.mockSaveDialog({
-    filePath: '/tmp/exported-profile.code-profile',
+    filePath,
     canceled: false,
+  })
+  await Electron.mockDialog({
+    response: 0,
   })
   await Editor.closeAll()
 }
 
-export const run = async ({ Profile }) => {
+export const run = async ({ Profile, Explorer, Workspace }) => {
   await Profile.export({
     name: 'test',
   })
+  await Explorer.shouldHaveItem('exported-profile.code-profile')
+  await Workspace.setFiles([])
 }
