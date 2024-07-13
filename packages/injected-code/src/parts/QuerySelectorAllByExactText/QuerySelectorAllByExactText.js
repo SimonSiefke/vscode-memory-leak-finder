93aced76ac8b3bc6639cf661b2ac5ea8f9110a78
Simon Siefke <simon.siefke@gmail.com>
2024-07-13 12:58:11 +0200

    add fn

---
+++ packages/injected-code/src/parts/QuerySelectorAllByExactText/QuerySelectorAllByExactText.js
@@ -0,0 +1,16 @@
+import * as Assert from '../Assert/Assert.js'
+import * as ParseSpecialSelectorBody from '../ParseSpecialSelectorBody/ParseSpecialSelectorBody.js'
+import * as SpecialSelectorPrefix from '../SpecialSelectorPrefix/SpecialSelectorPrefix.js'
+
+export const querySelectorAll = (roots, body) => {
+  Assert.array(roots)
+  Assert.string(body)
+  const text = ParseSpecialSelectorBody.parseString(body, SpecialSelectorPrefix.HasText)
+  const matches = []
+  for (const element of roots) {
+    if (element.textContent === text) {
+      matches.push(element)
+    }
+  }
+  return matches
+}
