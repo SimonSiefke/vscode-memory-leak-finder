a5193d0c49d1470992d1b22d0a0bd3c89fe431d7
Simon Siefke <simon.siefke@gmail.com>
2024-07-13 23:23:06 +0200

    feature: adjust variable name (#1212)

--- packages/memory-leak-finder/src/parts/GetNamedClosureCount/GetNamedClosureCount.js
+++ packages/memory-leak-finder/src/parts/GetNamedClosureCount/GetNamedClosureCount.js
@@ -9,10 +9,10 @@ import * as Root from '../Root/Root.js'
  * @returns {Promise<any>}
  */
 export const getNamedClosureCount = async (session, objectGroup) => {
-  const outFile = join(Root.root, '.vscode-heapsnapshots', `array-count.json`)
+  const outFile = join(Root.root, '.vscode-heapsnapshots', `closure-count.json`)
   await HeapSnapshot.takeHeapSnapshot(session, outFile)
   const content = await readFile(outFile, 'utf8')
   const value = JSON.parse(content)
-  const arrayCountMap = await HeapSnapshotFunctions.getNamedClosureCountFromHeapSnapshot(value)
-  return arrayCountMap
+  const counts = await HeapSnapshotFunctions.getNamedClosureCountFromHeapSnapshot(value)
+  return counts
 }
