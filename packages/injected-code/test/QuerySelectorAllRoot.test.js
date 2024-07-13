13c474cc326aed485378acd91a1c52e1e957ef1d
Simon Siefke <simon.siefke@gmail.com>
2024-07-13 12:55:32 +0200

    Discard changes to packages/injected-code/test/QuerySelectorAllRoot.test.js

--- packages/injected-code/test/QuerySelectorAllRoot.test.js
+++ packages/injected-code/test/QuerySelectorAllRoot.test.js
@@ -4,14 +4,6 @@
 import * as QuerySelectorAllRoot from '../src/parts/QuerySelectorAllRoot/QuerySelectorAllRoot.js'
 import { test, expect } from '@jest/globals'
 
-test('querySelector - match', () => {
-  const root = document.createElement('div')
-  const element = document.createElement('h1')
-  root.append(element)
-  const selector = 'h1'
-  expect(QuerySelectorAllRoot.querySelector(root, selector)).toEqual(element)
-})
-
 test('querySelectorAll - html element - match', () => {
   const root = document.createElement('div')
   const element = document.createElement('h1')
@@ -85,7 +77,7 @@ test('querySelectorAll - text - match', () => {
   element.textContent = 'test'
   root.append(element)
   const selector = ':has-text("test")'
-  expect(QuerySelectorAllRoot.querySelectorAll(root, selector)).toEqual([root])
+  expect(QuerySelectorAllRoot.querySelectorAll(root, selector)).toEqual([element])
 })
 
 test('querySelectorAll - text - no match', () => {
@@ -107,7 +99,7 @@ test('querySelectorAll - element with text', () => {
   expect(QuerySelectorAllRoot.querySelectorAll(root, selector)).toEqual([element1])
 })
 
-test.skip('querySelectorAll - alternative text selector', () => {
+test('querySelectorAll - alternative text selector', () => {
   const root = document.createElement('div')
   const element1 = document.createElement('h1')
   element1.textContent = '1'
@@ -124,40 +116,5 @@ test('querySelectorAll - text with colon', () => {
   element.textContent = 'Preferences: Open Keyboard Shortcuts'
   root.append(element)
   const selector = ':has-text("Preferences: Open Keyboard Shortcuts")'
-  expect(QuerySelectorAllRoot.querySelectorAll(root, selector)).toEqual([root])
-})
-
-test('querySelectorAll - element with text - no exact match', () => {
-  const root = document.createElement('div')
-  const element = document.createElement('h1')
-  const child1 = document.createElement('span')
-  child1.textContent = 'abc'
-  const child2 = document.createElement('span')
-  child2.textContent = 'def'
-  element.append(child1, child2)
-  root.append(element)
-  const selector = 'h1:has-text("abc")'
-  expect(QuerySelectorAllRoot.querySelectorAll(root, selector)).toEqual([])
-})
-
-test('querySelectorAll - element with text - exact match', () => {
-  const root = document.createElement('div')
-  const element = document.createElement('h1')
-  element.textContent = 'abc'
-  root.append(element)
-  const selector = 'h1:has-text("abc")'
   expect(QuerySelectorAllRoot.querySelectorAll(root, selector)).toEqual([element])
-})
-
-test('querySelectorAll - element with text - child element match', () => {
-  const root = document.createElement('div')
-  const element = document.createElement('h1')
-  const child1 = document.createElement('span')
-  child1.textContent = 'abc'
-  const child2 = document.createElement('span')
-  child2.textContent = 'def'
-  element.append(child1, child2)
-  root.append(element)
-  const selector = 'h1 *:has-text("abc")'
-  expect(QuerySelectorAllRoot.querySelectorAll(root, selector)).toEqual([child1])
 })
