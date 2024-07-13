1e37ea99b54988e7aee7fa5a97249876ece9cc99
Simon Siefke <simon.siefke@gmail.com>
2024-07-13 09:01:09 +0200

    feature: add profile export test (#1201)

---
+++ packages/e2e/src/profile-export.js
@@ -0,0 +1,15 @@
+export const skip = true
+
+export const setup = async ({ Editor, Electron }) => {
+  await Electron.mockSaveDialog({
+    filePath: '/tmp/exported-profile.code-profile',
+    canceled: false,
+  })
+  await Editor.closeAll()
+}
+
+export const run = async ({ Profile }) => {
+  await Profile.export({
+    name: 'test',
+  })
+}
