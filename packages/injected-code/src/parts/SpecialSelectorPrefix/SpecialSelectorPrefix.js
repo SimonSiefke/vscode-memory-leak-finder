960fc97bf11ed439b3cc25a7d1cf47dfb93540e7
Simon Siefke <simon.siefke@gmail.com>
2024-07-13 12:59:31 +0200

    parse exact text selector

--- packages/injected-code/src/parts/SpecialSelectorPrefix/SpecialSelectorPrefix.js
+++ packages/injected-code/src/parts/SpecialSelectorPrefix/SpecialSelectorPrefix.js
@@ -1,4 +1,5 @@
 export const HasText = ':has-text('
+export const HasExactText = ':has-exact-text('
 export const Nth = ':nth('
 export const InternalEnterFrame = ':internal-enter-frame('
 export const EnterShadow = ':enter-shadow('
