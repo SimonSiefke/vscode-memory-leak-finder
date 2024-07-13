9db3f0d1c3f43ff782ca68c0dc0f63d6c66394da
Simon Siefke <simon.siefke@gmail.com>
2024-07-13 08:40:01 +0200

    feature: add option to mock electron save dialog

--- packages/page-object/src/parts/Electron/Electron.js
+++ packages/page-object/src/parts/Electron/Electron.js
@@ -16,5 +16,18 @@ export const create = ({ electronApp, VError }) => {
         throw new VError(error, `Failed to mock electron dialog`)
       }
     },
+    async mockSaveDialog(response) {
+      try {
+        const responseString = JSON.stringify(JSON.stringify(response))
+        await this.evaluate(`(() => {
+  const electron = globalThis._VSCODE_NODE_MODULES["electron"];
+  electron.dialog.showSaveDialog = () => {
+    return JSON.parse(${responseString})
+  }
+})()`)
+      } catch (error) {
+        throw new VError(error, `Failed to mock electron save dialog`)
+      }
+    },
   }
 }
