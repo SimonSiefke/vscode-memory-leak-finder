b0604fbe10d5ff35aafb74415edda1856c968ecf
Simon Siefke <simon.siefke@gmail.com>
2024-08-10 09:56:54 +0200

    feature: add string difference measure

---
+++ packages/memory-leak-finder/src/parts/MeasureStringDifference/MeasureStringDifference.js
@@ -0,0 +1,24 @@
+import * as CompareCount from '../CompareCount/CompareCount.js'
+import * as GetStringCount from '../GetStringCount/GetStringCount.js'
+import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+
+export const id = MeasureId.StringDifference
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
+}
+
+export const start = (session, objectGroup) => {
+  return GetStringCount.getStringCount(session, objectGroup)
+}
+
+export const stop = (session, objectGroup) => {
+  return GetStringCount.getStringCount(session, objectGroup)
+}
+
+export const compare = CompareCount.compareCount
+
+export const isLeak = IsLeakCount.isLeakCount
