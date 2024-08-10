502110d33c27ed71c3b4c4cdbeceaba7b4870d58
Simon Siefke <simon.siefke@gmail.com>
2024-08-10 10:01:08 +0200

    improve measure

--- packages/memory-leak-finder/src/parts/MeasureStringDifference/MeasureStringDifference.js
+++ packages/memory-leak-finder/src/parts/MeasureStringDifference/MeasureStringDifference.js
@@ -3,6 +3,7 @@ import * as GetStrings from '../GetStrings/GetStrings.js'
 import * as IsLeakCount from '../IsLeakCount/IsLeakCount.js'
 import * as MeasureId from '../MeasureId/MeasureId.js'
 import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+import * as CompareStrings from '../CompareStrings/CompareStrings.js'
 
 export const id = MeasureId.StringDifference
 
@@ -19,6 +20,8 @@ export const stop = (session, objectGroup) => {
   return GetStrings.getStrings(session, objectGroup)
 }
 
-export const compare = CompareCount.compareCount
+export const compare = CompareStrings.compareStrings
 
-export const isLeak = IsLeakCount.isLeakCount
+export const isLeak = (newStrings) => {
+  return newStrings.length > 0
+}
