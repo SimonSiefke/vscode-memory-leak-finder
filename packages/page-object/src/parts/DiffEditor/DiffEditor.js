549f59c765bceb1bbddd629d1de9382bb34d13af
Simon Siefke <simon.siefke@gmail.com>
2024-08-10 10:07:29 +0200

    feature: add notebook diff editor test (#1233)

--- packages/page-object/src/parts/DiffEditor/DiffEditor.js
+++ packages/page-object/src/parts/DiffEditor/DiffEditor.js
@@ -1,5 +1,6 @@
-import * as Explorer from '../Explorer/Explorer.js'
+import * as Character from '../Character/Character.js'
 import * as ContextMenu from '../ContextMenu/ContextMenu.js'
+import * as Explorer from '../Explorer/Explorer.js'
 import * as SideBar from '../SideBar/SideBar.js'
 
 export const create = ({ page, expect, VError }) => {
@@ -33,6 +34,30 @@ export const create = ({ page, expect, VError }) => {
         await sideBar.hide()
       } catch (error) {
         throw new VError(error, `Failed to open diff editor`)
+      }
+    },
+    async shouldHaveOriginalEditor(text) {
+      try {
+        const editor = page.locator('.editor.original')
+        const editorLines = editor.locator('.view-lines')
+        const actualText = text.replaceAll(Character.NewLine, Character.EmptyString).replaceAll(Character.Space, Character.NonBreakingSpace)
+        await expect(editorLines).toHaveText(actualText, {
+          timeout: 3000,
+        })
+      } catch (error) {
+        throw new VError(error, `Failed to assert original editor contents`)
+      }
+    },
+    async shouldHaveModifiedEditor(text) {
+      try {
+        const editor = page.locator('.editor.modified')
+        const editorLines = editor.locator('.view-lines')
+        const actualText = text.replaceAll(Character.NewLine, Character.EmptyString).replaceAll(Character.Space, Character.NonBreakingSpace)
+        await expect(editorLines).toHaveText(actualText, {
+          timeout: 3000,
+        })
+      } catch (error) {
+        throw new VError(error, `Failed to assert modified editor contents`)
       }
     },
   }
