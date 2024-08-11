9a219b65bca4c74c618c0339fe025f8f014a83b5
Simon Siefke <simon.siefke@gmail.com>
2024-08-11 12:46:29 +0200

    feature: add debug javascript hover test

---
+++ packages/e2e/src/debug-javascript-hover.js
@@ -0,0 +1,37 @@
+export const skip = true
+
+export const setup = async ({ Editor, Workspace, Explorer, RunAndDebug }) => {
+  await Workspace.setFiles([
+    {
+      name: 'index.js',
+      content: `let x = 1
+
+setInterval(()=>{
+  x++
+}, 1000)`,
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('index.js')
+  await RunAndDebug.removeAllBreakpoints()
+  await Editor.open('index.js')
+  await Editor.setBreakpoint(4)
+  await RunAndDebug.runAndWaitForPaused({
+    file: 'index.js',
+    line: 4,
+    callStackSize: 11,
+  })
+}
+
+export const run = async ({ DebugConsole }) => {
+  // await DebugConsole.type('glob')
+  // await DebugConsole.shouldHaveCompletions(['global', 'globalThis'])
+  // await DebugConsole.clearInput()
+}
+
+export const teardown = async ({ RunAndDebug, Editor }) => {
+  await RunAndDebug.stop()
+  await RunAndDebug.removeAllBreakpoints()
+  await Editor.closeAll()
+}
