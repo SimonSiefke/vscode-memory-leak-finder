ddf410982d4592ec3453b922cbb5f946ab61bb36
Simon Siefke <simon.siefke@gmail.com>
2024-08-11 10:43:55 +0200

    feature: add debug console evaluate test

---
+++ packages/e2e/src/debug-javascript-evaluate.js
@@ -0,0 +1,31 @@
+export const skip = true
+
+export const setup = async ({ Editor, Workspace, Explorer, RunAndDebug }) => {
+  await Workspace.setFiles([
+    {
+      name: 'index.js',
+      content: `let x = 1
+
+setInterval(()=>{
+  x++
+}, 1000)`,
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('index.js')
+  await RunAndDebug.removeAllBreakpoints()
+  await Editor.open('index.js')
+  await Editor.setBreakpoint(4)
+  await RunAndDebug.runAndWaitForPaused()
+}
+
+export const run = async ({ DebugConsole }) => {
+  await DebugConsole.evaluate('x', '1')
+}
+
+export const teardown = async ({ RunAndDebug, Editor }) => {
+  await RunAndDebug.stop()
+  await RunAndDebug.removeAllBreakpoints()
+  await Editor.closeAll()
+}
