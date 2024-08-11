e5d6638505659bffb59f016e4069626a8f6107c3
Simon Siefke <simon.siefke@gmail.com>
2024-08-11 09:10:48 +0200

    fix error

--- packages/test-worker-commands/src/parts/LocatorBlur/LocatorBlur.js
+++ packages/test-worker-commands/src/parts/LocatorBlur/LocatorBlur.js
@@ -1,9 +1,8 @@
 import * as Assert from '../Assert/Assert.js'
 import * as EvaluateInUtilityContext from '../EvaluateInUtilityContext/EvaluateInUtilityContext.js'
 
-export const blur = async (locator, text) => {
+export const blur = async (locator) => {
   Assert.object(locator)
-  Assert.string(text)
   await EvaluateInUtilityContext.evaluateInUtilityContext(
     {
       functionDeclaration: '(locator, fnName, options) => test.performAction(locator, fnName, options)',
@@ -15,9 +14,7 @@ export const blur = async (locator, text) => {
           value: 'blur',
         },
         {
-          value: {
-            text,
-          },
+          value: {},
         },
       ],
       awaitPromise: true,
