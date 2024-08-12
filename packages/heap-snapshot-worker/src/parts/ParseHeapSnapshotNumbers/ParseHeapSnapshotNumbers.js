3143fbcd2835d1cb713fc431629c036b547bdd80
Simon Siefke <simon.siefke@gmail.com>
2024-08-12 19:33:36 +0200

    feature: parse heap numbers

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotNumbers/ParseHeapSnapshotNumbers.js
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotNumbers/ParseHeapSnapshotNumbers.js
@@ -1,4 +1,15 @@
+import * as ParseHeapSnapshot from '../ParseHeapSnapshot/ParseHeapSnapshot.js'
+
+const isNumberNode = (node) => {
+  return node.type === 'number'
+}
+
 export const parseHeapSnapshotNumbers = (heapsnapshot) => {
-  console.log({ heapsnapshot })
+  // console.time('parse')
+  const { parsedNodes, graph } = ParseHeapSnapshot.parseHeapSnapshot(heapsnapshot)
+  // console.timeEnd('parse')
+  const numberNodes = parsedNodes.filter(isNumberNode)
+  console.log({ numberNodes })
+
   return 123
 }
