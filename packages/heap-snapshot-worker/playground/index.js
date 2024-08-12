c38f6792aa1d5573470e2f97e7af1e9eaf6f422d
Simon Siefke <simon.siefke@gmail.com>
2024-08-12 22:19:54 +0200

    improve measure

--- packages/heap-snapshot-worker/playground/index.js
+++ packages/heap-snapshot-worker/playground/index.js
@@ -7,7 +7,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const root = join(__dirname, '..', '..', '..')
 
-const fn = commandMap['HeapSnapshot.parseNumbers']
+// const fn = commandMap['HeapSnapshot.parseNumbers']
+const fn = commandMap['HeapSnapshot.parseNamedClosureCount']
 
 // const c = await readFile(join(root, '.vscode-heapsnapshots', 'lvce-web.json'), 'utf8')
 const v = JSON.parse(await readFile(join(root, '.vscode-heapsnapshots', '1.json'), 'utf8'))
--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotNumbers/ParseHeapSnapshotNumbers.js
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotNumbers/ParseHeapSnapshotNumbers.js
@@ -10,6 +10,21 @@ export const parseHeapSnapshotNumbers = (heapsnapshot) => {
   // console.timeEnd('parse')
   const numberNodes = parsedNodes.filter(isNumberNode)
   console.log({ numberNodes })
+  const id = 6889
+  const edges = graph[id]
+  console.log({ edges })
+  const otherIndex = 54
+  const other = parsedNodes[otherIndex]
+  console.log({ other })
+  const otherId = 109
+  const otherEdges = graph[otherId]
+  console.log({ otherEdges })
+
+  console.log({ dependendCode: parsedNodes[171], mapCoed: parsedNodes[39] })
+
+  const otherEdges2 = graph[343]
+  const otherEdges3 = graph[79]
+  console.log({ otherEdges2, otherEdges3 })
 
   return 123
 }
