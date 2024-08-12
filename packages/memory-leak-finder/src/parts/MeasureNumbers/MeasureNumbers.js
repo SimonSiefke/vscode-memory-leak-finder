0aedd249acfc19d348d20684c683099c95a5cf48
Simon Siefke <simon.siefke@gmail.com>
2024-08-12 22:28:40 +0200

    add numbers measure

---
+++ packages/memory-leak-finder/src/parts/MeasureNumbers/MeasureNumbers.js
@@ -0,0 +1,29 @@
+import * as GetNumbers from '../GetNumbers/GetNumbers.js'
+import * as MeasureId from '../MeasureId/MeasureId.js'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.js'
+
+export const id = MeasureId.Numbers
+
+export const create = (session) => {
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
+}
+
+export const start = (session, objectGroup) => {
+  return GetNumbers.getNumbers(session, objectGroup)
+}
+
+export const stop = (session, objectGroup) => {
+  return GetNumbers.getNumbers(session, objectGroup)
+}
+
+export const compare = (before, after) => {
+  return {
+    before,
+    after,
+  }
+}
+
+export const isLeak = () => {
+  return true
+}
