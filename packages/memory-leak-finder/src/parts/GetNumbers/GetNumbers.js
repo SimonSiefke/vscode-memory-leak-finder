37c0cce6a5ff7051e7701e6fad4c90a71f633b28
Simon Siefke <simon.siefke@gmail.com>
2024-08-12 22:27:12 +0200

    sort numbers

--- packages/memory-leak-finder/src/parts/GetNumbers/GetNumbers.js
+++ packages/memory-leak-finder/src/parts/GetNumbers/GetNumbers.js
@@ -1,20 +1,49 @@
-import { readFile } from 'node:fs/promises'
-import { join } from 'node:path'
-import * as HeapSnapshot from '../HeapSnapshot/HeapSnapshot.js'
-import * as HeapSnapshotFunctions from '../HeapSnapshotFunctions/HeapSnapshotFunctions.js'
-import * as Root from '../Root/Root.js'
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+import * as Arrays from '../Arrays/Arrays.js'
+
+const compareNumber = (a, b) => {
+  return b - a
+}
+
+const sortNumbers = (numbers) => {
+  return Arrays.toSorted(numbers, compareNumber)
+}
+
 /**
  *
  * @param {any} session
  * @returns {Promise<number[]>}
  */
 export const getNumbers = async (session, objectGroup) => {
-  const outFile = join(Root.root, '.vscode-heapsnapshots', `1.json`)
-  await HeapSnapshot.takeHeapSnapshot(session, outFile, {
-    captureNumericValue: true,
+  const prototype = await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: PrototypeExpression.Array,
+    returnByValue: false,
+    objectGroup,
   })
-  const content = await readFile(outFile, 'utf8')
-  const value = JSON.parse(content)
-  const numbers = await HeapSnapshotFunctions.parseHeapSnapshotNumbers(value)
+  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
+    prototypeObjectId: prototype.objectId,
+    objectGroup,
+  })
+  const result = await DevtoolsProtocolRuntime.callFunctionOn(session, {
+    functionDeclaration: `function () {
+  const objects = this
+
+  const numbers = []
+  for(const object of objects){
+    for(const item of object){
+      if(typeof item === 'number'){
+        numbers.push(item)
+      }
+    }
+  }
   return numbers
+}`,
+    objectId: objects.objects.objectId,
+    returnByValue: true,
+    objectGroup,
+  })
+
+  const sortedNumbers = sortNumbers(result)
+  return sortedNumbers
 }
