fb78e2a08acb9a8f6f4b52c69050f6296bb26cd0
Simon Siefke <simon.siefke@gmail.com>
2024-08-13 22:10:21 +0200

    feature: add setSize chart

--- packages/charts/src/parts/CreateSetSizeChart/CreateSetSizeChart.js
+++ packages/charts/src/parts/CreateSetSizeChart/CreateSetSizeChart.js
@@ -1,15 +1,15 @@
 import * as CreateChart from '../CreateChart/CreateChart.js'
-import * as GetMapSizeData from '../GetMapSizeData/GetMapSizeData.js'
+import * as GetSetSizeData from '../GetSetSizeData/GetSetSizeData.js'
 
 export const name = 'map-size'
 
-export const getData = GetMapSizeData.getMapSizeData
+export const getData = GetSetSizeData.getSetSizeData
 
 export const createChart = (data) => {
   return CreateChart.createChart(data, {
     x: 'index',
     y: 'count',
     xLabel: 'Index',
-    yLabel: 'Map Size',
+    yLabel: 'Set Size',
   })
 }
