5f59065bdaa13307cbbb7a67644cf89006cbca36
Simon Siefke <simon.siefke@gmail.com>
2024-08-14 19:49:43 +0200

    add test

--- packages/e2e/src/editor.no-autofixes-available.js
+++ packages/e2e/src/editor.no-autofixes-available.js
@@ -1,34 +1,24 @@
 export const skip = true
 
-export const beforeSetup = async ({ tmpDir, writeFile, join }) => {
-  await writeFile(
-    join(tmpDir, 'file.css'),
-    `h1 {
+export const beforeSetup = async ({ Workspace }) => {
+  await Workspace.setFiles([
+    {
+      name: 'file.css',
+      content: `h1 {
   font-size: 20px
 }
 
 h2 {
   font-size: 15px;
 }`,
-  )
+    },
+  ])
 }
 
 export const setup = async ({ Editor }) => {
   await Editor.open('file.css')
 }
 
-export const run = async ({ page, expect }) => {
-  const inlineFolded = page.locator('.inline-folded')
-  await expect(inlineFolded).toBeHidden()
-  const collapsedIcon = page.locator('.codicon-folding-collapsed').first()
-  await expect(collapsedIcon).toBeHidden()
-  const foldingIcon = page.locator('.codicon-folding-expanded').first()
-  await expect(foldingIcon).toBeVisible()
-  const firstIcon = foldingIcon.first()
-  await firstIcon.click()
-  await expect(inlineFolded).toBeVisible()
-  await expect(collapsedIcon).toBeVisible()
-  await collapsedIcon.click()
-  await expect(inlineFolded).toBeHidden()
-  await expect(collapsedIcon).toBeHidden()
+export const run = async ({ Editor }) => {
+  await Editor.goToFile({ file: 'file.css', line: 2, column: 2 })
 }
