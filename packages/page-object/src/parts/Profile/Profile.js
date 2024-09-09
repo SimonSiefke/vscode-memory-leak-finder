be903539af41c8b8a7e05b3e2fde5924e4b32b56
Simon Siefke <simon.siefke@gmail.com>
2024-09-09 21:25:39 +0200

    feature: add new profiles editor test (#1276)

--- packages/page-object/src/parts/Profile/Profile.js
+++ packages/page-object/src/parts/Profile/Profile.js
@@ -1,27 +1,60 @@
 import * as QuickPick from '../QuickPick/QuickPick.js'
 import * as WellKnownCommands from '../WellKnownCommands/WellKnownCommands.js'
+import * as ContextMenu from '../ContextMenu/ContextMenu.js'
 
 export const create = ({ page, expect, VError }) => {
   return {
+    async removeOtherProfiles() {
+      try {
+        const profilesList = page.locator('.profiles-list')
+        const profileListIems = profilesList.locator(`.profile-list-item`)
+        const contextMenu = ContextMenu.create({ page, expect, VError })
+        const count = await profileListIems.count()
+        for (let i = 2; i < count; i++) {
+          const second = profileListIems.nth(1)
+          await contextMenu.open(second)
+          await contextMenu.shouldHaveItem('Delete')
+          await contextMenu.select('Delete')
+          await expect(profileListIems).toHaveCount(count - i + 1)
+        }
+      } catch (error) {
+        throw new VError(error, `Failed to remove other profiles`)
+      }
+    },
     async create(info) {
       try {
         await page.waitForIdle()
         const quickPick = QuickPick.create({ page, expect, VError })
-        await quickPick.executeCommand(WellKnownCommands.ProfilesCreateProfile, {
+        await quickPick.executeCommand(WellKnownCommands.ProfilesNewProfile, {
           stayVisible: true,
         })
-        const input = page.locator('[placeholder="Profile name"]')
+        const profilesEditor = page.locator('.profiles-editor')
+        await expect(profilesEditor).toBeVisible()
+        const profilesList = page.locator('.profiles-list')
+        await expect(profilesList).toBeVisible()
+        if (info.removeOthers) {
+          await this.removeOtherProfiles()
+        }
+        const input = page.locator('.profile-row-container .input')
         await expect(input).toBeVisible()
+        await input.focus()
         await expect(input).toBeFocused()
-        await input.type(info.name)
+        await input.fill(info.name)
         await page.waitForIdle()
-        const createButton = page.locator('.monaco-button', {
+        const createButton = page.locator('.profile-row-container .monaco-button', {
           hasText: 'Create',
         })
         await createButton.click()
+
+        const profileListItem = profilesList.locator(`.profile-list-item[aria-label="${info.name}"]`)
+        await expect(profileListItem).toBeVisible()
+        const useButton = profileListItem.locator('[aria-label="Use this Profile for Current Window"]')
+        await useButton.click()
+
         const profileBadge = page.locator('.profile-badge', {
           hasText: 'TE',
         })
+        await expect(profileListItem).toBeVisible()
         await expect(profileBadge).toBeVisible()
       } catch (error) {
         throw new VError(error, `Failed to create profile`)
