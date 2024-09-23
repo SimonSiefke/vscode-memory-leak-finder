4479744ca24c4a90d0c291c2d6d45d2a2ea6193a
Simon Siefke <simon.siefke@gmail.com>
2024-09-23 22:07:57 +0200

    add test

--- packages/e2e/src/terminal.execute.js
+++ packages/e2e/src/terminal.execute.js
@@ -4,15 +4,10 @@ export const setup = async ({ Terminal, Workspace, SideBar }) => {
   await Terminal.killAll()
   await Workspace.setFiles([])
   await SideBar.hide()
-  await new Promise((r) => {
-    setTimeout(r, 1000)
-  })
   await Terminal.show()
-  // await Terminal.focus()
 }
 
 export const run = async ({ Terminal }) => {
   await Terminal.execute('echo test > test.txt')
-  // await Terminal.split()
-  // await Terminal.killSecond()
+  await Terminal.clear()
 }
