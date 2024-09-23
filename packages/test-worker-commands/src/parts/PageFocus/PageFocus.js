d9a034d9f703933f12f57de97fa60275f6abff4f
Simon Siefke <simon.siefke@gmail.com>
2024-09-23 22:06:02 +0200

    overwrite

--- packages/test-worker-commands/src/parts/PageFocus/PageFocus.js
+++ packages/test-worker-commands/src/parts/PageFocus/PageFocus.js
@@ -1,27 +1,24 @@
 import { VError } from '@lvce-editor/verror'
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 
+// TODO emulate a focused page?
 const script = `(async function () {
-  const el = globalThis._____electron
-  return 123
-  // const electron = require('electron')
-  // const { BrowserWindow } = electron
-  // const browserWindows = BrowserWindow.getAllWindows()
-  // const browserWindow = browserWindows[0]
-  // if(!browserWindow){
-  //   throw new Error("no browser window found")
-  // }
-  // browserWindow.focus()
+  const electron = globalThis._____electron
+  const { BrowserWindow } = electron
+  const browserWindows = BrowserWindow.getAllWindows()
+  const browserWindow = browserWindows[0]
+  if(!browserWindow){
+    throw new Error("no browser window found")
+  }
+  browserWindow.focus()
 })()`
 
-export const focus = async ({ electronRpc, electronObjectId }) => {
-  console.log({ electronRpc, electronObjectId })
+export const focus = async ({ electronRpc }) => {
   try {
-    const r = await DevtoolsProtocolRuntime.evaluate(electronRpc, {
+    await DevtoolsProtocolRuntime.evaluate(electronRpc, {
       expression: script,
       awaitPromise: true,
     })
-    console.log({ r })
   } catch (error) {
     throw new VError(error, `Failed to focus page`)
   }
