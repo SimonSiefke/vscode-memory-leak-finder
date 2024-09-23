c9335db67ace692493c73b0efbd1096c7491c4ef
Simon Siefke <simon.siefke@gmail.com>
2024-09-23 22:02:05 +0200

    Discard changes to packages/test-worker-commands/src/parts/PageFocus/PageFocus.js

--- packages/test-worker-commands/src/parts/PageFocus/PageFocus.js
+++ packages/test-worker-commands/src/parts/PageFocus/PageFocus.js
@@ -1,28 +1,19 @@
-import { VError } from '@lvce-editor/verror'
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 
-const script = `(async function () {
-  const el = globalThis._____electron
-  return 123
-  // const electron = require('electron')
-  // const { BrowserWindow } = electron
-  // const browserWindows = BrowserWindow.getAllWindows()
-  // const browserWindow = browserWindows[0]
-  // if(!browserWindow){
-  //   throw new Error("no browser window found")
-  // }
-  // browserWindow.focus()
-})()`
+const script = `function () {
+  const electron = this
+  const { BrowserWindow } = electron
+  const browserWindows = BrowserWindow.getAllWindows()
+  const browserWindow = browserWindows[0]
+  if(!browserWindow){
+    throw new Error("no browser window found")
+  }
+  browserWindow.focus()
+}`
 
 export const focus = async ({ electronRpc, electronObjectId }) => {
-  console.log({ electronRpc, electronObjectId })
-  try {
-    const r = await DevtoolsProtocolRuntime.evaluate(electronRpc, {
-      expression: script,
-      awaitPromise: true,
-    })
-    console.log({ r })
-  } catch (error) {
-    throw new VError(error, `Failed to focus page`)
-  }
+  await DevtoolsProtocolRuntime.callFunctionOn(electronRpc, {
+    functionDeclaration: script,
+    objectId: electronObjectId,
+  })
 }
