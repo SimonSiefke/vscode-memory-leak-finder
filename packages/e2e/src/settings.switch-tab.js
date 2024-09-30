3814a4c89fdd6f1533ab0d40d03621b3ec3c8922
Simon Siefke <simon.siefke@gmail.com>
2024-09-30 19:14:26 +0200

    enable test

--- packages/e2e/src/settings.switch-tab.js
+++ packages/e2e/src/settings.switch-tab.js
@@ -6,13 +6,6 @@ export const setup = async ({ Editor, SettingsEditor }) => {
 }
 
 export const run = async ({ SettingsEditor }) => {
-  await SettingsEditor.focusOutline('Commonly Used')
-  await SettingsEditor.focusOutline('Text Editor')
-  await SettingsEditor.focusOutline('Workbench')
-  await SettingsEditor.focusOutline('Window')
-  await SettingsEditor.focusOutline('Features')
-  await SettingsEditor.focusOutline('Application')
-  await SettingsEditor.focusOutline('Security')
-  await SettingsEditor.focusOutline('Extensions')
-  await SettingsEditor.focusOutline('Commonly Used')
+  await SettingsEditor.openTab('Workspace')
+  await SettingsEditor.openTab('User')
 }
