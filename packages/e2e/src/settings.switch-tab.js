79f1b9b3526ac557e68452833665e1afafc81f4d
Simon Siefke <simon.siefke@gmail.com>
2024-09-30 19:05:18 +0200

    feature: add settings switch tab test

---
+++ packages/e2e/src/settings.switch-tab.js
@@ -0,0 +1,19 @@
+export const skip = true
+export const setup = async ({ Editor, SettingsEditor }) => {
+  await Editor.closeAll()
+  await SettingsEditor.open()
+  await SettingsEditor.ensureIdle()
+  await SettingsEditor.collapseOutline()
+}
+
+export const run = async ({ SettingsEditor }) => {
+  await SettingsEditor.focusOutline('Commonly Used')
+  await SettingsEditor.focusOutline('Text Editor')
+  await SettingsEditor.focusOutline('Workbench')
+  await SettingsEditor.focusOutline('Window')
+  await SettingsEditor.focusOutline('Features')
+  await SettingsEditor.focusOutline('Application')
+  await SettingsEditor.focusOutline('Security')
+  await SettingsEditor.focusOutline('Extensions')
+  await SettingsEditor.focusOutline('Commonly Used')
+}
