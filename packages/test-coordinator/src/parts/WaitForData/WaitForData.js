2c7fad22e24ed47f09107454c54e886278962975
Simon Siefke <simon.siefke@gmail.com>
2024-10-31 12:16:10 +0100

    tsc

--- packages/test-coordinator/src/parts/WaitForData/WaitForData.js
+++ packages/test-coordinator/src/parts/WaitForData/WaitForData.js
@@ -3,6 +3,7 @@ import { once } from 'node:events'
 export const waitForData = async (stream, key, errorChecker) => {
   for (let i = 0; i < 10; i++) {
     const [data] = await once(stream, 'data')
+    // @ts-ignore
     if (data.includes(key)) {
       return data
     }
