7b7caf50675645856e0496d15db6076fffb1677a
Simon Siefke <simon.siefke@gmail.com>
2024-12-11 21:59:55 +0100

    debug

--- packages/memory-leak-worker/src/parts/MemoryLeakFinderStart/MemoryLeakFinderStart.js
+++ packages/memory-leak-worker/src/parts/MemoryLeakFinderStart/MemoryLeakFinderStart.js
@@ -1,9 +1,19 @@
 import * as MemoryLeakFinderState from '../MemoryLeakFinderState/MemoryLeakFinderState.js'
 
+const doStart = async (instanceId) => {
+  const measure = MemoryLeakFinderState.get(instanceId)
+  if (!measure) {
+    throw new Error(`no measure found`)
+  }
+  const result = await measure.start()
+  return result
+}
+
 export const start = async (instanceId) => {
   const measure = MemoryLeakFinderState.get(instanceId)
   if (!measure) {
     throw new Error(`no measure found`)
   }
-  return measure.start()
+  const result = await measure.start()
+  return result
 }
