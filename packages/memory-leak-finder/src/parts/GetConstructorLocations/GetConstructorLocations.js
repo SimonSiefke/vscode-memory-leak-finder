5d8a2e6e490778720cb74e12091728b7f3bcb41d
Simon Siefke <simon.siefke@gmail.com>
2024-12-11 18:59:00 +0100

    measure instance counts in pr

--- packages/memory-leak-finder/src/parts/GetConstructorLocations/GetConstructorLocations.js
+++ packages/memory-leak-finder/src/parts/GetConstructorLocations/GetConstructorLocations.js
@@ -19,13 +19,18 @@ export const getConstructorLocations = async (session, objectGroup, map) => {
     returnByValue: false,
     objectGroup,
   })
+  console.time('properties')
   const fnResult3 = await DevtoolsProtocolRuntime.getProperties(session, {
     objectId: fnResult2.objectId,
     generatePreview: false,
     ownProperties: true,
   })
+  console.timeEnd('properties')
   const descriptors = GetDescriptorValues.getDescriptorValues(fnResult3.result)
   const functionObjectIds = GetFunctionObjectIds.getFunctionObjectIds(descriptors)
+  console.log({ functionObjectIds })
+  console.time('locations')
   const functionLocations = await GetFunctionLocations.getFunctionLocations(session, functionObjectIds)
+  console.timeEnd('locations')
   return functionLocations
 }
