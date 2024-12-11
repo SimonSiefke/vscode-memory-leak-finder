d53a0b55ac94b4cc7960bd28f262f88b75665631
Simon Siefke <simon.siefke@gmail.com>
2024-12-11 19:47:55 +0100

    feature: add weakRefCount measure (#1326)

--- packages/memory-leak-finder/src/parts/Measures/Measures.js
+++ packages/memory-leak-finder/src/parts/Measures/Measures.js
@@ -80,5 +80,6 @@ export * as MeasureSymbolCount from '../MeasureSymbolCount/MeasureSymbolCount.js
 export * as MeasureSymbols from '../MeasureSymbols/MeasureSymbols.js'
 export * as MeasureUserStringCount from '../MeasureUserStringCount/MeasureUserStringCount.js'
 export * as MeasureWeakMapCount from '../MeasureWeakMapCount/MeasureWeakMapCount.js'
+export * as MeasureWeakRefCount from '../MeasureWeakRefCount/MeasureWeakRefCount.js'
 export * as MeasureWeakSetCount from '../MeasureWeakSetCount/MeasureWeakSetCount.js'
 export * as MeasureWidgetCount from '../MeasureWidgetCount/MeasureWidgetCount.js'
