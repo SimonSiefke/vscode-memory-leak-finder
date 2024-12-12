16bf2fb61cf5b7920622926d8641d8dbfa2fd404
Simon Siefke <simon.siefke@gmail.com>
2024-12-12 21:46:50 +0100

    Discard changes to packages/memory-leak-finder/src/parts/GetConstructorLocations/GetConstructorLocations.js

--- packages/memory-leak-finder/src/parts/GetConstructorLocations/GetConstructorLocations.js
+++ packages/memory-leak-finder/src/parts/GetConstructorLocations/GetConstructorLocations.js
@@ -13,24 +13,19 @@ export const getConstructorLocations = async (session, objectGroup, map) => {
     array.push(instanceConstructor)
   }
 
-  globalThis.yy=array
   return array
 }`,
     objectId: map.objectId,
     returnByValue: false,
     objectGroup,
   })
-  console.time('properties')
   const fnResult3 = await DevtoolsProtocolRuntime.getProperties(session, {
     objectId: fnResult2.objectId,
     generatePreview: false,
     ownProperties: true,
   })
-  console.timeEnd('properties')
   const descriptors = GetDescriptorValues.getDescriptorValues(fnResult3.result)
   const functionObjectIds = GetFunctionObjectIds.getFunctionObjectIds(descriptors)
-  console.time('locations')
   const functionLocations = await GetFunctionLocations.getFunctionLocations(session, functionObjectIds)
-  console.timeEnd('locations')
   return functionLocations
 }
