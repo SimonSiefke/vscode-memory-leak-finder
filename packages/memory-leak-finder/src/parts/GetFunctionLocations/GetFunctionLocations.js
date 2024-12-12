d4d4385cc0eb1e2ff43b22c635946b34176763a1
Simon Siefke <simon.siefke@gmail.com>
2024-12-12 21:47:01 +0100

    Discard changes to packages/memory-leak-finder/src/parts/GetFunctionLocations/GetFunctionLocations.js

--- packages/memory-leak-finder/src/parts/GetFunctionLocations/GetFunctionLocations.js
+++ packages/memory-leak-finder/src/parts/GetFunctionLocations/GetFunctionLocations.js
@@ -1,15 +1,14 @@
 import * as Assert from '../Assert/Assert.js'
 import * as GetFunctionLocation from '../GetFunctionLocation/GetFunctionLocation.js'
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
 
-const BATCH_SIZE = 500
+const BATCH_SIZE = 50
 
 const processBatch = async (session, objectIds, startIndex) => {
   const promises = []
   const endIndex = Math.min(startIndex + BATCH_SIZE, objectIds.length)
 
   for (let i = startIndex; i < endIndex; i++) {
-    promises.push(GetFunctionLocation.getFunctionLocation(session, objectIds[i], i))
+    promises.push(GetFunctionLocation.getFunctionLocation(session, objectIds[i]))
   }
 
   return Promise.all(promises)
@@ -22,16 +21,7 @@ export const getFunctionLocations = async (session, objectIds) => {
   const results = []
 
   for (let i = 0; i < objectIds.length; i += BATCH_SIZE) {
-    console.time('batch' + i)
     const batchResults = await processBatch(session, objectIds, i)
-
-    // give browser some time
-
-    // await DevtoolsProtocolRuntime.evaluate(session, {
-    //   expression: `new Promise(resolve => { requestIdleCallback(resolve) })`,
-    //   awaitPromise: true,
-    // })
-    console.timeEnd('batch' + i)
     results.push(...batchResults)
   }
 
