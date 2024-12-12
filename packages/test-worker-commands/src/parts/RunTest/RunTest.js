2d32b58e9c86c42966093af5a990d24b56b98a5c
Simon Siefke <simon.siefke@gmail.com>
2024-12-12 21:34:38 +0100

    Discard changes to packages/test-worker-commands/src/parts/RunTest/RunTest.js

--- packages/test-worker-commands/src/parts/RunTest/RunTest.js
+++ packages/test-worker-commands/src/parts/RunTest/RunTest.js
@@ -1,21 +1,11 @@
 import * as Assert from '../Assert/Assert.js'
 import * as PageObjectState from '../PageObjectState/PageObjectState.js'
 import * as RunTestWithCallback from '../RunTestWithCallback/RunTestWithCallback.js'
-import * as WaitForCrash from '../WaitForCrash/WaitForCrash.js'
 
 export const runTest = async (connectionId, absolutePath, forceRun, runMode) => {
   Assert.number(connectionId)
   Assert.string(absolutePath)
   const pageObject = PageObjectState.getPageObject(connectionId)
-  const firstWindow = PageObjectState.getFirstWindow(connectionId)
-  const crashInfo = WaitForCrash.waitForCrash(firstWindow)
-  const testResultPromise = RunTestWithCallback.runTestWithCallback(pageObject, absolutePath, forceRun, runMode)
-  const intermediateResult = await Promise.race([crashInfo.promise, testResultPromise])
-  console.log({ intermediateResult })
-  if (intermediateResult && intermediateResult.crashed) {
-    console.log('throw error')
-    throw new Error(`Target crashed`)
-  }
-  crashInfo.cleanup()
-  return intermediateResult
+  const skipped = await RunTestWithCallback.runTestWithCallback(pageObject, absolutePath, forceRun, runMode)
+  return skipped
 }
