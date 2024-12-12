2e96f3a7f381eee32ab94546cf3cb446623c6efe
Simon Siefke <simon.siefke@gmail.com>
2024-12-12 19:50:33 +0100

    Discard changes to packages/memory-leak-worker/src/parts/MemoryLeakFinderStop/MemoryLeakFinderStop.js

--- packages/memory-leak-worker/src/parts/MemoryLeakFinderStop/MemoryLeakFinderStop.js
+++ packages/memory-leak-worker/src/parts/MemoryLeakFinderStop/MemoryLeakFinderStop.js
@@ -1,7 +1,6 @@
 import * as MemoryLeakFinderState from '../MemoryLeakFinderState/MemoryLeakFinderState.js'
-import * as WaitForCrash from '../WaitForCrash/WaitForCrash.js'
 
-const doStop = async (instanceId) => {
+export const stop = async (instanceId) => {
   const measure = MemoryLeakFinderState.get(instanceId)
   if (!measure) {
     throw new Error(`no measure found`)
@@ -9,15 +8,4 @@ const doStop = async (instanceId) => {
   const result = await measure.stop()
   await measure.releaseResources()
   return result
-}
-
-export const stop = async (instanceId, electronTargetId) => {
-  const crashInfo = WaitForCrash.waitForCrash(electronTargetId)
-  const resultPromise = doStop(instanceId)
-  const intermediateResult = await Promise.race([crashInfo.promise, resultPromise])
-  if (intermediateResult && intermediateResult.crashed) {
-    throw new Error('target crashed')
-  }
-  crashInfo.cleanup()
-  return intermediateResult
 }
