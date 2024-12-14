e9411e3a7302e0ba391e69348ec8323a0a463f30
Simon Siefke <simon.siefke@gmail.com>
2024-12-14 01:02:18 +0100

    feature: optimize numberCount measure (#1340)

--- packages/memory-leak-finder/src/parts/GetNumberCount/GetNumberCount.js
+++ packages/memory-leak-finder/src/parts/GetNumberCount/GetNumberCount.js
@@ -1,11 +1,50 @@
-import * as GetNumbers from '../GetNumbers/GetNumbers.js'
+import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
+
 /**
  *
  * @param {any} session
  * @returns {Promise<number>}
  */
 export const getNumberCount = async (session, objectGroup) => {
-  const numbers = await GetNumbers.getNumbers(session, objectGroup)
-  const numberCount = numbers.length
+  const prototype = await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: PrototypeExpression.Object,
+    returnByValue: false,
+    objectGroup,
+  })
+  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
+    prototypeObjectId: prototype.objectId,
+    objectGroup,
+  })
+  const result = await DevtoolsProtocolRuntime.callFunctionOn(session, {
+    functionDeclaration: `function () {
+  const objects = this
+
+  const numbers = []
+
+  const getValues = object => {
+    try {
+      return Object.values(object)
+    } catch {
+      return []
+    }
+  }
+
+  let numberCount = 0
+
+  for(const object of objects){
+    const values = getValues(object)
+    for(const value of values){
+      if(typeof value === 'number'){
+        numberCount++
+      }
+    }
+  }
   return numberCount
+}`,
+    objectId: objects.objects.objectId,
+    returnByValue: true,
+    objectGroup,
+  })
+  return result
 }
