041f289b9f4ae42b361943afb42810c8aaa6608d
Simon Siefke <simon.siefke@gmail.com>
2025-02-07 18:11:09 +0100

    test

--- packages/e2e/src/settings.checkbox.js
+++ packages/e2e/src/settings.checkbox.js
@@ -2,8 +2,8 @@ export const setup = async ({ Editor, SettingsEditor }) => {
   await Editor.closeAll()
   await SettingsEditor.open()
   await SettingsEditor.search({
-    value: 'Editor: Auto Closing Comments',
-    resultCount: 2,
+    value: 'editor.autoClosingComments',
+    resultCount: 1,
   })
   await SettingsEditor.enableCheckBox({
     name: 'comments.visible',
