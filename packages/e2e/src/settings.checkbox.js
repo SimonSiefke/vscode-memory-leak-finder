1a9577e1ddbe5fd16e8c1f12f2bbeb3208c28203
Simon Siefke <simon.siefke@gmail.com>
2025-02-09 22:42:18 +0100

    feature: update vscode version (#1360)

--- packages/e2e/src/settings.checkbox.js
+++ packages/e2e/src/settings.checkbox.js
@@ -2,8 +2,8 @@ export const setup = async ({ Editor, SettingsEditor }) => {
   await Editor.closeAll()
   await SettingsEditor.open()
   await SettingsEditor.search({
-    value: 'Editor: Auto Closing Comments',
-    resultCount: 2,
+    value: 'comments.visible',
+    resultCount: 1,
   })
   await SettingsEditor.enableCheckBox({
     name: 'comments.visible',
