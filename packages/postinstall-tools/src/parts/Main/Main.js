6e011eb45331549533a7b8f032a0505b967c2181
Simon Siefke <simon.siefke@gmail.com>
2025-02-23 21:18:25 +0100

    fix error

--- packages/postinstall-tools/src/parts/Main/Main.js
+++ packages/postinstall-tools/src/parts/Main/Main.js
@@ -3,6 +3,6 @@ import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
 export const main = async () => {
   const ipc = await DownloadWorker.launch()
-  await JsonRpc.invoke('Download.downloadFfmpegMaybe')
+  await JsonRpc.invoke(ipc, 'Download.downloadFfmpegMaybe')
   ipc.dispose()
 }
