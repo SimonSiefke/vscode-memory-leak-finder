ba166b8921525319938d9942df92a63ac67538cd
Simon Siefke <simon.siefke@gmail.com>
2025-02-23 20:01:45 +0100

    feature: add downloadCursor command

--- packages/download-worker/src/parts/CommandMap/CommandMap.js
+++ packages/download-worker/src/parts/CommandMap/CommandMap.js
@@ -1,7 +1,9 @@
+import * as DownloadAndUnzipCursor from '../DownloadAndUnzipCursor/DownloadAndUnzipCursor.js'
 import * as DownloadAndUnzipVscode from '../DownloadAndUnzipVscode/DownloadAndUnzipVscode.js'
 import * as DownloadFfmpeg from '../DownloadFfmpeg/DownloadFfmpeg.js'
 
 export const commandMap = {
   'Download.downloadAndUnzipVscode': DownloadAndUnzipVscode.downloadAndUnzipVscode,
   'Download.downloadFfmpeg': DownloadFfmpeg.downloadFfmpeg,
+  'Download.downloadAndUnzipCursor': DownloadAndUnzipCursor.downloadAndUnzipCursor,
 }
