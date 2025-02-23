7a2eec0d102f327ef0c6aa08fc1a56a3a63f4685
Simon Siefke <simon.siefke@gmail.com>
2025-02-23 21:37:12 +0100

    improve error message

--- packages/storage-worker/src/parts/ExecuteSql/ExecuteSql.js
+++ packages/storage-worker/src/parts/ExecuteSql/ExecuteSql.js
@@ -1,11 +1,11 @@
 export const executeSql = async (db, statement) => {
-  const { resolve, reject, promise } = Promise.withResolvers();
+  const { resolve, reject, promise } = Promise.withResolvers()
   db.all(statement, (error, rows) => {
     if (error) {
-      reject(new Error(`failed to get rows`));
+      reject(new Error(`failed to execute sql: ${error}`))
     }
-    resolve(rows);
-  });
-  const rows = await promise;
-  return rows;
-};
+    resolve(rows)
+  })
+  const rows = await promise
+  return rows
+}
