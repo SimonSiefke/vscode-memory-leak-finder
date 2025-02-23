31f46b3dbffbd13e746ff9bc2387dad892344bb2
Simon Siefke <simon.siefke@gmail.com>
2025-02-23 14:43:33 +0100

    improve launch

--- packages/test-coordinator/src/parts/LaunchCursor/LaunchCursor.js
+++ packages/test-coordinator/src/parts/LaunchCursor/LaunchCursor.js
@@ -2,18 +2,14 @@ import { copyFile, mkdir, rm } from 'node:fs/promises'
 import { dirname } from 'node:path'
 import * as CreateTestWorkspace from '../CreateTestWorkspace/CreateTestWorkspace.js'
 import * as DefaultVscodeSettingsPath from '../DefaultVscodeSettingsPath/DefaultVsCodeSettingsPath.js'
-import * as GetBinaryPath from '../GetBinaryPath/GetBinaryPath.js'
-import * as GetUserDataDir from '../GetUserDataDir/GetUserDataDir.js'
+import * as DownloadAndUnzipCursor from '../DownloadAndUnzipCursor/DownloadAndUnzipCursor.js'
 import * as GetExtensionsDir from '../GetExtensionsDir/GetExtensionsDir.js'
+import * as GetUserDataDir from '../GetUserDataDir/GetUserDataDir.js'
 import * as GetVsCodeArgs from '../GetVsCodeArgs/GetVsCodeArgs.js'
 import * as GetVsCodeEnv from '../GetVsCodeEnv/GetVsCodeEnv.js'
 import * as GetVscodeRuntimeDir from '../GetVscodeRuntimeDir/GetVscodeRuntimeDir.js'
-import * as IsCi from '../IsCi/IsCi.js'
 import * as LaunchElectron from '../LaunchElectron/LaunchElectron.js'
 import { join } from '../Path/Path.js'
-import * as RemoveVscodeBackups from '../RemoveVscodeBackups/RemoveVscodeBackups.js'
-import * as RemoveVscodeGlobalStorage from '../RemoveVscodeGlobalStorage/RemoveVscodeGlobalStorage.js'
-import * as RemoveVscodeWorkspaceStorage from '../RemoveVscodeWorkspaceStorage/RemoveVscodeWorkspaceStorage.js'
 import * as Root from '../Root/Root.js'
 import { VError } from '../VError/VError.js'
 
@@ -28,7 +24,7 @@ export const launchCursor = async ({ headlessMode, cwd }) => {
     // }
     // await RemoveVscodeBackups.removeVscodeBackups()
     const runtimeDir = GetVscodeRuntimeDir.getVscodeRuntimeDir()
-    const binaryPath = await GetBinaryPath.getBinaryPath()
+    const binaryPath = await DownloadAndUnzipCursor.downloadAndUnzipCursor()
     const userDataDir = GetUserDataDir.getUserDataDir()
     const extensionsDir = GetExtensionsDir.getExtensionsDir()
     await rm(extensionsDir, { recursive: true, force: true })
