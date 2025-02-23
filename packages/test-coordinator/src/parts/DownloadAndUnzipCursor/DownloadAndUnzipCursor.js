3b8bb0550af3c886b837ceb9cf6e8c6d65abe528
Simon Siefke <simon.siefke@gmail.com>
2025-02-23 14:37:04 +0100

    improve appimage extract

--- packages/test-coordinator/src/parts/DownloadAndUnzipCursor/DownloadAndUnzipCursor.js
+++ packages/test-coordinator/src/parts/DownloadAndUnzipCursor/DownloadAndUnzipCursor.js
@@ -2,8 +2,10 @@ import { VError } from '@lvce-editor/verror'
 import * as Download from '../Download/Download.js'
 import * as Root from '../Root/Root.js'
 import * as MakeExecutable from '../MakeExecutable/MakeExecutable.js'
+import * as ExtractAppImage from '../ExtractAppImage/ExtractAppImage.js'
 import { join } from 'path'
 import { rm } from 'fs/promises'
+import { existsSync } from 'fs'
 
 // TODO support macos and windows also and different architecture
 const downloadUrl = 'https://dl.todesktop.com/230313mzl4w4u92/versions/0.42.4/linux/zip/x64'
@@ -17,10 +19,12 @@ export const downloadAndUnzipCursor = async () => {
     // 4. return binary path
 
     const outFile = join(Root.root, '.vscode-tool-downloads', 'cursor.AppImage')
-
-    await rm(outFile, { recursive: true, force: true })
-    await Download.download('cursor', downloadUrl, outFile)
+    if (!existsSync(outFile)) {
+      await rm(outFile, { recursive: true, force: true })
+      await Download.download('cursor', downloadUrl, outFile)
+    }
     await MakeExecutable.makeExecutable(outFile)
+    await ExtractAppImage.extractAppImage(outFile)
     console.log('done')
     return ''
   } catch (error) {
--- packages/test-coordinator/src/parts/ExtractAppImage/ExtractAppImage.js
+++ packages/test-coordinator/src/parts/ExtractAppImage/ExtractAppImage.js
@@ -1,9 +1,13 @@
 import { VError } from '@lvce-editor/verror'
 import { execa } from 'execa'
+import { dirname } from 'path'
 
 export const extractAppImage = async (appImagePath) => {
   try {
-    await execa(appImagePath, '--appimage-extract')
+    const folder = dirname(appImagePath)
+    await execa(appImagePath, ['--appimage-extract'], {
+      cwd: folder,
+    })
   } catch (error) {
     throw new VError(error, `Failed to extract appImage`)
   }
