08963122e42f4256c501ee2c4a0a94a50bc2ec37
Simon Siefke <simon.siefke@gmail.com>
2025-03-27 20:04:55 +0100

    fix measure

--- packages/memory-leak-finder/src/parts/GetCssInlineStyleCount/GetCssInlineStyleCount.js
+++ packages/memory-leak-finder/src/parts/GetCssInlineStyleCount/GetCssInlineStyleCount.js
@@ -1,4 +1,5 @@
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.js'
 
 /**
  *
@@ -6,10 +7,43 @@ import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js
  * @returns {Promise<number>}
  */
 export const getCssInlineStyleCount = async (session, objectGroup) => {
-  const ruleCount = await DevtoolsProtocolRuntime.evaluate(session, {
-    expression: `1+1`,
+  const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
+    expression: PrototypeExpression.Node,
+    returnByValue: false,
+    objectGroup,
+  })
+  const objects = await DevtoolsProtocolRuntime.queryObjects(session, {
+    prototypeObjectId: prototypeDescriptor.objectId,
+    objectGroup,
+  })
+  const fnResult1 = await DevtoolsProtocolRuntime.callFunctionOn(session, {
+    functionDeclaration: `function(){
+const objects = this
+
+const getInlineStyleCount = node => {
+  try {
+    const styleArray = [...node.style]
+    return styleArray.length
+  } catch {
+    return 0
+  }
+}
+
+const getInlineStyleCounts = (nodes) => {
+  let total = 0
+  for (const node of nodes) {
+    const count = getInlineStyleCount(node)
+    total += count
+  }
+  return total
+}
+
+const totalCount = getInlineStyleCounts(objects)
+return totalCount
+}`,
+    objectId: objects.objects.objectId,
     returnByValue: true,
     objectGroup,
   })
-  return ruleCount
+  return fnResult1
 }
