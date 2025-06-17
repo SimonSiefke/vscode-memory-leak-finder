0109094ee404a9528e11bd5f8f26cd90ee52d078
Simon Siefke <simon.siefke@gmail.com>
2025-06-17 22:31:42 +0200

    feature: make ideversion configurable in page object

--- packages/test-coordinator/src/parts/PageObject/PageObject.js
+++ packages/test-coordinator/src/parts/PageObject/PageObject.js
@@ -2,11 +2,12 @@ import * as Assert from '../Assert/Assert.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as TestWorkerCommandType from '../TestWorkerCommandType/TestWorkerCommandType.js'
 
-export const create = (ipc, connectionId, isFirstConnection, headlessMode, timeouts) => {
+export const create = (ipc, connectionId, isFirstConnection, headlessMode, timeouts, ideVersion = '') => {
   Assert.object(ipc)
   Assert.number(connectionId)
   Assert.boolean(isFirstConnection)
   Assert.boolean(headlessMode)
   Assert.boolean(timeouts)
-  return JsonRpc.invoke(ipc, TestWorkerCommandType.PageObjectCreate, connectionId, isFirstConnection, headlessMode, timeouts)
+  Assert.string(ideVersion)
+  return JsonRpc.invoke(ipc, TestWorkerCommandType.PageObjectCreate, connectionId, isFirstConnection, headlessMode, timeouts, ideVersion)
 }
