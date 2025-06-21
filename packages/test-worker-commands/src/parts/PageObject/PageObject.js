15985c38bb9e0244c45243aa5c0387a3414a952f
Simon Siefke <simon.siefke@gmail.com>
2025-06-21 15:41:06 +0200

    feature: make ideversion configurable in page object (#1440)

--- packages/test-worker-commands/src/parts/PageObject/PageObject.js
+++ packages/test-worker-commands/src/parts/PageObject/PageObject.js
@@ -8,11 +8,12 @@ import * as PageObjectState from '../PageObjectState/PageObjectState.js'
 import { VError } from '../VError/VError.js'
 import * as WaitForVsCodeToBeReady from '../WaitForVsCodeToBeReady/WaitForVsCodeToBeReady.js'
 
-export const create = async (connectionId, isFirstConnection, isHeadless, timeouts) => {
+export const create = async (connectionId, isFirstConnection, isHeadless, timeouts, ideVersion) => {
   try {
     Assert.number(connectionId)
     Assert.boolean(isFirstConnection)
     Assert.boolean(timeouts)
+    Assert.string(ideVersion)
     const pageObjectPath = GetPageObjectPath.getPageObjectPath()
     const pageObjectModule = await ImportScript.importScript(pageObjectPath)
     const electronApp = ElectronAppState.get(connectionId)
@@ -28,6 +29,7 @@ export const create = async (connectionId, isFirstConnection, isHeadless, timeou
       expect: Expect.expect,
       VError,
       electronApp,
+      vscodeVersion: ideVersion,
     }
     const pageObject = pageObjectModule.create(pageObjectContext)
     PageObjectState.set(connectionId, { pageObject, firstWindow })
