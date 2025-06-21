f81b2123f4a00cbfdda64fc775895bce286ae192
Simon Siefke <simon.siefke@gmail.com>
2025-06-21 17:29:15 +0200

    feature: move create into separate file

---
+++ packages/page-object/src/parts/Create/Create.js
@@ -0,0 +1,14 @@
+import * as Parts from '../Parts/Parts.js'
+
+export const create = (context) => {
+  const api = Object.create(null)
+  for (const [key, value] of Object.entries(Parts)) {
+    if (key === 'WellKnownCommands') {
+      api[key] = value
+    } else {
+      // @ts-ignore
+      api[key] = value.create(context)
+    }
+  }
+  return api
+}
