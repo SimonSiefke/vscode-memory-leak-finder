5a19e274eadd52f82c457cdaf2f805cb46c7d12d
Simon Siefke <simon.siefke@gmail.com>
2025-06-22 22:53:27 +0200

    fix update

--- packages/injected-code/src/parts/ElementActionType/ElementActionType.js
+++ packages/injected-code/src/parts/ElementActionType/ElementActionType.js
@@ -8,12 +8,18 @@ const getNewValue = (value, selectionStart, selectionEnd, text) => {
   return value.slice(0, selectionStart) + text + value.slice(selectionEnd)
 }
 
+const isInputElement = (element) => {
+  return element && (element instanceof HTMLInputElement || element instanceof HTMLTextAreaElement)
+}
+
 export const type = (element, options) => {
   const selectionStart = element.selectionStart
   const selectionEnd = element.selectionEnd
   const oldValue = element.value || ''
   const newValue = getNewValue(oldValue, selectionStart, selectionEnd, options.text)
-  element.value = newValue
+  if (isInputElement(element)) {
+    element.value = newValue
+  }
   dispatchEditContextUpdate(element, newValue)
   DispatchEvent.input(element, {})
   DispatchEvent.change(element, {})
