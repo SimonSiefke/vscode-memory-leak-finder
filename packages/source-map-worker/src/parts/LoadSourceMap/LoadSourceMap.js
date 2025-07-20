92ecc457f88fc71fd0408bbd63dd30483d889beb
Simon Siefke <simon.siefke@gmail.com>
2025-07-20 12:16:34 +0200

    feature: load sourcemap using worker

--- packages/source-map-worker/src/parts/LoadSourceMap/LoadSourceMap.js
+++ packages/source-map-worker/src/parts/LoadSourceMap/LoadSourceMap.js
@@ -1,21 +1,24 @@
+import * as Callback from '../Callback/Callback.js'
+import * as Command from '../Command/Command.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import { loadSourceMapWorkerPath } from '../LoadSourceMapWorkerPath/LoadSourceMapWorkerPath.js'
 import { VError } from '../VError/VError.js'
-import * as LoadSourceMapModule from '../LoadSourceMapModule/LoadSourceMapModule.js'
-
-const getProtocol = (url) => {
-  const colonIndex = url.indexOf(':')
-  if (colonIndex === -1) {
-    console.warn(`Unsupported source map url ${url}`)
-    return 'noop'
-  }
-  return url.slice(0, colonIndex)
-}
 
 export const loadSourceMap = async (url) => {
   try {
-    const protocol = getProtocol(url)
-    const module = await LoadSourceMapModule.getModule(protocol)
-    const data = await module.loadSourceMap(url)
-    return data
+    const ipc = await IpcParent.create({
+      method: IpcParentType.NodeWorkerThread,
+      url: loadSourceMapWorkerPath,
+      stdio: 'inherit',
+      execArgv: [],
+    })
+    HandleIpc.handleIpc(ipc, Command.execute, Callback.resolve)
+    const sourceMap = await JsonRpc.invoke(ipc, 'LoadSourceMap.loadSourceMap', url)
+    await ipc.dispose()
+    return sourceMap
   } catch (error) {
     throw new VError(error, `Failed to load source map for ${url}`)
   }
