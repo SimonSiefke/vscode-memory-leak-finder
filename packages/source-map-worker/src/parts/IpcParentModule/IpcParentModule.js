56550cea735cb42e30ad853db94e8ef1e7eaf289
Simon Siefke <simon.siefke@gmail.com>
2025-07-20 10:46:39 +0200

    fix

--- packages/source-map-worker/src/parts/IpcParentModule/IpcParentModule.js
+++ packages/source-map-worker/src/parts/IpcParentModule/IpcParentModule.js
@@ -4,8 +4,6 @@ export const getModule = (method) => {
   switch (method) {
     case IpcParentType.NodeWorkerThread:
       return import('../IpcParentWithNodeWorkerThread/IpcParentWithNodeWorkerThread.js')
-    case IpcParentType.NodeForkedProcess:
-      return import('../IpcParentWithNodeForkedProcess/IpcParentWithNodeForkedProcess.js')
     default:
       throw new Error('unexpected ipc type')
   }
