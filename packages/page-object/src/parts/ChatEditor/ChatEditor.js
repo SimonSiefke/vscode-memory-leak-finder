c4b366a4eeacf24787d10978c7e5552f545f7ce9
Simon Siefke <simon.siefke@gmail.com>
2025-07-20 11:33:34 +0200

    improve chat api

--- packages/page-object/src/parts/ChatEditor/ChatEditor.js
+++ packages/page-object/src/parts/ChatEditor/ChatEditor.js
@@ -12,5 +12,20 @@ export const create = ({ expect, page, VError }) => {
         throw new VError(error, `Failed to open chat editor`)
       }
     },
+    async sendMessage(message) {
+      try {
+        const editContext = page.locator('.native-edit-context')
+        await expect(editContext).toBeFocused()
+        await editContext.setValue(message)
+        await page.waitForIdle()
+        const chatView = page.locator('.interactive-session')
+        const sendButton = chatView.locator('[aria-label^="Send and Dispatch"]')
+        await sendButton.click()
+        await page.waitForIdle()
+        // TODO check that message is visible and response also
+      } catch (error) {
+        throw new VError(error, `Failed to send chat message`)
+      }
+    },
   }
 }
