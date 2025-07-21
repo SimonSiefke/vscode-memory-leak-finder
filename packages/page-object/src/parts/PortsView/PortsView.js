c19921f8a789bc36991b047220688d22baabd369
Simon Siefke <simon.siefke@gmail.com>
2025-07-21 22:43:49 +0200

    improve ports pageobject

--- packages/page-object/src/parts/PortsView/PortsView.js
+++ packages/page-object/src/parts/PortsView/PortsView.js
@@ -1,15 +1,26 @@
+import * as QuickPick from '../QuickPick/QuickPick.js'
+import * as WellKnownCommands from '../WellKnownCommands/WellKnownCommands.js'
+import * as Panel from '../Panel/Panel.js'
+
 export const create = ({ expect, page, VError }) => {
   return {
     async open() {
       try {
-        // TODO
+        const quickPick = QuickPick.create({ expect, page, VError })
+        await quickPick.executeCommand(WellKnownCommands.FocusPortsView)
+        const portsView = page.locator('#\\~remote\\.forwardedPortsContainer')
+        await expect(portsView).toBeVisible()
+        await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to open ports view`)
       }
     },
     async close() {
       try {
-        // TODO close panel
+        const portsView = page.locator('#\\~remote\\.forwardedPortsContainer')
+        await expect(portsView).toBeVisible()
+        const panel = Panel.create({ expect, page, VError })
+        await panel.hide()
       } catch (error) {
         throw new VError(error, `Failed to close ports view`)
       }
