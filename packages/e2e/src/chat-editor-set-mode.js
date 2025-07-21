cef8ebcc5695dd59bb06ec65850c5f868006c4f3
Simon Siefke <simon.siefke@gmail.com>
2025-07-21 23:42:24 +0200

    fix: tear down chat editors (#1490)

--- packages/e2e/src/chat-editor-set-mode.js
+++ packages/e2e/src/chat-editor-set-mode.js
@@ -9,3 +9,7 @@ export const run = async ({ ChatEditor }) => {
   await ChatEditor.setMode('Edit')
   await ChatEditor.setMode('Ask')
 }
+
+export const teardown = async ({ Editor }) => {
+  await Editor.closeAll()
+}
