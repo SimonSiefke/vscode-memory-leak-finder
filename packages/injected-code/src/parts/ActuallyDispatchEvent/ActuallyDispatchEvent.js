22b247efed0d0be6e5c8c08d4a180be1a2df2e01
Simon Siefke <simon.siefke@gmail.com>
2025-07-22 15:31:15 +0200

    Discard changes to packages/injected-code/src/parts/ActuallyDispatchEvent/ActuallyDispatchEvent.js

--- packages/injected-code/src/parts/ActuallyDispatchEvent/ActuallyDispatchEvent.js
+++ packages/injected-code/src/parts/ActuallyDispatchEvent/ActuallyDispatchEvent.js
@@ -3,13 +3,5 @@ import * as GetEventClass from '../GetEventClass/GetEventClass.js'
 export const actuallyDispatchEvent = (element, eventType, options) => {
   const constructor = GetEventClass.getEventClass(eventType)
   const event = new constructor(eventType, options)
-  console.log({
-    dispatch: true,
-    eventType,
-    options,
-    element,
-    constructor,
-    event,
-  })
   element.dispatchEvent(event)
 }
