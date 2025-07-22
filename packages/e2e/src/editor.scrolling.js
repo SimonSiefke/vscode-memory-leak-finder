88d15eb427c6f6e4f02960fc9652339a0fcd690e
Simon Siefke <simon.siefke@gmail.com>
2025-07-22 14:27:59 +0200

    Discard changes to packages/e2e/src/editor.scrolling.js

--- packages/e2e/src/editor.scrolling.js
+++ packages/e2e/src/editor.scrolling.js
@@ -1,4 +1,4 @@
-// export const skip = true
+export const skip = true
 
 const generateFileContent = () => {
   return Array(200).fill('sample text').join('\n')
@@ -15,11 +15,42 @@ export const setup = async ({ Editor, Workspace }) => {
   await Editor.open('file.txt')
 }
 
-export const run = async ({ Editor }) => {
-  await Editor.moveScrollBar(200, 23)
-  await Editor.moveScrollBar(0, 0)
+export const run = async ({ page, expect }) => {
+  const editor = page.locator('.editor-instance')
+  await expect(editor).toBeVisible()
+  const scrollbar = editor.locator('.scrollbar.vertical').first()
+  await scrollbar.hover()
+  const scrollbarSlider = scrollbar.locator('.slider')
+
+  const elementBox1 = await scrollbarSlider.boundingBox()
+  if (!elementBox1) {
+    throw new Error('Unable to find bounding box on element')
+  }
+
+  const elementCenterX = elementBox1.x + elementBox1.width / 2
+  const elementCenterY = elementBox1.y + elementBox1.height / 2
+
+  const xOffset = 0
+  const yOffset = 200
+
+  await scrollbarSlider.hover()
+  await page.mouse.down()
+  await page.mouse.move(elementCenterX + xOffset, elementCenterY + yOffset)
+  await page.mouse.up()
+
+  const elementBox2 = await scrollbarSlider.boundingBox()
+  if (!elementBox2) {
+    throw new Error('Unable to find bounding box on element')
+  }
+
+  const elementCenterX2 = elementBox1.x + elementBox1.width / 2
+  const elementCenterY2 = elementBox1.y + elementBox1.height / 2
+
+  await page.mouse.down()
+  await page.mouse.move(elementCenterX2 + xOffset, elementCenterY2 - yOffset)
+  await page.mouse.up()
 }
 
 export const teardown = async ({ Editor }) => {
-  // await Editor.closeAll()
+  await Editor.closeAll()
 }
