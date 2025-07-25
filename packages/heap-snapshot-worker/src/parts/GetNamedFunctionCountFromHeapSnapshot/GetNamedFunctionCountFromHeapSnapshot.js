cde497bdd4096f0698d5de68760d0915adac898d
Simon Siefke <simon.siefke@gmail.com>
2025-07-25 22:36:21 +0200

    add sourcemap url

--- packages/heap-snapshot-worker/src/parts/GetNamedFunctionCountFromHeapSnapshot/GetNamedFunctionCountFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetNamedFunctionCountFromHeapSnapshot/GetNamedFunctionCountFromHeapSnapshot.js
@@ -6,11 +6,12 @@ import * as HeapSnapshotState from '../HeapSnapshotState/HeapSnapshotState.js'
 const normalizeFunctionObjects = (functionObjects) => {
   const normalized = []
   for (const functionObject of functionObjects) {
-    const { url, lineIndex, columnIndex, name } = functionObject
+    const { url, lineIndex, columnIndex, name, sourceMapUrl } = functionObject
     const displayUrl = `${url}:${lineIndex}:${columnIndex}`
     normalized.push({
       url: displayUrl,
       name,
+      sourceMapUrl,
     })
   }
   return normalized
