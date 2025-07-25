f3a5e53a43d5033485435cf92cfaa400aa3a6805
Simon Siefke <simon.siefke@gmail.com>
2025-07-25 16:15:31 +0200

    feature: parse heapsnapshot locations (#1505)

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshot/ParseHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshot/ParseHeapSnapshot.js
@@ -1,8 +1,18 @@
 import * as ParseHeapSnapshotInternal from '../ParseHeapSnapshotInternal/ParseHeapSnapshotInternal.js'
 
 export const parseHeapSnapshot = (heapsnapshot) => {
-  const { snapshot, nodes, edges, strings } = heapsnapshot
+  const { snapshot, nodes, edges, strings, locations } = heapsnapshot
   const { meta } = snapshot
-  const { node_types, node_fields, edge_types, edge_fields } = meta
-  return ParseHeapSnapshotInternal.parseHeapSnapshotInternal(nodes, node_fields, node_types[0], edges, edge_fields, edge_types[0], strings)
+  const { node_types, node_fields, edge_types, edge_fields, location_fields } = meta
+  return ParseHeapSnapshotInternal.parseHeapSnapshotInternal(
+    nodes,
+    node_fields,
+    node_types[0],
+    edges,
+    edge_fields,
+    edge_types[0],
+    strings,
+    locations,
+    location_fields,
+  )
 }
