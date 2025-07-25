48d87c91a25f7d510acfe01eef03f04f63ece8ca
Simon Siefke <simon.siefke@gmail.com>
2025-07-25 13:46:41 +0200

    feature: parse heapsnapshot locations

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternal/ParseHeapSnapshotInternal.js
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternal/ParseHeapSnapshotInternal.js
@@ -3,8 +3,19 @@ import * as CleanNodes from '../CleanNodes/CleanNodes.js'
 import * as ParseHeapSnapshotInternalEdges from '../ParseHeapSnapshotInternalEdges/ParseHeapSnapshotInternalEdges.js'
 import * as ParseHeapSnapshotInternalGraph from '../ParseHeapSnapshotInternalGraph/ParseHeapSnapshotInternalGraph.js'
 import * as ParseHeapSnapshotInternalNodes from '../ParseHeapSnapshotInternalNodes/ParseHeapSnapshotInternalNodes.js'
+import * as ParseHeapSnapshotLocations from '../ParseHeapSnapshotLocations/ParseHeapSnapshotLocations.js'
 
-export const parseHeapSnapshotInternal = (nodes, nodeFields, nodeTypes, edges, edgeFields, edgeTypes, strings) => {
+export const parseHeapSnapshotInternal = (
+  nodes,
+  nodeFields,
+  nodeTypes,
+  edges,
+  edgeFields,
+  edgeTypes,
+  strings,
+  locations,
+  locationFields,
+) => {
   Assert.array(nodes)
   Assert.array(nodeFields)
   Assert.array(nodeTypes)
@@ -12,6 +23,8 @@ export const parseHeapSnapshotInternal = (nodes, nodeFields, nodeTypes, edges, e
   Assert.array(edgeFields)
   Assert.array(edgeTypes)
   Assert.array(strings)
+  Assert.array(locations)
+  Assert.array(locationFields)
   const parsedNodes = ParseHeapSnapshotInternalNodes.parseHeapSnapshotInternalNodes(nodes, nodeFields, nodeTypes, strings)
   const parsedEdges = ParseHeapSnapshotInternalEdges.parseHeapSnapshotInternalEdges(
     edges,
@@ -22,8 +35,10 @@ export const parseHeapSnapshotInternal = (nodes, nodeFields, nodeTypes, edges, e
   )
   const graph = ParseHeapSnapshotInternalGraph.parseHeapSnapshotInternalGraph(parsedNodes, parsedEdges)
   const cleanNodes = CleanNodes.cleanNode(parsedNodes)
+  const parsedLocations = ParseHeapSnapshotLocations.parseHeapSnapshotLocations(locations, locationFields, nodeFields.length)
   return {
     parsedNodes: cleanNodes,
     graph,
+    locations: parsedLocations,
   }
 }
