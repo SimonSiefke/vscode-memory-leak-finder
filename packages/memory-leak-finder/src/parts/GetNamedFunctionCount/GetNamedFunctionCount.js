455b1fe8346e9ec195d51e7c0d63c909fdd09ed5
Simon Siefke <simon.siefke@gmail.com>
2025-07-25 12:15:27 +0200

    filter out classes also

--- packages/memory-leak-finder/src/parts/GetNamedFunctionCount/GetNamedFunctionCount.js
+++ packages/memory-leak-finder/src/parts/GetNamedFunctionCount/GetNamedFunctionCount.js
@@ -1,4 +1,5 @@
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.js'
+import { filterNamedFunctions } from '../FilterNamedFunctions/FilterNamedFunctions.js'
 import * as GetDescriptorValues from '../GetDescriptorValues/GetDescriptorValues.js'
 import * as GetFunctionObjectIds from '../GetFunctionObjectIds/GetFunctionObjectIds.js'
 import * as GetNamedFunctionLocations from '../GetNamedFunctionLocations/GetNamedFunctionLocations.js'
@@ -18,37 +19,14 @@ export const getNamedFunctionCount = async (session, objectGroup, scriptMap, inc
     prototypeObjectId: prototypeDescriptor.objectId,
     objectGroup,
   })
-  const fnResult2 = await DevtoolsProtocolRuntime.callFunctionOn(session, {
-    functionDeclaration: `function(){
-  const functions = this
-
-  const getDescription = fn => {
-    try {
-      return fn?.toString()
-    } catch {
-      return 'toString error'
-    }
-  }
-
-  const isNonNative = fn => {
-    const description = getDescription(fn)
-    return !description.includes('[native code]')
-  }
-
-  const nonNativeFunctions = functions.filter(isNonNative)
-  return nonNativeFunctions
-}`,
-    objectId: objects.objects.objectId,
-    returnByValue: false,
-    objectGroup,
-  })
-
+  const fnResult2 = await filterNamedFunctions(session, objects, objectGroup)
   const fnResult3 = await DevtoolsProtocolRuntime.getProperties(session, {
     objectId: fnResult2.objectId,
     generatePreview: false,
     ownProperties: true,
   })
   const descriptors = GetDescriptorValues.getDescriptorValues(fnResult3.result)
+  const descriptorsJson = JSON.stringify(descriptors, null, 2) + '\n'
   const functionObjectIds = GetFunctionObjectIds.getFunctionObjectIds(descriptors)
   const functionLocations = await GetNamedFunctionLocations.getNamedFunctionLocations(
     session,
