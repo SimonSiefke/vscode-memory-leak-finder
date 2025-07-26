bb6ff68fb6a3dfe6495cac39ad1d2abfb4ee2926
Simon Siefke <simon.siefke@gmail.com>
2025-07-26 15:34:40 +0200

    feature: remove ./ when computing source map

--- packages/load-source-map-worker/src/parts/CleanSource/CleanSource.js
+++ packages/load-source-map-worker/src/parts/CleanSource/CleanSource.js
@@ -8,4 +8,5 @@ export const cleanSource = (source) => {
     .replace('out-vscode/vs/workbench/file:/mnt/vss/_work/1/s', '')
     .replace('file:/Users/cloudtest/vss/_work/1/s/', '')
     .replace(RE_VS, './src/vs/')
+    .replace('./src', 'src')
 }
