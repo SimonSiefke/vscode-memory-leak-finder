c68cc4f18bdd186337dd2da31586cdc4fcc4a948
Simon Siefke <simon.siefke@gmail.com>
2025-07-26 10:07:20 +0200

    feature: move aggregate into separate file (#1510)

--- packages/heap-snapshot-worker/src/parts/GetNamedFunctionCountFromHeapSnapshot/GetNamedFunctionCountFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetNamedFunctionCountFromHeapSnapshot/GetNamedFunctionCountFromHeapSnapshot.js
@@ -2,50 +2,14 @@ import * as Assert from '../Assert/Assert.js'
 import { getFunctionsWithLocations } from '../GetFunctionsWithLocations/GetFunctionsWithLocations.js'
 import * as ParseHeapSnapshot from '../ParseHeapSnapshot/ParseHeapSnapshot.js'
 import * as HeapSnapshotState from '../HeapSnapshotState/HeapSnapshotState.js'
-
-const normalizeFunctionObjects = (functionObjects) => {
-  const normalized = []
-  for (const functionObject of functionObjects) {
-    const { url, lineIndex, columnIndex, name, sourceMapUrl } = functionObject
-    const displayUrl = `${url}:${lineIndex}:${columnIndex}`
-    normalized.push({
-      url: displayUrl,
-      name,
-      sourceMapUrl,
-    })
-  }
-  return normalized
-}
-
-const aggregateFunctionObjects = (functionObjects) => {
-  const map = Object.create(null)
-  for (const { url } of functionObjects) {
-    map[url] ||= 0
-    map[url]++
-  }
-  const seen = Object.create(null)
-  const aggregated = []
-  for (const { url, sourceMapUrl, name } of functionObjects) {
-    if (url in seen) {
-      continue
-    }
-    seen[url] = true
-    aggregated.push({
-      name,
-      url,
-      sourceMapUrl,
-      count: map[url],
-    })
-  }
-  aggregated.sort((a, b) => b.count - a.count)
-  return aggregated
-}
+import { normalizeFunctionObjects } from '../NormalizeFunctionObjects/NormalizeFunctionObjects.js'
+import { aggregateFunctionObjects } from '../AggregateFunctionObjects/AggregateFunctionObjects.js'
 
 export const getNamedFunctionCountFromHeapSnapshot = async (id, scriptMap) => {
   const heapsnapshot = HeapSnapshotState.get(id)
   Assert.object(heapsnapshot)
   console.time('parse')
-  const { parsedNodes, graph, locations } = ParseHeapSnapshot.parseHeapSnapshot(heapsnapshot)
+  const { parsedNodes, locations } = ParseHeapSnapshot.parseHeapSnapshot(heapsnapshot)
   console.timeEnd('parse')
   console.time('getLocations')
   const functionsWithLocations = getFunctionsWithLocations(parsedNodes, locations, scriptMap)
@@ -56,7 +20,10 @@ export const getNamedFunctionCountFromHeapSnapshot = async (id, scriptMap) => {
   console.time('aggregate')
   const aggregated = aggregateFunctionObjects(normalized)
   console.timeEnd('aggregate')
-  return aggregated
+  console.time('sort')
+  const sorted = aggregated.toSorted((a, b) => b.count - a.count)
+  console.timeEnd('sort')
+  return sorted
 }
 
 // const path = join(import.meta.dirname, '../../../../../.vscode-heapsnapshots', '1.json')
