8780687b23334ded61487a93d21275214e373d04
Simon Siefke <simon.siefke@gmail.com>
2025-07-26 15:17:44 +0200

    feature: normalize sourcemap when downloading it (#1512)

---
+++ packages/load-source-map-worker/src/parts/DownloadSourceMap/DownloadSourceMap.js
@@ -0,0 +1,10 @@
+import got from 'got'
+import { createWriteStream } from 'node:fs'
+import { mkdir } from 'node:fs/promises'
+import { dirname } from 'node:path'
+import { pipeline } from 'node:stream/promises'
+
+export const downloadSourceMap = async (url, outFilePath) => {
+  await mkdir(dirname(outFilePath), { recursive: true })
+  await pipeline(got.stream(url), createWriteStream(outFilePath))
+}
---
+++ packages/load-source-map-worker/src/parts/NormalizeSourceMap/NormalizeSourceMap.js
@@ -0,0 +1,29 @@
+import { mkdir, writeFile } from 'fs/promises'
+import path, { basename, dirname, join, resolve } from 'path'
+import { cleanSource } from '../CleanSource/CleanSource.js'
+import { readJson } from '../ReadJson/ReadJson.js'
+
+export const normalizeSourceMap = async (originalPath, outFilePath) => {
+  const data = await readJson(originalPath)
+  const cleanSources = data.sources.map(cleanSource)
+  const outDirName = path.dirname(path.dirname(originalPath))
+  const hash = basename(originalPath).replace('.js.map.original', '')
+  const sourcesPath = join(outDirName, '.vscode-sources', hash)
+  for (let i = 0; i < data.sourcesContent.length; i++) {
+    const source = cleanSources[i]
+    const sourcePath = resolve(sourcesPath, source)
+    if (!sourcePath.startsWith(sourcesPath)) {
+      console.log({ sourcesPath, sourcePath })
+      throw new Error(`cannot write to file outside of source: ${sourcePath}`)
+    }
+    await mkdir(dirname(sourcePath), { recursive: true })
+    await writeFile(sourcePath, data.sourcesContent[i])
+  }
+  const newData = {
+    ...data,
+    sources: cleanSources,
+    sourcesContent: [],
+  }
+  await writeFile(outFilePath, JSON.stringify(newData, null, 2) + '\n')
+  // TODO clean names also, some names dont seems to be quite correct
+}
