6fcd7d6285a05ab255493cbfc19b40691df85d4c
Simon Siefke <simon.siefke@gmail.com>
2025-07-26 09:31:27 +0200

    feature: clean source

--- packages/source-map-worker/src/parts/CleanSource/CleanSource.js
+++ packages/source-map-worker/src/parts/CleanSource/CleanSource.js
@@ -2,5 +2,5 @@ export const cleanSource = (source) => {
   if (!source) {
     return ''
   }
-  return source.replace('out-vscode/vs/workbench/file:/mnt/vss/_work/1/s', '')
+  return source.replace('out-vscode/vs/workbench/file:/mnt/vss/_work/1/s', '').replace('file:/Users/cloudtest/vss/_work/1/s/', '')
 }
