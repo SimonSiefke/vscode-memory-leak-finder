7c42667c9127c1a4a7ababfe33114ac10bf72ca3
Simon Siefke <simon.siefke@gmail.com>
2025-07-28 11:57:56 +0200

    feature: improve cleaning source

--- packages/load-source-map-worker/src/parts/CleanSource/CleanSource.js
+++ packages/load-source-map-worker/src/parts/CleanSource/CleanSource.js
@@ -4,6 +4,10 @@ export const cleanSource = (source) => {
   if (!source) {
     return ''
   }
+  const vscodeIndex = source.indexOf('vscode')
+  if (vscodeIndex !== -1) {
+    return source.slice(vscodeIndex + 'vscode'.length + 1)
+  }
   return source
     .replace('out-vscode/vs/workbench/file:/mnt/vss/_work/1/s', '')
     .replace('file:/Users/cloudtest/vss/_work/1/s/', '')
