4793c19c8758f3879c0abdafedd491485f5c3cec
Simon Siefke <simon.siefke@gmail.com>
2025-07-31 00:07:25 +0200

    feature: improve named function count chart

---
+++ packages/charts/src/parts/CreateNamedFunctionCountChart/CreateNamedFunctionCountChart.js
@@ -0,0 +1,99 @@
+import { fixHtmlNamespace } from '../FixXmlNamespace/FixXmlNamespace.js'
+import * as GetNamedFunctionCountData from '../GetNamedFunctionCountData/GetNamedFunctionCountData.js'
+import * as Plot from '../Plot/Plot.js'
+
+export const name = 'named-function-count'
+
+export const getData = GetNamedFunctionCountData.getNamedFunctionCountData
+
+export const createChart = (data) => {
+  const x = 0
+  const y = 0
+  const baseHtml = Plot.plot({
+    style: 'overflow: visible;background:white',
+    marginLeft: 150,
+    labelArrow: false,
+    // zer
+    // axis: 'top',
+
+    y: {
+      // grid: true,
+      label: 'Name',
+      domain: data.map((d) => d.name),
+      inset: 0,
+
+      // padding: 20,
+    },
+    x: {
+      label: 'Count',
+      inset: 0,
+    },
+    marks: [
+      Plot.rectX(data, {
+        x: 'delta',
+        y: 'name',
+        fill: 'black',
+        rx1: 2,
+        rx2: 2,
+        strokeWidth: 2,
+        fillOpacity: 0.75,
+        inset: 3,
+        // y1: 55,
+      }),
+      Plot.axisY({
+        textAnchor: 'end',
+        fill: 'black',
+        fontSize: 7,
+        tickSize: 0,
+        dx: 5,
+      }),
+      Plot.axisX({
+        // textAnchor: 'end',
+        // fill: 'black',
+        fontSize: 8,
+        tickSize: 0,
+        // dx: 5,
+      }),
+      // Plot.axisY({ label: 'Responses (%)' }),
+      // Plot.text(data, {
+      //   x: 'delta',
+      //   y: 'name',
+      //   text: 'name',
+      //   textAnchor: 'end',
+      //   dx: -3,
+      //   fill: 'snow',
+      //   fontSize: 3,
+      //   fontWeight: 400,
+      //   dy: 0,
+      // }),
+      // Plot.text(data, {
+      //   x: 'delta',
+      //   y: 'name',
+      //   text: (data, index) => {
+      //     return `Delta: ${data.delta}`
+      //   },
+      //   textAnchor: 'end',
+      //   dx: -3,
+      //   fill: 'snow',
+      //   fontSize: 3,
+      //   fontWeight: 400,
+      //   dy: 3,
+      // }),
+      // Plot.text(data, {
+      //   x: 'delta',
+      //   y: 'name',
+      //   text: (data, index) => {
+      //     return `src/vs/base/common/myfile.ts:21`
+      //   },
+      //   textAnchor: 'end',
+      //   dx: -3,
+      //   fill: 'snow',
+      //   fontSize: 1.5,
+      //   fontWeight: 400,
+      //   dy: -3,
+      // }),
+    ],
+  }).outerHTML
+  const finalHtml = fixHtmlNamespace(baseHtml)
+  return finalHtml
+}
