7388077d76ab88811678b367cad78e5c74addf54
Simon Siefke <simon.siefke@gmail.com>
2025-07-31 18:20:01 +0200

    Discard changes to packages/heap-snapshot-worker/src/parts/LoadHeapSnapshot/LoadHeapSnapshot.js

--- packages/heap-snapshot-worker/src/parts/LoadHeapSnapshot/LoadHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/LoadHeapSnapshot/LoadHeapSnapshot.js
@@ -19,8 +19,6 @@ const makeMemoryEfficient = (heapSnapshot) => {
 export const loadHeapSnapshot = async (path) => {
   const content = await readFile(path, 'utf8')
   const value = JSON.parse(content)
-  const mergedStrings = value.strings.join('\n')
-  value.merged = mergedStrings
-  // const efficient = makeMemoryEfficient(value)
-  HeapSnapshotState.set(path, value)
+  const efficient = makeMemoryEfficient(value)
+  HeapSnapshotState.set(path, efficient)
 }
