c91adf157d4ccb36d55e689552b75b1f30d04551
Simon Siefke <simon.siefke@gmail.com>
2025-07-31 17:18:58 +0200

    import

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotArray/ParseHeapSnapshotArray.js
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotArray/ParseHeapSnapshotArray.js
@@ -1,5 +1,3 @@
-import { parseHeapSnapshotMetaDataIndices } from '../ParseHeapSnapshotMetaDataIndices/ParseHeapSnapshotMetaDataIndices.js'
-
 // Pre-computed lookup tables for faster character classification
 const charTypes = new Uint8Array(128) // ASCII characters 0-127
 const DIGIT = 1
@@ -9,11 +7,14 @@ const OTHER = 0
 
 // Initialize lookup table
 for (let i = 0; i < 128; i++) {
-  if (i >= 48 && i <= 57) { // '0' to '9'
+  if (i >= 48 && i <= 57) {
+    // '0' to '9'
     charTypes[i] = DIGIT
-  } else if (i === 44 || i === 32 || i === 9 || i === 10) { // comma, space, tab, newline
+  } else if (i === 44 || i === 32 || i === 9 || i === 10) {
+    // comma, space, tab, newline
     charTypes[i] = SEPARATOR
-  } else if (i === 93) { // ']'
+  } else if (i === 93) {
+    // ']'
     charTypes[i] = CLOSING_BRACKET
   } else {
     charTypes[i] = OTHER
