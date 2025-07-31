057c86cf1b44b59763c651db3ecc86d441a35b68
Simon Siefke <simon.siefke@gmail.com>
2025-07-31 17:28:47 +0200

    move lookup table

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotArray/ParseHeapSnapshotArray.js
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotArray/ParseHeapSnapshotArray.js
@@ -1,25 +1,11 @@
+import { createCharTypesLookupTable } from '../CreateCharTypesLookupTable/CreateCharTypesLookupTable.js'
+
 // Pre-computed lookup tables for faster character classification
-const charTypes = new Uint8Array(128) // ASCII characters 0-127
+const charTypes = createCharTypesLookupTable()
 const DIGIT = 1
 const SEPARATOR = 2
 const CLOSING_BRACKET = 3
 const OTHER = 0
-
-// Initialize lookup table
-for (let i = 0; i < 128; i++) {
-  if (i >= 48 && i <= 57) {
-    // '0' to '9'
-    charTypes[i] = DIGIT
-  } else if (i === 44 || i === 32 || i === 9 || i === 10) {
-    // comma, space, tab, newline
-    charTypes[i] = SEPARATOR
-  } else if (i === 93) {
-    // ']'
-    charTypes[i] = CLOSING_BRACKET
-  } else {
-    charTypes[i] = OTHER
-  }
-}
 
 /**
  * Parses comma-separated numbers from a Uint8Array buffer into a Uint32Array
