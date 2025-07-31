47c78e520edbb04b4a802be87e8ced8ab8695dcb
Simon Siefke <simon.siefke@gmail.com>
2025-07-31 17:31:04 +0200

    simplify

--- packages/heap-snapshot-worker/src/parts/CreateCharTypesLookupTable/CreateCharTypesLookupTable.js
+++ packages/heap-snapshot-worker/src/parts/CreateCharTypesLookupTable/CreateCharTypesLookupTable.js
@@ -4,15 +4,24 @@ const SEPARATOR = 2
 const CLOSING_BRACKET = 3
 const OTHER = 0
 
+// Character code constants for better readability
+const CHAR_0 = '0'.charCodeAt(0)
+const CHAR_9 = '9'.charCodeAt(0)
+const CHAR_COMMA = ','.charCodeAt(0)
+const CHAR_SPACE = ' '.charCodeAt(0)
+const CHAR_TAB = '\t'.charCodeAt(0)
+const CHAR_NEWLINE = '\n'.charCodeAt(0)
+const CHAR_CLOSING_BRACKET = ']'.charCodeAt(0)
+
 // Initialize lookup table
 for (let i = 0; i < 128; i++) {
-  if (i >= 48 && i <= 57) {
+  if (i >= CHAR_0 && i <= CHAR_9) {
     // '0' to '9'
     charTypes[i] = DIGIT
-  } else if (i === 44 || i === 32 || i === 9 || i === 10) {
+  } else if (i === CHAR_COMMA || i === CHAR_SPACE || i === CHAR_TAB || i === CHAR_NEWLINE) {
     // comma, space, tab, newline
     charTypes[i] = SEPARATOR
-  } else if (i === 93) {
+  } else if (i === CHAR_CLOSING_BRACKET) {
     // ']'
     charTypes[i] = CLOSING_BRACKET
   } else {
