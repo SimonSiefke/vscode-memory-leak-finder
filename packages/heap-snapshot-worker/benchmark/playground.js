87863d3d8369f4e4be03b475f249736173c34f12
Simon Siefke <simon.siefke@gmail.com>
2025-07-31 17:16:25 +0200

    move

---
+++ packages/heap-snapshot-worker/benchmark/playground.js
@@ -0,0 +1,14 @@
+import { join } from 'path'
+import { commandMap } from '../src/parts/CommandMap/CommandMap.js'
+import { loadHeapSnapshot2 } from '../src/parts/LoadHeapSnapshot2/LoadHeapSnapshot2.js'
+
+const main = async () => {
+  const path = join(import.meta.dirname, '../../../.vscode-heapsnapshots', '1.json')
+  console.time('load')
+  console.log(process.memoryUsage())
+  await loadHeapSnapshot2(path)
+  console.log(process.memoryUsage())
+  console.timeEnd('load')
+}
+
+main()
