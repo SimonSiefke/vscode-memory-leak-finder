4109e3334646638539a119872e1c039b9e63e0a1
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 17:17:16 +0200

    debug

--- packages/heap-snapshot-worker/src/parts/GetLocationFieldOffsets/GetLocationFieldOffsets.js
+++ packages/heap-snapshot-worker/src/parts/GetLocationFieldOffsets/GetLocationFieldOffsets.js
@@ -1,1 +1,14 @@
- 
+export const getLocationFieldOffsets = (locationFields) => {
+  const objectIndexOffset = locationFields.indexOf('object_index')
+  const scriptIdOffset = locationFields.indexOf('script_id')
+  const lineOffset = locationFields.indexOf('line')
+  const columnOffset = locationFields.indexOf('column')
+
+  return {
+    itemsPerLocation: locationFields.length,
+    objectIndexOffset,
+    scriptIdOffset,
+    lineOffset,
+    columnOffset,
+  }
+}
