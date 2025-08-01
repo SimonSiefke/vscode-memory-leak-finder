8c31abb4ead4a4ff48501ffa7d8e624e03f16dfe
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 09:14:53 +0200

    fix index

--- packages/heap-snapshot-worker/src/parts/GetUniqueLocationsResult/GetUniqueLocationsResult.js
+++ packages/heap-snapshot-worker/src/parts/GetUniqueLocationsResult/GetUniqueLocationsResult.js
@@ -5,17 +5,19 @@ export const getUniqueLocationsResult = (locationMap, locations) => {
   const values = Object.values(locationMap)
 
   const result = new Uint32Array(values.length * ITEMS_PER_RESULT)
+  let resultIndex = 0
   for (const value of values) {
     const { count, index } = value
     const objectIndex = locations[index * ITEMS_PER_LOCATION]
     const scriptId = locations[index * ITEMS_PER_LOCATION + 1]
     const line = locations[index * ITEMS_PER_LOCATION + 2]
     const column = locations[index * ITEMS_PER_LOCATION + 3]
-    result[index * ITEMS_PER_RESULT] = objectIndex
-    result[index * ITEMS_PER_RESULT + 1] = scriptId
-    result[index * ITEMS_PER_RESULT + 2] = line
-    result[index * ITEMS_PER_RESULT + 3] = column
-    result[index * ITEMS_PER_RESULT + 4] = count
+    result[resultIndex * ITEMS_PER_RESULT] = objectIndex
+    result[resultIndex * ITEMS_PER_RESULT + 1] = scriptId
+    result[resultIndex * ITEMS_PER_RESULT + 2] = line
+    result[resultIndex * ITEMS_PER_RESULT + 3] = column
+    result[resultIndex * ITEMS_PER_RESULT + 4] = count
+    resultIndex++
   }
 
   return result
