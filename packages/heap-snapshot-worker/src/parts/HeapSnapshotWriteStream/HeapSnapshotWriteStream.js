36a8c5d5526c6a4024cbb637caccdd99df8c80b6
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 05:54:07 +0200

    feature: improve performance for parsing heapsnapshots (#1526)

--- packages/heap-snapshot-worker/src/parts/HeapSnapshotWriteStream/HeapSnapshotWriteStream.js
+++ packages/heap-snapshot-worker/src/parts/HeapSnapshotWriteStream/HeapSnapshotWriteStream.js
@@ -1,12 +1,11 @@
+// based on chrome devtools HeapSnapshotLoader.ts (https://github.com/ChromeDevTools/devtools-frontend/blob/main/front_end/entrypoints/heap_snapshot_worker/HeapSnapshotLoader.ts), BSD-3-Clause license
+
 import { Writable } from 'node:stream'
+import { concatArray, concatUint32Array } from '../ConcatArray/ConcatArray.js'
 import * as HeapSnapshotParsingState from '../HeapSnapshotParsingState/HeapSnapshotParsingState.js'
+import { parseHeapSnapshotArray } from '../ParseHeapSnapshotArray/ParseHeapSnapshotArray.js'
 import { parseHeapSnapshotArrayHeader } from '../ParseHeapSnapshotArrayHeader/ParseHeapSnapshotArrayHeader.js'
 import { EMPTY_DATA, parseHeapSnapshotMetaData } from '../ParseHeapSnapshotMetaData/ParseHeapSnapshotMetaData.js'
-import { parseHeapSnapshotArray } from '../ParseHeapSnapshotArray/ParseHeapSnapshotArray.js'
-
-const concatArray = (array, other) => {
-  return new Uint8Array(Buffer.concat([array, other]))
-}
 
 const decodeArray = (data) => {
   return new TextDecoder().decode(data)
@@ -14,16 +13,15 @@ const decodeArray = (data) => {
 
 export class HeapSnapshotWriteStream extends Writable {
   constructor() {
-    super({
-      objectMode: true,
-    })
-    this.state = HeapSnapshotParsingState.SearchingSnapshotMetaData
+    super()
+    this.arrayIndex = 0
     this.data = new Uint8Array()
-    this.snapshotTokenIndex = -1
+    this.edges = new Uint32Array()
+    this.intermediateArray = new Uint32Array(this.writableHighWaterMark)
+    this.locations = new Uint32Array()
     this.metaData = {}
     this.nodes = new Uint32Array()
-    this.edges = new Uint32Array()
-    this.arrayIndex = 0
+    this.state = HeapSnapshotParsingState.SearchingSnapshotMetaData
   }
 
   writeMetaData(chunk) {
@@ -33,12 +31,12 @@ export class HeapSnapshotWriteStream extends Writable {
     if (metaData === EMPTY_DATA) {
       return
     }
-    const nodeCount = metaData.data.node_count
-    const edgeCount = metaData.data.edge_count
-    this.metaData = metaData
+    const nodeCount = metaData.data.node_count * metaData.data.meta.node_fields.length
+    const edgeCount = metaData.data.edge_count * metaData.data.meta.edge_fields.length
     this.data = this.data.slice(metaData.endIndex)
-    this.nodes = new Uint32Array(nodeCount)
     this.edges = new Uint32Array(edgeCount)
+    this.metaData = metaData
+    this.nodes = new Uint32Array(nodeCount)
     this.state = HeapSnapshotParsingState.ParsingNodesMetaData
   }
 
@@ -46,12 +44,11 @@ export class HeapSnapshotWriteStream extends Writable {
     this.data = concatArray(this.data, chunk)
     const dataString = decodeArray(this.data)
     const endIndex = parseHeapSnapshotArrayHeader(dataString, nodeName)
-    console.log({ endIndex })
     if (endIndex === -1) {
       return
     }
+    this.arrayIndex = 0
     this.data = this.data.slice(endIndex)
-    console.log(decodeArray(this.data.slice(0, 100)))
     this.state = nextState
   }
 
@@ -60,14 +57,20 @@ export class HeapSnapshotWriteStream extends Writable {
   }
 
   writeArrayData(chunk, array, nextState) {
-    this.data = concatArray(this.data, chunk)
+    this.data = concatArray(this.data, chunk) // TODO try to avoid concatenating arrays
     const { dataIndex, arrayIndex, done } = parseHeapSnapshotArray(this.data, array, this.arrayIndex)
     if (dataIndex === -1) {
       return
     }
+    if (arrayIndex > array.length) {
+      throw new RangeError(`Array index ${arrayIndex} is out of bounds for array of length ${array.length}`)
+    }
     this.arrayIndex = arrayIndex
     this.data = this.data.slice(dataIndex)
     if (done) {
+      if (arrayIndex !== array.length) {
+        throw new RangeError(`Incorrect number of nodes in heapsnapshot, expected ${array.length}, but got ${arrayIndex}`)
+      }
       this.state = nextState
     }
   }
@@ -81,11 +84,35 @@ export class HeapSnapshotWriteStream extends Writable {
   }
 
   writeParsingEdges(chunk) {
-    this.writeArrayData(chunk, this.edges, HeapSnapshotParsingState.Done)
-    // this.state = HeapSnapshotParsingState.Done
+    this.writeArrayData(chunk, this.edges, HeapSnapshotParsingState.ParsingLocationsMetaData)
   }
 
-  async _write(chunk, encoding, callback) {
+  writeParsingLocationsMetaData(chunk) {
+    this.writeParsingArrayMetaData(chunk, 'locations', HeapSnapshotParsingState.ParsingLocations)
+  }
+
+  writeResizableArrayData(chunk, nextState) {
+    // Parse the chunk directly - no concatenation needed due to stateful parsing
+    const { dataIndex, arrayIndex, done } = parseHeapSnapshotArray(chunk, this.intermediateArray, 0)
+    if (dataIndex === -1) {
+      return
+    }
+
+    // Concatenate the parsed numbers to the main array
+    const parsedNumbers = this.intermediateArray.slice(0, arrayIndex)
+    this.locations = concatUint32Array(this.locations, parsedNumbers)
+
+    if (done) {
+      this.data = chunk.slice(dataIndex)
+      this.state = nextState
+    }
+  }
+
+  writeParsingLocations(chunk) {
+    this.writeResizableArrayData(chunk, HeapSnapshotParsingState.Done)
+  }
+
+  handleChunk(chunk) {
     switch (this.state) {
       case HeapSnapshotParsingState.SearchingSnapshotMetaData:
         this.writeMetaData(chunk)
@@ -102,11 +129,33 @@ export class HeapSnapshotWriteStream extends Writable {
       case HeapSnapshotParsingState.ParsingEdges:
         this.writeParsingEdges(chunk)
         break
+      case HeapSnapshotParsingState.ParsingLocationsMetaData:
+        this.writeParsingLocationsMetaData(chunk)
+        break
+      case HeapSnapshotParsingState.ParsingLocations:
+        this.writeParsingLocations(chunk)
+        break
+      case HeapSnapshotParsingState.Done:
+        break
       default:
         break
     }
+  }
+
+  _write(chunk, encoding, callback) {
+    this.handleChunk(chunk)
     callback()
   }
 
-  async start() {}
+  start() {}
+
+  getResult() {
+    return {
+      metaData: this.metaData,
+      edges: this.edges,
+      nodes: this.nodes,
+      locations: this.locations,
+      // TODO parse strings?
+    }
+  }
 }
---
+++ packages/heap-snapshot-worker/src/parts/LoadHeapSnapshot2/LoadHeapSnapshot2.js
@@ -0,0 +1,26 @@
+// based on chrome devtools HeapSnapshotLoader.ts (https://github.com/ChromeDevTools/devtools-frontend/blob/main/front_end/entrypoints/heap_snapshot_worker/HeapSnapshotLoader.ts), BSD-3-Clause license
+import { createReadStream } from 'node:fs'
+import { prepareHeapSnapshot } from '../PrepareHeapSnapshot/PrepareHeapSnapshot.js'
+
+/**
+ * @param {string} path
+ * @returns {Promise<void>}
+ */
+export const loadHeapSnapshot2 = async (path) => {
+  const read = createReadStream(path)
+
+  const { metaData, nodes, edges, locations } = await prepareHeapSnapshot(read)
+
+  console.log({ locations: locations.length })
+
+  // for (const node of edges) {
+  //   if (node > 2 ** 16 - 1) {
+  //     console.log('is large', node)
+  //   }
+  // }
+  // console.log({ metaData, nodes, edges })
+
+  // TODO create lightweight facade around nodes and edges,
+  // making traversing nodes and edges fast and efficient
+  // similar to chrome devtools
+}
