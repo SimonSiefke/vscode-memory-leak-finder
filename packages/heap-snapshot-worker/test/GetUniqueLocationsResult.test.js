5384314c59e3f408feff8fc08e1f68126615c8c5
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 07:38:04 +0200

    test

---
+++ packages/heap-snapshot-worker/test/GetUniqueLocationsResult.test.js
@@ -0,0 +1,10 @@
+import { expect, test } from '@jest/globals'
+import { getUniqueLocationMap } from '../src/parts/GetUniqueLocationMap/GetUniqueLocationMap.js'
+import { getUniqueLocationsResult } from '../src/parts/GetUniqueLocationsResult/GetUniqueLocationsResult.js'
+
+test('getUniqueLocationMap - merge duplicates', () => {
+  const locations = new Uint32Array([1, 2, 3, 4, 1, 2, 3, 4])
+  const locationMap = getUniqueLocationMap(locations)
+  const result = getUniqueLocationsResult(locationMap, locations)
+  expect(result).toEqual(new Uint32Array([0, 0, 0, 2]))
+})
