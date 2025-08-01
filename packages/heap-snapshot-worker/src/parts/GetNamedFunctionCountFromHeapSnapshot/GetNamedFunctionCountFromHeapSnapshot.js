1b26a4baf64c79c582f3ecc3135683719a59e69f
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 13:06:37 +0200

    undo

--- packages/heap-snapshot-worker/src/parts/GetNamedFunctionCountFromHeapSnapshot/GetNamedFunctionCountFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetNamedFunctionCountFromHeapSnapshot/GetNamedFunctionCountFromHeapSnapshot.js
@@ -8,11 +8,21 @@ import { aggregateFunctionObjects } from '../AggregateFunctionObjects/AggregateF
 export const getNamedFunctionCountFromHeapSnapshot = async (id, scriptMap, minCount) => {
   const heapsnapshot = HeapSnapshotState.get(id)
   Assert.object(heapsnapshot)
+  console.time('parse')
   const { parsedNodes, locations } = ParseHeapSnapshot.parseHeapSnapshot(heapsnapshot)
+  console.timeEnd('parse')
+  console.time('getLocations')
   const functionsWithLocations = getFunctionsWithLocations(parsedNodes, locations, scriptMap)
+  console.timeEnd('getLocations')
+  console.time('normalize')
   const normalized = normalizeFunctionObjects(functionsWithLocations)
+  console.timeEnd('normalize')
+  console.time('aggregate')
   const aggregated = aggregateFunctionObjects(normalized)
+  console.timeEnd('aggregate')
+  console.time('sort')
   const sorted = aggregated.toSorted((a, b) => b.count - a.count)
   const limited = sorted.filter((item) => item.count >= minCount)
+  console.timeEnd('sort')
   return limited
 }
