6758ee19b7338a49ec090879b92ea40f88e82351
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 17:10:06 +0200

    simplify key

--- packages/heap-snapshot-worker/src/parts/GetUniqueLocationMap/GetUniqueLocationMap.js
+++ packages/heap-snapshot-worker/src/parts/GetUniqueLocationMap/GetUniqueLocationMap.js
@@ -1,34 +1,25 @@
+import { getLocationKey } from '../GetLocationKey/GetLocationKey.js'
+
 const ITEMS_PER_LOCATION = 4
 const scriptOffset = 1
 const lineOffset = 2
 const columnOffset = 3
 
-export const getUniqueLocationMap = (locations, scriptMap = {}) => {
+export const getUniqueLocationMap = (locations) => {
   const locationMap = Object.create(null)
-  let debugCount = 0
-
   for (let i = 0; i < locations.length; i += ITEMS_PER_LOCATION) {
-    let scriptIdIndex = locations[i + scriptOffset]
+    const scriptId = locations[i + scriptOffset]
     const lineIndex = locations[i + lineOffset]
     const columnIndex = locations[i + columnOffset]
-
-    // Get the URL from scriptMap, fallback to empty string if not found
-    const script = scriptMap[scriptIdIndex]
-    const url = script?.url || ''
-
-    // Use line:column as key to avoid script ID inconsistencies between snapshots
-    // This allows matching functions even when script IDs change between snapshots
-    const locationKey = `${lineIndex}:${columnIndex}`
-
-    if (locationKey in locationMap) {
-      locationMap[locationKey].count++
+    const key = getLocationKey(scriptId, lineIndex, columnIndex)
+    if (key in locationMap) {
+      locationMap[key].count++
     } else {
-      locationMap[locationKey] = {
+      locationMap[key] = {
         count: 1,
         index: i / ITEMS_PER_LOCATION,
       }
     }
   }
-
   return locationMap
 }
