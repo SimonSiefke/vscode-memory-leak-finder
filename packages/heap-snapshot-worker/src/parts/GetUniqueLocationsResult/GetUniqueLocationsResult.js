e9942e4e04541a48bccd8134ae17a708d55c7456
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 07:41:55 +0200

    remove log

--- packages/heap-snapshot-worker/src/parts/GetUniqueLocationsResult/GetUniqueLocationsResult.js
+++ packages/heap-snapshot-worker/src/parts/GetUniqueLocationsResult/GetUniqueLocationsResult.js
@@ -5,14 +5,12 @@ export const getUniqueLocationsResult = (locationMap, locations) => {
   const values = Object.values(locationMap)
 
   const result = new Uint32Array(values.length * ITEMS_PER_RESULT)
-  console.log({ values })
   for (const value of values) {
     const { count, index } = value
     const objectIndex = locations[index * ITEMS_PER_LOCATION]
     const scriptId = locations[index * ITEMS_PER_LOCATION + 1]
     const line = locations[index * ITEMS_PER_LOCATION + 2]
     const column = locations[index * ITEMS_PER_LOCATION + 3]
-    console.log({ objectIndex, scriptId, line, column })
     result[index * ITEMS_PER_RESULT] = objectIndex
     result[index * ITEMS_PER_RESULT + 1] = scriptId
     result[index * ITEMS_PER_RESULT + 2] = line
