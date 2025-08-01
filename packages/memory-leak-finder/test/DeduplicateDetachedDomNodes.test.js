3c8d49977dde1e7bc56f1431337e26d25a3e0ffb
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 07:28:29 +0200

    feature: format code (#1527)

--- packages/memory-leak-finder/test/DeduplicateDetachedDomNodes.test.js
+++ packages/memory-leak-finder/test/DeduplicateDetachedDomNodes.test.js
@@ -1,5 +1,5 @@
 import * as DeduplicateDetachedDomNodes from '../src/parts/DeduplicateDetachedDomNodes/DeduplicateDetachedDomNodes.js'
-import {test, expect} from '@jest/globals'
+import { test, expect } from '@jest/globals'
 
 test('deduplicateDetachedDomNodes', () => {
   const detachedDomNodes = [
