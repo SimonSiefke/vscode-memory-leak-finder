b332a0ac7ea7718ca1b8059a3c270bbc731c00f7
Simon Siefke <simon.siefke@gmail.com>
2025-08-01 17:11:36 +0200

    Discard changes to packages/heap-snapshot-worker/src/parts/AggregateFunctionObjects/AggregateFunctionObjects.js

--- packages/heap-snapshot-worker/src/parts/AggregateFunctionObjects/AggregateFunctionObjects.js
+++ packages/heap-snapshot-worker/src/parts/AggregateFunctionObjects/AggregateFunctionObjects.js
@@ -1,11 +1,9 @@
 export const aggregateFunctionObjects = (functionObjects) => {
   const map = Object.create(null)
-
   for (const { url } of functionObjects) {
     map[url] ||= 0
     map[url]++
   }
-
   const seen = Object.create(null)
   const aggregated = []
   for (const { url, sourceMapUrl, name } of functionObjects) {
