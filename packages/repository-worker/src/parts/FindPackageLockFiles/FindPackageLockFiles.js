a439c60d54ba052d2956b17a70f58955b530984e
Simon Siefke <simon.siefke@gmail.com>
2025-08-02 22:31:39 +0200

    use file uris

--- packages/repository-worker/src/parts/FindPackageLockFiles/FindPackageLockFiles.js
+++ packages/repository-worker/src/parts/FindPackageLockFiles/FindPackageLockFiles.js
@@ -1,22 +1,26 @@
 import { glob } from 'node:fs/promises'
 import { join } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
 import { VError } from '@lvce-editor/verror'
 
 /**
  * Finds all package-lock.json files in a directory using glob
- * @param {string} dir - Directory to search in
- * @returns {Promise<string[]>} - Array of full paths to package-lock.json files
+ * @param {string} dirUri - File URI of the directory to search in
+ * @returns {Promise<string[]>} - Array of file URIs to package-lock.json files
  */
-export const findPackageLockFiles = async (dir) => {
+export const findPackageLockFiles = async (dirUri) => {
   try {
-    const packageLockPaths = await Array.fromAsync(glob('**/package-lock.json', {
+    const dir = fileURLToPath(dirUri)
+    const globIterator = glob('**/package-lock.json', {
       cwd: dir,
       exclude: ['**/node_modules/**']
-    }))
+    })
+    const packageLockPaths = await Array.fromAsync(globIterator)
     const absolutePaths = packageLockPaths.map((path) => join(dir, path))
+    const fileUris = absolutePaths.map((path) => pathToFileURL(path).href)
 
-    return absolutePaths
+    return fileUris
   } catch (error) {
-    throw new VError(error, `Failed to find package-lock.json files in directory '${dir}'`)
+    throw new VError(error, `Failed to find package-lock.json files in directory '${dirUri}'`)
   }
 }
