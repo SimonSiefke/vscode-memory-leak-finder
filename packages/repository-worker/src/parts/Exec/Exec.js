b6b373155265508e451241980994395a48023cca
Simon Siefke <simon.siefke@gmail.com>
2025-08-02 23:29:56 +0200

    tsc

--- packages/repository-worker/src/parts/Exec/Exec.js
+++ packages/repository-worker/src/parts/Exec/Exec.js
@@ -5,13 +5,13 @@ import { execa } from 'execa'
  * @param {string} command - The command to execute
  * @param {string[]} args - Array of arguments to pass to the command
  * @param {Object} options - Additional options for execa
- * @returns {Promise<{stdout: string, stderr: string}>} The stdout and stderr from the command
+ * @returns {Promise<{stdout: string, stderr: string, exitCode: number}>} The stdout, stderr, and exit code from the command
  */
 export const exec = async (command, args, options = {}) => {
   const result = await execa(command, args, options)
   return {
     stdout: result.stdout || '',
     stderr: result.stderr || '',
-    exitCode: result.exitCode,
+    exitCode: result.exitCode || 0,
   }
 }
