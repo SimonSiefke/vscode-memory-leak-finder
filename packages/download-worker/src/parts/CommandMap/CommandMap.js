0f10b4f1d2b2b7681295185cf5b25d29660b51f5
Simon Siefke <simon.siefke@gmail.com>
2025-08-02 22:14:06 +0200

    move code

--- packages/download-worker/src/parts/CommandMap/CommandMap.js
+++ packages/download-worker/src/parts/CommandMap/CommandMap.js
@@ -1,21 +1,11 @@
 import * as DownloadAndUnzipCursor from '../DownloadAndUnzipCursor/DownloadAndUnzipCursor.js'
 import * as DownloadAndUnzipVscode from '../DownloadAndUnzipVscode/DownloadAndUnzipVscode.js'
-import * as DownloadAndBuildVscodeFromCommit from '../DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.js'
 import * as DownloadFfmpeg from '../DownloadFfmpeg/DownloadFfmpeg.js'
 import * as DownloadFfmpegMaybe from '../DownloadFfmpegMaybe/DownloadFfmpegMaybe.js'
-import { setupNodeModulesFromCache } from '../SetupNodeModulesFromCache/SetupNodeModulesFromCache.js'
-import { cacheNodeModules } from '../CacheNodeModules/CacheNodeModules.js'
-import { cleanupNodeModules } from '../CleanupNodeModules/CleanupNodeModules.js'
-import * as ResolveCommitHash from '../ResolveCommitHash/ResolveCommitHash.js'
 
 export const commandMap = {
   'Download.downloadAndUnzipCursor': DownloadAndUnzipCursor.downloadAndUnzipCursor,
   'Download.downloadAndUnzipVscode': DownloadAndUnzipVscode.downloadAndUnzipVscode,
-  'Download.downloadAndBuildVscodeFromCommit': DownloadAndBuildVscodeFromCommit.downloadAndBuildVscodeFromCommit,
   'Download.downloadFfmpeg': DownloadFfmpeg.downloadFfmpeg,
   'Download.downloadFfmpegMaybe': DownloadFfmpegMaybe.downloadFfmpegMaybe,
-  'VscodeNodeModulesCache.setupNodeModulesFromCache': setupNodeModulesFromCache,
-  'VscodeNodeModulesCache.cacheNodeModules': cacheNodeModules,
-  'VscodeNodeModulesCache.cleanupNodeModules': cleanupNodeModules,
-  'ResolveCommitHash.resolveCommitHash': ResolveCommitHash.resolveCommitHash,
 }
--- packages/test-coordinator/src/parts/DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.js
+++ packages/test-coordinator/src/parts/DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.js
@@ -1,12 +1,8 @@
-import * as DownloadWorker from '../DownloadWorker/DownloadWorker.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import { downloadAndBuildVscodeFromCommit as downloadAndBuildVscodeFromCommitImpl } from '../../../../repository-process/src/main.js'
 
 /**
  * @param {string} commitHash
  */
 export const downloadAndBuildVscodeFromCommit = async (commitHash) => {
-  const ipc = await DownloadWorker.launch()
-  const path = await JsonRpc.invoke(ipc, 'Download.downloadAndBuildVscodeFromCommit', commitHash)
-  ipc.dispose()
-  return path
+  return await downloadAndBuildVscodeFromCommitImpl(commitHash)
 }
