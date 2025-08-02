4cab985c8f180d379263dec1d9bcb86ba0a75336
Simon Siefke <simon.siefke@gmail.com>
2025-08-02 23:37:11 +0200

    format code

--- packages/repository-worker/src/parts/CheckCacheExists/CheckCacheExists.js
+++ packages/repository-worker/src/parts/CheckCacheExists/CheckCacheExists.js
@@ -14,9 +14,9 @@ export const checkCacheExists = async (commitHash) => {
   try {
     const cacheDir = join(Root.root, VSCODE_NODE_MODULES_CACHE_DIR)
     const cachedNodeModulesPath = join(cacheDir, commitHash)
-    
+
     return await pathExists(cachedNodeModulesPath)
   } catch (error) {
     throw new VError(error, `Failed to check if cache exists for commit hash: ${commitHash}`)
   }
-} 
+}
