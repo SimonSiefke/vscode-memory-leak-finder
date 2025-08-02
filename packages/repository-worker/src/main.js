f53407a8839417e34238a4c056cd6e806cfe6e99
Simon Siefke <simon.siefke@gmail.com>
2025-08-02 22:20:31 +0200

    add main

--- packages/repository-worker/src/main.js
+++ packages/repository-worker/src/main.js
@@ -1,13 +1,3 @@
-import { downloadAndBuildVscodeFromCommit } from './parts/DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.js'
-import { resolveCommitHash } from './parts/ResolveCommitHash/ResolveCommitHash.js'
-import { setupNodeModulesFromCache } from './parts/SetupNodeModulesFromCache/SetupNodeModulesFromCache.js'
-import { cacheNodeModules } from './parts/CacheNodeModules/CacheNodeModules.js'
-import { cleanupNodeModules } from './parts/CleanupNodeModules/CleanupNodeModules.js'
+import { main } from './parts/Main/Main.js'
 
-export {
-  downloadAndBuildVscodeFromCommit,
-  resolveCommitHash,
-  setupNodeModulesFromCache,
-  cacheNodeModules,
-  cleanupNodeModules,
-}
+main()
