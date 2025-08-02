78f78019e9f53474fb5bc4339a92a0a6ed9ec449
Simon Siefke <simon.siefke@gmail.com>
2025-08-02 23:53:36 +0200

    remove unused code

--- packages/test-coordinator/src/parts/DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.js
+++
@@ -1,8 +0,0 @@
-import { downloadAndBuildVscodeFromCommit as downloadAndBuildVscodeFromCommitImpl } from '../../../../repository-process/src/main.js'
-
-/**
- * @param {string} commitHash
- */
-export const downloadAndBuildVscodeFromCommit = async (commitHash) => {
-  return await downloadAndBuildVscodeFromCommitImpl(commitHash)
-}
