3a10af6e4c881f29b5a7f563dd2bf8d6fdeabad1
Simon Siefke <simon.siefke@gmail.com>
2025-08-04 00:18:16 +0200

    better code

--- packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
@@ -1,67 +1,51 @@
 import { prepareHeapSnapshot } from '../PrepareHeapSnapshot/PrepareHeapSnapshot.js'
 import { readFile } from 'node:fs/promises'
 
-const ITEMS_PER_NODE = 7
-
 /**
  * @param {string} pathUri
  * @returns {Promise<Array>}
  */
 export const getRegexpObjectsFromHeapSnapshot = async (pathUri) => {
-  // Read and parse the heap snapshot file
+  // Read strings from the original JSON file
   const content = await readFile(pathUri, 'utf8')
   const heapSnapshot = JSON.parse(content)
   const strings = heapSnapshot.strings || []
 
-  // Use the proper parsing function to get parsed nodes and graph
-  const { snapshot, nodes, edges, locations } = heapSnapshot
-  const { meta } = snapshot
-  const { node_types, node_fields, edge_types, edge_fields, location_fields } = meta
-  
-  // Import the proper parser
-  const ParseHeapSnapshotInternal = await import('../ParseHeapSnapshotInternal/ParseHeapSnapshotInternal.js')
-  const { parsedNodes, graph } = ParseHeapSnapshotInternal.parseHeapSnapshotInternal(
-    nodes,
-    node_fields,
-    node_types[0],
-    edges,
-    edge_fields,
-    edge_types[0],
-    strings,
-    locations,
-    location_fields,
-  )
+  // Use fast prepareHeapSnapshot
+  const { metaData, nodes } = await prepareHeapSnapshot(pathUri)
+  const { node_types, node_fields } = metaData.data.meta
 
-  // Filter for regexp objects
-  const regexpObjects = parsedNodes
-    .filter(node => node.type === 'regexp')
-    .map(node => {
-      // Try to find the actual regex pattern
-      let actualPattern = node.name || '<dummy>'
-      
-      // Look through edges for 'source' property
-      const nodeEdges = graph[node.id] || []
-      for (const edge of nodeEdges) {
-        if (edge.name === 'source') {
-          // Find the target node
-          const targetNode = parsedNodes.find(n => n.id === edge.index)
-          if (targetNode && targetNode.name && targetNode.name !== '<dummy>') {
-            actualPattern = targetNode.name
-            break
-          }
-        }
-      }
-      
-      return {
-        id: node.id,
-        name: actualPattern,
-        pattern: actualPattern,
-        selfSize: node.self_size || node.selfSize,
-        edgeCount: node.edge_count || node.edgeCount,
-        traceNodeId: node.trace_node_id || node.traceNodeId,
-        detachedness: node.detachedness,
-      }
-    })
+  const regexpTypeIndex = node_types[0].indexOf('regexp')
+  const ITEMS_PER_NODE = node_fields.length
+
+  if (regexpTypeIndex === -1) {
+    return []
+  }
+
+  const regexpObjects = []
+
+  for (let i = 0; i < nodes.length; i += ITEMS_PER_NODE) {
+    const typeIndex = nodes[i + node_fields.indexOf('type')]
+    if (typeIndex === regexpTypeIndex) {
+      const nameIndex = nodes[i + node_fields.indexOf('name')]
+      const id = nodes[i + node_fields.indexOf('id')]
+      const selfSize = nodes[i + node_fields.indexOf('self_size')]
+      const edgeCount = nodes[i + node_fields.indexOf('edge_count')]
+      const detachedness = nodes[i + node_fields.indexOf('detachedness')]
+
+      const pattern = strings[nameIndex] || ''
+
+      regexpObjects.push({
+        id,
+        name: pattern,
+        pattern,
+        selfSize,
+        edgeCount,
+        traceNodeId: 0, // Not available in this heap snapshot format
+        detachedness,
+      })
+    }
+  }
 
   return regexpObjects
 }
