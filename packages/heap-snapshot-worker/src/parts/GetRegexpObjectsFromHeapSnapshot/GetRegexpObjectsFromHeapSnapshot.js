e52c15b9223d8cc0b2b1b433aefdc55946bcba15
Simon Siefke <simon.siefke@gmail.com>
2025-08-04 00:18:40 +0200

    better code

--- packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
@@ -18,6 +18,14 @@ export const getRegexpObjectsFromHeapSnapshot = async (pathUri) => {
   const regexpTypeIndex = node_types[0].indexOf('regexp')
   const ITEMS_PER_NODE = node_fields.length
 
+  // Calculate field indices once
+  const typeFieldIndex = node_fields.indexOf('type')
+  const nameFieldIndex = node_fields.indexOf('name')
+  const idFieldIndex = node_fields.indexOf('id')
+  const selfSizeFieldIndex = node_fields.indexOf('self_size')
+  const edgeCountFieldIndex = node_fields.indexOf('edge_count')
+  const detachedFieldIndex = node_fields.indexOf('detachedness')
+
   if (regexpTypeIndex === -1) {
     return []
   }
@@ -25,13 +33,13 @@ export const getRegexpObjectsFromHeapSnapshot = async (pathUri) => {
   const regexpObjects = []
 
   for (let i = 0; i < nodes.length; i += ITEMS_PER_NODE) {
-    const typeIndex = nodes[i + node_fields.indexOf('type')]
+    const typeIndex = nodes[i + typeFieldIndex]
     if (typeIndex === regexpTypeIndex) {
-      const nameIndex = nodes[i + node_fields.indexOf('name')]
-      const id = nodes[i + node_fields.indexOf('id')]
-      const selfSize = nodes[i + node_fields.indexOf('self_size')]
-      const edgeCount = nodes[i + node_fields.indexOf('edge_count')]
-      const detachedness = nodes[i + node_fields.indexOf('detachedness')]
+      const nameIndex = nodes[i + nameFieldIndex]
+      const id = nodes[i + idFieldIndex]
+      const selfSize = nodes[i + selfSizeFieldIndex]
+      const edgeCount = nodes[i + edgeCountFieldIndex]
+      const detachedness = nodes[i + detachedFieldIndex]
 
       const pattern = strings[nameIndex] || ''
 
