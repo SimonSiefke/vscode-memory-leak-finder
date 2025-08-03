ddf330f6a6247dbbb936bf3206462071e259c3b5
Simon Siefke <simon.siefke@gmail.com>
2025-08-03 11:08:21 +0200

    add checkout test

---
+++ packages/repository-worker/test/CheckoutCommit.test.js
@@ -0,0 +1,42 @@
+import { expect, test, jest } from '@jest/globals'
+
+const mockExec = jest.fn()
+
+jest.unstable_mockModule('../src/parts/Exec/Exec.js', () => ({
+  exec: mockExec,
+}))
+
+const { checkoutCommit } = await import('../src/parts/CheckoutCommit/CheckoutCommit.js')
+
+test('checkoutCommit executes git checkout command', async () => {
+  const repoPath = '/test/repo'
+  const commit = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0'
+
+  mockExec.mockResolvedValue({ stdout: '', stderr: '', exitCode: 0 })
+
+  await checkoutCommit(repoPath, commit)
+
+  expect(mockExec).toHaveBeenCalledWith('git', ['checkout', commit], { cwd: repoPath })
+})
+
+test('checkoutCommit handles different commit formats', async () => {
+  const repoPath = '/test/repo'
+  const commit = 'main'
+
+  mockExec.mockResolvedValue({ stdout: '', stderr: '', exitCode: 0 })
+
+  await checkoutCommit(repoPath, commit)
+
+  expect(mockExec).toHaveBeenCalledWith('git', ['checkout', commit], { cwd: repoPath })
+})
+
+test('checkoutCommit handles short commit hash', async () => {
+  const repoPath = '/test/repo'
+  const commit = 'a1b2c3d'
+
+  mockExec.mockResolvedValue({ stdout: '', stderr: '', exitCode: 0 })
+
+  await checkoutCommit(repoPath, commit)
+
+  expect(mockExec).toHaveBeenCalledWith('git', ['checkout', commit], { cwd: repoPath })
+}) 
