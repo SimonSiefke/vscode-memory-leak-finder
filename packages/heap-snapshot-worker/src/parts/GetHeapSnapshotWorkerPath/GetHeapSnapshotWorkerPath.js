64e25bf9f51806ead6610672dc27fc4b12ffd4ba
Simon Siefke <simon.siefke@gmail.com>
2025-08-03 15:15:09 +0200

    use dirname

--- packages/heap-snapshot-worker/src/parts/GetHeapSnapshotWorkerPath/GetHeapSnapshotWorkerPath.js
+++ packages/heap-snapshot-worker/src/parts/GetHeapSnapshotWorkerPath/GetHeapSnapshotWorkerPath.js
@@ -6,6 +6,6 @@ import { fileURLToPath } from 'node:url'
  * @returns {string} The absolute path to the heap snapshot parsing worker
  */
 export const getHeapSnapshotWorkerPath = () => {
-  const __dirname = dirname(fileURLToPath(import.meta.url))
+  const __dirname = import.meta.dirname
   return join(__dirname, '../../../../heap-snapshot-parsing-worker/bin/heap-snapshot-parsing-worker.js')
 }
