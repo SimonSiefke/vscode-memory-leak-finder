0a388fd64f8bc4c6a58ed03d4b765d2c854a706a
Simon Siefke <simon.siefke@gmail.com>
2025-08-04 00:21:56 +0200

    better code

--- packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
@@ -1,5 +1,6 @@
 import { prepareHeapSnapshot } from '../PrepareHeapSnapshot/PrepareHeapSnapshot.js'
 import { readFile } from 'node:fs/promises'
+import { getRegexpObjectsFromHeapSnapshotInternal } from '../GetRegexpObjectsFromHeapSnapshotInternal/GetRegexpObjectsFromHeapSnapshotInternal.js'
 
 /**
  * @param {string} pathUri
@@ -11,37 +12,9 @@ export const getRegexpObjectsFromHeapSnapshot = async (pathUri) => {
   const heapSnapshot = JSON.parse(content)
   const strings = heapSnapshot.strings || []
 
-  // Use fast prepareHeapSnapshot
+    // Use fast prepareHeapSnapshot 
   const { metaData, nodes } = await prepareHeapSnapshot(pathUri)
   const { node_types, node_fields } = metaData.data.meta
 
-  const regexpTypeIndex = node_types[0].indexOf('regexp')
-  const ITEMS_PER_NODE = node_fields.length
-
-  // Calculate field indices once
-  const typeFieldIndex = node_fields.indexOf('type')
-  const nameFieldIndex = node_fields.indexOf('name')
-  const idFieldIndex = node_fields.indexOf('id')
-
-  if (regexpTypeIndex === -1) {
-    return []
-  }
-
-  const regexpObjects = []
-
-  for (let i = 0; i < nodes.length; i += ITEMS_PER_NODE) {
-    const typeIndex = nodes[i + typeFieldIndex]
-    if (typeIndex === regexpTypeIndex) {
-      const nameIndex = nodes[i + nameFieldIndex]
-      const id = nodes[i + idFieldIndex]
-      const pattern = strings[nameIndex] || ''
-
-      regexpObjects.push({
-        id,
-        pattern,
-      })
-    }
-  }
-
-  return regexpObjects
+  return getRegexpObjectsFromHeapSnapshotInternal(strings, nodes, node_types, node_fields)
 }
