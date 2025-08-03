f31b18da3685a995b9da326757e929b111779c45
Simon Siefke <simon.siefke@gmail.com>
2025-08-03 09:30:32 +0200

    add more strict exec options

--- packages/repository-worker/src/parts/Exec/Exec.js
+++ packages/repository-worker/src/parts/Exec/Exec.js
@@ -4,7 +4,7 @@ import { execa } from 'execa'
  * Executes a command with arguments and returns stdout and stderr
  * @param {string} command - The command to execute
  * @param {string[]} args - Array of arguments to pass to the command
- * @param {Object} options - Additional options for execa
+ * @param {{cwd?:string}} options - Additional options for execa
  * @returns {Promise<{stdout: string, stderr: string, exitCode: number}>} The stdout, stderr, and exit code from the command
  */
 export const exec = async (command, args, options = {}) => {
