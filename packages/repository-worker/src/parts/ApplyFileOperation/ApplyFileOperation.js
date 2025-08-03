6c7c8c12681a8bbf1b0bb54d1d125b8d5ba1947c
Simon Siefke <simon.siefke@gmail.com>
2025-08-03 10:20:32 +0200

    add more tests

---
+++ packages/repository-worker/src/parts/ApplyFileOperation/ApplyFileOperation.js
@@ -0,0 +1,62 @@
+import { VError } from '@lvce-editor/verror'
+import { copy, makeDirectory, remove } from '../Filesystem/Filesystem.js'
+
+/**
+ * @typedef {Object} CopyOperation
+ * @property {'copy'} type
+ * @property {string} from
+ * @property {string} to
+ */
+
+/**
+ * @typedef {Object} MkdirOperation
+ * @property {'mkdir'} type
+ * @property {string} path
+ */
+
+/**
+ * @typedef {Object} RemoveOperation
+ * @property {'remove'} type
+ * @property {string} from
+ */
+
+/**
+ * @typedef {CopyOperation | MkdirOperation | RemoveOperation} FileOperation
+ */
+
+/**
+ * @param {FileOperation} operation
+ */
+export const applyFileOperation = async (operation) => {
+  try {
+    switch (operation.type) {
+      case 'copy': {
+        const fromPath = operation.from
+        const toPath = operation.to
+        await copy(fromPath, toPath)
+        console.log(`Copied: ${operation.from} -> ${operation.to}`)
+
+        break
+      }
+
+      case 'remove': {
+        const fromPath = operation.from
+        await remove(fromPath)
+        console.log(`Removed: ${operation.from}`)
+
+        break
+      }
+
+      case 'mkdir': {
+        const { path } = operation
+        await makeDirectory(path)
+        console.log(`Created directory: ${operation.path}`)
+
+        break
+      }
+      // No default
+    }
+  } catch (error) {
+    throw new VError(error, `Failed to apply file operation ${operation.type}`)
+  }
+} 
