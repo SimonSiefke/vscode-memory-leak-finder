a7ab79cb60bed123f6cd4378cd49147b8a1ae199
Simon Siefke <simon.siefke@gmail.com>
2025-08-04 23:52:08 +0200

    Discard changes to packages/heap-snapshot-parsing-worker/src/parts/DecodeArray/DecodeArray.js

--- packages/heap-snapshot-parsing-worker/src/parts/DecodeArray/DecodeArray.js
+++ packages/heap-snapshot-parsing-worker/src/parts/DecodeArray/DecodeArray.js
@@ -1,8 +1,3 @@
-/**
- * Decodes a Uint8Array to a string using TextDecoder
- * @param {Uint8Array} data - The data to decode
- * @returns {string} The decoded string
- */
 export const decodeArray = (data) => {
   return new TextDecoder().decode(data)
 }
