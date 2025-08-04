61f894e8898dc117d1c1befc0ef4988a4e07ba9b
Simon Siefke <simon.siefke@gmail.com>
2025-08-04 18:57:37 +0200

    update setup message

--- packages/cli/src/parts/GetHandleTestSetupMessage/GetHandleTestSetupMessage.js
+++ packages/cli/src/parts/GetHandleTestSetupMessage/GetHandleTestSetupMessage.js
@@ -1,7 +1,5 @@
-import chalk from 'chalk'
 import * as TestPrefix from '../TestPrefix/TestPrefix.js'
 
 export const getHandleTestSetupMessage = () => {
-  const setupMessage = chalk.dim('Launching IDE and connecting to devtools...')
-  return `\n${TestPrefix.Setup} ${setupMessage}\n`
+  return `\n${TestPrefix.Setup}\n`
 }
