0539119835b3d5ad4ecccfefc9f7aa8898e9da4b
Simon Siefke <simon.siefke@gmail.com>
2025-08-04 09:27:16 +0200

    map entries

--- packages/heap-snapshot-worker/src/parts/GetMapObjectsFromHeapSnapshotInternal/GetMapObjectsFromHeapSnapshotInternal.js
+++ packages/heap-snapshot-worker/src/parts/GetMapObjectsFromHeapSnapshotInternal/GetMapObjectsFromHeapSnapshotInternal.js
@@ -56,7 +56,9 @@ export const getMapObjectsFromHeapSnapshotInternal = (strings, nodes, node_types
           selfSize,
           edgeCount,
           detachedness,
+          nodeDataIndex: i, // Store node data index for later use
           variableNames: [], // Will be populated by scanning edges
+          entries: [], // Will be populated by extracting from table array
           size: null, // Will try to extract from internal structure
           note: 'Map object found in heap snapshot',
         }
@@ -152,7 +154,102 @@ export const getMapObjectsFromHeapSnapshotInternal = (strings, nodes, node_types
     currentEdgeOffset += edgeCount
   }
 
+  // Third pass: extract Map entries from table arrays
+  for (const mapObj of mapObjects) {
+    // Find the table array for this Map
+    let mapEdgeOffset = 0
+    for (let nodeIndex = 0; nodeIndex < mapObj.nodeDataIndex; nodeIndex += ITEMS_PER_NODE) {
+      mapEdgeOffset += nodes[nodeIndex + edgeCountFieldIndex]
+    }
+
+    let tableArrayNodeIndex = null
+
+    // Look for the "table" internal edge
+    for (let j = 0; j < mapObj.edgeCount; j++) {
+      const edgeIndex = (mapEdgeOffset + j) * ITEMS_PER_EDGE
+      const edgeType = edges[edgeIndex + edgeTypeFieldIndex]
+      const edgeNameOrIndex = edges[edgeIndex + edgeNameFieldIndex]
+      const edgeToNode = edges[edgeIndex + edgeToNodeFieldIndex]
+
+      const edgeTypeName = edgeTypes[edgeType] || `type_${edgeType}`
+      const edgeName = strings[edgeNameOrIndex] || `<string_${edgeNameOrIndex}>`
+
+      if (edgeTypeName === 'internal' && edgeName === 'table') {
+        tableArrayNodeIndex = edgeToNode
+        break
+      }
+    }
+
+    if (!tableArrayNodeIndex) {
+      continue // Skip if no table array found
+    }
+
+    // Extract entries from table array
+    const tableEdgeCount = nodes[tableArrayNodeIndex + edgeCountFieldIndex]
+
+    let tableEdgeOffset = 0
+    for (let nodeIndex = 0; nodeIndex < tableArrayNodeIndex; nodeIndex += ITEMS_PER_NODE) {
+      tableEdgeOffset += nodes[nodeIndex + edgeCountFieldIndex]
+    }
+
+    const entries = []
+
+    // Collect data entries from table array
+    for (let j = 0; j < tableEdgeCount; j++) {
+      const edgeIndex = (tableEdgeOffset + j) * ITEMS_PER_EDGE
+      const edgeType = edges[edgeIndex + edgeTypeFieldIndex]
+      const edgeToNode = edges[edgeIndex + edgeToNodeFieldIndex]
+
+      const edgeTypeName = edgeTypes[edgeType] || `type_${edgeType}`
+
+      // Get target info
+      const targetType = nodes[edgeToNode + typeFieldIndex]
+      const targetName = strings[nodes[edgeToNode + nameFieldIndex]] || ''
+      const targetTypeName = nodeTypes[targetType] || `type_${targetType}`
+
+      // Only collect meaningful entries (strings and numbers, not system objects)
+      if (edgeTypeName === 'internal' && (targetTypeName === 'string' || targetTypeName === 'number') && targetName) {
+        entries.push({
+          type: targetTypeName,
+          value: targetName
+        })
+      }
+    }
+
+    // Pair entries as key-value pairs (even indices = keys, odd indices = values)
+    for (let i = 0; i < entries.length - 1; i += 2) {
+      const key = entries[i]
+      const value = entries[i + 1]
+      if (key && value) {
+        mapObj.entries.push({
+          key: key.value,
+          value: value.value,
+          keyType: key.type,
+          valueType: value.type
+        })
+      }
+    }
+
+    // Update size based on number of entries
+    if (mapObj.entries.length > 0) {
+      mapObj.size = mapObj.entries.length
+    }
+  }
+
   // Filter out prototypes and system objects (only return Map objects with variable names)
   const namedMapObjects = mapObjects.filter((obj) => obj.variableNames.length > 0)
-  return namedMapObjects.sort((a, b) => a.id - b.id)
+  
+  // Remove internal fields from output
+  return namedMapObjects.map(obj => ({
+    id: obj.id,
+    name: obj.name,
+    type: obj.type,
+    selfSize: obj.selfSize,
+    edgeCount: obj.edgeCount,
+    detachedness: obj.detachedness,
+    variableNames: obj.variableNames,
+    entries: obj.entries,
+    size: obj.size,
+    note: obj.note
+  })).sort((a, b) => a.id - b.id)
 }
