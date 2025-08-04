cf54ae6c55fca1727423f8dc94601a3bac0f6e3a
Simon Siefke <simon.siefke@gmail.com>
2025-08-04 13:31:43 +0200

    Discard changes to packages/test-coordinator/test/DownloadAndBuildVscodeFromCommit.test.js

---
+++ packages/test-coordinator/test/DownloadAndBuildVscodeFromCommit.test.js
@@ -0,0 +1,11 @@
+import { expect, test } from '@jest/globals'
+import * as DownloadAndBuildVscodeFromCommit from '../src/parts/DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.js'
+
+test('downloadAndBuildVscodeFromCommit - function exists and is callable', () => {
+  expect(typeof DownloadAndBuildVscodeFromCommit.downloadAndBuildVscodeFromCommit).toBe('function')
+})
+
+test('downloadAndBuildVscodeFromCommit - function signature is correct', () => {
+  const fn = DownloadAndBuildVscodeFromCommit.downloadAndBuildVscodeFromCommit
+  expect(fn.length).toBe(1) // Should accept one parameter (commitHash)
+})
