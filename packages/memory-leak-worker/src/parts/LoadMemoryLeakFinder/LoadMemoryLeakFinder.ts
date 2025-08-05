4e0827c4b0399b91ffb11523dcd97134bde68699
Simon Siefke <simon.siefke@gmail.com>
2025-08-05 21:21:31 +0200

    fix

--- packages/memory-leak-worker/src/parts/LoadMemoryLeakFinder/LoadMemoryLeakFinder.ts
+++ packages/memory-leak-worker/src/parts/LoadMemoryLeakFinder/LoadMemoryLeakFinder.ts
@@ -1,3 +1,4 @@
+// TODO move that code here
 export const loadMemoryLeakFinder = async () => {
   const MemoryLeakFinder = await import('../../../../memory-leak-finder/src/index.js')
   return MemoryLeakFinder
