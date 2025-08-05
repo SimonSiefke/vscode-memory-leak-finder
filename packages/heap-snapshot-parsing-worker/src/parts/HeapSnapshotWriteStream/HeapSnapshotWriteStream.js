14709ffc7d4895d216afb8cccc7fa1321ee64acf
Simon Siefke <simon.siefke@gmail.com>
2025-08-05 11:53:58 +0200

    feature: validate heapsnapshots

--- packages/heap-snapshot-parsing-worker/src/parts/HeapSnapshotWriteStream/HeapSnapshotWriteStream.js
+++ packages/heap-snapshot-parsing-worker/src/parts/HeapSnapshotWriteStream/HeapSnapshotWriteStream.js
@@ -25,7 +25,7 @@ class HeapSnapshotWriteStream extends Writable {
     this.options = options
     this.state = HeapSnapshotParsingState.SearchingSnapshotMetaData
     this.strings = []
-    this.validate = false // TODO
+    this.validate = options.validate || true
   }
 
   /**
