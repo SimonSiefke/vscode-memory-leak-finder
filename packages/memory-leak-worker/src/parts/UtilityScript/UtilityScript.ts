d519b5181240b127e36d6547f33aa2a63f645e13
Simon Siefke <simon.siefke@gmail.com>
2025-08-05 21:18:36 +0200

    fix

--- packages/memory-leak-worker/src/parts/UtilityScript/UtilityScript.ts
+++ packages/memory-leak-worker/src/parts/UtilityScript/UtilityScript.ts
@@ -3,6 +3,6 @@ import { join } from 'node:path'
 import * as Root from '../Root/Root.ts'
 import * as EncodingType from '../EncodingType/EncodingType.ts'
 
-const injectedCode = readFileSync(join(Root.root, 'packages', 'injected-code', 'dist', 'injectedCode.ts'), EncodingType.Utf8)
+const injectedCode = readFileSync(join(Root.root, 'packages', 'injected-code', 'dist', 'injectedCode.js'), EncodingType.Utf8)
 
 export const utilityScript = injectedCode
