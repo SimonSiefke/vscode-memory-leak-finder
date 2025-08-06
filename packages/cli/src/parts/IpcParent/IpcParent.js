f4763d24873b41f45ea833e8a9e89baff4ef2ab5
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 11:25:33 +0200

    rmove unused code

--- packages/cli/src/parts/IpcParent/IpcParent.js
+++
@@ -1,8 +0,0 @@
-import * as IpcParentModule from '../IpcParentModule/IpcParentModule.js'
-
-export const create = async ({ method, ...options }) => {
-  const module = IpcParentModule.getModule(method)
-  // @ts-ignore
-  const rpc = await module.create(options)
-  return rpc
-}
