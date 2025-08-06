11d1ba5e6fda4df765bb4c7fe36e50d37a98cadf
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 11:07:42 +0200

    feature: add chart worker package to try to parallelize charts creation

---
+++ packages/chart-worker/src/parts/CommandMap/CommandMap.js
@@ -0,0 +1,5 @@
+import * as CreateChat from '../CreateChart/CreateChart.js'
+
+export const commandMap = {
+  'Chart.create': CreateChat.createChart,
+}
---
+++ packages/chart-worker/src/parts/CreateChart/CreateChart.js
@@ -0,0 +1,19 @@
+import { fixHtmlNamespace } from '../FixXmlNamespace/FixXmlNamespace.js'
+import * as Plot from '../Plot/Plot.js'
+
+export const createChart = (data, { x, y, xLabel, yLabel }) => {
+  const baseHtml = Plot.plot({
+    style: 'overflow: visible;background:white',
+    marginLeft: 60,
+    y: {
+      grid: true,
+      label: yLabel,
+    },
+    x: {
+      label: xLabel,
+    },
+    marks: [Plot.lineY(data, { x, y })],
+  }).outerHTML
+  const finalHtml = fixHtmlNamespace(baseHtml)
+  return finalHtml
+}
