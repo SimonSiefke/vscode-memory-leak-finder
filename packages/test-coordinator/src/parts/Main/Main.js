12b519545c831096737b60552e0e43bd1e060a76
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 19:58:59 +0200

    fix: commands not registered

--- packages/test-coordinator/src/parts/Main/Main.js
+++ packages/test-coordinator/src/parts/Main/Main.js
@@ -8,6 +8,6 @@ import * as CommandMapRef from '../CommandMapRef/CommandMapRef.js'
 export const main = async () => {
   Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   CommandState.registerCommands(CommandMap.commandMap)
-  const rpc = await IpcChild.listen({ method: IpcChildType.Auto() })
+  const rpc = await IpcChild.listen({ method: IpcChildType.Auto(), commandMap: CommandMapRef.commandMapRef })
   CliProcess.set(rpc)
 }
