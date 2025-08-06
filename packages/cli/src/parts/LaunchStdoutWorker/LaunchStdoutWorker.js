9f0c6a1ad69f91fb227984c7e4e5738a46bd6bda
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 11:19:33 +0200

    debug worker

--- packages/cli/src/parts/LaunchStdoutWorker/LaunchStdoutWorker.js
+++ packages/cli/src/parts/LaunchStdoutWorker/LaunchStdoutWorker.js
@@ -1,14 +1,12 @@
+import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
 import * as GetStdoutWorkerUrl from '../GetStdoutWorkerUrl/GetStdoutWorkerUrl.js'
-import * as IpcParent from '../IpcParent/IpcParent.js'
-import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 
 export const launchStdoutWorker = async () => {
   const url = GetStdoutWorkerUrl.getStdoutWorkerUrl()
-  const rpc = await IpcParent.create({
-    method: IpcParentType.NodeWorkerThread,
+  const rpc = await NodeWorkerRpcParent.create({
     path: url,
-    name: 'Stdout Worker',
-    ref: false,
+    // name: 'Stdout Worker',
+    // ref: false,
     stdio: 'inherit',
     commandMap: {},
     argv: ['--ipc-type=worker-thread'],
