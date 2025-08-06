7ded2be99899aee9205348e68d98b46a36f31caa
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 19:11:19 +0200

    feature: add chart worker package to try to parallelize charts creation (#1596)

--- packages/charts/src/parts/GenerateCharts/GenerateCharts.js
+++ packages/charts/src/parts/GenerateCharts/GenerateCharts.js
@@ -12,11 +12,17 @@ const visitors = Object.values(Charts).map((value) => {
 })
 
 export const generateCharts = async () => {
+  console.time('total')
   for (const visitor of visitors) {
+    console.time('data')
     const data = await visitor.getData()
+    console.timeEnd('data')
+    console.time('svg')
     const svg = visitor.fn(data)
+    console.timeEnd('svg')
     const outPath = join(Root.root, '.vscode-charts', `${visitor.name}.svg`)
     await mkdir(dirname(outPath), { recursive: true })
     await writeFile(outPath, svg)
   }
+  console.timeEnd('total')
 }
