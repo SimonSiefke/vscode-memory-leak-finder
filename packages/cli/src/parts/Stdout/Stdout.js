03308a4e48a43e01deb2dea73aee238c5027de9c
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 23:19:39 +0200

    fix stdout

--- packages/cli/src/parts/Stdout/Stdout.js
+++ packages/cli/src/parts/Stdout/Stdout.js
@@ -1,21 +1,6 @@
 import { string } from '@lvce-editor/assert'
-import * as StdoutWorker from '../StdoutWorker/StdoutWorker.js'
 
 export const write = async (data) => {
   string(data)
   process.stdout.write(data)
-  // await StdoutWorker.invoke('Stdout.write', data)
-
-  // process.stdout.write('\n')
-  // await new Promise((r) => {
-  //   setTimeout(r, 100)
-  // })
-  // process.stdout.on('drain', () => {
-  //   console.log('did drain now')
-  // })
-  // await new Promise((r) => {
-  // setTimeout(r, 1)
-  // })
-  // console.log('did write')
-  // console.log({ data })
 }
