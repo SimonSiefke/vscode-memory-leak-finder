e9c8011d2389bf78796793a198ceb0fed5bce698
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 11:14:11 +0200

    tsc

--- packages/test-coordinator/src/parts/StorageWorker/StorageWorker.js
+++ packages/test-coordinator/src/parts/StorageWorker/StorageWorker.js
@@ -1,14 +1,14 @@
+import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.js'
 import * as GetStorageWorkerUrl from '../GetStorageWorkerUrl/GetStorageWorkerUrl.js'
-import * as IpcParent from '../IpcParent/IpcParent.js'
-import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 
 export const launch = async () => {
   const url = GetStorageWorkerUrl.getStorageWorkerUrl()
-  const rpc = await IpcParent.create({
-    method: IpcParentType.NodeWorkerThread,
+  const rpc = await NodeWorkerRpcParent.create({
     path: url,
     stdio: 'inherit',
     execArgv: [],
+    commandMap: CommandMapRef.commandMapRef,
   })
   return rpc
 }
