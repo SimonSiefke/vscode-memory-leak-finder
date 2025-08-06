2c15dc280fc0cea9ac0f718e1255a5bff01f43b1
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 20:00:41 +0200

    simplify

--- packages/test-coordinator/src/parts/Main/Main.js
+++ packages/test-coordinator/src/parts/Main/Main.js
@@ -1,13 +1,5 @@
-import * as CliProcess from '../CliProcess/CliProcess.js'
-import * as CommandMap from '../CommandMap/CommandMap.js'
-import * as CommandState from '../CommandState/CommandState.js'
-import * as IpcChild from '../IpcChild/IpcChild.js'
-import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-import * as CommandMapRef from '../CommandMapRef/CommandMapRef.js'
+import * as Listen from '../Listen/Listen.js'
 
 export const main = async () => {
-  Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
-  CommandState.registerCommands(CommandMap.commandMap)
-  const rpc = await IpcChild.listen({ method: IpcChildType.Auto(), commandMap: CommandMapRef.commandMapRef })
-  CliProcess.set(rpc)
+  await Listen.listen()
 }
