9d5fb4bc118b90b3d447ac097ada8887fea51a3c
Simon Siefke <simon.siefke@gmail.com>
2025-08-06 11:26:31 +0200

    tsc

--- packages/cli/src/parts/CreateTestCoordinatorAndListen/CreateTestCoordinatorAndListen.js
+++ packages/cli/src/parts/CreateTestCoordinatorAndListen/CreateTestCoordinatorAndListen.js
@@ -1,13 +1,12 @@
 import * as HandleTestStderrData from '../HandleTestStderrData/HandleTestStderrData.js'
 import * as HandleTestStdoutData from '../HandleTestStdoutData/HandleTestStdoutData.js'
-import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as TestCoordinator from '../TestCoordinator/TestCoordinator.js'
 
 export const createTestCoordinatorAndListen = async () => {
-  const worker = await TestCoordinator.listen(IpcParentType.NodeWorkerThread)
+  const rpc = await TestCoordinator.listen()
   // @ts-ignore
-  worker.stdout.on('data', HandleTestStdoutData.handleStdoutData)
+  rpc.stdout.on('data', HandleTestStdoutData.handleStdoutData)
   // @ts-ignore
-  worker.stderr.on('data', HandleTestStderrData.handleStderrData)
-  return worker
+  rpc.stderr.on('data', HandleTestStderrData.handleStderrData)
+  return rpc
 }
--- packages/cli/src/parts/TestCoordinator/TestCoordinator.js
+++ packages/cli/src/parts/TestCoordinator/TestCoordinator.js
@@ -1,12 +1,12 @@
+import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.js'
 import * as GetTestCoordinatorUrl from '../GetTestCoordinatorUrl/GetTestCoordinatorUrl.js'
-import * as IpcParent from '../IpcParent/IpcParent.js'
 
-export const listen = async (method) => {
+export const listen = async () => {
   const url = GetTestCoordinatorUrl.getTestCoordinatorUrl()
-  const rpc = await IpcParent.create({
-    method,
+  const rpc = await NodeWorkerRpcParent.create({
     path: url,
+    // @ts-ignore
     name: 'Test Coordinator',
     ref: false,
     argv: ['--ipc-type=worker-thread'],
