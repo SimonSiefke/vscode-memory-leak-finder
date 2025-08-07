bfc197e11c18c797a229a7b22969001979a758a0
Simon Siefke <simon.siefke@gmail.com>
2025-08-07 15:28:14 +0200

    remove unused code

--- packages/initialization-worker/src/parts/Listen/Listen.js
+++ packages/initialization-worker/src/parts/Listen/Listen.js
@@ -1,12 +1,8 @@
 import { NodeWorkerRpcClient } from '@lvce-editor/rpc'
-import * as CliProcess from '../CliProcess/CliProcess.js'
 import * as CommandMap from '../CommandMap/CommandMap.js'
-import * as CommandMapRef from '../CommandMapRef/CommandMapRef.js'
 
 export const listen = async () => {
-  Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
-  const rpc = await NodeWorkerRpcClient.create({
-    commandMap: CommandMapRef.commandMapRef,
+  await NodeWorkerRpcClient.create({
+    commandMap: CommandMap.commandMap,
   })
-  CliProcess.set(rpc)
 }
