71bc923f51fcb6682d9d8bfc97a2fafa537a4b9e
Simon Siefke <simon.siefke@gmail.com>
2025-08-07 23:19:38 +0200

    feature: add stdout worker to prepare for custom github actions formatter

---
+++ packages/stdout-worker/src/parts/GetHandleTestPassedMessage/GetHandleTestPassedMessage.ts
@@ -0,0 +1,15 @@
+import chalk from 'chalk'
+import * as FormatAsSeconds from '../FormatAsSeconds/FormatAsSeconds.ts'
+import * as TestPrefix from '../TestPrefix/TestPrefix.ts'
+
+const formatDuration = (duration) => {
+  return `(${FormatAsSeconds.formatAsSeconds(duration)})`
+}
+
+export const getHandleTestPassedMessage = (file, relativeDirName, fileName, duration, isLeak) => {
+  const messageRelativeDirName = chalk.dim(relativeDirName + '/')
+  const messageFileName = chalk.bold(fileName)
+  const messageDuration = formatDuration(duration)
+  const prefix = isLeak ? TestPrefix.Leak : TestPrefix.Pass
+  return `${prefix} ${messageRelativeDirName}${messageFileName} ${messageDuration}\n`
+}
---
+++ packages/stdout-worker/src/parts/GetHandleTestSetupMessage/GetHandleTestSetupMessage.ts
@@ -0,0 +1,5 @@
+import * as TestPrefix from '../TestPrefix/TestPrefix.ts'
+
+export const getHandleTestSetupMessage = () => {
+  return `\n${TestPrefix.Setup}\n`
+}
