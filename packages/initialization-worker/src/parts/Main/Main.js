e1f69c947942cec1f27f4784513d717c4826f4ca
Simon Siefke <simon.siefke@gmail.com>
2025-08-07 19:37:29 +0200

    dispose

--- packages/initialization-worker/src/parts/Main/Main.js
+++ packages/initialization-worker/src/parts/Main/Main.js
@@ -1,5 +1,17 @@
+import { parentPort } from 'node:worker_threads'
 import * as Listen from '../Listen/Listen.js'
+import * as Disposables from '../Disposables/Disposables.js'
 
 export const main = async () => {
+  process.on('exit', async () => {
+    console.log('exiting')
+    await Disposables.disposeAll()
+  })
+  process.on('disconnect', () => {
+    console.log('disocnnect')
+  })
+  parentPort.on('close', () => {
+    console.log('parent gone')
+  })
   await Listen.listen()
 }
