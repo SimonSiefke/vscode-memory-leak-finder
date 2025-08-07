61c5eed032f27028da7a85959987114b2df64af4
Simon Siefke <simon.siefke@gmail.com>
2025-08-07 21:02:35 +0200

    format

--- packages/heap-snapshot-parsing-worker/src/parts/ParseHeapSnapshotArrayHeader/ParseHeapSnapshotArrayHeader.ts
+++ packages/heap-snapshot-parsing-worker/src/parts/ParseHeapSnapshotArrayHeader/ParseHeapSnapshotArrayHeader.ts
@@ -1,14 +1,11 @@
 export const parseHeapSnapshotArrayHeader = (data, name) => {
   const token = `"${name}":`
-
   const tokenIndex = data.indexOf(token)
-
   if (tokenIndex === -1) {
     return -1
   }
   const startIndex = tokenIndex + token.length
   const bracketIndex = data.indexOf('[', startIndex)
-
   if (bracketIndex === -1) {
     return -1
   }
