94cc036ea1682cb646f66b94c1f22e45a3f3a6e7
Simon Siefke <simon.siefke@gmail.com>
2025-08-07 20:19:42 +0200

    log

--- packages/test-coordinator/src/parts/Exit/Exit.js
+++ packages/test-coordinator/src/parts/Exit/Exit.js
@@ -1,15 +1,9 @@
 import * as Disposables from '../Disposables/Disposables.js'
 import * as ExitCode from '../ExitCode/ExitCode.js'
 import * as Process from '../Process/Process.js'
-import * as Time from '../Time/Time.js'
 
 export const exit = async () => {
-  const s = Time.now()
   await Disposables.disposeAll()
-  const e = Time.now()
-  console.log(`finished worker cleanup in ${e - s}ms`)
-  const used = process.memoryUsage().heapUsed / 1024 / 1024
-  console.log(`worker uses approximately ${Math.round(used * 100) / 100} MB`)
   setTimeout(() => {
     Process.exit(ExitCode.Success)
   }, 0)
