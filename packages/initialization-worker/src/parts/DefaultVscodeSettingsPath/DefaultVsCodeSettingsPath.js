90099763cb9d82171964ca4ec97ffe446e126fed
Simon Siefke <simon.siefke@gmail.com>
2025-08-07 16:57:24 +0200

    feature: add initialization worker (#1613)

---
+++ packages/initialization-worker/src/parts/DefaultVscodeSettingsPath/DefaultVsCodeSettingsPath.js
@@ -0,0 +1,12 @@
+import { join } from 'node:path'
+import * as Root from '../Root/Root.js'
+
+export const defaultVsCodeSettingsPath = join(
+  Root.root,
+  'packages',
+  'test-coordinator',
+  'src',
+  'parts',
+  'DefaultVsCodeSettings',
+  'DefaultVsCodeSettings.json',
+)
