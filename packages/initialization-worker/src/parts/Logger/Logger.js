bf9631e24b5643a4b26d509eb805bb40a4bb86e7
Simon Siefke <simon.siefke@gmail.com>
2025-08-07 19:55:03 +0200

    remove log

--- packages/initialization-worker/src/parts/Logger/Logger.js
+++ packages/initialization-worker/src/parts/Logger/Logger.js
@@ -1,10 +1,3 @@
-import { createWriteStream } from 'node:fs'
-
-const logPath = '/home/simon/.cache/repos/vscode-memory-leak-finder/.vscode-charts/log.txt'
-
-const stream = createWriteStream(logPath)
-
 export const log = (...args) => {
-  stream.write(JSON.stringify(args))
   console.log(...args)
 }
--- packages/test-coordinator/src/parts/Exit/Exit.js
+++ packages/test-coordinator/src/parts/Exit/Exit.js
@@ -1,7 +1,7 @@
+import * as Disposables from '../Disposables/Disposables.js'
+import * as ExitCode from '../ExitCode/ExitCode.js'
 import * as Process from '../Process/Process.js'
 import * as Time from '../Time/Time.js'
-import * as ExitCode from '../ExitCode/ExitCode.js'
-import * as Disposables from '../Disposables/Disposables.js'
 
 export const exit = async () => {
   const s = Time.now()
