96cd658883618a35c53df98855a0aa6ca799c969
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 17:35:53 +0200

    simplify

---
+++ packages/heap-snapshot-worker/src/parts/GetActualValue/GetActualValue.ts
@@ -0,0 +1,208 @@
+import type { Snapshot } from '../Snapshot/Snapshot.js'
+import { getNodeEdges } from '../GetNodeEdges/GetNodeEdges.ts'
+
+/**
+ * Gets the actual value of a node by following references for strings and numbers
+ * @param targetNode - The target node to get the value for
+ * @param snapshot - The heap snapshot data
+ * @param edgeMap - The edge map for fast lookups
+ * @param visited - Set of visited node IDs to prevent circular references
+ * @returns The actual value as a string
+ */
+export const getActualValue = (
+  targetNode: any,
+  snapshot: Snapshot,
+  edgeMap: Uint32Array,
+  visited: Set<number> = new Set(),
+): string => {
+  if (!targetNode || visited.has(targetNode.id)) {
+    return `[Circular ${targetNode?.id || 'Unknown'}]`
+  }
+  visited.add(targetNode.id)
+
+  const { nodes, edges, strings, meta } = snapshot
+  const nodeFields = meta.node_fields
+  const nodeTypes = meta.node_types
+  const edgeFields = meta.edge_fields
+
+  const ITEMS_PER_NODE = nodeFields.length
+  const ITEMS_PER_EDGE = edgeFields.length
+
+  // Get field indices
+  const typeFieldIndex = nodeFields.indexOf('type')
+  const nameFieldIndex = nodeFields.indexOf('name')
+  const idFieldIndex = nodeFields.indexOf('id')
+  const edgeCountFieldIndex = nodeFields.indexOf('edge_count')
+
+  const edgeTypeFieldIndex = edgeFields.indexOf('type')
+  const edgeNameFieldIndex = edgeFields.indexOf('name_or_index')
+  const edgeToNodeFieldIndex = edgeFields.indexOf('to_node')
+
+  // Get edge type names
+  const edgeTypes = meta.edge_types[0] || []
+  const EDGE_TYPE_PROPERTY = edgeTypes.indexOf('property')
+  const EDGE_TYPE_INTERNAL = edgeTypes.indexOf('internal')
+
+  // Get node type names
+  const nodeTypeNames = nodeTypes[0] || []
+  const NODE_TYPE_STRING = nodeTypeNames.indexOf('string')
+  const NODE_TYPE_NUMBER = nodeTypeNames.indexOf('number')
+  const NODE_TYPE_OBJECT = nodeTypeNames.indexOf('object')
+  const NODE_TYPE_ARRAY = nodeTypeNames.indexOf('array')
+
+  const nodeType = targetNode.type
+  const nodeTypeName = getNodeTypeName(targetNode, nodeTypes)
+
+  // For strings, return the actual string value
+  if (nodeType === NODE_TYPE_STRING) {
+    const stringValue = getNodeName(targetNode, strings)
+    return stringValue || `[String ${targetNode.id}]`
+  }
+
+  // For numbers, return the actual number value
+  if (nodeType === NODE_TYPE_NUMBER) {
+    const numberValue = targetNode.name?.toString()
+    return numberValue || `[Number ${targetNode.id}]`
+  }
+
+  // For code objects, try to follow internal references to find string/number values
+  if (nodeTypeName === 'code') {
+    // Find the node index for this target node
+    let targetNodeIndex = -1
+    for (let i = 0; i < nodes.length; i += ITEMS_PER_NODE) {
+      if (nodes[i + idFieldIndex] === targetNode.id) {
+        targetNodeIndex = i / ITEMS_PER_NODE
+        break
+      }
+    }
+
+    if (targetNodeIndex !== -1) {
+      // Get edges using the edge map for fast lookup
+      const nodeEdges = getNodeEdges(targetNodeIndex, edgeMap, nodes, edges, nodeFields, edgeFields)
+
+      // Collect all string/number values from internal edges
+      const internalStringValues: string[] = []
+      const incomingStringValues: string[] = []
+      const numberValues: string[] = []
+
+      // Check edges from this code object
+      for (const edge of nodeEdges) {
+        // Follow internal edges to find string/number/object/array values
+        if (edge.type === EDGE_TYPE_INTERNAL) {
+          const referencedNode = parseNode(edge.toNode, nodes, nodeFields)
+          if (referencedNode) {
+            const referencedType = referencedNode.type
+
+            if (referencedType === NODE_TYPE_STRING) {
+              // string
+              const stringValue = getNodeName(referencedNode, strings)
+              if (stringValue) {
+                internalStringValues.push(stringValue)
+              }
+            } else if (referencedType === NODE_TYPE_NUMBER) {
+              // number
+              const numberValue = referencedNode.name?.toString()
+              if (numberValue) {
+                numberValues.push(numberValue)
+              }
+            } else if (referencedType === NODE_TYPE_OBJECT) {
+              // object
+              return `[Object ${referencedNode.id}]`
+            } else if (referencedType === NODE_TYPE_ARRAY) {
+              // array
+              return `[Array ${referencedNode.id}]`
+            }
+          }
+        }
+      }
+
+      // Also check incoming references to see if any have string names
+      let currentEdgeOffset = 0
+      for (let sourceNodeIndex = 0; sourceNodeIndex < nodes.length; sourceNodeIndex += ITEMS_PER_NODE) {
+        const sourceEdgeCount = nodes[sourceNodeIndex + edgeCountFieldIndex]
+
+        for (let j = 0; j < sourceEdgeCount; j++) {
+          const edgeIndex = (currentEdgeOffset + j) * ITEMS_PER_EDGE
+          const edgeToNode = edges[edgeIndex + edgeToNodeFieldIndex]
+
+          if (edgeToNode === targetNodeIndex) {
+            const edgeType = edges[edgeIndex + edgeTypeFieldIndex]
+            const edgeNameIndex = edges[edgeIndex + edgeNameFieldIndex]
+
+            // If this is an internal edge with a string name, that might be the value
+            if (edgeType === EDGE_TYPE_INTERNAL && edgeNameIndex < strings.length) {
+              const edgeName = strings[edgeNameIndex]
+              if (edgeName && edgeName !== '') {
+                incomingStringValues.push(edgeName)
+              }
+            }
+          }
+        }
+        currentEdgeOffset += sourceEdgeCount
+      }
+
+      // Prioritize incoming references over internal references
+      // Look for specific meaningful values first, then return the first available
+      const allIncomingValues = [...incomingStringValues]
+      const allInternalValues = [...internalStringValues]
+
+      // Prioritize "1" if it exists in incoming references
+      const oneIndex = allIncomingValues.indexOf('1')
+      if (oneIndex !== -1) {
+        return '"1"'
+      }
+
+      // Return the first incoming string value found, or first internal string value, or first number value
+      if (allIncomingValues.length > 0) {
+        return `"${allIncomingValues[0]}"`
+      } else if (allInternalValues.length > 0) {
+        return `"${allInternalValues[0]}"`
+      } else if (numberValues.length > 0) {
+        return numberValues[0]
+      }
+    }
+  }
+
+  // For other types, return the standard format
+  if (nodeType === NODE_TYPE_OBJECT) {
+    return `[Object ${targetNode.id}]`
+  } else if (nodeType === NODE_TYPE_ARRAY) {
+    return `[Array ${targetNode.id}]`
+  } else {
+    return `[${nodeTypeName || 'Unknown'} ${targetNode.id}]`
+  }
+}
+
+// Helper function to parse a node from the flat array
+const parseNode = (nodeIndex: number, nodes: Uint32Array, nodeFields: readonly string[]): any => {
+  const ITEMS_PER_NODE = nodeFields.length
+  const nodeStart = nodeIndex * ITEMS_PER_NODE
+  if (nodeStart >= nodes.length) {
+    return null
+  }
+
+  const node: any = {}
+  for (let i = 0; i < nodeFields.length; i++) {
+    const fieldIndex = nodeStart + i
+    if (fieldIndex < nodes.length) {
+      node[nodeFields[i]] = nodes[fieldIndex]
+    }
+  }
+  return node
+}
+
+// Helper function to get node name as string
+const getNodeName = (node: any, strings: readonly string[]): string | null => {
+  if (node && node.name !== undefined && strings[node.name]) {
+    return strings[node.name]
+  }
+  return null
+}
+
+// Helper function to get node type name
+const getNodeTypeName = (node: any, nodeTypes: readonly (readonly string[])[]): string | null => {
+  if (nodeTypes[0] && Array.isArray(nodeTypes[0]) && node.type !== undefined) {
+    return (nodeTypes[0] as readonly string[])[node.type]
+  }
+  return null
+}
--- packages/heap-snapshot-worker/src/parts/GetObjectsWithPropertiesInternal/GetObjectsWithPropertiesInternal.ts
+++ packages/heap-snapshot-worker/src/parts/GetObjectsWithPropertiesInternal/GetObjectsWithPropertiesInternal.ts
@@ -1,6 +1,7 @@
 import type { Snapshot } from '../Snapshot/Snapshot.js'
 import { createEdgeMap } from '../CreateEdgeMap/CreateEdgeMap.ts'
 import { getNodeEdges } from '../GetNodeEdges/GetNodeEdges.ts'
+import { getActualValue } from '../GetActualValue/GetActualValue.ts'
 
 export interface ObjectWithProperty {
   id: number
@@ -49,6 +50,10 @@ export const getObjectsWithPropertiesInternal = (snapshot: Snapshot, propertyNam
   const ITEMS_PER_NODE = nodeFields.length
   const ITEMS_PER_EDGE = edgeFields.length
 
+  // Get edge type names
+  const edgeTypes = meta.edge_types[0] || []
+  const EDGE_TYPE_PROPERTY = edgeTypes.indexOf('property')
+
   // Create edge map for fast lookups
   const edgeMap = createEdgeMap(nodes, nodeFields)
 
@@ -85,145 +90,14 @@ export const getObjectsWithPropertiesInternal = (snapshot: Snapshot, propertyNam
     return null
   }
 
-  // Helper function to follow references and get actual value
-  const getActualValue = (targetNode: any, visited: Set<number> = new Set()): string => {
-    if (!targetNode || visited.has(targetNode.id)) {
-      return `[Circular ${targetNode?.id || 'Unknown'}]`
-    }
-    visited.add(targetNode.id)
-
-    const nodeType = targetNode.type
-    const nodeTypeName = getNodeTypeName(targetNode)
-
-    // For strings, return the actual string value
-    if (nodeType === 2) {
-      const stringValue = getNodeName(targetNode)
-      return stringValue || `[String ${targetNode.id}]`
-    }
-
-    // For numbers, return the actual number value
-    if (nodeType === 7) {
-      const numberValue = targetNode.name?.toString()
-      return numberValue || `[Number ${targetNode.id}]`
-    }
-
-    // For code objects, try to follow internal references to find string/number values
-    if (nodeTypeName === 'code') {
-      // Find the node index for this target node
-      let targetNodeIndex = -1
-      for (let i = 0; i < nodes.length; i += ITEMS_PER_NODE) {
-        if (nodes[i + idFieldIndex] === targetNode.id) {
-          targetNodeIndex = i / ITEMS_PER_NODE
-          break
-        }
-      }
-
-      if (targetNodeIndex !== -1) {
-        // Get edges using the edge map for fast lookup
-        const nodeEdges = getNodeEdges(targetNodeIndex, edgeMap, nodes, edges, nodeFields, edgeFields)
-
-        // Collect all string/number values from internal edges
-        const internalStringValues: string[] = []
-        const incomingStringValues: string[] = []
-        const numberValues: string[] = []
-
-        // Check edges from this code object
-        for (const edge of nodeEdges) {
-          // Follow internal edges to find string/number/object/array values
-          if (edge.type === 3) {
-            // internal edge
-            const referencedNode = parseNode(edge.toNode)
-            if (referencedNode) {
-              const referencedType = referencedNode.type
-
-              if (referencedType === 2) {
-                // string
-                const stringValue = getNodeName(referencedNode)
-                if (stringValue) {
-                  internalStringValues.push(stringValue)
-                }
-              } else if (referencedType === 7) {
-                // number
-                const numberValue = referencedNode.name?.toString()
-                if (numberValue) {
-                  numberValues.push(numberValue)
-                }
-              } else if (referencedType === 3) {
-                // object
-                return `[Object ${referencedNode.id}]`
-              } else if (referencedType === 1) {
-                // array
-                return `[Array ${referencedNode.id}]`
-              }
-            }
-          }
-        }
-
-        // Also check incoming references to see if any have string names
-        let currentEdgeOffset = 0
-        for (let sourceNodeIndex = 0; sourceNodeIndex < nodes.length; sourceNodeIndex += ITEMS_PER_NODE) {
-          const sourceEdgeCount = nodes[sourceNodeIndex + edgeCountFieldIndex]
-
-          for (let j = 0; j < sourceEdgeCount; j++) {
-            const edgeIndex = (currentEdgeOffset + j) * ITEMS_PER_EDGE
-            const edgeToNode = edges[edgeIndex + edgeToNodeFieldIndex]
-
-            if (edgeToNode === targetNodeIndex) {
-              const edgeType = edges[edgeIndex + edgeTypeFieldIndex]
-              const edgeNameIndex = edges[edgeIndex + edgeNameFieldIndex]
-
-              // If this is an internal edge with a string name, that might be the value
-              if (edgeType === 3 && edgeNameIndex < strings.length) {
-                const edgeName = strings[edgeNameIndex]
-                if (edgeName && edgeName !== '') {
-                  incomingStringValues.push(edgeName)
-                }
-              }
-            }
-          }
-          currentEdgeOffset += sourceEdgeCount
-        }
-
-        // Prioritize incoming references over internal references
-        // Look for specific meaningful values first, then return the first available
-        const allIncomingValues = [...incomingStringValues]
-        const allInternalValues = [...internalStringValues]
-
-        // Prioritize "1" if it exists in incoming references
-        const oneIndex = allIncomingValues.indexOf('1')
-        if (oneIndex !== -1) {
-          return '"1"'
-        }
-
-        // Return the first incoming string value found, or first internal string value, or first number value
-        if (allIncomingValues.length > 0) {
-          return `"${allIncomingValues[0]}"`
-        } else if (allInternalValues.length > 0) {
-          return `"${allInternalValues[0]}"`
-        } else if (numberValues.length > 0) {
-          return numberValues[0]
-        }
-      }
-    }
-
-    // For other types, return the standard format
-    if (nodeType === 3) {
-      return `[Object ${targetNode.id}]`
-    } else if (nodeType === 1) {
-      return `[Array ${targetNode.id}]`
-    } else {
-      return `[${nodeTypeName || 'Unknown'} ${targetNode.id}]`
-    }
-  }
-
   // Iterate through each node and scan its edges
   for (let nodeIndex = 0; nodeIndex < nodes.length; nodeIndex += ITEMS_PER_NODE) {
     const nodeEdges = getNodeEdges(nodeIndex / ITEMS_PER_NODE, edgeMap, nodes, edges, nodeFields, edgeFields)
 
     // Scan this node's edges
     for (const edge of nodeEdges) {
-      // Check if it's a property edge (type 2) with the target property name
-      if (edge.type === 2 && edge.nameIndex === propertyNameIndex) {
+      // Check if it's a property edge with the target property name
+      if (edge.type === EDGE_TYPE_PROPERTY && edge.nameIndex === propertyNameIndex) {
         // Parse the source node (the object that has the property)
         const sourceNode = parseNode(nodeIndex / ITEMS_PER_NODE)
         // Parse the target node (the property value)
@@ -240,7 +114,7 @@ export const getObjectsWithPropertiesInternal = (snapshot: Snapshot, propertyNam
           }
 
           // Get the actual value by following references
-          result.propertyValue = getActualValue(targetNode)
+          result.propertyValue = getActualValue(targetNode, snapshot, edgeMap)
 
           results.push(result)
         }
