f3b290ba092de56032d9e56b822ef779bb4b6e2a
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 10:17:50 +0200

    parse

--- packages/heap-snapshot-worker/src/parts/GetBigintObjectsFromHeapSnapshotInternal/GetBigintObjectsFromHeapSnapshotInternal.js
+++ packages/heap-snapshot-worker/src/parts/GetBigintObjectsFromHeapSnapshotInternal/GetBigintObjectsFromHeapSnapshotInternal.js
@@ -1,16 +1,12 @@
 import { computeHeapSnapshotIndices } from '../ComputeHeapSnapshotIndices/ComputeHeapSnapshotIndices.js'
 
 /**
- * @param {Array} strings
- * @param {Uint32Array} nodes
- * @param {Array} node_types
- * @param {Array} node_fields
- * @param {Uint32Array} edges
- * @param {Array} edge_types
- * @param {Array} edge_fields
+ * @param {any} snapshot
  * @returns {Array}
  */
-export const getBigintObjectsFromHeapSnapshotInternal = (strings, nodes, node_types, node_fields, edges, edge_types, edge_fields) => {
+export const getBigintObjectsFromHeapSnapshotInternal = (snapshot) => {
+  const { nodes, strings, edges, metaData } = snapshot
+  const { node_types, node_fields, edge_types, edge_fields } = metaData.data.meta
   const {
     bigintTypeIndex,
     ITEMS_PER_NODE,
