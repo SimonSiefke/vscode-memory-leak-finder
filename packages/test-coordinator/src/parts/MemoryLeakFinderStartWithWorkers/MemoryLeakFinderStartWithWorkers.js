d73389f2fd4b515dd108ec3aedc7a8c4d7afffa5
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 23:49:28 +0200

    remove

--- packages/test-coordinator/src/parts/MemoryLeakFinderStartWithWorkers/MemoryLeakFinderStartWithWorkers.js
+++
@@ -1,9 +0,0 @@
-import * as Assert from '../Assert/Assert.js'
-import * as TestWorkerCommandType from '../TestWorkerCommandType/TestWorkerCommandType.js'
-
-export const startWithWorkers = (rpc, connectionId, targetId) => {
-  Assert.object(rpc)
-  Assert.number(connectionId)
-  Assert.string(targetId)
-  return rpc.invoke(TestWorkerCommandType.MemoryLeakFinderStartWithWorkers, connectionId, targetId)
-}
--- packages/test-coordinator/src/parts/MemoryLeakFinderStopWithWorkers/MemoryLeakFinderStopWithWorkers.js
+++
@@ -1,9 +0,0 @@
-import * as Assert from '../Assert/Assert.js'
-import * as TestWorkerCommandType from '../TestWorkerCommandType/TestWorkerCommandType.js'
-
-export const stopWithWorkers = (rpc, connectionId, targetId) => {
-  Assert.object(rpc)
-  Assert.number(connectionId)
-  Assert.string(targetId)
-  return rpc.invoke(TestWorkerCommandType.MemoryLeakFinderStopWithWorkers, connectionId, targetId)
-}
