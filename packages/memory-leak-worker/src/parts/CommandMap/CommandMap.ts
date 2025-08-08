bdf3c826da2695c9400325b44c1a19d0779b86b3
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 23:51:40 +0200

    tsc

--- packages/memory-leak-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/memory-leak-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,5 @@
 import * as ConnectDevtools from '../ConnectDevtools/ConnectDevtools.ts'
 import * as MemoryLeakFinderCompare from '../MemoryLeakFinderCompare/MemoryLeakFinderCompare.ts'
-import * as MemoryLeakFinderCompareWithWorkers from '../MemoryLeakFinderCompareWithWorkers/MemoryLeakFinderCompareWithWorkers.ts'
 import * as MemoryLeakFinderSetup from '../MemoryLeakFinderSetup/MemoryLeakFinderSetup.ts'
 import * as MemoryLeakFinderStart from '../MemoryLeakFinderStart/MemoryLeakFinderStart.ts'
 import * as MemoryLeakFinderStop from '../MemoryLeakFinderStop/MemoryLeakFinderStop.ts'
@@ -9,7 +8,6 @@ import * as MemoryLeakWorkerCommandType from '../MemoryLeakWorkerCommandType/Mem
 export const commandMap: Record<string, any> = {
   [MemoryLeakWorkerCommandType.ConnectDevtools]: ConnectDevtools.connectDevtools,
   [MemoryLeakWorkerCommandType.MemoryLeakFinderCompare]: MemoryLeakFinderCompare.compare,
-  [MemoryLeakWorkerCommandType.MemoryLeakFinderCompareWithWorkers]: MemoryLeakFinderCompareWithWorkers.compareWithWorkers,
   [MemoryLeakWorkerCommandType.MemoryLeakFinderSetup]: MemoryLeakFinderSetup.setup,
   [MemoryLeakWorkerCommandType.MemoryLeakFinderStart]: MemoryLeakFinderStart.start,
   [MemoryLeakWorkerCommandType.MemoryLeakFinderStop]: MemoryLeakFinderStop.stop,
