4a28a9ee4e3ecfbee18dc80156e432bf4146192c
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 10:54:29 +0200

    tsc

--- packages/heap-snapshot-worker/src/parts/GetThingCountFromHeapSnapshot/GetThingCountFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetThingCountFromHeapSnapshot/GetThingCountFromHeapSnapshot.js
@@ -2,9 +2,16 @@ import { computeHeapSnapshotIndices } from '../ComputeHeapSnapshotIndices/Comput
 import { getThingCountFromHeapSnapshotInternal } from '../GetObjectCountFromHeapSnapshotInternal/GetThingCountFromHeapSnapshotInternal.js'
 import { prepareHeapSnapshot } from '../PrepareHeapSnapshot/PrepareHeapSnapshot.js'
 
+/**
+ *
+ * @param {import('../Snapshot/Snapshot.ts').Snapshot} snapshot
+ * @param {*} typeName
+ * @param {*} objectName
+ * @returns
+ */
 export const getThingCountFromHeapSnapshot = (snapshot, typeName, objectName) => {
-  const { nodes, strings, metaData } = snapshot
-  const { node_types, node_fields, edge_types, edge_fields } = metaData.data.meta
+  const { nodes, strings, meta } = snapshot
+  const { node_types, node_fields, edge_types, edge_fields } = meta
   const typeIndex = node_types[0].indexOf(typeName)
   const { ITEMS_PER_EDGE, typeFieldIndex, nameFieldIndex } = computeHeapSnapshotIndices(node_types, node_fields, edge_types, edge_fields)
   const count = getThingCountFromHeapSnapshotInternal(nodes, strings, ITEMS_PER_EDGE, typeFieldIndex, nameFieldIndex, typeIndex, objectName)
