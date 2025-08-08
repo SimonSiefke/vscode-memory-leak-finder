492fe38457c1a2b77741bc42f3058c779ac7092b
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 14:44:14 +0200

    tsc

--- packages/heap-snapshot-worker/src/parts/GetNamedMapCountFromHeapSnapshotInternal/GetNamedMapCountFromHeapSnapshotInternal.js
+++ packages/heap-snapshot-worker/src/parts/GetNamedMapCountFromHeapSnapshotInternal/GetNamedMapCountFromHeapSnapshotInternal.js
@@ -5,46 +5,47 @@
  */
 export const getNamedMapCountFromHeapSnapshotInternal = (snapshot) => {
   const { nodes, strings, edges, meta } = snapshot
-  const { node_types, node_fields, edge_types, edge_fields } = meta
-  
+  const { node_fields, edge_fields } = meta
+
   const ITEMS_PER_NODE = node_fields.length
   const ITEMS_PER_EDGE = edge_fields.length
-  
+
   // Find the index of "Map" in the strings array
   const mapStringIndex = strings.indexOf('Map')
   if (mapStringIndex === -1) {
     return []
   }
-  
+
   // Find all Map objects
   const mapNodes = []
   for (let i = 0; i < nodes.length; i += ITEMS_PER_NODE) {
     const nodeType = nodes[i]
     const nodeNameIndex = nodes[i + 1]
     const nodeId = nodes[i + 2]
-    
-    if (nodeType === 4 && nodeNameIndex === mapStringIndex) { // type 4 = object
+
+    if (nodeType === 4 && nodeNameIndex === mapStringIndex) {
+      // type 4 = object
       mapNodes.push({
         nodeIndex: i / ITEMS_PER_NODE,
         id: nodeId,
-        edgeCount: nodes[i + 4]
+        edgeCount: nodes[i + 4],
       })
     }
   }
-  
+
   // For each Map object, find its names and size
   const result = []
-  
+
   for (const mapNode of mapNodes) {
     const names = new Set()
     let size = 0
-    
+
     // Find edges that point to this Map object
     for (let i = 0; i < edges.length; i += ITEMS_PER_EDGE) {
       const edgeType = edges[i]
       const edgeNameOrIndex = edges[i + 1]
       const edgeToNode = edges[i + 2]
-      
+
       if (edgeToNode === mapNode.nodeIndex) {
         // This edge points to our Map object
         if (edgeNameOrIndex < strings.length) {
@@ -55,11 +56,11 @@ export const getNamedMapCountFromHeapSnapshotInternal = (snapshot) => {
         }
       }
     }
-    
+
     // Find edges from this Map object to count its entries
     const mapNodeStart = mapNode.nodeIndex * ITEMS_PER_NODE
     const edgeCount = nodes[mapNodeStart + 4]
-    
+
     // Count the number of key-value pairs by looking at edges from this Map
     let edgeIndex = 0
     for (let i = 0; i < edges.length; i += ITEMS_PER_EDGE) {
@@ -67,26 +68,26 @@ export const getNamedMapCountFromHeapSnapshotInternal = (snapshot) => {
       if (edgeFromNode === mapNode.nodeIndex) {
         edgeIndex++
         if (edgeIndex > edgeCount) break
-        
+
         // Count element edges (key-value pairs)
         const edgeType = edges[i]
-        if (edgeType === 1) { // element edge
+        if (edgeType === 1) {
+          // element edge
           size++
         }
       }
     }
-    
+
     // Convert names Set to array or string
     const nameArray = Array.from(names)
     const name = nameArray.length === 1 ? nameArray[0] : nameArray
-    
+
     result.push({
       id: mapNode.id,
       name: name,
-      size: size
+      size: size,
     })
   }
-  
+
   return result
 }
-
