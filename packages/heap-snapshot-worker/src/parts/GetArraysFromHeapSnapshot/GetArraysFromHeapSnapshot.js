d3566be4fe666bed9b33bd2ff9a198bb12b61c89
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 11:02:10 +0200

    tsc

--- packages/heap-snapshot-worker/src/parts/GetArraysFromHeapSnapshot/GetArraysFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetArraysFromHeapSnapshot/GetArraysFromHeapSnapshot.js
@@ -1,17 +1,17 @@
-import { prepareHeapSnapshot } from '../PrepareHeapSnapshot/PrepareHeapSnapshot.js'
 import { getArraysFromHeapSnapshotInternal } from '../GetArraysFromHeapSnapshotInternal/GetArraysFromHeapSnapshotInternal.js'
 import * as ParseHeapSnapshot from '../ParseHeapSnapshot/ParseHeapSnapshot.js'
+import { prepareHeapSnapshot } from '../PrepareHeapSnapshot/PrepareHeapSnapshot.js'
 
 export const getArraysFromHeapSnapshot = async (pathUri) => {
   const snapshot = await prepareHeapSnapshot(pathUri, {
     parseStrings: true,
   })
 
-  const { nodes, strings, edges, metaData } = snapshot
-  const { node_types, node_fields, edge_types, edge_fields } = metaData.data.meta
+  const { nodes, strings, edges, meta } = snapshot
+  const { node_types, node_fields, edge_types, edge_fields } = meta
 
   // Also get parsed nodes and graph for name mapping
-  const { parsedNodes, graph } = ParseHeapSnapshot.parseHeapSnapshot({ nodes, strings, edges, snapshot: metaData.data })
+  const { parsedNodes, graph } = ParseHeapSnapshot.parseHeapSnapshot({ nodes, strings, edges, snapshot: { meta } })
 
   return getArraysFromHeapSnapshotInternal(strings, nodes, node_types, node_fields, edges, edge_types, edge_fields, parsedNodes, graph)
 }
