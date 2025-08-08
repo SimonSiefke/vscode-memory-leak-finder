e33659d7c98b9ca979f97182078cdcaf8bb61a5a
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 11:19:57 +0200

    log

--- packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
+++ packages/heap-snapshot-worker/src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js
@@ -9,7 +9,5 @@ export const getRegexpObjectsFromHeapSnapshot = async (pathUri) => {
   const snapshot = await prepareHeapSnapshot(pathUri, {
     parseStrings: true,
   })
-
-  console.log({ snapshot: snapshot.snapshot })
   return getRegexpObjectsFromHeapSnapshotInternal(snapshot)
 }
