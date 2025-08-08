3b492da86a2609336ccd5c83b969361427acc812
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 08:54:02 +0200

    feature: remove console log (#1629)

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotStringsCount/ParseHeapSnapshotStringsCount.js
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotStringsCount/ParseHeapSnapshotStringsCount.js
@@ -4,7 +4,6 @@ export const parseHeapSnapshotStringsCount = async (path) => {
   const snapshot = await prepareHeapSnapshot(path, {
     parseStrings: true,
   })
-  console.log({ snapshot })
   const { strings } = snapshot
   return strings.length
 }
