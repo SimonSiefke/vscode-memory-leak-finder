1721a64402a454c0ce93d47756ba9c00172f8b7f
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 10:53:12 +0200

    tsc

--- packages/heap-snapshot-worker/test/GetStringsInternal.test.ts
+++ packages/heap-snapshot-worker/test/GetStringsInternal.test.ts
@@ -7,7 +7,13 @@ test('getStrings - extracts strings array from heap snapshot file', async () => 
     meta: {
       node_types: [['hidden', 'array', 'string', 'object']],
       node_fields: ['type', 'name', 'id'],
+      edge_fields: [],
+      edge_types: [],
+      location_fields: [],
     },
+    node_count: 1,
+    edge_count: 1,
+    extra_native_bytes: 0,
     nodes: new Uint32Array([0, 0, 0]),
     edges: new Uint32Array([]),
     strings: ['first string', 'second string', '(heap number)'],
