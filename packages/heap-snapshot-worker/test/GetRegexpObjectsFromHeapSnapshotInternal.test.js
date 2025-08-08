00e456731240fa1c23f9dd70cb5c80afa4e2c82c
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 10:06:14 +0200

    test

--- packages/heap-snapshot-worker/test/GetRegexpObjectsFromHeapSnapshotInternal.test.js
+++ packages/heap-snapshot-worker/test/GetRegexpObjectsFromHeapSnapshotInternal.test.js
@@ -1,32 +1,100 @@
 import { expect, test } from '@jest/globals'
-import { getRegexpObjectsFromHeapSnapshot } from '../src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js'
-import { join } from 'path'
+import * as GetRegexpObjectsFromHeapSnapshot from '../src/parts/GetRegexpObjectsFromHeapSnapshot/GetRegexpObjectsFromHeapSnapshot.js'
+import { getRegexpObjectsFromHeapSnapshotInternal } from '../src/parts/GetRegexpObjectsFromHeapSnapshotInternal/GetRegexpObjectsFromHeapSnapshotInternal.js'
 
-test.skip('should return array of regex objects from heap snapshot', async () => {
-  const path = join(process.cwd(), '..', '..', '.vscode-heapsnapshots', '0.json')
-  const regexObjects = await getRegexpObjectsFromHeapSnapshot(path)
-
-  expect(Array.isArray(regexObjects)).toBe(true)
-  expect(regexObjects.length).toBe(435)
-
-  // Check that objects have the expected structure
-  regexObjects.forEach(obj => {
-    expect(obj).toHaveProperty('id')
-    expect(obj).toHaveProperty('pattern')
-    expect(typeof obj.id).toBe('number')
-    expect(typeof obj.pattern).toBe('string')
-  })
-
-  // Check that we have some expected regex patterns
-  const hasExpectedPatterns = regexObjects.some(obj =>
-    obj.pattern.includes('\\d+') ||
-    obj.pattern.includes('\\s') ||
-    obj.pattern.includes('.*')
-  )
-  expect(hasExpectedPatterns).toBe(true)
-
-  // Check that objects are sorted by pattern length
-  for (let i = 1; i < regexObjects.length; i++) {
-    expect(regexObjects[i].pattern.length).toBeGreaterThanOrEqual(regexObjects[i-1].pattern.length)
+test('getRegexpObjectsFromHeapSnapshot - no regexp objects', async () => {
+  // prettier-ignore
+  const testData = {
+    snapshot: {
+      meta: {
+        node_types: [['hidden', 'array', 'string', 'object']],
+        node_fields: ['type', 'name', 'id', 'self_size', 'edge_count', 'trace_node_id', 'detachedness'],
+        edge_types: [['context', 'element', 'property', 'internal', 'hidden', 'shortcut', 'weak']],
+        edge_fields: ['type', 'name_or_index', 'to_node'],
+        location_fields: ['object_index', 'script_id', 'line', 'column'],
+      },
+      node_count: 1,
+      edge_count: 0,
+    },
+    nodes: [
+      0, 1, 100, 64, 0, 0, 0, // hidden object
+    ],
+    edges: [],
+    locations: [],
+    strings: ['', 'test'],
   }
+  const result = getRegexpObjectsFromHeapSnapshotInternal(testData)
+  expect(result).toEqual([])
+})
+
+test('getRegexpObjectsFromHeapSnapshot - single regexp object', async () => {
+  // prettier-ignore
+  const testData = {
+    snapshot: {
+      meta: {
+        node_types: [['hidden', 'array', 'string', 'object', 'regexp']],
+        node_fields: ['type', 'name', 'id', 'self_size', 'edge_count', 'trace_node_id', 'detachedness'],
+        edge_types: [['context', 'element', 'property', 'internal', 'hidden', 'shortcut', 'weak']],
+        edge_fields: ['type', 'name_or_index', 'to_node'],
+        location_fields: ['object_index', 'script_id', 'line', 'column'],
+      },
+      node_count: 1,
+      edge_count: 0,
+    },
+    nodes: [
+      4, 1, 200, 32, 0, 0, 0, // regexp object
+    ],
+    edges: [],
+    locations: [],
+    strings: ['', '/test/gi'],
+  }
+  const result = getRegexpObjectsFromHeapSnapshotInternal(testData)
+  expect(result).toEqual([
+    {
+      id: 200,
+      pattern: '/test/gi',
+    },
+  ])
+})
+
+test('getRegexpObjectsFromHeapSnapshot - multiple regexp objects', async () => {
+  // prettier-ignore
+  const testData = {
+    snapshot: {
+      meta: {
+        node_types: [['hidden', 'array', 'string', 'object', 'regexp']],
+        node_fields: ['type', 'name', 'id', 'self_size', 'edge_count', 'trace_node_id', 'detachedness'],
+        edge_types: [['context', 'element', 'property', 'internal', 'hidden', 'shortcut', 'weak']],
+        edge_fields: ['type', 'name_or_index', 'to_node'],
+        location_fields: ['object_index', 'script_id', 'line', 'column'],
+      },
+      node_count: 4,
+      edge_count: 0,
+    },
+    nodes: [
+      4, 1, 200, 32, 1, 0, 0, // first regexp
+      0, 0, 201, 16, 0, 0, 0, // hidden object
+      4, 2, 202, 28, 0, 0, 0, // second regexp
+      4, 3, 203, 24, 2, 0, 0, // third regexp
+    ],
+    edges: [],
+    locations: [],
+    strings: ['', '/test/gi', '/hello/i', '/world/g'],
+  }
+
+  const result = await getRegexpObjectsFromHeapSnapshotInternal(testData)
+  expect(result).toEqual([
+    {
+      id: 200,
+      pattern: '/test/gi',
+    },
+    {
+      id: 202,
+      pattern: '/hello/i',
+    },
+    {
+      id: 203,
+      pattern: '/world/g',
+    },
+  ])
 })
