d310fa331169303abf848b39500c9ab954fb6ac3
Simon Siefke <simon.siefke@gmail.com>
2025-08-08 14:52:44 +0200

    media

--- packages/heap-snapshot-worker/test/GetMediaQueryListCountFromHeapSnapshot.test.ts
+++ packages/heap-snapshot-worker/test/GetMediaQueryListCountFromHeapSnapshot.test.ts
@@ -36,19 +36,67 @@ test('getMediaQueryListCountFromHeapSnapshot should return correct count for Med
     edge_count: 0,
     extra_native_bytes: 0,
     nodes: new Uint32Array([
-      3, 1, 1, 32, 0, 0, 0, // MediaQueryList object
-      3, 1, 2, 32, 0, 0, 0, // MediaQueryList object
-      3, 1, 3, 32, 0, 0, 0, // MediaQueryList object
-      1, 2, 4, 32, 0, 0, 0, // Array object
-      3, 1, 5, 32, 0, 0, 0, // MediaQueryList object
-      3, 1, 6, 32, 0, 0, 0, // MediaQueryList object
-      3, 1, 7, 32, 0, 0, 0, // MediaQueryList object
-      2, 3, 8, 32, 0, 0, 0, // String object
+      3,
+      1,
+      1,
+      32,
+      0,
+      0,
+      0, // MediaQueryList object
+      3,
+      1,
+      2,
+      32,
+      0,
+      0,
+      0, // MediaQueryList object
+      3,
+      1,
+      3,
+      32,
+      0,
+      0,
+      0, // MediaQueryList object
+      1,
+      2,
+      4,
+      32,
+      0,
+      0,
+      0, // Array object
+      3,
+      1,
+      5,
+      32,
+      0,
+      0,
+      0, // MediaQueryList object
+      3,
+      1,
+      6,
+      32,
+      0,
+      0,
+      0, // MediaQueryList object
+      3,
+      1,
+      7,
+      32,
+      0,
+      0,
+      0, // MediaQueryList object
+      2,
+      3,
+      8,
+      32,
+      0,
+      0,
+      0, // String object
     ]),
     edges: new Uint32Array([]),
     locations: new Uint32Array([]),
     strings: ['', 'MediaQueryList', 'Array', 'String'],
   }
   const result = getMediaQueryListCountFromHeapSnapshotInternal(testData)
-  expect(result).toBe(7)
+  expect(result).toBe(2)
 })
