9b208bc1f8e103b22cd10ce604e3cbfda064fb0c
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 13:47:12 +0200

    test

--- packages/video-recording-worker/jest.config.js
+++ packages/video-recording-worker/jest.config.js
@@ -1,6 +1,7 @@
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
@@ -9,13 +10,19 @@ export default {
       },
     ],
   },
-  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   testMatch: ['<rootDir>/test/**/*.ts', '<rootDir>/test/**/*.js'],
   injectGlobals: false,
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
       branches: 70,
@@ -24,4 +31,9 @@ export default {
       statements: 70,
     },
   },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/test/',
+    '/bin/',
+  ],
 }
