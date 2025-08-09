8690b834693e15efad8a313795b11eb4f81d4801
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 18:22:28 +0200

    Discard changes to packages/cli/src/parts/HandleTestRunning/HandleTestRunning.ts

--- packages/cli/src/parts/HandleTestRunning/HandleTestRunning.ts
+++ packages/cli/src/parts/HandleTestRunning/HandleTestRunning.ts
@@ -1,11 +1,11 @@
-import * as IsGithubActions from '../IsGithubActions/IsGithubActions.ts'
+import * as GetHandleTestRunningMessage from '../GetHandleTestRunningMessage/GetHandleTestRunningMessage.ts'
 import * as Stdout from '../Stdout/Stdout.ts'
-import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
+import * as IsGithubActions from '../IsGithubActions/IsGithubActions.ts'
 
 export const handleTestRunning = async (file: string, relativeDirName: string, fileName: string, isFirst: boolean): Promise<void> => {
   if (IsGithubActions.isGithubActions) {
     return
   }
-  const message = await StdoutWorker.invoke('Stdout.getHandleTestRunningMessage', file, relativeDirName, fileName, isFirst)
+  const message = GetHandleTestRunningMessage.getHandleTestRunningMessage(file, relativeDirName, fileName, isFirst)
   await Stdout.write(message)
 }
