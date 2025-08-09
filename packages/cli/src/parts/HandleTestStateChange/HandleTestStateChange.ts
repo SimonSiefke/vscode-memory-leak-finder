d44323df3772970f628af129858d4f2dae3939ff
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 18:22:44 +0200

    Discard changes to packages/cli/src/parts/HandleTestStateChange/HandleTestStateChange.ts

--- packages/cli/src/parts/HandleTestStateChange/HandleTestStateChange.ts
+++ packages/cli/src/parts/HandleTestStateChange/HandleTestStateChange.ts
@@ -1,25 +1,25 @@
+import * as AnsiEscapes from '../AnsiEscapes/AnsiEscapes.ts'
+import * as GetTestClearMessage from '../GetTestClearMessage/GetTestClearMessage.ts'
 import * as IsGithubActions from '../IsGithubActions/IsGithubActions.ts'
 import * as StdinDataState from '../StdinDataState/StdinDataState.ts'
 import * as Stdout from '../Stdout/Stdout.ts'
-import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 import * as TestStateOutput from '../TestStateOutput/TestStateOutput.ts'
 
-const getFullMessage = async (message: string): Promise<string> => {
-  const clearMessage = await StdoutWorker.invoke('Stdout.getTestClearMessage')
+const getFullMessage = (message: string): string => {
+  const clearMessage = GetTestClearMessage.getTestClearMessage()
   let fullMessage = ''
   if (!IsGithubActions.isGithubActions) {
     fullMessage += clearMessage
   }
   fullMessage += message
   if (!IsGithubActions.isGithubActions && !StdinDataState.isBuffering()) {
-    const clear = await StdoutWorker.invoke('Stdout.getClear')
-    fullMessage = clear + fullMessage
+    fullMessage = AnsiEscapes.clear + fullMessage
   }
   fullMessage += TestStateOutput.clearPending()
   return fullMessage
 }
 
 export const handleTestStateChange = async (message: string): Promise<void> => {
-  const fullMessage = await getFullMessage(message)
+  const fullMessage = getFullMessage(message)
   await Stdout.write(fullMessage)
 }
