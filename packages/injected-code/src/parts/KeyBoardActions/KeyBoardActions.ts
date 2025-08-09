8a827912d6629ff470ab01b84dbc9534bc90da7e
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 10:31:02 +0200

    feature: move injected code to typescript

---
+++ packages/injected-code/src/parts/KeyBoardActions/KeyBoardActions.ts
@@ -0,0 +1,41 @@
+import * as Assert from '../Assert/Assert.ts'
+import * as DispatchEvent from '../DispatchEvent/DispatchEvent.ts'
+import * as GetKeyCode from '../GetKeyCode/GetKeyCode.ts'
+
+export const press = (options, element = document.activeElement) => {
+  Assert.object(options)
+  const allOptions = [options]
+  if (options.ctrlKey) {
+    allOptions.unshift({
+      ...options,
+      key: 'Control',
+      keyCode: GetKeyCode.getKeyCode('Control'),
+      code: GetKeyCode.getCode('Control'),
+    })
+  }
+  if (options.metaKey) {
+    allOptions.unshift({
+      ...options,
+      key: 'Meta',
+      keyCode: GetKeyCode.getKeyCode('Meta'),
+      code: GetKeyCode.getKeyCode('Meta'),
+    })
+  }
+  if (options.shiftKey) {
+    allOptions.unshift({
+      ...options,
+      key: 'Shift',
+      keyCode: GetKeyCode.getKeyCode('Shift'),
+      code: GetKeyCode.getKeyCode('Shift'),
+    })
+  }
+  for (const option of allOptions) {
+    DispatchEvent.keyDown(element, option)
+  }
+  for (const option of allOptions) {
+    DispatchEvent.keyPress(element, option)
+  }
+  for (const option of allOptions) {
+    DispatchEvent.keyUp(element, option)
+  }
+}
