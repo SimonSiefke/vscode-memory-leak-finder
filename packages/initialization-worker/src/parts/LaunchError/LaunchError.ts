8e154e943dc65228b7ea760b7d02dce7543a38da
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 15:40:02 +0200

    tsc

--- packages/initialization-worker/src/parts/LaunchError/LaunchError.ts
+++ packages/initialization-worker/src/parts/LaunchError/LaunchError.ts
@@ -1,6 +1,7 @@
 import * as ErrorCodes from '../ErrorCodes/ErrorCodes.ts'
 
 export class LaunchError extends Error {
+  code: string
   constructor(message) {
     super(message)
     this.name = 'LaunchError'
