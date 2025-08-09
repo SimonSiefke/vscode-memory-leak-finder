f2f6c94d9fa3093df17f51ab7e346f3cad041aa1
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 15:58:49 +0200

    test

--- packages/cli/test/HandleTestsFinished.test.ts
+++ packages/cli/test/HandleTestsFinished.test.ts
@@ -27,11 +27,42 @@ jest.unstable_mockModule('../src/parts/StdinDataState/StdinDataState.ts', () => 
   }
 })
 
+jest.unstable_mockModule('../src/parts/StdoutWorker/StdoutWorker.ts', () => {
+  return {
+    invoke: jest.fn().mockImplementation((method, ...args) => {
+      if (method === 'Stdout.getAllTestsFinishedMessage') {
+        const [passed, failed, skipped, leaked, total, duration, filterValue, isWatchMode] = args
+        if (filterValue === 'abc') {
+          return Promise.resolve('\n\u001B[1mTest Suites:\u001B[22m \u001B[1m\u001B[31m1 failed\u001B[39m\u001B[22m, \u001B[1m\u001B[32m2 passed\u001B[39m\u001B[22m, 3 total\n' +
+            '\u001B[1mTime:\u001B[22m        3.000 s\n' +
+            '\u001B[2mRan all test suites matching\u001B[22m /abc/i\u001B[2m.\u001B[22m\n' +
+            '\n' +
+            '\u001B[1mWatch Usage: \u001B[22m\u001B[2mPress \u001B[22mw\u001B[2m to show more.\u001B[22m')
+        } else if (leaked > 0) {
+          return Promise.resolve('\n' +
+            '\u001B[1mTest Suites:\u001B[22m \u001B[1m\u001B[31m1 failed\u001B[39m\u001B[22m, \u001B[1m\u001B[32m2 passed\u001B[39m\u001B[22m, \u001B[1m\u001B[34m1 leaked\u001B[39m\u001B[22m, 4 total\n' +
+            '\u001B[1mTime:\u001B[22m        3.000 s\n' +
+            '\u001B[2mRan all test suites.\u001B[22m\n' +
+            '\n' +
+            '\u001B[1mWatch Usage: \u001B[22m\u001B[2mPress \u001B[22mw\u001B[2m to show more.\u001B[22m')
+        } else {
+          return Promise.resolve('\n\u001B[1mTest Suites:\u001B[22m \u001B[1m\u001B[31m1 failed\u001B[39m\u001B[22m, \u001B[1m\u001B[32m2 passed\u001B[39m\u001B[22m, 3 total\n' +
+            '\u001B[1mTime:\u001B[22m        3.000 s\n' +
+            '\u001B[2mRan all test suites.\u001B[22m\n' +
+            '\n' +
+            '\u001B[1mWatch Usage: \u001B[22m\u001B[2mPress \u001B[22mw\u001B[2m to show more.\u001B[22m')
+        }
+      }
+      return Promise.resolve('')
+    }),
+  }
+})
+
 const Stdout = await import('../src/parts/Stdout/Stdout.ts')
 const HandleTestsFinished = await import('../src/parts/HandleTestsFinished/HandleTestsFinished.ts')
 
-test('handleTestsFinished - no filter value', () => {
-  HandleTestsFinished.handleTestsFinished(2, 1, 0, 0, 3, 3000, '')
+test('handleTestsFinished - no filter value', async () => {
+  await HandleTestsFinished.handleTestsFinished(2, 1, 0, 0, 3, 3000, '')
   expect(Stdout.write).toHaveBeenCalledTimes(1)
   expect(Stdout.write).toHaveBeenCalledWith(
     '\n\u001B[1mTest Suites:\u001B[22m \u001B[1m\u001B[31m1 failed\u001B[39m\u001B[22m, \u001B[1m\u001B[32m2 passed\u001B[39m\u001B[22m, 3 total\n' +
@@ -42,8 +73,8 @@ test('handleTestsFinished - no filter value', () => {
   )
 })
 
-test('handleTestsFinished - with filter value', () => {
-  HandleTestsFinished.handleTestsFinished(2, 1, 0, 0, 3, 3000, 'abc')
+test('handleTestsFinished - with filter value', async () => {
+  await HandleTestsFinished.handleTestsFinished(2, 1, 0, 0, 3, 3000, 'abc')
   expect(Stdout.write).toHaveBeenCalledTimes(1)
   expect(Stdout.write).toHaveBeenCalledWith(
     '\n\u001B[1mTest Suites:\u001B[22m \u001B[1m\u001B[31m1 failed\u001B[39m\u001B[22m, \u001B[1m\u001B[32m2 passed\u001B[39m\u001B[22m, 3 total\n' +
@@ -54,8 +85,8 @@ test('handleTestsFinished - with filter value', () => {
   )
 })
 
-test('handleTestsFinished - with leak', () => {
-  HandleTestsFinished.handleTestsFinished(2, 1, 0, 1, 4, 3000, '')
+test('handleTestsFinished - with leak', async () => {
+  await HandleTestsFinished.handleTestsFinished(2, 1, 0, 1, 4, 3000, '')
   expect(Stdout.write).toHaveBeenCalledTimes(1)
   expect(Stdout.write).toHaveBeenCalledWith(
     '\n' +
