5fc6d4baab687376c540bc15f6272afce280f94c
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 18:26:34 +0200

    use worker

--- packages/cli/src/parts/GetAllTestsFinishedMessage/GetAllTestsFinishedMessage.ts
+++ packages/cli/src/parts/GetAllTestsFinishedMessage/GetAllTestsFinishedMessage.ts
@@ -1,35 +1,24 @@
-import chalk from 'chalk'
-import * as Character from '../Character/Character.ts'
-import * as FormatAsSeconds from '../FormatAsSeconds/FormatAsSeconds.ts'
-import * as WatchUsageShort from '../WatchUsageShort/WatchUsageShort.ts'
+import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 
-const ranAllTestSuits = chalk.dim('Ran all test suites.')
-const ranAllTestSuitsMatching = chalk.dim('Ran all test suites matching')
-const dot = chalk.dim('.')
-const time = chalk.bold(`Time:`)
-
-const testSuites = chalk.bold(`Test Suites:`)
-
-const getRanAllTestSuitesMessage = (filterValue: string): string => {
-  if (filterValue === Character.EmptyString) {
-    return ranAllTestSuits
-  }
-  return `${ranAllTestSuitsMatching} /${filterValue}/i${dot}`
-}
-
-export const getAllTestsFinishedMessage = (passed: number, failed: number, skipped: number, leaked: number, total: number, duration: number, filterValue: string, isWatchMode: boolean): string => {
-  const failedMessage = failed ? `${chalk.bold.red(`${failed} failed`)}, ` : Character.EmptyString
-  const skippedMessage = skipped ? `${chalk.bold.yellow(`${skipped} skipped`)}, ` : Character.EmptyString
-  const passedMessage = passed ? `${chalk.bold.green(`${passed} passed`)}, ` : Character.EmptyString
-  const leakedMessage = leaked ? `${chalk.bold.blue(`${leaked} leaked`)}, ` : Character.EmptyString
-  const durationMessage = FormatAsSeconds.formatAsSeconds(duration)
-  const ranAllTestSuitesMessage = getRanAllTestSuitesMessage(filterValue)
-  let message = `
-${testSuites} ${failedMessage}${skippedMessage}${passedMessage}${leakedMessage}${total} total
-${time}        ${durationMessage}
-${ranAllTestSuitesMessage}\n`
-  if (isWatchMode) {
-    message += `${WatchUsageShort.print()}`
-  }
-  return message
+export const getAllTestsFinishedMessage = async (
+  passed: number,
+  failed: number,
+  skipped: number,
+  leaked: number,
+  total: number,
+  duration: number,
+  filterValue: string,
+  isWatchMode: boolean,
+): Promise<string> => {
+  return StdoutWorker.invoke(
+    'Stdout.getAllTestsFinishedMessage',
+    passed,
+    failed,
+    skipped,
+    leaked,
+    total,
+    duration,
+    filterValue,
+    isWatchMode,
+  )
 }
