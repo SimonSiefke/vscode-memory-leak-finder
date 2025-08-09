abe66a09ed5bdc888522417f11301b9987f4c7b5
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 09:25:34 +0200

    feature: add cli types (#1656)

--- packages/cli/src/parts/FormatAsSeconds/FormatAsSeconds.ts
+++ packages/cli/src/parts/FormatAsSeconds/FormatAsSeconds.ts
@@ -1,4 +1,4 @@
-export const formatAsSeconds = (durationInMs) => {
+export const formatAsSeconds = (durationInMs: number): string => {
   const durationInSeconds = durationInMs / 1000
   return `${durationInSeconds.toFixed(3)} s`
 }
