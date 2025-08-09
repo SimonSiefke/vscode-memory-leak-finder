ae6bc34e9803522c61cb30d6f94070e82ac6f8e0
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 08:45:45 +0200

    feature: move connectWorkers into separate file

--- packages/test-coordinator/src/parts/PrepareTests/PrepareTests.js
+++ packages/test-coordinator/src/parts/PrepareTests/PrepareTests.js
@@ -1,11 +1,8 @@
 import * as CanUseIdleCallback from '../CanUseIdleCallback/CanUseIdleCallback.js'
-import * as ConnectDevtools from '../ConnectDevtools/ConnectDevtools.js'
-import * as ConnectElectron from '../ConnectElectron/ConnectElectron.js'
+import { connectWorkers } from '../ConnectWorkers/ConnectWorkers.js'
 import * as KillExistingIdeInstances from '../KillExistingIdeInstances/KillExistingIdeInstances.js'
-import * as MemoryLeakWorker from '../MemoryLeakWorker/MemoryLeakWorker.js'
 import * as PageObject from '../PageObject/PageObject.js'
 import { prepareBoth } from '../PrepareBoth/PrepareBoth.js'
-import * as VideoRecording from '../VideoRecording/VideoRecording.js'
 
 export const prepareTests = async (rpc, cwd, headlessMode, recordVideo, connectionId, timeouts, ide, ideVersion, vscodePath, commit) => {
   // TODO move whole ide launch into separate worker
@@ -22,25 +19,9 @@ export const prepareTests = async (rpc, cwd, headlessMode, recordVideo, connecti
     isFirstConnection,
     canUseIdleCallback,
   )
-
-  // TODO connect workers in parallel
-  const {} = await ConnectElectron.connectElectron(rpc, connectionId, headlessMode, webSocketUrl, isFirstConnection, canUseIdleCallback)
-  if (recordVideo) {
-    await VideoRecording.start(devtoolsWebSocketUrl)
-  }
-  await MemoryLeakWorker.startWorker(devtoolsWebSocketUrl)
-  await ConnectDevtools.connectDevtools(
-    rpc,
-    connectionId,
-    devtoolsWebSocketUrl,
-    monkeyPatchedElectronId,
-    electronObjectId,
-    isFirstConnection,
-  )
-
+  await connectWorkers(rpc, connectionId, headlessMode, webSocketUrl, isFirstConnection, canUseIdleCallback)
   await initializationWorkerRpc.invoke('Initialize.undoMonkeyPatch')
   await PageObject.create(rpc, connectionId, isFirstConnection, headlessMode, timeouts, ideVersion)
-
   return {
     rpc,
     webSocketUrl,
