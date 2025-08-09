c744e562f4ea2ecf082333d28a85c213714cca73
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 15:44:30 +0200

    test

---
+++ packages/load-source-map-worker/jest.config.js
@@ -0,0 +1,25 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '#ansi-styles': 'ansi-styles/index.js',
+    '#supports-color': 'supports-color/index.js',
+  },
+  testMatch: ['<rootDir>/test/**/*.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/.vscode-test/'],
+  injectGlobals: false,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: { branches: 0, functions: 0, lines: 0, statements: 0 },
+  },
+}
