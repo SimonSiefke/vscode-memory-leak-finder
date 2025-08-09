e6e7092e2a9c078d68f87d8178fc3bdbeb4f0c1e
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 12:55:38 +0200

    feature: add cli types (#1666)

--- packages/cli/src/parts/GetStdoutWorkerUrl/GetStdoutWorkerUrl.ts
+++ packages/cli/src/parts/GetStdoutWorkerUrl/GetStdoutWorkerUrl.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import * as Root from '../Root/Root.ts'
 
-export const getStdoutWorkerUrl = () => {
+export const getStdoutWorkerUrl = (): string => {
   const url = join(Root.root, 'packages/stdout-worker/bin/stdout-worker.js')
   return url
 }
