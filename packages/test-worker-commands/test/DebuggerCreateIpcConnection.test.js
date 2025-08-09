feb6dfc7c03e9e7deeb7fbcaae4bf2e6a9340de2
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 21:54:25 +0200

    test

--- packages/test-worker-commands/test/DebuggerCreateIpcConnection.test.js
+++ packages/test-worker-commands/test/DebuggerCreateIpcConnection.test.js
@@ -1,23 +1,8 @@
-import { jest, test, expect, beforeEach } from '@jest/globals'
+import { expect, test } from '@jest/globals'
 
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('ws', () => {
-  return {
-    WebSocket: jest.fn(),
-  }
-})
-
-const ws = await import('ws')
 const DebuggerCreateIpcConnection = await import('../src/parts/DebuggerCreateIpcConnection/DebuggerCreateIpcConnection.js')
 
 test('createConnection - error - invalid url', async () => {
-  // @ts-ignore
-  ws.WebSocket.mockImplementation((url) => {
-    throw new SyntaxError(`Invalid URL: ${url}`)
-  })
   await expect(DebuggerCreateIpcConnection.createConnection('abc')).rejects.toThrow(
     new Error(`Failed to create websocket connection: TypeError: Invalid URL`),
   )
