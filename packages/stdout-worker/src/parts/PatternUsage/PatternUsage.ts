7ba4bd090a2e6ff9427510134b7ae65b38042754
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 18:10:39 +0200

    feature: add more functions to stdout worker (#1676)

---
+++ packages/stdout-worker/src/parts/PatternUsage/PatternUsage.ts
@@ -0,0 +1,17 @@
+import chalk from 'chalk'
+import * as CliKeys from '../CliKeys/CliKeys.ts'
+
+const patternModeUsage = chalk.bold('Pattern Mode Usage')
+const press = chalk.dim('\u203A Press')
+const toExitPatternMode = chalk.dim('to exit pattern mode.')
+const toFilterByARegexPattern = chalk.dim(`to filter by a regex pattern.`)
+const pattern = chalk.dim(' pattern \u203A')
+
+export const print = (): string => {
+  return `
+${patternModeUsage}
+ ${press} ${CliKeys.Escape} ${toExitPatternMode}
+ ${press} ${CliKeys.Enter} ${toFilterByARegexPattern}
+
+${pattern} `
+}
