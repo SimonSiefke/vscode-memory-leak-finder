d7639d774af05e325320f5a5621ffc43899b6cf4
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 13:34:26 +0200

    test

--- packages/initialization-worker/jest.config.js
+++ packages/initialization-worker/jest.config.js
@@ -1,14 +1,10 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+    }],
   },
   testEnvironment: 'node',
 }
