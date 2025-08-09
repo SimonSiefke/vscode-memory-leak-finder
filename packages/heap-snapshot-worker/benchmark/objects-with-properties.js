d864d0972d868a07368911020ffcbc664b5c7393
Simon Siefke <simon.siefke@gmail.com>
2025-08-09 17:26:19 +0200

    test

--- packages/heap-snapshot-worker/benchmark/objects-with-properties.js
+++ packages/heap-snapshot-worker/benchmark/objects-with-properties.js
@@ -20,7 +20,7 @@ async function testGetObjectsWithProperties() {
     const oldStateObjects = getObjectsWithPropertiesInternal(snapshot, 'oldState', 2)
     console.log(`Refactored function found ${oldStateObjects.length} objects with "oldState" property`)
 
-    console.log({ oldStateObjects })
+    console.log(JSON.stringify(oldStateObjects, null, 2))
   } catch (error) {
     console.error('Error testing getObjectsWithProperties:', error)
   }
