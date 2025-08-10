600e24de782f51533ce75ea3789ae7df68a8c643
Simon Siefke <simon.siefke@gmail.com>
2025-08-10 12:01:59 +0200

    ipc

--- packages/download-worker/src/parts/Main/Main.ts
+++ packages/download-worker/src/parts/Main/Main.ts
@@ -1,13 +1,8 @@
-import * as Callback from '../Callback/Callback.ts'
-import * as Command from '../Command/Command.ts'
+import { NodeWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as CommandState from '../CommandState/CommandState.ts'
-import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
-import * as IpcChild from '../IpcChild/IpcChild.ts'
-import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
 export const main = async (): Promise<void> => {
-  CommandState.registerCommands(CommandMap.commandMap)
-  const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
-  HandleIpc.handleIpc(ipc, Command.execute, Callback.resolve)
+  await NodeWorkerRpcClient.create({
+    commandMap: CommandMap.commandMap,
+  })
 }
