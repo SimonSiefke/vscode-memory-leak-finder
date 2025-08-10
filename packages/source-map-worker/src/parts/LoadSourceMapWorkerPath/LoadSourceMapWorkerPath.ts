7931be20ea0a8685348015fe1e9ae0736bb0417b
Simon Siefke <simon.siefke@gmail.com>
2025-08-10 12:03:43 +0200

    path

--- packages/source-map-worker/src/parts/LoadSourceMapWorkerPath/LoadSourceMapWorkerPath.ts
+++ packages/source-map-worker/src/parts/LoadSourceMapWorkerPath/LoadSourceMapWorkerPath.ts
@@ -1,4 +1,4 @@
 import { join } from 'path'
 import * as Root from '../Root/Root.js'
 
-export const loadSourceMapWorkerPath: string = join(Root.root, 'packages', 'load-source-map-worker', 'src', 'loadSourceMapWorkerMain.js')
+export const loadSourceMapWorkerPath: string = join(Root.root, 'packages', 'load-source-map-worker', 'src', 'loadSourceMapWorkerMain.ts')
