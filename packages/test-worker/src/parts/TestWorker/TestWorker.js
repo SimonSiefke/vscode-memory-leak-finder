c64ac7ac1da117dd1ee7cac91bcf5724091fc072
Simon Siefke <simon.siefke@gmail.com>
2025-08-10 15:44:48 +0200

    feature: use rpc for test worker

--- packages/test-worker/src/parts/TestWorker/TestWorker.js
+++ packages/test-worker/src/parts/TestWorker/TestWorker.js
@@ -1,14 +1,11 @@
+import { NodeWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.js'
-import * as CommandState from '../CommandState/CommandState.js'
-import * as HandleIpc from '../HandleIpc/HandleIpc.js'
-import * as IpcChild from '../IpcChild/IpcChild.js'
-import * as IpcChildType from '../IpcChildType/IpcChildType.js'
 import * as ParentProcess from '../ParentProcess/ParentProcess.js'
 
 export const listen = async () => {
   const commandMap = await CommandMap.load()
-  CommandState.registerCommands(commandMap)
-  const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
-  HandleIpc.handleIpc(ipc)
-  ParentProcess.setIpc(ipc)
+  const rpc = await NodeWorkerRpcClient.create({
+    commandMap,
+  })
+  ParentProcess.setRpc(rpc)
 }
