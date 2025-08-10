c82713f3593e3b77e6058662d121869953fe4096
Simon Siefke <simon.siefke@gmail.com>
2025-08-10 11:58:49 +0200

    ts

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternalObjects/ParseHeapSnapshotInternalObjects.ts
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternalObjects/ParseHeapSnapshotInternalObjects.ts
@@ -1,21 +1,22 @@
 import * as Assert from '../Assert/Assert.js'
 import * as CamelCase from '../CamelCase/CamelCase.js'
 import * as CreateHeapSnapshotNode from '../CreateHeapSnapshotNode/CreateHeapSnapshotNode.js'
+import type { HeapSnapshotNode } from '../CreateHeapSnapshotNode/CreateHeapSnapshotNode.js'
 
 export const parseHeapSnapshotObjects = (
-  values,
-  valueFields,
-  valueTypes,
-  typeKey,
-  nameKey,
-  indexMultiplierKey,
-  indexMultiplier,
-  strings,
-) => {
+  values: Uint32Array,
+  valueFields: string[],
+  valueTypes: string[][],
+  typeKey: string,
+  nameKey: string,
+  indexMultiplierKey: string,
+  indexMultiplier: number,
+  strings: string[],
+): HeapSnapshotNode[] => {
   // Assert.array(values)
   Assert.array(valueFields)
   Assert.array(valueTypes)
-  const parsed = []
+  const parsed: HeapSnapshotNode[] = []
   const nodeFieldCount = valueFields.length
   const camelCaseNodeFields = valueFields.map(CamelCase.camelCase)
   for (let i = 0; i < values.length; i += nodeFieldCount) {
