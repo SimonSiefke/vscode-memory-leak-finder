98587754074b91307ec0c963259484049acbeca9
Simon Siefke <simon.siefke@gmail.com>
2025-08-10 09:32:14 +0200

    async

--- packages/cli/test/GetHandleTestSetupMessage.test.ts
+++ packages/cli/test/GetHandleTestSetupMessage.test.ts
@@ -2,12 +2,12 @@ import { expect, test } from '@jest/globals'
 import * as GetHandleTestSetupMessage from '../src/parts/GetHandleTestSetupMessage/GetHandleTestSetupMessage.ts'
 
 test('getHandleTestSetupMessage - should return formatted setup message', () => {
-  const result = GetHandleTestSetupMessage.getHandleTestSetupMessage()
+  const result = await GetHandleTestSetupMessage.getHandleTestSetupMessage()
   expect(result).toBe('\n\u001b[0m\u001b[7m\u001b[33m SETUP \u001b[39m\u001b[27m\u001b[0m\n')
 })
 
-test('getHandleTestSetupMessage - should always return the same message', () => {
-  const result1 = GetHandleTestSetupMessage.getHandleTestSetupMessage()
-  const result2 = GetHandleTestSetupMessage.getHandleTestSetupMessage()
+test('getHandleTestSetupMessage - should always return the same message', async () => {
+  const result1 = await GetHandleTestSetupMessage.getHandleTestSetupMessage()
+  const result2 = await GetHandleTestSetupMessage.getHandleTestSetupMessage()
   expect(result1).toBe(result2)
 })
