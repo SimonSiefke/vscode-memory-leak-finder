ed4e1f2a3d438c0bae159d59d49c5cb976d8430e
Simon Siefke <simon.siefke@gmail.com>
2025-08-10 09:32:57 +0200

    async

--- packages/cli/test/GetHandleTestSetupMessage.test.ts
+++ packages/cli/test/GetHandleTestSetupMessage.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
 import * as GetHandleTestSetupMessage from '../src/parts/GetHandleTestSetupMessage/GetHandleTestSetupMessage.ts'
 
-test('getHandleTestSetupMessage - should return formatted setup message', () => {
+test('getHandleTestSetupMessage - should return formatted setup message', async () => {
   const result = await GetHandleTestSetupMessage.getHandleTestSetupMessage()
   expect(result).toBe('\n\u001b[0m\u001b[7m\u001b[33m SETUP \u001b[39m\u001b[27m\u001b[0m\n')
 })
