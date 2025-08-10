67fba8e1b1ddcb8e811b6f926e088251505aec93
Simon Siefke <simon.siefke@gmail.com>
2025-08-10 14:12:33 +0200

    simplify code

--- packages/page-object/src/parts/WaitForApplicationToBeReady/WaitForApplicationToBeReady.ts
+++ packages/page-object/src/parts/WaitForApplicationToBeReady/WaitForApplicationToBeReady.ts
@@ -1,54 +1,30 @@
-export const waitForApplicationToBeReadyInternal = async ({ page }) => {
+const isDevtoolsCannotFindContextError = (error) => {
+  return (
+    error.name === 'DevtoolsProtocolError' &&
+    (error.message === 'Cannot find context with specified id' || error.message === 'uniqueContextId not found')
+  )
+}
+
+export const waitForApplicationToBeReady = async ({ page, expect, VError }): Promise<void> => {
   try {
-    const main = firstWindow.locator(selectors.mainContent)
+    const main = page.locator('[role="main"]')
     await expect(main).toBeVisible({
       timeout: 30_000,
     })
   } catch (error) {
-    if (errorHandlers?.isRetryableError?.(error)) {
+    if (isDevtoolsCannotFindContextError(error)) {
       // ignore and try again
-      const main = firstWindow.locator(selectors.mainContent)
+      const main = page.locator('[role="main"]')
       await expect(main).toBeVisible({
         timeout: 30_000,
       })
-      return firstWindow
+      return page
     } else {
       throw error
     }
   }
-
-  if (selectors.readyNotification) {
-    const notification = firstWindow.locator(selectors.readyNotification)
-    await expect(notification).toBeVisible({
-      timeout: 15_000,
-    })
-  }
-
-  return firstWindow
-}
-
-export const waitForApplicationToBeReady = async ({ page, expect, VError }) => {
-  return await waitForApplicationToBeReadyInternal({
-    electronApp,
-    isHeadless,
-    isFirstConnection,
-    expect,
-    selectors: {
-      mainContent: '[role="main"]',
-      readyNotification: 'text=All installed extensions are temporarily disabled.',
-    },
-    eventHandlers: {
-      waitForEvent: pageEventState?.waitForEvent,
-      eventTypes: {
-        interactiveTime: pageEventType?.InteractiveTime || 'InteractiveTime',
-        networkIdle: pageEventType?.NetworkIdle || 'networkIdle',
-      },
-      timeoutConstants: {
-        interactiveTime: timeoutConstants?.InteractiveTime || 20_000,
-      },
-    },
-    errorHandlers: {
-      isRetryableError: isDevtoolsCannotFindContextError,
-    },
+  const notification = page.locator('text=All installed extensions are temporarily disabled.')
+  await expect(notification).toBeVisible({
+    timeout: 15_000,
   })
 }
