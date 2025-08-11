f5e971f50689a38b6d2a893136791091c4a00f01
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:23:50 +0200

    simplify code

--- packages/cli/src/parts/PatternUsage/PatternUsage.ts
+++ packages/cli/src/parts/PatternUsage/PatternUsage.ts
@@ -3,7 +3,3 @@ import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 export const print = async (): Promise<string> => {
   return StdoutWorker.invoke('Stdout.getPatternUsageMessage')
 }
-
-export const clearAndPrint = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.getPatternUsageMessageFull')
-}
