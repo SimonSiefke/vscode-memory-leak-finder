e993a85d7e96f2bbfab64dd90ad20af17ae27f70
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 15:26:36 +0200

    fix

--- packages/repository-worker/src/parts/LaunchFileSystemWorker/LaunchFileSystemWorker.ts
+++ packages/repository-worker/src/parts/LaunchFileSystemWorker/LaunchFileSystemWorker.ts
@@ -2,8 +2,8 @@ import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
 import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.ts'
 import * as FileSystemWorkerPath from '../FileSystemWorkerPath/FileSystemWorkerPath.ts'
 
-export const launchFileSystemWorker = () => {
-  const rpc = NodeWorkerRpcParent.create({
+export const launchFileSystemWorker = async (): Promise<void> => {
+  const rpc = await NodeWorkerRpcParent.create({
     commandMap: {},
     path: FileSystemWorkerPath.fileSystemWorkerPath,
   })
