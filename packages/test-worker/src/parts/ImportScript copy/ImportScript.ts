71cad7ffd2da3fe5dc90780ad2e741586ec09ccd
Simon Siefke <simon.siefke@gmail.com>
2025-08-12 00:06:44 +0200

    feature: merge test worker and test worker commands

--- packages/test-worker/src/parts/ImportScript copy/ImportScript.ts
+++ packages/test-worker/src/parts/ImportScript copy/ImportScript.ts
@@ -1,7 +1,7 @@
 import { pathToFileURL } from 'url'
 import { VError } from '../VError/VError.ts'
 
-export const importScript = async (path) => {
+export const importScript = async (path: string) => {
   try {
     const url = pathToFileURL(path).toString()
     return await import(url)
