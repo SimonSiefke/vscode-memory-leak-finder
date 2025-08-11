608b4f7fd59ce5c5dbc3e9e743e8bb10af3d7129
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:54:33 +0200

    test

--- packages/cli/test/InitialStart.test.ts
+++ packages/cli/test/InitialStart.test.ts
@@ -1,5 +1,6 @@
 import { beforeEach, expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
+import * as StdoutWorker from '../src/parts/StdoutWorker/StdoutWorker.ts'
 
 beforeEach(() => {
   jest.resetModules()
@@ -12,21 +13,17 @@ jest.unstable_mockModule('../src/parts/Stdout/Stdout.ts', () => {
   }
 })
 
-jest.unstable_mockModule('../src/parts/StdoutWorker/StdoutWorker.ts', () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: (method: string) => {
-      if (method === 'Stdout.getWatchUsageMessage') {
-        return 'watch usage'
-      }
-      throw new Error(`unexpected method ${method}`)
-    },
-  })
-
-  return {
-    invoke: mockRpc.invoke.bind(mockRpc),
-  }
+const mockRpc = MockRpc.create({
+  commandMap: {},
+  invoke: (method: string) => {
+    if (method === 'Stdout.getWatchUsageMessage') {
+      return 'watch usage'
+    }
+    throw new Error(`unexpected method ${method}`)
+  },
 })
+
+StdoutWorker.set(mockRpc)
 
 jest.unstable_mockModule('../src/parts/WatchUsage/WatchUsage.ts', () => {
   return {
