7645b8fdd6ddc7df9a5e1d315fbe575b1b0a328b
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 15:32:43 +0200

    remove unused code

--- packages/test-coordinator/src/parts/GetRepositoryWorkerUrl/GetRepositoryWorkerUrl.ts
+++
@@ -1,7 +0,0 @@
-import { join } from 'node:path'
-import * as Root from '../Root/Root.ts'
-
-export const getRepositoryWorkerUrl = () => {
-  const url = join(Root.root, 'packages/repository-worker/bin/repository-worker.js')
-  return url
-}
