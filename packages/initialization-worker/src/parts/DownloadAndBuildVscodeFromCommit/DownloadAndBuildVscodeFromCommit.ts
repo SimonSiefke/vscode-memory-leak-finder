02d37dd95903ae2ab290c5f11a0c3e20b02a9f91
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 22:13:08 +0200

    fix

--- packages/initialization-worker/src/parts/DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.ts
+++ packages/initialization-worker/src/parts/DownloadAndBuildVscodeFromCommit/DownloadAndBuildVscodeFromCommit.ts
@@ -2,7 +2,7 @@ import * as RepositoryWorker from '../RepositoryWorker/RepositoryWorker.ts'
 
 export const downloadAndBuildVscodeFromCommit = async (commitHash: string): Promise<string> => {
   const rpc = await RepositoryWorker.launch()
-  const path = await rpc.invoke('Download.downloadAndBuildVscodeFromCommit', commitHash)
+  const path = await rpc.invoke('Repository.downloadAndBuildVscodeFromCommit', commitHash)
   await rpc.dispose()
   return path
 }
