08f2dfba63a4ef4d3da1b4f40f22a82099843a6e
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:32:44 +0200

    remove unused code

--- packages/cli/src/parts/GetCursorLeft/GetCursorLeft.ts
+++
@@ -1,5 +0,0 @@
-import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
-
-export const getCursorLeft = (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.getCursorLeft')
-}
--- packages/cli/src/parts/GetEraseLine/GetEraseLine.ts
+++
@@ -1,5 +0,0 @@
-import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
-
-export const getEraseLine = (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.getEraseLine')
-}
