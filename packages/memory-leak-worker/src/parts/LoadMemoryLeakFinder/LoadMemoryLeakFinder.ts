04091293ef328076e259ea53a6a7e2942a9543c1
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 17:36:52 +0000

    file extension

--- packages/memory-leak-worker/src/parts/LoadMemoryLeakFinder/LoadMemoryLeakFinder.ts
+++ packages/memory-leak-worker/src/parts/LoadMemoryLeakFinder/LoadMemoryLeakFinder.ts
@@ -1,4 +1,4 @@
-const memoryLeakFinderUrl:string= '../../../../memory-leak-finder/src/index.js'
+const memoryLeakFinderUrl:string= '../../../../memory-leak-finder/src/index.ts'
 
 // TODO move that code here
 export const loadMemoryLeakFinder = async () => {
