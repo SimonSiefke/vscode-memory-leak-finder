e28b80fb96808c5bf562e21dfb9d8218877e8a73
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 09:36:28 +0200

    test

---
+++ packages/test-coordinator/jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: ['<rootDir>/test/**/*.ts'],
+  injectGlobals: false,
+}
+
+
