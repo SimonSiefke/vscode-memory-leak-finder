2eaa6dd10b0ab557ae5c6eb524a09f7b14a4862e
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 15:19:11 +0200

    tsc

--- packages/test-coordinator/src/parts/GetRepositoryWorkerUrl/GetRepositoryWorkerUrl.ts
+++ packages/test-coordinator/src/parts/GetRepositoryWorkerUrl/GetRepositoryWorkerUrl.ts
@@ -1,5 +1,5 @@
 import { join } from 'node:path'
-import * as Root from '../Root/Root.js'
+import * as Root from '../Root/Root.ts'
 
 export const getRepositoryWorkerUrl = () => {
   const url = join(Root.root, 'packages/repository-worker/bin/repository-worker.js')
---
+++ packages/test-coordinator/src/parts/RepositoryWorker/RepositoryWorker.ts
@@ -0,0 +1,13 @@
+import { NodeWorkerRpcParent, Rpc } from '@lvce-editor/rpc'
+import * as GetRepositoryWorkerUrl from '../GetRepositoryWorkerUrl/GetRepositoryWorkerUrl.ts'
+
+export const launch = async (): Promise<Rpc> => {
+  const url = GetRepositoryWorkerUrl.getRepositoryWorkerUrl()
+  const rpc = await NodeWorkerRpcParent.create({
+    path: url,
+    stdio: 'inherit',
+    execArgv: [],
+    commandMap: {},
+  })
+  return rpc
+}
