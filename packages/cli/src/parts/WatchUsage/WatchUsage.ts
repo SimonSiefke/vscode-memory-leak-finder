af8109efe130dd52430ccf1be19fb841e5195bbd
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:28:08 +0200

    remove unused code

--- packages/cli/src/parts/WatchUsage/WatchUsage.ts
+++ packages/cli/src/parts/WatchUsage/WatchUsage.ts
@@ -3,11 +3,3 @@ import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 export const print = async (): Promise<string> => {
   return StdoutWorker.invoke('Stdout.getWatchUsageMessage')
 }
-
-export const clearAndPrint = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.getWatchUsageMessageFull')
-}
-
-export const printShowDetails = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.getWatchUsageShowDetailsMessage')
-}
