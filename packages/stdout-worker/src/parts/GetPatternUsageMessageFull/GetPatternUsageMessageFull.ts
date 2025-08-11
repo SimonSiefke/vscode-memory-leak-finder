de6c60c90b847b1258fe71b0bb67ae744ff8e2e4
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:31:29 +0200

    Delete packages/stdout-worker/src/parts/GetPatternUsageMessageFull/GetPatternUsageMessageFull.ts

--- packages/stdout-worker/src/parts/GetPatternUsageMessageFull/GetPatternUsageMessageFull.ts
+++
@@ -1,8 +0,0 @@
-import * as GetAnsiEscapes from '../GetAnsiEscapes/GetAnsiEscapes.ts'
-import * as GetPatternUsageMessage from '../GetPatternUsageMessage/GetPatternUsageMessage.ts'
-
-export const getPatternUsageMessageFull = (): string => {
-  const clear = GetAnsiEscapes.getClear()
-  const body = GetPatternUsageMessage.getPatternUsageMessage()
-  return clear + '\n' + body
-}
