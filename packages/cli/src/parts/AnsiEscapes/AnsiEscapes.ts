a715a681e2c049425c3bb429a69c2c886a6a91a1
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:43:44 +0200

    fix command ids

--- packages/cli/src/parts/AnsiEscapes/AnsiEscapes.ts
+++ packages/cli/src/parts/AnsiEscapes/AnsiEscapes.ts
@@ -1,35 +1,35 @@
 import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 
 export const cursorUp = async (count: number): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.cursorUp', count)
+  return StdoutWorker.invoke('Stdout.getCursorUp', count)
 }
 
 export const eraseLine = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.eraseLine')
+  return StdoutWorker.invoke('Stdout.getEraseLine')
 }
 
 export const cursorLeft = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.cursorLeft')
+  return StdoutWorker.invoke('Stdout.getCursorLeft')
 }
 
 export const eraseScreen = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.eraseScreen')
+  return StdoutWorker.invoke('Stdout.getEraseScreen')
 }
 
 export const eraseDown = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.eraseDown')
+  return StdoutWorker.invoke('Stdout.getEraseDown')
 }
 
 export const backspace = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.backspace')
+  return StdoutWorker.invoke('Stdout.getBackspace')
 }
 
 export const cursorBackward = async (length: number): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.cursorBackward', length)
+  return StdoutWorker.invoke('Stdout.getCursorBackward', length)
 }
 
 export const eraseEndLine = async (): Promise<string> => {
-  return StdoutWorker.invoke('Stdout.eraseEndLine')
+  return StdoutWorker.invoke('Stdout.getEraseEndLine')
 }
 
 export const clear = async (isWindows: boolean): Promise<string> => {
