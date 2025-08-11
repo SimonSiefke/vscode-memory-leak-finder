0ec05341fb16f3eeb5b578772a311a510c85d547
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:55:40 +0200

    feature: update stdout commands

--- packages/stdout-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/stdout-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,39 +1,41 @@
 import * as GetAllTestsFinishedMessage from '../GetAllTestsFinishedMessage/GetAllTestsFinishedMessage.ts'
 import * as GetAnsiEscapes from '../GetAnsiEscapes/GetAnsiEscapes.ts'
+import * as GetGitHubFileErrorMessage from '../GetGitHubFileErrorMessage/GetGitHubFileErrorMessage.ts'
+import * as GetGitHubGroupEndMessage from '../GetGitHubGroupEndMessage/GetGitHubGroupEndMessage.ts'
+import * as GetGitHubGroupStartMessage from '../GetGitHubGroupStartMessage/GetGitHubGroupStartMessage.ts'
 import * as GetHandleTestFailedMessage from '../GetHandleTestFailedMessage/GetHandleTestFailedMessage.ts'
 import * as GetHandleTestPassedMessage from '../GetHandleTestPassedMessage/GetHandleTestPassedMessage.ts'
 import * as GetHandleTestRunningMessage from '../GetHandleTestRunningMessage/GetHandleTestRunningMessage.ts'
 import * as GetHandleTestSetupMessage from '../GetHandleTestSetupMessage/GetHandleTestSetupMessage.ts'
 import * as GetHandleTestSkippedMessage from '../GetHandleTestSkippedMessage/GetHandleTestSkippedMessage.ts'
 import * as GetInterruptedMessage from '../GetInterruptedMessage/GetInterruptedMessage.ts'
-import * as GetTestClearMessage from '../GetTestClearMessage/GetTestClearMessage.ts'
-import * as GetWatchUsageMessage from '../GetWatchUsageMessage/GetWatchUsageMessage.ts'
 import * as GetPatternUsageMessage from '../GetPatternUsageMessage/GetPatternUsageMessage.ts'
-import * as GetGitHubGroupStartMessage from '../GetGitHubGroupStartMessage/GetGitHubGroupStartMessage.ts'
-import * as GetGitHubGroupEndMessage from '../GetGitHubGroupEndMessage/GetGitHubGroupEndMessage.ts'
-import * as GetGitHubFileErrorMessage from '../GetGitHubFileErrorMessage/GetGitHubFileErrorMessage.ts'
+import * as GetTestClearMessage from '../GetTestClearMessage/GetTestClearMessage.ts'
+import * as GetTestsUnexpectedErrorMessage from '../GetTestsUnexpectedErrorMessage/GetTestsUnexpectedErrorMessage.ts'
+import * as GetWatchUsageMessage from '../GetWatchUsageMessage/GetWatchUsageMessage.ts'
 
 export const commandMap: Record<string, (...args: any[]) => any> = {
-  'Stdout.getHandleTestFailedMessage': GetHandleTestFailedMessage.getHandleTestFailedMessage,
   'Stdout.getAllTestsFinishedMessage': GetAllTestsFinishedMessage.getAllTestsFinishedMessage,
+  'Stdout.getBackspace': GetAnsiEscapes.getBackspace,
+  'Stdout.getClear': GetAnsiEscapes.getClear,
+  'Stdout.getCursorBackward': GetAnsiEscapes.getCursorBackward,
+  'Stdout.getCursorLeft': GetAnsiEscapes.getCursorLeft,
+  'Stdout.getCursorUp': GetAnsiEscapes.getCursorUp,
+  'Stdout.getEraseDown': GetAnsiEscapes.getEraseDown,
+  'Stdout.getEraseEndLine': GetAnsiEscapes.getEraseEndLine,
+  'Stdout.getEraseLine': GetAnsiEscapes.getEraseLine,
+  'Stdout.getEraseScreen': GetAnsiEscapes.getEraseScreen,
+  'Stdout.getGitHubFileErrorMessage': GetGitHubFileErrorMessage.getGitHubFileErrorMessage,
+  'Stdout.getGitHubGroupEndMessage': GetGitHubGroupEndMessage.getGitHubGroupEndMessage,
+  'Stdout.getGitHubGroupStartMessage': GetGitHubGroupStartMessage.getGitHubGroupStartMessage,
+  'Stdout.getHandleTestFailedMessage': GetHandleTestFailedMessage.getHandleTestFailedMessage,
   'Stdout.getHandleTestPassedMessage': GetHandleTestPassedMessage.getHandleTestPassedMessage,
   'Stdout.getHandleTestRunningMessage': GetHandleTestRunningMessage.getHandleTestRunningMessage,
   'Stdout.getHandleTestSetupMessage': GetHandleTestSetupMessage.getHandleTestSetupMessage,
   'Stdout.gethandleTestSkippedMessage': GetHandleTestSkippedMessage.getHandleTestSkippedMessage,
   'Stdout.getInterruptedMessage': GetInterruptedMessage.getInterruptedMessage,
-  'Stdout.getTestClearMessage': GetTestClearMessage.getTestClearMessage,
-  'Stdout.getWatchUsageMessage': GetWatchUsageMessage.getWatchUsageMessage,
-  'Stdout.getCursorUp': GetAnsiEscapes.getCursorUp,
-  'Stdout.getEraseLine': GetAnsiEscapes.getEraseLine,
-  'Stdout.getCursorLeft': GetAnsiEscapes.getCursorLeft,
-  'Stdout.getEraseScreen': GetAnsiEscapes.getEraseScreen,
-  'Stdout.getEraseDown': GetAnsiEscapes.getEraseDown,
-  'Stdout.getBackspace': GetAnsiEscapes.getBackspace,
-  'Stdout.getCursorBackward': GetAnsiEscapes.getCursorBackward,
-  'Stdout.getEraseEndLine': GetAnsiEscapes.getEraseEndLine,
-  'Stdout.getClear': GetAnsiEscapes.getClear,
   'Stdout.getPatternUsageMessage': GetPatternUsageMessage.getPatternUsageMessage,
-  'Stdout.getGitHubGroupStartMessage': GetGitHubGroupStartMessage.getGitHubGroupStartMessage,
-  'Stdout.getGitHubGroupEndMessage': GetGitHubGroupEndMessage.getGitHubGroupEndMessage,
-  'Stdout.getGitHubFileErrorMessage': GetGitHubFileErrorMessage.getGitHubFileErrorMessage,
+  'Stdout.getTestClearMessage': GetTestClearMessage.getTestClearMessage,
+  'Stdout.getTestsUnexpectedErrorMessage': GetTestsUnexpectedErrorMessage.getTestsUnexpectedErrorMessage,
+  'Stdout.getWatchUsageMessage': GetWatchUsageMessage.getWatchUsageMessage,
 }
