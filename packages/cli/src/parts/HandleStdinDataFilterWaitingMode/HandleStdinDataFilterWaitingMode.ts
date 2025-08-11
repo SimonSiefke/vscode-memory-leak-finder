280718ff4a65bdc30dd6c312f8020002aeee4bc3
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:14:55 +0200

    fix

--- packages/cli/src/parts/HandleStdinDataFilterWaitingMode/HandleStdinDataFilterWaitingMode.ts
+++ packages/cli/src/parts/HandleStdinDataFilterWaitingMode/HandleStdinDataFilterWaitingMode.ts
@@ -78,6 +78,7 @@ export const handleStdinDataFilterWaitingMode = async (state: FilterWaitingState
         stdout: [...state.stdout, prefix + top],
       }
     }
+    case AnsiKeys.ArrowUp:
     case AnsiKeys.ArrowDown:
       return state
     default:
