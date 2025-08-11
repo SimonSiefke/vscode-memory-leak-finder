33bce4e938b21369500e76579ad6aea5a5cf7f68
Simon Siefke <simon.siefke@gmail.com>
2025-08-11 23:26:50 +0200

    use async

--- packages/cli/src/parts/AnsiEscapes/AnsiEscapes.ts
+++ packages/cli/src/parts/AnsiEscapes/AnsiEscapes.ts
@@ -1,37 +1,37 @@
 import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 
-export const cursorUp = (count: number) => {
+export const cursorUp = async (count: number) => {
   return StdoutWorker.invoke('Stdout.cursorUp', count)
 }
 
-export const eraseLine = () => {
+export const eraseLine = async () => {
   return StdoutWorker.invoke('Stdout.eraseLine')
 }
 
-export const cursorLeft = () => {
+export const cursorLeft = async () => {
   return StdoutWorker.invoke('Stdout.cursorLeft')
 }
 
-export const eraseScreen = () => {
+export const eraseScreen = async () => {
   return StdoutWorker.invoke('Stdout.eraseScreen')
 }
 
-export const eraseDown = () => {
+export const eraseDown = async () => {
   return StdoutWorker.invoke('Stdout.eraseDown')
 }
 
-export const backspace = () => {
+export const backspace = async () => {
   return StdoutWorker.invoke('Stdout.backspace')
 }
 
-export const cursorBackward = () => {
+export const cursorBackward = async () => {
   return StdoutWorker.invoke('Stdout.cursorBackward')
 }
 
-export const eraseEndLine = () => {
+export const eraseEndLine = async () => {
   return StdoutWorker.invoke('Stdout.eraseEndLine')
 }
 
-export const clear = (isWindows: boolean) => {
+export const clear = async (isWindows: boolean) => {
   return StdoutWorker.invoke('Stdout.clear', isWindows)
 }
