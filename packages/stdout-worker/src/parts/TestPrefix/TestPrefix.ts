50d9d7c962e606fd7bd0a5c7d693d318d22415d3
Simon Siefke <simon.siefke@gmail.com>
2025-08-21 18:20:40 +0200

    feature: add new messages to stdout worker (#1738)

--- packages/stdout-worker/src/parts/TestPrefix/TestPrefix.ts
+++ packages/stdout-worker/src/parts/TestPrefix/TestPrefix.ts
@@ -3,9 +3,13 @@ import * as TestPrefixText from '../TestPrefixText/TestPrefixText.ts'
 
 export const Pass: string = chalk.reset.inverse.bold.green(TestPrefixText.Pass)
 
+export const Initialized: string = chalk.reset.inverse.bold.green(TestPrefixText.Initialized)
+
 export const Leak: string = chalk.reset.inverse.blue(TestPrefixText.Leak)
 
 export const Runs: string = chalk.reset.inverse.yellow.bold(TestPrefixText.Runs)
+
+export const Initialize: string = chalk.reset.inverse.yellow.bold(TestPrefixText.Initializing)
 
 export const Setup: string = chalk.reset.inverse.yellow(TestPrefixText.Setup)
 
