cae4be82f81335edf7194dd8bc2296206890d0b9
Simon Siefke <simon.siefke@gmail.com>
2025-08-21 20:52:38 +0200

    remove unused code

--- packages/heap-snapshot-worker/test/GetLocations.test.ts
+++ packages/heap-snapshot-worker/test/GetLocations.test.ts
@@ -158,7 +158,7 @@ test('should handle large values', () => {
   }
 
   const indices = [0]
-  const locationIndices = new Uint32Array([0])
+  const locationIndices = new Uint32Array([0]) // Maps object index 0 to location array index 0
 
   const result = getLocations(snapshot, indices, locationIndices)
 
