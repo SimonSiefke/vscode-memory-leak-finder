95df36d2e9a6a7f17bca9b0e29286ce0f3c82b8f
Simon Siefke <simon.siefke@gmail.com>
2025-08-21 18:05:17 +0200

    fix runtime error

--- packages/repository-worker/src/parts/GetRestoreFileOperations/GetRestoreFileOperations.ts
+++ packages/repository-worker/src/parts/GetRestoreFileOperations/GetRestoreFileOperations.ts
@@ -1,4 +1,4 @@
-import { FileOperation } from '../FileOperation/FileOperation.ts'
+import type { FileOperation } from '../FileOperation/FileOperation.ts'
 import * as Path from '../Path/Path.ts'
 
 export const getRestoreNodeModulesFileOperations = (from: string, to: string, pathsToRestore: string[]): readonly FileOperation[] => {
