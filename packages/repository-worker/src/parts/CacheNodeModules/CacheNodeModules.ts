17d9d8477d10ac210d2f0c6c18818cd95ce9b0c3
Simon Siefke <simon.siefke@gmail.com>
2025-08-21 17:52:53 +0200

    remove unused code

--- packages/repository-worker/src/parts/CacheNodeModules/CacheNodeModules.ts
+++ packages/repository-worker/src/parts/CacheNodeModules/CacheNodeModules.ts
@@ -1,5 +1,4 @@
 import { VError } from '@lvce-editor/verror'
-import * as Filesystem from '../Filesystem/Filesystem.ts'
 import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.ts'
 import * as GetCacheFileOperations from '../GetCacheFileOperations/GetCacheFileOperations.ts'
 import * as Path from '../Path/Path.ts'
@@ -7,7 +6,7 @@ import * as Path from '../Path/Path.ts'
 export const addNodeModulesToCache = async (repoPath: string, commitHash: string, cacheDir: string) => {
   try {
     const cachedNodeModulesPath = Path.join(cacheDir, commitHash)
-    const allNodeModulesPaths = await Filesystem.findFiles('**/node_modules', { cwd: repoPath })
+    const allNodeModulesPaths = await FileSystemWorker.findFiles('**/node_modules', { cwd: repoPath })
     const nodeModulesPaths = allNodeModulesPaths.filter((path) => !path.includes('node_modules/node_modules') && !path.includes('.git'))
     const fileOperations = await GetCacheFileOperations.getCacheFileOperations(
       repoPath,
