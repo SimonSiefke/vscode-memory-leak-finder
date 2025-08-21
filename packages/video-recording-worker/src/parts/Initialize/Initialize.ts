d9ba13beaa7d05caedf908efab36deff74a760dd
Simon Siefke <simon.siefke@gmail.com>
2025-08-21 13:03:57 +0200

    download ffmpeg

--- packages/video-recording-worker/src/parts/Initialize/Initialize.ts
+++ packages/video-recording-worker/src/parts/Initialize/Initialize.ts
@@ -1,11 +1,14 @@
 import { existsSync } from 'fs'
 import * as GetFfmpegPath from '../GetFfmpegPath/GetFfmpegPath.ts'
+import * as DownloadWorker from '../DownloadWorker/DownloadWorker.js'
 
-export const intialize = () => {
+export const intialize = async () => {
   const ffmpegPath = GetFfmpegPath.getFfmpegPath()
   if (existsSync(ffmpegPath)) {
     return
   }
-  console.log('downloading ffmpeg')
-  // TODO download ffmpeg
+  console.log('[downloading ffmpeg]')
+  const rpc = await DownloadWorker.launch()
+  await rpc.invoke('Download.downloadFfmpegMaybe')
+  await rpc.dispose()
 }
