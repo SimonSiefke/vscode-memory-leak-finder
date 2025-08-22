3ae217f8cbaecaa372db8a96e651e24ca1d4d371
Simon Siefke <simon.siefke@gmail.com>
2025-08-22 22:36:32 +0200

    tsc

--- packages/heap-snapshot-worker/src/parts/FileSystem/FileSystem.ts
+++ packages/heap-snapshot-worker/src/parts/FileSystem/FileSystem.ts
@@ -1,10 +1,10 @@
 import * as NodeFs from 'node:fs/promises'
 import * as NodeFsSync from 'node:fs'
 
-export const readDir = (path) => {
+export const readDir = async (path: string): Promise<readonly string[]> => {
   return NodeFs.readdir(path)
 }
 
-export const readFileSync = (path, encoding) => {
+export const readFileSync = (path: string, encoding: 'utf8'): string => {
   return NodeFsSync.readFileSync(path, encoding)
 }
