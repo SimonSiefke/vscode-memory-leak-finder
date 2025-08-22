ecb263534eec87c9c6ce4aaf565101bf5e721ec3
Simon Siefke <simon.siefke@gmail.com>
2025-08-22 17:39:56 +0200

    debug

--- packages/heap-snapshot-worker/src/parts/AddLocationsToAstNodes/AddLocationsToAstNodes.ts
+++ packages/heap-snapshot-worker/src/parts/AddLocationsToAstNodes/AddLocationsToAstNodes.ts
@@ -2,12 +2,16 @@ import type { AstNode, ObjectNode } from '../AstNode/AstNode.ts'
 import type { Snapshot } from '../Snapshot/Snapshot.ts'
 import { findClosureLocationsForObjectId } from '../FindClosureLocationsForNode/FindClosureLocationsForNode.ts'
 
+const isObjectNode = (node: AstNode): node is ObjectNode => {
+  return node.type === 'object'
+}
+
 export const addLocationsToAstNodes = (snapshot: Snapshot, nodes: readonly AstNode[]): readonly AstNode[] => {
   return nodes.map((node) => {
-    if (node.type === 'object') {
+    if (isObjectNode(node)) {
       const locations = findClosureLocationsForObjectId(snapshot, node.id)
       if (locations.length) {
-        const withLocations: ObjectNode = { ...(node as ObjectNode), closureLocations: locations }
+        const withLocations: ObjectNode = { ...node, closureLocations: locations }
         return withLocations
       }
     }
