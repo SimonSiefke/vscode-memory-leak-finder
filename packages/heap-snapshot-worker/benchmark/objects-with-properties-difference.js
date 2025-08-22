d50cfeabad799da6368b3f5ec116d857d395a499
Simon Siefke <simon.siefke@gmail.com>
2025-08-22 11:29:57 +0200

    debug

--- packages/heap-snapshot-worker/benchmark/objects-with-properties-difference.js
+++ packages/heap-snapshot-worker/benchmark/objects-with-properties-difference.js
@@ -8,10 +8,10 @@ async function testGetObjectsWithProperties() {
 
   // Load the actual heap snapshot file
   // const heapSnapshotPath = '/home/simon/.cache/repos/vscode-memory-leak-finder/.vscode-heapsnapshots/abc2.heapsnapshot'
-  const beforePath = '/home/simon/.cache/repos/vscode-memory-leak-finder/.vscode-heapsnapshots/0.json'
-  const afterPath = '/home/simon/.cache/repos/vscode-memory-leak-finder/.vscode-heapsnapshots/1.json'
+  const beforePath = '/home/simon/.cache/repos/vscode-memory-leak-finder/.vscode-heapsnapshots/before.heapsnapshot'
+  const afterPath = '/home/simon/.cache/repos/vscode-memory-leak-finder/.vscode-heapsnapshots/after.heapsnapshot'
   const resultPath = join(import.meta.dirname, '..', '.tmp', 'difference.json')
-  const property = 'dispose'
+  const property = 'iAmLeaking'
   const depth = 1
 
   try {
@@ -30,13 +30,13 @@ async function testGetObjectsWithProperties() {
     console.log('Heap snapshot loaded successfully')
 
     console.log('\n=== Testing AST Function ===')
-    console.time('check-ast')
+    console.time('diff')
     // TODO maybe use worker for property query, since it takes some time
     // TODO the preview would only be needed at the end, meaning much less previews needed instead
     // of creating a preview for all objects
 
     const difference = getAddedObjectsWithPropertiesInternalAst(snapshot1, snapshot2, property, depth)
-    console.timeEnd('check-ast')
+    console.timeEnd('diff')
 
     // console.log('count 1', astObjects1.length)
     // console.log('count 2', astObjects2.length)
