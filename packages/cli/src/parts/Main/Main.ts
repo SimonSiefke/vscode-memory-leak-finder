c25934710c0b324bbc959436f72a298aab1433fc
Simon Siefke <simon.siefke@gmail.com>
2025-08-22 16:22:16 +0200

    fix: lint (#1748)

--- packages/cli/src/parts/Main/Main.ts
+++ packages/cli/src/parts/Main/Main.ts
@@ -2,6 +2,6 @@ import * as Cli from '../Cli/Cli.ts'
 
 export const main = async (): Promise<void> => {
   const argv: string[] = process.argv.slice(2)
-  const env = process.env
+  const {env} = process
   await Cli.run(process.platform, argv, env)
 }
