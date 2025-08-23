1aa2638483d97d65e4c281812500ce7a53b9eb6c
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 12:25:25 +0200

    feature: improve heapsnapshot functions measure (#1781)

---
+++ packages/heap-snapshot-worker/src/parts/CompareHeapSnapshotsFunctions2/CompareHeapSnapshotsFunctions2.ts
@@ -0,0 +1,25 @@
+import {
+  compareHeapSnapshotFunctionsInternal2,
+  type CompareResult,
+  type CompareFunctionsOptions,
+} from '../CompareHeapSnapshotsFunctionsInternal2/CompareHeapSnapshotsFunctionsInternal2.ts'
+import { prepareHeapSnapshot } from '../PrepareHeapSnapshot/PrepareHeapSnapshot.ts'
+
+export const compareHeapSnapshotFunctions2 = async (
+  pathA: string,
+  pathB: string,
+  options: CompareFunctionsOptions,
+): Promise<readonly CompareResult[]> => {
+  const [snapshotA, snapshotB] = await Promise.all([
+    prepareHeapSnapshot(pathA, {
+      parseStrings: true,
+    }),
+    prepareHeapSnapshot(pathB, {
+      parseStrings: true,
+    }),
+  ])
+  console.time('compare')
+  const result = compareHeapSnapshotFunctionsInternal2(snapshotA, snapshotB, options)
+  console.timeEnd('compare')
+  return result
+}
