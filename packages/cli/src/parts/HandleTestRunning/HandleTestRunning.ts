cc33d2ccc5d57273b2ac98ba6e766968f8d68861
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 15:33:12 +0200

    tsc

--- packages/cli/src/parts/HandleTestRunning/HandleTestRunning.ts
+++ packages/cli/src/parts/HandleTestRunning/HandleTestRunning.ts
@@ -1,5 +1,4 @@
 import * as GetHandleTestRunningMessage from '../GetHandleTestRunningMessage/GetHandleTestRunningMessage.ts'
-import * as StdinDataState from '../StdinDataState/StdinDataState.ts'
 import * as Stdout from '../Stdout/Stdout.ts'
 
 export const handleTestRunning = async (file: string, relativeDirName: string, fileName: string, isFirst: boolean): Promise<void> => {
