6f750c180b085d21034a02b5a13f23400e74bcd3
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 14:42:19 +0200

    simplify code

--- packages/memory-leak-finder/src/parts/WriteScriptMap/WriteScriptMap.ts
+++ packages/memory-leak-finder/src/parts/WriteScriptMap/WriteScriptMap.ts
@@ -1,4 +1,3 @@
-import { existsSync } from 'node:fs'
 import { mkdir, writeFile } from 'node:fs/promises'
 import { dirname, join } from 'node:path'
 import * as Root from '../Root/Root.ts'
@@ -9,8 +8,6 @@ export const writeScriptMap = async (
 ): Promise<void> => {
   const outFile = join(Root.root, '.vscode-script-maps', `${id}.json`)
   const dir = dirname(outFile)
-  if (!existsSync(dir)) {
-    await mkdir(dir, { recursive: true })
-  }
+  await mkdir(dir, { recursive: true })
   await writeFile(outFile, JSON.stringify(scriptMap, null, 2) + '\n')
 }
