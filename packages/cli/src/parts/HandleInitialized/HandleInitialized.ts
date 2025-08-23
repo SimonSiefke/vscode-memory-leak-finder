b1ca3cf5e80521aaa75bd9071cfb3bfbe227d08d
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 10:17:53 +0200

    fix: dont print intialize progress twice in github actions

--- packages/cli/src/parts/HandleInitialized/HandleInitialized.ts
+++ packages/cli/src/parts/HandleInitialized/HandleInitialized.ts
@@ -1,17 +1,18 @@
 import * as AnsiEscapes from '../AnsiEscapes/AnsiEscapes.ts'
 import { getInitializedMessage } from '../GetInitializedMessage/GetInitializedMessage.ts'
-import { isGithubActions, isWindows } from '../StdinDataState/StdinDataState.ts'
+import * as StdinDataState from '../StdinDataState/StdinDataState.ts'
+import { isWindows } from '../StdinDataState/StdinDataState.ts'
 import * as Stdout from '../Stdout/Stdout.ts'
 import * as TestStateOutput from '../TestStateOutput/TestStateOutput.ts'
 
 // TODO use functional approach returning new stdout state property without side effects
 export const handleInitialized = async (time: number): Promise<void> => {
-  const isGithub = isGithubActions()
+  if (StdinDataState.isGithubActions()) {
+    return
+  }
   const message = await getInitializedMessage(time)
   let fullMessage = ''
-  if (!isGithub) {
-    fullMessage += await AnsiEscapes.clear(isWindows())
-  }
+  fullMessage += await AnsiEscapes.clear(isWindows())
   fullMessage += message
   fullMessage += TestStateOutput.clearPending()
   await Stdout.write(fullMessage)
