02f5f7bb8e9cd5a9ba47846db23ec4fba2e4e7ba
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 11:09:08 +0200

    fix: close application on unexpected error (#1775)

--- packages/cli/src/parts/HandleStdinData/HandleStdinData.ts
+++ packages/cli/src/parts/HandleStdinData/HandleStdinData.ts
@@ -1,57 +1,9 @@
 import * as GetNewStdinState from '../GetNewStdinState/GetNewStdinState.ts'
-import * as HandleExit from '../HandleExit/HandleExit.ts'
-import * as KillWorkers from '../KillWorkers/KillWorkers.ts'
-import * as ModeType from '../ModeType/ModeType.ts'
-import * as PreviousFilters from '../PreviousFilters/PreviousFilters.ts'
-import * as StartRunning from '../StartRunning/StartRunning.ts'
 import * as StdinDataState from '../StdinDataState/StdinDataState.ts'
-import * as Stdout from '../Stdout/Stdout.ts'
+import * as UpdateState from '../UpdateState/UpdateState.ts'
 
 export const handleStdinData = async (key: string): Promise<void> => {
   const state = StdinDataState.getState()
   const newState = await GetNewStdinState.getNewStdinState(state, key)
-  if (newState === state) {
-    return
-  }
-  if (newState.stdout && newState.stdout.length > 0) {
-    await Stdout.write(newState.stdout.join(''))
-  }
-  if (newState.previousFilters.length > 0) {
-    await PreviousFilters.addAll(newState.previousFilters)
-  }
-  if (newState.mode === ModeType.Exit) {
-    return HandleExit.handleExit()
-  }
-  StdinDataState.setState({
-    ...newState,
-    stdout: [],
-    previousFilters: [],
-  })
-  if (newState.mode === ModeType.Running) {
-    await StartRunning.startRunning({
-      filterValue: state.value,
-      headlessMode: state.headless,
-      color: true,
-      checkLeaks: state.checkLeaks,
-      recordVideo: state.recordVideo,
-      cwd: state.cwd,
-      runs: state.runs,
-      measure: state.measure,
-      measureAfter: state.measureAfter,
-      timeouts: state.timeouts,
-      timeoutBetween: state.timeoutBetween,
-      restartBetween: state.restartBetween,
-      runMode: state.runMode,
-      ide: state.ide,
-      ideVersion: state.ideVersion,
-      vscodePath: '',
-      commit: '',
-      setupOnly: false,
-      workers: state.workers,
-      isWindows: state.isWindows,
-    })
-  }
-  if (newState.mode === ModeType.Interrupted) {
-    await KillWorkers.killWorkers()
-  }
+  await UpdateState.updateState(newState)
 }
