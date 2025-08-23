8e34b991951c0934890d2e903937d076814eeb46
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 08:28:42 +0200

    fix: caching of vscode version in ci not working (#1773)

--- scripts/computeNodeModulesCacheKey.js
+++ scripts/computeNodeModulesCacheKey.js
@@ -28,6 +28,7 @@ const locations = [
   '.github/workflows/ci.yml',
   '.github/workflows/release.yml',
   'scripts/computeNodeModulesCacheKey.js',
+  'packages/initialization-worker/src/parts/VsCodeVersion/VsCodeVersion.ts',
 ]
 
 const packagesFolder = join(root, 'packages')
