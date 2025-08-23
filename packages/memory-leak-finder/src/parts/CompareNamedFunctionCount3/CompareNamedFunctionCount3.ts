cf0e6847322c5a30eadeaf35a553de4dc1b7fe4b
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 19:31:27 +0200

    feature: improve functions comparison (#1792)

--- packages/memory-leak-finder/src/parts/CompareNamedFunctionCount3/CompareNamedFunctionCount3.ts
+++ packages/memory-leak-finder/src/parts/CompareNamedFunctionCount3/CompareNamedFunctionCount3.ts
@@ -3,6 +3,17 @@ import * as HeapSnapshotWorker from '../HeapSnapshotWorker/HeapSnapshotWorker.ts
 export const compareNamedFunctionCount3 = async (beforePath: string, afterPath: string): Promise<readonly any[]> => {
   const options = {
     minCount: 2,
+    excludeOriginalPaths: [
+      'editStack.ts',
+      'event.ts',
+      'functional.ts',
+      'lazy.ts',
+      'lifecycle.ts',
+      'numbers.ts',
+      'ternarySearchTree.ts',
+      'undoRedoService.ts',
+      'uri.ts',
+    ],
   }
   const result = await HeapSnapshotWorker.invoke('HeapSnapshot.compareFunctions2', beforePath, afterPath, options)
   return result
