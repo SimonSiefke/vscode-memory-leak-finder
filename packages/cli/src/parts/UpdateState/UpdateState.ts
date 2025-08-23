a92f7227afced080d05e262ee79a99f06c47c25f
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 09:49:57 +0200

    fix: close application on unexpected error

---
+++ packages/cli/src/parts/UpdateState/UpdateState.ts
@@ -0,0 +1,51 @@
+import * as HandleExit from '../HandleExit/HandleExit.ts'
+import * as KillWorkers from '../KillWorkers/KillWorkers.ts'
+import * as ModeType from '../ModeType/ModeType.ts'
+import * as PreviousFilters from '../PreviousFilters/PreviousFilters.ts'
+import * as StartRunning from '../StartRunning/StartRunning.ts'
+import * as StdinDataState from '../StdinDataState/StdinDataState.ts'
+import * as Stdout from '../Stdout/Stdout.ts'
+
+export const updateState = async (newState: any): Promise<void> => {
+  const state = StdinDataState.getState()
+  if (newState === state) {
+    return
+  }
+  if (newState.stdout && newState.stdout.length > 0) {
+    await Stdout.write(newState.stdout.join(''))
+  }
+  if (newState.previousFilters.length > 0) {
+    await PreviousFilters.addAll(newState.previousFilters)
+  }
+  if (newState.mode === ModeType.Exit || newState.mode === ModeType.FinishedRunning) {
+    return HandleExit.handleExit()
+  }
+  StdinDataState.setState({ ...newState, stdout: [], previousFilters: [] })
+  if (state.mode !== ModeType.Running && newState.mode === ModeType.Running) {
+    await StartRunning.startRunning(
+      state.value,
+      state.headless,
+      /* color */ true,
+      state.checkLeaks,
+      state.recordVideo,
+      state.cwd,
+      state.runs,
+      state.measure,
+      state.measureAfter,
+      state.timeouts,
+      state.timeoutBetween,
+      state.restartBetween,
+      state.runMode,
+      state.ide,
+      state.ideVersion,
+      '',
+      '',
+      false,
+      state.workers,
+      state.isWindows,
+    )
+  }
+  if (newState.mode === ModeType.Interrupted) {
+    await KillWorkers.killWorkers()
+  }
+}
