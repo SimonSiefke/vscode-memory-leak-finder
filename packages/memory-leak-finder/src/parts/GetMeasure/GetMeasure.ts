2a5ffaa577b1d5a6a1b92029adbc88c93cfa0d26
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 09:30:29 +0200

    tsc

--- packages/memory-leak-finder/src/parts/GetMeasure/GetMeasure.ts
+++ packages/memory-leak-finder/src/parts/GetMeasure/GetMeasure.ts
@@ -8,7 +8,9 @@ interface Measure {
 export const getMeasure = (MemoryLeakFinder: any, measureId: string): Measure => {
   const camelCaseId = CamelCase.camelCase(measureId)
   for (const measure of Object.values(MemoryLeakFinder.Measures)) {
+    // @ts-ignore
     if (measure.id === camelCaseId) {
+      // @ts-ignore
       return measure
     }
   }
