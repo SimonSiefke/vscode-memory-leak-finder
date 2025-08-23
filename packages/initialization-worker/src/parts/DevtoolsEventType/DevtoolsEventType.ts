9bcfd77371fae95749ac664597cdd6c4c5783459
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 22:20:52 +0200

    remove unused code

--- packages/initialization-worker/src/parts/DevtoolsEventType/DevtoolsEventType.ts
+++ packages/initialization-worker/src/parts/DevtoolsEventType/DevtoolsEventType.ts
@@ -1,18 +1,3 @@
 export const DebuggerPaused = 'Debugger.paused'
-export const DebuggerResumed = 'Debugger.resumed'
-export const DebuggerScriptParsed = 'Debugger.scriptParsed'
-
-export const PageFrameNavigated = 'Page.frameNavigated'
-export const PageLifeCycleEvent = 'Page.lifecycleEvent'
-export const PageLoadEventFired = 'Page.loadEventFired'
-
-export const RuntimeExecutionContextCreated = 'Runtime.executionContextCreated'
-export const RuntimeExecutionContextDestroyed = 'Runtime.executionContextDestroyed'
-export const RuntimeExecutionContextsCleared = 'Runtime.executionContextsCleared'
 
 export const TargetAttachedToTarget = 'Target.attachedToTarget'
-export const TargetDetachedFromTarget = 'Target.detachedFromTarget'
-export const TargetTargetCrashed = 'Target.targetCrashed'
-export const TargetTargetCreated = 'Target.targetCreated'
-export const TargetTargetDestroyed = 'Target.targetDestroyed'
-export const TargetTargetInfoChanged = 'Target.targetInfoChanged'
