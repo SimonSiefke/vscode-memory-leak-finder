a4073e1c64fe01868f4b48817ca68a8665de28d1
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 18:23:01 +0200

    formt

--- packages/memory-leak-finder/src/parts/CompareNamedFunctionCount3/CompareNamedFunctionCount3.ts
+++ packages/memory-leak-finder/src/parts/CompareNamedFunctionCount3/CompareNamedFunctionCount3.ts
@@ -3,10 +3,7 @@ import * as HeapSnapshotWorker from '../HeapSnapshotWorker/HeapSnapshotWorker.ts
 export const compareNamedFunctionCount3 = async (beforePath: string, afterPath: string): Promise<readonly any[]> => {
   const options = {
     minCount: 2,
-    excludeOriginalPaths:['event.ts', 'functional.ts', 'lifecycle.ts', 'uri.ts', 
-'ternarySearchTree.ts'
-
-    ]
+    excludeOriginalPaths: ['event.ts', 'functional.ts', 'lifecycle.ts', 'uri.ts', 'ternarySearchTree.ts'],
   }
   const result = await HeapSnapshotWorker.invoke('HeapSnapshot.compareFunctions2', beforePath, afterPath, options)
   return result
