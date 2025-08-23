9633a54e4d2f766b98ab498e3f8e3cd177a1267a
Simon Siefke <simon.siefke@gmail.com>
2025-08-23 21:31:03 +0200

    feature: simplify cli code (#1794)

--- packages/cli/src/parts/GetInitializedMessage/GetInitializedMessage.ts
+++ packages/cli/src/parts/GetInitializedMessage/GetInitializedMessage.ts
@@ -1,9 +1,5 @@
-import { formatDuration } from '../FormatDuration/FormatDuration.ts'
-import { Initialized } from '../TestPrefix/TestPrefix.ts'
+import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 
 export const getInitializedMessage = async (time: number): Promise<string> => {
-  const formattedTime = formatDuration(time)
-
-  const messageFileName = `${Initialized} ${formattedTime}`
-  return `${messageFileName}\n`
+  return StdoutWorker.invoke('Stdout.getInitializedMessage', time)
 }
