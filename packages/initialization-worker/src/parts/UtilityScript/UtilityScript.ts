17771612a33a4e44a922d1bb059351dfd4e0d238
Simon Siefke <simon.siefke@gmail.com>
2025-08-24 12:07:14 +0200

    feature: run build if outfile doesn't exist (#1797)

--- packages/initialization-worker/src/parts/UtilityScript/UtilityScript.ts
+++ packages/initialization-worker/src/parts/UtilityScript/UtilityScript.ts
@@ -1,8 +1,22 @@
-import { readFileSync } from 'node:fs'
+import { existsSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import * as Root from '../Root/Root.ts'
+import * as LaunchFileSystemWorker from '../LaunchFileSystemWorker/LaunchFileSystemWorker.ts'
 
-export const getUtilityScript = (): string => {
-  const injectedCode = readFileSync(join(Root.root, 'packages', 'injected-code', 'dist', 'injectedCode.js'), 'utf8')
+const utiltyScriptPath = join(Root.root, 'packages', 'injected-code', 'dist', 'injectedCode.js')
+
+const runBuild = async () => {
+  const rpc = await LaunchFileSystemWorker.launchFileSystemWorker()
+  await rpc.invoke('FileSystem.exec', `npm`, ['run', 'build'], {
+    cwd: Root.root,
+  })
+  await rpc.dispose()
+}
+
+export const getUtilityScript = async (): Promise<string> => {
+  if (!existsSync(utiltyScriptPath)) {
+    await runBuild()
+  }
+  const injectedCode = readFileSync(utiltyScriptPath, 'utf-8')
   return injectedCode
 }
