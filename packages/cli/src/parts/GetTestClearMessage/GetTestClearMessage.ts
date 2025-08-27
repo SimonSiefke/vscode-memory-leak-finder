d6570138ee18333b0299dbbce612f430be2ed8f4
Simon Siefke <simon.siefke@gmail.com>
2025-08-27 21:37:35 +0200

    feature: query message from worker

--- packages/cli/src/parts/GetTestClearMessage/GetTestClearMessage.ts
+++ packages/cli/src/parts/GetTestClearMessage/GetTestClearMessage.ts
@@ -1,8 +1,5 @@
-const clear = '\r\u001B[K\r\u001B[1A'
-const height = 2
-const clearMessage = clear.repeat(height)
+import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 
-// TODO move this to stdout worker
 export const getTestClearMessage = async (): Promise<string> => {
-  return clearMessage
+  return StdoutWorker.invoke('Stdout.getTestClearMessage')
 }
