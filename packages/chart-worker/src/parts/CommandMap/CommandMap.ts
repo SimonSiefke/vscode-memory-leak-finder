3167b2241b938f74ff25024d2fee0bb519334f62
Simon Siefke <simon.siefke@gmail.com>
2025-08-27 22:18:13 +0200

    chart

--- packages/chart-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chart-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as CreateChat from '../CreateChart/CreateChart.ts'
+import * as CreateBarChart from '../CreateBarChart/CreateBarChart.ts'
 
 export const commandMap = {
   'Chart.create': CreateChat.createChart,
+  'Chart.createBarChart': CreateBarChart.createBarChart,
 }
---
+++ packages/chart-worker/src/parts/CreateBarChart/CreateBarChart.ts
@@ -0,0 +1,29 @@
+import { fixHtmlNamespace } from '../FixXmlNamespace/FixXmlNamespace.ts'
+import * as Plot from '../Plot/Plot.ts'
+
+export const createBarChart = (data) => {
+  const baseHtml = Plot.plot({
+    marginLeft: 90,
+    x: { axis: null },
+    y: { label: null },
+    marks: [
+      Plot.barX(data, {
+        x: 'value',
+        y: 'name',
+        sort: { y: 'x', reverse: true, limit: 10 },
+      }),
+
+      Plot.text(data, {
+        text: (d) => `${Math.floor(d.value / 1000)} B`,
+        y: 'name',
+        x: 'value',
+        textAnchor: 'end',
+        dx: -3,
+        fill: 'white',
+      }),
+    ],
+  }).outerHTML
+
+  const finalHtml = fixHtmlNamespace(baseHtml)
+  return finalHtml
+}
