7e2e7995656335ed97c5ff26cf071abead84ad32
Simon Siefke <simon.siefke@gmail.com>
2025-08-27 20:19:32 +0200

    disable extra message

--- packages/cli/src/parts/HandleTestRunning/HandleTestRunning.ts
+++ packages/cli/src/parts/HandleTestRunning/HandleTestRunning.ts
@@ -1,7 +1,11 @@
 import * as GetHandleTestRunningMessage from '../GetHandleTestRunningMessage/GetHandleTestRunningMessage.ts'
 import * as Stdout from '../Stdout/Stdout.ts'
+import * as StdinDataState from '../StdinDataState/StdinDataState.ts'
 
 export const handleTestRunning = async (file: string, relativeDirName: string, fileName: string, isFirst: boolean): Promise<void> => {
+  if (StdinDataState.isGithubActions()) {
+    return
+  }
   const message = await GetHandleTestRunningMessage.getHandleTestRunningMessage(file, relativeDirName, fileName, isFirst)
   await Stdout.write(message)
 }
