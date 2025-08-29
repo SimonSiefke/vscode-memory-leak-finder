6a6d0ed090cbe8b9e1ef1cf8eccd5ed5b2e065c0
Simon Siefke <simon.siefke@gmail.com>
2025-08-29 20:15:40 +0200

    feature: use worker for querying original name

--- packages/source-map-worker/src/parts/GetOriginalClassName/GetOriginalClassName.ts
+++ packages/source-map-worker/src/parts/GetOriginalClassName/GetOriginalClassName.ts
@@ -1,30 +1,5 @@
-const RE_CLASSNAME: RegExp = /^[a-zA-Z\d]+/
-const classPrefix: string = 'class '
-const extendsPrefix: string = 'extends'
+import * as OriginalNameWorker from '../OriginalNameWorker/OriginalNameWorker.ts'
 
 export const getOriginalClassName = async (sourceContent: string, originalLine: number, originalColumn: number): Promise<string> => {
-  if (!sourceContent) {
-    return 'unknown'
-  }
-  const lines: string[] = sourceContent.split('\n')
-  for (let i = originalLine; i >= 0; i--) {
-    const line: string = lines[i]
-    const classIndex: number = line.indexOf(classPrefix)
-    if (classIndex !== -1) {
-      const rest: string = line.slice(classIndex + classPrefix.length)
-      const match: RegExpMatchArray | null = rest.match(RE_CLASSNAME)
-      if (match) {
-        const originalClassName: string = match[0]
-        if (originalClassName === extendsPrefix) {
-          const other: string = rest.slice(extendsPrefix.length + 1)
-          const otherMatch: RegExpMatchArray | null = other.match(RE_CLASSNAME)
-          if (otherMatch) {
-            return `class extends ` + otherMatch[0]
-          }
-        }
-        return originalClassName
-      }
-    }
-  }
-  return 'unknown'
+  return OriginalNameWorker.invoke('OriginalName.getOriginalName', sourceContent, originalLine, originalColumn)
 }
---
+++ packages/source-map-worker/src/parts/OriginalNameWorker/OriginalNameWorker.ts
@@ -0,0 +1,33 @@
+import { NodeWorkerRpcParent, Rpc } from '@lvce-editor/rpc'
+import { getOriginalNameWorkerPath } from '../OriginalNameWorkerPath/OriginalNameWorkerPath.ts'
+
+let rpcPromise: Promise<Rpc> | undefined
+
+const create = async () => {
+  const rpc = await NodeWorkerRpcParent.create({
+    path: getOriginalNameWorkerPath(),
+    commandMap: {},
+  })
+  return rpc
+}
+
+const getOrCreateRpc = () => {
+  if (!rpcPromise) {
+    rpcPromise = create()
+  }
+  return rpcPromise
+}
+
+export const invoke = async (method: string, ...params: readonly any[]) => {
+  const rpc = await getOrCreateRpc()
+  return rpc.invoke(method, ...params)
+}
+
+export const dispose = async () => {
+  if (rpcPromise) {
+    const promise = rpcPromise
+    rpcPromise = undefined
+    const rpc = await promise
+    rpc.dispose()
+  }
+}
