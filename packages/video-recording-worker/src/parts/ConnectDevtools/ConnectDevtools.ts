f491f88219c8e94b91568b191df751996789772d
Simon Siefke <simon.siefke@gmail.com>
2025-08-29 20:55:21 +0200

    remove unsed

--- packages/video-recording-worker/src/parts/ConnectDevtools/ConnectDevtools.ts
+++ packages/video-recording-worker/src/parts/ConnectDevtools/ConnectDevtools.ts
@@ -5,24 +5,14 @@ import * as DebuggerCreateSessionRpcConnection from '../DebuggerCreateSessionRpc
 import * as DevtoolsEventType from '../DevtoolsEventType/DevtoolsEventType.ts'
 import { DevtoolsProtocolPage, DevtoolsProtocolTarget } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 import * as HandleFrame from '../HandleFrame/HandleFrame.ts'
-import * as ObjectType from '../ObjectType/ObjectType.ts'
 import * as PTimeout from '../PTimeout/PTimeout.ts'
 import * as ScreencastQuality from '../ScreencastQuality/ScreencastQuality.ts'
-import * as SessionState from '../SessionState/SessionState.ts'
 import { waitForAttachedEvent } from '../WaitForAttachedEvent/WaitForAttachedEvent.ts'
 
 export const connectDevtools = async (devtoolsWebSocketUrl: string, attachedToPageTimeout: number): Promise<void> => {
   Assert.string(devtoolsWebSocketUrl)
   const browserIpc = await DebuggerCreateIpcConnection.createConnection(devtoolsWebSocketUrl)
   const browserRpc = DebuggerCreateRpcConnection.createRpc(browserIpc)
-
-  SessionState.addSession('browser', {
-    type: ObjectType.Browser,
-    objectType: ObjectType.Browser,
-    url: '',
-    sessionId: '',
-    rpc: browserRpc,
-  })
 
   const eventPromise = waitForAttachedEvent(browserRpc, attachedToPageTimeout)
 
