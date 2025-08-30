d4a46715f4c6fa9b661c4f15a8ea3150987b7907
Simon Siefke <simon.siefke@gmail.com>
2025-08-30 12:05:58 +0200

    improve versions

--- packages/download-worker/src/parts/DownloadGithubCopilot/DownloadGithubCopilot.ts
+++ packages/download-worker/src/parts/DownloadGithubCopilot/DownloadGithubCopilot.ts
@@ -3,11 +3,11 @@ import { downloadExtensions } from '../DownloadExtensions/DownloadExtensions.ts'
 const extensions = [
   {
     id: `GitHub/copilot-chat`,
-    version: '0.31.2025082904',
+    version: '0.30.3',
   },
   {
     id: 'GitHub/copilot',
-    version: '1.364.1768',
+    version: '1.364.0',
   },
 ]
 
