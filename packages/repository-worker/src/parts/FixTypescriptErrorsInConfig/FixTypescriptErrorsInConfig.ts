547ede80431e6909490811bdaca3d6795796b7e3
Simon Siefke <simon.siefke@gmail.com>
2025-09-01 12:27:55 +0200

    refactor

---
+++ packages/repository-worker/src/parts/FixTypescriptErrorsInConfig/FixTypescriptErrorsInConfig.ts
@@ -0,0 +1,23 @@
+import { computeTsIgnoreOperations } from '../ComputeTsIgnoreOperations/ComputeTsIgnoreOperations.ts'
+import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.ts'
+import { parseTscErrors } from '../ParseTscErrors/ParseTscErrors.ts'
+import * as Path from '../Path/Path.ts'
+import { runTsc } from '../RunTsc/RunTsc.ts'
+
+export const fixTypescriptErrorsInConfig = async (configPath: string): Promise<void> => {
+  const cwd = Path.join(configPath, '..')
+  const result = await runTsc(cwd)
+  if (result.exitCode === 0) {
+    return
+  }
+  const output = `${result.stdout}\n${result.stderr}`
+  const locations = parseTscErrors(output, cwd)
+  if (locations.length === 0) {
+    return
+  }
+  const operations = await computeTsIgnoreOperations(locations)
+  if (operations.length === 0) {
+    return
+  }
+  await FileSystemWorker.applyFileOperations(operations)
+}
