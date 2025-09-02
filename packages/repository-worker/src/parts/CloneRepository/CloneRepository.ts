e6a840e57ec470decfebdbf71bd175f45f4f7c94
Simon Siefke <simon.siefke@gmail.com>
2025-09-02 18:49:33 +0000

    fix

--- packages/repository-worker/src/parts/CloneRepository/CloneRepository.ts
+++ packages/repository-worker/src/parts/CloneRepository/CloneRepository.ts
@@ -6,7 +6,7 @@ import { exec } from '../Exec/Exec.ts'
 export const cloneRepository = async (repoUrl:string, repoPath:string, commit:string):Promise<void> => {
   try {
     await mkdir(repoPath, {recursive:true})
-    await exec('git', ['init'], {cwd: repoPath})
+    await exec('git', ['-c',  'init.defaultbranch=main', 'init'], {cwd: repoPath})
     await exec('git', ['remote', 'add', 'origin', repoUrl], {cwd:repoPath})
     await exec('git',['fetch', '--depth', '1', 'origin', commit], {cwd:repoPath})
   } catch (error) {
