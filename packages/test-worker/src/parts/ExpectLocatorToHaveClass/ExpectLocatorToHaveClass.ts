3845a93ee8ba2aba505a1ca4dce8bbcb830c4346
Simon Siefke <simon.siefke@gmail.com>
2025-09-03 14:12:40 +0200

    feature: increase tab timeout, trying to make test less flaky

--- packages/test-worker/src/parts/ExpectLocatorToHaveClass/ExpectLocatorToHaveClass.ts
+++ packages/test-worker/src/parts/ExpectLocatorToHaveClass/ExpectLocatorToHaveClass.ts
@@ -1,7 +1,8 @@
 import * as ExpectLocatorSingleElementCondition from '../ExpectLocatorSingleElementCondition/ExpectLocatorSingleElementCondition.ts'
 
-export const toHaveClass = (locator, className) => {
+export const toHaveClass = (locator, className, options) => {
   return ExpectLocatorSingleElementCondition.checkSingleElementCondition('toHaveClass', locator, {
     className,
+    ...options,
   })
 }
