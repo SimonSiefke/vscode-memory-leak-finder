9948acdcb36bbe4647a7381e7adb1f2b122f2def
Simon Siefke <simon.siefke@gmail.com>
2025-09-03 18:24:04 +0000

    fix: try to fix error when copying node modules

--- packages/repository-worker/src/parts/InstallDependencies/InstallDependencies.ts
+++ packages/repository-worker/src/parts/InstallDependencies/InstallDependencies.ts
@@ -1,5 +1,7 @@
 import { VError } from '@lvce-editor/verror'
 import { exec } from '../Exec/Exec.ts'
+import { join } from 'node:path'
+import { rm } from 'node:fs/promises'
 
 const doInstallDependencies = async (cwd: string, useNice: boolean) => {
   if (useNice) {
@@ -8,9 +10,23 @@ const doInstallDependencies = async (cwd: string, useNice: boolean) => {
   return exec('npm', ['ci'], { cwd, env: process.env })
 }
 
+
+const removePotentiallyConflictingFiles=async (cwd:string):Promise<void>=>{
+  const toRemove=[
+    '/test/mcp/node_modules/npm-run-all/node_modules/which/bin'
+  ]
+  const absolutePaths=toRemove.map(item=>{
+    return join(cwd, item )
+  })
+  for(const item of absolutePaths){
+    await rm(item, {recursive:true,force:true })
+  }
+}
+
 export const installDependencies = async (cwd: string, useNice: boolean): Promise<void> => {
   try {
     const child = doInstallDependencies(cwd, useNice)
+    await removePotentiallyConflictingFiles(cwd)
     await child
   } catch (error) {
     throw new VError(error, `Failed to install dependencies in directory '${cwd}'`)
