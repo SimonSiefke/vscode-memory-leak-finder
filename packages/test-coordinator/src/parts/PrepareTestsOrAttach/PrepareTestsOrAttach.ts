41c43b58ab03c45fcb41ce94c59d4b94dc4ae163
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 17:22:56 +0200

    feature: pass sessionId to test worker (#1915)

--- packages/test-coordinator/src/parts/PrepareTestsOrAttach/PrepareTestsOrAttach.ts
+++ packages/test-coordinator/src/parts/PrepareTestsOrAttach/PrepareTestsOrAttach.ts
@@ -66,7 +66,7 @@ export const prepareTestsOrAttach = async (
     state.utilityContext = result.utilityContext
     return testWorkerRpc
   }
-  const { webSocketUrl, devtoolsWebSocketUrl, electronObjectId, parsedVersion, utilityContext } = await state.promise
+  const { webSocketUrl, devtoolsWebSocketUrl, electronObjectId, parsedVersion, utilityContext, sessionId, targetId } = await state.promise
   const isFirstConnection = false
   const canUseIdleCallback = CanUseIdleCallback.canUseIdleCallback(headlessMode)
   await ConnectDevtools.connectDevtools(
@@ -84,6 +84,8 @@ export const prepareTestsOrAttach = async (
     parsedVersion,
     timeouts,
     utilityContext,
+    sessionId,
+    targetId,
   )
   return testWorkerRpc
 }
