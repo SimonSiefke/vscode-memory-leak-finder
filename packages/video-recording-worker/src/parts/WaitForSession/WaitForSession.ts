ccc818cc0e5f963c00b7b44830cc666a5bac6769
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 18:25:59 +0200

    feature: simplify video connection (#1917)

---
+++ packages/video-recording-worker/src/parts/WaitForSession/WaitForSession.ts
@@ -0,0 +1,28 @@
+import * as DebuggerCreateSessionRpcConnection from '../DebuggerCreateSessionRpcConnection/DebuggerCreateSessionRpcConnection.ts'
+import { DevtoolsProtocolTarget } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
+import { waitForAttachedEvent } from '../WaitForAttachedEvent/WaitForAttachedEvent.ts'
+
+export const waitForSession = async (browserRpc: any, attachedToPageTimeout: number) => {
+  const eventPromise = waitForAttachedEvent(browserRpc, attachedToPageTimeout)
+
+  await DevtoolsProtocolTarget.setAutoAttach(browserRpc, {
+    autoAttach: true,
+    waitForDebuggerOnStart: true,
+    flatten: true,
+  })
+
+  const event = await eventPromise
+
+  if (!event) {
+    throw new Error(`Failed to attach to page`)
+  }
+  const { sessionId, targetInfo } = event.params
+  const sessionRpc = DebuggerCreateSessionRpcConnection.createSessionRpcConnection(browserRpc, sessionId)
+
+  // TODO can remove attachment now
+  return {
+    sessionId,
+    targetId: targetInfo.targetId,
+    sessionRpc,
+  }
+}
