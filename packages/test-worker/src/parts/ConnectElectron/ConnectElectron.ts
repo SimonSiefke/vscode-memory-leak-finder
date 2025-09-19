425b142c03934ebc46b29d5caab23799e8e75f83
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 13:12:39 +0200

    Discard changes to packages/test-worker/src/parts/ConnectElectron/ConnectElectron.ts

--- packages/test-worker/src/parts/ConnectElectron/ConnectElectron.ts
+++ packages/test-worker/src/parts/ConnectElectron/ConnectElectron.ts
@@ -1,6 +1,9 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as DebuggerCreateIpcConnection from '../DebuggerCreateIpcConnection/DebuggerCreateIpcConnection.ts'
 import * as DebuggerCreateRpcConnection from '../DebuggerCreateRpcConnection/DebuggerCreateRpcConnection.ts'
+import * as DevtoolsEventType from '../DevtoolsEventType/DevtoolsEventType.ts'
+import { DevtoolsProtocolDebugger, DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
+import * as ScenarioFunctions from '../ScenarioFunctions/ScenarioFunctions.ts'
 
 export const connectElectron = async (
   connectionId: number,
@@ -16,5 +19,17 @@ export const connectElectron = async (
   Assert.boolean(canUseIdleCallback)
   const electronIpc = await DebuggerCreateIpcConnection.createConnection(webSocketUrl)
   const electronRpc = DebuggerCreateRpcConnection.createRpc(electronIpc, canUseIdleCallback)
+
+  electronRpc.on(DevtoolsEventType.DebuggerResumed, ScenarioFunctions.handleResumed)
+  electronRpc.on(DevtoolsEventType.DebuggerScriptParsed, ScenarioFunctions.handleScriptParsed)
+  electronRpc.on(DevtoolsEventType.RuntimeExecutionContextCreated, ScenarioFunctions.handleRuntimeExecutionContextCreated)
+  electronRpc.on(DevtoolsEventType.RuntimeExecutionContextDestroyed, ScenarioFunctions.handleRuntimeExecutionContextDestroyed)
+
+  await Promise.all([
+    DevtoolsProtocolDebugger.enable(electronRpc), // TODO debugger might not be needed
+    DevtoolsProtocolRuntime.enable(electronRpc),
+    DevtoolsProtocolRuntime.runIfWaitingForDebugger(electronRpc),
+  ])
+
   return electronRpc
 }
