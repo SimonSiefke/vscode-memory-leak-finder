93b6a2b7d03f35f391f6502e98b9ba2e55156133
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 13:15:07 +0200

    tsc

--- packages/test-worker/src/parts/ExpectLocatorToHaveWindowCount/ExpectLocatorToHaveWindowCount.ts
+++ packages/test-worker/src/parts/ExpectLocatorToHaveWindowCount/ExpectLocatorToHaveWindowCount.ts
@@ -1,5 +1,5 @@
 import { ExpectError } from '../ExpectError/ExpectError.ts'
-// import * as SessionState from '../SessionState/SessionState.ts'
+import * as SessionState from '../SessionState/SessionState.ts'
 
 const isWindow = (session) => {
   return session.type === 'page'
