3353de78e168b2fc41adb84a89775f144ebd2182
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 13:34:02 +0200

    Discard changes to packages/test-worker/src/parts/CommandMap/CommandMap.ts

--- packages/test-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/test-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,4 +1,6 @@
 import * as ConnectDevtools from '../ConnectDevtools/ConnectDevtools.ts'
+import * as ImportPageObject from '../ImportPageObject/ImportPageObject.ts'
+import * as PageObject from '../PageObject/PageObject.ts'
 import * as RunTest from '../RunTest/RunTest.ts'
 import * as SetupTest from '../SetupTest/SetupTest.ts'
 import * as TearDownTest from '../TearDownTest/TearDownTest.ts'
@@ -6,6 +8,8 @@ import * as TestWorkerCommandType from '../TestWorkerCommandType/TestWorkerComma
 
 export const commandMap = {
   [TestWorkerCommandType.ConnectDevtools]: ConnectDevtools.connectDevtools,
+  [TestWorkerCommandType.ImportPageObjectModule]: ImportPageObject.importPageObject,
+  [TestWorkerCommandType.PageObjectCreate]: PageObject.create,
   [TestWorkerCommandType.RunTest]: RunTest.runTest,
   [TestWorkerCommandType.SetupTest]: SetupTest.setupTest,
   [TestWorkerCommandType.TearDownTest]: TearDownTest.tearDownTest,
