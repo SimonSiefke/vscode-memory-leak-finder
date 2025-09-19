9388bec6761259a29d12d4b1949d57b8b9c26928
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 17:48:23 +0200

    todo

--- packages/memory-leak-finder/src/parts/ConnectDevtools/ConnectDevtools.ts
+++ packages/memory-leak-finder/src/parts/ConnectDevtools/ConnectDevtools.ts
@@ -1,15 +1,9 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as DebuggerCreateIpcConnection from '../DebuggerCreateIpcConnection/DebuggerCreateIpcConnection.ts'
 import * as DebuggerCreateRpcConnection from '../DebuggerCreateRpcConnection/DebuggerCreateRpcConnection.ts'
-import * as DebuggerCreateSessionRpcConnection from '../DebuggerCreateSessionRpcConnection/DebuggerCreateSessionRpcConnection.ts'
 import { DevtoolsProtocolPage, DevtoolsProtocolRuntime, DevtoolsProtocolTarget } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 import * as GetCombinedMeasure from '../GetCombinedMeasure/GetCombinedMeasure.ts'
 import * as MemoryLeakFinderState from '../MemoryLeakFinderState/MemoryLeakFinderState.ts'
-import * as ObjectType from '../ObjectType/ObjectType.ts'
-import * as PTimeout from '../PTimeout/PTimeout.ts'
-import * as SessionState from '../SessionState/SessionState.ts'
-import * as TimeoutConstants from '../TimeoutConstants/TimeoutConstants.ts'
-import { waitForAttachedEvent } from '../WaitForAttachedEvent/WaitForAttachedEvent.ts'
 import { waitForSession } from '../WaitForSession/WaitForSession.ts'
 
 export const connectDevtools = async (
@@ -18,6 +12,7 @@ export const connectDevtools = async (
   measureId: string,
   attachedToPageTimeout: number,
 ): Promise<void> => {
+  // TODO connect to electron and node processes if should measure node
   Assert.string(devtoolsWebSocketUrl)
   const browserIpc = await DebuggerCreateIpcConnection.createConnection(devtoolsWebSocketUrl)
   const browserRpc = DebuggerCreateRpcConnection.createRpc(browserIpc)
