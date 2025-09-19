7c124494764a93adf2a0419962ebb2573983a08a
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 13:34:08 +0200

    Discard changes to packages/test-worker/src/parts/ConnectDevtools/ConnectDevtools.ts

--- packages/test-worker/src/parts/ConnectDevtools/ConnectDevtools.ts
+++ packages/test-worker/src/parts/ConnectDevtools/ConnectDevtools.ts
@@ -4,18 +4,11 @@ import * as DebuggerCreateIpcConnection from '../DebuggerCreateIpcConnection/Deb
 import * as DebuggerCreateRpcConnection from '../DebuggerCreateRpcConnection/DebuggerCreateRpcConnection.ts'
 import * as DevtoolsEventType from '../DevtoolsEventType/DevtoolsEventType.ts'
 import { DevtoolsProtocolTarget } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
-import * as DisableTimeouts from '../DisableTimeouts/DisableTimeouts.ts'
 import * as ElectronApp from '../ElectronApp/ElectronApp.ts'
-import * as Expect from '../Expect/Expect.ts'
-import * as ImportScript from '../ImportScript/ImportScript.ts'
+import * as ElectronAppState from '../ElectronAppState/ElectronAppState.ts'
 import * as ObjectType from '../ObjectType/ObjectType.ts'
-import * as Page from '../Page/Page.ts'
-import * as PageObjectState from '../PageObjectState/PageObjectState.ts'
 import * as ScenarioFunctions from '../ScenarioFunctions/ScenarioFunctions.ts'
 import * as SessionState from '../SessionState/SessionState.ts'
-import { VError } from '../VError/VError.ts'
-import { waitForSession } from '../WaitForSession/WaitForSession.ts'
-import { waitForUtilityExecutionContext } from '../WaitForUtilityExecutionContext/WaitForUtilityExecutionContext.ts'
 
 export const connectDevtools = async (
   connectionId: number,
@@ -26,13 +19,10 @@ export const connectDevtools = async (
   webSocketUrl: string,
   canUseIdleCallback: boolean,
   idleTimeout: number,
-  pageObjectPath: string,
-  isHeadless: boolean,
-  parsedIdeVersion: any,
-  timeouts: boolean,
 ) => {
   Assert.number(connectionId)
   Assert.string(devtoolsWebSocketUrl)
+  // Assert.string(monkeyPatchedElectronId)
   Assert.boolean(isFirstConnection)
 
   const electronRpc = await connectElectron(connectionId, headlessMode, webSocketUrl, isFirstConnection, canUseIdleCallback)
@@ -63,7 +53,6 @@ export const connectDevtools = async (
   browserRpc.on(DevtoolsEventType.TargetTargetDestroyed, ScenarioFunctions.handleTargetDestroyed)
   browserRpc.on(DevtoolsEventType.TargetTargetInfoChanged, ScenarioFunctions.handleTargetInfoChanged)
 
-  // TODO probably not needed
   await Promise.all([
     DevtoolsProtocolTarget.setAutoAttach(browserRpc, {
       autoAttach: true,
@@ -74,41 +63,10 @@ export const connectDevtools = async (
       discover: true,
     }),
   ])
-
-  const { sessionRpc, sessionId, targetId } = await waitForSession(browserRpc, 5000)
-
-  const utilityContext = await waitForUtilityExecutionContext(sessionRpc)
-
-  console.log('got util', utilityContext)
-  const firstWindow = Page.create({
-    electronObjectId,
-    electronRpc,
-    sessionId,
-    targetId,
-    rpc: sessionRpc,
-    idleTimeout,
-    utilityContext,
-  })
-
-  // TODO wait for utility execution context
   const electronApp = ElectronApp.create({
     electronRpc,
     electronObjectId,
     idleTimeout,
   })
-  const pageObjectContext = {
-    page: firstWindow,
-    expect: Expect.expect,
-    VError,
-    electronApp,
-    ideVersion: parsedIdeVersion,
-  }
-  const pageObjectModule = await ImportScript.importScript(pageObjectPath)
-  const pageObject = await pageObjectModule.create(pageObjectContext)
-  await pageObject.WaitForApplicationToBeReady.waitForApplicationToBeReady()
-  if (timeouts === false) {
-    // TODO this should be part of initialization worker
-    await DisableTimeouts.disableTimeouts(firstWindow)
-  }
-  PageObjectState.set(connectionId, pageObject)
+  ElectronAppState.set(connectionId, electronApp)
 }
