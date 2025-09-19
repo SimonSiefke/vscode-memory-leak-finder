d863d885cbc31c5664ecd66a3716e3be4e9af155
Simon Siefke <simon.siefke@gmail.com>
2025-09-19 14:45:52 +0200

    tsc

--- packages/test-worker/src/parts/PageEvaluate/PageEvaluate.ts
+++ packages/test-worker/src/parts/PageEvaluate/PageEvaluate.ts
@@ -1,19 +1,19 @@
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 // import * as ExecutionContextState from '../ExecutionContextState/ExecutionContextState.ts'
 import { ExpectError } from '../ExpectError/ExpectError.ts'
 import * as PTimeout from '../PTimeout/PTimeout.ts'
+import * as PageObjectState from '../PageObjectState/PageObjectState.ts'
 
 export const evaluate = async (rpc, { expression, awaitPromise = false, replMode = false }) => {
   try {
-    const utilityExecutionContext = await ExecutionContextState.waitForUtilityExecutionContext(rpc.sessionId)
+    const connectionId = 1
+    const pageObject = PageObjectState.getPageObject(connectionId)
     const result = await PTimeout.pTimeout(
-      DevtoolsProtocolRuntime.evaluate(rpc, {
+      pageObject.evaluateInUtilityContext.evaluate({
         expression,
         returnByValue: true,
         generatePreview: true,
         awaitPromise,
         replMode,
-        uniqueContextId: utilityExecutionContext.uniqueId,
       }),
       {
         milliseconds: 3000,
