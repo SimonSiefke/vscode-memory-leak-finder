ed9630c502beffd9d54430dbc860ca86f88a8c0b
Simon Siefke <simon.siefke@gmail.com>
2025-09-22 22:37:26 +0200

    test

--- packages/original-name-worker/test/GetOriginalClassName.test.ts
+++ packages/original-name-worker/test/GetOriginalClassName.test.ts
@@ -380,10 +380,3 @@ test('getOriginalClassName - context key constructor', () => {
   const originalColumn = 18
   expect(GetOriginalClassName.getOriginalClassName(sourceContent, originalLine, originalColumn, originalFileName)).toBe('ContextKeyAndExpr')
 })
-
-test('getOriginalClassName - context key constructor', () => {
-  const sourceContent = readFileSync('/home/simon/.cache/repos/vscode/src/vs/platform/contextkey/common/contextkey.ts', 'utf8')
-  const originalLine = 1584
-  const originalColumn = 18
-  expect(GetOriginalClassName.getOriginalClassName(sourceContent, originalLine, originalColumn, originalFileName)).toBe('ContextKeyAndExpr')
-})
