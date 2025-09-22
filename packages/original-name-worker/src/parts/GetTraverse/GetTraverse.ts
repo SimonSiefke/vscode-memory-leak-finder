098ba67e914f57002c5f3cab791296288795f520
Simon Siefke <simon.siefke@gmail.com>
2025-09-22 18:11:07 +0200

    lint

--- packages/original-name-worker/src/parts/GetTraverse/GetTraverse.ts
+++ packages/original-name-worker/src/parts/GetTraverse/GetTraverse.ts
@@ -4,7 +4,9 @@ import type * as t from '@babel/types'
 
 // Handle different module formats for @babel/traverse
 const getTraverseFunction = (): ((ast: t.File, visitors: Visitor) => void) => {
-  return typeof traverse === 'function' ? traverse : (traverse as any).default || (traverse as any).traverse
+  return typeof traverse === 'function'
+    ? traverse
+    : (traverse as any).default || (traverse as any).traverse
 }
 
 export const traverseAst = (ast: t.File, visitors: Visitor): void => {
