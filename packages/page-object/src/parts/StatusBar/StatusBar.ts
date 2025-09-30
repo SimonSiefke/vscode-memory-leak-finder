ebcd4c4ae5e56e8db9defc0ea097da2935bfcac8
Simon Siefke <simon.siefke@gmail.com>
2025-09-30 22:27:34 +0200

    feature: improve status e2e test

--- packages/page-object/src/parts/StatusBar/StatusBar.ts
+++ packages/page-object/src/parts/StatusBar/StatusBar.ts
@@ -1,6 +1,3 @@
-// TODO avoid using timeout
-const SHORT_TIMEOUT = 250
-
 export const create = ({ page, VError, expect }) => {
   return {
     async click(label) {
@@ -11,59 +8,72 @@ export const create = ({ page, VError, expect }) => {
         throw new VError(error, `Failed to click status bar item ${label}`)
       }
     },
-    async item(id) {
+    getSelector(id) {
+      const selector = `#${id.replaceAll('.', '\\.')}`
+      return selector
+    },
+    async hideItem(id) {
       try {
-        const selector = `#${id.replaceAll('.', '\\.')}`
+        await page.waitForIdle()
+        const selector = this.getSelector(id)
         const item = page.locator(selector).first()
         await expect(item).toBeVisible()
-        return {
-          async shouldHaveText(text) {
-            await expect(item).toHaveText(text)
-          },
-          async click() {
-            await item.click()
-          },
-          async hide() {
-            await item.click({
-              button: 'right',
-            })
-            const contextMenu = page.locator('.context-view.monaco-menu-container .actions-container')
-            await expect(contextMenu).toBeVisible()
-            await expect(contextMenu).toBeFocused()
-            const contextMenuItem = contextMenu
-              .locator('.action-item', {
-                hasText: "Hide 'Problems'",
-              })
-              .first()
-            await expect(contextMenuItem).toBeVisible()
-            await contextMenuItem.focus()
-            await page.waitForTimeout(SHORT_TIMEOUT)
-            await contextMenuItem.click({ force: true })
-            await expect(contextMenu).toBeHidden()
-            await expect(item).toBeHidden()
-          },
-          async show() {
-            await expect(item).toBeHidden()
-            const statusBar = page.locator('.part.statusbar')
-            await statusBar.click({
-              button: 'right',
-            })
-            const contextMenu = page.locator('.context-view.monaco-menu-container .actions-container')
-            await expect(contextMenu).toBeVisible()
-            await expect(contextMenu).toBeFocused()
-            const contextMenuItem = contextMenu
-              .locator('.action-item', {
-                hasText: 'Problems',
-              })
-              .first()
-            await page.waitForTimeout(SHORT_TIMEOUT)
-            await contextMenuItem.click({ force: true })
-            await expect(contextMenu).toBeHidden()
-            await expect(item).toBeVisible()
-          },
-        }
+        await item.click({
+          button: 'right',
+        })
+        const contextMenu = page.locator('.context-view.monaco-menu-container .actions-container')
+        await expect(contextMenu).toBeVisible()
+        await expect(contextMenu).toBeFocused()
+        const contextMenuItem = contextMenu
+          .locator('.action-item', {
+            hasText: "Hide 'Problems'",
+          })
+          .first()
+        await expect(contextMenuItem).toBeVisible()
+        await page.waitForIdle()
+        await contextMenuItem.click({ force: true })
+        await page.waitForIdle()
+        await expect(contextMenu).toBeHidden()
+        await expect(item).toBeHidden()
       } catch (error) {
-        throw new VError(error, `Failed to click status bar item ${id}`)
+        throw new VError(error, `Failed to hide status bar item "${id}"`)
+      }
+    },
+    async showItem(id) {
+      try {
+        await page.waitForIdle()
+        const selector = this.getSelector(id)
+        const item = page.locator(selector).first()
+        await expect(item).toBeHidden()
+        const statusBar = page.locator('.part.statusbar')
+        await statusBar.click({
+          button: 'right',
+        })
+        const contextMenu = page.locator('.context-view.monaco-menu-container .actions-container')
+        await expect(contextMenu).toBeVisible()
+        await page.waitForIdle()
+        await expect(contextMenu).toBeFocused()
+        const contextMenuItem = contextMenu
+          .locator('.action-item', {
+            hasText: 'Problems',
+          })
+          .first()
+        await page.waitForIdle()
+        await contextMenuItem.click({ force: true })
+        await expect(contextMenu).toBeHidden()
+        await expect(item).toBeVisible()
+      } catch (error) {
+        throw new VError(error, `Failed to show status bar item`)
+      }
+    },
+    async selectItem(id) {
+      try {
+        const selector = this.getSelector(id)
+        const item = page.locator(selector).first()
+        await expect(item).toBeVisible()
+        await item.click()
+      } catch (error) {
+        throw new VError(error, `Failed to select status bar item`)
       }
     },
   }
