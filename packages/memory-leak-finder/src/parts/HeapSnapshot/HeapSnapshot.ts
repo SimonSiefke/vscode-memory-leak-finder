82044a74750846a8cf4fed292fb794caa410fb00
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 19:01:59 +0200

    Discard changes to packages/memory-leak-finder/src/parts/HeapSnapshot/HeapSnapshot.ts

--- packages/memory-leak-finder/src/parts/HeapSnapshot/HeapSnapshot.ts
+++ packages/memory-leak-finder/src/parts/HeapSnapshot/HeapSnapshot.ts
@@ -9,13 +9,9 @@ export const takeHeapSnapshot = async (session, outFile, options = {}) => {
   Assert.object(session)
   Assert.string(outFile)
   await mkdir(dirname(outFile), { recursive: true })
-  console.log('created outdir')
   const stream = HeapSnapshotStream.create(session, options)
-  console.log('created stream', outFile)
   const writeStream = createWriteStream(outFile)
   const pipelinePromise = pipeline(stream, writeStream)
   await stream.start()
-  console.log('stream start')
   await pipelinePromise
-  console.log('heapsnapshot finish')
 }
