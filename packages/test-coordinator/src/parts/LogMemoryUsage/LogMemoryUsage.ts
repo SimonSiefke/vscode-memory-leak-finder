add8babd9a1a1975c97b245f6c76788fa870b0ac
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 23:39:18 +0200

    Delete packages/test-coordinator/src/parts/LogMemoryUsage/LogMemoryUsage.ts

--- packages/test-coordinator/src/parts/LogMemoryUsage/LogMemoryUsage.ts
+++
@@ -1,9 +0,0 @@
-export const logMemoryUsage = (context: string): void => {
-  const memUsage = process.memoryUsage()
-  const heapUsedMB = Math.round(memUsage.heapUsed / 1024 / 1024)
-  const heapTotalMB = Math.round(memUsage.heapTotal / 1024 / 1024)
-  const rssMB = Math.round(memUsage.rss / 1024 / 1024)
-  const externalMB = Math.round(memUsage.external / 1024 / 1024)
-  
-  console.log(`[TEST-COORDINATOR] ${context}: heap=${heapUsedMB}MB/${heapTotalMB}MB, rss=${rssMB}MB, external=${externalMB}MB`)
-}
