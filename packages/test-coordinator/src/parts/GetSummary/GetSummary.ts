9a32ba601f338a997ace15e0702b9b985efcbeeb
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 23:42:10 +0200

    remove unused

--- packages/test-coordinator/src/parts/GetSummary/GetSummary.ts
+++
@@ -1,9 +0,0 @@
-export const getSummary = (result) => {
-  if (result && result.eventListeners) {
-    return { eventListeners: result.eventListeners }
-  }
-  if (result && result.summary) {
-    return result.summary
-  }
-  return { result }
-}
