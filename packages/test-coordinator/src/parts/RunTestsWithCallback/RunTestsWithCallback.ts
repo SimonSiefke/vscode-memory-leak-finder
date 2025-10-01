beddcd4da6868f7124e4d0f22f364ce895314f87
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 08:43:04 +0200

    forma

--- packages/test-coordinator/src/parts/RunTestsWithCallback/RunTestsWithCallback.ts
+++ packages/test-coordinator/src/parts/RunTestsWithCallback/RunTestsWithCallback.ts
@@ -231,7 +231,6 @@ export const runTestsWithCallback = async ({
           }
         }
       } catch (error) {
-        
         if (wasOriginallySkipped) {
           skippedFailed++
         } else {
