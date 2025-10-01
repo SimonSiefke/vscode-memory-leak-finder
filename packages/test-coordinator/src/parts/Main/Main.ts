d797a6f0a952bccc2193742ec4521c5bc6a89058
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 23:39:58 +0200

    Discard changes to packages/test-coordinator/src/parts/Main/Main.ts

--- packages/test-coordinator/src/parts/Main/Main.ts
+++ packages/test-coordinator/src/parts/Main/Main.ts
@@ -1,23 +1,10 @@
 import * as Listen from '../Listen/Listen.ts'
-import * as LogMemoryUsage from '../LogMemoryUsage/LogMemoryUsage.ts'
 
 const handleUncaughtExceptionMonitor = (error) => {
   console.error(`[test-coordinator]: Unhandled Error: ${error}`)
 }
 
 export const main = async () => {
-  LogMemoryUsage.logMemoryUsage('test-coordinator started')
   process.on('uncaughtExceptionMonitor', handleUncaughtExceptionMonitor)
-  
-  // Set up periodic memory monitoring every 30 seconds
-  const memoryMonitorInterval = setInterval(() => {
-    LogMemoryUsage.logMemoryUsage('periodic check')
-  }, 30000)
-  
-  // Clean up interval on process exit
-  process.on('exit', () => {
-    clearInterval(memoryMonitorInterval)
-  })
-  
   await Listen.listen()
 }
