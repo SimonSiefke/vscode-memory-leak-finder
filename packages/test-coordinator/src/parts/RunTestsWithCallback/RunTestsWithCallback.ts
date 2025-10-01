3a14109d3323bb546e280ccc536f9d6b95030575
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 08:29:42 +0200

    feature: better handle errors in test setup

--- packages/test-coordinator/src/parts/RunTestsWithCallback/RunTestsWithCallback.ts
+++ packages/test-coordinator/src/parts/RunTestsWithCallback/RunTestsWithCallback.ts
@@ -169,6 +169,11 @@ export const runTestsWithCallback = async ({
         const testSkipped = testResult.skipped
         wasOriginallySkipped = testResult.wasOriginallySkipped
 
+        // Check if setup failed and we have error information
+        if (testResult.error) {
+          throw testResult.error
+        }
+
         if (recordVideo) {
           await VideoRecording.addChapter(currentVideoRpc, dirent, start)
         }
@@ -220,6 +225,7 @@ export const runTestsWithCallback = async ({
           }
         }
       } catch (error) {
+        
         if (wasOriginallySkipped) {
           skippedFailed++
         } else {
