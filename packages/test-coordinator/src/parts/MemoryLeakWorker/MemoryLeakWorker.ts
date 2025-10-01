b2a1c5f50c0f354a0548ce48db2cce4b35bd95a4
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 23:39:44 +0200

    Discard changes to packages/test-coordinator/src/parts/MemoryLeakWorker/MemoryLeakWorker.ts

--- packages/test-coordinator/src/parts/MemoryLeakWorker/MemoryLeakWorker.ts
+++ packages/test-coordinator/src/parts/MemoryLeakWorker/MemoryLeakWorker.ts
@@ -2,7 +2,6 @@ import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
 import * as Assert from '../Assert/Assert.ts'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 import * as MemoryLeakWorkerUrl from '../MemoryLeakWorkerUrl/MemoryLeakWorkerUrl.ts'
-import * as LogMemoryUsage from '../LogMemoryUsage/LogMemoryUsage.ts'
 
 export const startWorker = async (
   devtoolsWebsocketUrl: string,
@@ -16,22 +15,11 @@ export const startWorker = async (
   inspectPtyHost: boolean,
 ) => {
   Assert.string(devtoolsWebsocketUrl)
-  LogMemoryUsage.logMemoryUsage(`launching memory-leak-finder worker for connection ${connectionId}`)
   const rpc = await NodeWorkerRpcParent.create({
     path: MemoryLeakWorkerUrl.memoryLeakWorkerUrl,
     stdio: 'inherit',
     commandMap: CommandMapRef.commandMapRef,
   })
-  LogMemoryUsage.logMemoryUsage(`memory-leak-finder worker created for connection ${connectionId}`)
-  
-  // Add disposal logging
-  const originalDispose = rpc.dispose.bind(rpc)
-  rpc.dispose = async () => {
-    LogMemoryUsage.logMemoryUsage(`disposing memory-leak-finder worker for connection ${connectionId}`)
-    await originalDispose()
-    LogMemoryUsage.logMemoryUsage(`memory-leak-finder worker disposed for connection ${connectionId}`)
-  }
-  
   await rpc.invoke(
     'ConnectDevtools.connectDevtools',
     devtoolsWebsocketUrl,
@@ -44,6 +32,5 @@ export const startWorker = async (
     inspectExtensions,
     inspectPtyHost,
   )
-  LogMemoryUsage.logMemoryUsage(`memory-leak-finder worker connected for connection ${connectionId}`)
   return rpc
 }
