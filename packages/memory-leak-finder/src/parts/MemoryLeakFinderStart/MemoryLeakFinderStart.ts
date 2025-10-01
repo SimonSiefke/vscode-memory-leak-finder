ba392d8615f935f2f2302472b954426179c55d6e
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 23:39:00 +0200

    Discard changes to packages/memory-leak-finder/src/parts/MemoryLeakFinderStart/MemoryLeakFinderStart.ts

--- packages/memory-leak-finder/src/parts/MemoryLeakFinderStart/MemoryLeakFinderStart.ts
+++ packages/memory-leak-finder/src/parts/MemoryLeakFinderStart/MemoryLeakFinderStart.ts
@@ -1,20 +1,16 @@
 import * as MemoryLeakFinderState from '../MemoryLeakFinderState/MemoryLeakFinderState.ts'
 import * as WaitForCrash from '../WaitForCrash/WaitForCrash.ts'
-import * as LogMemoryUsage from '../LogMemoryUsage/LogMemoryUsage.ts'
 
 const doStart = async (connectionId: number): Promise<any> => {
   const measure = MemoryLeakFinderState.get(connectionId)
   if (!measure) {
     throw new Error(`no measure found`)
   }
-  LogMemoryUsage.logMemoryUsage(`before starting measurement for connection ${connectionId}`)
   const result = await measure.start()
-  LogMemoryUsage.logMemoryUsage(`after starting measurement for connection ${connectionId}`)
   return result
 }
 
 export const start = async (connectionId: number, electronTargetId: string): Promise<void> => {
-  LogMemoryUsage.logMemoryUsage(`starting memory leak finder for connection ${connectionId}`)
   const crashInfo = WaitForCrash.waitForCrash(electronTargetId)
   const resultPromise = doStart(connectionId)
   const intermediateResult = await Promise.race([crashInfo.promise, resultPromise])
@@ -23,5 +19,4 @@ export const start = async (connectionId: number, electronTargetId: string): Pro
   }
   crashInfo.dispose()
   MemoryLeakFinderState.update(connectionId, { before: intermediateResult })
-  LogMemoryUsage.logMemoryUsage(`completed memory leak finder start for connection ${connectionId}`)
 }
