694c41049745481125f34c35f98a26eb21d29036
Simon Siefke <simon.siefke@gmail.com>
2025-10-01 18:51:46 +0200

    debug

--- packages/memory-leak-finder/src/parts/GetMeasureRpc/GetMeasureRpc.ts
+++ packages/memory-leak-finder/src/parts/GetMeasureRpc/GetMeasureRpc.ts
@@ -19,33 +19,36 @@ export const getMeasureRpc = async (
 
   const browserRpc = await DebuggerCreateIpcConnection.createConnection(devtoolsWebSocketUrl)
   const { sessionRpc } = await waitForSession(browserRpc, attachedToPageTimeout)
+
+  // Connect to the appropriate debug port based on the flags
+  if (inspectSharedProcess) {
+    await sessionRpc.dispose()
+    const sharedProcessRpc = await connectToDevtoolsWithJsonUrl(SHARED_PROCESS_PORT)
+    return sharedProcessRpc
+  }
+  if (inspectExtensions) {
+    await sessionRpc.dispose()
+    const extensionsRpc = await connectToDevtoolsWithJsonUrl(EXTENSIONS_PORT)
+    return extensionsRpc
+  }
+  if (inspectPtyHost) {
+    await sessionRpc.dispose()
+    const ptyhostRpc = await connectToDevtoolsWithJsonUrl(PTYHOST_PORT)
+    return ptyhostRpc
+  }
+  if (measureNode) {
+    await sessionRpc.dispose()
+    const electronRpc = await DebuggerCreateIpcConnection.createConnection(electronWebSocketUrl)
+    return electronRpc
+  }
+
   await Promise.all([
     DevtoolsProtocolTarget.setAutoAttach(sessionRpc, {
       autoAttach: true,
       waitForDebuggerOnStart: false,
       flatten: true,
     }),
-    DevtoolsProtocolRuntime.enable(sessionRpc),
-    DevtoolsProtocolRuntime.runIfWaitingForDebugger(sessionRpc),
   ])
-
-  // Connect to the appropriate debug port based on the flags
-  if (inspectSharedProcess) {
-    const sharedProcessRpc = await connectToDevtoolsWithJsonUrl(SHARED_PROCESS_PORT)
-    return sharedProcessRpc
-  }
-  if (inspectExtensions) {
-    const extensionsRpc = await connectToDevtoolsWithJsonUrl(EXTENSIONS_PORT)
-    return extensionsRpc
-  }
-  if (inspectPtyHost) {
-    const ptyhostRpc = await connectToDevtoolsWithJsonUrl(PTYHOST_PORT)
-    return ptyhostRpc
-  }
-  if (measureNode) {
-    const electronRpc = await DebuggerCreateIpcConnection.createConnection(electronWebSocketUrl)
-    return electronRpc
-  }
 
   return sessionRpc
 }
