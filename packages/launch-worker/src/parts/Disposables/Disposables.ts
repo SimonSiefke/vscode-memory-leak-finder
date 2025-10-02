1a6ac34bec5cdc7dd60d81a7950f830003849f71
Simon Siefke <simon.siefke@gmail.com>
2025-10-02 10:18:51 +0200

    tsc

--- packages/launch-worker/src/parts/Disposables/Disposables.ts
+++ packages/launch-worker/src/parts/Disposables/Disposables.ts
@@ -4,7 +4,7 @@ export const add = (fn) => {
   disposables.push(fn)
 }
 
-const disposeAll = async () => {
+export const disposeAll = async () => {
   for (const fn of disposables) {
     await fn()
   }
