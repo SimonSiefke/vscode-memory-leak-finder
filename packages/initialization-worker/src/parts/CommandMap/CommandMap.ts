57f0c9c0cab3269d84a5adfdc43ce0e02fe57672
Simon Siefke <simon.siefke@gmail.com>
2025-10-02 13:27:32 +0200

    tsc

--- packages/initialization-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/initialization-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,5 @@
 import * as PrepareBoth from '../PrepareBoth/PrepareBoth.ts'
-import * as Exit from '../Exit/Exit.ts'
 
 export const commandMap = {
   'Initialize.prepare': PrepareBoth.prepareBoth,
-  'Initialize.exit': Exit.exit,
 }
