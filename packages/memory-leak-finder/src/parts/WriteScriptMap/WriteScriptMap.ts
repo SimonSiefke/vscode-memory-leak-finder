fa0f79f19fc61f464a577f7d7c69638706cdace9
Simon Siefke <simon.siefke@gmail.com>
2025-10-02 14:49:15 +0200

    remove unused

--- packages/memory-leak-finder/src/parts/WriteScriptMap/WriteScriptMap.ts
+++ packages/memory-leak-finder/src/parts/WriteScriptMap/WriteScriptMap.ts
@@ -6,8 +6,10 @@ export const writeScriptMap = async (
   scriptMap: Record<number, { readonly url?: string; readonly sourceMapUrl?: string }>,
   id: number,
 ): Promise<void> => {
+  console.log('writing scriptmaps', id)
   const outFile = join(Root.root, '.vscode-script-maps', `${id}.json`)
   const dir = dirname(outFile)
   await mkdir(dir, { recursive: true })
   await writeFile(outFile, JSON.stringify(scriptMap, null, 2) + '\n')
+  console.log('write scriptmaps')
 }
