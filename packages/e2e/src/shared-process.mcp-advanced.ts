4d9ba6de8da9b206e0bf1eee95ea9063c77f793b
Simon Siefke <simon.siefke@gmail.com>
2025-10-13 19:50:44 +0200

    simplify

--- packages/e2e/src/shared-process.mcp-advanced.ts
+++ packages/e2e/src/shared-process.mcp-advanced.ts
@@ -5,11 +5,10 @@ export const setup = async ({ Editor }: TestContext): Promise<void> => {
 }
 
 export const run = async ({ MCP }: TestContext): Promise<void> => {
-  const serverName = 'my-advanced-mcp-server'
   const server = await MCP.createMCPServer()
   await MCP.addServer({
     serverUrl: server.url,
-    serverName,
+    serverName: 'my-advanced-mcp-server',
   })
 
   await MCP.removeServer(serverName)
