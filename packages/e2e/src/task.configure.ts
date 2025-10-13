280a92d6115407bd68b7a38c3c2719995298aace
Simon Siefke <simon.siefke@gmail.com>
2025-10-13 17:03:20 +0300

    feature: improve task test (#2028)

--- packages/e2e/src/task.configure.ts
+++ packages/e2e/src/task.configure.ts
@@ -2,8 +2,9 @@ import type { TestContext } from '../types.ts'
 
 export const skip = true
 
-export const setup = async ({ Editor }: TestContext): Promise<void> => {
+export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> => {
   await Editor.closeAll()
+  await Workspace.setFiles([])
 }
 
 export const run = async ({ Task }: TestContext): Promise<void> => {
