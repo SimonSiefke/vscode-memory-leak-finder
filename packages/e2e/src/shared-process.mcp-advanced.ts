2bae81e206e7cce4f2ff417fca9a4757128c249b
Simon Siefke <simon.siefke@gmail.com>
2025-10-13 20:13:22 +0200

    clean code

--- packages/e2e/src/shared-process.mcp-advanced.ts
+++ packages/e2e/src/shared-process.mcp-advanced.ts
@@ -3,14 +3,13 @@ import type { TestContext } from '../types.ts'
 export const skip = 1
 
 export const setup = async ({ Editor, MCP }: TestContext): Promise<void> => {
-  await Editor.closeAll()
   await MCP.removeAllServers()
+  await Editor.closeAll()
 }
 
 export const run = async ({ MCP }: TestContext): Promise<void> => {
   await MCP.addServer({
     serverName: 'my-advanced-mcp-server',
   })
-
   await MCP.removeAllServers()
 }
