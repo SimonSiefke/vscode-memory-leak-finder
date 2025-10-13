1021205b7a6d69050cd7b1700b013bc9b8d411d7
Simon Siefke <simon.siefke@gmail.com>
2025-10-13 18:49:20 +0200

    skip

--- packages/e2e/src/shared-process.mcp-advanced.ts
+++ packages/e2e/src/shared-process.mcp-advanced.ts
@@ -1,5 +1,7 @@
 import type { TestContext } from '../types.ts'
 
+export const skip = 1
+
 export const setup = async ({ Editor, Server }: TestContext): Promise<void> => {
   await Editor.closeAll()
 
