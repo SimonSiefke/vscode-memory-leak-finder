59df93b04b36ed1f49a5823551f86561d5e27b7a
Simon Siefke <simon.siefke@gmail.com>
2025-10-14 12:51:00 +0000

    update node

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,30 +1,3 @@
 ## update and install some things we should probably have
-apt-get update
-apt-get install -y \
-  curl \
-  git \
-  gnupg2 \
-  jq \
-  sudo \
-  zsh \
-  python3 \
-  build-essential \
-  g++ \
-  libx11-dev \
-  libxkbfile-dev \
-  libsecret-1-dev \
-  python-is-python3 \
-  python3-setuptools
 
-# install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-# install node
-/root/.nvm/nvm.sh install 22.19.0
-
-# setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+echo ok
