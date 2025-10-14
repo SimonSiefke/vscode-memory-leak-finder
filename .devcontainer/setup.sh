87dd8776f78d04dd554e599acd43c8688db5a40c
Simon Siefke <simon.siefke@gmail.com>
2025-10-14 12:36:55 +0000

    remove yarn

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,12 +16,6 @@ apt-get install -y \
   python-is-python3 \
   python3-setuptools
 
-
-# set-up and install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install yarn -y
-
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
