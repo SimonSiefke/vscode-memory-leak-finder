c5a5b044150be69be9b3d91866149fe2e63b85da
Simon Siefke <simon.siefke@gmail.com>
2025-10-14 12:29:54 +0000

    feature: add python3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,14 +6,15 @@ apt-get install -y \
   gnupg2 \
   jq \
   sudo \
-  zsh
+  zsh \
+  python3
 
 # set-up and install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get install yarn -y
 
-# instsal nvm
+# install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # setup and install oh-my-zsh
