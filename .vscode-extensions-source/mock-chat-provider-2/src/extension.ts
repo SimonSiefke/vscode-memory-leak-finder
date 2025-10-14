b5b9a07a003eaa90a03689e3eb37fe66bf03b6fe
Simon Siefke <simon.siefke@gmail.com>
2025-10-14 14:23:19 +0200

    feature: update mock chat extension

---
+++ .vscode-extensions-source/mock-chat-provider-2/src/extension.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import * as vscode from 'vscode'
+import { SampleChatModelProvider } from './provider'
+
+export function activate(_: vscode.ExtensionContext) {
+  vscode.lm.registerLanguageModelChatProvider('sample', new SampleChatModelProvider())
+}
+
+export function deactivate() {}
--- packages/e2e/src/base.ts
+++ packages/e2e/src/base.ts
@@ -2,4 +2,5 @@ import type { TestContext } from '../types.ts'
 
 export const run = async ({ Workbench }: TestContext): Promise<void> => {
   await Workbench.shouldBeVisible()
+  await new Promise((r) => {})
 }
