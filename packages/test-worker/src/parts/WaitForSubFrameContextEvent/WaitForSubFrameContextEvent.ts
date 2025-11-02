1e2b4739d53eeb183c52e8629c9c6499a69436c2
Simon Siefke <simon.siefke@gmail.com>
2025-11-02 18:54:30 +0100

    log

--- packages/test-worker/src/parts/WaitForSubFrameContextEvent/WaitForSubFrameContextEvent.ts
+++ packages/test-worker/src/parts/WaitForSubFrameContextEvent/WaitForSubFrameContextEvent.ts
@@ -6,15 +6,8 @@ export const waitForSubFrameContextEvent = (rpc, urlRegex, timeout, signal: Abor
   let matchingFrameId = ''
   const cleanupMaybe = () => {
     if (matchingFrameId && matchingFrameId in loaded) {
-      console.log('DO CLEANUp')
       cleanup({
         frameId: matchingFrameId,
-      })
-    } else {
-      console.log('NO CLEANUP')
-      console.log({
-        loaded,
-        matchingFrameId,
       })
     }
   }
@@ -26,27 +19,20 @@ export const waitForSubFrameContextEvent = (rpc, urlRegex, timeout, signal: Abor
     cleanupMaybe()
   }
   const handleFrameStoppedLoading = (event) => {
-    console.log(event)
     loaded[event.params.frameId] = true
     cleanupMaybe()
   }
   const handleDocumentOpened = (event) => {
-    console.log(event)
     if (urlRegex.test(event.params.frame.url)) {
-      console.log('MATCH')
       matchingFrameId = event.params.frame.id
       loaded[event.params.frame.id] = true
-    } else {
-      console.log('NO MATCH')
     }
     cleanupMaybe()
   }
   const handleTimeout = () => {
-    console.log('timeout')
     cleanup(null)
   }
   const handleAbort = () => {
-    console.log('abort')
     cleanup(null)
   }
   const cleanup = (result) => {
