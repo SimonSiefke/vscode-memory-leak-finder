da4cb1d7a3c4dd5ae1fc8ef40d329a5b30f81619
Simon Siefke <simon.siefke@gmail.com>
2025-11-02 14:30:20 +0100

    better code

--- packages/e2e/src/markdown-preview.open.ts
+++ packages/e2e/src/markdown-preview.open.ts
@@ -17,7 +17,7 @@ export const setup = async ({ Workspace, Explorer, Editor }: TestContext): Promi
 // @ts-ignore
 export const run = async ({ QuickPick, WellKnownCommands, MarkdownPreview }: TestContext): Promise<void> => {
   await QuickPick.executeCommand(WellKnownCommands.MarkdownOpenPreviewToTheSide)
-  await MarkdownPreview.shouldBeVisible()
-  await MarkdownPreview.shouldHaveHeading('hello-world')
+  const subFrame = await MarkdownPreview.shouldBeVisible()
+  await MarkdownPreview.shouldHaveHeading(subFrame, 'hello-world')
   // await Editor.close('Preview index.md')
 }
--- packages/page-object/src/parts/MarkdownPreview/MarkdownPreview.ts
+++ packages/page-object/src/parts/MarkdownPreview/MarkdownPreview.ts
@@ -1,6 +1,14 @@
 export const create = ({ expect, page, VError, electronApp }) => {
   return {
-    async shouldHaveHeading(id) {
+    async shouldHaveHeading(subFrame, id) {
+      try {
+        const heading = subFrame.locator(`#${id}`)
+        await expect(heading).toBeVisible()
+      } catch (error) {
+        throw new VError(error, `Failed to check that markdown preview has heading ${id}`)
+      }
+    },
+    async shouldBeVisible() {
       try {
         await page.waitForIdle()
         const webView = page.locator('.webview')
@@ -14,22 +22,7 @@ export const create = ({ expect, page, VError, electronApp }) => {
         const subFrame = await childPage.waitForSubIframe({
           url: /extensionId=vscode.markdown-language-features/,
         })
-        const heading = subFrame.locator(`#${id}`)
-        await expect(heading).toBeVisible()
-      } catch (error) {
-        throw new VError(error, `Failed to check that markdown preview has heading ${id}`)
-      }
-    },
-    async shouldBeVisible() {
-      try {
-        await page.waitForIdle()
-        const webView = page.locator('.webview')
-        await expect(webView).toBeVisible()
-        await expect(webView).toHaveClass('ready')
-        await electronApp.waitForIframe({
-          url: /extensionId=vscode.markdown-language-features/,
-          injectUtilityScript: false,
-        })
+        return subFrame
       } catch (error) {
         throw new VError(error, `Failed to check that markdown preview is visible`)
       }
