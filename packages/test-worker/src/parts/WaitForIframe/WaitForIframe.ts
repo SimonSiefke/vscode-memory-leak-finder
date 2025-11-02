4630f30f0093de3b83905ce3c29c051277e25911
Simon Siefke <simon.siefke@gmail.com>
2025-11-02 14:28:23 +0100

    debug

--- packages/test-worker/src/parts/WaitForIframe/WaitForIframe.ts
+++ packages/test-worker/src/parts/WaitForIframe/WaitForIframe.ts
@@ -44,7 +44,6 @@ export const waitForIframe = async ({
 
   let iframeUtilityContext = undefined
 
-  console.trace({ injectUtilityScript })
   const utilityExecutionContextName = 'utility'
 
   if (injectUtilityScript) {
