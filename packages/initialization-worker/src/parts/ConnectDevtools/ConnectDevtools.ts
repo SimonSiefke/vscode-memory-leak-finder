6e607da03b117f4b7b892047186d92b221dad7ff
Simon Siefke <simon.siefke@gmail.com>
2025-11-02 20:14:53 +0300

    feature: add utility context to top page, but not all pages (#2049)

--- packages/initialization-worker/src/parts/ConnectDevtools/ConnectDevtools.ts
+++ packages/initialization-worker/src/parts/ConnectDevtools/ConnectDevtools.ts
@@ -3,20 +3,26 @@ import * as DebuggerCreateRpcConnection from '../DebuggerCreateRpcConnection/Deb
 import { DevtoolsProtocolPage, DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 import * as UtilityScript from '../UtilityScript/UtilityScript.ts'
 import { waitForSession } from '../WaitForSession/WaitForSession.ts'
+import { waitForUtilityExecutionContext } from '../WaitForUtilityExecutionContext/WaitForUtilityExecutionContext.ts'
 
 export const connectDevtools = async (devtoolsWebSocketUrl: string, attachedToPageTimeout: number): Promise<any> => {
   const browserIpc = await DebuggerCreateIpcConnection.createConnection(devtoolsWebSocketUrl)
   const browserRpc = DebuggerCreateRpcConnection.createRpc(browserIpc)
   const { sessionRpc, sessionId, targetId } = await waitForSession(browserRpc, 30_000)
-  const script = await UtilityScript.getUtilityScript()
-  await Promise.all([
-    DevtoolsProtocolPage.enable(sessionRpc),
-    DevtoolsProtocolPage.addScriptToEvaluateOnNewDocument(sessionRpc, {
-      source: script,
-      worldName: 'utility',
-    }),
-    DevtoolsProtocolRuntime.runIfWaitingForDebugger(sessionRpc),
-  ])
+  await DevtoolsProtocolRuntime.runIfWaitingForDebugger(sessionRpc)
+  const { frameTree } = await DevtoolsProtocolPage.getFrameTree(sessionRpc)
+  const frameId = frameTree.frame.id
+  const executionContextPromise = waitForUtilityExecutionContext(sessionRpc)
+  await DevtoolsProtocolPage.createIsolatedWorld(sessionRpc, {
+    frameId: frameId,
+    worldName: 'utility',
+  })
+  const utilityContext = await executionContextPromise
+  const utilityScript = await UtilityScript.getUtilityScript()
+  await DevtoolsProtocolRuntime.evaluate(sessionRpc, {
+    uniqueContextId: utilityContext.uniqueId,
+    expression: utilityScript,
+  })
   return {
     sessionRpc,
     sessionId,
