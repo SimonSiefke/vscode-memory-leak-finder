20d4e5872830a50be62ccdba70373960224c5026
Simon Siefke <simon.siefke@gmail.com>
2025-11-02 16:27:51 +0100

    debug

--- packages/test-worker/src/parts/WaitForSubFrameContext/WaitForSubFrameContext.ts
+++ packages/test-worker/src/parts/WaitForSubFrameContext/WaitForSubFrameContext.ts
@@ -1,6 +1,30 @@
-import * as DevtoolsEventType from '../DevtoolsEventType/DevtoolsEventType.ts'
+import { DevtoolsProtocolPage } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 import { waitForSubFrameContextEvent } from '../WaitForSubFrameContextEvent/WaitForSubFrameContextEvent.ts'
 
-export const waitForSubFrame = (rpc, urlRegex, timeout) => {
-  return waitForSubFrameContextEvent(rpc, urlRegex, timeout)
+const getMatchingSubFrame = (frames, url) => {
+  for (const frame of frames) {
+    if (url.test(frame.url)) {
+      return frame
+    }
+  }
+  return undefined
+}
+
+export const waitForSubFrame = async (rpc, urlRegex, timeout) => {
+  const eventPromise = waitForSubFrameContextEvent(rpc, urlRegex, timeout)
+  await DevtoolsProtocolPage.enable(rpc)
+  const subFrame = await eventPromise
+  if (!subFrame) {
+    throw new Error(`no matching frame found`)
+  }
+  const { frameTree } = await DevtoolsProtocolPage.getFrameTree(rpc)
+  const childFrames = frameTree.childFrames.map((item) => item.frame)
+  await DevtoolsProtocolPage.disable(rpc)
+  // console.log({ subFrame })
+
+  const matchingFrame = getMatchingSubFrame(childFrames, urlRegex)
+  if (!matchingFrame) {
+    throw new Error(`no matching frame found`)
+  }
+  return matchingFrame
 }
--- packages/test-worker/src/parts/WaitForSubIframe/WaitForSubIframe.ts
+++ packages/test-worker/src/parts/WaitForSubIframe/WaitForSubIframe.ts
@@ -1,16 +1,7 @@
 import { DevtoolsProtocolPage, DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 import * as UtilityScript from '../UtilityScript/UtilityScript.ts'
-import { waitForSubFrameContext } from '../WaitForSubFrameContext/WaitForSubFrameContext.ts'
+import { waitForSubFrame } from '../WaitForSubFrameContext/WaitForSubFrameContext.ts'
 import { waitForUtilityExecutionContext } from '../WaitForUtilityExecutionContext/WaitForUtilityExecutionContext.ts'
-
-const getMatchingSubFrame = (frames, url) => {
-  for (const frame of frames) {
-    if (url.test(frame.url)) {
-      return frame
-    }
-  }
-  return undefined
-}
 
 export const waitForSubIframe = async ({ electronRpc, url, electronObjectId, idleTimeout, browserRpc, sessionRpc, createPage }) => {
   // TODO
@@ -19,19 +10,8 @@ export const waitForSubIframe = async ({ electronRpc, url, electronObjectId, idl
   // 3. enable page api
   // 4. resolve promise with execution context id and frame Id, clean up listeners
 
-  const subFramePromise = waitForSubFrameContext(sessionRpc, url, 3_000)
-  await DevtoolsProtocolPage.enable(sessionRpc)
-  const subFrame = await subFramePromise
+  const subFrame = await waitForSubFrame(sessionRpc, url, 3_000)
   if (!subFrame) {
-    throw new Error(`no matching frame found`)
-  }
-  const { frameTree } = await DevtoolsProtocolPage.getFrameTree(sessionRpc)
-  const childFrames = frameTree.childFrames.map((item) => item.frame)
-  await DevtoolsProtocolPage.disable(sessionRpc)
-  // console.log({ subFrame })
-
-  const matchingFrame = getMatchingSubFrame(childFrames, url)
-  if (!matchingFrame) {
     throw new Error(`no matching frame found`)
   }
 
@@ -39,7 +19,7 @@ export const waitForSubIframe = async ({ electronRpc, url, electronObjectId, idl
   const executionContextPromise = waitForUtilityExecutionContext(sessionRpc, utilityExecutionContextName)
 
   await DevtoolsProtocolPage.createIsolatedWorld(sessionRpc, {
-    frameId: matchingFrame.id,
+    frameId: subFrame.id,
     worldName: utilityExecutionContextName,
   })
 
