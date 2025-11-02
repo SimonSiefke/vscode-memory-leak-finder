1945485e41d334f393a5fc15b92483e4a22236d5
Simon Siefke <simon.siefke@gmail.com>
2025-11-02 20:41:24 +0100

    feature: improve sourcemap source cleaning

--- packages/load-source-map-worker/src/parts/CleanSource/CleanSource.ts
+++ packages/load-source-map-worker/src/parts/CleanSource/CleanSource.ts
@@ -1,16 +1,26 @@
 const RE_VS = /^vs\//
 
+const RE_EXTERNAL_COMMON_JS = /external commonjs "[a-zA-Z\-\d]+"/
+
+const commonJsExternalReplace = (match) => {
+  return match.replaceAll(' ', '/').replaceAll('"', '')
+}
+
 export const cleanSource = (source: string): string => {
   if (!source) {
     return ''
   }
-  const vscodeIndex = source.indexOf('vscode')
+  const vscodeIndex = source.indexOf('vscode/')
   if (vscodeIndex !== -1) {
-    return source.slice(vscodeIndex + 'vscode'.length + 1)
+    return source.slice(vscodeIndex + 'vscode/'.length)
   }
   return source
+    .replace('webpack://markdown-math', 'extensions/markdown-math')
+    .replace('webpack://markdown-language-features/./', 'extensions/markdown-language-features/')
+    .replace('webpack://markdown-language-features/', 'extensions/markdown-language-features/')
     .replace('out-vscode/vs/workbench/file:/mnt/vss/_work/1/s', '')
     .replace('file:/Users/cloudtest/vss/_work/1/s/', '')
     .replace(RE_VS, './src/vs/')
     .replace('./src', 'src')
+    .replace(RE_EXTERNAL_COMMON_JS, commonJsExternalReplace)
 }
--- packages/load-source-map-worker/test/CleanSource.test.ts
+++ packages/load-source-map-worker/test/CleanSource.test.ts
@@ -5,3 +5,15 @@ test('cleanSource', () => {
   const input = '/test/repos/vscode/src/vs/workbench/services/untitled/common/untitledTextEditorInput.ts'
   expect(cleanSource(input)).toBe('src/vs/workbench/services/untitled/common/untitledTextEditorInput.ts')
 })
+
+test('cleanSource - markdown language features', () => {
+  const input = 'webpack://markdown-language-features/./node_modules/@microsoft/applicationinsights-common/dist-es5/ThrottleMgr.js'
+  expect(cleanSource(input)).toBe(
+    'extensions/markdown-language-features/node_modules/@microsoft/applicationinsights-common/dist-es5/ThrottleMgr.js',
+  )
+})
+
+test.only('cleanSource - external commonjs vscode', () => {
+  const input = 'webpack://markdown-language-features/external commonjs "vscode"'
+  expect(cleanSource(input)).toBe('extensions/markdown-language-features/external/commonjs/vscode')
+})
