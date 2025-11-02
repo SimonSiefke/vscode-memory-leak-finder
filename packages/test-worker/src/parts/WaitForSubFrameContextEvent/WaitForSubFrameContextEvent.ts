e8d845ad6349e7d0973c0158600f0ad2f411a8a8
Simon Siefke <simon.siefke@gmail.com>
2025-11-02 15:52:31 +0100

    rename

---
+++ packages/test-worker/src/parts/WaitForSubFrameContextEvent/WaitForSubFrameContextEvent.ts
@@ -0,0 +1,60 @@
+import * as DevtoolsEventType from '../DevtoolsEventType/DevtoolsEventType.ts'
+
+export const waitForSubFrameContextEvent = (rpc, urlRegex, timeout) => {
+  const { resolve, promise } = Promise.withResolvers()
+  const loaded = Object.create(null)
+  let matchingFrameId = ''
+  const cleanupMaybe = () => {
+    if (matchingFrameId && matchingFrameId in loaded) {
+      console.log('DO CLEANUp')
+      cleanup({
+        frameId: matchingFrameId,
+      })
+    } else {
+      console.log('NO CLEANUP')
+      console.log({
+        loaded,
+        matchingFrameId,
+      })
+    }
+  }
+  const handleFrameNavigation = (event) => {
+    console.log(event)
+    if (urlRegex.test(event.params.url)) {
+      matchingFrameId = event.params.frameId
+    }
+    cleanupMaybe()
+  }
+  const handleFrameStoppedLoading = (event) => {
+    console.log(event)
+    loaded[event.params.frameId] = true
+    cleanupMaybe()
+  }
+  const handleDocumentOpened = (event) => {
+    console.log(event)
+    if (urlRegex.test(event.params.frame.url)) {
+      console.log('MATCH')
+      matchingFrameId = event.params.frame.id
+      loaded[event.params.frame.id] = true
+    } else {
+      console.log('NO MATCH')
+    }
+    cleanupMaybe()
+  }
+  const handleTimeout = () => {
+    console.log('timeout')
+    cleanup(null)
+  }
+  const cleanup = (result) => {
+    rpc.off(DevtoolsEventType.PageFrameRequestedNavigation, handleFrameNavigation)
+    rpc.off(DevtoolsEventType.PageFrameStoppedLoading, handleFrameStoppedLoading)
+    rpc.off(DevtoolsEventType.PageDocumentOpened, handleDocumentOpened)
+    clearTimeout(timeoutRef)
+    resolve(result)
+  }
+  rpc.on(DevtoolsEventType.PageFrameRequestedNavigation, handleFrameNavigation)
+  rpc.on(DevtoolsEventType.PageFrameStoppedLoading, handleFrameStoppedLoading)
+  rpc.on(DevtoolsEventType.PageDocumentOpened, handleDocumentOpened)
+  const timeoutRef = setTimeout(handleTimeout, timeout)
+  return promise
+}
