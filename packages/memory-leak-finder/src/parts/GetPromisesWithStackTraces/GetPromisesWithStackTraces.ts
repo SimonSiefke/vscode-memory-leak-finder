5549902e96461ede94a1488bf098183aebabb9d2
Simon Siefke <simon.siefke@gmail.com>
2025-11-10 14:34:15 +0100

    add fn

--- packages/memory-leak-finder/src/parts/GetPromisesWithStackTraces/GetPromisesWithStackTraces.ts
+++ packages/memory-leak-finder/src/parts/GetPromisesWithStackTraces/GetPromisesWithStackTraces.ts
@@ -2,7 +2,7 @@ import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts
 import * as GetDescriptorValues from '../GetDescriptorValues/GetDescriptorValues.ts'
 import * as PrototypeExpression from '../PrototypeExpression/PrototypeExpression.ts'
 
-export const getPromises = async (session, objectGroup) => {
+export const getPromisesWithStackTraces = async (session, objectGroup) => {
   const prototype = await DevtoolsProtocolRuntime.evaluate(session, {
     expression: PrototypeExpression.Promise,
     returnByValue: false,
@@ -12,6 +12,7 @@ export const getPromises = async (session, objectGroup) => {
     prototypeObjectId: prototype.objectId,
     objectGroup,
   })
+  console.log({ objects })
   const fnResult1 = await DevtoolsProtocolRuntime.getProperties(session, {
     objectId: objects.objects.objectId,
     ownProperties: true,
@@ -20,5 +21,6 @@ export const getPromises = async (session, objectGroup) => {
     nonIndexedPropertiesOnly: false,
   })
   const descriptors = GetDescriptorValues.getDescriptorValues(fnResult1.result)
+  console.log(descriptors)
   return descriptors
 }
