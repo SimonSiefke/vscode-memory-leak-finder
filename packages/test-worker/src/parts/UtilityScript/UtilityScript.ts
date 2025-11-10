3f5a6b6f5ba94bf833841b254cac418369fd9ec0
Simon Siefke <simon.siefke@gmail.com>
2025-11-10 17:35:11 +0300

    fix: build utility script before use (#2067)

--- packages/test-worker/src/parts/UtilityScript/UtilityScript.ts
+++ packages/test-worker/src/parts/UtilityScript/UtilityScript.ts
@@ -1,12 +1,21 @@
 import { existsSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
+import * as LaunchFileSystemWorker from '../LaunchFileSystemWorker/LaunchFileSystemWorker.ts'
 import * as Root from '../Root/Root.ts'
 
 const utiltyScriptPath = join(Root.root, 'packages', 'injected-code', 'dist', 'injectedCode.js')
 
+const runBuild = async () => {
+  const rpc = await LaunchFileSystemWorker.launchFileSystemWorker()
+  await rpc.invoke('FileSystem.exec', `npm`, ['run', 'build'], {
+    cwd: join(Root.root, 'packages', 'build'),
+  })
+  await rpc.dispose()
+}
+
 export const getUtilityScript = async (): Promise<string> => {
   if (!existsSync(utiltyScriptPath)) {
-    throw new Error(`utility script not found`)
+    await runBuild()
   }
   const injectedCode = readFileSync(utiltyScriptPath, 'utf-8')
   return injectedCode
