567fe24ea1704616b6276d12fc1a42d6d95439eb
Simon Siefke <simon.siefke@gmail.com>
2025-11-24 00:08:58 +0100

    feature: add test to move editor to terminal area

--- packages/injected-code/src/parts/KeyBoardActions/KeyBoardActions.ts
+++ packages/injected-code/src/parts/KeyBoardActions/KeyBoardActions.ts
@@ -18,30 +18,30 @@ const getAllOptions = (options) => {
   }
   const allOptions = [options]
 
-  if (options.ctrlKey) {
-    allOptions.unshift({
-      ...options,
-      key: 'Control',
-      keyCode: GetKeyCode.getKeyCode('Control'),
-      code: GetKeyCode.getCode('Control'),
-    })
-  }
-  if (options.metaKey) {
-    allOptions.unshift({
-      ...options,
-      key: 'Meta',
-      keyCode: GetKeyCode.getKeyCode('Meta'),
-      code: GetKeyCode.getKeyCode('Meta'),
-    })
-  }
-  if (options.shiftKey) {
-    allOptions.unshift({
-      ...options,
-      key: 'Shift',
-      keyCode: GetKeyCode.getKeyCode('Shift'),
-      code: GetKeyCode.getKeyCode('Shift'),
-    })
-  }
+  // if (options.ctrlKey) {
+  //   allOptions.unshift({
+  //     ...options,
+  //     key: 'Control',
+  //     keyCode: GetKeyCode.getKeyCode('Control'),
+  //     code: GetKeyCode.getCode('Control'),
+  //   })
+  // }
+  // if (options.metaKey) {
+  //   allOptions.unshift({
+  //     ...options,
+  //     key: 'Meta',
+  //     keyCode: GetKeyCode.getKeyCode('Meta'),
+  //     code: GetKeyCode.getKeyCode('Meta'),
+  //   })
+  // }
+  // if (options.shiftKey) {
+  //   allOptions.unshift({
+  //     ...options,
+  //     key: 'Shift',
+  //     keyCode: GetKeyCode.getKeyCode('Shift'),
+  //     code: GetKeyCode.getKeyCode('Shift'),
+  //   })
+  // }
   return allOptions
 }
 
@@ -56,7 +56,11 @@ export const press = (options, element = document.activeElement) => {
   Assert.object(options)
   const allOptions = getAllOptions(options)
   for (const option of allOptions) {
-    DispatchEvent.keyDown(element, option)
+    const event = DispatchEvent.keyDown(element, option)
+    // @ts-ignore
+    if (event?.defaultPrevented) {
+      return
+    }
   }
   for (const option of allOptions) {
     DispatchEvent.keyPress(element, option)
