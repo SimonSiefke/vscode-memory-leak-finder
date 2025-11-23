3967631599f3f13c32d8bbfa11d56466d74accec
Simon Siefke <simon.siefke@gmail.com>
2025-11-23 22:10:00 +0100

    tsc

--- packages/page-object/src/parts/WaitForApplicationToBeReady/WaitForApplicationToBeReady.ts
+++ packages/page-object/src/parts/WaitForApplicationToBeReady/WaitForApplicationToBeReady.ts
@@ -45,6 +45,7 @@ export const create = ({ page, expect, VError, ideVersion }) => {
           expect,
           VError,
           ideVersion,
+          electronApp,
         })
         await terminal.show()
         await terminal.killAll()
