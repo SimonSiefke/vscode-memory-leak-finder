0ef1ceec8ad8437fc7a9a21169647c133ce69f37
Simon Siefke <simon.siefke@gmail.com>
2025-11-24 16:24:30 +0100

    feature: handle enoent error gracefully when computing original classname

--- packages/original-name-worker/src/parts/GetOriginalClassNameFromFile/GetOriginalClassNameFromFile.ts
+++ packages/original-name-worker/src/parts/GetOriginalClassNameFromFile/GetOriginalClassNameFromFile.ts
@@ -2,9 +2,13 @@ import { readFile } from 'node:fs/promises'
 import { basename } from 'node:path'
 import * as GetOriginalClassName from '../GetOriginalClassName/GetOriginalClassName.ts'
 import { VError } from '@lvce-editor/verror'
+import { existsSync } from 'node:fs'
 
 export const getOriginalClassNameFromFile = async (originalCodePath: string, originalLine: number, originalColumn: number) => {
   try {
+    if (!existsSync(originalCodePath)) {
+      return ''
+    }
     const originalCode: string = await readFile(originalCodePath, 'utf8')
     const baseName = basename(originalCodePath)
     const originalClassName: string = GetOriginalClassName.getOriginalClassName(originalCode, originalLine, originalColumn, baseName)
