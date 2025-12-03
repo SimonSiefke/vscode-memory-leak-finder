71511640daaa64f3740af8cac7df6ef6b629a8c9
Simon Siefke <simon.siefke@gmail.com>
2025-12-03 09:57:45 +0100

    todo

--- packages/source-map-worker/src/parts/AddOriginalPositions/AddOriginalPositions.ts
+++ packages/source-map-worker/src/parts/AddOriginalPositions/AddOriginalPositions.ts
@@ -2,6 +2,7 @@ import type { IntermediateItem } from '../IntermediateItem/IntermediateItem.ts'
 import * as OriginalNameWorker from '../OriginalNameWorker/OriginalNameWorker.ts'
 import type { OriginalPosition } from '../OriginalPosition/OriginalPosition.ts'
 
+// TODO rename to addoriginalNames
 export const addOriginalPositions = async (intermediateItems: readonly IntermediateItem[]): Promise<readonly OriginalPosition[]> => {
   const rpc = await OriginalNameWorker.create()
   const finalResults: OriginalPosition[] = []
