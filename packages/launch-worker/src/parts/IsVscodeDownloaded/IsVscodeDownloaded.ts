bc919dfcfeb28a25a05b7ca55d747f20155c8abf
Simon Siefke <simon.siefke@gmail.com>
2025-12-03 08:49:59 +0100

    feature: add functions to cache result of downloadAndUnzipVsCode

---
+++ packages/launch-worker/src/parts/IsVscodeDownloaded/IsVscodeDownloaded.ts
@@ -0,0 +1,8 @@
+import * as DownloadWorker from '../DownloadWorker/DownloadWorker.ts'
+
+export const isVscodeDownloaded = async (vscodeVersion: string, vscodePath: string, commit: string): Promise<boolean> => {
+  const rpc = await DownloadWorker.launch()
+  const result = await rpc.invoke('Download.isVscodeDownloaded', vscodeVersion, vscodePath, commit)
+  await rpc.dispose()
+  return result
+}
