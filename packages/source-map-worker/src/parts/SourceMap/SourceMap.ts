2a491685b733aa1c4ea89fb93b8a81d0c083372b
Simon Siefke <simon.siefke@gmail.com>
2025-12-03 12:54:51 +0300

    feature: compute original names after computing source map positions (#2158)

--- packages/source-map-worker/src/parts/SourceMap/SourceMap.ts
+++ packages/source-map-worker/src/parts/SourceMap/SourceMap.ts
@@ -1,28 +1,22 @@
 import { join, resolve } from 'node:path'
 import type { RawSourceMap } from 'source-map'
 import { SourceMapConsumer } from 'source-map'
+import * as AddOriginalPositions from '../AddOriginalPositions/AddOriginalPositions.ts'
 import * as Assert from '../Assert/Assert.ts'
-import * as GetOriginalClassName from '../GetOriginalClassName/GetOriginalClassName.ts'
-import * as OriginalNameWorker from '../OriginalNameWorker/OriginalNameWorker.ts'
+import type { IntermediateItem } from '../IntermediateItem/IntermediateItem.ts'
+import type { OriginalPosition } from '../OriginalPosition/OriginalPosition.ts'
 import { root } from '../Root/Root.ts'
-
-interface OriginalPosition {
-  source: string | null
-  line: number | null
-  column: number | null
-  name: string | null
-}
 
 export const getOriginalPositions = async (
   sourceMap: RawSourceMap,
   positions: number[],
   classNames: boolean,
   hash: string,
-): Promise<OriginalPosition[]> => {
+): Promise<readonly OriginalPosition[]> => {
   Assert.object(sourceMap)
   Assert.array(positions)
-  const originalPositions = await SourceMapConsumer.with(sourceMap, null, async (consumer) => {
-    const originalPositions: OriginalPosition[] = []
+  const intermediateItems: IntermediateItem[] = await SourceMapConsumer.with(sourceMap, null, async (consumer) => {
+    const items: IntermediateItem[] = []
     for (let i = 0; i < positions.length; i += 2) {
       const line: number = positions[i]
       const column: number = positions[i + 1]
@@ -30,26 +24,29 @@ export const getOriginalPositions = async (
         line: line + 1,
         column: column + 1,
       })
+      let codePath: string | null = null
+      let needsOriginalName = false
       if (classNames && originalPosition.source && originalPosition.line !== null && originalPosition.column !== null) {
         const index: number = sourceMap.sources.indexOf(originalPosition.source)
         if (index !== -1) {
-          // TODO maybe compute this separately
           const sourceFileRelativePath: string = sourceMap.sources[index]
-          const originalCodePath: string = resolve(join(root, '.vscode-sources', hash, sourceFileRelativePath))
-          const originalClassName: string = await GetOriginalClassName.getOriginalClassNameFromFile(
-            originalCodePath,
-            originalPosition.line,
-            originalPosition.column,
-          )
-          originalPosition.name = originalClassName
+          codePath = resolve(join(root, '.vscode-sources', hash, sourceFileRelativePath))
+          needsOriginalName = true
         }
       }
-      originalPositions.push(originalPosition)
+      items.push({
+        line: originalPosition.line,
+        column: originalPosition.column,
+        codePath,
+        name: originalPosition.name,
+        needsOriginalName,
+        source: originalPosition.source,
+      })
     }
-    return originalPositions
+    return items
   })
 
-  // TODO add original names here, and cache computations
-  await OriginalNameWorker.dispose()
-  return originalPositions
+  const finalResults: readonly OriginalPosition[] = await AddOriginalPositions.addOriginalPositions(intermediateItems)
+
+  return finalResults
 }
