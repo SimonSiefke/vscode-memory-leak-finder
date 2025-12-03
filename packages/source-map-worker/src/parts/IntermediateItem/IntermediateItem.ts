580f833e1189520fcdd6b86428aaf963ffbd034c
Simon Siefke <simon.siefke@gmail.com>
2025-12-04 01:32:43 +0300

    Revert "feature: compute original names after computing source map positions …" (#2162)

--- packages/source-map-worker/src/parts/IntermediateItem/IntermediateItem.ts
+++
@@ -1,8 +0,0 @@
-export interface IntermediateItem {
-  readonly line: number | null
-  readonly column: number | null
-  readonly codePath: string | null
-  readonly name: string | null
-  readonly needsOriginalName: boolean
-  readonly source: string | null
-}
