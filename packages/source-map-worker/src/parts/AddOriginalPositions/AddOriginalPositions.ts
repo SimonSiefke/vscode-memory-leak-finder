cafdba5aa20f61f3c83b96ff94b6be25c3f67612
Simon Siefke <simon.siefke@gmail.com>
2025-12-03 09:53:46 +0100

    feature: compute original names after computing source map positions

---
+++ packages/source-map-worker/src/parts/AddOriginalPositions/AddOriginalPositions.ts
@@ -0,0 +1,28 @@
+import type { IntermediateItem } from '../IntermediateItem/IntermediateItem.ts'
+import * as OriginalNameWorker from '../OriginalNameWorker/OriginalNameWorker.ts'
+import type { OriginalPosition } from '../OriginalPosition/OriginalPosition.ts'
+
+export const addOriginalPositions = async (intermediateItems: readonly IntermediateItem[]): Promise<readonly OriginalPosition[]> => {
+  const rpc = await OriginalNameWorker.create()
+  const finalResults: OriginalPosition[] = []
+  for (const item of intermediateItems) {
+    if (item.needsOriginalName && item.codePath && item.line !== null && item.column !== null) {
+      const originalName: string = await rpc.invoke('OriginalName.getOriginalNameFromFile', item.codePath, item.line, item.column)
+      finalResults.push({
+        line: item.line,
+        column: item.column,
+        name: originalName || null,
+        source: item.source,
+      })
+    } else {
+      finalResults.push({
+        line: item.line,
+        column: item.column,
+        name: item.name,
+        source: item.source,
+      })
+    }
+  }
+  await rpc.dispose()
+  return finalResults
+}
