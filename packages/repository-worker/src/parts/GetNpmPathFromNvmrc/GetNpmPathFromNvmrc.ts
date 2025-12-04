9f8045264853e11a1bb99653ebb89c6825823457
Simon Siefke <simon.siefke@gmail.com>
2025-12-04 10:25:07 +0100

    fix: nvm path

--- packages/repository-worker/src/parts/GetNpmPathFromNvmrc/GetNpmPathFromNvmrc.ts
+++ packages/repository-worker/src/parts/GetNpmPathFromNvmrc/GetNpmPathFromNvmrc.ts
@@ -7,6 +7,12 @@ export const getNpmPathFromNvmrc = async (repoPath: string): Promise<string> => 
   const nvmrcContent = await FileSystemWorker.readFileContent(nvmrcPath)
   const nodeVersion = nvmrcContent.trim().replace(/^v/, '')
   const homeDir = homedir()
+  const execPath = process.execPath
+  const nvmIndex = execPath.indexOf('.nvm')
+  if (nvmIndex !== -1) {
+    const rest = execPath.slice(nvmIndex)
+    return Path.join(rest, '.nvm', 'versions', 'node', `v${nodeVersion}`, 'bin', 'npm')
+  }
   // TODO make this work on macos and windows also and possibly on linux with other folder structures
   const npmPath = Path.join(homeDir, '.nvm', 'versions', 'node', `v${nodeVersion}`, 'bin', 'npm')
   return npmPath
