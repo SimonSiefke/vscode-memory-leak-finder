464434a0fe5806721cd6d6c948cfc1e576fc5fe9
Simon Siefke <simon.siefke@gmail.com>
2025-12-07 08:52:15 +0100

    Revert "Revert "feature: compute original names after computing source map po…"
    This reverts commit 580f833e1189520fcdd6b86428aaf963ffbd034c.

--- packages/source-map-worker/src/parts/GetOriginalClassName/GetOriginalClassName.ts
+++
@@ -1,5 +0,0 @@
-import * as OriginalNameWorker from '../OriginalNameWorker/OriginalNameWorker.ts'
-
-export const getOriginalClassNameFromFile = async (originalPath: string, originalLine: number, originalColumn: number): Promise<string> => {
-  return OriginalNameWorker.invoke('OriginalName.getOriginalNameFromFile', originalPath, originalLine, originalColumn)
-}
--- packages/source-map-worker/src/parts/OriginalNameWorker/OriginalNameWorker.ts
+++ packages/source-map-worker/src/parts/OriginalNameWorker/OriginalNameWorker.ts
@@ -1,33 +1,10 @@
-import { NodeWorkerRpcParent, type Rpc } from '@lvce-editor/rpc'
+import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
 import { getOriginalNameWorkerPath } from '../OriginalNameWorkerPath/OriginalNameWorkerPath.ts'
 
-let rpcPromise: Promise<Rpc> | undefined
-
-const create = async () => {
+export const create = async () => {
   const rpc = await NodeWorkerRpcParent.create({
     path: getOriginalNameWorkerPath(),
     commandMap: {},
   })
   return rpc
-}
-
-const getOrCreateRpc = () => {
-  if (!rpcPromise) {
-    rpcPromise = create()
-  }
-  return rpcPromise
-}
-
-export const invoke = async (method: string, ...params: readonly any[]) => {
-  const rpc = await getOrCreateRpc()
-  return rpc.invoke(method, ...params)
-}
-
-export const dispose = async () => {
-  if (rpcPromise) {
-    const promise = rpcPromise
-    rpcPromise = undefined
-    const rpc = await promise
-    rpc.dispose()
-  }
 }
