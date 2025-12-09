442af7a434d9fb9bb88d5bb47bd5c5a5bd709b23
Simon Siefke <simon.siefke@gmail.com>
2025-12-09 20:29:08 +0100

    command

--- packages/page-object/src/parts/SimpleBrowser/SimpleBrowser.ts
+++ packages/page-object/src/parts/SimpleBrowser/SimpleBrowser.ts
@@ -1,7 +1,21 @@
+import { createServer } from 'node:http'
+import * as QuickPick from '../QuickPick/QuickPick.ts'
+import * as WellKnownCommands from '../WellKnownCommands/WellKnownCommands.ts'
+
 export const create = ({ page, expect, VError, ideVersion }) => {
   return {
     async show(url) {
       try {
+        const server = createServer((req, res) => {
+          res.statusCode = 200
+          res.end('<h1>hello world</h1>')
+        })
+        const { resolve, promise } = Promise.withResolvers()
+        server.once('listening', resolve)
+        await promise
+
+        const quickPick = QuickPick.create({ page, expect, VError })
+        await quickPick.executeCommand(WellKnownCommands.SimpleBrowserShow)
         // TODO maybe create a mock http server
         // with a custom index html file to use
         // as url
