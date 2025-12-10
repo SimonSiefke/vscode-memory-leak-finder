3a61ad676ab089a8c4b3445c005bce2f1c3bbee7
Simon Siefke <simon.siefke@gmail.com>
2025-12-10 10:49:26 +0100

    feature: add logpoint test

---
+++ packages/e2e/src/debug-logpoint.ts
@@ -0,0 +1,33 @@
+import type { TestContext } from '../types.ts'
+
+export const setup = async ({ Editor, Workspace, Explorer, RunAndDebug }: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      name: 'index.js',
+      content: `let x = 1
+
+setInterval(()=>{
+  x++
+}, 1000)`,
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('index.js')
+  await RunAndDebug.removeAllBreakpoints()
+  await Editor.open('index.js')
+  await Editor.setLogpoint(4, 'x = {x}')
+}
+
+export const run = async ({ RunAndDebug, DebugConsole }: TestContext): Promise<void> => {
+  await RunAndDebug.startRunAndDebug()
+  await DebugConsole.show()
+  await DebugConsole.shouldHaveLogpointOutput('x = 1')
+  await RunAndDebug.stop()
+}
+
+export const teardown = async ({ RunAndDebug, Editor }: TestContext): Promise<void> => {
+  await RunAndDebug.stop()
+  await RunAndDebug.removeAllBreakpoints()
+  await Editor.closeAll()
+}
