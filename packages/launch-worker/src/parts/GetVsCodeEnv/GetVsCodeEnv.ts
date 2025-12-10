c1ec86abbed16f132f15328fcbe5f7d01fd9e839
Simon Siefke <simon.siefke@gmail.com>
2025-12-10 14:11:29 +0100

    proxy

--- packages/launch-worker/src/parts/GetVsCodeEnv/GetVsCodeEnv.ts
+++ packages/launch-worker/src/parts/GetVsCodeEnv/GetVsCodeEnv.ts
@@ -13,34 +13,5 @@ export const getVsCodeEnv = async ({ runtimeDir, processEnv }) => {
   delete env.NODE_OPTIONS
   delete env.ELECTRON_RUN_AS_NODE
 
-  // Check for proxy state file and set HTTP_PROXY/HTTPS_PROXY if proxy is configured
-  try {
-    const { readFile } = await import('fs/promises')
-    const { existsSync } = await import('fs')
-    const { join } = await import('path')
-    const { dirname } = await import('path')
-    const { fileURLToPath } = await import('url')
-    const __dirname = dirname(fileURLToPath(import.meta.url))
-    const root = join(__dirname, '..', '..', '..', '..', '..')
-    const proxyStateFile = join(root, '.vscode-proxy-state.json')
-
-    if (existsSync(proxyStateFile)) {
-      const content = await readFile(proxyStateFile, 'utf8')
-      const proxyState = JSON.parse(content)
-      if (proxyState.proxyUrl) {
-        env.HTTP_PROXY = proxyState.proxyUrl
-        env.HTTPS_PROXY = proxyState.proxyUrl
-        env.http_proxy = proxyState.proxyUrl
-        env.https_proxy = proxyState.proxyUrl
-        // Don't proxy localhost connections
-        env.NO_PROXY = 'localhost,127.0.0.1,0.0.0.0'
-        env.no_proxy = 'localhost,127.0.0.1,0.0.0.0'
-        console.log(`[GetVsCodeEnv] Set proxy environment variables: HTTP_PROXY=${proxyState.proxyUrl}`)
-      }
-    }
-  } catch (error) {
-    // Ignore errors reading proxy state
-  }
-
   return env
 }
