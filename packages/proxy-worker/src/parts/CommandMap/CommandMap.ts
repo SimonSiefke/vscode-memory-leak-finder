acfaebad4006ff99d255d3b831a46d98be75ef20
Simon Siefke <simon.siefke@gmail.com>
2025-12-10 21:34:07 +0300

    feature: add proxy worker functions (#2185)

--- packages/proxy-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/proxy-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,11 @@
-export const commandMap = {}
+import * as Proxy from '../Proxy/Proxy.ts'
+import * as ConvertRequestsToMocks from '../ConvertRequestsToMocks/ConvertRequestsToMocks.ts'
+
+export const commandMap = {
+  'ConvertRequestsToMocks.convertRequestsToMocksMain': ConvertRequestsToMocks.convertRequestsToMocksMain,
+  'Proxy.createHttpProxyServer': Proxy.createHttpProxyServer,
+  'Proxy.disposeProxyServer': Proxy.disposeProxyServer,
+  'Proxy.getCACertPath': Proxy.getCACertPath,
+  'Proxy.getProxyEnvVars': Proxy.getProxyEnvVars,
+  'Proxy.setupProxy': Proxy.setupProxy,
+}
