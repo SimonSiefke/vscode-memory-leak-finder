a2eacb03a4ddbc53d2216a3d30b0ecea57e1ee49
Simon Siefke <simon.siefke@gmail.com>
2025-12-10 18:52:45 +0100

    sort

--- packages/proxy-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/proxy-worker/src/parts/CommandMap/CommandMap.ts
@@ -2,10 +2,10 @@ import * as Proxy from '../Proxy/Proxy.ts'
 import * as ConvertRequestsToMocks from '../ConvertRequestsToMocks/ConvertRequestsToMocks.ts'
 
 export const commandMap = {
+  'ConvertRequestsToMocks.convertRequestsToMocksMain': ConvertRequestsToMocks.convertRequestsToMocksMain,
   'Proxy.createHttpProxyServer': Proxy.createHttpProxyServer,
   'Proxy.disposeProxyServer': Proxy.disposeProxyServer,
   'Proxy.getCACertPath': Proxy.getCACertPath,
   'Proxy.getProxyEnvVars': Proxy.getProxyEnvVars,
   'Proxy.setupProxy': Proxy.setupProxy,
-  'ConvertRequestsToMocks.convertRequestsToMocksMain': ConvertRequestsToMocks.convertRequestsToMocksMain,
 }
