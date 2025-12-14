23e3e3b700522d372e116ec0f3a547e1b52e1d7d
Simon Siefke <simon.siefke@gmail.com>
2025-12-14 19:27:19 +0100

    fix

--- packages/source-map-worker/src/parts/OriginalNameWorker/OriginalNameWorker.ts
+++ packages/source-map-worker/src/parts/OriginalNameWorker/OriginalNameWorker.ts
@@ -1,8 +1,6 @@
 import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
 import { getOriginalNameWorkerPath } from '../OriginalNameWorkerPath/OriginalNameWorkerPath.ts'
 
-let rpcPromise: Promise<Rpc> | undefined
-
 export const create = async () => {
   const rpc = await NodeWorkerRpcParent.create({
     path: getOriginalNameWorkerPath(),
