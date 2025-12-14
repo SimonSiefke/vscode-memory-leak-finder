6dc5e9f5494c1155ab321acb63e2bca7e902b96d
Simon Siefke <simon.siefke@gmail.com>
2025-12-14 18:43:37 +0300

    feature: use options object (#2201)

--- packages/launch-worker/src/parts/Launch/Launch.ts
+++ packages/launch-worker/src/parts/Launch/Launch.ts
@@ -4,28 +4,49 @@ import * as Disposables from '../Disposables/Disposables.ts'
 import { getInitializationWorkerUrl } from '../GetInitializationWorkerUrl/GetInitializationWorkerUrl.ts'
 import * as LaunchIde from '../LaunchIde/LaunchIde.ts'
 
-export const launch = async (
-  headlessMode: boolean,
-  cwd: string,
-  ide: string,
-  vscodePath: string,
-  vscodeVersion: string,
-  commit: string,
-  insidersCommit: string,
-  connectionId: number,
-  isFirstConnection: boolean,
-  canUseIdleCallback: boolean,
-  attachedToPageTimeout: number,
-  inspectSharedProcess: boolean,
-  inspectExtensions: boolean,
-  inspectPtyHost: boolean,
-  enableExtensions: boolean,
-  inspectPtyHostPort: number,
-  inspectSharedProcessPort: number,
-  inspectExtensionsPort: number,
-  enableProxy: boolean,
-  useProxyMock: boolean,
-): Promise<any> => {
+export interface LaunchOptions {
+  readonly headlessMode: boolean
+  readonly cwd: string
+  readonly ide: string
+  readonly vscodePath: string
+  readonly vscodeVersion: string
+  readonly commit: string
+  readonly insidersCommit: string
+  readonly connectionId: number
+  readonly isFirstConnection: boolean
+  readonly canUseIdleCallback: boolean
+  readonly attachedToPageTimeout: number
+  readonly inspectSharedProcess: boolean
+  readonly inspectExtensions: boolean
+  readonly inspectPtyHost: boolean
+  readonly enableExtensions: boolean
+  readonly inspectPtyHostPort: number
+  readonly inspectSharedProcessPort: number
+  readonly inspectExtensionsPort: number
+  readonly enableProxy: boolean
+  readonly useProxyMock: boolean
+}
+
+export const launch = async (options: LaunchOptions): Promise<any> => {
+  const {
+    headlessMode,
+    cwd,
+    ide,
+    vscodePath,
+    vscodeVersion,
+    commit,
+    insidersCommit,
+    attachedToPageTimeout,
+    inspectSharedProcess,
+    inspectExtensions,
+    inspectPtyHost,
+    enableExtensions,
+    inspectPtyHostPort,
+    inspectSharedProcessPort,
+    inspectExtensionsPort,
+    enableProxy,
+    useProxyMock,
+  } = options
   const { child } = await LaunchIde.launchIde({
     headlessMode,
     cwd,
