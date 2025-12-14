24b75d9c46340f22e23aa8eaeb9947b257193e87
Simon Siefke <simon.siefke@gmail.com>
2025-12-14 22:12:55 +0300

    feature: add terminal find test (#2205)

---
+++ packages/e2e/src/terminal.search.ts
@@ -0,0 +1,26 @@
+import type { TestContext } from '../types.js'
+
+export const skip = true
+
+export const setup = async ({ Terminal, Workspace, SideBar }: TestContext): Promise<void> => {
+  await Terminal.killAll()
+  await Workspace.setFiles([])
+  await SideBar.hide()
+  // @ts-ignore
+  await Terminal.show({
+    waitForReady: true,
+  })
+  // @ts-ignore
+  await Terminal.type('echo abc')
+  // @ts-ignore
+  await Terminal.openFind()
+}
+
+export const run = async ({ Terminal }: TestContext): Promise<void> => {
+  // @ts-ignore
+  await Terminal.clearFindInput()
+  // @ts-ignore
+  await Terminal.setFindInput('abc')
+  // @ts-ignore
+  await Terminal.clearFindInput()
+}
