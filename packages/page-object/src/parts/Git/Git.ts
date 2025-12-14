c7771ab16fc69cf817c5dded90088c40bbc839c5
Simon Siefke <simon.siefke@gmail.com>
2025-12-15 00:19:51 +0100

    simplify

--- packages/page-object/src/parts/Git/Git.ts
+++ packages/page-object/src/parts/Git/Git.ts
@@ -3,30 +3,26 @@ import { join } from 'node:path'
 import * as Root from '../Root/Root.ts'
 
 export const create = ({ electronApp, page, expect, VError }) => {
+  const workspace = join(Root.root, '.vscode-test-workspace')
   return {
     async init() {
-      const workspace = join(Root.root, '.vscode-test-workspace')
       await execa('git', ['init'], { cwd: workspace })
       await execa('git', ['config', 'user.name', 'Test User'], { cwd: workspace })
       await execa('git', ['config', 'user.email', 'test@example.com'], { cwd: workspace })
       await page.waitForIdle()
     },
     async add() {
-      const workspace = join(Root.root, '.vscode-test-workspace')
       await execa('git', ['add', '-f', '.'], { cwd: workspace })
     },
     async commit(message) {
-      const workspace = join(Root.root, '.vscode-test-workspace')
       await execa('git', ['commit', '-m', message], { cwd: workspace })
       await page.waitForIdle()
     },
     async checkoutBranch(branchName) {
-      const workspace = join(Root.root, '.vscode-test-workspace')
       await execa('git', ['checkout', branchName], { cwd: workspace })
       await page.waitForIdle()
     },
     async createBranch(branchName) {
-      const workspace = join(Root.root, '.vscode-test-workspace')
       await execa('git', ['checkout', '-b', branchName], { cwd: workspace })
       await page.waitForIdle()
     },
