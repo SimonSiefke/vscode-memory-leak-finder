c2d2084588a41f4e89092ff7ed67d080f2536e84
Simon Siefke <simon.siefke@gmail.com>
2025-12-15 03:06:25 +0300

    feature: add switch branch test (#2213)

---
+++ packages/e2e/src/source-control.switch-branch.ts
@@ -0,0 +1,42 @@
+import type { TestContext } from '../types.ts'
+
+export const skip = 1
+
+export const setup = async ({ Workspace, Git, Editor, ActivityBar, SourceControl, Explorer }: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      name: 'b.txt',
+      content: 'content in main branch',
+    },
+  ])
+  await Git.init()
+  await Git.add()
+  await Git.commit('initial commit')
+  await Git.createBranch('b')
+  await Git.checkoutBranch('b')
+  await Workspace.add({
+    name: 'b.txt',
+    content: 'content in branch b',
+  })
+  await Git.add()
+  await Git.commit('update b.txt in branch b')
+  await Git.checkoutBranch('main')
+  await ActivityBar.showSourceControl()
+  await SourceControl.shouldHaveHistoryItem('initial commit')
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.shouldHaveItem('b.txt')
+  await Editor.open('b.txt')
+}
+
+export const run = async ({ SourceControl, Editor }: TestContext): Promise<void> => {
+  await Editor.shouldHaveText('content in main branch')
+  await SourceControl.checkoutBranch('b')
+  await Editor.close()
+  await Editor.open('b.txt')
+  await Editor.shouldHaveText('content in branch b')
+  await SourceControl.checkoutBranch('main')
+  await Editor.close()
+  await Editor.open('b.txt')
+  await Editor.shouldHaveText('content in main branch')
+}
