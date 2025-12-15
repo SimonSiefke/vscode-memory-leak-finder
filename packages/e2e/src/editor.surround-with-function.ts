78aef04b1878db40a93806a4102a414c7b1cb367
Simon Siefke <simon.siefke@gmail.com>
2025-12-16 00:56:26 +0100

    test

--- packages/e2e/src/editor.surround-with-function.ts
+++ packages/e2e/src/editor.surround-with-function.ts
@@ -38,6 +38,8 @@ export const setup = async ({ Editor, Workspace, Explorer }: TestContext): Promi
 }
 
 export const run = async ({ Editor, Workspace }: TestContext): Promise<void> => {
+  await Editor.shouldHaveText(originalContent)
+
   // @ts-ignore
   await Editor.setCursor(2, 3)
   await Editor.selectAll()
