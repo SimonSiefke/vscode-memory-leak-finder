6f5e18255a3b6a452428ae8ac630ec506d58ed96
Simon Siefke <simon.siefke@gmail.com>
2025-12-15 03:06:10 +0300

    feature: add markdown switch test (#2212)

---
+++ packages/e2e/src/markdown-preview.switch.ts
@@ -0,0 +1,53 @@
+import type { TestContext } from '../types.js'
+
+export const skip = 1
+
+export const setup = async ({
+  Workbench,
+  Workspace,
+  Explorer,
+  Editor,
+  QuickPick,
+  // @ts-ignore
+  WellKnownCommands,
+  MarkdownPreview,
+}: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      name: 'index.md',
+      content: `# hello world`,
+    },
+    {
+      name: 'other.md',
+      content: `# abc`,
+    },
+  ])
+  await Editor.closeAll()
+  await Explorer.focus()
+  await Explorer.refresh()
+  await Explorer.shouldHaveItem('index.md')
+  await Editor.open('index.md')
+  await QuickPick.executeCommand(WellKnownCommands.MarkdownOpenPreviewToTheSide)
+  const subFrame = await MarkdownPreview.shouldBeVisible()
+  // @ts-ignore
+  await MarkdownPreview.shouldHaveHeading(subFrame, 'hello-world')
+  // @ts-ignore
+  await Workbench.focusLeftEditorGroup()
+}
+
+// @ts-ignore
+export const run = async ({ Workbench, Editor, QuickPick, WellKnownCommands, MarkdownPreview }: TestContext): Promise<void> => {
+  await Editor.open('other.md')
+  const subFrame2 = await MarkdownPreview.shouldBeVisible()
+  // @ts-ignore
+  await MarkdownPreview.shouldHaveHeading(subFrame2, 'abc')
+  await Editor.open('index.md')
+  const subFrame = await MarkdownPreview.shouldBeVisible()
+  // @ts-ignore
+  await MarkdownPreview.shouldHaveHeading(subFrame, 'hello-world')
+}
+
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.save({ viaKeyBoard: true })
+  await Editor.closeAll()
+}
