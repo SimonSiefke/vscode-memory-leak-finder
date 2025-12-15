11de806f3bb448b1c31fa9dff566c85fc28762fc
Simon Siefke <simon.siefke@gmail.com>
2025-12-15 01:14:04 +0100

    fix

--- packages/e2e/src/editor.toggle-semantic-tokens.ts
+++ packages/e2e/src/editor.toggle-semantic-tokens.ts
@@ -54,9 +54,9 @@ export const run = async ({ Editor, SettingsEditor }: TestContext): Promise<void
   })
   await Editor.switchToTab('file.ts')
   await Editor.focus()
-  await Editor.click('class')
+  await Editor.click('myFunction')
   await Editor.inspectTokens()
-  await Editor.shouldHaveInspectedToken('class5 chars')
+  await Editor.shouldHaveInspectedToken('myFunction10 chars')
   await Editor.shouldHaveSemanticToken('class')
   await Editor.closeInspectedTokens()
 }
