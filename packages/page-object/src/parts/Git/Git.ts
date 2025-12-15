67c54895cb8d1311bf0af986db37626b152767ee
Simon Siefke <simon.siefke@gmail.com>
2025-12-15 03:06:25 +0300

    feature: add switch branch test (#2213)

--- packages/page-object/src/parts/Git/Git.ts
+++ packages/page-object/src/parts/Git/Git.ts
@@ -1,23 +1,36 @@
 import { execa } from 'execa'
-import { join } from 'node:path'
+import { join, dirname } from 'node:path'
+import { mkdir } from 'node:fs/promises'
 import * as Root from '../Root/Root.ts'
 
 export const create = ({ electronApp, page, expect, VError }) => {
+  const workspace = join(Root.root, '.vscode-test-workspace')
+  const workspaceParent = dirname(workspace)
+  const gitEnv = {
+    GIT_CEILING_DIRECTORIES: workspaceParent,
+  }
   return {
     async init() {
-      const workspace = join(Root.root, '.vscode-test-workspace')
-      await execa('git', ['init'], { cwd: workspace })
-      await execa('git', ['config', 'user.name', 'Test User'], { cwd: workspace })
-      await execa('git', ['config', 'user.email', 'test@example.com'], { cwd: workspace })
+      await mkdir(workspace, { recursive: true })
+      await execa('git', ['init'], { cwd: workspace, env: { ...process.env, ...gitEnv } })
+      await execa('git', ['config', 'user.name', 'Test User'], { cwd: workspace, env: { ...process.env, ...gitEnv } })
+      await execa('git', ['config', 'user.email', 'test@example.com'], { cwd: workspace, env: { ...process.env, ...gitEnv } })
       await page.waitForIdle()
     },
     async add() {
-      const workspace = join(Root.root, '.vscode-test-workspace')
-      await execa('git', ['add', '.'], { cwd: workspace })
+      await execa('git', ['add', '-f', '.'], { cwd: workspace, env: { ...process.env, ...gitEnv } })
+      await page.waitForIdle()
     },
     async commit(message) {
-      const workspace = join(Root.root, '.vscode-test-workspace')
-      await execa('git', ['commit', '-m', message], { cwd: workspace })
+      await execa('git', ['commit', '-m', message], { cwd: workspace, env: { ...process.env, ...gitEnv } })
+      await page.waitForIdle()
+    },
+    async checkoutBranch(branchName) {
+      await execa('git', ['checkout', branchName], { cwd: workspace, env: { ...process.env, ...gitEnv } })
+      await page.waitForIdle()
+    },
+    async createBranch(branchName) {
+      await execa('git', ['checkout', '-b', branchName], { cwd: workspace, env: { ...process.env, ...gitEnv } })
       await page.waitForIdle()
     },
   }
