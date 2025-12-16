dd23712964601edec62c3a6386daeb1cee05f02a
Simon Siefke <simon.siefke@gmail.com>
2025-12-17 00:12:42 +0300

    feature: add github pull requests e2e test (#2220)

---
+++ packages/e2e/src/github-pull-requests.open.ts
@@ -0,0 +1,35 @@
+import type { TestContext } from '../types.js'
+
+export const skip = 1
+
+export const requiresNetwork = true
+
+export const setup = async ({
+  GitHubPullRequests,
+  Workspace,
+  Extensions,
+  Editor,
+  ExtensionDetailView,
+  Git,
+}: TestContext): Promise<void> => {
+  await Editor.closeAll()
+  await Workspace.setFiles([])
+  await Git.cloneRepository('https://github.com/octocat/Hello-World.git')
+  await Extensions.show()
+  await Extensions.search('github pull requests')
+  await Extensions.first.shouldBe('GitHub Pull Requests')
+  await Extensions.first.click()
+  await ExtensionDetailView.installExtension()
+  await Editor.closeAll()
+  await GitHubPullRequests.focusView()
+}
+
+export const run = async ({ GitHubPullRequests, ActivityBar, SideBar }: TestContext): Promise<void> => {
+  await SideBar.hide()
+  await ActivityBar.showExplorer()
+  await GitHubPullRequests.focusView()
+}
+
+export const teardown = async ({ SideBar }: TestContext) => {
+  await SideBar.hide()
+}
