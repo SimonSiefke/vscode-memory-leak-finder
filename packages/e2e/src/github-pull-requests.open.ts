6d0c68d92b1c1745f52d0df9f68745a7d681494b
Simon Siefke <simon.siefke@gmail.com>
2025-12-16 21:34:54 +0100

    open

---
+++ packages/e2e/src/github-pull-requests.open.ts
@@ -0,0 +1,28 @@
+import type { TestContext } from '../types.js'
+
+export const skip = 1
+
+export const setup = async ({
+  GitHubPullRequests,
+  Workspace,
+  Extensions,
+  Editor,
+  ExtensionDetailView,
+  Git,
+}: TestContext): Promise<void> => {
+  await Editor.closeAll()
+  await Workspace.setFiles([])
+  await Git.cloneRepository('https://github.com/octocat/Hello-World.git')
+  await Extensions.show()
+  await Extensions.search('github pull requests')
+  await Extensions.first.shouldBe('GitHub Pull Requests')
+  await Extensions.first.click()
+  await ExtensionDetailView.installExtension()
+  await Editor.closeAll()
+  await GitHubPullRequests.focusView()
+}
+
+export const run = async ({ GitHubPullRequests, SideBar }: TestContext): Promise<void> => {
+  await SideBar.hide()
+  await GitHubPullRequests.focusView()
+}
