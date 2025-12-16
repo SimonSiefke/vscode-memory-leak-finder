d8972239d71ef01dc23852f6fae24eab7bc817c4
Simon Siefke <simon.siefke@gmail.com>
2025-12-17 00:16:43 +0100

    feature: try to make profile extensions test less flaky

--- packages/page-object/src/parts/RunningExtensions/RunningExtensions.ts
+++ packages/page-object/src/parts/RunningExtensions/RunningExtensions.ts
@@ -29,29 +29,39 @@ export const create = ({ expect, page, VError }) => {
     },
     async startProfilingExtensionHost() {
       try {
+        await page.waitForIdle()
         const actionLabel = page.locator('.action-label[aria-label="Start Extension Host Profile"]')
         await expect(actionLabel).toBeVisible()
+        await page.waitForIdle()
         await actionLabel.click()
+        await page.waitForIdle()
         await expect(actionLabel).toBeHidden({
           timeout: 15_000,
         })
         const stopLabel = page.locator('.action-label[aria-label="Stop Extension Host Profile"]')
         await expect(stopLabel).toBeVisible()
+        await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to start profiling extension host`)
       }
     },
     async stopProfilingExtensionHost() {
       try {
+        await page.waitForIdle()
         const actionLabel = page.locator('.action-label[aria-label="Stop Extension Host Profile"]')
         await expect(actionLabel).toBeVisible()
+        await page.waitForIdle()
         await actionLabel.click()
+        await page.waitForIdle()
         await expect(actionLabel).toBeHidden()
+        await page.waitForIdle()
         const startLabel = page.locator('.action-label[aria-label="Start Extension Host Profile"]')
         await expect(startLabel).toBeVisible()
+        await page.waitForIdle()
         const gitExtensionProfileTime = page.locator('.editor-container [aria-label="git"] .extension .profile-time')
         await expect(gitExtensionProfileTime).toBeVisible()
         await expect(gitExtensionProfileTime).toHaveText(/^Profile: /)
+        await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to stop profiling extension host`)
       }
