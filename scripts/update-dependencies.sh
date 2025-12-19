e651e16b72f008481a72f2a2abfea885e236ed42
Simon Siefke <simon.siefke@gmail.com>
2025-12-19 14:38:51 +0300

    feature: update dependencies (#2254)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -41,10 +41,13 @@ cd packages/heap-snapshot-parsing-worker && updateDependencies && cd ../../ &&
 cd packages/heap-snapshot-worker && updateDependencies && cd ../../ &&
 cd packages/initialization-worker && updateDependencies && cd ../../ &&
 cd packages/injected-code && updateDependencies && cd ../../ &&
+cd packages/launch-worker && updateDependencies && cd ../../ &&
 cd packages/load-source-map-worker && updateDependencies && cd ../../ &&
 cd packages/memory-leak-finder && updateDependencies && cd ../../ &&
 cd packages/original-name-worker && updateDependencies && cd ../../ &&
 cd packages/page-object && updateDependencies && cd ../../ &&
+cd packages/prompts && updateDependencies && cd ../../ &&
+cd packages/proxy-worker && updateDependencies && cd ../../ &&
 cd packages/repository-worker && updateDependencies && cd ../../ &&
 cd packages/source-map-worker && updateDependencies && cd ../../ &&
 cd packages/stdout-worker && updateDependencies && cd ../../ &&
