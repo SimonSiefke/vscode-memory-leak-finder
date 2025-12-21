f6f7f68f0e43a471c2782f7666ec4a29f45164f2
Simon Siefke <simon.siefke@gmail.com>
2025-12-21 15:29:16 +0100

    move functions

---
+++ packages/proxy-worker/src/parts/ConvertRequestsToMocks/HasConfigEntry/HasConfigEntry.ts
@@ -0,0 +1,15 @@
+import type { MockConfigEntry } from '../../MockConfigEntry/MockConfigEntry.ts'
+import * as MatchesPattern from '../MatchesPattern/MatchesPattern.ts'
+
+export const hasConfigEntry = (config: MockConfigEntry[], hostname: string, pathname: string, method: string): boolean => {
+  const methodLower = method.toLowerCase()
+  for (const entry of config) {
+    const hostnameMatch = MatchesPattern.matchesPattern(hostname, entry.hostname) || hostname.includes(entry.hostname)
+    const pathnameMatch = MatchesPattern.matchesPattern(pathname, entry.pathname)
+    const methodMatch = MatchesPattern.matchesPattern(methodLower, entry.method.toLowerCase())
+    if (hostnameMatch && pathnameMatch && methodMatch) {
+      return true
+    }
+  }
+  return false
+}
