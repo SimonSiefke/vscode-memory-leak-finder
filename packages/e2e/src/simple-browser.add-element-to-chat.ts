4532d9cd1663bd31ab988043e61463f7c9bb5d0e
Simon Siefke <simon.siefke@gmail.com>
2025-12-21 23:31:29 +0100

    add todo

--- packages/e2e/src/simple-browser.add-element-to-chat.ts
+++ packages/e2e/src/simple-browser.add-element-to-chat.ts
@@ -2,22 +2,37 @@ import type { TestContext } from '../types.js'
 
 export const skip = 1
 
-export const setup = async ({ Editor, SimpleBrowser, Workspace }: TestContext): Promise<void> => {
+export const setup = async ({ Editor, SimpleBrowser, Workspace, Electron }: TestContext): Promise<void> => {
   await Workspace.setFiles([])
   await Editor.closeAll()
   await SimpleBrowser.createMockServer({
     id: 'simple-browser-show',
     port: 3001,
   })
-}
-
-export const run = async ({ Editor, SimpleBrowser }: TestContext): Promise<void> => {
   await SimpleBrowser.show({
     port: 3001,
   })
+  await Electron.mockWebViewDebugger({
+    async 'Overlay.inspectNodeRequested'(emitter) {
+      await emitter.emit('Overlay.inspectNodeRequested')
+      return {
+        backendNodeId: 1234,
+      }
+    },
+    //  TODO
+  })
+}
+
+export const run = async ({ Editor, SimpleBrowser }: TestContext): Promise<void> => {
   await SimpleBrowser.addElementToChat({
     selector: 'h1',
   })
+  // TODO in order to mock selected node,
+  // do it like this
+  // 1. find all instances of webview Debugger
+  // 2. filter to find the one matching the simple browser
+  // 3. overwrite debugger.prototype.sendCommand and when it is called, send a fake inspectNoderequested event
+  // 4. undo the mocks from 3.
   await new Promise((r) => {})
   await Editor.closeAll()
 }
