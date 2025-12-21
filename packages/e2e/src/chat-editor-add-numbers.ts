2191bd4f9cc775bc24e903187b1d476e34a9ca00
Simon Siefke <simon.siefke@gmail.com>
2025-12-21 15:20:31 +0100

    remove unused

--- packages/e2e/src/chat-editor-add-numbers.ts
+++ packages/e2e/src/chat-editor-add-numbers.ts
@@ -2,8 +2,7 @@ import type { TestContext } from '../types.js'
 
 export const skip = 1
 
-export const setup = async ({ Editor, ChatEditor, NetworkInterceptor }: TestContext): Promise<void> => {
-  await NetworkInterceptor.enable()
+export const setup = async ({ Editor, ChatEditor }: TestContext): Promise<void> => {
   await Editor.closeAll()
   await ChatEditor.open()
 }
@@ -17,7 +16,6 @@ export const run = async ({ ChatEditor }: TestContext): Promise<void> => {
   await ChatEditor.clearAll()
 }
 
-export const teardown = async ({ Editor, NetworkInterceptor }: TestContext): Promise<void> => {
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
   await Editor.closeAll()
-  await NetworkInterceptor.disable()
 }
