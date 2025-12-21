ddc9db60a87c40e21fb455634c757254600587e2
Simon Siefke <simon.siefke@gmail.com>
2025-12-21 21:35:46 +0100

    fix

--- packages/page-object/src/parts/Git/Git.ts
+++ packages/page-object/src/parts/Git/Git.ts
@@ -7,6 +7,53 @@ export const create = ({ page, VError }) => {
   const workspace = join(Root.root, '.vscode-test-workspace')
 
   return {
+    async add() {
+      try {
+        await page.waitForIdle()
+        await execa('git', ['add', '-f', '.'], { cwd: workspace, env: { ...process.env } })
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to add`)
+      }
+    },
+    async checkoutBranch(branchName: string) {
+      try {
+        await page.waitForIdle()
+        await execa('git', ['checkout', branchName], { cwd: workspace, env: { ...process.env } })
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to checkout branch ${branchName}`)
+      }
+    },
+    async cloneRepository(repoUrl: string) {
+      // Clear the workspace first
+      const dirents = await readdir(workspace).catch(() => [])
+      for (const dirent of dirents) {
+        const absolutePath = join(workspace, dirent)
+        await rm(absolutePath, { force: true, recursive: true })
+      }
+      // Clone directly into the workspace directory
+      await execa('git', ['clone', repoUrl, '.'], { cwd: workspace, env: { ...process.env } })
+      await page.waitForIdle()
+    },
+    async commit(message: string) {
+      try {
+        await page.waitForIdle()
+        await execa('git', ['commit', '-m', message], { cwd: workspace, env: { ...process.env } })
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to commit`)
+      }
+    },
+    async createBranch(branchName: string) {
+      try {
+        await page.waitForIdle()
+        await execa('git', ['checkout', '-b', branchName], { cwd: workspace, env: { ...process.env } })
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to create branch ${branchName}`)
+      }
+    },
     async init() {
       try {
         await execa('git', ['init'], { cwd: workspace, env: { ...process.env } })
@@ -20,53 +67,6 @@ export const create = ({ page, VError }) => {
       } catch (error) {
         throw new VError(error, `Failed to init`)
       }
-    },
-    async add() {
-      try {
-        await page.waitForIdle()
-        await execa('git', ['add', '-f', '.'], { cwd: workspace, env: { ...process.env } })
-        await page.waitForIdle()
-      } catch (error) {
-        throw new VError(error, `Failed to add`)
-      }
-    },
-    async commit(message: string) {
-      try {
-        await page.waitForIdle()
-        await execa('git', ['commit', '-m', message], { cwd: workspace, env: { ...process.env } })
-        await page.waitForIdle()
-      } catch (error) {
-        throw new VError(error, `Failed to commit`)
-      }
-    },
-    async checkoutBranch(branchName: string) {
-      try {
-        await page.waitForIdle()
-        await execa('git', ['checkout', branchName], { cwd: workspace, env: { ...process.env } })
-        await page.waitForIdle()
-      } catch (error) {
-        throw new VError(error, `Failed to checkout branch ${branchName}`)
-      }
-    },
-    async createBranch(branchName: string) {
-      try {
-        await page.waitForIdle()
-        await execa('git', ['checkout', '-b', branchName], { cwd: workspace, env: { ...process.env } })
-        await page.waitForIdle()
-      } catch (error) {
-        throw new VError(error, `Failed to create branch ${branchName}`)
-      }
-    },
-    async cloneRepository(repoUrl: string) {
-      // Clear the workspace first
-      const dirents = await readdir(workspace).catch(() => [])
-      for (const dirent of dirents) {
-        const absolutePath = join(workspace, dirent)
-        await rm(absolutePath, { force: true, recursive: true })
-      }
-      // Clone directly into the workspace directory
-      await execa('git', ['clone', repoUrl, '.'], { cwd: workspace, env: { ...process.env } })
-      await page.waitForIdle()
     },
   }
 }
