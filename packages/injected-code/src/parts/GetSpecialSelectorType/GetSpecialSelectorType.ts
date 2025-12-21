c440bf9a00f10210d5d843d2167f9d411d9b23ce
Simon Siefke <simon.siefke@gmail.com>
2025-12-21 17:50:47 +0300

    feature: sort imports (#2271)

--- packages/injected-code/src/parts/GetSpecialSelectorType/GetSpecialSelectorType.ts
+++ packages/injected-code/src/parts/GetSpecialSelectorType/GetSpecialSelectorType.ts
@@ -3,16 +3,16 @@ import * as SpecialSelectorPrefix from '../SpecialSelectorPrefix/SpecialSelector
 
 export const getSpecialSelectorType = (specialSelector) => {
   switch (specialSelector) {
-    case SpecialSelectorPrefix.HasText:
-      return SelectorType.Text
-    case SpecialSelectorPrefix.Nth:
-      return SelectorType.Nth
-    case SpecialSelectorPrefix.InternalEnterFrame:
-      return SelectorType.InternalEnterFrame
     case SpecialSelectorPrefix.EnterShadow:
       return SelectorType.EnterShadow
     case SpecialSelectorPrefix.HasExactText:
       return SelectorType.ExactText
+    case SpecialSelectorPrefix.HasText:
+      return SelectorType.Text
+    case SpecialSelectorPrefix.InternalEnterFrame:
+      return SelectorType.InternalEnterFrame
+    case SpecialSelectorPrefix.Nth:
+      return SelectorType.Nth
     default:
       return SelectorType.None
   }
