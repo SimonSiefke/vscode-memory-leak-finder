df62844e210dba1a34e54de6fb16554823a06417
Simon Siefke <simon.siefke@gmail.com>
2025-12-21 17:28:46 +0300

    Discard changes to packages/proxy-worker/src/parts/SanitizeFilename/SanitizeFilename.ts

--- packages/proxy-worker/src/parts/SanitizeFilename/SanitizeFilename.ts
+++ packages/proxy-worker/src/parts/SanitizeFilename/SanitizeFilename.ts
@@ -1,3 +1,3 @@
 export const sanitizeFilename = (url: string): string => {
-  return url.replaceAll(/[^a-zA-Z0-9]/g, '_').slice(0, 200)
+  return url.replace(/[^a-zA-Z0-9]/g, '_').substring(0, 200)
 }
