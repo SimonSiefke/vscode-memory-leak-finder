9a57b4f13ef23aa781e8976622f9232b70e4b6f0
Simon Siefke <simon.siefke@gmail.com>
2025-12-21 17:21:43 +0300

    feature: add session types (#2264)

--- packages/memory-leak-finder/src/parts/AddStackTracesToEventListeners/AddStackTracesToEventListeners.ts
+++ packages/memory-leak-finder/src/parts/AddStackTracesToEventListeners/AddStackTracesToEventListeners.ts
@@ -1,6 +1,7 @@
 import * as AddStackTraceToEventListener from '../AddStackTraceToEventListener/AddStackTraceToEventListener.ts'
+import type { Session } from '../Session/Session.ts'
 
-export const addStackTracesToEventListeners = async (session, leakingEventListeners) => {
+export const addStackTracesToEventListeners = async (session: Session, leakingEventListeners: readonly any[]) => {
   const promises: Promise<any>[] = []
   // TODO could optimize this by doing one call and returning all matching stack traces from it
   for (const leakingEventListener of leakingEventListeners) {
