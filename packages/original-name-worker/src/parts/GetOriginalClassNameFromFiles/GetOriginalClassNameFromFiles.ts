9f42e65eeff61481dba41a41bfc89220a493018a
Simon Siefke <simon.siefke@gmail.com>
2025-12-22 21:50:44 +0100

    fix property

--- packages/original-name-worker/src/parts/GetOriginalClassNameFromFiles/GetOriginalClassNameFromFiles.ts
+++ packages/original-name-worker/src/parts/GetOriginalClassNameFromFiles/GetOriginalClassNameFromFiles.ts
@@ -3,7 +3,7 @@ import { getOriginalClassNameFromFile } from '../GetOriginalClassNameFromFile/Ge
 export const getOriginalClassNameFromFiles = async (items: readonly any[]) => {
   const mapped: string[] = []
   for (const item of items) {
-    const resolved = await getOriginalClassNameFromFile(item.originalCodePath, item.originalLine, item.originalColumn)
+    const resolved = await getOriginalClassNameFromFile(item.codePath, item.originalLine, item.originalColumn)
     mapped.push(resolved)
   }
   return mapped
