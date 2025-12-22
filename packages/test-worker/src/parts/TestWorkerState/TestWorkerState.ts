34c0b519a7a4b8ed40e05a399f39c2b697259bc6
Simon Siefke <simon.siefke@gmail.com>
2025-12-22 19:37:21 +0100

    Delete packages/test-worker/src/parts/TestWorkerState/TestWorkerState.ts

--- packages/test-worker/src/parts/TestWorkerState/TestWorkerState.ts
+++
@@ -1,9 +0,0 @@
-let inspectExtensions = false
-
-export const setInspectExtensions = (value: boolean): void => {
-  inspectExtensions = value
-}
-
-export const getInspectExtensions = (): boolean => {
-  return inspectExtensions
-}
