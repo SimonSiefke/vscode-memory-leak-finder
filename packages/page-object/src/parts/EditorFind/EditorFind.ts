c9213bf54b825676339f76d5556d95313ce1b145
Simon Siefke <simon.siefke@gmail.com>
2025-12-22 05:45:21 +0300

    feature: add find e2e test (#2300)

---
+++ packages/page-object/src/parts/EditorFind/EditorFind.ts
@@ -0,0 +1,70 @@
+export const create = ({ page, expect, VError, ideVersion }) => {
+  return {
+    async setSearchValue(value: string) {
+      try {
+        await page.waitForIdle()
+        const input = page.locator('.find-part .monaco-findInput textarea[aria-label="Find"]')
+        await input.setValue(value)
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to set search value`)
+      }
+    },
+    async openReplace() {
+      try {
+        const findWidget = page.locator('.find-widget.visible')
+        await page.waitForIdle()
+        const toggleReplace = findWidget.locator('[aria-label="Toggle Replace"]')
+        await expect(toggleReplace).toBeVisible()
+        const expanded = await toggleReplace.getAttribute('aria-expanded')
+        if (expanded === 'true') {
+          return
+        }
+        await page.waitForIdle()
+        await toggleReplace.click()
+        await page.waitForIdle()
+        await expect(toggleReplace).toHaveAttribute('aria-expanded', 'true')
+        await page.waitForIdle()
+        const replace = page.locator('.replace-part .monaco-findInput textarea[aria-label="Replace"]')
+        await expect(replace).toBeVisible()
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to open replace`)
+      }
+    },
+    async setReplaceValue(value: string) {
+      try {
+        await page.waitForIdle()
+        const findWidget = page.locator('.find-widget.visible')
+        await expect(findWidget).toBeVisible()
+        await page.waitForIdle()
+        const toggleReplace = findWidget.locator('[aria-label="Toggle Replace"]')
+        await expect(toggleReplace).toBeVisible()
+        await page.waitForIdle()
+        const replace = page.locator('.replace-part .monaco-findInput textarea[aria-label="Replace"]')
+        await expect(replace).toBeVisible()
+        await page.waitForIdle()
+        await replace.focus()
+        await page.waitForIdle()
+        await replace.setValue(value)
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to set replace value`)
+      }
+    },
+    async replace() {
+      try {
+        await page.waitForIdle()
+        const findWidget = page.locator('.find-widget.visible')
+        await expect(findWidget).toBeVisible()
+        await page.waitForIdle()
+        const button = findWidget.locator('[aria-label^="Replace All"][tabIndex="0"]')
+        await expect(button).toBeVisible()
+        await button.click()
+        await page.waitForIdle()
+      } catch (error) {
+        throw new VError(error, `Failed to replace`)
+      }
+    },
+  }
+}
