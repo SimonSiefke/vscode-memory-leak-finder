7a71c59ba5e6de827b12e3b4711f688fcad74722
Simon Siefke <simon.siefke@gmail.com>
2025-12-22 12:01:09 +0100

    feature: add source control view as tree

---
+++ packages/e2e/src/source-control.view-as-tree.ts
@@ -0,0 +1,23 @@
+import type { TestContext } from '../types.js'
+
+export const skip = true
+
+export const setup = async ({ ActivityBar, Editor, Explorer, Workspace, SourceControl }: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      content: '<h1>hello world</h1>',
+      name: 'index.html',
+    },
+  ])
+  await Workspace.initializeGitRepository()
+  await Editor.closeAll()
+  await Explorer.focus()
+  await ActivityBar.showSourceControl()
+  await SourceControl.shouldHaveUnstagedFile('index.html')
+  await SourceControl.stageFile('index.html')
+}
+
+export const run = async ({ SourceControl }: TestContext): Promise<void> => {
+  await SourceControl.viewAsTree()
+  await SourceControl.viewAsList()
+}
