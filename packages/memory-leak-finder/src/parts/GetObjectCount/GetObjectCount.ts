cdbd3ea85f6384253dc7b936f01857455f5de66e
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 21:33:00 +0100

    tsc

--- packages/memory-leak-finder/src/parts/GetObjectCount/GetObjectCount.ts
+++ packages/memory-leak-finder/src/parts/GetObjectCount/GetObjectCount.ts
@@ -2,7 +2,7 @@ import type { Session } from '../Session/Session.ts'
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 import * as GetRemoteObjectLength from '../GetRemoteObjectLength/GetRemoteObjectLength.ts'
 
-export const getObjectCount = async (session: Session, prototype: string, objectGroup: string | undefined = undefined): Promise<number> => {
+export const getObjectCount = async (session: Session, prototype: string, objectGroup: string): Promise<number> => {
   const prototypeDescriptor = await DevtoolsProtocolRuntime.evaluate(session, {
     expression: prototype,
     returnByValue: false,
