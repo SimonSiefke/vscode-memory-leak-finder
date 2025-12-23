a96059fe99c4997ccdf71a3e444f8bd52a529772
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 22:39:45 +0100

    fn

--- packages/memory-leak-finder/src/parts/MeasureMapCount/MeasureMapCount.ts
+++ packages/memory-leak-finder/src/parts/MeasureMapCount/MeasureMapCount.ts
@@ -1,41 +1,27 @@
-import type { IScriptHandler } from '../IScriptHandler/IScriptHandler.ts'
+import * as CompareCount from '../CompareCount/CompareCount.ts'
 import * as GetMapCount from '../GetMapCount/GetMapCount.ts'
+import * as IsLeakCount from '../IsLeakCount/IsLeakCount.ts'
 import * as MeasureId from '../MeasureId/MeasureId.ts'
 import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.ts'
-import * as ScriptHandler from '../ScriptHandler/ScriptHandler.ts'
 import * as TargetId from '../TargetId/TargetId.ts'
-import * as WriteScriptMap from '../WriteScriptMap/WriteScriptMap.ts'
 
 export const id = MeasureId.MapCount
 
-export const targets = [TargetId.Browser]
+export const targets = [TargetId.Browser, TargetId.Node, TargetId.Worker]
 
 export const create = (session) => {
   const objectGroup = ObjectGroupId.create()
-  const scriptHandler = ScriptHandler.create()
-  return [session, objectGroup, scriptHandler]
+  return [session, objectGroup]
 }
 
-export const start = async (session, objectGroup, scriptHandler: IScriptHandler) => {
-  await scriptHandler.start(session)
-  const id = 0
-  const result = await GetMapCount.getMapCount(session, objectGroup, id)
-  await WriteScriptMap.writeScriptMap(scriptHandler.scriptMap, id)
-  return result
+export const start = (session, objectGroup) => {
+  return GetMapCount.getMapCount(session)
 }
 
-export const stop = async (session, objectGroup, scriptHandler: IScriptHandler) => {
-  const id = 1
-  const result = await GetMapCount.getMapCount(session, objectGroup, id)
-  await WriteScriptMap.writeScriptMap(scriptHandler.scriptMap, id)
-  await scriptHandler.stop(session)
-  return result
+export const stop = (session, objectGroup) => {
+  return GetMapCount.getMapCount(session)
 }
 
-export const compare = (before, after) => {
-  return { after, before }
-}
+export const compare = CompareCount.compareCount
 
-export const isLeak = ({ after, before }) => {
-  return after > before
-}
+export const isLeak = IsLeakCount.isLeakCount
