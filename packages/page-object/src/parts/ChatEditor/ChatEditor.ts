37c9427e684b080fdb0fe63840ed6b2238e47658
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 12:34:29 +0100

    tsc

--- packages/page-object/src/parts/ChatEditor/ChatEditor.ts
+++ packages/page-object/src/parts/ChatEditor/ChatEditor.ts
@@ -139,16 +139,8 @@ export const create = ({ expect, ideVersion, page, VError }) => {
         await page.waitForIdle()
         await expect(lines).toHaveText('')
         await page.waitForIdle()
-<<<<<<< HEAD
-        const requestMessage = chatView.locator(`.monaco-list-row.request[aria-label="${message}"]`)
-        await expect(requestMessage).toBeVisible()
-        await page.waitForIdle()
-
-        if (expectedResponse) {
-=======
 
         if (verify) {
->>>>>>> origin/main
           const row = chatView.locator(`.monaco-list-row[aria-label="${message}"]`)
           await expect(row).toBeVisible()
           await page.waitForIdle()
