d33239714f904e0c5159e250e230a20ae885aeb0
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 12:42:16 +0300

    feature: use one rpc call to compute original names (#2308)

--- packages/original-name-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/original-name-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import * as GetOriginalClassName from '../GetOriginalClassName/GetOriginalClassName.ts'
 import * as GetOriginalClassNameFromFile from '../GetOriginalClassNameFromFile/GetOriginalClassNameFromFile.ts'
+import { getOriginalClassNameFromFiles } from '../GetOriginalClassNameFromFiles/GetOriginalClassNameFromFiles.ts'
 
 export const commandMap: Record<string, any> = {
   'OriginalName.getOriginalName': GetOriginalClassName.getOriginalClassName,
   'OriginalName.getOriginalNameFromFile': GetOriginalClassNameFromFile.getOriginalClassNameFromFile,
+  'OriginalName.getOriginalNameFromFiles': getOriginalClassNameFromFiles,
 }
--- packages/source-map-worker/src/parts/AddOriginalPositions/AddOriginalPositions.ts
+++ packages/source-map-worker/src/parts/AddOriginalPositions/AddOriginalPositions.ts
@@ -2,27 +2,25 @@ import type { IntermediateItem } from '../IntermediateItem/IntermediateItem.ts'
 import type { OriginalPosition } from '../OriginalPosition/OriginalPosition.ts'
 import * as OriginalNameWorker from '../OriginalNameWorker/OriginalNameWorker.ts'
 
+const getOriginalNames = async (items: readonly IntermediateItem[]): Promise<readonly string[]> => {
+  await using rpc = await OriginalNameWorker.create()
+  const originalNames = await rpc.invoke('OriginalName.getOriginalNameFromFiles', items)
+  return originalNames
+}
+
 // TODO rename to addoriginalNames
 export const addOriginalPositions = async (intermediateItems: readonly IntermediateItem[]): Promise<readonly OriginalPosition[]> => {
-  await using rpc = await OriginalNameWorker.create()
+  const names = await getOriginalNames(intermediateItems)
   const finalResults: OriginalPosition[] = []
-  for (const item of intermediateItems) {
-    if (item.codePath && item.line !== null && item.column !== null) {
-      const originalName: string = await rpc.invoke('OriginalName.getOriginalNameFromFile', item.codePath, item.line, item.column)
-      finalResults.push({
-        column: item.column,
-        line: item.line,
-        name: originalName || item.name || '',
-        source: item.source,
-      })
-    } else {
-      finalResults.push({
-        column: item.column,
-        line: item.line,
-        name: item.name,
-        source: item.source,
-      })
-    }
+  for (let i = 0; i < intermediateItems.length; i++) {
+    const item = intermediateItems[i]
+    const originalName = names[i]
+    finalResults.push({
+      column: item.column,
+      line: item.line,
+      name: originalName || item.name || '',
+      source: item.source,
+    })
   }
   return finalResults
 }
