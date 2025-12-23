8da47ab95cd4465373a210d62ad28755d3cc8a2c
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 13:14:30 +0100

    unzip

--- packages/compression-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/compression-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import { decompressBody } from '../DecompressBody/DecompressBody.ts'
+import { unzip } from '../Unzip/Unzip.ts'
 
 export const commandMap = {
   'Compression.decompressBody': decompressBody,
+  'Compression.unzip': unzip,
 }
