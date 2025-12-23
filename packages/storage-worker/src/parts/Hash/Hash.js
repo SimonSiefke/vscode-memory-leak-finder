6b8a26eb785f9d2bddff80dff7005aac7fbf15a9
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 22:06:24 +0300

    feature: use stricter tsconfig for storage worker (#2339)

--- packages/storage-worker/src/parts/Hash/Hash.js
+++ packages/storage-worker/src/parts/Hash/Hash.js
@@ -1,5 +1,8 @@
 import { createHash } from 'node:crypto'
 
+/**
+ * @param {any} object
+ */
 export const hash = (object) => {
   const string = JSON.stringify(object)
   const hash = createHash('sha1')
