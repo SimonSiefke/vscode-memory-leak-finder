1b5a476127f0b102587edad4209c94929a19356b
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 21:35:45 +0100

    tsc

--- packages/memory-leak-finder/src/parts/MeasureCanvasRenderingContext2dCount/MeasureCanvasRenderingContext2dCount.ts
+++ packages/memory-leak-finder/src/parts/MeasureCanvasRenderingContext2dCount/MeasureCanvasRenderingContext2dCount.ts
@@ -2,6 +2,7 @@ import * as CompareCount from '../CompareCount/CompareCount.ts'
 import * as GetCanvasRenderingContext2dCount from '../GetCanvasRenderingContext2dCount/GetCanvasRenderingContext2dCount.ts'
 import * as IsLeakCount from '../IsLeakCount/IsLeakCount.ts'
 import * as MeasureId from '../MeasureId/MeasureId.ts'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.ts'
 import * as TargetId from '../TargetId/TargetId.ts'
 import type { Session } from '../Session/Session.ts'
 
@@ -10,15 +11,16 @@ export const id = MeasureId.CanvasRenderingContext2dCount
 export const targets = [TargetId.Browser]
 
 export const create = (session: Session) => {
-  return [session]
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
 }
 
-export const start = (session: Session) => {
-  return GetCanvasRenderingContext2dCount.getCanvasRenderingContext2dCount(session)
+export const start = (session: Session, objectGroup: string) => {
+  return GetCanvasRenderingContext2dCount.getCanvasRenderingContext2dCount(session, objectGroup)
 }
 
-export const stop = (session: Session) => {
-  return GetCanvasRenderingContext2dCount.getCanvasRenderingContext2dCount(session)
+export const stop = (session: Session, objectGroup: string) => {
+  return GetCanvasRenderingContext2dCount.getCanvasRenderingContext2dCount(session, objectGroup)
 }
 
 export const compare = CompareCount.compareCount
--- packages/memory-leak-finder/src/parts/MeasureEditContextCount/MeasureEditContextCount.ts
+++ packages/memory-leak-finder/src/parts/MeasureEditContextCount/MeasureEditContextCount.ts
@@ -2,6 +2,7 @@ import * as CompareCount from '../CompareCount/CompareCount.ts'
 import * as GetEditContextCount from '../GetEditContextCount/GetEditContextCount.ts'
 import * as IsLeakCount from '../IsLeakCount/IsLeakCount.ts'
 import * as MeasureId from '../MeasureId/MeasureId.ts'
+import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.ts'
 import * as TargetId from '../TargetId/TargetId.ts'
 import type { Session } from '../Session/Session.ts'
 
@@ -10,15 +11,16 @@ export const id = MeasureId.EditContextCount
 export const targets = [TargetId.Browser]
 
 export const create = (session: Session) => {
-  return [session]
+  const objectGroup = ObjectGroupId.create()
+  return [session, objectGroup]
 }
 
-export const start = (session: Session) => {
-  return GetEditContextCount.getEditContextCount(session)
+export const start = (session: Session, objectGroup: string) => {
+  return GetEditContextCount.getEditContextCount(session, objectGroup)
 }
 
-export const stop = (session: Session) => {
-  return GetEditContextCount.getEditContextCount(session)
+export const stop = (session: Session, objectGroup: string) => {
+  return GetEditContextCount.getEditContextCount(session, objectGroup)
 }
 
 export const compare = CompareCount.compareCount
