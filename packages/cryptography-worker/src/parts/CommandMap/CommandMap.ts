9da0299a47df8cfe10082f69110005d601e106ac
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 19:25:45 +0100

    add fn

--- packages/cryptography-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/cryptography-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,6 @@
 import { generateCA } from '../GenerateCA/GenerateCA.ts'
 import { generateCertificateForDomain } from '../GenerateCertificateForDomain/GenerateCertificateForDomain.ts'
+import { getCertificateForDomain } from '../GetCertificateForDomain/GetCertificateForDomain.ts'
 import { getEcdsaKeyPair } from '../GetEcdsaKeyPair/GetEcdsaKeyPair.ts'
 import { getRsaKeyPair } from '../GetRsaKeyPair/GetRsaKeyPair.ts'
 import { replaceJwtToken } from '../ReplaceJwtToken/ReplaceJwtToken.ts'
@@ -7,6 +8,7 @@ import { replaceJwtToken } from '../ReplaceJwtToken/ReplaceJwtToken.ts'
 export const commandMap = {
   'Cryptography.generateCA': generateCA,
   'Cryptography.generateCertificateForDomain': generateCertificateForDomain,
+  'Cryptography.getCertificateForDomain': getCertificateForDomain,
   'Cryptography.getEcdsaKeyPair': getEcdsaKeyPair,
   'Cryptography.getRsaKeyPair': getRsaKeyPair,
   'Cryptography.replaceJwtToken': replaceJwtToken,
