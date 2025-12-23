ef76156347545c3c3919808eaf3b02638e05bfb6
Simon Siefke <simon.siefke@gmail.com>
2025-12-23 15:09:40 +0100

    feature: modernize dirname

--- packages/initialization-worker/src/parts/Root/Root.ts
+++ packages/initialization-worker/src/parts/Root/Root.ts
@@ -1,6 +1,6 @@
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = import.meta.dirname
 
 export const root = join(__dirname, '..', '..', '..', '..', '..')
--- packages/memory-leak-finder/src/parts/Root/Root.ts
+++ packages/memory-leak-finder/src/parts/Root/Root.ts
@@ -1,6 +1,6 @@
 import path, { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = import.meta.dirname
 
 export const root = path.join(__dirname, '../../../../../')
