5047b5354991932a104cc024f064981875ac3301
Simon Siefke <simon.siefke@gmail.com>
2025-12-24 15:22:23 +0100

    dat

--- packages/test-coordinator/src/parts/FetchAllInsidersVersions/FetchAllInsidersVersions.ts
+++ packages/test-coordinator/src/parts/FetchAllInsidersVersions/FetchAllInsidersVersions.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import * as FetchCommits from '../FetchCommits/FetchCommits.ts'
-=======
 import * as os from 'node:os'
 import * as FetchVscodeApi from '../FetchVscodeApi/FetchVscodeApi.ts'
 import * as GetVscodePlatformName from '../GetVscodePlatformName/GetVscodePlatformName.ts'
->>>>>>> origin/main
 
 export interface IVersionMetadata {
   readonly commit: string
@@ -15,7 +12,6 @@ export interface IVersionMetadata {
 }
 
 export const fetchAllInsidersVersions = async (): Promise<IVersionMetadata[]> => {
-<<<<<<< HEAD
   const commits = await FetchCommits.fetchCommits()
   const versions: IVersionMetadata[] = commits.map((commit) => ({
     commit: commit.commit,
@@ -24,11 +20,5 @@ export const fetchAllInsidersVersions = async (): Promise<IVersionMetadata[]> =>
     url: '',
     version: '',
   }))
-=======
-  const platformName = GetVscodePlatformName.getVscodePlatformName(process.platform, os.arch())
-  const quality = 'insider'
-  const url = `https://update.code.visualstudio.com/api/versions/${platformName}/${quality}`
-  const versions = await FetchVscodeApi.fetchVscodeApi<IVersionMetadata[]>(url)
->>>>>>> origin/main
   return versions
 }
