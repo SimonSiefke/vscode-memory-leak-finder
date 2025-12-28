d5b6bc8cf7954a306eaf7bf242c06a009091e2dc
Simon Siefke <simon.siefke@gmail.com>
2025-12-29 00:43:24 +0300

    feature: move source map worker launch into a separate file (#2410)

--- packages/memory-leak-finder/src/parts/GetCleanPositionsMap/GetCleanPositionsMap.ts
+++ packages/memory-leak-finder/src/parts/GetCleanPositionsMap/GetCleanPositionsMap.ts
@@ -1,27 +1,7 @@
-import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
-import { join } from 'node:path'
-import * as Root from '../Root/Root.ts'
-
-const sourceMapWorkerPath = join(Root.root, 'packages', 'source-map-worker', 'src', 'sourceMapWorkerMain.ts')
-
-const launchSourceMapWorker = async () => {
-  const rpc = await NodeWorkerRpcParent.create({
-    commandMap: {},
-    path: sourceMapWorkerPath,
-    stdio: 'inherit',
-  })
-  return {
-    invoke(method, ...params) {
-      return rpc.invoke(method, ...params)
-    },
-    async [Symbol.asyncDispose]() {
-      await rpc.dispose()
-    },
-  }
-}
+import * as LaunchSourceMapWorker from '../LaunchSourceMapWorker/LaunchSourceMapWorker.ts'
 
 export const getCleanPositionsMap = async (sourceMapUrlMap, classNames) => {
-  await using rpc = await launchSourceMapWorker()
+  await using rpc = await LaunchSourceMapWorker.launchSourceMapWorker()
   const response = await rpc.invoke('SourceMap.getCleanPositionsMap', sourceMapUrlMap, classNames)
   return response
 }
