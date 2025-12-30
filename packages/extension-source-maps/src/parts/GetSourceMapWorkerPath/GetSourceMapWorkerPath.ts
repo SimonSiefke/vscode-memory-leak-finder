38e66c400d56215745f586e41772160a0d348ecd
Simon Siefke <simon.siefke@gmail.com>
2025-12-30 23:09:35 +0100

    fix

--- packages/extension-source-maps/src/parts/GetSourceMapWorkerPath/GetSourceMapWorkerPath.ts
+++ packages/extension-source-maps/src/parts/GetSourceMapWorkerPath/GetSourceMapWorkerPath.ts
@@ -2,6 +2,6 @@ import { resolve } from 'node:path'
 import { root } from '../Root/Root.ts'
 
 export const getSourceMapWorkerPath = (): string => {
-  const sourceMapWorkerPath: string = resolve(root, 'source-map-worker/src/sourceMapWorkerMain.ts')
+  const sourceMapWorkerPath: string = resolve(root, 'packages/source-map-worker/src/sourceMapWorkerMain.ts')
   return sourceMapWorkerPath
 }
