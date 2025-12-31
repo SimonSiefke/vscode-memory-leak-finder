48ad1129241d3d2f7f65cd652a78064699c4cb17
Simon Siefke <simon.siefke@gmail.com>
2025-12-31 13:52:10 +0100

    rm

--- packages/test-worker/src/parts/PlatformState/PlatformState.ts
+++
@@ -1,9 +0,0 @@
-let platform: string = ''
-
-export const setPlatform = (newPlatform: string): void => {
-  platform = newPlatform
-}
-
-export const getPlatform = (): string => {
-  return platform
-}
