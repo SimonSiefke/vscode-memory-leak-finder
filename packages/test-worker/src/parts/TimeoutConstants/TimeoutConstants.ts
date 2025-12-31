24baec0660b91d2ac0323dc5c93e2f84d7b41585
Simon Siefke <simon.siefke@gmail.com>
2025-12-31 13:50:57 +0100

    fix

--- packages/test-worker/src/parts/TimeoutConstants/TimeoutConstants.ts
+++ packages/test-worker/src/parts/TimeoutConstants/TimeoutConstants.ts
@@ -1,19 +1,1 @@
-import * as IsWindows from '../IsWindows/IsWindows.ts'
-import * as PlatformState from '../PlatformState/PlatformState.ts'
-
-export const getAttachToPage = (): number => {
-  const platform = PlatformState.getPlatform()
-  return IsWindows.isWindows(platform) ? 5000 : 1000
-}
-
-const DefaultExecutionContext = 500
 export const PageEvent = 2000
-const SessionState = 500
-const Target = 8000
-
-export const getUtilityExecutionContext = (): number => {
-  const platform = PlatformState.getPlatform()
-  return IsWindows.isWindows(platform) ? 16_000 : 8500
-}
-
-const InteractiveTime = 120_000
