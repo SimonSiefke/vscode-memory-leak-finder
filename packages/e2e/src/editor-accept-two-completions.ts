eddf8667957978647a1cb7bd72f727f0d4f2f688
Simon Siefke <simon.siefke@gmail.com>
2026-01-01 14:36:48 +0300

    feature: add accept multiple completions e2e test (#2465)

---
+++ packages/e2e/src/editor-accept-two-completions.ts
@@ -0,0 +1,48 @@
+import type { TestContext } from '../types.js'
+
+export const skip = 1
+
+export const setup = async ({ Editor, Workspace, SideBar }: TestContext): Promise<void> => {
+  await SideBar.hide()
+  await Workspace.setFiles([
+    {
+      content: `h1 {
+  visibil
+}`,
+      name: 'index.css',
+    },
+  ])
+  await Editor.open('index.css')
+  await Editor.shouldHaveText(`h1 {
+  visibil
+}`)
+  await Editor.shouldHaveBreadCrumb('h1')
+  // @ts-ignore
+  await Editor.setCursor(2, 8)
+}
+
+export const run = async ({ Editor, Suggest }: TestContext): Promise<void> => {
+  // @ts-ignore
+  await Suggest.open('visibility, Property')
+  // @ts-ignore
+  await Suggest.accept('visibility, Property')
+  const space = ' '
+  await Editor.shouldHaveText(`h1 {
+  visibility:${space}
+}`)
+  // @ts-ignore
+  await Suggest.accept(`hidden, Value`)
+  await Editor.shouldHaveText(`h1 {
+  visibility: hidden
+}`)
+  await Editor.undo()
+  await Editor.undo()
+  await Editor.shouldHaveText(`h1 {
+  visibil
+}`)
+}
+
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.save({ viaKeyBoard: true })
+  await Editor.closeAll()
+}
