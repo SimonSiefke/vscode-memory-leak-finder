20babe1e9c66fb4cf9bc708fcf091ae168c73649
Simon Siefke <simon.siefke@gmail.com>
2026-01-01 15:46:48 +0100

    better handle empty sourcemap

--- packages/load-source-map-worker/src/parts/LoadSourceMapFromFile/LoadSourceMapFromFile.ts
+++ packages/load-source-map-worker/src/parts/LoadSourceMapFromFile/LoadSourceMapFromFile.ts
@@ -1,9 +1,14 @@
 import { readFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
+import { emptySourceMap } from '../EmptySourceMap/EmptySourceMap.ts'
 
 export const loadSourceMap = async (url: string): Promise<any> => {
-  const path = fileURLToPath(url)
-  const content = await readFile(path, 'utf8')
-  const data = JSON.parse(content)
-  return data
+  try {
+    const path = fileURLToPath(url)
+    const content = await readFile(path, 'utf8')
+    const data = JSON.parse(content)
+    return data
+  } catch {
+    return emptySourceMap
+  }
 }
