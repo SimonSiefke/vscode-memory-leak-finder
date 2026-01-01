c504402fde1a75eeedd8e861af60d760a70606e5
Simon Siefke <simon.siefke@gmail.com>
2026-01-01 12:49:41 +0300

    feature: ask stdout worker for initializing message (#2460)

--- packages/cli/src/parts/GetInitializingMessage/GetInitializingMessage.ts
+++ packages/cli/src/parts/GetInitializingMessage/GetInitializingMessage.ts
@@ -1,7 +1,6 @@
-import { Initialize } from '../TestPrefix/TestPrefix.ts'
+import * as StdoutWorker from '../StdoutWorker/StdoutWorker.ts'
 
 // TODO when initialization is done, clear the intializing message and print instead that initialization is done with the time it took
 export const getInitializingMessage = async (): Promise<string> => {
-  const messageFileName = Initialize
-  return `${messageFileName}\n`
+  return StdoutWorker.invoke('Stdout.getInitializingMessage')
 }
