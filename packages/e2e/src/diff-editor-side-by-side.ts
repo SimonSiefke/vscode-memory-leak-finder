0a97a4d0af45e015a7a97c48829d7ee383f3b8ec
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 01:26:20 +0300

    feature: improve diff editor test (#2478)

--- packages/e2e/src/diff-editor-side-by-side.ts
+++ packages/e2e/src/diff-editor-side-by-side.ts
@@ -23,22 +23,28 @@ export const setup = async ({ Editor, Explorer, Workspace }: TestContext): Promi
   ])
   await Editor.closeAll()
   await Explorer.focus()
+  await Explorer.refresh()
   await Explorer.shouldHaveItem('a.txt')
   await Explorer.shouldHaveItem('b.txt')
   await Explorer.shouldHaveItem('c.txt')
   await Explorer.shouldHaveItem('d.txt')
 }
 
-// @ts-ignore
-export const run = async ({ DiffEditor, Editor, expect, page }: TestContext): Promise<void> => {
-  await DiffEditor.open('a.txt', 'b.txt')
-  await DiffEditor.expectOriginal('a')
-  await DiffEditor.expectModified('b')
-  await Editor.splitRight()
-  await DiffEditor.open('c.txt', 'd.txt')
-  await DiffEditor.expectOriginal('c')
-  await DiffEditor.expectModified('d')
-  const diffEditors = page.locator('.diff-editor')
-  await expect(diffEditors).toHaveCount(2)
+export const run = async ({ DiffEditor, Editor }: TestContext): Promise<void> => {
+  // @ts-ignore
+  await DiffEditor.open({
+    file1: 'a.txt',
+    file2: 'b.txt',
+    file1Content: 'a',
+    file2Content: 'b',
+  })
+  await Editor.splitDown()
+  // @ts-ignore
+  await DiffEditor.open({
+    file1: 'c.txt',
+    file2: 'd.txt',
+    file1Content: 'c',
+    file2Content: 'd',
+  })
   await Editor.closeAll()
 }
