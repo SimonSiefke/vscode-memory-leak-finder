964dd3f5b929d85b413b6ef904dd347c0043c9c2
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 14:53:53 +0100

    e2e

--- packages/e2e/src/extension-editor-side-by-side.ts
+++ packages/e2e/src/extension-editor-side-by-side.ts
@@ -2,20 +2,30 @@ import type { TestContext } from '../types.ts'
 
 export const skip = 1
 
-export const setup = async ({ Editor, Extensions }: TestContext): Promise<void> => {
+export const setup = async ({ Editor, Extensions, SideBar, Workspace }: TestContext): Promise<void> => {
   await Editor.closeAll()
+  await Workspace.setFiles([])
+  await SideBar.hide()
+  // @ts-ignore
+  await Editor.splitRight({ groupCount: 0 })
+  await Editor.focusLeftEditorGroup()
   await Extensions.show()
-  await Extensions.search('@builtin html')
-  await Extensions.first.shouldBe('HTML Language Basics')
+  await Extensions.search('@builtin basics')
+  await Extensions.first.shouldBe('C/C++ Language Basics')
 }
 
-export const run = async ({ Editor, ExtensionDetailView, Extensions }: TestContext): Promise<void> => {
+export const run = async ({ Editor, ExtensionDetailView, Extensions, SideBar }: TestContext): Promise<void> => {
   await Extensions.first.click()
-  await ExtensionDetailView.shouldHaveHeading('HTML Language Basics')
-  await Editor.splitRight()
-  await Extensions.search('@builtin typescript')
-  await Extensions.first.shouldBe('TypeScript Language Basics')
-  await Extensions.first.click()
+  await ExtensionDetailView.shouldHaveHeading('C/C++ Language Basics')
+  await Editor.focusRightEditorGroup()
+  // @ts-ignore
+  await Extensions.second.click()
+  // await SideBar.hide()
+  // await Extensions.show()
+  // await Extensions.search('@builtin typescript')
+  // await Extensions.first.shouldBe('TypeScript Language Basics')
+  await new Promise((r) => {})
+  // TODO second editor
   await ExtensionDetailView.shouldHaveHeading('TypeScript Language Basics')
   await Editor.closeAll()
 }
