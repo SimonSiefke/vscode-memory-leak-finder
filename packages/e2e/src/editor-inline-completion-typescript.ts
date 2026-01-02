f075059914143ab7086ca2121a218a46651b15ac
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 14:05:44 +0100

    ts

---
+++ packages/e2e/src/editor-inline-completion-typescript.ts
@@ -0,0 +1,46 @@
+import type { TestContext } from '../types.js'
+
+export const skip = 1
+
+export const setup = async ({ Editor, Extensions, Workspace, SideBar }: TestContext): Promise<void> => {
+  await Editor.closeAll()
+  await Workspace.setFiles([
+    {
+      content: 'a',
+      name: 'test.ts',
+    },
+  ])
+  // @ts-ignore
+  await Extensions.add({
+    path: '.vscode-extensions-source/inline-completion-provider',
+    expectedName: 'inline-completion-provider',
+  })
+  await Editor.open('test.ts')
+  await Editor.shouldHaveBreadCrumb('test.ts')
+  await Extensions.show()
+  await Extensions.search(`@id:e2e-tests.inline-completion-provider`)
+  await Extensions.first.shouldHaveActivationTime()
+  await SideBar.hide()
+  // @ts-ignore
+  await Editor.waitforTextFileReady('test.ts')
+  await Editor.shouldHaveText('a')
+  await Editor.deleteAll()
+  await Editor.save({ viaKeyBoard: true })
+}
+
+export const run = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.shouldHaveText('')
+  await Editor.type('a')
+  // @ts-ignore
+  await Editor.shouldHaveInlineCompletion('bcdef')
+  // @ts-ignore
+  await Editor.acceptInlineCompletion()
+  await Editor.shouldHaveText('abcdef')
+  await Editor.deleteAll()
+  await Editor.save({ viaKeyBoard: true })
+}
+
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.save({ viaKeyBoard: true })
+  await Editor.closeAll()
+}
