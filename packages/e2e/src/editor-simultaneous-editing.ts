8461b5ee936838fe3a49f45e2ab40a0e6fa6a374
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 11:54:33 +0100

    e2e

--- packages/e2e/src/editor-simultaneous-editing.ts
+++ packages/e2e/src/editor-simultaneous-editing.ts
@@ -10,25 +10,31 @@ export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> =
   await Editor.closeAll()
   await Editor.open('file.txt')
   await Editor.shouldHaveText('initial text')
+  await Editor.splitRight()
+  await Editor.focusLeftEditorGroup()
 }
 
 export const run = async ({ Editor }: TestContext): Promise<void> => {
-  await Editor.splitRight()
-
-  await Editor.focusLeftEditorGroup()
+  // @ts-ignore
+  await Editor.shouldHaveText('initial text', 'file.txt', 1)
+  // @ts-ignore
+  await Editor.shouldHaveText('initial text', 'file.txt', 2)
   await Editor.type('left ')
-
   // @ts-ignore
   await Editor.shouldHaveText('left initial text', 'file.txt', 1)
   // @ts-ignore
   await Editor.shouldHaveText('left initial text', 'file.txt', 2)
-
   await Editor.focusRightEditorGroup()
   await Editor.type('right ')
-  await new Promise((r) => {})
-  // await page.waitForIdle()
+  // @ts-ignore
+  await Editor.shouldHaveText('right left initial text', 'file.txt', 1)
+  // @ts-ignore
+  await Editor.shouldHaveText('right left initial text', 'file.txt', 2)
+  await Editor.undo()
+  await Editor.undo()
+  await Editor.saveAll()
+}
 
-  // const rightText = 'left right initial text'.replaceAll(' ', nonBreakingSpace)
-  // await expect(leftEditorLines).toHaveText(rightText)
-  // await expect(rightEditorLines).toHaveText(rightText)
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.closeAll()
 }
