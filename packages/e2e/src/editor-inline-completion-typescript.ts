38fc77ccb3e2e488d15f4081db0340b6f51d8181
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 14:06:01 +0100

    e2e

--- packages/e2e/src/editor-inline-completion-typescript.ts
+++ packages/e2e/src/editor-inline-completion-typescript.ts
@@ -2,7 +2,7 @@ import type { TestContext } from '../types.js'
 
 export const skip = 1
 
-export const setup = async ({ Editor, Extensions, Workspace, SideBar }: TestContext): Promise<void> => {
+export const setup = async ({ Editor, Workspace, SideBar }: TestContext): Promise<void> => {
   await Editor.closeAll()
   await Workspace.setFiles([
     {
