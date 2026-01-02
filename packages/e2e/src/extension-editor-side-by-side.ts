18eba40c5ccd6542c05a5b06d8cefc4a10906da1
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 09:11:07 +0100

    feature: add extension editor side by side test

---
+++ packages/e2e/src/extension-editor-side-by-side.ts
@@ -0,0 +1,20 @@
+import type { TestContext } from '../types.ts'
+
+export const setup = async ({ Editor, Extensions }: TestContext): Promise<void> => {
+  await Editor.closeAll()
+  await Extensions.show()
+  await Extensions.search('@builtin html')
+  await Extensions.first.shouldBe('HTML Language Basics')
+}
+
+export const run = async ({ Editor, ExtensionDetailView, Extensions }: TestContext): Promise<void> => {
+  await Extensions.first.click()
+  await ExtensionDetailView.shouldHaveHeading('HTML Language Basics')
+  await Editor.splitRight()
+  await Extensions.search('@builtin typescript')
+  await Extensions.first.shouldBe('TypeScript Language Basics')
+  await Extensions.first.click()
+  await ExtensionDetailView.shouldHaveHeading('TypeScript Language Basics')
+  await Editor.closeAll()
+}
+
