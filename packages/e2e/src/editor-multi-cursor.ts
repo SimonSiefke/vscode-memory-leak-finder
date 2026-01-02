b554c2f322776e5d9dbab7ab3b7099d7e18f653a
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 22:07:12 +0100

    e2e

---
+++ packages/e2e/src/editor-multi-cursor.ts
@@ -0,0 +1,34 @@
+import type { TestContext } from '../types.js'
+
+export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      content: `a
+b
+c`,
+      name: 'file.txt',
+    },
+  ])
+  await Editor.closeAll()
+  await Editor.open('file.txt')
+  await Editor.setCursor(1, 1)
+}
+
+export const run = async ({ Editor }: TestContext): Promise<void> => {
+  // @ts-ignore
+  await Editor.addCursorBelow()
+  // @ts-ignore
+  await Editor.addCursorBelow()
+  await Editor.type('1')
+  await Editor.shouldHaveText(`a1
+b1
+c1`)
+  await Editor.undo()
+  await Editor.shouldHaveText(`a
+b
+c`)
+}
+
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.closeAll()
+}
