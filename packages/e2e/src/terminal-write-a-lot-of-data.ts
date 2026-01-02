7de64281eec478ada0d7534fbd2ff4aaa54b2f9f
Simon Siefke <simon.siefke@gmail.com>
2026-01-02 22:34:59 +0300

    feature: add terminal write e2e test (#2501)

---
+++ packages/e2e/src/terminal-write-a-lot-of-data.ts
@@ -0,0 +1,28 @@
+import type { TestContext } from '../types.js'
+
+export const skip = true
+
+export const setup = async ({ SideBar, Terminal, Workspace }: TestContext): Promise<void> => {
+  await Terminal.killAll()
+  await Workspace.setFiles([
+    {
+      content: `for(let i=0;i<60_000;i++){
+  process.stdout.write('abc\\n')
+}`,
+      name: 'file.js',
+    },
+  ])
+  await SideBar.hide()
+  // @ts-ignore
+  await Terminal.show({
+    waitForReady: true,
+  })
+}
+
+export const run = async ({ Terminal, Workspace }: TestContext): Promise<void> => {
+  await Terminal.execute('node file.js && touch test.txt', {
+    waitForFile: 'test.txt',
+  })
+  await Terminal.clear()
+  await Workspace.remove('test.txt')
+}
