6ff918207595d5d40e29ad70d0cf312aa5688dee
Simon Siefke <simon.siefke@gmail.com>
2026-01-03 09:11:59 +0100

    feature: disable inline completions for now

--- packages/e2e/src/editor-inline-completion.ts
+++ packages/e2e/src/editor-inline-completion.ts
@@ -2,6 +2,8 @@ import type { TestContext } from '../types.ts'
 
 export const skip = 1
 
+export const requiresNetwork = 1
+
 export const setup = async ({ Editor, Extensions, Workspace, SideBar }: TestContext): Promise<void> => {
   await Editor.closeAll()
   await Workspace.setFiles([
