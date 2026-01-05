db55fa08cce154dfaa6a4cbcbe5b528063edf50a
Simon Siefke <simon.siefke@gmail.com>
2026-01-05 17:52:20 +0300

    feature: use network worker for downloading file (#2564)

---
+++ packages/download-worker/src/parts/LaunchNetworkWorker/LaunchNetworkWorker.ts
@@ -0,0 +1,24 @@
+import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
+import { join } from 'node:path'
+import { root } from '../Root/Root.ts'
+
+const getNetworkWorkerUrl = (): string => {
+  const url = join(root, 'packages/network-worker/src/main.ts')
+  return url
+}
+
+export const launchNetworkWorker = async () => {
+  const url = getNetworkWorkerUrl()
+  const rpc = await NodeWorkerRpcParent.create({
+    commandMap: {},
+    execArgv: [],
+    path: url,
+    stdio: 'inherit',
+  })
+  return {
+    invoke: rpc.invoke.bind(rpc),
+    async [Symbol.asyncDispose]() {
+      await rpc.dispose()
+    },
+  }
+}
