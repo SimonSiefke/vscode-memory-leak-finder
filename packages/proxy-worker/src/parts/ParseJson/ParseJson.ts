e03041ace63e64c4e7b8cb2ba7b3f3027960ab5f
Simon Siefke <simon.siefke@gmail.com>
2026-01-08 15:21:50 +0100

    fix: response body

--- packages/proxy-worker/src/parts/ParseJson/ParseJson.ts
+++ packages/proxy-worker/src/parts/ParseJson/ParseJson.ts
@@ -1,6 +1,15 @@
-export const parseJsonIfApplicable = (body: string, contentType: string | string[] | undefined): string | object => {
+export const parseJsonIfApplicable = (body: string | number[], contentType: string | string[] | undefined): string | object => {
+  // Handle case where body is an array of numbers (bytes) instead of a string
+  // This can happen when data is serialized through RPC and Buffers get converted to arrays
+  let bodyString: string
+  if (Array.isArray(body)) {
+    bodyString = new TextDecoder().decode(new Uint8Array(body))
+  } else {
+    bodyString = body
+  }
+
   if (!contentType) {
-    return body
+    return bodyString
   }
 
   const contentTypeStr = Array.isArray(contentType) ? contentType[0] : contentType
@@ -9,12 +18,12 @@ export const parseJsonIfApplicable = (body: string, contentType: string | string
   // Check if content type is JSON
   if (normalizedContentType.includes('application/json') || normalizedContentType.includes('text/json')) {
     try {
-      return JSON.parse(body)
+      return JSON.parse(bodyString)
     } catch {
       // If parsing fails, return as string
-      return body
+      return bodyString
     }
   }
 
-  return body
+  return bodyString
 }
