68c470c8bb577d45e97df6ceeb7a5082e8a9d9b8
Simon Siefke <simon.siefke@gmail.com>
2026-01-08 22:56:27 +0100

    feature: update vscode version (#2586)

--- packages/cli/src/parts/VsCodeVersion/VsCodeVersion.ts
+++ packages/cli/src/parts/VsCodeVersion/VsCodeVersion.ts
@@ -1,1 +1,1 @@
-export const vscodeVersion = process.env.VSCODE_VERSION || 'insiders:3d862df21449d5ead0b0b1fbe6d43eb05490cb1c' // january 2026
+export const vscodeVersion = process.env.VSCODE_VERSION || '1.108.0'
