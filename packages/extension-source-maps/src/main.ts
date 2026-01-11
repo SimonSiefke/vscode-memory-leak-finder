2846e8c60d8846b7a0436a1b842cbef3b9ac79a1
Simon Siefke <simon.siefke@gmail.com>
2026-01-11 17:09:46 +0100

    feature: try to create js debug sourcemaps

--- packages/extension-source-maps/src/main.ts
+++ packages/extension-source-maps/src/main.ts
@@ -5,11 +5,20 @@ import { root } from './parts/Root/Root.ts'
 const main = async (): Promise<void> => {
   const cacheDir = join(root, '.extension-source-maps-cache')
 
+  // Generate sourcemaps for copilot-chat
+  // await GenerateExtensionSourceMaps.generateExtensionSourceMaps({
+  //   cacheDir,
+  //   extensionName: 'copilot-chat',
+  //   repoUrl: 'git@github.com:microsoft/vscode-copilot-chat.git',
+  //   version: 'v0.35.3',
+  // })
+
+  // Generate sourcemaps for vscode-js-debug
   await GenerateExtensionSourceMaps.generateExtensionSourceMaps({
     cacheDir,
-    extensionName: 'copilot-chat',
-    repoUrl: 'git@github.com:microsoft/vscode-copilot-chat.git',
-    version: 'v0.35.3',
+    extensionName: 'vscode-js-debug',
+    repoUrl: 'git@github.com:microsoft/vscode-js-debug.git',
+    version: 'v1.105.0',
   })
 }
 
