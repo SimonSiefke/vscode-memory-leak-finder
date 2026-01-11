232daad205c707056ef242b55f706646754c8719
Simon Siefke <simon.siefke@gmail.com>
2026-01-11 23:26:11 +0100

    feature: improve task run test

--- packages/e2e/src/task-run.ts
+++ packages/e2e/src/task-run.ts
@@ -2,12 +2,14 @@ import type { TestContext } from '../types.ts'
 
 export const skip = true
 
-export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> => {
+export const setup = async ({ Editor, Workspace, SideBar, Task }: TestContext): Promise<void> => {
   await Editor.closeAll()
   await Workspace.setFiles([])
+  await SideBar.hide()
+  await Task.open()
 }
 
-export const run = async ({ Task }: TestContext): Promise<void> => {
-  await Task.open()
+export const run = async ({ Task, Terminal }: TestContext): Promise<void> => {
   await Task.run('echo')
+  await Terminal.clear()
 }
