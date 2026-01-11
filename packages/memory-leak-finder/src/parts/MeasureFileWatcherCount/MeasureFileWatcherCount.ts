5c142c9b1074f97325cf7caf7bdb1ac32c859f8d
Simon Siefke <simon.siefke@gmail.com>
2026-01-11 19:15:56 +0100

    tsc

--- packages/memory-leak-finder/src/parts/MeasureFileWatcherCount/MeasureFileWatcherCount.ts
+++ packages/memory-leak-finder/src/parts/MeasureFileWatcherCount/MeasureFileWatcherCount.ts
@@ -2,25 +2,20 @@ import * as CompareCount from '../CompareCount/CompareCount.ts'
 import * as GetFileWatcherCount from '../GetFileWatcherCount/GetFileWatcherCount.ts'
 import * as IsLeakCount from '../IsLeakCount/IsLeakCount.ts'
 import * as MeasureId from '../MeasureId/MeasureId.ts'
-import * as MemoryLeakFinderState from '../MemoryLeakFinderState/MemoryLeakFinderState.ts'
 
 export const id = MeasureId.FileWatcherCount
 
 export const targets: readonly any[] = []
 
-export const create = (connectionId: number) => {
-  return [connectionId]
+export const create = (connectionId: number, pid: number) => {
+  return [connectionId, pid]
 }
 
-export const start = async (connectionId: number): Promise<number> => {
-  const state = MemoryLeakFinderState.get(connectionId)
-  const pid = state?.pid
+export const start = async (connectionId: number, pid: number): Promise<number> => {
   return await GetFileWatcherCount.getFileWatcherCount(pid)
 }
 
-export const stop = async (connectionId: number): Promise<number> => {
-  const state = MemoryLeakFinderState.get(connectionId)
-  const pid = state?.pid
+export const stop = async (connectionId: number, pid: number): Promise<number> => {
   return await GetFileWatcherCount.getFileWatcherCount(pid)
 }
 
