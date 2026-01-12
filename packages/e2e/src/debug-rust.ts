02787f7157b53080e0b867a9505cf64125df20df
Simon Siefke <simon.siefke@gmail.com>
2026-01-12 23:29:26 +0100

    feature: add rust e2e test

---
+++ packages/e2e/src/debug-rust.ts
@@ -0,0 +1,48 @@
+import type { TestContext } from '../types.ts'
+
+export const skip = 1
+
+export const requiresNetwork = true
+
+export const setup = async ({ Editor, Extensions, RunAndDebug, Workspace }: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      content: `fn add(a: i32, b: i32) -> i32 {
+    let result = a + b;
+    result
+}
+
+fn main() {
+    let x = add(1, 2);
+    println!("{}", x);
+}
+`,
+      name: 'main.rs',
+    },
+  ])
+  await Extensions.install({
+    id: 'rust-lang.rust-analyzer',
+    name: 'rust-analyzer',
+  })
+  await Extensions.install({
+    id: 'vadimcn.vscode-lldb',
+    name: 'CodeLLDB',
+  })
+  await Editor.closeAll()
+  await RunAndDebug.removeAllBreakpoints()
+}
+
+export const run = async ({ ActivityBar, Editor, RunAndDebug }: TestContext): Promise<void> => {
+  await Editor.open('main.rs')
+  await ActivityBar.showRunAndDebug()
+  await Editor.setBreakpoint(2)
+  await RunAndDebug.runAndWaitForPaused({
+    debugLabel: 'LLDB: Debug',
+    file: 'main.rs',
+    hasCallStack: false,
+    line: 2,
+  })
+  await RunAndDebug.stop()
+  await RunAndDebug.removeAllBreakpoints()
+  await Editor.closeAll()
+}
