318659802c4f1474475c5eec5fb712278ce1ff3a
Simon Siefke <simon.siefke@gmail.com>
2026-01-14 02:47:19 +0100

    tsc

--- packages/e2e/src/terminal-toggle-shell-integration.ts
+++ packages/e2e/src/terminal-toggle-shell-integration.ts
@@ -2,7 +2,7 @@ import type { TestContext } from '../types.js'
 
 export const skip = 1
 
-export const setup = async ({ Editor, Panel, SettingsEditor, SideBar, Terminal, Workspace }: TestContext): Promise<void> => {
+export const setup = async ({ Editor, Panel, SettingsEditor, SideBar, Workspace }: TestContext): Promise<void> => {
   await Editor.closeAll()
   await SideBar.hide()
   await Panel.hide()
