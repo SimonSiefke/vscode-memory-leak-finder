27608b444d6bad8f8aedc1e7ee8ab42c4f69d752
Simon Siefke <simon.siefke@gmail.com>
2026-01-14 02:31:01 +0100

    feature: add peek definition e2e test

---
+++ packages/e2e/src/typescript-peek-definition.ts
@@ -0,0 +1,47 @@
+import type { TestContext } from '../types.ts'
+
+export const skip = true
+
+export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      content: `{}
+`,
+      name: 'tsconfig.json',
+    },
+    {
+      content: `export class DebugNameData {
+	constructor(
+		public readonly owner: DebugOwner | undefined,
+		public readonly debugNameSource: DebugNameSource | undefined,
+		public readonly referenceFn: Function | undefined,
+	) { }
+
+	public getDebugName(target: object): string | undefined {
+		return getDebugName(target, this);
+	}
+}
+`,
+      name: 'debugNameData.ts',
+    },
+    {
+      name: 'file.ts',
+      content: `import { DebugNameData } from './debugNameData.ts'
+
+const element = new DebugNameData(1, 2, 3)`,
+    },
+  ])
+  await Editor.open('file.ts')
+  await Editor.shouldHaveSquigglyError()
+}
+
+export const run = async ({ Editor, Notification }: TestContext): Promise<void> => {
+  // TODO open peek definition
+  // await Editor.goToSourceDefinition({ hasDefinition: false })
+  // await Notification.closeAll()
+  await new Promise((r) => {})
+}
+
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.closeAll()
+}
