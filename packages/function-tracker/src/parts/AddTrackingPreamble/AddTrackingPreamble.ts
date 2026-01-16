e0c4d7ed6c17b1c3e4053796a2272617fe43c3ed
Simon Siefke <simon.siefke@gmail.com>
2026-01-16 23:01:00 +0100

    fix

--- packages/function-tracker/src/parts/AddTrackingPreamble/AddTrackingPreamble.ts
+++ packages/function-tracker/src/parts/AddTrackingPreamble/AddTrackingPreamble.ts
@@ -1,10 +1,5 @@
 import { trackingCode } from '../TrackingCode/TrackingCode.js'
 
-export const addTrackingPreamble = async (code: string): Promise<string> => {
-  try {
-    return trackingCode + '\n' + code
-  } catch (error) {
-    console.error('Error adding tracking preamble:', error)
-    return code // Return original code if preamble addition fails
-  }
+export const addTrackingPreamble = (code: string): string => {
+  return trackingCode + '\n' + code
 }
