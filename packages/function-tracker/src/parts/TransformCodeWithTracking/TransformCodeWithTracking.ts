b3cddc5867ec0cf49b435412a944a95b91a9191a
Simon Siefke <simon.siefke@gmail.com>
2026-01-16 21:34:47 +0100

    tsc

--- packages/function-tracker/src/parts/TransformCodeWithTracking/TransformCodeWithTracking.ts
+++ packages/function-tracker/src/parts/TransformCodeWithTracking/TransformCodeWithTracking.ts
@@ -4,26 +4,26 @@ import generate from '@babel/generator'
 import { TransformOptions } from '../Types/Types.js'
 import { createFunctionWrapperPlugin } from '../CreateFunctionWrapperPlugin/CreateFunctionWrapperPlugin.js'
 
-export const transformCodeWithTracking = async (code: string, options: TransformOptions = {}): Promise<string> => {
+export const transformCodeWithTracking = (code: string, options: TransformOptions = {}): string => {
   try {
-    
     const ast = parser.parse(code, {
       sourceType: 'module',
-      plugins: ['jsx', 'typescript', 'decorators-legacy']
+      plugins: ['jsx', 'typescript', 'decorators-legacy'],
     })
-    
+
     // Transform the original code with proper file context
     const plugin = createFunctionWrapperPlugin(options)
+    // @ts-ignore
     traverse.default(ast, plugin.visitor, undefined, ast)
-    
+
     const result = generate.default(ast, {
       retainLines: false,
       compact: false,
       comments: true,
       minified: false,
-      jsonCompatibleStrings: false
+      jsonCompatibleStrings: false,
     } as any)
-    
+
     return result.code
   } catch (error) {
     console.error('Error transforming code with tracking:', error)
