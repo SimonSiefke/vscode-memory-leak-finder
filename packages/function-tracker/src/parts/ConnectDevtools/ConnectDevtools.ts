22725c802f353abc82a7a824f4124a3b478b4ff9
Simon Siefke <simon.siefke@gmail.com>
2026-01-17 16:00:52 +0100

    remove unused code

--- packages/function-tracker/src/parts/ConnectDevtools/ConnectDevtools.ts
+++ packages/function-tracker/src/parts/ConnectDevtools/ConnectDevtools.ts
@@ -1,5 +1,4 @@
 import * as DebuggerCreateIpcConnection from '../DebuggerCreateIpcConnection/DebuggerCreateIpcConnection.ts'
-import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
 import { setSessionRpc } from '../GetFunctionStatistics/GetFunctionStatistics.ts'
 import { waitForSession } from '../WaitForSession/WaitForSession.ts'
 
@@ -26,40 +25,11 @@ export const connectDevtools = async (
     throw new Error('measureId must be a non-empty string')
   }
 
-  console.log('called')
   const browserRpc = await DebuggerCreateIpcConnection.createConnection(devtoolsWebSocketUrl)
-  console.log('browser')
-  const { sessionRpc, sessionId } = await waitForSession(browserRpc, 19990)
-  console.log('sess')
+  const { sessionRpc } = await waitForSession(browserRpc, 19990)
 
-  console.log('session rpc', sessionRpc)
-
-  sessionRpc.on('Fetch.requestPaused', (event) => {
-    console.log('paused', event)
-  })
-
-  console.log('session id', sessionId)
-  await new Promise((r) => {
-    setTimeout(r, 3000)
-  })
-  // await sessionRpc.invoke('Network.enable')
-  // await sessionRpc.invoke('Network.setRequestInterception', {
-  //   patterns: [{ urlPattern: '*.js', requestStage: 'Request' }],
-  // })
-
-  console.log('before call')
-  // await DevtoolsProtocolRuntime.enable(sessionRpc)
-  // await DevtoolsProtocolRuntime.runIfWaitingForDebugger(sessionRpc)
-
-  console.log('fetch enabled')
-  // void sessionRpc
-  //   .invoke('Runtime.evaluate', {
-  //     expression: trackingCode,
-  //   })
-  //   .catch((error) => {
-  //     console.error('err', error)
-  //   })
-
+  // TODO seems to be not working. maybe electron bug?
+  sessionRpc.on('Fetch.requestPaused', (event) => {})
   // Store sessionRpc for GetFunctionStatistics
   setSessionRpc(sessionRpc)
 }
