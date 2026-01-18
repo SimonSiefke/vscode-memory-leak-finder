600e58b58ed5ef392a40a1f9d422bf8695276451
Simon Siefke <simon.siefke@gmail.com>
2026-01-18 21:47:29 +0100

    fix

--- packages/page-object/src/parts/NotebookInlineChat/NotebookInlineChat.ts
+++ packages/page-object/src/parts/NotebookInlineChat/NotebookInlineChat.ts
@@ -6,17 +6,12 @@ export const create = ({ expect, page, VError }: any) => {
         const notebook = page.locator('.notebook-editor')
         await expect(notebook).toBeVisible()
         await page.waitForIdle()
-        const notebookInlineChat = page.locator('.notebook-inline-chat:not(.hide)')
-        await expect(notebookInlineChat).toBeVisible()
-        const editor = notebookInlineChat.locator('.monaco-editor[data-uri^="chatSessionInput"]')
-        await expect(editor).toBeVisible()
+        const chat = page.locator('.chat-widget')
+        await expect(chat).toBeVisible()
         await page.waitForIdle()
         await page.keyboard.press('Escape')
         await page.waitForIdle()
-
-        // it is what it is...
-        const notebookInlineChatHidden = page.locator('.notebook-inline-chat.hide')
-        await expect(notebookInlineChatHidden).toBeVisible()
+        await expect(chat).toBeHidden()
         await page.waitForIdle()
       } catch (error) {
         throw new VError(error, `Failed to hide notebook inline chat`)
