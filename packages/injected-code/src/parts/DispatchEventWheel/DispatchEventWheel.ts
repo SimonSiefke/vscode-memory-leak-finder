61c29e0fad1db010abd1bda022f3b6cf133758bc
Simon Siefke <simon.siefke@gmail.com>
2026-01-18 18:54:26 +0100

    fix

--- packages/injected-code/src/parts/DispatchEventWheel/DispatchEventWheel.ts
+++ packages/injected-code/src/parts/DispatchEventWheel/DispatchEventWheel.ts
@@ -1,6 +1,6 @@
 import * as ActuallyDispatchEvent from '../ActuallyDispatchEvent/ActuallyDispatchEvent.ts'
 import * as DomEventType from '../DomEventType/DomEventType.ts'
 
-export const wheel = (element, options) => {
+export const wheel = (element: Element, options: WheelEventInit) => {
   ActuallyDispatchEvent.actuallyDispatchEvent(element, DomEventType.Wheel, options)
 }
