d388f786cebb4fa7583ca5e01483f3056e37b05b
Simon Siefke <simon.siefke@gmail.com>
2026-01-18 20:31:04 +0100

    Discard changes to packages/injected-code/src/parts/ElementActionClick/ElementActionClick.ts

--- packages/injected-code/src/parts/ElementActionClick/ElementActionClick.ts
+++ packages/injected-code/src/parts/ElementActionClick/ElementActionClick.ts
@@ -1,25 +1,22 @@
 import * as DispatchEvent from '../DispatchEvent/DispatchEvent.ts'
 
-export const click = (element: Element, options: MouseEventInit & { button?: number | 'right' }): void => {
+export const click = (element, options) => {
   const rect = element.getBoundingClientRect()
-  const mutableOptions = options as MouseEventInit & { button?: number | 'right'; clientX?: number; clientY?: number; cancelable?: boolean; bubbles?: boolean }
-  mutableOptions.clientX = (rect.left + rect.right) / 2
-  mutableOptions.clientY = (rect.top + rect.bottom) / 2
-  mutableOptions.cancelable = true
-  mutableOptions.bubbles = true
-  let buttonValue: number | 'right' | undefined = options.button
-  if (buttonValue === 'right') {
-    buttonValue = 2
-    mutableOptions.button = 2
+  options.clientX = (rect.left + rect.right) / 2
+  options.clientY = (rect.top + rect.bottom) / 2
+  options.cancelable = true
+  options.bubbles = true
+  if (options.button === 'right') {
+    options.button = 2
   }
-  DispatchEvent.pointerDown(element, mutableOptions)
-  DispatchEvent.mouseDown(element, mutableOptions)
-  if (buttonValue !== 2 /* right */) {
-    DispatchEvent.click(element, mutableOptions)
+  DispatchEvent.pointerDown(element, options)
+  DispatchEvent.mouseDown(element, options)
+  if (options.button !== 2 /* right */) {
+    DispatchEvent.click(element, options)
   }
-  DispatchEvent.mouseUp(element, mutableOptions)
-  DispatchEvent.pointerUp(element, mutableOptions)
-  if (buttonValue === 2 /* right */) {
-    DispatchEvent.contextMenu(element, mutableOptions)
+  DispatchEvent.mouseUp(element, options)
+  DispatchEvent.pointerUp(element, options)
+  if (options.button === 2 /* right */) {
+    DispatchEvent.contextMenu(element, options)
   }
 }
