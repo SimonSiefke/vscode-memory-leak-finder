63bc5d8f376f25d0639f4e92b1775efa79207fd4
Simon Siefke <simon.siefke@gmail.com>
2026-01-18 20:35:55 +0100

    fix

--- packages/injected-code/src/parts/QuerySelector/QuerySelector.ts
+++ packages/injected-code/src/parts/QuerySelector/QuerySelector.ts
@@ -4,11 +4,6 @@ export const querySelectorAll = (selector: string): readonly Element[] => {
   return QuerySelectorAllRoot.querySelectorAll(document.documentElement, selector)
 }
 
-/**
- *
- * @param {any} selector
- * @returns {HTMLElement|undefined}
- */
 export const querySelector = (selector: string): HTMLElement | undefined => {
   return QuerySelectorAllRoot.querySelector(document.documentElement, selector)
 }
