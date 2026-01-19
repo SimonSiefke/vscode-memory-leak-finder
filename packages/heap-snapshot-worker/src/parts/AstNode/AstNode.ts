ff363f193481b1fb31b1df1e3e95e4978c783638
Simon Siefke <simon.siefke@gmail.com>
2026-01-19 15:19:26 +0100

    ts

--- packages/heap-snapshot-worker/src/parts/AstNode/AstNode.ts
+++ packages/heap-snapshot-worker/src/parts/AstNode/AstNode.ts
@@ -86,7 +86,7 @@ export interface CodeNode extends BaseAstNode {
 
 export interface UnknownNode extends BaseAstNode {
   readonly type: 'unknown'
-  readonly value?: string
+  readonly value?: any
 }
 
 export type AstNode =
