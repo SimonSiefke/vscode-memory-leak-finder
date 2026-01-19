b9892e347b764d46d87d06d074d27aa6c9e85dbc
Simon Siefke <simon.siefke@gmail.com>
2026-01-19 15:30:53 +0100

    update call

--- packages/function-tracker/src/parts/CreateFunctionWrapperPlugin/CreateFunctionWrapperPlugin.ts
+++ packages/function-tracker/src/parts/CreateFunctionWrapperPlugin/CreateFunctionWrapperPlugin.ts
@@ -26,7 +26,13 @@ export const createFunctionWrapperPlugin = (options: CreateFunctionWrapperPlugin
     }
 
     return t.expressionStatement(
-      t.callExpression(t.identifier('trackFunctionCall'), [t.numericLiteral(scriptId), t.numericLiteral(line), t.numericLiteral(column)]),
+      t.callExpression(
+        t.memberExpression(
+          t.memberExpression(t.identifier('globalThis'), t.identifier('test')),
+          t.identifier('trackFunctionCall'),
+        ),
+        [t.numericLiteral(scriptId), t.numericLiteral(line), t.numericLiteral(column)],
+      ),
     )
   }
 
