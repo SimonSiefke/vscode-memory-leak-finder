dcbfc95d000491a35e6939113963c56b21701fbc
Simon Siefke <simon.siefke@gmail.com>
2026-01-19 20:34:37 +0100

    feature: improve function tracker api

--- packages/function-tracker/src/parts/CommandMap/CommandMap.ts
+++ packages/function-tracker/src/parts/CommandMap/CommandMap.ts
@@ -1,10 +1,12 @@
 import { getFunctionStatistics } from '../GetFunctionStatistics/GetFunctionStatistics.ts'
-import * as SocketServer from '../SocketServer/SocketServer.ts'
+import * as HttpServer from '../SocketServer/SocketServer.ts'
+import { preGenerateWorkbench } from '../PreGenerateWorkbench/PreGenerateWorkbench.ts'
 import { writeFunctionStatistics } from '../WriteFunctionStatistics/WriteFunctionStatistics.ts'
 
 export const commandMap = {
   'FunctionTracker.getFunctionStatistics': getFunctionStatistics,
-  'FunctionTracker.startSocketServer': SocketServer.startSocketServer,
-  'FunctionTracker.stopSocketServer': SocketServer.stopSocketServer,
+  'FunctionTracker.startServer': HttpServer.startServer,
+  'FunctionTracker.stopServer': HttpServer.stopServer,
+  'FunctionTracker.preGenerateWorkbench': preGenerateWorkbench,
   'FunctionTracker.writeFunctionStatistics': writeFunctionStatistics,
 }
