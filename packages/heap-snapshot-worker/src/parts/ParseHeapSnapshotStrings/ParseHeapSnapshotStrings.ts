d0f7eef6af9a81201b867ca40bf841ad04d335ca
Simon Siefke <simon.siefke@gmail.com>
2026-01-19 15:06:17 +0100

    tsc

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotStrings/ParseHeapSnapshotStrings.ts
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotStrings/ParseHeapSnapshotStrings.ts
@@ -2,6 +2,9 @@ import * as HeapSnapshotState from '../HeapSnapshotState/HeapSnapshotState.ts'
 
 export const parseHeapSnapshotStrings = (id: number): readonly string[] => {
   const heapsnapshot = HeapSnapshotState.get(id)
+  if (!heapsnapshot) {
+    throw new Error(`snapshot not found`)
+  }
 
   const { strings } = heapsnapshot
   if (!Array.isArray(strings)) {
