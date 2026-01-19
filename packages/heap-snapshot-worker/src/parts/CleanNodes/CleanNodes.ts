fb331434edeab3a12b1c588a282f0997b666b7f0
Simon Siefke <simon.siefke@gmail.com>
2026-01-19 15:10:00 +0100

    Discard changes to packages/heap-snapshot-worker/src/parts/CleanNodes/CleanNodes.ts

--- packages/heap-snapshot-worker/src/parts/CleanNodes/CleanNodes.ts
+++ packages/heap-snapshot-worker/src/parts/CleanNodes/CleanNodes.ts
@@ -1,11 +1,5 @@
 import * as CleanNode from '../CleanNode/CleanNode.ts'
 
-interface NodeWithIdNameType {
-  readonly id: number
-  readonly name: string
-  readonly type: string
-}
-
-export const cleanNode = (nodes: readonly NodeWithIdNameType[]): readonly NodeWithIdNameType[] => {
+export const cleanNode = (nodes) => {
   return nodes.map(CleanNode.cleanNode)
 }
