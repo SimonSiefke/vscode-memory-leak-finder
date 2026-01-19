8c2226e4ce9085b659681dc4f8a5c848ad5628a0
Simon Siefke <simon.siefke@gmail.com>
2026-01-19 15:44:45 +0100

    fix

--- packages/initialization-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/initialization-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,5 @@
-import * as ConnectFunctionTracker from '../ConnectFunctionTracker/ConnectFunctionTracker.ts'
 import * as PrepareBoth from '../PrepareBoth/PrepareBoth.ts'
 
 export const commandMap = {
-  'Initialize.connectFunctionTracker': ConnectFunctionTracker.connectFunctionTracker,
   'Initialize.prepare': PrepareBoth.prepareBoth,
 }
