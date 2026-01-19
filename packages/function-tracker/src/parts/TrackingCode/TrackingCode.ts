f5aca6218d3e5710d96dcd0ae3dd8bedaf758fae
Simon Siefke <simon.siefke@gmail.com>
2026-01-19 15:21:45 +0100

    unused

--- packages/function-tracker/src/parts/TrackingCode/TrackingCode.ts
+++
@@ -1,9 +0,0 @@
-export const trackingCode: string = `
-globalThis.___functionStatistics = Object.create(null)
-
-const trackFunctionCall = (scriptId, line, column) => {
-  const key = \`\${scriptId}:\${line}:\${column})\`
-  const current = globalThis.___functionStatistics[key] || 0
-  globalThis.___functionStatistics[key]++
-}
-`
