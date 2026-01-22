1fddc63aebbc1c85a5643f96e7c47f50e26a3843
Simon Siefke <simon.siefke@gmail.com>
2026-01-22 23:17:29 +0100

    debug

--- packages/memory-leak-finder/src/parts/MeasureIpcMessageCount/MeasureIpcMessageCount.ts
+++ packages/memory-leak-finder/src/parts/MeasureIpcMessageCount/MeasureIpcMessageCount.ts
@@ -1,11 +1,9 @@
-import type { Session } from '../Session/Session.ts'
-import * as CompareCount from '../CompareCount/CompareCount.ts'
-import * as GetIpcMessageCount from '../GetIpcMessageCount/GetIpcMessageCount.ts'
 import * as GetIpcMessages from '../GetIpcMessages/GetIpcMessages.ts'
 import * as IsLeakCount from '../IsLeakCount/IsLeakCount.ts'
 import * as MeasureId from '../MeasureId/MeasureId.ts'
 import * as ObjectGroupId from '../ObjectGroupId/ObjectGroupId.ts'
 import * as ReleaseObjectGroup from '../ReleaseObjectGroup/ReleaseObjectGroup.ts'
+import type { Session } from '../Session/Session.ts'
 import * as TargetId from '../TargetId/TargetId.ts'
 
 export const id = MeasureId.IpcMessageCount
@@ -18,22 +16,19 @@ export const create = (session: Session) => {
 }
 
 export const start = async (session: Session, objectGroup: string) => {
-  // Write IPC messages to file
-  await GetIpcMessages.getIpcMessages(session)
-  return GetIpcMessageCount.getIpcMessageCount(session, objectGroup)
+  return GetIpcMessages.getIpcMessages(session)
 }
 
 export const stop = async (session: Session, objectGroup: string) => {
-  // Write IPC messages to file
-  await GetIpcMessages.getIpcMessages(session)
-  const result = await GetIpcMessageCount.getIpcMessageCount(session, objectGroup)
-  return result
+  return await GetIpcMessages.getIpcMessages(session)
 }
 
 export const releaseResources = async (session: Session, objectGroup: string) => {
   await ReleaseObjectGroup.releaseObjectGroup(session, objectGroup)
 }
 
-export const compare = CompareCount.compareCount
+export const compare = (before, after) => {
+  return {}
+}
 
 export const isLeak = IsLeakCount.isLeakCount
