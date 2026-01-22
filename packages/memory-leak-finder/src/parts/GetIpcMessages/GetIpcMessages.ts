84f6c780d5e7a1a84ecd4582414f4fda16174b42
Simon Siefke <simon.siefke@gmail.com>
2026-01-22 23:21:22 +0100

    result

--- packages/memory-leak-finder/src/parts/GetIpcMessages/GetIpcMessages.ts
+++ packages/memory-leak-finder/src/parts/GetIpcMessages/GetIpcMessages.ts
@@ -1,6 +1,5 @@
-import type { Session } from '../Session/Session.ts'
 import { DevtoolsProtocolRuntime } from '../DevtoolsProtocol/DevtoolsProtocol.ts'
-import * as GetValue from '../GetValue/GetValue.ts'
+import type { Session } from '../Session/Session.ts'
 
 export const getIpcMessages = async (session: Session): Promise<void> => {
   // Get the IPC messages array from global state
@@ -9,26 +8,5 @@ export const getIpcMessages = async (session: Session): Promise<void> => {
     generatePreview: true,
     returnByValue: true,
   })
-  console.log({ result })
-
-  if (Map) {
-    return
-  }
-
-  const messages = GetValue.getValue(result.result)
-
-  // Write to file
-  if (messages && messages.length > 0) {
-    const fs = require('fs')
-    const path = '/tmp/ipc-messages.txt'
-    const timestamp = new Date().toISOString()
-    const logEntry = `[${timestamp}] IPC Messages: ${JSON.stringify(messages, null, 2)}\n`
-
-    try {
-      fs.appendFileSync(path, logEntry)
-    } catch (error) {
-      // If file doesn't exist, create it
-      fs.writeFileSync(path, logEntry)
-    }
-  }
+  return result
 }
