798dee0db867eb845e67112f1fe809f20a5f8271
Simon Siefke <simon.siefke@gmail.com>
2026-01-23 12:53:59 +0100

    Discard changes to packages/initialization-worker/src/parts/DevtoolsProtocol/DevtoolsProtocol.ts

--- packages/initialization-worker/src/parts/DevtoolsProtocol/DevtoolsProtocol.ts
+++ packages/initialization-worker/src/parts/DevtoolsProtocol/DevtoolsProtocol.ts
@@ -1,6 +1,6 @@
 const url = new URL('../../../../devtools-protocol/src/index.js', import.meta.url).toString()
 const module = await import(url)
 
-const { DevtoolsProtocolDebugger, DevtoolsProtocolRuntime, DevtoolsProtocolTarget, DevtoolsProtocolPage } = module
+const { DevtoolsProtocolDebugger, DevtoolsProtocolRuntime, DevtoolsProtocolTarget } = module
 
-export { DevtoolsProtocolRuntime, DevtoolsProtocolTarget, DevtoolsProtocolDebugger, DevtoolsProtocolPage }
+export { DevtoolsProtocolRuntime, DevtoolsProtocolTarget, DevtoolsProtocolDebugger }
