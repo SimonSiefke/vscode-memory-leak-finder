053a7522be19517177c35b39c06c6cd1d1765d62
Simon Siefke <simon.siefke@gmail.com>
2026-01-23 12:56:59 +0100

    Delete packages/launch-worker/src/parts/GetFunctionTrackerUrl/GetFunctionTrackerUrl.ts

--- packages/launch-worker/src/parts/GetFunctionTrackerUrl/GetFunctionTrackerUrl.ts
+++
@@ -1,9 +0,0 @@
-import { join } from 'node:path'
-
-const __dirname = import.meta.dirname
-
-export const getFunctionTrackerUrl = (): string => {
-  const root = join(__dirname, '..', '..', '..', '..', '..')
-  const url = join(root, 'packages', 'function-tracker', 'src', 'main.ts')
-  return url
-}
