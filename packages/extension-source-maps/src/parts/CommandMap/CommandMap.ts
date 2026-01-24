af3be20bc1b048924dc4a01bb64e24fb942ed0cb
Simon Siefke <simon.siefke@gmail.com>
2026-01-24 14:59:40 +0100

    debug

--- packages/extension-source-maps/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-source-maps/src/parts/CommandMap/CommandMap.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import * as MapPathToSourceMapUrl from '../MapPathToSourceMapUrl/MapPathToSourceMapUrl.ts'
 
 export const commandMap: Record<string, (...args: any[]) => any> = {
   'ExtensionSourceMap.mapPathToSourceMapUrl': MapPathToSourceMapUrl.mapPathToSourceMapUrl,
-=======
-import { resolveFromPath } from '../ResolveFromPath/ResolveFromPath.ts'
-
-export const commandMap: Record<string, (...args: any[]) => any> = {
-  'ExtensionSourceMaps.resolve': resolveFromPath,
->>>>>>> feature/resolve
 }
+
