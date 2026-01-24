db996c05c613866d307466daef5cfa2c4eceb918
Simon Siefke <simon.siefke@gmail.com>
2026-01-24 12:27:50 +0100

    feature: add source map coordinator

---
+++ packages/source-map-coordinator/src/parts/LoadSourceMap/LoadSourceMap.ts
@@ -0,0 +1,30 @@
+import { NodeWorkerRpcParent } from '@lvce-editor/rpc'
+import { loadSourceMapWorkerPath } from '../LoadSourceMapWorkerPath/LoadSourceMapWorkerPath.ts'
+import { VError } from '../VError/VError.ts'
+
+const launchLoadSourceMapWorker = async () => {
+  const rpc = await NodeWorkerRpcParent.create({
+    commandMap: {},
+    execArgv: [],
+    path: loadSourceMapWorkerPath,
+    stdio: 'inherit',
+  })
+  return {
+    invoke(method: string, ...params: any[]) {
+      return rpc.invoke(method, ...params)
+    },
+    async [Symbol.asyncDispose]() {
+      await rpc.dispose()
+    },
+  }
+}
+
+export const loadSourceMap = async (url: string, hash: string): Promise<any> => {
+  try {
+    await using rpc = await launchLoadSourceMapWorker()
+    const sourceMap = await rpc.invoke('LoadSourceMap.loadSourceMap', url, hash)
+    return sourceMap
+  } catch (error) {
+    throw new VError(error, `Failed to load source map for ${url}`)
+  }
+}
