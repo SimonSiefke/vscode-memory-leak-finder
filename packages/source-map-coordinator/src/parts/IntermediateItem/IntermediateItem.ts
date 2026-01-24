983f2bcc99736d0f4d3df638e8283ff055d2946d
Simon Siefke <simon.siefke@gmail.com>
2026-01-24 12:43:54 +0100

    fix

--- packages/source-map-coordinator/src/parts/IntermediateItem/IntermediateItem.ts
+++
@@ -1,8 +0,0 @@
-export interface IntermediateItem {
-  readonly codePath: string | null
-  readonly column: number | null
-  readonly line: number | null
-  readonly name: string | null
-  readonly source: string | null
-  readonly sourcesHash: string | null
-}
