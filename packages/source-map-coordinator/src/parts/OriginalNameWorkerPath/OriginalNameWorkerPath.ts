bc52d4ea960b8e9b7f3e3f081f0183632c2a40af
Simon Siefke <simon.siefke@gmail.com>
2026-01-24 12:35:52 +0100

    fix

--- packages/source-map-coordinator/src/parts/OriginalNameWorkerPath/OriginalNameWorkerPath.ts
+++
@@ -1,7 +0,0 @@
-import { join } from 'node:path'
-import { root } from '../Root/Root.ts'
-
-export const getOriginalNameWorkerPath = (): string => {
-  const workerPath: string = join(root, 'packages/original-name-worker/src/originalNameWorkerMain.ts')
-  return workerPath
-}
--- packages/source-map-coordinator/src/parts/OriginalPosition/OriginalPosition.ts
+++
@@ -1,7 +0,0 @@
-export interface OriginalPosition {
-  readonly column: number | null
-  readonly line: number | null
-  readonly name: string | null
-  readonly source: string | null
-  readonly sourcesHash: string | null
-}
