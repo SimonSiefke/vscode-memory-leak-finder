2e23a3aae1aa90370974c84a010b5ec4d9dadf69
Simon Siefke <simon.siefke@gmail.com>
2026-01-24 14:54:42 +0100

    fix

--- packages/source-map-coordinator/src/parts/GetCleanPositionsMap/GetCleanPositionsMap.ts
+++ packages/source-map-coordinator/src/parts/GetCleanPositionsMap/GetCleanPositionsMap.ts
@@ -13,36 +13,43 @@ interface CleanPositionMap {
 }
 
 const isExtensionFile = (url: string): boolean => {
-  return url.includes('/ms-vscode.js-debug') || url.includes('/github.copilot-chat')
+  return url.includes('ms-vscode.js-debug')
 }
 
 export const getCleanPositionsMap = async (sourceMapUrlMap: SourceMapUrlMap, classNames: boolean): Promise<CleanPositionMap> => {
   const cleanPositionMap: CleanPositionMap = Object.create(null)
-  
+
   // Launch workers based on the types of files we need to process
   let sourceMapWorker: any = null
   let extensionSourceMapWorker: any = null
-  
+
   for (const [key, value] of Object.entries(sourceMapUrlMap)) {
     if (!key) {
       cleanPositionMap[key] = []
       continue
     }
-    
+
     const hash = Hash.hash(key)
-    
+
     if (isExtensionFile(key)) {
       // Use extension-source-maps worker for extension files
       if (!extensionSourceMapWorker) {
         extensionSourceMapWorker = await launchExtensionSourceMapWorker()
       }
-      
+
       // Get the source map URL from the extension source maps worker
       const sourceMapUrl = await extensionSourceMapWorker.invoke('ExtensionSourceMap.mapPathToSourceMapUrl', key, process.cwd())
-      
+
       if (sourceMapUrl) {
         const sourceMap = await LoadSourceMap.loadSourceMap(sourceMapUrl, hash)
-        const originalPositions = await extensionSourceMapWorker.invoke('SourceMap.getCleanPositionsMap2', sourceMap, value, classNames, hash, key)
+        const originalPositions = await extensionSourceMapWorker.invoke(
+          'SourceMap.getCleanPositionsMap2',
+          sourceMap,
+          value,
+          classNames,
+          hash,
+          key,
+        )
         const cleanPositions = originalPositions.map(GetCleanPosition.getCleanPosition)
         cleanPositionMap[key] = cleanPositions
       } else {
@@ -53,14 +60,14 @@ export const getCleanPositionsMap = async (sourceMapUrlMap: SourceMapUrlMap, cla
       if (!sourceMapWorker) {
         sourceMapWorker = await launchSourceMapWorker()
       }
-      
+
       const sourceMap = await LoadSourceMap.loadSourceMap(key, hash)
       const originalPositions = await sourceMapWorker.invoke('SourceMap.getCleanPositionsMap2', sourceMap, value, classNames, hash, key)
       const cleanPositions = originalPositions.map(GetCleanPosition.getCleanPosition)
       cleanPositionMap[key] = cleanPositions
     }
   }
-  
+
   // Clean up workers
   if (sourceMapWorker) {
     await sourceMapWorker[Symbol.asyncDispose]()
@@ -68,6 +75,6 @@ export const getCleanPositionsMap = async (sourceMapUrlMap: SourceMapUrlMap, cla
   if (extensionSourceMapWorker) {
     await extensionSourceMapWorker[Symbol.asyncDispose]()
   }
-  
+
   return cleanPositionMap
 }
