458c21bdf19685439ce6c6477bc6adccb039b784
Simon Siefke <simon.siefke@gmail.com>
2026-01-24 12:34:56 +0100

    fix

--- packages/source-map-coordinator/src/parts/GetCleanPositionsMap/GetCleanPositionsMap.ts
+++ packages/source-map-coordinator/src/parts/GetCleanPositionsMap/GetCleanPositionsMap.ts
@@ -1,7 +1,7 @@
 import * as GetCleanPosition from '../GetCleanPosition/GetCleanPosition.ts'
 import * as Hash from '../Hash/Hash.ts'
+import { launchSourceMapWorker } from '../LaunchSourceMapWorker/LaunchSourceMapWorker.ts'
 import * as LoadSourceMap from '../LoadSourceMap/LoadSourceMap.ts'
-import * as SourceMap from '../SourceMap/SourceMap.ts'
 
 interface SourceMapUrlMap {
   [key: string]: number[]
@@ -11,11 +11,8 @@ interface CleanPositionMap {
   [key: string]: any[]
 }
 
-export const getCleanPositionsMap = async (
-  sourceMapUrlMap: SourceMapUrlMap,
-  classNames: boolean,
-  hash?: string,
-): Promise<CleanPositionMap> => {
+export const getCleanPositionsMap = async (sourceMapUrlMap: SourceMapUrlMap, classNames: boolean): Promise<CleanPositionMap> => {
+  await using sourceMapWorker = await launchSourceMapWorker()
   const cleanPositionMap: CleanPositionMap = Object.create(null)
   for (const [key, value] of Object.entries(sourceMapUrlMap)) {
     if (!key) {
@@ -24,7 +21,7 @@ export const getCleanPositionsMap = async (
     }
     const hash = Hash.hash(key)
     const sourceMap = await LoadSourceMap.loadSourceMap(key, hash)
-    const originalPositions = await SourceMap.getOriginalPositions(sourceMap, value, classNames, hash, key)
+    const originalPositions = await sourceMapWorker.invoke('SourceMap.getCleanPositionsMap2', sourceMap, value, classNames, hash, key)
     const cleanPositions = originalPositions.map(GetCleanPosition.getCleanPosition)
     cleanPositionMap[key] = cleanPositions
   }
