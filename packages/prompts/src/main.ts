628e48d45d631ca86e64a9053b6f284ecb96800e
Simon Siefke <simon.siefke@gmail.com>
2026-01-25 22:51:05 +0100

    Discard changes to packages/prompts/src/main.ts

--- packages/prompts/src/main.ts
+++ packages/prompts/src/main.ts
@@ -9,7 +9,7 @@ const root = join(dirname, '..', '..', '..')
 
 const localVscodePath = '/home/simon/.cache/repos/vscode'
 
-const relativePath = `/home/simon/.cache/repos/vscode-memory-leak-finder/.vscode-memory-leak-finder-results/node/named-function-count3/window-open-new.json`
+const relativePath = `/home/simon/Downloads/vscode-memory-leak-finder-results-linux/extension-host/named-function-count3/editor-cross-file-rename.json`
 
 const main = async () => {
   const absolutePath = isAbsolute(relativePath) ? relativePath : join(root, relativePath)
