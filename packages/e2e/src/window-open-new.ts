71c4be7a3a806f8855e3bbff913e4998ea982be3
Simon Siefke <simon.siefke@gmail.com>
2026-01-26 17:38:35 +0100

    fix

--- packages/e2e/src/window-open-new.ts
+++ packages/e2e/src/window-open-new.ts
@@ -4,10 +4,6 @@ export const skip = 1
 
 export const run = async ({ Workbench }: TestContext): Promise<void> => {
   const newWindow = await Workbench.openNewWindow()
-
-  // Wait for the new window's workbench to be visible instead of fixed timeout
   await newWindow.shouldBeVisible()
-
-  // Close the window
   await newWindow.close()
 }
