09674338c9432f7674cb944c78915aa801e3dfbf
Simon Siefke <simon.siefke@gmail.com>
2026-01-28 19:05:41 +0100

    exclude some data

--- packages/memory-leak-finder/src/parts/CompareFileDescriptors/CompareFileDescriptors.ts
+++ packages/memory-leak-finder/src/parts/CompareFileDescriptors/CompareFileDescriptors.ts
@@ -16,15 +16,7 @@ export interface FileDescriptorDelta {
 
 const isExcludedPath = (target: string): boolean => {
   // Exclude irrelevant folders from measurement
-  const excludedPatterns = [
-    'Dictionaries',
-    'Local Storage',
-    'dmabuf',
-    'SharedStorage',
-    'nssdb',
-    'systemd',
-    'memfs',
-  ]
+  const excludedPatterns = ['Dictionaries', 'Local Storage', 'dmabuf', 'SharedStorage', 'nssdb', 'systemd', 'memfs']
 
   // Also exclude /dev paths except /dev/ptmx and /dev/udmabuf which are handled separately
   if (target.startsWith('/dev/') && target !== '/dev/ptmx' && target !== '/dev/udmabuf') {
