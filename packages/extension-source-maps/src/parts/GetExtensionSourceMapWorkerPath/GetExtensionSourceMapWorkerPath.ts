70c75b3b71cfc52ecb7b8187c70df3c09ee8d218
Simon Siefke <simon.siefke@gmail.com>
2026-01-30 22:12:09 +0100

    Delete packages/extension-source-maps/src/parts/GetExtensionSourceMapWorkerPath/GetExtensionSourceMapWorkerPath.ts

--- packages/extension-source-maps/src/parts/GetExtensionSourceMapWorkerPath/GetExtensionSourceMapWorkerPath.ts
+++
@@ -1,6 +0,0 @@
-import { join } from 'node:path'
-import * as Root from '../Root/Root.ts'
-
-export const getExtensionSourceMapWorkerPath = (): string => {
-  return join(Root.root, 'packages', 'extension-source-maps', 'src', 'extensionSourceMapWorkerMain.ts')
-}
