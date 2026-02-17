91586a16b59898e00aaa0bcf04fe344443587ffc
Simon Siefke <simon.siefke@gmail.com>
2026-02-17 14:02:01 +0100

    debug

--- packages/cli/src/parts/HandleInitialized/HandleInitialized.ts
+++ packages/cli/src/parts/HandleInitialized/HandleInitialized.ts
@@ -1,16 +1,8 @@
 import * as AnsiEscapes from '../AnsiEscapes/AnsiEscapes.ts'
 import { getInitializedMessage } from '../GetInitializedMessage/GetInitializedMessage.ts'
-import { getInitializingMessage } from '../GetInitializingMessage/GetInitializingMessage.ts'
 import * as StdinDataState from '../StdinDataState/StdinDataState.ts'
 import * as Stdout from '../Stdout/Stdout.ts'
 import * as TestStateOutput from '../TestStateOutput/TestStateOutput.ts'
-
-const countRenderedLines = (text: string): number => {
-  if (!text) {
-    return 0
-  }
-  return text.split('\n').length - (text.endsWith('\n') ? 1 : 0)
-}
 
 // TODO use functional approach returning new stdout state property without side effects
 export const handleInitialized = async (time: number): Promise<void> => {
@@ -22,12 +14,7 @@ export const handleInitialized = async (time: number): Promise<void> => {
   const message = await getInitializedMessage(time)
   let fullMessage = ''
   if (capturedInitializationOutput) {
-    const initializingMessage = await getInitializingMessage()
-    const linesToClear = countRenderedLines(initializingMessage + capturedOutput)
-    if (linesToClear > 0) {
-      fullMessage += await AnsiEscapes.cursorUp(linesToClear)
-      fullMessage += await AnsiEscapes.eraseDown()
-    }
+    fullMessage += await AnsiEscapes.clear(StdinDataState.isWindows())
   } else if (StdinDataState.isWatchMode()) {
     fullMessage += await AnsiEscapes.clear(StdinDataState.isWindows())
   }
