1270161dd9e9ae403baec9ba3388a98cfc9f22db
Simon Siefke <simon.siefke@gmail.com>
2026-02-17 13:40:32 +0100

    newline

--- packages/stdout-worker/src/parts/GetHandleTestRunningMessage/GetHandleTestRunningMessage.ts
+++ packages/stdout-worker/src/parts/GetHandleTestRunningMessage/GetHandleTestRunningMessage.ts
@@ -4,6 +4,6 @@ import * as TestPrefix from '../TestPrefix/TestPrefix.ts'
 export const getHandleTestRunningMessage = (file: string, relativeDirName: string, fileName: string, isFirst: boolean): string => {
   const messageRelativeDirName: string = chalk.dim(relativeDirName + '/')
   const messageFileName: string = chalk.bold(fileName)
-  const prefix: string = isFirst ? '\n' : '\n'
+  const prefix: string = isFirst ? '' : '\n'
   return `${prefix}${TestPrefix.Runs} ${messageRelativeDirName}${messageFileName}\n`
 }
