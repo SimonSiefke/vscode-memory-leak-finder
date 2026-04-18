3eb997eb24ab9e86c4b11aaba10c971f9e6e0b31
Simon Siefke <simon.siefke@gmail.com>
2026-04-18 09:05:04 +0200

    feature: add platform-specific skip condition for darwin in toggle files (#2742)

--- packages/e2e/src/quick-pick-icon-theme.ts
+++ packages/e2e/src/quick-pick-icon-theme.ts
@@ -1,5 +1,7 @@
 import type { TestContext } from '../types.ts'
 
+export const skip = process.platform === 'darwin'
+
 const isThemeActionItem = (label: string): boolean => {
   return /additional file icon themes/i.test(label)
 }
