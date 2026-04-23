22b7175991756984ca8a9c7ef20eb60caa226626
Simon Siefke <simon.siefke@gmail.com>
2026-04-23 23:10:58 +0200

    skip

--- packages/e2e/src/timeline-view-git-history.ts
+++ packages/e2e/src/timeline-view-git-history.ts
@@ -1,6 +1,6 @@
 import type { TestContext } from '../types.ts'
 
-export const skip = false
+export const skip = true
 
 export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> => {
   await Workspace.setFiles([])
