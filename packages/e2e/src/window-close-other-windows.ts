f21247953e631ef30a5b5ae1acc1c1f963d911fb
Simon Siefke <simon.siefke@gmail.com>
2026-04-30 01:09:38 +0200

    feature: add window close other windows functionality for Electron tests

---
+++ packages/e2e/src/window-close-other-windows.ts
@@ -0,0 +1,19 @@
+import type { TestContext } from '../types.ts'
+
+export const skip = process.platform === 'darwin'
+
+export const run = async ({ Electron, QuickPick, TitleBar, WellKnownCommands, Window, Workbench }: TestContext): Promise<void> => {
+  await Workbench.shouldBeVisible()
+  await Electron.waitForWindowCount(1)
+
+  await TitleBar.showMenuFile()
+  await TitleBar.selectMenuItem(WellKnownCommands.NewWindow)
+
+  await Electron.waitForWindowCount(2)
+
+  await Window.focus()
+  await QuickPick.executeCommand(WellKnownCommands.CloseOtherWindows)
+
+  await Electron.waitForWindowCount(1)
+  await Workbench.shouldBeVisible()
+}
