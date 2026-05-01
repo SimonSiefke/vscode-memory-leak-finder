7b3407ac7409eb32c76731a0572957bb4459b492
Simon Siefke <simon.siefke@gmail.com>
2026-05-02 00:04:28 +0200

    feature: add Timeout interface and implementation for waitMinutes method

---
+++ packages/page-object/src/parts/Timeout/Timeout.ts
@@ -0,0 +1,16 @@
+import { setTimeout as delay } from 'node:timers/promises'
+
+import type { CreateParams } from '../CreateParams/CreateParams.ts'
+
+export const create = ({ VError, page }: CreateParams) => {
+  return {
+    async waitMinutes(minutes: number): Promise<void> {
+      try {
+        await page.waitForIdle()
+        await delay(minutes * 60 * 1000)
+      } catch (error) {
+        throw new VError(error, `Failed to wait ${minutes} minute(s)`)
+      }
+    },
+  }
+}
