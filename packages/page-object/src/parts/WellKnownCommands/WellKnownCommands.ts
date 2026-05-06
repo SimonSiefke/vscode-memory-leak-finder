6d1dcde90139c8f4723eca16dc8fe3f47b5a59be
Simon Siefke <simon.siefke@gmail.com>
2026-05-06 15:22:53 +0200

    fix

--- packages/page-object/src/parts/WellKnownCommands/WellKnownCommands.ts
+++ packages/page-object/src/parts/WellKnownCommands/WellKnownCommands.ts
@@ -1,5 +1,5 @@
 export const AddCursorBelow = 'Add Cursor Below'
-export const BrowserAddConsoleLogsToChat = 'Browser: Add Console Logs to Chat'
+export const BrowserAddConsoleLogsToChat = 'Add Console Logs to Chat'
 export const AutoFix = 'Auto Fix...'
 export const ChangeLanguageMode = 'Change Language Mode'
 export const ClearAllNotifications = 'Notifications: Clear All Notifications'
