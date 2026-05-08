725aa01ad3e2d3371dbb557e3c6f15832ab9997e
Simon Siefke <simon.siefke@gmail.com>
2026-05-08 15:53:32 +0200

    ssh

--- packages/e2e/src/ssh-server-reload.ts
+++ packages/e2e/src/ssh-server-reload.ts
@@ -1,6 +1,13 @@
 import type { TestContext } from '../types.ts'
 
-export const setup = async ({ SshServer, Workbench }: TestContext): Promise<void> => {
+export const setup = async ({ SshServer, Workbench, Extensions, Electron }: TestContext): Promise<void> => {
+  await Electron.mockDialog({
+    response: 1,
+  })
+  await Extensions.install({
+    id: 'ms-vscode-remote.remote-ssh',
+    name: 'Remote - SSH',
+  })
   const connection = await SshServer.launch()
   await Workbench.connectToSsh(connection)
   await SshServer.shouldBeConnected(connection)
