05884bbfe6494a9d3b0fac9c0ddb5895a7cca0e1
Simon Siefke <simon.siefke@gmail.com>
2026-05-08 17:14:51 +0200

    feat: add SSH server functionality and workbench reload tests

---
+++ packages/e2e/src/ssh-server-reload.ts
@@ -0,0 +1,21 @@
+import type { TestContext } from '../types.ts'
+
+export const setup = async ({ SshServer, Workbench, Extensions, Electron }: TestContext): Promise<void> => {
+  await Electron.mockDialog({
+    response: 1,
+  })
+  await Extensions.install({
+    id: 'ms-vscode-remote.remote-ssh',
+    name: 'Remote - SSH',
+  })
+  const connection = await SshServer.launch()
+  await Workbench.connectToSsh(connection)
+}
+
+export const run = async ({ Workbench }: TestContext): Promise<void> => {
+  await Workbench.reload()
+}
+
+export const teardown = async ({ SshServer }: TestContext): Promise<void> => {
+  await SshServer.dispose()
+}
