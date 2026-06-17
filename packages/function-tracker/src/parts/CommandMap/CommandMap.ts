339d2356d0a26a44a58b382c117d6adb57e65271
Simon Siefke <simon.siefke@gmail.com>
2026-06-17 23:10:27 +0200

    fix: reorder and update imports, enhance function tracking, and impro… (#2914)

--- packages/function-tracker/src/parts/CommandMap/CommandMap.ts
+++ packages/function-tracker/src/parts/CommandMap/CommandMap.ts
@@ -1,12 +1,12 @@
 import { getFunctionStatistics } from '../GetFunctionStatistics/GetFunctionStatistics.ts'
-import * as HttpServer from '../SocketServer/SocketServer.ts'
 import { preGenerateWorkbench } from '../PreGenerateWorkbench/PreGenerateWorkbench.ts'
+import * as HttpServer from '../SocketServer/SocketServer.ts'
 import { writeFunctionStatistics } from '../WriteFunctionStatistics/WriteFunctionStatistics.ts'
 
 export const commandMap = {
   'FunctionTracker.getFunctionStatistics': getFunctionStatistics,
+  'FunctionTracker.preGenerateWorkbench': preGenerateWorkbench,
   'FunctionTracker.startServer': HttpServer.startServer,
   'FunctionTracker.stopServer': HttpServer.stopServer,
-  'FunctionTracker.preGenerateWorkbench': preGenerateWorkbench,
   'FunctionTracker.writeFunctionStatistics': writeFunctionStatistics,
 }
