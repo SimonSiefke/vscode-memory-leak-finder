6b92db2ce9c3f7609adc3e319accc0cd19fa2166
Simon Siefke <simon.siefke@gmail.com>
2026-06-18 00:10:02 +0200

    fix

--- packages/test-coordinator/src/parts/FormatPaths/FormatPaths.ts
+++ packages/test-coordinator/src/parts/FormatPaths/FormatPaths.ts
@@ -1,6 +1,6 @@
 import * as FormatPath from '../FormatPath/FormatPath.ts'
 
 export const formatPaths = (cwd, testsPath, dirents) => {
-  const formattedPaths: any[] = Array.from(dirents, dirent => FormatPath.formatPath(cwd, testsPath, dirent));
+  const formattedPaths: any[] = Array.from(dirents, (dirent) => FormatPath.formatPath(cwd, testsPath, dirent))
   return formattedPaths
 }
