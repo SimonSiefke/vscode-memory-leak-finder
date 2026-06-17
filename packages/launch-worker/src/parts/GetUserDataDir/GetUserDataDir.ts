eb98533b48f8fe0111d89e8801e0a81c942bcadc
Simon Siefke <simon.siefke@gmail.com>
2026-06-17 08:50:16 +0200

    fix: use posix tmp path for macos user data dir

--- packages/launch-worker/src/parts/GetUserDataDir/GetUserDataDir.ts
+++ packages/launch-worker/src/parts/GetUserDataDir/GetUserDataDir.ts
@@ -1,9 +1,9 @@
-import { join } from 'node:path'
+import { join, posix } from 'node:path'
 import * as Root from '../Root/Root.ts'
 
 export const getUserDataDir = (platform: string = process.platform) => {
   if (platform === 'darwin') {
-    return join('/tmp', `vmlf-${process.pid}-ud`)
+    return posix.join('/tmp', `vmlf-${process.pid}-ud`)
   }
   return join(Root.root, '.vscode-user-data-dir')
 }
