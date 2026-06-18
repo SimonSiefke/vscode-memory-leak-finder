a998f04ee6a9bfd9d749b5d1a226608a0f381468
Simon Siefke <simon.siefke@gmail.com>
2026-06-18 09:50:11 +0200

    debug c

--- packages/e2e/src/debug-c.ts
+++ packages/e2e/src/debug-c.ts
@@ -16,6 +16,52 @@ int main() {
 `,
       name: 'main.c',
     },
+    {
+      content: JSON.stringify(
+        {
+          version: '0.2.0',
+          configurations: [
+            {
+              name: 'Debug C file',
+              type: 'cppdbg',
+              request: 'launch',
+              program: '${workspaceFolder}/main',
+              args: [],
+              stopAtEntry: false,
+              cwd: '${workspaceFolder}',
+              environment: [],
+              externalConsole: false,
+              MIMode: 'gdb',
+              miDebuggerPath: '/usr/bin/gdb',
+              preLaunchTask: 'Build C file',
+            },
+          ],
+        },
+        null,
+        2,
+      ),
+      name: '.vscode/launch.json',
+    },
+    {
+      content: JSON.stringify(
+        {
+          version: '2.0.0',
+          tasks: [
+            {
+              label: 'Build C file',
+              type: 'shell',
+              command: 'gcc',
+              args: ['-g', '${workspaceFolder}/main.c', '-o', '${workspaceFolder}/main'],
+              group: 'build',
+              problemMatcher: ['$gcc'],
+            },
+          ],
+        },
+        null,
+        2,
+      ),
+      name: '.vscode/tasks.json',
+    },
   ])
   await Extensions.install({
     id: 'ms-vscode.cpptools',
@@ -30,10 +76,11 @@ export const run = async ({ ActivityBar, Editor, RunAndDebug }: TestContext): Pr
   await ActivityBar.showRunAndDebug()
   await Editor.setBreakpoint(4)
   await RunAndDebug.runAndWaitForPaused({
-    debugLabel: ['gcc - Build and debug active file', 'C/C++: gcc build and debug active file'],
+    debugLabel: 'Debug C file',
     file: 'main.c',
     hasCallStack: false,
     line: 2,
+    viaIcon: true,
   })
   await RunAndDebug.stop()
   await RunAndDebug.removeAllBreakpoints()
