b4012d94b70bcc92cbfc109f08ac8bc7069bc8f0
Simon Siefke <simon.siefke@gmail.com>
2026-07-06 02:58:45 +0200

    feature: make tracked allocations chart more readable (#3011)

--- packages/charts/src/parts/GetTrackedAllocationsByFileData/GetTrackedAllocationsByFileData.ts
+++ packages/charts/src/parts/GetTrackedAllocationsByFileData/GetTrackedAllocationsByFileData.ts
@@ -2,6 +2,7 @@ import { existsSync } from 'node:fs'
 import { readdir } from 'node:fs/promises'
 import { join } from 'node:path'
 import { readJson } from '../ReadJson/ReadJson.ts'
+import { TrackedAllocationsChartLimit } from '../TrackedAllocationsChartLimit/TrackedAllocationsChartLimit.ts'
 
 type TrackedAllocation = {
   readonly collectedCount?: number
@@ -44,7 +45,7 @@ const getAllocationFileCounts = (trackedAllocations: readonly TrackedAllocation[
     existing.created += item.createdCount || 0
     countsBySource.set(name, existing)
   }
-  return [...countsBySource.values()].sort((a, b) => b.created - a.created || b.collected - a.collected).slice(0, 10_000)
+  return [...countsBySource.values()].sort((a, b) => b.created - a.created || b.collected - a.collected)
 }
 
 export const getTrackedAllocationsByFileData = async (basePath: string) => {
@@ -61,9 +62,12 @@ export const getTrackedAllocationsByFileData = async (basePath: string) => {
     const filePath = join(resultsPath, dirent)
     const rawData = await readJson(filePath)
     const trackedAllocations = getTrackedAllocations(rawData)
+    const fileData = getAllocationFileCounts(trackedAllocations)
+    const limitedData = fileData.slice(0, TrackedAllocationsChartLimit)
     allData.push({
-      data: getAllocationFileCounts(trackedAllocations),
+      data: limitedData,
       filename: dirent.replace('.json', ''),
+      omittedEntryCount: fileData.length - limitedData.length,
     })
   }
   return allData
