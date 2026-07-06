080de6ea367abf1f35c0b070c5ed963957e01b48
Simon Siefke <simon.siefke@gmail.com>
2026-07-06 21:06:04 +0000

    ci

--- packages/initialization-worker/src/parts/IsImportantErrorMessage/IsImportantErrorMessage.ts
+++ packages/initialization-worker/src/parts/IsImportantErrorMessage/IsImportantErrorMessage.ts
@@ -35,7 +35,7 @@ export const isImportantErrorMessage = (data: string): boolean => {
   if (data.includes(`For help, see: https://nodejs.org/en/docs/inspector`)) {
     return false
   }
-  if(data.includes(`For help, see: https://nodejs.org/learn/getting-started/debugging`)){
+  if (data.includes(`For help, see: https://nodejs.org/learn/getting-started/debugging`)) {
     return false
   }
   if (data.trim() === 'Debugger attached.') {
