61d76b2fbf6c0465c352602df2c8b608ad60a4a9
Simon Siefke <simon.siefke@gmail.com>
2026-07-18 09:30:46 +0000

    fix: disambiguate moved explorer file

--- packages/e2e/src/explorer-fs-move-file.ts
+++ packages/e2e/src/explorer-fs-move-file.ts
@@ -56,6 +56,7 @@ export const run = async ({ Explorer, Workspace }: TestContext): Promise<void> =
   await Explorer.refresh()
 
   // Verify file moved from source to destination
+  await Explorer.collapse('destination-folder')
   await Explorer.expand('source-folder')
   await Explorer.not.toHaveItem('file-in-source.txt')
   await Explorer.shouldHaveItem('nested-file.txt') // Other file should remain
