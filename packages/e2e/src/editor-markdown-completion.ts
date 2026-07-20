2e41dae5a7f44922e7ad14ab5a104cf1bd08dc44
Simon Siefke <simon.siefke@gmail.com>
2026-07-20 19:39:51 +0000

    feature: add markdown completion test

---
+++ packages/e2e/src/editor-markdown-completion.ts
@@ -0,0 +1,30 @@
+import type { TestContext } from '../types.js'
+
+export const skip = 1
+
+export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> => {
+  await Workspace.setFiles([
+    {
+      content: '# test',
+      name: 'index.md',
+    },
+  ])
+  await Editor.open('index.md')
+  await Editor.shouldHaveBreadCrumb('test')
+  await Editor.setCursor(1, 13)
+}
+
+export const run = async ({ Editor, Suggest }: TestContext): Promise<void> => {
+  await Suggest.open('visibility, Property')
+  await Suggest.close()
+  await Editor.deleteAll()
+  await Editor.shouldHaveText('')
+  await Editor.type('[test](#')
+  await Editor.shouldHaveText('h1 { visibil }')
+  await Editor.setCursor(1, 13)
+}
+
+export const teardown = async ({ Editor }: TestContext): Promise<void> => {
+  await Editor.save({ viaKeyBoard: true })
+  await Editor.closeAll()
+}
