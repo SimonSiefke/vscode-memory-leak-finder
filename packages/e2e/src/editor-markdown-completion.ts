14a900a3c8ebfd565e2691bfcc5e8809558fe339
Simon Siefke <simon.siefke@gmail.com>
2026-07-20 19:54:47 +0000

    md

--- packages/e2e/src/editor-markdown-completion.ts
+++ packages/e2e/src/editor-markdown-completion.ts
@@ -12,15 +12,15 @@ export const setup = async ({ Editor, Workspace }: TestContext): Promise<void> =
   ])
   await Editor.open('index.md')
   await Editor.shouldHaveBreadCrumb('# test')
-  await Editor.setCursor(2, 0)
+  await Editor.setCursor(2, 1)
 }
 
 export const run = async ({ Editor, Suggest }: TestContext): Promise<void> => {
-  await Suggest.open('visibility, Property')
-  await Suggest.close()
-  await Editor.deleteAll()
-  await Editor.shouldHaveText('')
-  await Editor.type('[test](#')
+  // await Suggest.open('visibility, Property')
+  // await Suggest.close()
+  // await Editor.deleteAll()
+  // await Editor.shouldHaveText('')
+  // await Editor.type('[test](#')
   await new Promise(r=>{})
   await Editor.shouldHaveText('h1 { visibil }')
   await Editor.setCursor(1, 13)
