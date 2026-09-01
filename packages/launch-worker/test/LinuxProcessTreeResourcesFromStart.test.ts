70c6881dc709b313ddb461207d296b863bf58e95
Simon Siefke <simon.siefke@gmail.com>
2026-09-01 21:07:56 +0000

    test: support Windows resource paths

--- packages/launch-worker/test/LinuxProcessTreeResourcesFromStart.test.ts
+++ packages/launch-worker/test/LinuxProcessTreeResourcesFromStart.test.ts
@@ -1,12 +1,12 @@
 import { expect, test } from '@jest/globals'
+import { basename } from 'node:path'
 import * as LinuxProcessTreeResourcesFromStart from '../src/parts/LinuxProcessTreeResourcesFromStart/LinuxProcessTreeResourcesFromStart.ts'
 
 test('enables launch-time collection for both public measure spellings', () => {
-  expect(LinuxProcessTreeResourcesFromStart.getConfig('linux-process-tree-resources-from-start', 7)).toMatchObject({
-    enabled: true,
-    metadataPath: expect.stringContaining('/7.json'),
-    perfOutputPath: expect.stringContaining('/7.perf.txt'),
-  })
+  const config = LinuxProcessTreeResourcesFromStart.getConfig('linux-process-tree-resources-from-start', 7)
+  expect(config.enabled).toBe(true)
+  expect(basename(config.metadataPath)).toBe('7.json')
+  expect(basename(config.perfOutputPath)).toBe('7.perf.txt')
   expect(LinuxProcessTreeResourcesFromStart.getConfig('linuxProcessTreeResourcesFromStart', 7).enabled).toBe(true)
 })
 
